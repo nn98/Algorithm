9e6767a589a4203ee70c9f020efc530e7f92e14e
NN98 <jkllhgb@gmail.com>
2019-05-23 15:52:22 +0900

    0523 P1159,P1357

--- src/BaekJoon/P1159.java
+++ src/BaekJoon/P1159.java
@@ -9,11 +9,14 @@ public class P1159 {
 		Scanner s=new Scanner(System.in);
 		int N=s.nextInt();
 		int[] arr=new int[26];
+		boolean g=true;
 		for(int i=0;i<N;i++) arr[s.next().charAt(0)-97]++;
 		for(int i=0;i<arr.length;i++) {
-			if(arr[i]>4)
+			if(arr[i]>4) {
 				System.out.printf("%c",i+97);
+				g=false;
+			}
 		}
+		if(g) System.out.print("PREDAJA");
 	}
-
 }
