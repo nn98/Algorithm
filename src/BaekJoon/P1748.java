e96fa0d3257b60db60dd8cd75412d5533eed2d6a
NN98 <jkllhgb@gmail.com>
2019-05-30 17:37:08 +0900

    0530 P1748 Xucking complication

--- src/BaekJoon/P1748.java
+++ src/BaekJoon/P1748.java
@@ -7,19 +7,22 @@ public class P1748 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
-		String[] n=s.next().split("");
-		int r=0,c=n.length;
-		if(c<1) {
-			for(int i=1;i<=Integer.parseInt(n[n.length-1]);i++) r++;
-		}
+		int m=s.nextInt();
+		String t=""+m;
+		String[] n=t.split("");
+		int r=0,c=n.length,k=1;
+		if(c<1) r=Integer.parseInt(t);
 		else {
-			for(int i=c-1;i>1;i--) r+=9*Math.pow(10, i-1);
-			for(int i=1;i<=Integer.parseInt(n[n.length-1]);i++) r++;
+//			System.out.println(r);
+			for(int i=1;i<c;i++) r+=9*i*(i>1?Math.pow(10, i-1):1);
+//			System.out.println(r);
+			m-=(int) Math.pow(10, t.length()-1)-1;
+//			System.out.println(m+" "+c);
+			r+=c*m;
 		}
 		//		r+=N%10+(N%100*2)+(N%1000*3)+(N%10000*4)+(N%100000*5)+(N%1000000*6)+(N%10000000*7)+(N%100000000*8);
 		//		9 180 2700 36000  
 		//		for(int j=0;j<)
 		System.out.println(r);
 	}
-
 }
