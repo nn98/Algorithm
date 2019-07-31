0b2618b24d50ac00490ee0c0e574e2120082e07a
nn98 <jkllhgb@gmail.com>
2019-07-31 14:44:15 +0900

    0731 - P10814 P11053 Alg Lec-P1260 P2606 P11725 P17362 P17363

--- src/BaekJoon/P11053.java
+++ src/BaekJoon/P11053.java
@@ -8,14 +8,25 @@ public class P11053 {
 	static int[] arr,history;
 
 	static int sol(int index,int sum) {
+		boolean c=true;
+		int big=index+1;
 		for(int i=index+1;i<arr.length;i++) {
 			if(arr[i]>arr[index]) {
-				int r=sol(i,sum);
-				history[index]=++r;
-				return history[index];
+				c=false;
+				big=arr[big]<=arr[i]?big:i;
 			}
 		}
-		history[index]=++sum;
+		if(!c) {
+			System.out.println("index "+index+", big "+big);
+			int r=sol(big,sum);
+			r++;
+			if(history[index]!=0)
+				history[index]=history[index]<r?r:history[index];
+			else
+				history[index]=r;
+		}
+		if(c)
+			history[index]=++sum;
 		return history[index];
 	}
 
@@ -27,8 +38,8 @@ public class P11053 {
 		history=new int[n];
 		for(int i=0;i<n;i++) arr[i]=s.nextInt();
 		for(int i=0;i<n;i++) sol(i,0);
-//		System.out.println(Arrays.toString(arr));
-//		System.out.println(Arrays.toString(history));
+				System.out.println(Arrays.toString(arr));
+				System.out.println(Arrays.toString(history));
 		int r=0;
 		for(int i=0;i<history.length;i++) if(history[i]>r)r=history[i];
 		System.out.println(r);
