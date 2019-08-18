38a62e850c17b38b49226d4961c3488a0147905a
nn98 <jkllhgb@gmail.com>
2019-08-19 04:44:36 +0900

    0819 QuickSort, P1009, P1094, P1252, P1874, P10828, P15668, P15688

--- src/BaekJoon/P15688.java
+++ src/BaekJoon/P15688.java
@@ -7,15 +7,47 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class P15688 {
+	//시간초과
+	static int arr[];
+	
+	static void swap(int i,int j) {
+//		????????????????
+//		System.out.println(arr[i]+" "+arr[j]);
+//		arr[i]=(arr[i]+arr[j]);
+//		System.out.println(arr[i]+" "+arr[j]);
+//		arr[j]=arr[i]-arr[j];
+//		System.out.println(arr[i]+" "+arr[j]);
+//		arr[i]=arr[i]-arr[j];
+//		System.out.println(arr[i]+" "+arr[j]);
+		int t=arr[i];
+		arr[i]=arr[j];
+		arr[j]=t;
+	}
+	
+	static int partition(int f,int t) {
+		int v=arr[t],i=f-1;
+		for(int j=f;j<=t-1;j++) if(arr[j]<v) swap(++i,j);
+		swap(i+1,t);
+		return i+1;
+	}
+	
+	static void qSort(int f,int t) {
+		if(f>=t) return;
+		int m=partition(f,t);
+		qSort(f,m-1);
+		qSort(m+1,t);
+	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N=Integer.parseInt(br.readLine()),arr[]=new int[N];
-        for(int i=0;i<N;i++) {
-        	
-        }
+        int N=Integer.parseInt(br.readLine());
+        arr=new int[N];
+        for(int i=0;i<N;i++) arr[i]=Integer.parseInt(br.readLine());
+//        System.out.println(Arrays.toString(arr));
+        qSort(0,N-1);
+//        System.out.println(Arrays.toString(arr));
+        for(int i=0;i<N;i++) System.out.println(arr[i]);
 	}
-
 }
---
+++ src/BaekJoon/P1874_4.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P1874_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),arr[]=new int[n],brr[]=new int[n],config=1,count=1,index=0;
+		for(int i=0;i<n;i++) arr[i]=s.nextInt();
+		while(config<n) {
+			if(count<arr[config]) {
+				
+			}
+		}
+	}
+
+}
