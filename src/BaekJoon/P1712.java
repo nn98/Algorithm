c622d48dde664526dbdff7d635728b3328b46678
nn98 <jkllhgb@gmail.com>
2019-09-29 04:52:29 +0900

    1712

--- src/BaekJoon/P1712.java
+++ src/BaekJoon/P1712.java
@@ -6,6 +6,7 @@ public class P1712 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		//== ¾Æ´Ï¶ó <=;
 		int a=s.nextInt(),b=-s.nextInt()+s.nextInt(),r=b==0?-1:a/b+1;
 		System.out.print(r);
 	}
