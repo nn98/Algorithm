0bda3b51a10437b070878436b521c45231a33d97
nn98 <jkllhgb@gmail.com>
2019-09-29 04:52:27 +0900

    1712

---
+++ src/BaekJoon/P1712.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P1712 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=-s.nextInt()+s.nextInt(),r=b==0?-1:a/b+1;
+		System.out.print(r);
+	}
+}
