8b27e1bb818b9b0b754649d54df5b72f11b18533
nn98 <jkllhgb@gmail.com>
2019-10-05 15:29:05 +0900

    10993

---
+++ src/BaekJoon/P10993.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+public class P10993 {
+
+	public static void main(String[] args) {
+		int n=new java.util.Scanner(System.in).nextInt();
+		if(n==1)System.out.println("*");
+		if(n==2) {
+			System.out.println("*****");
+			System.out.println(" *** ");
+			System.out.println("  *  ");
+		}
+	}
+
+}
