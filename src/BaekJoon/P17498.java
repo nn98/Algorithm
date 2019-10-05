d287227bad0030351a1438659ee34274f6d496cf
nn98 <jkllhgb@gmail.com>
2019-10-05 13:00:52 +0900

    17498

---
+++ src/BaekJoon/P17498.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P17498 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt(),y=s.nextInt(),d=s.nextInt(),r=0,a[][]=new int[x][y];
+		for(int i=0;i<x;i++)
+			for(int j=0;j<y;j++)a[i][j]=s.nextInt();
+		
+	}
+
+}
