c418eaaef605616ad94321052942dd0ec141fb6a
nn98 <jkllhgb@gmail.com>
2019-10-06 07:22:14 +0900

    10993 G

--- src/BaekJoon/P10993_2.java
+++ src/BaekJoon/P10993_2.java
@@ -5,7 +5,7 @@ public class P10993_2 {
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),m=1;
 		for(int i=1;i<n;i++)m+=Math.pow(2, i)*2;
-		System.out.println(n+" "+m);
+//		System.out.println(n+" "+m);
 	}
 
 }
