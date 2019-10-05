c8e66aeca239fb0ce4aadc2d032dc49811472a55
nn98 <jkllhgb@gmail.com>
2019-10-05 13:01:02 +0900

    17498

--- src/BaekJoon/P17498.java
+++ src/BaekJoon/P17498.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class P17498 {
 
+	static Scanner s=new Scanner(System.in);
+	static int x=s.nextInt(),y=s.nextInt(),d=s.nextInt(),r=0,a[][]=new int[x][y];
+	
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int x=s.nextInt(),y=s.nextInt(),d=s.nextInt(),r=0,a[][]=new int[x][y];
 		for(int i=0;i<x;i++)
 			for(int j=0;j<y;j++)a[i][j]=s.nextInt();
 		
