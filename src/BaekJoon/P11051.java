1a190dac012a759b3391ed4d75ab4e512c0d02d6
nn98 <jkllhgb@gmail.com>
2019-10-13 00:20:05 +0900

    11051

--- src/BaekJoon/P11051.java
+++ src/BaekJoon/P11051.java
@@ -1,11 +1,26 @@
 package BaekJoon;
 
-import java.util.*;
+import java.math.BigInteger;
+import java.util.Scanner;
 class P11051 {
 	public static void main(String[]Z) {
-		Scanner S = new Scanner(System.in);
-		int N = S.nextInt(), K = S.nextInt(), a = 1, b = 1, i = 0;
-		for(; i < K;b *= i)a *= N-i++;
-		System.out.print(a/b);
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),m=n-k;
+		if(k==0||m==0) {
+			System.out.println(1);
+			return;
+		}
+		if(m>k) {
+			int t=m;
+			m=k;
+			k=t;
+		}
+		BigInteger a=new BigInteger(""+n);
+//		System.out.println(n+" "+k+" "+m);
+		for(;--n>k;)a=a.multiply(new BigInteger(""+n));
+//		System.out.println(a);
+		for(;m>1;m--)a=a.divide(new BigInteger(""+m));
+		System.out.println(a);
+		System.out.print(a.mod(new BigInteger("100007")));
 	}
 }
