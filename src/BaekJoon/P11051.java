92521a3b893290dfc2dc56e504c60768dee45dc0
nn98 <jkllhgb@gmail.com>
2019-10-12 20:01:26 +0900

    11051

--- src/BaekJoon/P11051.java
+++ src/BaekJoon/P11051.java
@@ -1,10 +1,11 @@
 package BaekJoon;
 
-public class P11051 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+class P11051 {
+	public static void main(String[]Z) {
+		Scanner S = new Scanner(System.in);
+		int N = S.nextInt(), K = S.nextInt(), a = 1, b = 1, i = 0;
+		for(; i < K;b *= i)a *= N-i++;
+		System.out.print(a/b);
 	}
-
 }
