d80e2b1ec5c9a1f3a86964456b1de357fcc90154
nn98 <jkllhgb@gmail.com>
2019-10-12 20:00:39 +0900

    11050

---
+++ src/BaekJoon/P11050_S.java
@@ -0,0 +1,2 @@
+package BaekJoon;
+import java.util.*;class P11050_S{public static void main(String[]Z){Scanner S=new Scanner(System.in);int N=S.nextInt(),K=S.nextInt(),a=1,b=1,i=0;for(;i<K;b*=i)a*=N-i++;System.out.print(a/b);}}
