48bcd97cc5f6562d1fbe65083bb33cd343fc9e50
nn98 <jkllhgb@gmail.com>
2019-10-12 13:34:53 +0900

    1620

--- src/BaekJoon/P1620_S.java
+++ src/BaekJoon/P1620_S.java
@@ -1,5 +1,15 @@
 package BaekJoon;
-
-public class P1620_S {
-
+import java.util.*;
+class P1620_S{
+	public static void main(String[]z){
+		Scanner c=new Scanner(System.in);
+		HashMap<String,String>map=new HashMap<>();
+		int n=c.nextInt(),m=c.nextInt();
+		for(int i=1;i<=n;i++){
+			String s=c.next();
+			map.put(s, ""+i);
+			map.put(""+i,s);
+		}
+		for(int i=0;i<m;i++)System.out.println(map.get(c.next()));
+	}
 }
