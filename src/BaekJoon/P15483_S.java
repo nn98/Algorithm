7fd31c4d7a2877bb979ab4de78abb7bca79c365c
nn98 <jkllhgb@gmail.com>
2019-10-14 20:51:13 +0900

    15483 S

---
+++ src/BaekJoon/P15483_S.java
@@ -0,0 +1,3 @@
+package BaekJoon;
+
+import java.util.*;class P15483_S{public static void main(String[]z){Scanner s=new Scanner(System.in);String a=s.next(),b=s.next();int x=a.length()+1,y=b.length()+1,r[][]=new int[x][y],i=1,j=1;for(;i<x;i++)r[i][0]=i;for(i=1;i<y;i++)r[0][i]=i;for(i=1;i<x;i++){for(j=1;j<y;j++){if(a.charAt(i-1)==b.charAt(j-1))r[i][j]=r[i-1][j-1];else r[i][j]=Math.min(r[i-1][j-1],Math.min(r[i-1][j],r[i][j-1]))+1;}}System.out.print(r[x-1][y-1]);}}
