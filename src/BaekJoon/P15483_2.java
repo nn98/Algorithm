36a4f2c293d2d23e0e2196cddb70a0de07a36026
nn98 <jkllhgb@gmail.com>
2019-10-14 20:43:21 +0900

    15483

--- src/BaekJoon/P15483_2.java
+++ src/BaekJoon/P15483_2.java
@@ -1,10 +1,17 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P15483_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String a=s.next(),b=s.next();
+		int x=a.length()+1,y=b.length()+1,r[][]=new int[x][y];
+		for(int i=1;i<x;i++)r[i][0]=i;
+		for(int i=1;i<y;i++)r[0][i]=i;
+		for(int i=0;i<r.length;i++)System.out.println(Arrays.toString(r[i]));
 	}
 
 }
