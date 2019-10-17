a903d318a3bde37465db91eff9b150155bcadb0b
nn98 <jkllhgb@gmail.com>
2019-10-17 22:40:59 +0900

    1238

--- src/BaekJoon/P1238.java
+++ src/BaekJoon/P1238.java
@@ -1,9 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class P1238 {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in)
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()+1,m=s.nextInt(),x=s.nextInt(),a[][]=new int[n][n];
+		StringTokenizer st;
+		s.nextLine();
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(s.nextLine());
+			a[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]=Integer.parseInt(st.nextToken());
+			
+		}
 	}
-
 }
