51087ac300b76751bdc3e7806e676d4b51c2da0c
nn98 <jkllhgb@gmail.com>
2019-10-19 00:01:00 +0900

    14659

--- src/BaekJoon/P14659_U.java
+++ src/BaekJoon/P14659_U.java
@@ -6,7 +6,19 @@ public class P14659_U {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),v,c=0,m=0;
-		for(int )
+		int n=s.nextInt(),v=0,c=0,m=0,i=0;
+		for(;i<n;i++){
+			if(i<1)v=s.nextInt();
+			else {
+				int t=s.nextInt();
+				if(t<v)c++;
+				else{
+					v=t;
+					m=m>c?m:c;
+					c=0;
+				}
+			}
+		}
+		System.out.print(m>c?m:c);
 	}
 }
