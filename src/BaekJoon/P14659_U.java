ef4eaa77107753a92e90fa60cc120558dee4b3fc
nn98 <jkllhgb@gmail.com>
2019-10-18 22:31:07 +0900

    14659

--- src/BaekJoon/P14659_U.java
+++ src/BaekJoon/P14659_U.java
@@ -1,5 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P14659_U {
 
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),v,c=0,m=0;
+		for(int )
+	}
 }
