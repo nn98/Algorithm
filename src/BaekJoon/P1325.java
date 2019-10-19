b54e8f5470e193c98cb8c8c7a3bc1218ab3668a6
nn98 <jkllhgb@gmail.com>
2019-10-19 20:51:37 +0900

    1325

--- src/BaekJoon/P1325.java
+++ src/BaekJoon/P1325.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1325 {
-
+	static int n,a[][],m,x,y;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n][n];
+		for(m=s.nextInt();m>0;m--) {
+			x=s.nextInt()-1;
+			y=s.nextInt()-1;
+			a[x][y]++;
+			a[y][x]++;
+		}
 	}
-
 }
