8c2ab842044295322de2c784cb0fbee958d52fa2
nn98 <jkllhgb@gmail.com>
2019-10-19 21:48:36 +0900

    1325

---
+++ src/BaekJoon/P1325_G.java
@@ -0,0 +1,41 @@
+package BaekJoon;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P1325_G {
+
+	static ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[10001];
+	static boolean[]v;
+	static int[] ans = new int[10001];
+	static int cnt = 0;
+	
+	static void o(int i) {
+		v[i]=true;
+		for(int j:a[i]) {
+			if(!v[j]) {
+				ans[j]++;
+				o(j);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Scanner s=new Scanner(System.in);
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=s.nextInt(),m,i,j;
+		for(i=n;i>0;i--)a[i]=new ArrayList<Integer>();
+		for(m=s.nextInt();m>0;m--)a[s.nextInt()].add(s.nextInt());
+		m=0;
+		for(i=1;i<=n;i++) {
+			v=new boolean[10001];
+			o(i);
+		}
+		for(i=1;i<=n;i++)m=m>ans[i]?m:ans[i];
+		for(i=1;i<=n;i++)if(m==ans[i])w.write(i+" ");
+		w.flush();
+	}
+}
