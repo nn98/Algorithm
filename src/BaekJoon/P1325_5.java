0ceeff1ed432a3d8aaeaa973e66c2285ace91a0e
nn98 <jkllhgb@gmail.com>
2019-10-19 21:25:32 +0900

    1325

---
+++ src/BaekJoon/P1325_5.java
@@ -0,0 +1,40 @@
+package BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P1325_5 {
+	static int n,D[],p=0;
+	static boolean N[][],H[];
+	static void o(int i) {
+		H[i]=true;
+		int u=1;
+		for(int j=0;j<n;j++) {
+			if(N[i][j]) {
+				if(H[j])continue;
+				if(D[j]==0)o(j);
+				u+=D[j];
+			}
+		}
+		D[i]=u;
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		D=new int[n];
+		N=new boolean[n][n];
+		for(int m=s.nextInt();m>0;m--) {
+			int x=s.nextInt()-1,y=s.nextInt()-1;
+			N[y][x]=true;
+		}
+		for(int i=0;i<n;i++) {
+			H=new boolean[n];
+			if(D[i]==0)o(i);
+			p=p>D[i]?p:D[i];
+		}
+//		System.out.println(Arrays.toString(D));
+		for(int i=0;i<n;i++)
+			if(D[i]==p)
+				System.out.print(i+1+" ");
+	}
+}
