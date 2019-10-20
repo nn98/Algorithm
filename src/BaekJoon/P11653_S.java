0e565057e234d5d1a40c12f678877d0ea98c7048
nn98 <jkllhgb@gmail.com>
2019-10-20 22:19:32 +0900

    11653

---
+++ src/BaekJoon/P11653_S.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+class P11653_S {
+	public static void main(String[]z) {
+		int n=new java.util.Scanner(System.in).nextInt(),i=2;
+		for(;i<=n;i++) {
+			if(n%i==0) {
+				System.out.println(i);
+				n/=i--;
+			}
+		}
+	}
+
+}
