d2c06c5ff8d1e5206b3f7f918ca84720d3c222fa
nn98 <jkllhgb@gmail.com>
2019-10-20 23:21:09 +0900

    11945

--- src/BaekJoon/P11945_S.java
+++ src/BaekJoon/P11945_S.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P11945_S {
-
+	//hasNext 먹히네 프로그램종료 안따지는듯
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
