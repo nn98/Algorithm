66951409b5eef679ccdfcde17116c3b0376f9c1c
nn98 <jkllhgb@gmail.com>
2019-10-20 20:43:07 +0900

    1300

---
+++ src/BaekJoon/P1300.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P1300 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt();
+	}
+
+}
