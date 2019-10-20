d59d024c4779f53ea7815743c3df48a1c9b14381
nn98 <jkllhgb@gmail.com>
2019-10-20 23:16:43 +0900

    11945

--- src/BaekJoon/P11945_2.java
+++ src/BaekJoon/P11945_2.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P11945_2 {
-
+	//시발 런타임에러 뭐나는지만 알려줘도 알아먹겠는데 존나 에러임 ㅅㄱ ㅇㅈㄹ
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String[]in=s.nextLine().split(" ");
