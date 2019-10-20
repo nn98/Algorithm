71436771e379028bd4285edefa8687d5e29b2f75
nn98 <jkllhgb@gmail.com>
2019-10-20 22:29:26 +0900

    11586

---
+++ src/BaekJoon/P11586.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P11586 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,m;
+		StringBuilder[]a=new StringBuilder[n];
+		s.nextLine();
+		for(;i<n;i++)a[i]=new StringBuilder(s.nextLine());
+		switch(s.nextInt()) {
+		case 1:
+			for(i=0;i<n;i++)
+				System.out.println(a[i]);
+			break;
+		case 2:
+			for(i=0;i<n;i++)
+				System.out.println(a[i].reverse());
+			break;
+		case 3:
+			while(--i>=0)
+				System.out.println(a[i]);
+			break;
+		}
+	}
+
+}
