71acb4000ef46b096674dd98cf6002a12eddd4aa
nn98 <jkllhgb@gmail.com>
2019-10-20 22:59:58 +0900

    11945

--- src/BaekJoon/P11945.java
+++ src/BaekJoon/P11945.java
@@ -6,7 +6,7 @@ public class P11945 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),a[][]=new int[n][m],i=0,j;
+		int n=s.nextInt(),m=s.nextInt(),a[][]=new int[n][m],i=0,j=0;
 		for(;i<n;i++) {
 			j=0;
 			for(char c:s.next().toCharArray())
