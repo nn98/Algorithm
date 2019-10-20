17a6a5e006856bc84c6978f6b7dabb6c740afaf6
nn98 <jkllhgb@gmail.com>
2019-10-20 20:55:51 +0900

    1300

--- src/BaekJoon/P1300.java
+++ src/BaekJoon/P1300.java
@@ -1,12 +1,38 @@
 package BaekJoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class P1300 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt();
+		int n=s.nextInt(),k=s.nextInt(),i=1,x=1,y=1,t=1;
+		List<Integer>l=new ArrayList<>();
+		for(;i<=n*n;i++) {
+			t=x*y;
+			l.add(t);
+			if(i==k)break;
+//			System.out.println(t);
+//			if(i==k) {
+//				System.out.print(t);
+//				break;
+//			}
+//			if(x!=y)i++;
+//			if(i==k) {
+//				System.out.print(t);
+//				break;
+//			}
+			y++;
+			if(y>n) {
+				y=1;
+				x++;
+			}
+		}
+		Collections.sort(l);
+		System.out.print(l+" "+l.get(k-1));
 	}
 
 }
