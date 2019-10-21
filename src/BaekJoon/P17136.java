14d911a6ecce927329fffeae9ebefa489bc67615
nn98 <jkllhgb@gmail.com>
2019-10-22 00:23:25 +0900

    17136

--- src/BaekJoon/P17136.java
+++ src/BaekJoon/P17136.java
@@ -9,6 +9,7 @@ public class P17136 {
 	static boolean C=true;
 	static void o(int i,int j) {
 //		System.out.println(i+" "+j+" "+A[i][j]);
+		System.out.println(Arrays.toString(A[i]));
 		int t=1,d=0;
 		for(int a=0;a<4;a++) {
 			boolean c=true;
@@ -26,9 +27,11 @@ public class P17136 {
 			if(!c)break;
 			t++;
 		}
-		for(int x=i;x<=i+t-1&&x<10;x++)
-			for(int y=j;j<=j+t-1&&y<10;y++)
-				if(A[x][y]==1)A[x][y]=0;
+		for(int x=i;x<i+t&&x<10;x++)
+			for(int y=j;y<j+t&&y<10;y++)
+				A[x][y]=0;
+		System.out.println(Arrays.toString(A[i]));
+		System.out.println(Arrays.toString(A[i+1]));
 		r+=t;
 		if(k[t]==0) {
 			C=false;
@@ -36,19 +39,19 @@ public class P17136 {
 			return;
 		}
 		k[t]--;
-//		System.out.println(i+" "+j+" "+A[i][j]+" "+t);
+		System.out.println(i+" "+j+" "+A[i][j]+" "+t);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		for(int i=0;i<10;i++)
 			for(int j=0;j<10;j++)
 				A[i][j]=s.nextInt();
-		for(int i=0;i<10;i++)
-			for(int j=0;j<10;j++)
-				if(A[i][j]==1)
+		for(int i=0;i<10&&C;i++)
+			for(int j=0;j<10&&C;j++)
+				if(A[i][j]==1&&C)
 					o(i,j);
-//		for(int i=0;i<10;i++)
-//			System.out.println(Arrays.toString(A[i]));
+		for(int i=0;i<10;i++)
+			System.out.println(Arrays.toString(A[i]));
 		System.out.println(r);
 	}
 
