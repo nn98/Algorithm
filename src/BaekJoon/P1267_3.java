2938051abc1e5cc8ce8b3a9d09f1b8b4e4de8607
nn98 <jkllhgb@gmail.com>
2019-10-22 21:00:17 +0900

    1267

--- src/BaekJoon/P1267_3.java
+++ src/BaekJoon/P1267_3.java
@@ -1,5 +1,23 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1267_3 {
 
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],Y=0,M=0;
+		for(int i=0;i<n;i++)a[i]=s.nextInt();
+		for(int i=0;i<n;i++) {
+			Y+=(a[i]/30+1)*10;
+			M+=(a[i]/60+1)*15;
+//			System.out.println(Y+" "+M);
+		}
+		if(Y==M)
+			System.out.print("Y M "+Y);
+		else if(Y<M)
+			System.out.print("Y "+Y);
+		else
+			System.out.print("M "+M);
+	}
 }
