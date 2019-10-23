ae2065be2964f26757ae25572160d9de478a63f0
nn98 <jkllhgb@gmail.com>
2019-10-23 23:51:29 +0900

    11722

--- src/BaekJoon/P11722.java
+++ src/BaekJoon/P11722.java
@@ -1,10 +1,31 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P11722 {
 
-	static int n,a[],his[];
+	static int n,a[],h[],r=0;
+	static void o(int x,int u) {
+		if(h[x]>u)
+			return;
+		h[x]=u;
+		for(int i=x+1;i<n;i++) {
+			if(a[i]<a[x])
+				if(h[i]<=u)
+					o(i,u+1);
+		}
+		r=r>u?r:u;
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(int i=0;i<n;i++)a[i]=s.nextInt();
+		for(int i=0;i<n;i++)o(i,0);
+//		System.out.println(Arrays.toString(h));
+		System.out.print(r+1);
 	}
 
 }
