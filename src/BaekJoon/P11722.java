1c8d26364f8d6781f5fa810c3bb7ceb67e0db5fa
nn98 <jkllhgb@gmail.com>
2019-10-24 00:00:18 +0900

    11722

--- src/BaekJoon/P11722.java
+++ src/BaekJoon/P11722.java
@@ -3,27 +3,25 @@ package BaekJoon;
 import java.util.Scanner;
 public class P11722 {
 	//¿øÆ®
-	static int n,a[],h[],r=0;
-	static void o(int x,int u) {
-		if(h[x]>u)
-			return;
-		h[x]=u;
-		for(int i=x+1;i<n;i++) {
-			if(a[i]<a[x])
-				if(h[i]<=u)
-					o(i,u+1);
+	static int n,a[],h[],r=0,i=0;
+	static void o(int x,int u){
+		if(!(h[x]>u)){
+			h[x]=u;
+			for(i=x+1;i<n;i++){
+				if(a[i]<a[x])
+					if(h[i]<=u)
+						o(i,u+1);
+			}
+			r=r>u?r:u;
 		}
-		r=r>u?r:u;
 	}
-	public static void main(String[] args) {
+	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n];
 		h=new int[n];
-		for(int i=0;i<n;i++)a[i]=s.nextInt();
-		for(int i=0;i<n;i++)o(i,0);
-//		System.out.println(Arrays.toString(h));
+		for(;i<n;i++)a[i]=s.nextInt();
+		for(i=0;i<n;i++)o(i,0);
 		System.out.print(r+1);
 	}
-
 }
