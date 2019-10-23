fb65a38b026333e64875c35195d567fc36421686
nn98 <jkllhgb@gmail.com>
2019-10-23 23:54:27 +0900

    11722

---
+++ src/BaekJoon/P11722_U.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+public class P11722_U {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = Arrays.stream(new int[N]).map(i->sc.nextInt()).toArray(), dp = new int[N];
+
+		for(int i=0; i<N; i++)
+			for(int j=i-1; j>=0; j--)
+				if(a[i]<a[j] && dp[j]+1>dp[i])
+					dp[i]=dp[j]+1;
+
+		System.out.println(Arrays.stream(dp).max().getAsInt()+1);
+	}
+}
