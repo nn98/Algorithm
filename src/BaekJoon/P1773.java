74004d2b0bfbe9fec423583d894abf30298488c4
nn98 <jkllhgb@gmail.com>
2019-10-25 04:53:56 +0900

    1773

--- src/BaekJoon/P1773.java
+++ src/BaekJoon/P1773.java
@@ -1,10 +1,20 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1773 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),r=0;
+		boolean[]a=new boolean[m+1];
+		for(int i=0;i<n;i++) {
+			int t=s.nextInt(),j=t;
+			for(;j<=m;j+=t)
+				a[j]=true;
+		}
+		for(boolean c:a)if(c)r++;
+		System.out.print(r);
 	}
 
 }
