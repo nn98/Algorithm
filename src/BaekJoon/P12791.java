8f508d16cc4e32b43b1384b508a6f1d798ac9b88
nn98 <jkllhgb@gmail.com>
2019-10-25 05:10:21 +0900

    12791

--- src/BaekJoon/P12791.java
+++ src/BaekJoon/P12791.java
@@ -7,7 +7,7 @@ public class P12791 {
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j;
-		String[]a=("1967 DavidBowie.1969 SpaceOddity,1970 TheManWhoSoldTheWorld,1971 HunkyDory,1972 TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars,1973 AladdinSane,1973 PinUps,1974 DiamondDogs,1975 YoungAmericans,1976 StationToStation,1977 Low,1977 Heroes,1979 Lodger,1980 ScaryMonstersAndSuperCreeps,1983 LetsDance,1984 Tonight,1987 NeverLetMeDown,1993 BlackTieWhiteNoise,1995 1.Outside,1997 Earthling,1999 Hours,2002 Heathen,2003 Reality,2013 TheNextDay,2016 BlackStar").split(",");
+		String[]a=("1967 DavidBowie,1969 SpaceOddity,1970 TheManWhoSoldTheWorld,1971 HunkyDory,1972 TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars,1973 AladdinSane,1973 PinUps,1974 DiamondDogs,1975 YoungAmericans,1976 StationToStation,1977 Low,1977 Heroes,1979 Lodger,1980 ScaryMonstersAndSuperCreeps,1983 LetsDance,1984 Tonight,1987 NeverLetMeDown,1993 BlackTieWhiteNoise,1995 1.Outside,1997 Earthling,1999 Hours,2002 Heathen,2003 Reality,2013 TheNextDay,2016 BlackStar").split(",");
 		for(;i<n;i++) {
 			String r="\n";
 			int f=s.nextInt(),t=s.nextInt(),d,u=0;
