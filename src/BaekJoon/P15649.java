52e9b534680a253c4ba6f2ddac5f20373f37c7bb
nn98 <jkllhgb@gmail.com>
2019-10-25 04:46:21 +0900

    15649

--- src/BaekJoon/P15649.java
+++ src/BaekJoon/P15649.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P15649 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),f=1,i=2;
+		for(;i<=n;i++)f*=i;
+		for(i=n-m;i>1;i--)f/=i;
+		StringBuffer[]a=new StringBuffer[f];
+		
 	}
 
 }
