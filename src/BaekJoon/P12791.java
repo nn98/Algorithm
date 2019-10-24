98d432c0ae3e2143bb6861999c7eb01420a9d132
nn98 <jkllhgb@gmail.com>
2019-10-25 05:08:02 +0900

    12791

--- src/BaekJoon/P12791.java
+++ src/BaekJoon/P12791.java
@@ -4,34 +4,10 @@ import java.util.Scanner;
 
 public class P12791 {
 
-	public static void main(String[] args) {
+	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j;
-		String[]a=("1967 DavidBowie\r\n" + 
-				"1969 SpaceOddity\r\n" + 
-				"1970 TheManWhoSoldTheWorld\r\n" + 
-				"1971 HunkyDory\r\n" + 
-				"1972 TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars\r\n" + 
-				"1973 AladdinSane\r\n" + 
-				"1973 PinUps\r\n" + 
-				"1974 DiamondDogs\r\n" + 
-				"1975 YoungAmericans\r\n" + 
-				"1976 StationToStation\r\n" + 
-				"1977 Low\r\n" + 
-				"1977 Heroes\r\n" + 
-				"1979 Lodger\r\n" + 
-				"1980 ScaryMonstersAndSuperCreeps\r\n" + 
-				"1983 LetsDance\r\n" + 
-				"1984 Tonight\r\n" + 
-				"1987 NeverLetMeDown\r\n" + 
-				"1993 BlackTieWhiteNoise\r\n" + 
-				"1995 1.Outside\r\n" + 
-				"1997 Earthling\r\n" + 
-				"1999 Hours\r\n" + 
-				"2002 Heathen\r\n" + 
-				"2003 Reality\r\n" + 
-				"2013 TheNextDay\r\n" + 
-				"2016 BlackStar").replaceAll("\r","").split("\n");
+		String[]a=("1967 DavidBowie.1969 SpaceOddity,1970 TheManWhoSoldTheWorld,1971 HunkyDory,1972 TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars,1973 AladdinSane,1973 PinUps,1974 DiamondDogs,1975 YoungAmericans,1976 StationToStation,1977 Low,1977 Heroes,1979 Lodger,1980 ScaryMonstersAndSuperCreeps,1983 LetsDance,1984 Tonight,1987 NeverLetMeDown,1993 BlackTieWhiteNoise,1995 1.Outside,1997 Earthling,1999 Hours,2002 Heathen,2003 Reality,2013 TheNextDay,2016 BlackStar").split(",");
 		for(;i<n;i++) {
 			String r="\n";
 			int f=s.nextInt(),t=s.nextInt(),d,u=0;
@@ -42,8 +18,7 @@ public class P12791 {
 					u++;
 					r+=a[j]+"\n";
 				}
-				else if(d>t)
-					break;
+				else if(d>t)break;
 			}
 			System.out.print(u+r);
 		}
