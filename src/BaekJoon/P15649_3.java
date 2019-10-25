79e8e393dd5c593c212315b413ddf32e63a06217
nn98 <jkllhgb@gmail.com>
2019-10-26 05:56:13 +0900

    15649

--- src/BaekJoon/P15649_3.java
+++ src/BaekJoon/P15649_3.java
@@ -4,19 +4,19 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class P15649_2 {
+public class P15649_3 {
 
 	static int n,m,a[],i=1;
-	static List<StringBuffer>r;
+	static List<String>r;
 	
-	static void o(int x,StringBuffer s) {
+	static void o(int x,String s) {
 		if(x==n) {
 			if(s.length()==m*2)
 				r.add(s);
 			return;
 		}
-		o(x+1,new StringBuffer(s));
-		o(x+1,new StringBuffer(s.append(a[x]+" ")));
+		o(x+1,""+s);
+		o(x+1,s+" "+a[x]);
 	}
 	
 	public static void main(String[] args) {
@@ -26,8 +26,10 @@ public class P15649_2 {
 		a=new int[n];
 		r=new ArrayList<>();
 		for(;i<=n;i++)a[i-1]=i;
-		o(0,new StringBuffer());
-//		Collections.sort(r);
+		o(0,"");
+		Collections.sort(r);
+		for(String t:r)
+			System.out.println(t.substring(1));
 	}
 
 }
