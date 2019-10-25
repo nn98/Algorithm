03717f308a898305e874be1c9ffee90a3e3440ba
nn98 <jkllhgb@gmail.com>
2019-10-26 06:03:47 +0900

    15649

--- src/BaekJoon/P15649_3.java
+++ src/BaekJoon/P15649_3.java
@@ -13,10 +13,11 @@ public class P15649_3 {
 	static void o(int x,String s) {
 		if(x==n) {
 			if(s.length()==m*2) {
-				r.add(s);
-				if(s.split(" ").length>=2) {
+				r.add(s.substring(1));
+				r.add(new StringBuffer(s).reverse().toString());
+//				if(s.split(" ").length>=2) {
 //					r.add(new StringBuffer(s).reverse().toString());
-				}
+//				}
 			}
 			return;
 		}
@@ -35,7 +36,7 @@ public class P15649_3 {
 		o(0,"");
 		Collections.sort(r);
 		for(String t:r)
-			System.out.println(t.substring(1));
+			System.out.println(t);
 	}
 
 }
