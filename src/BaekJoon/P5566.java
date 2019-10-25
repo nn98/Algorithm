895b790421d5d419e9de2bb2d1672777cdf3696d
nn98 <jkllhgb@gmail.com>
2019-10-26 05:21:31 +0900

    5566

--- src/BaekJoon/P5566.java
+++ src/BaekJoon/P5566.java
@@ -1,5 +1,23 @@
 package BaekJoon;
 
-public class P5566 {
+import java.util.Scanner;
 
+public class P5566 {
+	public static void main(String[]z) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],w=0,i=0;
+		for(;i<n;i++)
+			a[i]=s.nextInt();
+		n--;
+		for(i=0;i<m&&w<n;i++) {
+			System.out.println(w);
+			w+=s.nextInt();
+			if(w>=n) {
+				i++;
+				break;
+			}
+			w+=a[w];
+		}
+		System.out.println(i);
+	}
 }
