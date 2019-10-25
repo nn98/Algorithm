5c7a888bc0a7b1db5ec1b32bb55a2e62b65c3483
nn98 <jkllhgb@gmail.com>
2019-10-26 05:40:48 +0900

    10539

--- src/BaekJoon/P10539.java
+++ src/BaekJoon/P10539.java
@@ -1,17 +1,14 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P10539 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=1,u=0,a=0;
-		for(;i<=n;i++) {
+		for(;i<=n;i++){
 			u=s.nextInt()*i;
 			System.out.print(u-a+" ");
 			a+=u-a;
 		}
 	}
-
 }
