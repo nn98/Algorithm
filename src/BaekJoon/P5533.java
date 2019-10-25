46f34b321528519cb383bb0872b3ac3f8840b078
nn98 <jkllhgb@gmail.com>
2019-10-26 05:02:52 +0900

    5533

--- src/BaekJoon/P5533.java
+++ src/BaekJoon/P5533.java
@@ -6,7 +6,7 @@ public class P5533 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[][]=new int[n][3],h[][]=new int[3][100],
+		int n=s.nextInt(),a[][]=new int[n][3],h[][]=new int[3][101],
 				r[]=new int[n],p;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<3;j++) {
@@ -15,6 +15,14 @@ public class P5533 {
 				h[j][p]++;
 			}
 		}
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<n;j++) {
+				p=a[j][i];
+				if(h[i][p]<2)
+					r[j]+=p;
+			}
+		}
+		for(int t:r)System.out.println(t);
 	}
 
 }
