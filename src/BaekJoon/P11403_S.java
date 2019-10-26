ff40175458331e45d920f00bebda7ac80b4129f1
nn98 <jkllhgb@gmail.com>
2019-10-26 20:17:48 +0900

    11403

--- src/BaekJoon/P11403_S.java
+++ src/BaekJoon/P11403_S.java
@@ -3,34 +3,19 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P11403_S {
-	static int n,a[][],i=0,j,h[];
-	static void o(int p,int q) {
-		if(h[q]>0)return;
-		h[q]++;
-		for(int k=0;k<n;k++) {
-			if(a[q][k]==1) {
-				a[p][k]=1;
-				o(p,k);
-			}
-		}
-	}
+	static int n,a[][],i=0,j,k=0,h[];
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n][n];
-
 		for(;i<n;i++)
 			for(j=0;j<n;j++)
 				a[i][j]=s.nextInt();
-		System.out.println();
-		for(i=0;i<n;i++) {
-			for(j=0;j<n;j++) {
-				if(a[i][j]==1) {
-					h=new int[n];
-					o(i,j);
-				}
-			}
-		}
+		for(;k<n;k++)
+			for(i=0;i<n;i++)
+				for(j=0;j<n;j++)
+					if(a[i][k]==1&&a[k][j]==1)
+						a[i][j]=1;
 		for(i=0;i<n;i++) {
 			String r="";
 			for(j=0;j<n;j++)
