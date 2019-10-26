2464bd9df49d79448bbbc672da030b7dfd58a350
nn98 <jkllhgb@gmail.com>
2019-10-26 19:31:41 +0900

    2942

--- src/BaekJoon/P2942_S.java
+++ src/BaekJoon/P2942_S.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P2942_S {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),m=a<b?a:b,i=0;
@@ -11,5 +9,4 @@ public class P2942_S {
 			if(a%i==0&&b%i==0)
 				System.out.println(i+" "+a/i+" "+b/i);
 	}
-
 }
