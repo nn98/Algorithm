55b5936850ded9b2dbb0b81b6f389626a94166d1
nn98 <jkllhgb@gmail.com>
2019-10-26 19:54:00 +0900

    11403

--- src/BaekJoon/P11403_2.java
+++ src/BaekJoon/P11403_2.java
@@ -1,5 +1,29 @@
 package BaekJoon;
 
-public class P11403_2 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class P11403_2 {
+	static int n,a[][],i=0,j,k;
+	public static void main(String[]z) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n][n];
+		for(;i<n;i++) {
+			for(j=0;j<n;j++) {
+				a[i][j]=s.nextInt();
+				for(k=0;k<n;k++)
+					if(a[j][k]==1)
+						a[i][k]=1;
+			}
+		}
+		for(i=0;i<n;i++)
+			System.out.println(Arrays.toString(a[i]));
+//		for(i=0;i<n;i++)
+//			for(j=0;j<n;j++)
+//				if(a[i][j]==1)
+//					o(i,j);
+		for(i=0;i<n;i++)
+			System.out.println(Arrays.toString(a[i]));
+	}
 }
