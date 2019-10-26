64f205e7fdccfc97c930ae34bfa69761d6258442
nn98 <jkllhgb@gmail.com>
2019-10-26 19:45:15 +0900

    11403

--- src/BaekJoon/P11403.java
+++ src/BaekJoon/P11403.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 
 public class P11403 {
-	static int n,a[][],i=0,j=0;
+	static int n,a[][],i=0,j;
 	static void o(int f,int w) {
-		if(f==w)return;
+		System.out.println(f+" "+w);
+		if(f==w||a[w][f]==1)return;
 		for(int k=0;k<n;k++) {
 			if(a[w][k]==1) {
 				a[f][k]=1;
@@ -19,8 +20,8 @@ public class P11403 {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n][n];
-		for(int i=0;i<n;i++)
-			for(int j=0;j<n;j++)
+		for(;i<n;i++)
+			for(j=0;j<n;j++)
 				a[i][j]=s.nextInt();
 		for(i=0;i<n;i++)
 			System.out.println(Arrays.toString(a[i]));
