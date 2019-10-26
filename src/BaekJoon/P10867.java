61646fab77602dca675c309721949f9196dc05b9
nn98 <jkllhgb@gmail.com>
2019-10-26 20:23:35 +0900

    10867

--- src/BaekJoon/P10867.java
+++ src/BaekJoon/P10867.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10867 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[2001];
+		for(;n>0;n--)
+			a[s.nextInt()+1000]++;
+		for(n=0;n<2001;n++)
+			if(a[n]>0)
+				System.out.print(n-1000+" ");
+		
 	}
 
 }
