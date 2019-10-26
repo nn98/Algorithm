6592890ccbcf29f7a9275a021529d9c6ee726609
nn98 <jkllhgb@gmail.com>
2019-10-26 20:30:37 +0900

    13235

---
+++ src/BaekJoon/P13235.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+public class P13235 {
+
+	public static void main(String[] args) {
+		String t=new java.util.Scanner(System.in).next(),r="true";
+		int i=0,l=t.length();
+		for(;i<l/2;i++)
+			if(t.charAt(i)!=t.charAt(l-i))
+				r="false";
+		System.out.print(r
+				);
+	}
+
+}
