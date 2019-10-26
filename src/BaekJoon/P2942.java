82fa5d68fc920155fc6350022d4a6f714d0181c3
nn98 <jkllhgb@gmail.com>
2019-10-26 19:26:51 +0900

    2942

---
+++ src/BaekJoon/P2942.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P2942 {
+
+	static int g(int a,int b) {
+		int t;
+		if(a>b) {
+			t=a;
+			a=b;
+			b=t;
+		}
+		while(a!=0) {
+			t=b%a;
+			b=a;
+			a=t;
+		}
+		return b;
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int R=s.nextInt(),G=s.nextInt(),m=g(R,G),i=0;
+		while(i++<=m)
+			if(m%i==0)
+				System.out.println(i+" "+R/i+" "+G/i);
+	}
+
+}
