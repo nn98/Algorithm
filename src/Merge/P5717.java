a33540a1c8683dba6ef80253aa1acc86b1409bcd
nn98 <38521355+nn98@users.noreply.github.com>
2019-11-07 15:36:14 +0900

    1107 By Lecture, For Merge

---
+++ src/Merge/P5717.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P5717 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b;
+		while(0!=(a=s.nextInt())&&0!=(b=s.nextInt())) {
+			System.out.println(a+b);
+		}
+	}
+}
