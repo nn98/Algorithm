ff99a81a2cfd815d43ca183b995d085fb8f43c61
nn98 <38521355+nn98@users.noreply.github.com>
2019-11-07 15:35:18 +0900

    1107 By Lecture, For Merge

---
+++ src/Merge/P4101.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P4101 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b;
+		while(0!=(a=s.nextInt())&&0!=(b=s.nextInt())) {
+			System.out.println(a>b?"Yes":"No");
+		}
+	}
+}
