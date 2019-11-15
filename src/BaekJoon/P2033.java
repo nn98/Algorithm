246f62832c4fc0404dd3f3b805e9999fa0dc5bb2
nn98 <jkllhgb@gmail.com>
2019-11-15 10:02:53 +0900

    2033

--- src/BaekJoon/P2033.java
+++ src/BaekJoon/P2033.java
@@ -3,10 +3,15 @@ package BaekJoon;
 public class P2033 {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),i=10;
+		int n=new java.util.Scanner(System.in).nextInt(),i=10,j,m;
 		for(;i<=n;i*=10) {
-			
+			m=n%i;
+			j=m/(i/10);
+			if(j>4)
+				n+=i;
+			n-=m;
 		}
+		System.out.print(n);
 	}
 
 }
