f1e978d09d18854407d725718e78a7e8129f4202
nn98 <jkllhgb@gmail.com>
2019-11-15 10:15:27 +0900

    2033

--- src/BaekJoon/P2033.java
+++ src/BaekJoon/P2033.java
@@ -6,14 +6,16 @@ public class P2033 {
 		int n=new java.util.Scanner(System.in).nextInt(),i=10,j,m;
 		if(n<10&&n>4)
 			n=10;
-		for(;i/10<n;i*=10) {
+		for(;i<n;i*=10) {
 			m=n%i;
 			j=m/(i/10);
 			if(j>4)
 				n+=i;
 			n-=m;
-			System.out.println(n);
+//			System.out.println(n+" "+i);
 		}
+		if(n/(i/10)>4)
+			n=i;
 		System.out.print(n);
 	}
 }
