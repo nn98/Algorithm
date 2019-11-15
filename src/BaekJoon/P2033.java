a0fbae96c74316d160f6a73f613322f4b1fa7116
nn98 <jkllhgb@gmail.com>
2019-11-15 11:29:02 +0900

    2033

--- src/BaekJoon/P2033.java
+++ src/BaekJoon/P2033.java
@@ -4,8 +4,8 @@ public class P2033 {
 	//문제를 제대로 읽자
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=10,j,m;
-		if(n<10&&n>4)
-			n=10;
+//		if(n<10&&n>4)
+//			n=10;
 		for(;i<n;i*=10) {
 			m=n%i;
 			j=m/(i/10);
