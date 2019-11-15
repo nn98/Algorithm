0efa0ed8817535f2fdebad9b4a76c067987817cc
nn98 <jkllhgb@gmail.com>
2019-11-15 11:51:00 +0900

    9093

--- src/BaekJoon/P9093.java
+++ src/BaekJoon/P9093.java
@@ -1,10 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P9093 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j;
+		s.nextLine();
+		for(;i<n;i++) {
+			String[]a=s.nextLine().split(" ");
+			for(String b:a)
+				for(j=b.length()-1;j>=0;j--)
+					System.out.println(b.charAt(j)+j==0?" ":"");
+		}
 	}
 
 }
