3362216daee1d16c759cb4393d2d9a315ea17441
nn98 <jkllhgb@gmail.com>
2019-11-19 16:10:54 +0900

    1918

--- src/BaekJoon/P1918_4.java
+++ src/BaekJoon/P1918_4.java
@@ -2,6 +2,7 @@ package BaekJoon;
 
 public class P1918_4 {
 	static N t=null;
+	private static String i;
 	static class N {
 		String v;
 		N N;
@@ -37,17 +38,35 @@ public class P1918_4 {
 //				r=new StringBuffer();
 		String r="",s[]=new java.util.Scanner(System.in).next().split("");
 		for(String i:s) {
+//			System.out.println("i: "+i);
 			switch(O(i)) {
 			case 0:
+				if(i.equals("("))
+					new N(i);
+				else {
+					while(!t.peek().equals("("))
+						System.out.print(t.pop());
+//					t.pop();
+				}
 				break;
 			case 1:
+				if(t!=null&&O(t.peek())<1)
+					System.out.print(t.pop());
+				else
+					new N(i);
 				break;
 			case 2:
+				if(t!=null&&O(t.peek())<2)
+					System.out.print(t.pop());
+				else
+					new N(i);
 				break;
 			default:
-				r+=i;
+				System.out.print(i);
 			}
 		}
+		while(t!=null)
+			System.out.print(t.pop());
 	}
 
 }
