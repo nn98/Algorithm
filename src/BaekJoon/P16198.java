f4595c659999183a3f0812348d26af9f2696e123
nn98 <jkllhgb@gmail.com>
2019-11-19 15:39:02 +0900

    16198

--- src/BaekJoon/P16198.java
+++ src/BaekJoon/P16198.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P16198 {
 
+	static int n,a[],h[],i=0,r=0;
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		System.out.println(Arrays.toString(a));
 	}
 
 }
