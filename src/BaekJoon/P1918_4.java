c782668861065309b11f511476d005bdc004eeaf
nn98 <jkllhgb@gmail.com>
2019-11-19 15:54:54 +0900

    1918

--- src/BaekJoon/P1918_4.java
+++ src/BaekJoon/P1918_4.java
@@ -3,8 +3,8 @@ package BaekJoon;
 public class P1918_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringBuffer s=new StringBuffer(new java.util.Scanner(System.in).next());
+		System.out.println(s);
 	}
 
 }
