8e7b93359ef05c6638c95ba03aebe313bbb6a2da
nn98 <jkllhgb@gmail.com>
2019-11-19 15:37:00 +0900

    1188

--- src/BaekJoon/P1188_S.java
+++ src/BaekJoon/P1188_S.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P1188_S {
 
 	public static void main(String[] args) {
+		//실제를 상상하며 계산
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=b,d;
 		for(;;) {
