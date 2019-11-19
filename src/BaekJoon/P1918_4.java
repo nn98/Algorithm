e0b1f5d8671bdcc830c9c27e99ab2fb93ee35a16
nn98 <jkllhgb@gmail.com>
2019-11-19 15:55:00 +0900

    1918

--- src/BaekJoon/P1918_4.java
+++ src/BaekJoon/P1918_4.java
@@ -1,10 +1,34 @@
 package BaekJoon;
 
 public class P1918_4 {
-
+	static N t=null;
+	static class N {
+		String v;
+		N N;
+		N(String v){
+			this.v=v;
+			this.N=t;
+			t=this;
+		}
+		String peek() {
+			return v;
+		}
+		String pop() {
+			String v=this.v;
+			t=t.N;
+			return v;
+		}
+		boolean isE() {
+			return N==null;
+		}
+	}
 	public static void main(String[] args) {
-		StringBuffer s=new StringBuffer(new java.util.Scanner(System.in).next());
-		System.out.println(s);
+//		StringBuffer s=new StringBuffer(new java.util.Scanner(System.in).next()),
+//				r=new StringBuffer();
+		String r,s[]=new java.util.Scanner(System.in).next().split("");
+		for(String i:s)new N(i);
+		while(t.isE())
+			System.out.println(t.pop());
 	}
 
 }
