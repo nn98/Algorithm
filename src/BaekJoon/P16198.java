3de22d4ea4d2145a8db53d6e7e1a7b6d24b7cba6
nn98 <jkllhgb@gmail.com>
2019-11-19 15:45:25 +0900

    16198

--- src/BaekJoon/P16198.java
+++ src/BaekJoon/P16198.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class P16198 {
 
-	static int n,a[],h[],i=0,r=0;
+	static int n,a[],h[],i=0,r=0,m=0;
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n];
 		h=new int[n];
-		for(;i<n;a[i++]=s.nextInt());
+		for(;i<n;a[i]=s.nextInt(),m=a[m]>a[i++]?m:i-1);
+		System.out.println(a[m]+" "+m);
 	}
 
 }
