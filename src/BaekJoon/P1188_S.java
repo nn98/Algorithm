9082f0b11937c8a8c95db9a133f10a64838921a8
nn98 <jkllhgb@gmail.com>
2019-11-19 15:29:15 +0900

    1188

--- src/BaekJoon/P1188_S.java
+++ src/BaekJoon/P1188_S.java
@@ -1,10 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1188_S {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=b,d;
+		for(;;) {
+			d=a%b;
+			if(d==0)System.out.println(c-b);
+			a=b;
+			b=d;
+		}
 	}
-
 }
