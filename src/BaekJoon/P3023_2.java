cb488734afce63c59febd1ebecbe15ded2711ef6
nn98 <jkllhgb@gmail.com>
2019-12-07 23:03:34 +0900

    3023

---
+++ src/BaekJoon/P3023_2.java
@@ -0,0 +1,21 @@
+package Merge;
+
+import java.util.Scanner;
+
+public class P3023_2 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt(),y=s.nextInt(),i=0,p,q,j;
+		char[][]a=new char[x][];
+		s.nextLine();
+		for(;i<x;i++)a[i]=s.nextLine().toCharArray();
+		p=s.nextInt()%x-1;
+		q=s.nextInt()%y-1;
+		for(i=0;i<x;i++) {
+			for(j=0;j<y;j++) {
+//				if()
+//				System.out.print();
+			}
+		}
+	}
+}
