a5327179b737fa696c24be8fd8bea19ff818df5d
nn98 <jkllhgb@gmail.com>
2019-12-07 23:03:22 +0900

    2909

---
+++ src/BaekJoon/P2909.java
@@ -0,0 +1,15 @@
+package Merge;
+
+import java.util.Scanner;
+
+public class P2909 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int p=s.nextInt(),c=s.nextInt(),d=(int) Math.pow(10,c),
+				a=p%d,b=d-a;
+//		System.out.println(a+" "+b);
+		System.out.print(a<b?p-a:p+b);
+	}
+
+}
