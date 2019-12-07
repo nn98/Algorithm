ee83ef6ea63cb33b3e0525116dd1c6bc6e5005c4
nn98 <jkllhgb@gmail.com>
2019-12-07 22:58:23 +0900

    1568

--- src/BaekJoon/P1568.java
+++ src/BaekJoon/P1568.java
@@ -1,12 +1,9 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
 public class P1568 {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=1,j=0;
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=0;
 		for(;n!=0;j++) {
 			if(i>n)i=1;
 			n-=i++;
