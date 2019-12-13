a81789103e42d7329147347767c5376fda96e5dc
nn98 <jkllhgb@gmail.com>
2019-12-13 20:47:17 +0900

    15857

---
+++ src/BaekJoon/P15857.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+public class P15857 {
+
+	public static void main(String[] args) {
+		char r;
+		switch(new java.util.Scanner(System.in).nextInt()) {
+		case 1:
+			r=97;
+			break;
+		case 2:
+			r=98;
+			break;
+		case 3:
+			r=99;
+			break;
+		case 4:
+			r=99;
+			break;
+		case 5:
+			r=100;
+			break;
+		case 6:
+			r=100;
+			break;
+		case 7:
+			r=100;
+			break;
+		case 8:
+			r=99;
+			break;
+		}
+	}
+}
