c3f6329558cfc02bd19aff818e6320e56f32eacd
nn98 <jkllhgb@gmail.com>
2019-12-24 02:49:11 +0900

    2355

--- src/BaekJoon/P2355_2.java
+++ src/BaekJoon/P2355_2.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2355_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		long a=s.nextLong(),b=s.nextLong(),t;
+		if(a>b) {
+			t=a;
+			a=b;
+			b=t;
+		}
+		System.out.print((a+b)*(b-a+1)/2);
 	}
-
 }
