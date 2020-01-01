153a5d2e9815cceb1c2338c496ad65546c7e5a4b
nn98 <jkllhgb@gmail.com>
2020-01-01 12:44:31 +0900

    10768

--- src/BaekJoon/P10768.java
+++ src/BaekJoon/P10768.java
@@ -1,13 +1,10 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P10768 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int m=s.nextInt(),d=s.nextInt();
 		System.out.print(m==2?(d==18?"Special":(d<18?"Before":"After")):(m<2?"Before":"After"));
 	}
-
 }
