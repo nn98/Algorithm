89cb63fc243d7d5e7de23f6033418c46e829e616
nn98 <jkllhgb@gmail.com>
2020-01-01 12:47:33 +0900

    11098

--- src/BaekJoon/P11098.java
+++ src/BaekJoon/P11098.java
@@ -1,7 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P11098 {
 	public static void main(String[]z) {
-		
+		Scanner s=new Scanner(System.in);
+		for(int n=s.nextInt();n-->0;) {
+			
+		}
 	}
 }
