7a3b75a80026d89d3a20556d2cb908467811194b
nn98 <jkllhgb@gmail.com>
2020-01-01 12:38:16 +0900

    10707

--- src/BaekJoon/P10707.java
+++ src/BaekJoon/P10707.java
@@ -1,10 +1,14 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10707 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),p=s.nextInt()
+				,X=a*p,Y=p<=c?b:b+(p-c)*d;
+		System.out.print(X<Y?X:Y);
+		
 	}
-
 }
