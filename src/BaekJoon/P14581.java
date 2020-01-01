66f054a6f15adf1f65d017510d04233182f35275
nn98 <jkllhgb@gmail.com>
2020-01-01 12:06:18 +0900

    14581

--- src/BaekJoon/P14581.java
+++ src/BaekJoon/P14581.java
@@ -4,7 +4,7 @@ public class P14581 {
 
 	public static void main(String[] args) {
 		String s=":fan:";
-		System.out.print(s+s+s+"\n"+s+new java.util.Scanner(System.in).next()+s+"\n"+s+s+s);
+		System.out.print(s+s+s+"\n"+s+":"+new java.util.Scanner(System.in).next()+":"+s+"\n"+s+s+s);
 	}
 
 }
