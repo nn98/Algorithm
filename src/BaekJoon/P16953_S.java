b29e7b808f47dc56172a81e60154bab26c068e98
nn98 <jkllhgb@gmail.com>
2020-01-01 12:23:42 +0900

    16953

--- src/BaekJoon/P16953_S.java
+++ src/BaekJoon/P16953_S.java
@@ -2,24 +2,20 @@ package BaekJoon;
 import java.util.*;
 class P16953_S{
 	public static void main(String[]z){
-		Scanner sc = new Scanner(System.in);
-		int cnt =1;
-		long A =sc.nextLong();
+		Scanner sc=new Scanner(System.in);
+		int cnt=1;
+		long A=sc.nextLong();
 		long B=sc.nextLong();
-
-		while(B>A) {
+		while(B>A){
 			cnt+=1;
-			if(B%2==0) {
+			if(B%2==0){
 				B=B>>1;
-			}else if((B-1)%10==0) B=B/10;
-			else {
+			}else if((B-1)%10==0)B=B/10;
+			else{
 				cnt=-1;
 				break;
 			}
 		}
-
-		System.out.println(A==B?cnt:-1);
-
+		System.out.print(A==B?cnt:-1);
 	}
-
 }
