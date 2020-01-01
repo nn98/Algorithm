2dc4ae9bb66c2461e3f802c4213e668a949dbdee
nn98 <jkllhgb@gmail.com>
2020-01-01 11:56:59 +0900

    1964

--- src/BaekJoon/P1964.java
+++ src/BaekJoon/P1964.java
@@ -6,11 +6,11 @@ public class P1964 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),r=5,a=5,b=2;
+		long n=s.nextInt(),r=5,a=3,b=1;
 		for(;--n>0;) {
-			r+=(a+b++);
+			r+=a++*2+b++;
+			r%=45678;
 		}
 		System.out.println(r);
 	}
-
 }
