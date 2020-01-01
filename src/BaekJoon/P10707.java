81fdd537a34fa9e7e7928d1e50ddcfad06a81f5a
nn98 <jkllhgb@gmail.com>
2020-01-01 12:40:07 +0900

    10707

--- src/BaekJoon/P10707.java
+++ src/BaekJoon/P10707.java
@@ -6,8 +6,7 @@ public class P10707 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),p=s.nextInt()
-				,X=a*p,Y=p<=c?b:b+(p-c)*d;
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),p=s.nextInt(),X=a*p,Y=p<=c?b:b+(p-c)*d;
 		System.out.print(X<Y?X:Y);
 		
 	}
