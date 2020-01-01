62d9cff297f3d1dd2e8b21a37c1675f87eeafa0d
nn98 <jkllhgb@gmail.com>
2020-01-01 12:30:21 +0900

    10769

--- src/BaekJoon/P10769.java
+++ src/BaekJoon/P10769.java
@@ -3,8 +3,12 @@ package BaekJoon;
 public class P10769 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[]a=new java.util.Scanner(System.in).next().split("");
+		int i=0,h=0,s=0;
+		for(;i<a.length;i++)
+			if(a[i].equals(":")&&a[i+1].equals("-"))
+				if(a[i+2].equals(")"))h++;
+				else if(a[i+2].equals(")"))s++;
+		System.out.print((h==0&&s==0)?"none":(h==s?"unsure":(h>s?"happy":"sad")));
 	}
-
 }
