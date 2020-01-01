1c6d701d737412d829fa006d98d681308511ee1a
nn98 <jkllhgb@gmail.com>
2020-01-01 12:49:23 +0900

    11098

--- src/BaekJoon/P11098.java
+++ src/BaekJoon/P11098.java
@@ -6,7 +6,17 @@ public class P11098 {
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
 		for(int n=s.nextInt();n-->0;) {
-			
+			int i=0;
+			String t="";
+			for(int m=s.nextInt();m-->0;) {
+				int j=s.nextInt();
+				String f=s.next();
+				if(j>i) {
+					i=j;
+					t=f;
+				}
+			}
+			System.out.println(t);
 		}
 	}
 }
