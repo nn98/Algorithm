c132d4903213905a4a697c7b78eda300b46c909c
nn98 <jkllhgb@gmail.com>
2020-01-01 12:14:50 +0900

    16953

--- src/BaekJoon/P16953.java
+++ src/BaekJoon/P16953.java
@@ -1,9 +1,25 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P16953 {
 
+	static int f,t,r=-1;
+	static void o(String x,int c) {
+		int X=Integer.parseInt(x);
+		if(X==t)
+			r=r==-1?c:r<c?r:c;
+		if(X>t)
+			return;
+		o(""+X*2,c+1);
+		o(x+"1",c+1);
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		f=s.nextInt();
+		t=s.nextInt();
+		o(""+f,0);
+		System.out.print(r);
 	}
 
 }
