caa73babb3016d502e4e765f29e72c1c1cdcc1d2
nn98 <jkllhgb@gmail.com>
2020-01-01 12:58:29 +0900

    11062

--- src/BaekJoon/P11062.java
+++ src/BaekJoon/P11062.java
@@ -1,10 +1,27 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P11062 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,a[];
+		for(;n-->0;) {
+			m=s.nextInt();
+			a=new int[m];
+			for(int i=0;i<m;a[i++]=s.nextInt());
+			int f=0,t=m-1,x=0,y=0;
+			for(int i=0;i<m;i++) {
+				int v=0;
+				if(a[f]>a[t])v=a[f++];
+				else v=a[t--];
+				if(i%2==0)
+					x+=v;
+				else y+=v;
+			}
+			System.out.print(y);
+		}
 	}
 
 }
