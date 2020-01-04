7aeeff17f982074808485462d7406e59d0f6a101
nn98 <jkllhgb@gmail.com>
2020-01-05 06:05:49 +0900

    17502

--- src/BaekJoon/P17502.java
+++ src/BaekJoon/P17502.java
@@ -13,6 +13,9 @@ public class P17502 {
 				if(a[n-2-i].equals("?"))
 					a[i]=a[n-2-i]="a";
 				else a[i]=a[n-2-i];
+			else
+				if(a[n-2-i].equals("?"))
+					a[n-2-i]=a[i];
 		}
 		for(String j:a)System.out.print(j);
 	}
