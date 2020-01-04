bfb944c55e30cb68688e3081fc1956664440c293
nn98 <jkllhgb@gmail.com>
2020-01-05 05:55:05 +0900

    17502

---
+++ src/BaekJoon/P17502.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P17502 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()+1,i=0;
+		String[]a=s.next().split("");
+		for(;i<n/2;i++) {
+			if(a[i].equals("?"))
+				if(a[n-2-i].equals("?"))
+					a[i]=a[n-2-i]="a";
+				else a[i]=a[n-2-i];
+		}
+		for(String j:a)System.out.print(j);
+	}
+
+}
