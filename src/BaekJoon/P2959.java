382e92eff6bf77993ab99be019bf3e504df98eae
nn98 <jkllhgb@gmail.com>
2020-01-05 03:18:41 +0900

    2959

--- src/BaekJoon/P2959.java
+++ src/BaekJoon/P2959.java
@@ -3,8 +3,9 @@ package BaekJoon;
 public class P2959 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=0,a[]=new int[4];
+		for(;i<4;a[i++]=)
 	}
 
 }
