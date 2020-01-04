897fc0d6ce3b187af6a9b092f49d04a738e6f6b9
nn98 <jkllhgb@gmail.com>
2020-01-05 03:24:28 +0900

    18108

--- src/BaekJoon/P18108.java
+++ src/BaekJoon/P18108.java
@@ -3,8 +3,7 @@ package BaekJoon;
 public class P18108 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print(new java.util.Scanner(System.in).nextInt());
 	}
 
 }
