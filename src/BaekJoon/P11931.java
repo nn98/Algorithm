9b363d91fa021fb5f00e150aa379a66a502f87a3
nn98 <jkllhgb@gmail.com>
2020-01-05 05:42:51 +0900

    11931

--- src/BaekJoon/P11931.java
+++ src/BaekJoon/P11931.java
@@ -9,9 +9,8 @@ public class P11931 {
 		boolean[]a=new boolean[2000001];
 		int i=s.nextInt();
 		for(;i-->0;)a[s.nextInt()+1000000]=true;
-		for(i=0;i<a.length;i++)
+		for(i=2000000;i>=0;i--)
 			if(a[i])
 				System.out.println(i-1000000);
 	}
-
 }
