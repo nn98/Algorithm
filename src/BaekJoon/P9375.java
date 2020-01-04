749058abcd4924a9c0205eb6025132e54230daf6
nn98 <jkllhgb@gmail.com>
2020-01-05 05:29:50 +0900

    9375

--- src/BaekJoon/P9375.java
+++ src/BaekJoon/P9375.java
@@ -1,11 +1,10 @@
 package BaekJoon;
-import java.util.HashMap;
-import java.util.Scanner;
-public class P9375 {
+import java.util.*;
+public class P9375{
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int num = s.nextInt();
-		for (int i = 0; i < num; i++){
+		int n= s.nextInt();
+		for (int i = 0; i++ <n;){
 			int N = s.nextInt();
 			HashMap<String, Integer> hm=new HashMap<>();
 			for (int j=0;j<N;j++){
@@ -14,9 +13,7 @@ public class P9375 {
 				if (hm.containsKey(s2)) {
 					int idx = hm.get(s2);
 					hm.put(s2, idx + 1);
-				} else {
-					hm.put(s2, 1);
-				}
+				} else hm.put(s2, 1);
 			}
 			int result = 1;
 			for (int val : hm.values()) {
