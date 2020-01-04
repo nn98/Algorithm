8f43ea1b85ab94c1de1e34db4f0b5c3cc58c53c0
nn98 <jkllhgb@gmail.com>
2020-01-05 03:48:55 +0900

    3009

--- src/BaekJoon/P3009_2.java
+++ src/BaekJoon/P3009_2.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P3009_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=0,x=0,y=0;
+		for(;i++<3;x^=s.nextInt(),y^=s.nextInt());
+		System.out.print(x+" "+y);
 	}
-
 }
