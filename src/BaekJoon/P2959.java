9bf0882feb2b4cfa7812b9a1fdf1785f7bccc072
nn98 <jkllhgb@gmail.com>
2020-01-05 03:19:56 +0900

    2959

--- src/BaekJoon/P2959.java
+++ src/BaekJoon/P2959.java
@@ -1,11 +1,16 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2959 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=0,a[]=new int[4];
-		for(;i<4;a[i++]=)
+		for(;i<4;a[i++]=s.nextInt());
+		Arrays.sort(a);
+		System.out.print(a[0]*a[2]);
 	}
 
 }
