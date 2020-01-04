ef0cfa2abd328064a6f3636fdf8c9eb4fd830037
nn98 <jkllhgb@gmail.com>
2020-01-05 03:32:28 +0900

    18108

--- src/BaekJoon/P18108.java
+++ src/BaekJoon/P18108.java
@@ -3,7 +3,7 @@ package BaekJoon;
 public class P18108 {
 
 	public static void main(String[] args) {
-		System.out.print(new java.util.Scanner(System.in).nextInt());
+		System.out.print(new java.util.Scanner(System.in).nextInt()-543);
 	}
 
 }
