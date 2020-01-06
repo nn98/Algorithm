a2a1acb587878d40fcd6acbc6d2a269c69e60bf3
nn98 <jkllhgb@gmail.com>
2020-01-07 07:45:56 +0900

    2565

--- src/BaekJoon/P2565.java
+++ src/BaekJoon/P2565.java
@@ -2,6 +2,7 @@ package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
@@ -14,16 +15,23 @@ public class P2565 {
 			st=new StringTokenizer(br.readLine());
 			a[Integer.parseInt(st.nextToken())]=Integer.parseInt(st.nextToken());
 		}
+		System.out.println(Arrays.toString(a));
 		TreeSet<Integer>ts = new TreeSet<>();
 		boolean f=false;
 		for (int n = 1; n < N; n++){
-			if(!f)ts.add(a[n]);
-			else if(a[n]!=0){
-				A=a[n];
-				if(A<=ts.last())ts.remove(ts.ceiling(A));
-				ts.add(A);
+			if(a[n]!=0) {
+				if(!f) {
+					ts.add(a[n]);
+					f=true;
+				}
+				else {
+					A=a[n];
+					if(A<=ts.last())ts.remove(ts.ceiling(A));
+					ts.add(A);
+				}
 			}
 		}
+		System.out.println(ts.toString());
 		System.out.print(ts.size());
 	}
 }
