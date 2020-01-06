7e83ab4b6543a785ad8a38ed92019686939d29ae
nn98 <jkllhgb@gmail.com>
2020-01-07 06:45:58 +0900

    1365

--- src/BaekJoon/P1365_C2.java
+++ src/BaekJoon/P1365_C2.java
@@ -7,12 +7,14 @@ import java.util.StringTokenizer;
 
 public class P1365_C2 {
 
+	//시발 시간제한 1초->2초
 	static int n,a[],h[],i=0,m=0;
 	static int o(int x) {
 		int r=h[x];
 		if(r>0)return r;
 		r=1;
 		for(int i=x+1;i<n;i++)if(a[i]>a[x])r=Math.max(r,o(i)+1);
+		m=m>r?m:r;
 		return h[x]=r;
 	}
 	public static void main(String[] args) throws IOException {
@@ -23,8 +25,6 @@ public class P1365_C2 {
 		h=new int[n];
 		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
 		for(i=0;i<n;o(i++));
-		for(i=0;i<n;i++)if(h[i]>m)m=h[i];
 		System.out.print(n-m);
 	}
-
 }
