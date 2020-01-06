21bce0dfcce88a2555a31d1809dce3989cbe7cdf
nn98 <jkllhgb@gmail.com>
2020-01-07 08:20:09 +0900

    2568

--- src/BaekJoon/P2568.java
+++ src/BaekJoon/P2568.java
@@ -4,17 +4,12 @@ import java.io.*;
 import java.util.*;
 public class P2568 {
 	public static void main(String[]z)throws Exception{
-		ArrayList<Integer>r=new ArrayList<Integer>(50000);
-		System.out.println(r.size());
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int A,N=Integer.parseInt(br.readLine()),o=501,i=0,a[]=new int[o],x;
+		int A,N=Integer.parseInt(br.readLine()),o=501,i=0,a[]=new int[o];
 		StringTokenizer st;
-		StringBuffer F=new StringBuffer();
 		for(;i++<N;) {
 			st=new StringTokenizer(br.readLine());
-			int p=Integer.parseInt(st.nextToken()),q=Integer.parseInt(st.nextToken());
-			a[p]=q;
-			r.add(p,q);
+			a[Integer.parseInt(st.nextToken())]=Integer.parseInt(st.nextToken());
 		}
 		TreeSet<Integer>ts = new TreeSet<>();
 		boolean f=false;
@@ -22,26 +17,15 @@ public class P2568 {
 			if(a[i]!=0) {
 				if(!f) {
 					ts.add(a[i]);
-					x=i;
 					f=true;
 				}
 				else {
 					A=a[i];
-					if(A<=ts.last()) {
-						F.append(ts.ceiling(A)+" ");
-						ts.remove(ts.ceiling(A));
-					}
+					if(A<=ts.last())ts.remove(ts.ceiling(A));
 					ts.add(A);
-					
 				}
 			}
 		}
-//		System.out.println(F);
-//		System.out.println(ts.toString());
-		st=new StringTokenizer(F.toString());
-		F=new StringBuffer();
-		F.append(r.indexOf(Integer.parseInt(st.nextToken()))+" ");
-		System.out.println(F);
 		System.out.print(N-ts.size());
 	}
 }
