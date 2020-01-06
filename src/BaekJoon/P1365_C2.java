bf697aa40318547fc62a69c9151777c9b1302b1a
nn98 <jkllhgb@gmail.com>
2020-01-07 06:06:55 +0900

    1365

--- src/BaekJoon/P1365_C2.java
+++ src/BaekJoon/P1365_C2.java
@@ -1,10 +1,31 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P1365_C2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int n,a[],h[],i=0,m=0;
+	static int o(int x) {
+		int r=h[x];
+		if(r>0)return r;
 
+		r=1;
+		for(int i=x+1;i<n;i++) {
+			if(a[i]>a[x])r=Math.max(r,o(i)+1);
+		}
+
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader f=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(f.readLine());
+		StringTokenizer t=new StringTokenizer(f.readLine());
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		for(i=0;i<n;o(i++));
 	}
 
 }
