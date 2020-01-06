c7b43725eb127a0f766607652b3d43e2b87c3a86
nn98 <jkllhgb@gmail.com>
2020-01-07 07:56:00 +0900

    12738

--- src/BaekJoon/P12738.java
+++ src/BaekJoon/P12738.java
@@ -1,5 +1,19 @@
 package BaekJoon;
 
+import java.io.*;
+import java.util.*;
 public class P12738 {
-
+	public static void main(String[]z)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int A, N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		TreeSet<Integer>ts = new TreeSet<>();
+		ts.add(Integer.parseInt(st.nextToken()));
+		for (int n = 1; n < N; n++){
+			A = Integer.parseInt(st.nextToken());
+			if (A <= ts.last())ts.remove(ts.ceiling(A));
+			ts.add(A);
+		}
+		System.out.print(ts.size());
+	}
 }
