0c8a8fb01776877da29f27e29e42ead0494f1a30
nn98 <jkllhgb@gmail.com>
2020-01-07 05:20:28 +0900

    1365

--- src/BaekJoon/P1365_2.java
+++ src/BaekJoon/P1365_2.java
@@ -1,5 +1,16 @@
 package BaekJoon;
 
-public class P1365_2 {
+import java.util.Scanner;
 
+public class P1365_2 {
+	static int n,a[],h[],i=0,r;
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		
+		System.out.print(n-r);
+	}
 }
