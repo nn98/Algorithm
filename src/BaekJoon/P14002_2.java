7b4239971f7313a8207f74e2898aaf0b2826d4d2
nn98 <jkllhgb@gmail.com>
2020-01-07 07:56:59 +0900

    14002

---
+++ src/BaekJoon/P14002_2.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.*;
+public class P14002_2 {
+	public static void main(String[]z)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int A, N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		TreeSet<Integer>ts = new TreeSet<>();
+		ts.add(Integer.parseInt(st.nextToken()));
+		for (int n = 1; n < N; n++){
+			A = Integer.parseInt(st.nextToken());
+			if (A <= ts.last())ts.remove(ts.ceiling(A));
+			ts.add(A);
+		}
+		System.out.println(ts.size());
+		for(int i:ts)System.out.print(i+" ");
+	}
+}
