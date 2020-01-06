078f82743c3b0f5b726fd240c830493df945a9d7
nn98 <jkllhgb@gmail.com>
2020-01-07 07:31:43 +0900

    12015

--- src/BaekJoon/P12015.java
+++ src/BaekJoon/P12015.java
@@ -1,26 +1,19 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
-
+import java.io.*;
+import java.util.*;
 public class P12015 {
-
-	public static void main(String[] args)throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[]z)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int A, N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		TreeSet<Integer> ts = new TreeSet<>();
+		TreeSet<Integer>ts = new TreeSet<>();
 		ts.add(Integer.parseInt(st.nextToken()));
-		for (int n = 1; n < N; n++) {
+		for (int n = 1; n < N; n++){
 			A = Integer.parseInt(st.nextToken());
-			if (A <= ts.last()) {
-				ts.remove(ts.ceiling(A));
-			}
+			if (A <= ts.last())ts.remove(ts.ceiling(A));
 			ts.add(A);
 		}
 		System.out.print(ts.size());
 	}
-
 }
