c1240900c7b221c42f680c3fdfa5c25ddea67d7e
nn98 <jkllhgb@gmail.com>
2020-01-07 07:12:23 +0900

    10610

--- src/BaekJoon/P10610.java
+++ src/BaekJoon/P10610.java
@@ -5,9 +5,5 @@ import java.util.Scanner;
 public class P10610 {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),r=0,i=0;
-		for(;i++<5;r=s.nextInt()==n?r+1:r);
 	}
-
 }
