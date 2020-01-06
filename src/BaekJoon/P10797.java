f7547ca796445ee5fa261405fec36455b91d0bee
nn98 <jkllhgb@gmail.com>
2020-01-07 07:12:30 +0900

    10797

---
+++ src/BaekJoon/P10797.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P10797 {
+	public static void main(String[]a) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r=0,i=0;
+		for(;i++<5;r=s.nextInt()==n?r+1:r);
+	}
+}
