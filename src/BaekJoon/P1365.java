954f04a5f225bfa53e101295938c2039ec8c80c2
nn98 <jkllhgb@gmail.com>
2020-01-07 05:07:33 +0900

    1365

--- src/BaekJoon/P1365.java
+++ src/BaekJoon/P1365.java
@@ -1,12 +1,27 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1365 {
 
 	//LIS 최장증가부분수열
 	//LIS만보고 알았어야;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,a[],h[],i=0,r;
+	static void o(int x,int v) {
+		for(int j=x;j<n;j++) {
+			if(a[j]>a[x]&&h[j]<=v) {
+				o(j,v+1);
+			}
+		}
+		h[x]=h[x]>v?h[x]:v;
+		r=h[x]>r?h[x]:r;
 	}
-
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n;o(i++,0));
+	}
 }
