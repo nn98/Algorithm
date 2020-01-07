c13079b5091da66d7cf80cb45d64acc6d73c2f36
nn98 <jkllhgb@gmail.com>
2020-01-08 08:54:41 +0900

    9461

--- src/BaekJoon/P9461.java
+++ src/BaekJoon/P9461.java
@@ -1,10 +1,15 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P9461 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0;
+		for(;n-->0;) {
+			
+		}
 	}
 
 }
