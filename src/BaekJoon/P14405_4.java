7d353395a52a352f086cf17974ab974ec83f9e5a
nn98 <jkllhgb@gmail.com>
2020-01-08 08:44:04 +0900

    14405

--- src/BaekJoon/P14405_4.java
+++ src/BaekJoon/P14405_4.java
@@ -7,23 +7,28 @@ public class P14405_4 {
 		if(X.equals(""))System.out.print("NO");
 		else {
 			char[]a=X.toCharArray();
-			try {
-				for(int i=0;i<a.length-1;i++) {
-					if(a[i]=='p'&&a[i+1]=='i') {
-						i++;
-					}else if(a[i]=='k'&&a[i+1]=='a') {
-						i++;
-					}else if(a[i]=='p'&&a[i+1]=='h'&&a[i+2]=='u') {
-						i+=2;
-					}else {
-						System.out.print("NO");
-						return;
+			if(a.length>1) {
+				try {
+					for(int i=0;i<a.length-1;i++) {
+						if(a[i]=='p'&&a[i+1]=='i') {
+							i++;
+						}else if(a[i]=='k'&&a[i+1]=='a') {
+							i++;
+						}else if(a[i]=='c'&&a[i+1]=='h'&&a[i+2]=='u') {
+							i+=2;
+						}else {
+							System.out.print("NO");
+							return;
+						}
 					}
+					System.out.print("YES");
+				} catch(Exception e) {
+					System.out.print("NO");
+					return;
 				}
-				System.out.print("YES");
-			} catch(Exception e) {
+			}
+			else {
 				System.out.print("NO");
-				return;
 			}
 		}
 	}
