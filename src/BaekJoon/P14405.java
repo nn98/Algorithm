419482f138f0acfe2214fb3f40e11a6a61761127
nn98 <jkllhgb@gmail.com>
2020-01-07 09:02:51 +0900

    14405

--- src/BaekJoon/P14405.java
+++ src/BaekJoon/P14405.java
@@ -2,9 +2,35 @@ package BaekJoon;
 
 public class P14405 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static String A(String i) {
+		return i.replaceAll("pi","");
 	}
-
+	static String B(String i) {
+		return i.replaceAll("ka","");
+	}
+	static String C(String i) {
+		return i.replaceAll("chu","");
+	}
+	public static void main(String[] args) {
+		String in=new java.util.Scanner(System.in).next(),a,b,c,d,e,f;
+		a=A(in);
+		a=B(a);
+		a=C(a);
+		b=A(in);
+		b=C(b);
+		b=B(b);
+		c=B(in);
+		c=A(c);
+		c=C(c);
+		d=B(in);
+		d=C(d);
+		d=A(d);
+		e=C(in);
+		e=B(e);
+		e=A(e);
+		f=C(in);
+		f=A(f);
+		f=B(f);
+		System.out.print((a.equals("")||b.equals("")||c.equals("")||d.equals("")||e.equals("")||f.equals(""))?"YES":"NO");
+	}
 }
