1a2c9e15d3821b89687cba476ba68af6f8539c11
nn98 <jkllhgb@gmail.com>
2020-01-07 10:48:38 +0900

    14405

--- src/BaekJoon/P14405_3.java
+++ src/BaekJoon/P14405_3.java
@@ -1,9 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P14405_3 {
 
 	public static void main(String[] args) {
-		System.out.print((new java.util.Scanner(System.in).next().replaceAll("pi","").replaceAll("ka","").replaceAll("chu","")).equals("")?"YES":"NO");
+		Scanner s=new Scanner(System.in);
+		while(s.hasNext()) {
+		System.out.print((s.next().replaceAll("pi","").replaceAll("ka","").replaceAll("chu","")).equals("")?"YES":"NO");
+		return;
+		}
+		System.out.print("NO");
 	}
 
 }
