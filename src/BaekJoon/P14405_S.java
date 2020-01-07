9d8f21f0b693c2ae5fc19fc44044c32f02af88b3
nn98 <jkllhgb@gmail.com>
2020-01-08 08:49:42 +0900

    14405

--- src/BaekJoon/P14405_S.java
+++ src/BaekJoon/P14405_S.java
@@ -3,7 +3,7 @@ package BaekJoon;
 public class P14405_S {
 
 	public static void main(String[] args) {
-//		
+		System.out.print(new java.util.Scanner(System.in).next().replaceAll("pi|ka|chu", "").equals("")?"YES":"NO");
 	}
 
 }
