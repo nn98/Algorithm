1eed51fd46a6221807412e8eaa92c7eb9a605949
nn98 <jkllhgb@gmail.com>
2020-01-08 08:29:01 +0900

    5355

--- src/BaekJoon/P5355.java
+++ src/BaekJoon/P5355.java
@@ -1,9 +1,31 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5355 {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in)
+		Scanner s=new Scanner(System.in);
+		int i=s.nextInt(),j;
+		s.nextLine();
+		for(;i-->0;) {
+			String[]a=s.nextLine().split(" ");
+			double r=Double.parseDouble(a[0]);
+			for(j=0;j++<a.length;) {
+				switch(a[j]) {
+				case "@":
+					r*=3;
+					break;
+				case "#":
+					r-=7;
+					break;
+				case "%":
+					r+=5;
+					break;
+				}
+			}
+			System.out.printf("%.2f",r);
+		}
 	}
 
 }
