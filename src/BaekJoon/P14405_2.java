e88214467b6d411c4d004d2ce02c49a1e97f631f
nn98 <jkllhgb@gmail.com>
2020-01-07 09:11:36 +0900

    14405

--- src/BaekJoon/P14405_2.java
+++ src/BaekJoon/P14405_2.java
@@ -3,8 +3,15 @@ package BaekJoon;
 public class P14405_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		boolean c=true;
+		for(int i=0;c&&i<a.length-1;i++) {
+			if(a[i]=='p'&&a[i+1]=='i') i+=2;
+			else if(a[i]=='k'&&a[i+1]=='a') i+=2;
+			else if(i<a.length-2)if(a[i]=='c'&&a[i+1]=='h'&&a[i+2]=='u') i+=3;
+			else c=false;
+		}
+		System.out.print(c?"YES":"NO");
 	}
 
 }
