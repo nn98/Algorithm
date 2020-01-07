599946c4a421974c482911c3a6bb0d8b02144ca1
nn98 <jkllhgb@gmail.com>
2020-01-08 08:33:04 +0900

    14405

--- src/BaekJoon/P14405_3.java
+++ src/BaekJoon/P14405_3.java
@@ -7,8 +7,8 @@ public class P14405_3 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		while(s.hasNext()) {
-		System.out.print((s.next().replaceAll("pi","").replaceAll("ka","").replaceAll("chu","")).equals("")?"YES":"NO");
-		return;
+			System.out.print((s.next().replaceAll("pi","").replaceAll("ka","").replaceAll("chu","")).equals("")?"YES":"NO");
+			return;
 		}
 		System.out.print("NO");
 	}
