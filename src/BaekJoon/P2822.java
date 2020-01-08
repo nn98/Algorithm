4270fed7da153f5e010e6c029638bc1f19ee6bad
nn98 <jkllhgb@gmail.com>
2020-01-09 05:43:02 +0900

    2822

--- src/BaekJoon/P2822.java
+++ src/BaekJoon/P2822.java
@@ -7,16 +7,16 @@ public class P2822 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=1,j,r=0,c=0,z=151,a[]=new int[9];
-		boolean[]b=new boolean[z];
+		boolean[]b=new boolean[z],p=new boolean[z];
 		for(;i<9;b[a[i++]=s.nextInt()]=true);
-		String t="";
 		for(i=z;--i>0&&c<5;) {
 			if(b[i]) {
-				for(j=1;j<9;j++)t+=i==a[j]?j+" ":"";
+				for(j=1;j<9;j++)p[j]=i==a[j];
 				r+=i;
 				c++;
 			}
 		}
-		System.out.print(r+"\n"+t);
+		System.out.println(r);
+		for(i=1;i<z;System.out.print(p[i++]?i+" ":""));
 	}
 }
