7db2748ba8ff0ebf589c84cfaa4ddb027447d4ef
nn98 <jkllhgb@gmail.com>
2020-01-08 22:11:15 +0900

    9085

--- src/BaekJoon/P9085.java
+++ src/BaekJoon/P9085.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P9085 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i,m,r;
+		for(;n-->0;) {
+			m=s.nextInt();
+			r=0;
+			for(i=0;i++<m;r+=s.nextInt());
+			System.out.println(r);
+		}
 	}
 
 }
