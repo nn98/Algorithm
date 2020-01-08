793335aa762a29e915d1a71ba9a8dfe429185e9f
nn98 <jkllhgb@gmail.com>
2020-01-08 09:31:30 +0900

    14002

--- src/BaekJoon/P14002_5.java
+++ src/BaekJoon/P14002_5.java
@@ -1,37 +1,34 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-	//¼ôÄÚµù Áö¶ö
-public class P14002_5 {
-
-	static int n,a[],h[],i=0,m=0,y;
-	static StringBuffer[]H;
-	static void o(int x,int v,StringBuffer s) {
+import java.io.*;
+import java.util.*;
+public class P14002_5{
+	static int n,a[],h[],i=0,m=0,s[];
+	static void o(int x,int v){
 		h[x]=v;
-		s.append(a[x]+" ");
-		H[x]=s;
-//		System.out.println(x+" "+H[x]);
 		for(int i=x+1;i<n;i++)
 			if(a[i]>a[x]&&h[i]<=v)
-				o(i,v+1,new StringBuffer(s.toString()));
-		if(m<h[x]) {
+				o(i,v+1);
+		if(m<h[x])
 			m=h[x];
-			y=x;
-		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(r.readLine());
-		a=new int[n];
-		h=new int[n];
-		H=new StringBuffer[n];
+		a=new int[n+1];
+		h=new int[n+1];
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
-		//		System.out.println(Arrays.toString(a));
-		for(i=0;i<n;i++)if(h[i]==0)o(i,1,new StringBuffer());
-		System.out.println(m+"\n"+H[y]);
+		for(i=0;i<n;i++)if(h[i]==0)o(i,1);
+		System.out.println(m);
+		s=new int[m];
+		for(i=n;i>=0&&m>0;i--) {
+			if(h[i]==m) {
+				s[m-1]=a[i];
+				m--;
+			}
+		}
+		for(int j:s)
+			System.out.print(j+" ");
 	}
 }
