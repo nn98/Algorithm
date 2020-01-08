4e97a1c9a2883ef90ebb148835a407ccbb1fad51
nn98 <jkllhgb@gmail.com>
2020-01-08 09:47:23 +0900

    5576

---
+++ src/BaekJoon/P5576.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P5576 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int i=0,j,a[][]=new int[2][10];
+		for(;i<2;i++) {
+			for(j=0;j<10;)
+				a[i][j++]=s.nextInt();
+			Arrays.sort(a[i]);
+			System.out.print(a[i][9]+a[i][8]+a[i][7]+" ");
+		}
+		
+	}
+
+}
