98107faf845fcb3ea742d6a8fb89cd86ac71db6c
nn98 <jkllhgb@gmail.com>
2020-01-08 09:02:57 +0900

    9461

--- src/BaekJoon/P9461.java
+++ src/BaekJoon/P9461.java
@@ -1,15 +1,20 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P9461 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0;
+		int n=s.nextInt(),i=2,a[]=new int[100];
+		a[0]=1;
+		a[1]=1;
+		a[2]=1;
+		for(;++i<100;a[i]=(a[i-3]+a[i-2]));
+		System.out.println(Arrays.toString(a));
 		for(;n-->0;) {
-			
+			System.out.println(a[s.nextInt()-1]);
 		}
 	}
-
 }
