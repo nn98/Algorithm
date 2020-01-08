55b52572ab3d0f72a0f2ad7652bf85a0cb5b3d46
nn98 <jkllhgb@gmail.com>
2020-01-08 10:03:58 +0900

    5573

--- src/BaekJoon/P5573_2.java
+++ src/BaekJoon/P5573_2.java
@@ -1,10 +1,24 @@
 package BaekJoon;
 
-public class P5573_2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.io.*;
+import java.util.*;
+public class P5573_2{
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int x=Integer.parseInt(t.nextToken()),y=Integer.parseInt(t.nextToken()),n=Integer.parseInt(t.nextToken()),a[][]=new int[x][y],i=0,j;
+		for(;i<x;i++) {
+			t=new StringTokenizer(r.readLine());
+			for(j=0;j<y;a[i][j++]=Integer.parseInt(t.nextToken()));
+		}
+		for(;n-->0;) {
+			i=j=0;
+			for(;i<x&&j<y;) {
+				if(a[i][j]==0)a[i++][j]=1;
+				else a[i][j++]=0;
+			}
+			if(n==0)
+				System.out.println(++i+" "+(j+1));
+		}
 	}
-
 }
