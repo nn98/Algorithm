5fe00cdcfe13948b594333cd03b5e6bd2faa6861
nn98 <jkllhgb@gmail.com>
2020-01-09 05:56:15 +0900

    14501

--- src/BaekJoon/P14501.java
+++ src/BaekJoon/P14501.java
@@ -1,10 +1,22 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P14501 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,a[],b[],h[],i=0;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		b=new int[n];
+		h=new int[n];
+		for(;i<n;i++) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			a[i]=Integer.parseInt(t.nextToken());
+			b[i]=Integer.parseInt(t.nextToken());
+		}
 	}
-
 }
