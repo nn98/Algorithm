e4a60b04dd519302f479ff2b00c22a6c63931f93
nn98 <jkllhgb@gmail.com>
2020-01-09 05:24:04 +0900

    2810

--- src/BaekJoon/P2810.java
+++ src/BaekJoon/P2810.java
@@ -6,11 +6,12 @@ public class P2810 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),c=1,i=0;
+		int n=s.nextInt(),r=1,i=0;
 		char[]a=s.next().toCharArray();
 		for(;i<a.length;i++) {
-			
+			if(a[i]=='L')i++;
+			r++;
 		}
+		System.out.print(r);
 	}
-
 }
