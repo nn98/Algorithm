6bd2198389a0d149d5b133f8a23b25efaf4d1d95
nn98 <jkllhgb@gmail.com>
2020-01-09 05:42:58 +0900

    2822

--- src/BaekJoon/P2822.java
+++ src/BaekJoon/P2822.java
@@ -1,8 +1,22 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2822 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int i=1,j,r=0,c=0,z=151,a[]=new int[9];
+		boolean[]b=new boolean[z];
+		for(;i<9;b[a[i++]=s.nextInt()]=true);
+		String t="";
+		for(i=z;--i>0&&c<5;) {
+			if(b[i]) {
+				for(j=1;j<9;j++)t+=i==a[j]?j+" ":"";
+				r+=i;
+				c++;
+			}
+		}
+		System.out.print(r+"\n"+t);
 	}
 }
