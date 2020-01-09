0a4604f45952fd469bfa713853d102f65844c93c
nn98 <jkllhgb@gmail.com>
2020-01-10 07:09:37 +0900

    9507

--- src/BaekJoon/P9507.java
+++ src/BaekJoon/P9507.java
@@ -1,8 +1,19 @@
 package BaekJoon;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P9507 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j;
+		BigInteger[]a=new BigInteger[68];
+		a[0]=BigInteger.ONE;
+		for(;i++<67;) {
+			a[i]=a[i-1];
+			for(j=i;j-->0;a[i]=a[i].add(a[j]));
+		}
+		for(;n-->0;)System.out.println(a[s.nextInt()]);
 	}
-
 }
