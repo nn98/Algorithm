7c2081ab219e587e8af50f65630f2734f6433999
nn98 <jkllhgb@gmail.com>
2020-01-10 07:01:27 +0900

    14918

--- src/BaekJoon/P14918.java
+++ src/BaekJoon/P14918.java
@@ -1,12 +1,9 @@
 package BaekJoon;
 
-import java.util.Scanner;
-public class P14918 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a,b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println((a+b));
+import java.util.*;
+class P14918 {
+	public static void main(String[]z){
+		Scanner s=new Scanner(System.in);
+		System.out.print(s.nextInt()+s.nextInt());
 	}
 }
