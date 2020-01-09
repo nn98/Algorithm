d6c00ddadd51088f6c179f95f9cf607410b34184
nn98 <jkllhgb@gmail.com>
2020-01-10 07:16:08 +0900

    9507

--- src/BaekJoon/P9507_2.java
+++ src/BaekJoon/P9507_2.java
@@ -1,5 +1,18 @@
 package BaekJoon;
 
-public class P9507_2 {
+import java.util.*;
 
+public class P9507_2 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=1,j;
+		long[]a=new long[68];
+		a[i]=a[0]=1;
+		for(;i++<67;) {
+			j=i-1;
+			a[i]=a[j];
+			for(;j>0&&j>i-4;a[i]=a[i]+a[--j]);
+		}
+		for(;n-->0;)System.out.println(a[s.nextInt()]);
+	}
 }
