297fc6c10e31f0fd4c03c160fc26bc508393cd31
nn98 <jkllhgb@gmail.com>
2020-01-10 08:10:35 +0900

    1927

---
+++ src/BaekJoon/P1927.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class P1927 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		List<Integer>l=new ArrayList();
+		int n=s.nextInt(),m;
+		for(;n-->0;) {
+			m=s.nextInt();
+			if(m==0)System.out.println(l.isEmpty()?0:l.remove(0));
+			else {
+				l.add(m);
+				Collections.sort(l);
+			}
+		}
+	}
+
+}
