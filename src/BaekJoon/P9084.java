5e5dd715900ccd156d9a22feec907b0266b97426
nn98 <jkllhgb@gmail.com>
2020-01-10 05:58:49 +0900

    9084

--- src/BaekJoon/P9084.java
+++ src/BaekJoon/P9084.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P9084 {
 
-	public static void main(String[] args) {
-		
+	static int n,m,p,a[],h[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		for(n=Integer.parseInt(r.readLine());n-->0;) {
+			m=Integer.parseInt(r.readLine());
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			
+		}
 	}
-
 }
