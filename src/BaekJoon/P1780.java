9dda75b5812e43a60191378cf6606598990e7268
nn98 <jkllhgb@gmail.com>
2020-01-10 08:41:14 +0900

    1780

--- src/BaekJoon/P1780.java
+++ src/BaekJoon/P1780.java
@@ -6,6 +6,17 @@ import java.io.InputStreamReader;
 public class P1780 {
 
 	static int n,i,j,a[][],r[]=new int[3];
+	static void o(int x,int y,int s) {
+		boolean c=true;
+		int v=a[x][y];
+		for(int i=x;i<x+s&&c;i++)
+			for(int j=y;j<y+s&&c;j++)
+				c=a[i][j]==v;
+		if(c)r[v+1]++;
+		else{
+			
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(r.readLine());
