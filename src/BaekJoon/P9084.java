892650f8bb6984557e01433afb0d66d9f1cbf104
nn98 <jkllhgb@gmail.com>
2020-01-10 06:02:26 +0900

    9084

--- src/BaekJoon/P9084.java
+++ src/BaekJoon/P9084.java
@@ -6,7 +6,10 @@ import java.util.StringTokenizer;
 
 public class P9084 {
 
-	static int n,m,p,a[],h[],i;
+	static int n,m,p,a[],h[],i,c;
+	static void o(int x,int n) {
+		
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		for(n=Integer.parseInt(r.readLine());n-->0;) {
@@ -15,6 +18,8 @@ public class P9084 {
 			a=new int[m];
 			for(i=0;i<m;a[i++]=Integer.parseInt(t.nextToken()));
 			p=Integer.parseInt(r.readLine());
+			c=0;
+			
 		}
 	}
 }
