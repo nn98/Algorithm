855a6bc1b6c9a9ffae38a92ab097ce1e678ca98f
nn98 <jkllhgb@gmail.com>
2020-01-10 06:51:23 +0900

    9084

--- src/BaekJoon/P9084_3.java
+++ src/BaekJoon/P9084_3.java
@@ -1,10 +1,23 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P9084_3 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int n=s.nextInt(),m,p,i,j,a[],d[];
+		for(;n-->0;) {
+			m=s.nextInt();
+			a=new int[m+1];
+			for(i=0;i++<m;a[i]=s.nextInt());
+			p=s.nextInt();
+			d=new int[p+1];
+			for(i=0;i++<m;)
+				for(j=0;j++<p;)
+					d[j]+=d[j-a[i]];
+			System.out.println(d[p]);
+		}
 	}
 
 }
