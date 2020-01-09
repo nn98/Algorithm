b41745cbed3b63907737f3a128c6167b8a6bdd74
nn98 <jkllhgb@gmail.com>
2020-01-10 07:09:41 +0900

    9507

--- src/BaekJoon/P9507.java
+++ src/BaekJoon/P9507.java
@@ -7,9 +7,9 @@ public class P9507 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,j;
+		int n=s.nextInt(),i=1,j;
 		BigInteger[]a=new BigInteger[68];
-		a[0]=BigInteger.ONE;
+		a[i]=a[0]=BigInteger.ONE;
 		for(;i++<67;) {
 			a[i]=a[i-1];
 			for(j=i;j-->0;a[i]=a[i].add(a[j]));
