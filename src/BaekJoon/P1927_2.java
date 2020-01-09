7c9d08ae0a916b82e4eb689690c3e130e19c1456
nn98 <jkllhgb@gmail.com>
2020-01-10 08:14:21 +0900

    1927

--- src/BaekJoon/P1927_2.java
+++ src/BaekJoon/P1927_2.java
@@ -1,9 +1,19 @@
 package BaekJoon;
 
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class P1927_2 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m;
+		PriorityQueue<Integer>q=new PriorityQueue();
+		for(;n-->0;) {
+			m=s.nextInt();
+			if(m==0)System.out.println(q.isEmpty()?0:q.poll());
+			else q.add(m);
+		}
 	}
 
 }
