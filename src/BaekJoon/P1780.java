c2d372378a96ee24de64366ed9cfa54e0685cb6c
nn98 <jkllhgb@gmail.com>
2020-01-10 08:31:47 +0900

    1780

--- src/BaekJoon/P1780.java
+++ src/BaekJoon/P1780.java
@@ -1,9 +1,15 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P1780 {
 
-	public static void main(String[] args) {
+	static int n,i,j,a[][],r[]=new int[3];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n][n];
 		
 	}
-
 }
