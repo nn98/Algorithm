9c24de351a44c2d70d3e33a57cf5af77fa993125
nn98 <jkllhgb@gmail.com>
2020-01-10 08:15:16 +0900

    11279

---
+++ src/BaekJoon/P11279.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class P11279 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m;
+		PriorityQueue<Integer>q=new PriorityQueue<>((a, b) -> b - a);
+		for(;n-->0;) {
+			m=s.nextInt();
+			if(m==0)System.out.println(q.isEmpty()?0:q.poll());
+			else q.add(m);
+		}
+	}
+
+}
