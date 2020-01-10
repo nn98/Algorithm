a947ab090f893d42d5cc03e83584f74958aa2d27
nn98 <jkllhgb@gmail.com>
2020-01-10 12:29:48 +0900

    2668

--- src/BaekJoon/P2668.java
+++ src/BaekJoon/P2668.java
@@ -1,9 +1,23 @@
 package BaekJoon;
 
-public class P2668 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class P2668 {
+	static int n,a[],h[],i=0,r=0;
+	static void o(int x,int c,int O) {
+		if(c>n)return;
+		if(a[x]==O)h[O]=c;
+		else o(a[x],c+1,O);
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n;o(i,0,i++));
+		System.out.println(Arrays.toString(h));
 	}
 
 }
