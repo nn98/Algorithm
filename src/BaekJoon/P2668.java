f1f8eff1f0f6c4da7e7e9ac7c400123d065d9e4c
nn98 <jkllhgb@gmail.com>
2020-01-10 12:40:02 +0900

    2668

--- src/BaekJoon/P2668.java
+++ src/BaekJoon/P2668.java
@@ -4,20 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2668 {
-	static int n,a[],h[],i=0,r=0;
-	static void o(int x,int c,int O) {
-		System.out.println(x+" "+c+" "+O);
+	static int n,a[],h[],i=1,r=0;
+	static boolean[][]l;
+	static void o(int x,int c,StringBuffer b) {
+//		System.out.println(x+" "+c+" "+O);
+		StringBuffer B=new StringBuffer(b.append(x+" "));
 		if(c>n)return;
-		if(a[x]==O)h[O]=h[O]>c?h[O]:c;
-		else o(a[x],c+1,O);
+		if(a[x]==b.charAt(0)-'0') {
+			for(String s:b.toString().split(" "))
+				h[Integer.parseInt(s)]=1;
+		}
+		else o(a[x],c+1,B);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		n=s.nextInt();
+		n=s.nextInt()+1;
 		a=new int[n];
 		h=new int[n];
-		for(;i<n;a[i++]=s.nextInt()-1);
-		for(i=0;i<n;o(i,0,i++));
+		l=new boolean[n][n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n;o(i++,0,new StringBuffer()));
 		System.out.println(Arrays.toString(h));
 	}
 
