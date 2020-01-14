c806abaad3e1eeb2ae75885815cd0228b56b6d19
nn98 <jkllhgb@gmail.com>
2020-01-15 04:14:44 +0900

    5596

--- src/BaekJoon/P5596.java
+++ src/BaekJoon/P5596.java
@@ -1,10 +1,14 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5596 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int a=0,b=0,i=0;
+		for(;i++<8;)if(i<5)a+=s.nextInt();else b+=s.nextInt();
+		System.out.print(a>b?a:b);
 	}
 
 }
