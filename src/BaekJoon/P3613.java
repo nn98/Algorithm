61c0749f59a1603517906d9d86395716c3dac463
nn98 <jkllhgb@gmail.com>
2020-01-15 03:46:55 +0900

    3613

--- src/BaekJoon/P3613.java
+++ src/BaekJoon/P3613.java
@@ -3,8 +3,12 @@ package BaekJoon;
 public class P3613 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String n=new java.util.Scanner(System.in).next(),a[]=n.split("");
+		if(n.equals(n.toLowerCase())) {
+			if(n.contains("_")) {
+				for(String i:a);
+			}
+		}
 	}
 
 }
