532ddbe4dccf42d519b531f58c06c54e52d19f7d
nn98 <jkllhgb@gmail.com>
2020-01-15 04:09:03 +0900

    2845

--- src/BaekJoon/P2845.java
+++ src/BaekJoon/P2845.java
@@ -1,8 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2845 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()*s.nextInt(),i=0;
+		for(;i++<5;System.out.print(s.nextInt()-n+" "));
 	}
-
 }
