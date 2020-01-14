77bba552b05874330fe7076830380eed0c2d9cf0
nn98 <jkllhgb@gmail.com>
2020-01-15 04:22:47 +0900

    3028

--- src/BaekJoon/P3028_Q.java
+++ src/BaekJoon/P3028_Q.java
@@ -1,5 +1,17 @@
 package BaekJoon;
 
-public class P3028_Q {
+import java.util.Scanner;
 
+public class P3028_Q {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = 1;
+        String s = sc.next();
+        for (int e:s.getBytes()){
+            int a =e=='A'?3:e-'A';
+            if (a != b)
+                b = 6 -a - b;
+        }
+        System.out.println(b);
+    }
 }
