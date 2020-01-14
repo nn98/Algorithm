08257889355776af08126b50c9d4df742a69e2ab
nn98 <jkllhgb@gmail.com>
2020-01-15 04:20:42 +0900

    3028

--- src/BaekJoon/P3028.java
+++ src/BaekJoon/P3028.java
@@ -3,7 +3,42 @@ package BaekJoon;
 public class P3028 {
 
 	public static void main(String[] args) {
-		
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		int r=1;
+		for(char i:a) {
+			switch(i) {
+			case 'A':
+				switch(r) {
+				case 1:
+					r=2;
+					break;
+				case 2:
+					r=1;
+					break;
+				}
+				break;
+			case 'B':
+				switch(r) {
+				case 2:
+					r=3;
+					break;
+				case 3:
+					r=2;
+					break;
+				}
+				break;
+			case 'C':
+				switch(r) {
+				case 1:
+					r=3;
+					break;
+				case 3:
+					r=1;
+					break;
+				}
+				break;
+			}
+		}
 	}
 
 }
