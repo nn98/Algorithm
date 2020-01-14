9e1594a2917fc5b91a464fc2232651ebfbc4fa69
nn98 <jkllhgb@gmail.com>
2020-01-15 04:43:27 +0900

    3029

--- src/BaekJoon/P3029.java
+++ src/BaekJoon/P3029.java
@@ -7,7 +7,22 @@ public class P3029 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String[]a=s.next().split(":"),b=s.next().split(":");
-		int[][][][][]t;
+		int t[]=new int[3],i=3,f=0,A,B;
+		for(;i-->1;) {
+			A=Integer.parseInt(a[i]);
+			B=Integer.parseInt(b[i]);
+			if(f==1) {
+				f=0;
+				B--;
+			}
+			t[i]=B-A;
+			if(t[i]<0) {
+				if(i==0)t[i]+=24;
+				else t[i]+=60;
+				f=1;
+			}
+		}
+		for(;i<3;System.out.printf("%02d"+(i==2?"":":"),t[i++]));
 	}
 
 }
