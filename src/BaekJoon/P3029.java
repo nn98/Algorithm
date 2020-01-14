275c71c65d0c1f30193d1aa29d22743218b953e1
nn98 <jkllhgb@gmail.com>
2020-01-15 04:26:36 +0900

    3029

---
+++ src/BaekJoon/P3029.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P3029 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String[]a=s.next().split(":"),b=s.next().split(":");
+		int[][][][][]t;
+	}
+
+}
