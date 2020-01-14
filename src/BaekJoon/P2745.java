71965ea28161302454b2cf101f61bd7eeab9e5bd
nn98 <jkllhgb@gmail.com>
2020-01-14 20:45:38 +0900

    2745

--- src/BaekJoon/P2745.java
+++ src/BaekJoon/P2745.java
@@ -1,10 +1,21 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2745 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String n=s.next();
+		int b=s.nextInt(),p=1,r=0,l=n.length(),i=0;
+		while(l!=0) {
+			char t=n.charAt(--l);
+			if(t>64)t-=55;
+			else t-=48;
+			r+=t*p;
+			p*=b;
+		}
+		System.out.println(r);
 	}
 
 }
