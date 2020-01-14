ecff5e4f8c5e2fc9367411a6c342821fbe7a1c8c
nn98 <jkllhgb@gmail.com>
2020-01-14 21:01:55 +0900

    15820

--- src/BaekJoon/P15820.java
+++ src/BaekJoon/P15820.java
@@ -1,10 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P15820 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0;
+		boolean a=true,b=true;
+		String r;
+		for(;i++<n&&a;a=s.nextInt()==s.nextInt());
+		for(i=0;i++<m&&a;a=s.nextInt()==s.nextInt());
+		if(a&&b)r="Accepted";
+		else if(a)r="Why Wrong!!!";
+		else r="Wrong Answer";
+		System.out.print(r);
 	}
-
 }
