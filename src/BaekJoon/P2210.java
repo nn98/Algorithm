a08e4b31ab0b7b7dd900802ad89c842201de3cb9
nn98 <jkllhgb@gmail.com>
2020-01-14 20:52:26 +0900

    2210

--- src/BaekJoon/P2210.java
+++ src/BaekJoon/P2210.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class P2210 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static boolean[][]h=new boolean[5][5];
+	static String[][]a=new String[5][];
+	static int i=0,n,r,x[]= {0,1,0,-1},y[]= {1,0,-1,0};
+	public static void main(String[] args) throws IOException {
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		for(;i<5;a[i++]=r.readLine().split(" "));
 	}
-
 }
