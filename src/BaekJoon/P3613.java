0f660b369fd8e1a3e779313375bed5e28ca8feca
nn98 <jkllhgb@gmail.com>
2020-01-15 03:57:14 +0900

    3613

--- src/BaekJoon/P3613.java
+++ src/BaekJoon/P3613.java
@@ -3,10 +3,23 @@ package BaekJoon;
 public class P3613 {
 
 	public static void main(String[] args) {
-		String n=new java.util.Scanner(System.in).next(),a[]=n.split("");
+		String n=new java.util.Scanner(System.in).next();
+		int i=0;
 		if(n.equals(n.toLowerCase())) {
 			if(n.contains("_")) {
-				for(String i:a);
+				for(;i<n.length();i++) {
+					if(n.charAt(i)=='_')
+						System.out.print((char)(n.charAt(++i)-32));
+					else
+						System.out.print((char)n.charAt(i));
+				}
+			}else {System.out.print(n);}
+		}else {
+			for(;i<n.length();i++) {
+				if(n.charAt(i)<97)
+					System.out.print('_'+""+(char)(n.charAt(i)+32));
+				else
+					System.out.print((char)n.charAt(i));
 			}
 		}
 	}
