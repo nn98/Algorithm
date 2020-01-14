85256b23e4c937f685ea8a9aa6a8f48d3d9b00a5
nn98 <jkllhgb@gmail.com>
2020-01-15 03:46:59 +0900

    7572

--- src/BaekJoon/P7572.java
+++ src/BaekJoon/P7572.java
@@ -3,6 +3,7 @@ package BaekJoon;
 public class P7572{
 	public static void main(String[] args){
 		int n=new java.util.Scanner(System.in).nextInt()-4;
+		if(n<0)n+=60;
 		System.out.print((char)(n%12+'A')+""+n%10);
 	}
 }
