3b7f87c67e30fa563680aaeaa3c5a4db442cb879
nn98 <jkllhgb@gmail.com>
2020-01-15 03:27:23 +0900

    7572

--- src/BaekJoon/P7572.java
+++ src/BaekJoon/P7572.java
@@ -1,8 +1,7 @@
 package BaekJoon;
 
-public class P7572 {
-
-	public static void main(String[] args) {
+public class P7572{
+	public static void main(String[] args){
 		int n=new java.util.Scanner(System.in).nextInt()-4;
 		System.out.print((char)(n%12+'A')+""+n%10);
 	}
