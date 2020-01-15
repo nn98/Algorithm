73951e5591e2ab20619e2c0b20de731afead611e
nn98 <jkllhgb@gmail.com>
2020-01-15 20:20:43 +0900

    2783

--- src/BaekJoon/P2783.java
+++ src/BaekJoon/P2783.java
@@ -1,8 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2783 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		double r=s.nextDouble()*100/s.nextDouble();
+		System.out.println(r);
 	}
 
 }
