9f53cd70a36238eea3f04e7322a67a44c5122d4b
nn98 <jkllhgb@gmail.com>
2020-01-15 20:20:51 +0900

    2783

--- src/BaekJoon/P2783.java
+++ src/BaekJoon/P2783.java
@@ -6,8 +6,14 @@ public class P2783 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		double r=s.nextDouble()*100/s.nextDouble();
-		System.out.println(r);
+		double p=s.nextDouble(),v=s.nextDouble(),i=0,j=s.nextInt(),r=0;
+		for(;i++<j;) {
+			if(i==1)r=1000/v*p;
+			p=s.nextDouble();
+			v=s.nextDouble();
+			r=r<1000/v*p?r:1000/v*p;;
+		}
+		System.out.printf("%.2f",r);
 	}
 
 }
