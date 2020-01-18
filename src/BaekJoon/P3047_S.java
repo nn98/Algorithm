f44adde258912b9bdbda9ad02894ddb00455bef7
nn98 <jkllhgb@gmail.com>
2020-01-19 01:04:14 +0900

    3047

--- src/BaekJoon/P3047_S.java
+++ src/BaekJoon/P3047_S.java
@@ -1,7 +1,7 @@
 package BaekJoon;
 
 import java.util.*;
-public class P3047_S {
+public class P3047_S{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int i=0,a[]=new int[3];
