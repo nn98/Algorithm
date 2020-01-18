3598cee01ca94248b571ce7936f70f525fb893ab
nn98 <jkllhgb@gmail.com>
2020-01-18 23:44:44 +0900

    8320

--- src/BaekJoon/P8320_S.java
+++ src/BaekJoon/P8320_S.java
@@ -1,5 +1,11 @@
 package BaekJoon;
 
 public class P8320_S {
-
+	public static void main(String[] zzzz){
+		int N=new java.util.Scanner(System.in).nextInt(),c=0,i=1,j;
+		for(;i<=N;i++) 
+			for(j=i;j*i<=N;j++) 
+				c++;
+		System.out.print(c);
+	}
 }
