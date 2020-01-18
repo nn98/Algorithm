88af64b06ab217c61343908a3373e8292e27b7b2
nn98 <jkllhgb@gmail.com>
2020-01-18 23:52:52 +0900

    13225

--- src/BaekJoon/P13225.java
+++ src/BaekJoon/P13225.java
@@ -1,8 +1,20 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P13225 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),v,c,j;
+		for(;n-->0;) {
+			v=s.nextInt();
+			c=0;
+			for(j=1;j<v/2;j++)if(v%j==0)c+=2;
+			if(Math.sqrt(v)%1==0)c++;
+			System.out.println(n+" "+c);
+		}
+		
 	}
 
 }
