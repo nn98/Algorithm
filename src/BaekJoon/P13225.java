e346ab549b8e8af71f56b4df4348b57226095360
nn98 <jkllhgb@gmail.com>
2020-01-18 23:58:47 +0900

    13225

--- src/BaekJoon/P13225.java
+++ src/BaekJoon/P13225.java
@@ -10,11 +10,18 @@ public class P13225 {
 		for(;n-->0;) {
 			v=s.nextInt();
 			c=0;
-			for(j=1;j<=v/2;j++)if(v%j==0)if(j*j==v)c++;else c+=2;
+			for(j=1;j<v/2;j++){
+				if(v%j==0) {
+					if(j*j==v)
+						c++;
+					else c+=2;
+					System.out.println(j);
+				}
+			}
 			if(v==1)c=1;
 			System.out.println(v+" "+c);
 		}
-		
+
 	}
 
 }
