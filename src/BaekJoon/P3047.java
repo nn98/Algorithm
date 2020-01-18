25c367b32b19c34c95780f5114e724d118766d2a
nn98 <jkllhgb@gmail.com>
2020-01-19 00:58:20 +0900

    3047

--- src/BaekJoon/P3047.java
+++ src/BaekJoon/P3047.java
@@ -1,10 +1,7 @@
 package BaekJoon;
 
-import java.util.Arrays;
 import java.util.Scanner;
-
 public class P3047 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=0,a[]=new int[3],b[]=new int[3],x=0,y=0;
@@ -16,8 +13,6 @@ public class P3047 {
 		b[0]=a[y];
 		b[2]=a[x];
 		b[1]=a[3-x-y];
-//		System.out.println(Arrays.toString(a));
-//		System.out.println(Arrays.toString(b));
 		s.nextLine();
 		for(char t:s.nextLine().toCharArray())
 			System.out.print(b[t-'A']+" ");
