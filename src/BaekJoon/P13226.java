1a634ca7bec14ffaeb7c4c9f66880a14f1632dec
nn98 <jkllhgb@gmail.com>
2020-01-19 00:21:53 +0900

    13226

--- src/BaekJoon/P13226.java
+++ src/BaekJoon/P13226.java
@@ -5,8 +5,14 @@ class P13226{
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),v=1,c,j,a[]=new int[10000001];
-		for(;v<10000001;v++)
-			for(c=1;c<10000000/v;a[v*c++]++);
+		for (int i = 1; i <= 10000000; i++) { 
+			for (j = 1; j <= 10000000 / i; j++) { 
+				a[i * j]++; 
+				}
+			}
+
+		//		for(;v<10000001;v++)
+		//			for(c=1;c<10000000/v;a[v*c++]++);
 		for(int i=9999900;i<=10000000;i++)
 			System.out.println(a[i]);
 		for(;n-->0;) {
