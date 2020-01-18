4b6d64b3e113c713ad738061ab346c53d50a55fe
nn98 <jkllhgb@gmail.com>
2020-01-19 00:16:12 +0900

    13226

--- src/BaekJoon/P13226.java
+++ src/BaekJoon/P13226.java
@@ -1,27 +1,25 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
-public class P13226 {
-
-		public static void main(String[] args) {
-			Scanner s=new Scanner(System.in);
-			int n=s.nextInt(),v,c,j;
-			for(;n-->0;) {
-				v=s.nextInt();
-				c=0;
-				boolean[]a=new boolean[v+1];
-				for(j=1;j<=v/2;j++){
-					if(v%j==0) {
-						if(a[j])continue;
-						a[j]=true;
-						a[v/j]=true;
-						c+=2;
-						if(j*j==v)c--;
-					}
+import java.util.*;
+class P13226{
+	public static void main(String[]z){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),v,c,j;
+		for(;n-->0;) {
+			v=s.nextInt();
+			c=0;
+			boolean[]a=new boolean[v+1];
+			for(j=1;j<=v/2;j++){
+				if(v%j==0) {
+					if(a[j])continue;
+					a[j]=true;
+					a[v/j]=true;
+					c+=2;
+					if(j*j==v)c--;
 				}
-				System.out.println(v+" "+(v==1?v:c));
 			}
+			System.out.println(v+" "+(v==1?v:c));
 		}
-	
+	}
+
 }
