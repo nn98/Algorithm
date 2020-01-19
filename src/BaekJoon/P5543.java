6dfb7bc79a7f6bd42ba7cd5a14f01c64545207ee
nn98 <jkllhgb@gmail.com>
2020-01-20 03:32:08 +0900

    5543

--- src/BaekJoon/P5543.java
+++ src/BaekJoon/P5543.java
@@ -1,10 +1,8 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class P5543 {
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,m=2000,a;
 		for(;i++<2;a=s.nextInt(),n=n<a?n:a);
