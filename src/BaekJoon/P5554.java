46d08017e6d0d4d4fde69ef552dcf9a629062b54
nn98 <jkllhgb@gmail.com>
2020-01-20 03:26:39 +0900

    5554

---
+++ src/BaekJoon/P5554.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P5554 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=0,i=0;
+		for(;i++<4;r+=s.nextInt());
+		System.out.print(r/60+"\n"+r%60);
+	}
+
+}
