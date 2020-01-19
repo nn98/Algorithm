efdd5d429c9f58d64ef62b00b177043935dbe386
nn98 <jkllhgb@gmail.com>
2020-01-20 03:50:40 +0900

    5568

--- src/BaekJoon/P5568.java
+++ src/BaekJoon/P5568.java
@@ -1,11 +1,18 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class P5568 {
-
+	
+	static String[]a;
+	static int n,i=0;
+	static HashSet<String> s=new HashSet();
 	public static void main(String[] args) {
-		HashSet<String> s=new HashSet();
+		Scanner s=new Scanner(System.in);
+		a=new String[n=s.nextInt()];
+		for(i=s.nextInt();n>0;a[--n]=s.next());
+		System.out.println(Arrays.toString(a));
 	}
-
 }
