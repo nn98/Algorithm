a72d46ae837b0d65997993fbb57bfdcc2fc08308
nn98 <jkllhgb@gmail.com>
2020-01-20 03:35:02 +0900

    5543

--- src/BaekJoon/P5543_S.java
+++ src/BaekJoon/P5543_S.java
@@ -1,10 +1,7 @@
 package BaekJoon;
 
-public class P5543_S {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class P5543_S{
+	public static void main(String[] args){
+		System.out.println();
 	}
-
 }
