ec9f16ad4f5504aa9598492f902b57c0f1cb6549
nn98 <jkllhgb@gmail.com>
2020-01-20 23:35:55 +0900

    2246

--- src/BaekJoon/P2246_2.java
+++ src/BaekJoon/P2246_2.java
@@ -1,10 +1,26 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2246_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],b[]=new int[n];
+		for(int i=0;i<n;i++) {
+			a[i]=s.nextInt();
+			b[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			boolean c=true;
+			for(int j=0;j<n;j++) {
+				if(i!=j) {
+					System.out.println(a[i]<a[j]&&b[i]>b[j]);
+					System.out.println(a[i]>a[j]&&b[i]<b[j]);
+					c=(a[i]<a[j]&&b[i]>b[j])||(a[i]>a[j]&&b[i]<b[j]);
+				}
+			}
+		}
 	}
 
 }
