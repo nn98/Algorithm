cb7d23dc55f849e9ebf6f0a10d5eba8f3d286119
nn98 <jkllhgb@gmail.com>
2020-01-20 23:47:19 +0900

    2246

--- src/BaekJoon/P2246_2.java
+++ src/BaekJoon/P2246_2.java
@@ -3,7 +3,6 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P2246_2 {
-
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],r=0;
