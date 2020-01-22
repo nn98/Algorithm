2999bf3de4f7c94b8e9a0e51252b2fac14d34b6b
nn98 <jkllhgb@gmail.com>
2020-01-23 08:46:47 +0900

    9295

---
+++ src/BaekJoon/P9295.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P9295 {
+
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0;
+		for(;i++<n;System.out.println("Case "+i+": "+(s.nextInt()+s.nextInt())));
+	}
+}
