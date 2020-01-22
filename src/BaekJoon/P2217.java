228b78268e7f0e7014e261d174ef841676f2d9e1
nn98 <jkllhgb@gmail.com>
2020-01-22 23:53:21 +0900

    2217

--- src/BaekJoon/P2217.java
+++ src/BaekJoon/P2217.java
@@ -8,10 +8,10 @@ public class P2217 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],i=0,v,r;
-		for(;i<n;a[i]=s.nextInt());
+		for(;i<n;a[i++]=s.nextInt());
 		Arrays.sort(a);
 		v=r=a[--i];
-		
+		System.out.println(v);
 	}
 
 }
