5648730b647557c56a43e8a2d93121abda64217f
nn98 <jkllhgb@gmail.com>
2020-01-23 08:55:25 +0900

    10833

--- src/BaekJoon/P10833.java
+++ src/BaekJoon/P10833.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class P10833 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),r=0,i;
