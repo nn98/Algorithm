d6bea41220419b22385ff3f8257bc44c2253703b
nn98 <jkllhgb@gmail.com>
2020-01-23 08:36:27 +0900

    2921

--- src/BaekJoon/P2921.java
+++ src/BaekJoon/P2921.java
@@ -3,8 +3,8 @@ package BaekJoon;
 public class P2921 {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),r=0,a=3,b;
-		for(;n-->0;b=r,r+=(a+b),a+=3);
+		int n=new java.util.Scanner(System.in).nextInt(),r=0,a=3,b=0;
+		for(;n-->0;b=a+b,r+=(a+b),a+=3);
 		System.out.print(r);
 	}
 
