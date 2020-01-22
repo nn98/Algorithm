895f98321d961408223ba4c2a30cfb7197ac5acc
nn98 <jkllhgb@gmail.com>
2020-01-23 00:56:14 +0900

    2252

--- src/BaekJoon/P2252.java
+++ src/BaekJoon/P2252.java
@@ -1,8 +1,35 @@
 package BaekJoon;
 
+import java.util.*;
+
 public class P2252 {
 
-	public static void main(String[] args) {
+	static int n,m,a[],i=0,N,p,q;
+	static List<List<Integer>>l=new ArrayList();
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		N=n=s.nextInt();
+		m=s.nextInt();
+		a=new int[++N];
+		for(;i++<N;l.add(new ArrayList()));
+		for(i=0;i++<m;) {
+			p=s.nextInt();
+			q=s.nextInt();
+			l.get(p).add(q);
+			a[q]++;
+		}
+		o();
 	}
-
+	static void o() {
+		Queue<Integer>q=new LinkedList();
+		for(i=1;i<N;i++)if(a[i]==0)q.add(i);
+		for(i=0;i<n;i++) {
+			int v=q.remove();
+			System.out.print(v+" ");
+			for(int V:l.get(v)) {
+				a[V]--;
+				if(a[V]==0)q.add(V);
+			}
+		}
+	}
 }
