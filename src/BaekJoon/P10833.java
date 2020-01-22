6edfeec77525f2c77bfe6328bf4e7646b462b010
nn98 <jkllhgb@gmail.com>
2020-01-23 08:55:15 +0900

    10833

---
+++ src/BaekJoon/P10833.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P10833 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r=0,i;
+		for(;n-->0;i=s.nextInt(),r+=s.nextInt()%i);
+		System.out.print(r);
+	}
+
+}
