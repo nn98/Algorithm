79e9b3a165b096342f2afee98c9011beb3a441d1
nn98 <jkllhgb@gmail.com>
2020-01-23 15:16:12 +0900

    3460

--- src/BaekJoon/P3460.java
+++ src/BaekJoon/P3460.java
@@ -9,14 +9,13 @@ public class P3460 {
 		int n=s.nextInt(),m,i,j;
 		for(;n-->0;) {
 			m=s.nextInt();
-			for(i=0,j=1;j<m;i++) {
-				if(m%j==0) {
-					System.out.print(i+" ");
-					m-=j;
-				}
-				j*=2;
+			StringBuffer b=new StringBuffer();
+			for(j=1,i=0;j<m;j*=2,i++);
+			System.out.println(j);
+			for(;i>=0;j/=2,i--)if(m%j==0) {
+				System.out.print(" "+i);
+				m%=j;
 			}
 		}
 	}
-
 }
