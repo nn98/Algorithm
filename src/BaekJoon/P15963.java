541fd99e46ef93d977c266df42a39e770976386b
nn98 <jkllhgb@gmail.com>
2020-01-23 09:04:49 +0900

    15963

---
+++ src/BaekJoon/P15963.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P15963 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print(s.nextInt()==s.nextInt()?1:0);
+	}
+
+}
