f007e320ce6c040a5158b9f75027b328cdf67455
nn98 <jkllhgb@gmail.com>
2020-01-23 15:25:19 +0900

    3460

--- src/BaekJoon/P3460.java
+++ src/BaekJoon/P3460.java
@@ -10,7 +10,7 @@ public class P3460 {
 		for(;n-->0;) {
 			m=s.nextInt();
 			StringBuffer b=new StringBuffer();
-			for(j=1,i=0;j<m;j*=2,i++);
+			for(j=1,i=0;j<=m;j*=2,i++);
 			j/=2;
 			for(;--i>=0;j/=2) {
 				b.append(m/j==1?" "+i:"");
