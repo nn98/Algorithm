5b79f67752371e715c6d4dcc52bbb08cf6b30c84
nn98 <jkllhgb@gmail.com>
2020-01-23 15:25:59 +0900

    3460

--- src/BaekJoon/P3460_2.java
+++ src/BaekJoon/P3460_2.java
@@ -1,5 +1,22 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P3460_2 {
 
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,i,j;
+		for(;n-->0;) {
+			m=s.nextInt();
+			StringBuffer b=new StringBuffer();
+			for(j=1,i=0;j<=m;j*=2,i++);
+			j/=2;
+			for(;--i>=0;j/=2) {
+				b.append(m/j==1?" "+i:"");
+				m%=j;
+			}
+			System.out.println(b.reverse());
+		}
+	}
 }
