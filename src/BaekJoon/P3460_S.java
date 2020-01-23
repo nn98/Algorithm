9719d2aacc462b270f73668c82717f5ef0997d6f
nn98 <jkllhgb@gmail.com>
2020-01-23 15:39:41 +0900

    3460

--- src/BaekJoon/P3460_S.java
+++ src/BaekJoon/P3460_S.java
@@ -1,20 +1,17 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
-public class P3460_S {
+import java.util.*;
+public class P3460_S{
 	public static void main(String[]z){
 		Scanner sc=new Scanner(System.in);
-		int T=sc.nextInt();
-		while(T-->0) {
-			int n=sc.nextInt(), i=0;
-			while(n>0) {
-				if((n&1)==1)
-					System.out.print(i+" ");
+		int T=sc.nextInt(),n,i;
+		while(T-->0){
+			n=sc.nextInt();i=0;
+			while(n>0){
+				if((n&1)==1)System.out.print(i+" ");
 				i++;
 				n = n>>1;
 			}
-			System.out.println();
 		}
 	}
 }
