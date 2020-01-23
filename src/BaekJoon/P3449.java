07951f8a772af2630238d7054f1435f4f0d314ee
nn98 <jkllhgb@gmail.com>
2020-01-23 14:59:21 +0900

    3449

--- src/BaekJoon/P3449.java
+++ src/BaekJoon/P3449.java
@@ -1,10 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P3449 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r,i;
+		for(;n-->0;) {
+			String a=s.next(),b=s.next();
+			r=i=0;
+			for(;i<a.length();i++){
+				r+=a.charAt(i)==b.charAt(i)?1:0;
+			}
+		}
 	}
 
 }
