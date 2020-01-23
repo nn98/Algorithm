110e54a802e866d4e54aba81e0b194243f0437fa
nn98 <jkllhgb@gmail.com>
2020-01-23 15:09:04 +0900

    3460

--- src/BaekJoon/P3460.java
+++ src/BaekJoon/P3460.java
@@ -6,8 +6,14 @@ public class P3460 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0;
-		
+		int n=s.nextInt(),m,i,j;
+		for(;n-->0;) {
+			m=s.nextInt();
+			for(i=0,j=2;j>m;i++) {
+				if(m%j!=0)System.out.print(i+" ");
+				j*=2;
+			}
+		}
 	}
 
 }
