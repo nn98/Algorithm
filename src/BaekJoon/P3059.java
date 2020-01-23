b9471599e72b2ee7f3fee9c2881cbba2a8f458fb
nn98 <jkllhgb@gmail.com>
2020-01-23 14:16:27 +0900

    3059

--- src/BaekJoon/P3059.java
+++ src/BaekJoon/P3059.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P3059 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int n=s.nextInt(),a[],r,j;
+		for(;n-->0;) {
+			a=new int[91];
+			for(char i:s.next().toCharArray())a[i]++;
+			r=0;
+			for(j=65;j<91;r+=a[j]==1?j:0);
+			System.out.println(r);
+		}
 	}
-
 }
