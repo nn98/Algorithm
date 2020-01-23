267150dd0d0e5cd75bded1c9c693096592b930fb
nn98 <jkllhgb@gmail.com>
2020-01-23 10:46:24 +0900

    10214

--- src/BaekJoon/P10214.java
+++ src/BaekJoon/P10214.java
@@ -8,6 +8,9 @@ public class P10214 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i,a,b;
 		for(;n-->0;) {
+			a=0;
+			b=0;
+			for(i=0;i++<9;a+=s.nextInt(),b+=s.nextInt());
 			
 		}
 	}
