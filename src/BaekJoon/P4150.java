58738c8bbcb1681af2c912595e4e80c1913f25af
nn98 <jkllhgb@gmail.com>
2020-01-23 15:47:29 +0900

    4150

--- src/BaekJoon/P4150.java
+++ src/BaekJoon/P4150.java
@@ -1,5 +1,17 @@
 package BaekJoon;
 
-public class P4150 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class P4150 {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=2;
+		BigInteger a=BigInteger.ZERO,b=BigInteger.ONE;
+		for(;i<=n;i++) {
+			b=a.add(b);
+			a=b.subtract(a);
+		}
+		System.out.print(n<3?1:b);
+	}
 }
