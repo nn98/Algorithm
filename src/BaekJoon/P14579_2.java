7eb916e18bc7740f358989c3a6537b23511d2d2f
nn98 <jkllhgb@gmail.com>
2020-01-25 00:05:29 +0900

    14579

--- src/BaekJoon/P14579_2.java
+++ src/BaekJoon/P14579_2.java
@@ -16,8 +16,8 @@ public class P14579_2 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=1001,a[]=new int[i],j=1;
 		for(;j<i;a[j]=j+a[j-1],j++);
-		BigInteger r=new BigInteger(""+a[n]);
-		for(;n++<m;r=r.multiply(new BigInteger(""+a[n])));
+		BigInteger r=BigInteger.ONE;
+		for(;n<=m;r=r.multiply(new BigInteger(""+a[n++])));
 		System.out.print(r.mod(new BigInteger("14579")));
 	}
 }
