1c0b9df246e8b5de67108eb4d3eefea8e14366fb
nn98 <jkllhgb@gmail.com>
2020-01-25 00:00:38 +0900

    14579

--- src/BaekJoon/P14579_2.java
+++ src/BaekJoon/P14579_2.java
@@ -1,10 +1,15 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P14579_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int n=s.nextInt(),m=s.nextInt(),i=1001,a[]=new int[i],j=1;
+		for(;j<i;a[j]=j+a[j-1],j++);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
