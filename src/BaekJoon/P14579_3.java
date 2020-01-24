709e144d1e9f9330441218426144c20144a63b0a
nn98 <jkllhgb@gmail.com>
2020-01-25 00:06:53 +0900

    14579

--- src/BaekJoon/P14579_3.java
+++ src/BaekJoon/P14579_3.java
@@ -1,5 +1,15 @@
 package BaekJoon;
 
-public class P14579_3 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class P14579_3 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=1001,a[]=new int[i],j=1;
+		for(;j<i;a[j]=j+a[j-1],j++);
+		long r=1;
+		for(;n<=m;r*=a[n++],r%=14579);
+		System.out.print(r);
+	}
 }
