1f8d2d727e21667ceb9761d8498a0d36a19a0a3a
nn98 <jkllhgb@gmail.com>
2020-01-24 23:40:20 +0900

    14579

--- src/BaekJoon/P14579.java
+++ src/BaekJoon/P14579.java
@@ -1,8 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P14579 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],v=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1);
+		System.out.println(v);
 	}
 
 }
