49cd24965fda2d954ad80d517a051f6cd24dc25a
nn98 <jkllhgb@gmail.com>
2020-01-24 23:49:20 +0900

    14579

--- src/BaekJoon/P14579.java
+++ src/BaekJoon/P14579.java
@@ -13,8 +13,9 @@ public class P14579 {
 //		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],i=0;
 //		a[0]=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1);
 //		for(;i++<=m-n;a[i]=a[i-1]+(++n));
-		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],i=0,r=1;
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],i=0,r;
 		a[0]=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1);
+		r=a[i];
 		for(;i++<=m-n;r*=a[i]=a[i-1]+(++n));
 		System.out.print(r);
 	}
