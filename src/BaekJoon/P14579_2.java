35852108107566e29eb1145fe28ea9177d4ac91a
nn98 <jkllhgb@gmail.com>
2020-01-25 00:03:38 +0900

    14579

--- src/BaekJoon/P14579_2.java
+++ src/BaekJoon/P14579_2.java
@@ -7,6 +7,12 @@ import java.util.Scanner;
 public class P14579_2 {
 
 	public static void main(String[] args) {
+//		Scanner s=new Scanner(System.in);
+//		int n=s.nextInt(),m=s.nextInt(),i=1001,a[]=new int[i],j=1;
+//		for(;j<i;a[j]=j+a[j-1],j++);
+//		BigInteger r=new BigInteger(""+a[n]);
+//		for(;n++<m;r=r.multiply(new BigInteger(""+a[n])));
+//		System.out.print(r.mod(new BigInteger("14579")));
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=1001,a[]=new int[i],j=1;
 		for(;j<i;a[j]=j+a[j-1],j++);
