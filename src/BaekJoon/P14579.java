333219f19ddda1603f49a59a6a808834a7d5b3c4
nn98 <jkllhgb@gmail.com>
2020-01-24 23:43:39 +0900

    14579

--- src/BaekJoon/P14579.java
+++ src/BaekJoon/P14579.java
@@ -7,8 +7,9 @@ public class P14579 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-//		v=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1)
-		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],i=0;
+//		
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m-n+1],i=0,v=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1);
+		System.out.println(v);
 		a[0]=n%2==0?(1+n)*(n/2):(1+n)*(n/2)+(n/2+1);
 		for(;i++<m-n;a[i]=a[i-1]+++n);
 		System.out.println(Arrays.toString(a));
