67a3da08a356439ec859771d4aa4f78eb0a29593
nn98 <jkllhgb@gmail.com>
2020-01-27 08:09:21 +0900

    11726

--- src/BaekJoon/P11726.java
+++ src/BaekJoon/P11726.java
@@ -6,7 +6,7 @@ public class P11726 {
 		int i=3,s=1001,a[]=new int[s];
 		a[1]=1;
 		a[2]=2;
-		for(;i<s;a[i++]=a[i-1]+a[i-2]);
+		for(;i<s;a[i++]=(a[i-1]+a[i-2])%10007);
 		System.out.print(a[new java.util.Scanner(System.in).nextInt()]);
 	}
 }
