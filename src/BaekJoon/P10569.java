0739c35693917a745782e02fb4802b707aa55a89
nn98 <jkllhgb@gmail.com>
2020-01-26 15:34:02 +0900

    10569

--- src/BaekJoon/P10569.java
+++ src/BaekJoon/P10569.java
@@ -1,13 +1,10 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P10569 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a,b;
 		for(;n-->0;a=s.nextInt(),b=s.nextInt(),System.out.println((a-b-2)*-1));
 	}
-
 }
