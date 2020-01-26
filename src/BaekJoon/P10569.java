aa5d9e7cb56a7099336d803db50857815999d7d2
nn98 <jkllhgb@gmail.com>
2020-01-26 15:33:41 +0900

    10569

--- src/BaekJoon/P10569.java
+++ src/BaekJoon/P10569.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10569 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a=s.nextInt(),b=s.nextInt();
+		for(;--n>0;System.out.println((a-b-2)*-1),a=s.nextInt(),b=s.nextInt());
 	}
 
 }
