9c705b9003607d56c06a194eda94310db26f36f7
nn98 <jkllhgb@gmail.com>
2020-01-26 15:28:36 +0900

    11758

--- src/BaekJoon/P11758.java
+++ src/BaekJoon/P11758.java
@@ -6,7 +6,12 @@ public class P11758 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt(),r=a*d+;
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt(),r=a*d+c*f+e*b;
+		r=r-b*c-d*e-f*a;
+		if(r>0)r=1;
+		else if(r<0)r=-1;
+		else r=0;
+		System.out.print(r);
 		
 	}
 
