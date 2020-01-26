fb136b9665c6b48bfdfc6a101c80e150affd05ca
nn98 <jkllhgb@gmail.com>
2020-01-26 15:29:55 +0900

    11758

--- src/BaekJoon/P11758.java
+++ src/BaekJoon/P11758.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P11758 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt(),r=a*d+c*f+e*b;
