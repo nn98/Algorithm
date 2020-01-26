55b1e4ccd242fa0e2bfdcb6d1a1f2df7f722c5a3
nn98 <jkllhgb@gmail.com>
2020-01-26 16:15:40 +0900

    12755

--- src/BaekJoon/P12755_S.java
+++ src/BaekJoon/P12755_S.java
@@ -3,7 +3,13 @@ package BaekJoon;
 public class P12755_S {
 
 	public static void main(String[] args) {
-		int n;
+		int n=new java.util.Scanner(System.in).nextInt(),i=0,l;
+		while(n>0) {
+			i++;
+			String s=""+i;
+			n-=l=s.length();
+			if(n<=0)System.out.print(s.charAt(l-1+n));
+		}
 	}
 
 }
