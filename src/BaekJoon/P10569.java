cf3dbf3b5cae5f54fb785cb2a94528ac07fed8c3
nn98 <jkllhgb@gmail.com>
2020-01-26 15:36:07 +0900

    10569

--- src/BaekJoon/P10569.java
+++ src/BaekJoon/P10569.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class P10569 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a,b;
-		for(;n-->0;a=s.nextInt(),b=s.nextInt(),System.out.println((a-b-2)*-1));
+		int n=s.nextInt();
+		for(;n-->0;System.out.println((s.nextInt()-s.nextInt()-2)*-1));
 	}
 }
