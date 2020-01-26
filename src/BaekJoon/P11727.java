cedcfb9d2fb7d7d5de5cd7ee11b598808dee0fae
nn98 <jkllhgb@gmail.com>
2020-01-27 08:14:36 +0900

    11727

--- src/BaekJoon/P11727.java
+++ src/BaekJoon/P11727.java
@@ -1,5 +1,11 @@
 package BaekJoon;
 
 public class P11727 {
-
+	public static void main(String[] args) {
+		int i=2,s=1001,a[]=new int[s];
+		a[1]=1;
+		a[2]=2;
+		for(;++i<s;a[i]=(a[i-1]+a[i-2])%10007);
+		System.out.print(a[new java.util.Scanner(System.in).nextInt()]);
+	}
 }
