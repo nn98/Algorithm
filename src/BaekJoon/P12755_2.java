1751759dc7f2d33e31574b3f78a587dd896382b3
nn98 <jkllhgb@gmail.com>
2020-01-26 16:04:50 +0900

    1275

--- src/BaekJoon/P12755_2.java
+++ src/BaekJoon/P12755_2.java
@@ -1,5 +1,11 @@
 package BaekJoon;
-
+//https://joonas.tistory.com/45
 public class P12755_2 {
-
+	public static void main(String[]z){
+		int n=new java.util.Scanner(System.in).nextInt(),i=0,j=9,v=0,c=0,f=1,x=0,y;
+		for(;v<n;i++,c=v,v+=i*j,j*=10);
+		for(;x<i-1;x++)f*=10;
+		y=n-c-1;
+		System.out.print((""+(f+(x/i))).charAt(x%i));
+	}
 }
