0fd3f312ffdcff6c507a83126f4cae2a114418d4
nn98 <jkllhgb@gmail.com>
2020-01-26 15:25:22 +0900

    11758

--- src/BaekJoon/P11758.java
+++ src/BaekJoon/P11758.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P11758 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt(),r=a*d+;
+		
 	}
 
 }
