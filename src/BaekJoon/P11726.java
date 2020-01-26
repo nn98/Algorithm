028877a897786304b121102399de60be2be60757
nn98 <jkllhgb@gmail.com>
2020-01-27 08:10:11 +0900

    11726

--- src/BaekJoon/P11726.java
+++ src/BaekJoon/P11726.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class P11726 {
 
 	public static void main(String[] args) {
-		int i=3,s=1001,a[]=new int[s];
+		int i=2,s=1001,a[]=new int[s];
 		a[1]=1;
 		a[2]=2;
-		for(;i<s;a[i++]=(a[i-1]+a[i-2])%10007);
+		for(;++i<s;a[i]=(a[i-1]+a[i-2])%10007);
 		System.out.println(Arrays.toString(a));
 		System.out.print(a[new java.util.Scanner(System.in).nextInt()]);
 	}
