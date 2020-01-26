e312eda7e347bcc86a47d4e382371a0b4d743cb0
nn98 <jkllhgb@gmail.com>
2020-01-26 15:57:06 +0900

    12755

--- src/BaekJoon/P12755.java
+++ src/BaekJoon/P12755.java
@@ -4,15 +4,15 @@ public class P12755 {
 	public static void main(String[]z) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=9,v,c;
 		for(;i*j<n;i++,j*=10);
-		System.out.println(i+" "+j);
+//		System.out.println(i+" "+j);
 		i--;
 		j/=10;
 		n-=i*j;
-		System.out.println(n+" "+i+" "+j);
+//		System.out.println(n+" "+i+" "+j);
 		v=(int) Math.pow(10, i++);
-		System.out.println(v);
+//		System.out.println(v);
 		for(;n>i;n-=i,v++);
-		System.out.println(n+" "+v);
+//		System.out.println(n+" "+v);
 		System.out.println((""+v).charAt(n-1));
 	}
 }
