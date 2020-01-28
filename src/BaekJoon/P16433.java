aba472e9450977e7cccd011aac039cf54988831b
nn98 <jkllhgb@gmail.com>
2020-01-28 10:16:25 +0900

    16433

--- src/BaekJoon/P16433.java
+++ src/BaekJoon/P16433.java
@@ -9,7 +9,7 @@ public class P16433 {
 		int n=s.nextInt(),x=s.nextInt()%2,y=s.nextInt()%2,i=0,j;
 		for(;i++<n;) {
 			for(j=0;j++<=n;) {
-				System.out.print(n==j-1?"\n":((i%2^x)^(j%2^y))==1?"v":".");
+				System.out.print(n==j-1?"\n":((i%2^x)^(j%2^y))==0?"v":".");
 			}
 		}
 	}
