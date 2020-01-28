aa3efca8b469341f4fc3a2d9c85e5fedd46ed611
nn98 <jkllhgb@gmail.com>
2020-01-28 16:23:56 +0900

    2875

--- src/BaekJoon/P2875.java
+++ src/BaekJoon/P2875.java
@@ -1,8 +1,15 @@
 package BaekJoon;
 
+import java.util.Scanner;
 public class P2875 {
-
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt(),a=0,b=0,c=0;
+		while((n-a)+(m-b)>k) {
+			a+=2;
+			b++;
+			c++;
+		}
+		System.out.print(c-1);
 	}
-
 }
