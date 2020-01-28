59ccc6bd23ee186975ddb6952f9cf5a7e617a994
nn98 <jkllhgb@gmail.com>
2020-01-28 10:17:03 +0900

    16433

--- src/BaekJoon/P16433.java
+++ src/BaekJoon/P16433.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P16433 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),x=s.nextInt()%2,y=s.nextInt()%2,i=0,j;
