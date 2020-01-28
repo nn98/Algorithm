73643055d44b09c6d035f541ecfbfca4787f6a96
nn98 <jkllhgb@gmail.com>
2020-01-28 21:32:17 +0900

    10799

--- src/BaekJoon/P10799.java
+++ src/BaekJoon/P10799.java
@@ -3,8 +3,17 @@ package BaekJoon;
 public class P10799 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		int i=0,c=0,r=0;
+		for(;i<a.length;i++) {
+			if(a[i]=='(') {
+				if(a[i+1]==')') {
+					r+=c;
+					i++;
+				}
+				else c++;
+			}
+			else c--;
+		}
 	}
-
 }
