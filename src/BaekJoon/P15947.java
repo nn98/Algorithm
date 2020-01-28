6d76468b43e48591558e5a59a1050c09f1317ad8
nn98 <jkllhgb@gmail.com>
2020-01-28 09:27:45 +0900

    15947

--- src/BaekJoon/P15947.java
+++ src/BaekJoon/P15947.java
@@ -1,11 +1,13 @@
 package BaekJoon;
 
+import java.util.Arrays;
+
 public class P15947 {
 
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt();
 		String[]a="baby sukhwan tururu turu very cute tururu turu in bed tururu turu baby sukhwan".split(" ");
-		
+		System.out.println(Arrays.toString(a));
 	}
 
 }
