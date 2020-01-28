ebc889876b523a043a10aea48c1d2ae911840e85
nn98 <jkllhgb@gmail.com>
2020-01-28 10:33:37 +0900

    16212

--- src/BaekJoon/P16212_3.java
+++ src/BaekJoon/P16212_3.java
@@ -1,16 +1,13 @@
 package BaekJoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class P16212_3 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n];
 		for(;n-->0;a[n]=s.nextInt());
 		Arrays.sort(a);
-		for(int i:a);
+		for(int i:a)System.out.print(i+" ");;
 	}
-
 }
