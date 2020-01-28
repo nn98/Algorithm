5e82a11e3fc41c49a6e52992eadf4a3f71502bb1
nn98 <jkllhgb@gmail.com>
2020-01-28 16:45:16 +0900

    10610

--- src/BaekJoon/P10610_2.java
+++ src/BaekJoon/P10610_2.java
@@ -2,9 +2,8 @@ package BaekJoon;
 
 public class P10610_2 {
 
+	static char[]a=new java.util.Scanner(System.in).next().toCharArray();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
