d8295104ec0463cfe8e01299416ceedeb28298aa
nn98 <jkllhgb@gmail.com>
2020-01-28 09:30:30 +0900

    15947

--- src/BaekJoon/P15947.java
+++ src/BaekJoon/P15947.java
@@ -6,8 +6,7 @@ public class P15947 {
 
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt();
-		String[]a="baby sukhwan tururu turu very cute tururu turu in bed tururu turu baby sukhwan".split(" ");
-		System.out.println(a.length);
+		String r="ru",a[]="baby sukhwan tururu turu very cute tururu turu in bed tururu turu baby sukhwan".split(" "),b=a[n%14];
+		System.out.println(b);
 	}
-
 }
