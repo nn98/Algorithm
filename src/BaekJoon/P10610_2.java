2f40302877ac68ac2eefecade7a6d828a5921410
nn98 <jkllhgb@gmail.com>
2020-01-28 16:51:24 +0900

    10610 g

--- src/BaekJoon/P10610_2.java
+++ src/BaekJoon/P10610_2.java
@@ -3,7 +3,11 @@ package BaekJoon;
 public class P10610_2 {
 
 	static char[]a=new java.util.Scanner(System.in).next().toCharArray();
-	public static void main(String[] args) {
+	static boolean[]h;
+	static void o(int x) {
 		
+	}
+	public static void main(String[] args) {
+		for(int i=0;i<a.length;i++);
 	}
 }
