d11d183dc137d2187eaba10173b6b13a53d09a70
nn98 <jkllhgb@gmail.com>
2020-01-28 16:40:17 +0900

    2875

--- src/BaekJoon/P2875_S.java
+++ src/BaekJoon/P2875_S.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class P2875_S {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt();
 		System.out.print(Math.min(Math.min(n/2,m),(n+m-k)/3));
