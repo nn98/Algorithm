40fa265a62e4148f8b3d65dd685f77dbf9b3edeb
nn98 <jkllhgb@gmail.com>
2020-01-29 17:36:09 +0900

    18222

--- src/BaekJoon/P18222.java
+++ src/BaekJoon/P18222.java
@@ -6,9 +6,15 @@ public class P18222 {
 
 	public static void main(String[] args) {
 		BigInteger a=new BigInteger("2"),n=new BigInteger(new java.util.Scanner(System.in).next());
-		int i=0;
+		int i=0,c=0;
 		while(n.compareTo(a.pow(i+1))>=0)i++;
-		System.out.println(a.pow(i));
+		while(n.equals(BigInteger.ZERO)) {
+//			if(n.divide(a.pow(i)).equals(BigInteger.ONE))c++;
+//			c+=Integer.parseInt(n.divide(a.pow(i)).toString());
+			c+=n.divide(a.pow(i)).equals(BigInteger.ONE)?1:0;
+			i--;
+			n=n.mod(a.pow(i--));
+		}
 	}
 
 }
