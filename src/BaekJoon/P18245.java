fb8c512bf4ca45c0971987399026cdffd20ca30c
nn98 <jkllhgb@gmail.com>
2020-01-29 12:07:19 +0900

    18245

--- src/BaekJoon/P18245.java
+++ src/BaekJoon/P18245.java
@@ -7,13 +7,13 @@ public class P18245 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String t;
-		int i=1,j;
-		do {
+		int i=2,j;
+		for(;;) {
 			t=s.next();
 			for(j=0;j<t.length();j+=i)System.out.print(t.charAt(j));
 			i++;
 			System.out.println();
-		}while(!t.equals("Was"));
+		}
 	}
 
 }
