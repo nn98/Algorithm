43544a2f21d84775b1dd6915077dafd6146f0bdc
nn98 <jkllhgb@gmail.com>
2020-01-29 12:08:08 +0900

    18245

--- src/BaekJoon/P18245.java
+++ src/BaekJoon/P18245.java
@@ -10,6 +10,7 @@ public class P18245 {
 		int i=2,j;
 		for(;;) {
 			t=s.next();
+			if(t.equals("Was"))break;
 			for(j=0;j<t.length();j+=i)System.out.print(t.charAt(j));
 			i++;
 			System.out.println();
