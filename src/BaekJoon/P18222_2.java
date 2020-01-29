b70011d77190815563e2a1c2332ed072c9e88e76
nn98 <jkllhgb@gmail.com>
2020-01-30 01:12:36 +0900

    18222

--- src/BaekJoon/P18222_2.java
+++ src/BaekJoon/P18222_2.java
@@ -1,10 +1,12 @@
 package BaekJoon;
 
+import java.math.BigInteger;
+
 public class P18222_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long a=1,n=new java.util.Scanner(System.in).nextLong();
+		for(;a>n;a*=2);
 	}
 
 }
