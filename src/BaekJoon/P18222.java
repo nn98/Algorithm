ef27a81103a019e922e1c4603680ed85efa8e251
nn98 <jkllhgb@gmail.com>
2020-01-29 17:43:41 +0900

    18222

--- src/BaekJoon/P18222.java
+++ src/BaekJoon/P18222.java
@@ -7,13 +7,15 @@ public class P18222 {
 	public static void main(String[] args) {
 		BigInteger a=new BigInteger("2"),n=new BigInteger(new java.util.Scanner(System.in).next());
 		int i=0,c=0;
-		while(n.compareTo(a.pow(i+1))>=0)i++;
-		while(!n.equals(BigInteger.ZERO)) {
-//			if(n.divide(a.pow(i)).equals(BigInteger.ONE))c++;
-//			c+=Integer.parseInt(n.divide(a.pow(i)).toString());
-//			System.out.println(n.divide(a.pow(i)));
-			c+=n.divide(a.pow(i)).equals(BigInteger.ONE)?1:0;
-			n=n.mod(a.pow(i--));
+		if(!n.equals("1")) {
+			while(n.compareTo(a.pow(i+1))>=0)i++;
+			while(!n.equals(BigInteger.ZERO)) {
+				//			if(n.divide(a.pow(i)).equals(BigInteger.ONE))c++;
+				//			c+=Integer.parseInt(n.divide(a.pow(i)).toString());
+				//			System.out.println(n.divide(a.pow(i)));
+				c+=n.divide(a.pow(i)).equals(BigInteger.ONE)?1:0;
+				n=n.mod(a.pow(i--));
+			}
 		}
 		System.out.println(c%2);
 	}
