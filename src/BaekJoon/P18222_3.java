e01ecec0c6f32da16818f3ed64c22371632ddadb
nn98 <jkllhgb@gmail.com>
2020-01-30 12:41:41 +0900

    18222

--- src/BaekJoon/P18222_3.java
+++ src/BaekJoon/P18222_3.java
@@ -4,7 +4,14 @@ public class P18222_3 {
 
 	public static void main(String[] args) {
 		long n=new java.util.Scanner(System.in).nextLong()-1,c=0;
-		
+		while(n>0) {
+			System.out.println(n&1);
+			c+=n&1;
+			System.out.println(n);
+			n>>=1;
+			System.out.println(n);
+		}
+		System.out.println(c);
+		System.out.println(c&1);
 	}
-
 }
