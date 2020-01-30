5b43c6b697df98dd8ee89c4b79ca032a86ed6877
nn98 <jkllhgb@gmail.com>
2020-01-30 18:02:19 +0900

    1789

--- src/BaekJoon/P1789.java
+++ src/BaekJoon/P1789.java
@@ -3,8 +3,6 @@ package BaekJoon;
 public class P1789{
 	public static void main(String[] args){
 		long n=new java.util.Scanner(System.in).nextLong(),i=1,c=0;
-//		for(;n>0;c++,n-=i++);
-//		System.out.print(n<0?c-1:c);
 		for(;n-i>=0;c++,n-=i++);
 		System.out.print(c);
 	}
