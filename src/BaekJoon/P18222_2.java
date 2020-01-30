18e1762715e08e57ae9baf4f415b2c861115029e
nn98 <jkllhgb@gmail.com>
2020-01-30 12:33:34 +0900

    18222

--- src/BaekJoon/P18222_2.java
+++ src/BaekJoon/P18222_2.java
@@ -7,6 +7,6 @@ public class P18222_2{
 		long a=1,n=new java.util.Scanner(System.in).nextLong(),c=0;
 		for(;a<n;a*=2);
 		for(;n>0;c+=n/a,n%=a,a/=2);
-		System.out.println(c);
+		System.out.println(n==1?0:c%2);
 	}
 }
