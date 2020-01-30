ccf59da1f0717c73f1a7811c9edc419421b9b438
nn98 <jkllhgb@gmail.com>
2020-01-31 01:09:52 +0900

    10798

--- src/BaekJoon/P10798.java
+++ src/BaekJoon/P10798.java
@@ -7,8 +7,10 @@ public class P10798 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a="";
-		while(s.hasNext())a+=s.nextLine();
+		while(s.hasNext()) {
+			a+=s.nextLine();
+			if(a.charAt(a.length()-1)=='0')break;
+		}
 		System.out.println(a);
 	}
-
 }
