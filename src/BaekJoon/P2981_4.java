3ad134366ffc9da75e2ccf772028edb91efb20c4
nn98 <jkllhgb@gmail.com>
2020-01-30 18:05:08 +0900

    2981

--- src/BaekJoon/P2981_4.java
+++ src/BaekJoon/P2981_4.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2981_4 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=s.nextInt(),c=i;
+		for(;n-->1;) {
+			while(i%c==0)c--;
+			i=s.nextInt();
+		}
+		System.out.println(c);
 	}
 
 }
