db80ca41c05b336bc96ad7f4d5bfdb1513f3855c
nn98 <jkllhgb@gmail.com>
2020-01-31 00:59:19 +0900

    5063

--- src/BaekJoon/P5063.java
+++ src/BaekJoon/P5063.java
@@ -1,10 +1,15 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5063 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String a="advertise",b="does not matter",c="do not "+a;
+		int n=s.nextInt(),m;
+		for(;n-->0;) {
+			m=s.nextInt()-s.nextInt()+s.nextInt();
+			System.out.println(m==0?b:m<0?a:c);
+		}
 	}
-
 }
