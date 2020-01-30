2d63fa9f3839909f88a362404eac551c25559e8f
nn98 <jkllhgb@gmail.com>
2020-01-30 12:38:15 +0900

    18222

--- src/BaekJoon/P18222_3.java
+++ src/BaekJoon/P18222_3.java
@@ -3,8 +3,8 @@ package BaekJoon;
 public class P18222_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long n=new java.util.Scanner(System.in).nextLong()-1,c=0;
+		
 	}
 
 }
