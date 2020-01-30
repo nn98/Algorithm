3ea70baea23e960db7d3b65a88b3e388fa4c7834
nn98 <jkllhgb@gmail.com>
2020-01-31 01:20:09 +0900

    10798

--- src/BaekJoon/P10798.java
+++ src/BaekJoon/P10798.java
@@ -8,9 +8,20 @@ public class P10798 {
 		Scanner s=new Scanner(System.in);
 		String a="";
 		while(s.hasNext()) {
-			a+=s.nextLine();
+			a+=s.nextLine()+" ";
 			if(a.charAt(a.length()-1)=='0')break;
 		}
-		System.out.println(a);
+		String[]b=a.split(" ");
+		int i=0,j,c;
+		for(;;i++) {
+			c=0;
+			for(j=0;j<b.length;j++)
+				if(i<b[j].length()) {
+					System.out.print(b[j].charAt(i));
+					c=1;
+				}
+			if(c==0)break;
+		}
+//		System.out.println(a);
 	}
 }
