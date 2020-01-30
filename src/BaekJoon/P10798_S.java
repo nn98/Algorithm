f3748e0b3f368c3b62402473bc7bc0289d4c7207
nn98 <jkllhgb@gmail.com>
2020-01-31 01:28:35 +0900

    10798

--- src/BaekJoon/P10798_S.java
+++ src/BaekJoon/P10798_S.java
@@ -1,10 +1,21 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10798_S {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String[]b=new String[15];
+		int i=0,j,c;
+		while(s.hasNext())b[i++]=s.next();
+		for(i=0;;i++) {
+			c=0;
+			for(j=0;j<b.length&&b[j]!=null;j++)
+				if(i<b[j].length()) {
+					System.out.print(b[j].charAt(i));
+					c=1;
+				}
+			if(c==0)break;
+		}
 	}
-
 }
