d13a0b7b95db8b19147db160b834fb36fd401a9c
nn98 <jkllhgb@gmail.com>
2020-01-31 00:34:50 +0900

    2965

--- src/BaekJoon/P2965.java
+++ src/BaekJoon/P2965.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2965 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=0,a[]=new int[3],v;
+		for(;i<3;a[i++]=s.nextInt());
+		Arrays.sort(a);
+		v=a[1]=a[0];
+		v=v>a[2]-a[1]?v:a[2]-a[1];
 	}
-
 }
