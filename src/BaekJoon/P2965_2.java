2dff598778d7e28a36b2e1a2547246c9cb1c5053
nn98 <jkllhgb@gmail.com>
2020-01-31 00:38:57 +0900

    2965

--- src/BaekJoon/P2965_2.java
+++ src/BaekJoon/P2965_2.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2965_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
+		System.out.print(b-a>c-b?b-a-1:c-b-1);
 	}
 
 }
