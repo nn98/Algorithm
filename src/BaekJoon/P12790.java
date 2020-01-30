ee037db5c34fd6b58d291dcf1f1e5292c6493820
nn98 <jkllhgb@gmail.com>
2020-01-31 01:56:20 +0900

    12790

--- src/BaekJoon/P12790.java
+++ src/BaekJoon/P12790.java
@@ -8,23 +8,12 @@ public class P12790 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[4],i,r;
 		for(;n-->0;) {
-			r=0;
 			for(i=0;i<4;a[i++]=s.nextInt());
 			for(i=0;i<4;a[i++]+=s.nextInt());
-			for(i=0;i++<4;)switch(i) {
-			case 1:
-				r+=a[0]<1?1:a[0];
-				break;
-			case 2:
-				r+=5*(a[1]<1?1:a[1]);
-				break;
-			case 3:
-				r+=2*(a[2]<0?0:a[2]);
-				break;
-			case 4:
-				r+=2*a[3];
-			}
-			System.out.println(r);
+			if(a[0]<1)a[0]=1;
+			if(a[1]<1)a[1]=1;
+			if(a[2]<0)a[2]=0;
+			System.out.println(a[0]+a[1]*5+2*a[2]+2*a[3]);
 		}
 	}
 }
