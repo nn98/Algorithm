5b54295ab8f6010d4b94f753c08d6a915450462a
nn98 <jkllhgb@gmail.com>
2020-01-30 12:46:24 +0900

    18222

--- src/BaekJoon/P18222_3.java
+++ src/BaekJoon/P18222_3.java
@@ -1,17 +1,14 @@
 package BaekJoon;
 
 public class P18222_3 {
-
+//https://sys09270883.github.io/ps/3/
+//bit operation
 	public static void main(String[] args) {
 		long n=new java.util.Scanner(System.in).nextLong()-1,c=0;
 		while(n>0) {
-//			System.out.println(n&1);
 			c+=n&1;
-//			System.out.println(n);
 			n>>=1;
-//			System.out.println(n);
 		}
-//		System.out.println(c);
 		System.out.println(c&1);
 	}
 }
