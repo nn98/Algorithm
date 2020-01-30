f99ed99a143273642f375e91cbc6338fa5ddd355
nn98 <jkllhgb@gmail.com>
2020-01-30 12:28:14 +0900

    18222

--- src/BaekJoon/P18222_2.java
+++ src/BaekJoon/P18222_2.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class P18222_2 {
 	public static void main(String[] args) {
 		long a=1,n=new java.util.Scanner(System.in).nextLong();
-		for(;a>n;a*=2);
+		for(;a<n;a*=2);
 		System.out.println(a);
 	}
 }
