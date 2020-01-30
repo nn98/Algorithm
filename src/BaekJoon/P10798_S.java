f5aa6c8ae6f2a89cf6ce8ea9cde878f124a4403a
nn98 <jkllhgb@gmail.com>
2020-01-31 01:45:21 +0900

    10798

--- src/BaekJoon/P10798_S.java
+++ src/BaekJoon/P10798_S.java
@@ -1,21 +1,11 @@
 package BaekJoon;
 
-import java.util.Scanner;
-
+import java.util.*;
+//ww
 public class P10798_S {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		String[]b=new String[15];
-		int i=0,j,c;
-		while(s.hasNext())b[i++]=s.next();
-		for(i=0;;i++) {
-			c=0;
-			for(j=0;j<b.length&&b[j]!=null;j++)
-				if(i<b[j].length()) {
-					System.out.print(b[j].charAt(i));
-					c=1;
-				}
-			if(c==0)break;
-		}
+		List<String>[]l=new ArrayList[15];
+		
 	}
 }
