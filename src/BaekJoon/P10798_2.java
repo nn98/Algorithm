dcf37d8b66e76f625d7d158753501796b202dab7
nn98 <jkllhgb@gmail.com>
2020-01-31 01:21:39 +0900

    10798

--- src/BaekJoon/P10798_2.java
+++ src/BaekJoon/P10798_2.java
@@ -1,5 +1,22 @@
 package BaekJoon;
 
-public class P10798_2 {
+import java.util.Scanner;
 
+public class P10798_2 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String a="";
+		while(s.hasNext())a+=s.nextLine()+" ";
+		String[]b=a.split(" ");
+		int i=0,j,c;
+		for(;;i++) {
+			c=0;
+			for(j=0;j<b.length;j++)
+				if(i<b[j].length()) {
+					System.out.print(b[j].charAt(i));
+					c=1;
+				}
+			if(c==0)break;
+		}
+	}
 }
