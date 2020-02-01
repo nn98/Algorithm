893ade2f9414a5ab8036a8ad3e1849c40a63eaa9
nn98 <jkllhgb@gmail.com>
2020-02-01 16:20:19 +0900

    11004

--- src/BaekJoon/P11004.java
+++ src/BaekJoon/P11004.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P11004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],m=s.nextInt();
+		for(;n-->0;a[n]=s.nextInt());
+		Arrays.sort(a);
+		System.out.print(a[m-1]);
 	}
 
 }
