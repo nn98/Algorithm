f0177354710b61e6abf86f47cbcba628bbc4d95e
nn98 <jkllhgb@gmail.com>
2020-02-01 11:25:23 +0900

    1406

--- src/BaekJoon/P1406.java
+++ src/BaekJoon/P1406.java
@@ -20,7 +20,8 @@ public class P1406 {
 				if(i!=0)a[i-1]=a[i-1].substring(0,a[i-1].length()-1);
 				break;
 			case "P":
-				a[i]=s.next()+a[i];
+				if(i==a.length)a[i-1]+=s.next();
+				else a[i]=s.next()+a[i];
 				break;
 			}
 		}
