6c1bad7a87993f1fac6957b286ecd505e6d46ed1
nn98 <jkllhgb@gmail.com>
2020-02-01 14:16:11 +0900

    1406

--- src/BaekJoon/P1406_C.java
+++ src/BaekJoon/P1406_C.java
@@ -2,9 +2,15 @@ package BaekJoon;
 
 public class P1406_C {
 
+	class Node{
+		String v;
+		Node n;
+		Node(String v){
+			this.v=v;
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
