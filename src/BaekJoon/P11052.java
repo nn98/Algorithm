427e7e0d4925e08e88f4175c47e344c420e3e6f3
nn98 <jkllhgb@gmail.com>
2020-02-01 15:39:23 +0900

    11052

--- src/BaekJoon/P11052.java
+++ src/BaekJoon/P11052.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 
 public class P11052 {
 
-	static int n,a[],i=0;
-	static void o(int x,int v,int p) {
-		
+	static int n,a[],i=0,r=0;
+	static void o(int x,int v,int c) {
+		if(c==n)r=r>v?r:v;
+		else if(c<n) {
+			for(int j=0;j<n/x;j++)
+				o(x+1,v+a[x]*j,c+x*j);
+		}
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
@@ -15,6 +19,8 @@ public class P11052 {
 		a=new int[n+1];
 		for(;i++<n;a[i]=s.nextInt());
 		System.out.println(Arrays.toString(a));
+		o(1,0,0);
+		System.out.println(r);
 	}
 
 }
