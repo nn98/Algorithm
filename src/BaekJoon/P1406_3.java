d483988ca1d7c31a2d288dc3f1b703d66f93f5a3
nn98 <jkllhgb@gmail.com>
2020-02-01 11:45:28 +0900

    1406

--- src/BaekJoon/P1406_3.java
+++ src/BaekJoon/P1406_3.java
@@ -1,13 +1,11 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 public class P1406_3{
 	public static void main(String[]z)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		String t;
 		List<String>l=new ArrayList(Arrays.asList(r.readLine().split("")));
 		int n=Integer.parseInt(r.readLine()),i=l.size();
@@ -27,6 +25,6 @@ public class P1406_3{
 				break;
 			}	
 		}
-		System.out.print(l.toString());
+		
 	}
 }
