623a91d69972122641a6f0ede15db3ebc979ceba
nn98 <jkllhgb@gmail.com>
2020-02-01 15:33:57 +0900

    11052

--- src/BaekJoon/P11052.java
+++ src/BaekJoon/P11052.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 
 public class P11052 {
 
+	static int n,a[],i=0;
+	static void o(int x,int v,int p) {
+		
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n+1],i=0;
+		n=s.nextInt();
+		a=new int[n+1];
 		for(;i++<n;a[i]=s.nextInt());
 		System.out.println(Arrays.toString(a));
 	}
