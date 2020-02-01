abf64b6f4d056783e0478435d129463505d87cb8
nn98 <jkllhgb@gmail.com>
2020-02-01 15:32:09 +0900

    11052

--- src/BaekJoon/P11052.java
+++ src/BaekJoon/P11052.java
@@ -1,9 +1,15 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P11052 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n+1],i=0;
+		for(;i++<n;a[i]=s.nextInt());
+		System.out.println(Arrays.toString(a));
 	}
 
 }
