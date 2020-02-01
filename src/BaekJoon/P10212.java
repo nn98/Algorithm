69cf4d552e7a11b13f71c24509509baa1e1836bb
nn98 <jkllhgb@gmail.com>
2020-02-01 16:09:39 +0900

    10212

--- src/BaekJoon/P10212.java
+++ src/BaekJoon/P10212.java
@@ -1,9 +1,8 @@
 package BaekJoon;
 
 public class P10212 {
-
 	public static void main(String[] args) {
-		System.out.println(Math.random());
+		System.out.println("Yonsei");
+		System.out.println(Math.random()<.5?"Yonsei":"Korea");
 	}
-
 }
