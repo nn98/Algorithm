780af8736c37805b1d151941bebfe0db12f9e38c
nn98 <jkllhgb@gmail.com>
2020-02-01 11:33:58 +0900

    1406

--- src/BaekJoon/P1406_2.java
+++ src/BaekJoon/P1406_2.java
@@ -1,33 +1,31 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class P1406_2 {
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
-		String t,a[]=s.next().split("");
-		int n=s.nextInt(),i=a.length;
+		String t;
+		List<String>l=Arrays.asList(s.next().split(""));
+		int n=s.nextInt(),i=l.size();
 		for(;n-->0;) {
 			switch(t=s.next()) {
 			case "L":
 				if(i>0)i--;
-				while(a[i]==null)i--;
 				break;
 			case "D":
-//				while(i<a.length)if(a[i+1]!=null)i++;
-				if(i<a.length)i++;
-				if(i==a.length)break;
-				while(a[i]==null)i++;
+				if(i<l.size())i++;
 				break;
 			case "B":
-				if(i!=0)a[i-1]=a[i-1].substring(0,a[i-1].length()-1);
+				l.remove(i-1);
 				break;
 			case "P":
-				if(i==a.length)a[i-1]+=s.next();
-				else a[i]=s.next()+a[i];
+				l.add(i,s.next());
 				break;
 			}
 		}
-		for(String j:a)System.out.print(j);
+		for(String j:l)System.out.print(j);
 	}
 }
