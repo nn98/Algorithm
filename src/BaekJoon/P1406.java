c8e759a7cf3bfcd0b325bef062bedb4d80c32ef7
nn98 <jkllhgb@gmail.com>
2020-02-01 10:06:29 +0900

    1406

--- src/BaekJoon/P1406.java
+++ src/BaekJoon/P1406.java
@@ -1,8 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1406 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char t,a[]=s.next().toCharArray(),i=s.next();
 	}
 
 }
