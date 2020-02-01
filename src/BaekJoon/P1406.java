7b2b0051b0d7baefc26116ebcef1897199ba56ab
nn98 <jkllhgb@gmail.com>
2020-02-01 11:25:27 +0900

    1406

--- src/BaekJoon/P1406.java
+++ src/BaekJoon/P1406.java
@@ -11,10 +11,14 @@ public class P1406 {
 		for(;n-->0;) {
 			switch(t=s.next()) {
 			case "L":
-				while(i>0)if(a[i-1]!=null)i--;
+				if(i>0)i--;
+				while(a[i]==null)i--;
 				break;
 			case "D":
-				while(i<a.length)if(a[i+1]!=null)i++;
+//				while(i<a.length)if(a[i+1]!=null)i++;
+				if(i<a.length)i++;
+				if(i==a.length)break;
+				while(a[i]==null)i++;
 				break;
 			case "B":
 				if(i!=0)a[i-1]=a[i-1].substring(0,a[i-1].length()-1);
