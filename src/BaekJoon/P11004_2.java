03274eea65efe5a5848ad300bb8b8d3e381db1c2
nn98 <jkllhgb@gmail.com>
2020-02-01 16:38:43 +0900

    11004

--- src/BaekJoon/P11004_2.java
+++ src/BaekJoon/P11004_2.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
-public class P11004_2 {
+public class P11004_2{
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
