e6c0c829bbe37d02238ceb5dcb3c37b0cf1ff9ce
nn98 <jkllhgb@gmail.com>
2020-02-01 14:36:28 +0900

    1406

--- src/BaekJoon/P1406_C.java
+++ src/BaekJoon/P1406_C.java
@@ -1,17 +1,9 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 //https://dundung.tistory.com/6
+//LinkedList + Iterator b
+import java.io.*;
 public class P1406_C {
-
 	static class Node{
 		String v;
 		Node n;
@@ -30,26 +22,8 @@ public class P1406_C {
 		}
 		String t;
 		n=l;
-//		while(n!=null) {
-//			System.out.println(n.v);
-//			n=n.n;
-//		}String t;
 		int m=Integer.parseInt(R.readLine());
 		for(;m-->0;) {
-//			switch(t=r.readLine()){
-//			case "L":
-//				if(i>0)i--;
-//				break;
-//			case "D":
-//				if(i<l.size())i++;
-//				break;
-//			case "B":
-//				if(i>0)l.remove(i---1);
-//				break;
-//			default:
-//				l.add(i++,t.substring(t.length()-1));
-//				break;
-//			}
 			switch(t=R.readLine()){
 			case "L":
 				if(l==null)break;
