624781ba194a6a8f76991da175ba0debb3c7a849
nn98 <jkllhgb@gmail.com>
2020-02-01 14:23:52 +0900

    1406

--- src/BaekJoon/P1406_C.java
+++ src/BaekJoon/P1406_C.java
@@ -1,8 +1,14 @@
 package BaekJoon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+//https://dundung.tistory.com/6
 public class P1406_C {
 
-	class Node{
+	static class Node{
 		String v;
 		Node n;
 		Node(String v){
@@ -10,7 +16,41 @@ public class P1406_C {
 		}
 	}
 	public static void main(String[] args) {
-		
+		Node l=null,r=null,n;
+		Scanner s=new Scanner(System.in);
+		for(String i:s.next().split("")) {
+			n=new Node(i);
+			n.n=l;
+			l=n;
+		}
+		n=l;
+		while(n!=null) {
+			System.out.println(n.v);
+			n=n.n;
+		}
+		String t;
+		int m=s.nextInt();
+		for(;m-->0;) {
+			switch(t=s.next()){
+			case "L":
+				n=l;
+				l=l.n;
+				n.n=r;
+				r=n;
+				break;
+			case "D":
+				n=r;
+				r=r.n;
+				n.n=l;
+				l=n;
+				break;
+			case "B":
+				
+				break;
+			case "P":
+				break;
+			}
+		}
 	}
 
 }
