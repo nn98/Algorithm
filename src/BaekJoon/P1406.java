7d59e4673bdace815a17de8ab22d50afbd9ac439
nn98 <jkllhgb@gmail.com>
2020-02-01 10:06:34 +0900

    1406

--- src/BaekJoon/P1406.java
+++ src/BaekJoon/P1406.java
@@ -6,7 +6,24 @@ public class P1406 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		char t,a[]=s.next().toCharArray(),i=s.next();
+		String t,a[]=s.next().split("");
+		int n=s.nextInt(),i=0;
+		for(;n-->0;) {
+			switch(t=s.next()) {
+			case "L":
+				while(i>0)if(a[i-1]!=null)i--;
+				break;
+			case "D":
+				while(i<a.length)if(a[i+1]!=null)i++;
+				break;
+			case "B":
+				if(i!=0)a[i-1]=a[i-1].substring(0,a[i-1].length()-1);
+				break;
+			case "P":
+				a[i]=s.next()+a[i];
+				break;
+			}
+		}
+		for(String j:a)System.out.print(j);
 	}
-
 }
