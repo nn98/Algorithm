54c27174e325bcdff436f2c40e1c439680529ada
nn98 <jkllhgb@gmail.com>
2020-02-01 16:23:46 +0900

    11002

--- src/BaekJoon/P11004_2.java
+++ src/BaekJoon/P11004_2.java
@@ -1,9 +1,15 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P11004_2 {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken()),a[]=new int[n];
+		t=new StringTokenizer(r.readLine());
+		for(;n-->0;a[n]=Integer.parseInt(t.nextToken()));
 	}
-
 }
