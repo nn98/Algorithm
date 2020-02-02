d0431e40fe280f80c3725b00aeccaa70144487bc
nn98 <jkllhgb@gmail.com>
2020-02-02 19:26:51 +0900

    9465

--- src/BaekJoon/P9465.java
+++ src/BaekJoon/P9465.java
@@ -6,13 +6,13 @@ import java.util.StringTokenizer;
 
 public class P9465 {
 
-	static int n,m,a[][],l,i,j;
-	static boolean[][]h;
+	static int n,m,a[][],h[][],l,i,j;
 	static void o(int x,int v,int c) {
 		if(x==m)l=l>v?l:v;
 		else {
-			o(x+1,v,0);
-			if(c!=1)o(x+1,v+a[0][x],1);
+			h[c][x]=v;
+			if(h[0][x+1]<v)	o(x+1,v,0);
+			if(c!=1)if(h[1][x+1]<v+a[0][x])o(x+1,v+a[0][x],1);
 			if(c!=2)o(x+1,v+a[1][x],2);
 		}
 	}
@@ -22,7 +22,7 @@ public class P9465 {
 		for(;n-->0;) {
 			m=Integer.parseInt(r.readLine());
 			a=new int[2][m];
-			h=new boolean[2][m];
+			h=new int[2][m];
 			for(i=0;i<2;i++) {
 				StringTokenizer t=new StringTokenizer(r.readLine());
 				for(j=0;j<m;a[i][j++]=Integer.parseInt(t.nextToken()));
