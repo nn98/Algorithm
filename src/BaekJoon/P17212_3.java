2a72f5ff6029d61887963324f6782d6e3dd47602
nn98 <jkllhgb@gmail.com>
2020-02-02 22:23:16 +0900

    17212

--- src/BaekJoon/P17212_3.java
+++ src/BaekJoon/P17212_3.java
@@ -1,10 +1,10 @@
 package BaekJoon;
+//https://m.blog.naver.com/PostView.nhn?blogId=coke_mania&logNo=221551776740&proxyReferer=https%3A%2F%2Fwww.google.com%2F
 
 public class P17212_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
 	}
 
 }
