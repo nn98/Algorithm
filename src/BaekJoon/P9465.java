25a51eb3e333f976dfb584f427f22ab1216c7664
nn98 <jkllhgb@gmail.com>
2020-02-02 19:31:01 +0900

    9465

--- src/BaekJoon/P9465.java
+++ src/BaekJoon/P9465.java
@@ -13,7 +13,7 @@ public class P9465 {
 			h[c][x]=v;
 			if(h[0][x+1]<v)	o(x+1,v,0);
 			if(c!=1)if(h[1][x+1]<v+a[0][x])o(x+1,v+a[0][x],1);
-			if(c!=2)o(x+1,v+a[1][x],2);
+			if(c!=2)if(h[2][x+1]<v+a[1][x])o(x+1,v+a[1][x],2);
 		}
 	}
 	public static void main(String[] args)throws Exception{
@@ -22,7 +22,7 @@ public class P9465 {
 		for(;n-->0;) {
 			m=Integer.parseInt(r.readLine());
 			a=new int[2][m];
-			h=new int[2][m];
+			h=new int[3][m];
 			for(i=0;i<2;i++) {
 				StringTokenizer t=new StringTokenizer(r.readLine());
 				for(j=0;j<m;a[i][j++]=Integer.parseInt(t.nextToken()));
