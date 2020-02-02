a4bbba5a25ce2123fd13d8bb2227002c7206e1a9
nn98 <jkllhgb@gmail.com>
2020-02-02 22:31:17 +0900

    17212

--- src/BaekJoon/P17212_3.java
+++ src/BaekJoon/P17212_3.java
@@ -1,6 +1,5 @@
 package BaekJoon;
 //https://m.blog.naver.com/PostView.nhn?blogId=coke_mania&logNo=221551776740&proxyReferer=https%3A%2F%2Fwww.google.com%2F
-
 import java.util.Scanner;
 public class P17212_3 {
 	public static void main(String[] args) {
