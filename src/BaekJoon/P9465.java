79ce067ab23ff99da665d0822407e536d784628d
nn98 <jkllhgb@gmail.com>
2020-02-02 19:08:29 +0900

    9465

--- src/BaekJoon/P9465.java
+++ src/BaekJoon/P9465.java
@@ -1,10 +1,30 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P9465 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,a[][],l,i,j;
+	static boolean[][]h;
+	static void o(int x,int v,int c) {
+		if(x==m)l=l>v?l:v;
+		else {
+			
+		}
 	}
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		m=Integer.parseInt(r.readLine());
+		for(;n-->0;) {
+			a=new int[2][m];
+			h=new boolean[2][m];
+			for(i=0;i<2;i++) {
+			StringTokenizer t=new StringTokenizer(r.readLine());for(j=0;j<m;a[i][j]=Integer.parseInt(t.nextToken()));
+			o(0,0,0);
+			}
+		}
+	}
 }
