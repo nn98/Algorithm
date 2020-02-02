23f43b5697dd9fd30ffd89e62572423272a4cf45
nn98 <jkllhgb@gmail.com>
2020-02-02 21:41:13 +0900

    9465

--- src/BaekJoon/P9465.java
+++ src/BaekJoon/P9465.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class P9465 {
-
 	static int n,m,a[][],h[][],l,i,j;
 	static void o(int x,int v,int c) {
 		System.out.println(x+" "+c);
