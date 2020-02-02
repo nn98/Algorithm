8d2048c8a90b2bebf75135ff35d2c3f62cb01a26
nn98 <jkllhgb@gmail.com>
2020-02-02 22:37:07 +0900

    11052

--- src/BaekJoon/P11052_3.java
+++ src/BaekJoon/P11052_3.java
@@ -1,9 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P11052_3 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n+1],h[]=new int[n+1],i=0,j;
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=1;i<n;i++) {
+			
+		}
 	}
 
 }
