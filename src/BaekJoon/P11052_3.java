89bc99556284d020c83b7b95a61e60365155a14a
nn98 <jkllhgb@gmail.com>
2020-02-02 22:45:34 +0900

    11052

--- src/BaekJoon/P11052_3.java
+++ src/BaekJoon/P11052_3.java
@@ -7,15 +7,14 @@ public class P11052_3 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n+1],h[]=new int[n+1],i=0,j;
+		int n=s.nextInt(),a[]=new int[n+1],i=0,j;
 		for(;i++<n;a[i]=s.nextInt());
-		h[1]=a[1];
 		for(i=2;i<=n;i++) {
 			for(j=1;j<=i-j;j++) {
-				h[i]=a[i]>h[j]+h[i-j]?a[i]:h[j]+h[i-j];
+				a[i]=a[i]>a[j]+a[i-j]?a[i]:a[j]+a[i-j];
 				
 			}
 		}
-		System.out.print(Arrays.toString(h));
+		System.out.print(Arrays.toString(a));
 	}
 }
