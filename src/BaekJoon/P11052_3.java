1b876be3fddcae36884cbd6327f7edfe96d317bd
nn98 <jkllhgb@gmail.com>
2020-02-02 22:40:41 +0900

    11052

--- src/BaekJoon/P11052_3.java
+++ src/BaekJoon/P11052_3.java
@@ -10,7 +10,7 @@ public class P11052_3 {
 		int n=s.nextInt(),a[]=new int[n+1],h[]=new int[n+1],i=0,j;
 		for(;i<n;a[i++]=s.nextInt());
 		h[1]=a[1];
-		for(i=2;i<n;i++) {
+		for(i=2;i<=n;i++) {
 			for(j=1;j<=i-j;j++) {
 				h[i]=h[i]>h[j]+h[i-j]?h[i]:h[j]+h[i-j];
 			}
