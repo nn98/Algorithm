ae6c5f939600696bbe0be32387dc3167e3f62ae1
nn98 <jkllhgb@gmail.com>
2020-02-02 21:35:39 +0900

    9465

--- src/BaekJoon/P9465.java
+++ src/BaekJoon/P9465.java
@@ -10,11 +10,11 @@ public class P9465 {
 	static int n,m,a[][],h[][],l,i,j;
 	static void o(int x,int v,int c) {
 		System.out.println(x+" "+c);
-		h[c][x]=c==0?v:v+a[c-1][x];
+		h[c][x]=c==0?v:v+a[c==1?2:1][x];
 		if(x!=m-1) {
 			if(h[0][x+1]<v)	o(x+1,v,0);
-			if(c!=1)if(h[1][x+1]<v+a[0][x])o(x+1,v+a[0][x],1);
 			if(c!=2)if(h[2][x+1]<v+a[1][x])o(x+1,v+a[1][x],2);
+			if(c!=1)if(h[1][x+1]<v+a[0][x])o(x+1,v+a[0][x],1);
 		}else {
 			l=l>h[c][x]?l:h[c][x];
 		}
