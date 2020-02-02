267d608050afae71bcab1ce9615a910759007339
nn98 <jkllhgb@gmail.com>
2020-02-02 22:45:54 +0900

    11052

--- src/BaekJoon/P11052_3.java
+++ src/BaekJoon/P11052_3.java
@@ -1,7 +1,6 @@
 package BaekJoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class P11052_3 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
