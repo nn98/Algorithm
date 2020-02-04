7fb2545498ebea836b4815b59e4baa6668153501
nn98 <jkllhgb@gmail.com>
2020-02-05 00:23:43 +0900

    9251

--- src/BaekJoon/P9251_6.java
+++ src/BaekJoon/P9251_6.java
@@ -8,7 +8,7 @@ public class P9251_6 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
-		int i=1,j=1,c[][]=new int[10][10];
+		int i=1,j=1,c[][]=new int[1001][1001];
 		for(;i<=a.length();i++) {
 			for(j=1;j<=b.length();j++) {
 //				System.out.print(c[i-1][j-1]+" ");
@@ -19,7 +19,7 @@ public class P9251_6 {
 //				System.out.println(c[i][j]);
 			}
 		}
-//		for(int[]d:c)System.out.println(Arrays.toString(d));
-		System.out.print(c[i-1][j-1]);
+		for(int[]d:c)System.out.println(Arrays.toString(d));
+		System.out.print(c[i][j]);
 	}
 }
