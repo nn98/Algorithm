96e62926f84d881ad2fb25fd3cb532f8b8f43339
nn98 <jkllhgb@gmail.com>
2020-02-04 21:53:00 +0900

    14719

--- src/BaekJoon/P14719.java
+++ src/BaekJoon/P14719.java
@@ -1,9 +1,16 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P14719 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt(),y=s.nextInt(),i=0,j;
+		boolean[][]a=new boolean[x][y];
+		for(;i<y;i++)for(j=s.nextInt();j-->1;a[j][i]=true);
+		for(boolean[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
