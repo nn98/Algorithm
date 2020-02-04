c9023c0097d714afbfe6113d4809c0259fae9eef
nn98 <jkllhgb@gmail.com>
2020-02-04 22:00:51 +0900

    14719

--- src/BaekJoon/P14719.java
+++ src/BaekJoon/P14719.java
@@ -10,7 +10,7 @@ public class P14719 {
 		int x=s.nextInt(),y=s.nextInt(),i=0,j,v=0,r=0;
 		boolean c=false,a[][]=new boolean[x][y];
 		for(;i<y;i++)for(j=s.nextInt();j-->0;a[j][i]=true);
-		for(boolean[]b:a)System.out.println(Arrays.toString(b));
+//		for(boolean[]b:a)System.out.println(Arrays.toString(b));
 		for(i=0;i<x;i++) {
 			c=false;
 			v=0;
