242a2a94f6eede9879ff2c5aacee39699072fb4b
nn98 <jkllhgb@gmail.com>
2020-02-04 19:32:00 +0900

    14891

--- src/BaekJoon/P14891.java
+++ src/BaekJoon/P14891.java
@@ -1,20 +1,38 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P14891 {
 
 	static String[][]a=new String[4][];
-	static int i=0,t,j;
-	static void o(int c) {
-		
+	static int i=0,j;
+	static void o(int t,int c) {
+		if(c<0) {
+			if(t>0)
+				if(!a[t-1][2].equals(a[t][6]))o(t-1,1);
+			if(t<3)
+				if(!a[t+1][6].equals(a[t][2]))o(t+1,1);
+			String v=a[t][0];
+			for(int i=0;i<7;i++)a[t][i]=a[t][i+1];
+			a[t][7]=v;
+		}
+		else{
+			if(t>0)
+				if(!a[t-1][2].equals(a[t][6]))o(t-1,-1);
+			if(t<3)
+				if(!a[t+1][6].equals(a[t][2]))o(t+1,-1);
+			String v=a[t][7];
+			for(int i=7;i>0;i--)a[t][i]=a[t][i-1];
+			a[t][0]=v;
+		}
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		for(;i<4;a[i++]=s.next().split(""));
 		for(i=s.nextInt();i-->0;) {
-			t=s.nextInt();
-			o(s.nextInt());
+			o(s.nextInt(),s.nextInt());
+			for(String[]b:a)System.out.println(Arrays.toString(b));
 		}
 	}
 }
