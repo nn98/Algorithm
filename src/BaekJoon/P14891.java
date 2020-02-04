409c901a78ae0f1d9a5803f1ed7665057dfff2eb
nn98 <jkllhgb@gmail.com>
2020-02-04 19:12:57 +0900

    14891

--- src/BaekJoon/P14891.java
+++ src/BaekJoon/P14891.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class P14891 {
 
 	static String[][]a=new String[4][];
-	static int i=0,t;
+	static int i=0,t,j;
 	static void o(int c) {
 		
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		for(;i<4;a[i++]=s.next().split(""));
-		i=s.nextInt();
-		
+		for(i=s.nextInt();i-->0;) {
+			t=s.nextInt();
+			o(s.nextInt());
+		}
 	}
 }
