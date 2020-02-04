ec6ee68a8bf65f88b9051a656e72d963cdf7343d
nn98 <jkllhgb@gmail.com>
2020-02-04 23:49:36 +0900

    14501

--- src/BaekJoon/P14501_B.java
+++ src/BaekJoon/P14501_B.java
@@ -1,5 +1,16 @@
 package BaekJoon;
 
 public class P14501_B {
-
+	static int[] d = new int[21];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			int t = sc.nextInt();
+			int p = sc.nextInt();
+			d[i]=Math.max(d[i-1], d[i]);
+			d[i+t-1] = Math.max(d[i+t-1], d[i-1]+p);
+		}
+		System.out.print(d[n]);
+	}
 }
