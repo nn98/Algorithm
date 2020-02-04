18fb997aeba9ed9b78505cf0ab858a068b5b1d24
nn98 <jkllhgb@gmail.com>
2020-02-05 00:18:46 +0900

    9251

--- src/BaekJoon/P9251_6.java
+++ src/BaekJoon/P9251_6.java
@@ -14,12 +14,12 @@ public class P9251_6 {
 //				System.out.print(c[i-1][j-1]+" ");
 //				System.out.print(c[i-1][j]+" ");
 //				System.out.print(c[i][j-1]+" ");
-				System.out.println(a.charAt(i-1)+" "+b.charAt(j-1));
+//				System.out.println(a.charAt(i-1)+" "+b.charAt(j-1));
 				c[i][j]=Math.max(c[i-1][j-1],Math.max(c[i-1][j],c[i][j-1]))+(a.charAt(i-1)==b.charAt(j-1)?1:0);
-				System.out.println(c[i][j]);
+//				System.out.println(c[i][j]);
 			}
 		}
-		for(int[]d:c)System.out.println(Arrays.toString(d));
+//		for(int[]d:c)System.out.println(Arrays.toString(d));
 		System.out.print(c[i-1][j-1]);
 	}
 }
