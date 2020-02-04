3b85dc3408897839797ccf0afa637a088c75eeb4
nn98 <jkllhgb@gmail.com>
2020-02-04 17:57:42 +0900

    11724

--- src/BaekJoon/P11724_2.java
+++ src/BaekJoon/P11724_2.java
@@ -1,10 +1,15 @@
 package BaekJoon;
 //OnHold
 
+import java.util.Scanner;
+
 public class P11724_2 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()+1,m=s.nextInt(),r=0,i=1,j;
+		boolean[][]a=new boolean[n][n];
+		for(;m-->0;a[s.nextInt()][s.nextInt()]=true);
 		
 	}
-
 }
