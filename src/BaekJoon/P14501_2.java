91c5888b5369294ac56eb94c1b5658c17a0f7cc5
nn98 <jkllhgb@gmail.com>
2020-02-04 22:55:54 +0900

    14501

--- src/BaekJoon/P14501_2.java
+++ src/BaekJoon/P14501_2.java
@@ -1,11 +1,21 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P14501_2 {
 	static int n,a[],b[],h[],i;
 	static void o(int x) {
-		
+		if(x+a[x]>n)return;
+		else if(x+a[x]==n)h[x]=b[x];
+		else {
+			int v=0,X=x;
+			x+=a[x];
+			for(;x<n;x++) {
+				v=v>h[x]?v:h[x];
+			}
+			h[X]=v;
+		}
 	}
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
@@ -14,5 +24,7 @@ public class P14501_2 {
 		b=new int[n];
 		h=new int[n];
 		for(;i<n;a[i]=s.nextInt(),b[i++]=s.nextInt());
+		for(;i-->0;o(i));
+		System.out.println(Arrays.toString(h));
 	}
 }
