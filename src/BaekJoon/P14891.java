a001f79dfd859f20ec47cbc44db40a19929053d7
nn98 <jkllhgb@gmail.com>
2020-02-04 19:12:53 +0900

    14891

--- src/BaekJoon/P14891.java
+++ src/BaekJoon/P14891.java
@@ -1,9 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P14891 {
 
-	public static void main(String[] args) {
+	static String[][]a=new String[4][];
+	static int i=0,t;
+	static void o(int c) {
 		
 	}
-
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		for(;i<4;a[i++]=s.next().split(""));
+		i=s.nextInt();
+		
+	}
 }
