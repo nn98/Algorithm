b7d892fec744aac45e728ad4072382c76170de21
nn98 <jkllhgb@gmail.com>
2020-02-04 22:37:19 +0900

    14501

--- src/BaekJoon/P14501_2.java
+++ src/BaekJoon/P14501_2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class P14501_2 {
 	static int n,a[],b[],h[],i;
+	static void o(int x)
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
@@ -12,5 +13,4 @@ public class P14501_2 {
 		h=new int[n];
 		for(;i<n;a[i]=s.nextInt(),b[i++]=s.nextInt());
 	}
-
 }
