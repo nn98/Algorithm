69fc74d1b3fb715bceeaf5bdcbc26fe6af6ecdda
nn98 <jkllhgb@gmail.com>
2020-02-04 23:54:44 +0900

    14501

--- src/BaekJoon/P14501_B.java
+++ src/BaekJoon/P14501_B.java
@@ -3,18 +3,15 @@ package BaekJoon;
 //½Å±â
 import java.util.Arrays;
 import java.util.Scanner;
-
-public class P14501_B {
-	static int[] d = new int[21];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=1; i<=n; i++) {
-			int t = sc.nextInt();
-			int p = sc.nextInt();
+public class P14501_B{
+	public static void main(String[]z){
+		Scanner s = new Scanner(System.in);
+		int n=s.nextInt(),d[]=new int[21];;
+		for(int i=1;i<=n;i++) {
+			int t = s.nextInt();
+			int p = s.nextInt();
 			d[i]=Math.max(d[i-1], d[i]);
 			d[i+t-1] = Math.max(d[i+t-1], d[i-1]+p);
-			System.out.println(Arrays.toString(d));
 		}
 		System.out.print(d[n]);
 	}
