c3191e6070602ddc1b2edd1ffcd250c16ddb5010
nn98 <jkllhgb@gmail.com>
2020-02-05 00:11:14 +0900

    9251

--- src/BaekJoon/P9251_6.java
+++ src/BaekJoon/P9251_6.java
@@ -1,5 +1,6 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P9251_6 {
@@ -7,13 +8,13 @@ public class P9251_6 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
-		int i=1,j=1,c[][]=new int[1001][1001];
+		int i=1,j=1,c[][]=new int[10][10];
 		for(;i<a.length();i++) {
 			for(j=1;j<b.length();j++) {
 				c[i][j]=Math.max(c[i-1][j-1],Math.max(c[i-1][j],c[i][j-1]))+a.charAt(i-1)==b.charAt(j-1)?1:0;
 			}
 		}
-		
+		for(int[]d:c)System.out.println(Arrays.toString(d));
 		System.out.print(c[i-1][j-1]);
 	}
 }
