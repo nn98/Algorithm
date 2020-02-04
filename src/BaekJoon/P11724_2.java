d467bd0052639e55f0896f1a710173f825376733
nn98 <jkllhgb@gmail.com>
2020-02-04 18:07:28 +0900

    11724

--- src/BaekJoon/P11724_2.java
+++ src/BaekJoon/P11724_2.java
@@ -2,9 +2,7 @@ package BaekJoon;
 //OnHold
 
 import java.util.Scanner;
-
 public class P11724_2 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt()+1,m=s.nextInt(),r=0,i=1,j;
