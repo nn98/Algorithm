1fd2cb6d345a2b5b40383855658c86a3da101871
nn98 <jkllhgb@gmail.com>
2020-02-05 12:02:24 +0900

    9252

--- src/BaekJoon/P9252.java
+++ src/BaekJoon/P9252.java
@@ -8,6 +8,6 @@ public class P9252 {
 		String a=s.next(),b=s.next(),r="";
 		int i=1,j=1,c[][]=new int[1001][1001];
 		for(;i<=a.length();i++)for(j=1;j<=b.length();r+=c[i][j]>c[i-1][j-1]?a.charAt(i-1):"",j++)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
-		System.out.print(c[i-1][j-1]);
+		System.out.print(c[i-1][j-1]+"\n"+r);
 	}
 }
