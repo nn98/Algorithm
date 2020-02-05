595477c6281aa1cb3d46d998fef2dfaba85613e3
nn98 <jkllhgb@gmail.com>
2020-02-05 12:43:13 +0900

    5582

--- src/BaekJoon/P5582_2.java
+++ src/BaekJoon/P5582_2.java
@@ -1,9 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5582_2 {
 
 	public static void main(String[] args){
-		
+		Scanner s=new Scanner(System.in);
+		String a=s.next(),b=s.next();
+		int i=0,j,c[][]=new int[4005][4005],r=0;
+		for(;i<a.length();i++)
+			for(j=0;j<b.length();j++)
+				if(a.charAt(i)==b.charAt(j)) {
+					c[i+1][j+1]=c[i][j]+1;
+					r=Math.max(r,c[i+1][j+1]);
+				}
 	}
 
 }
