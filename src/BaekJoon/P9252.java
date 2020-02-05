98c0ff119dd9632f81be0f8e4e4c39ee54e3f915
nn98 <jkllhgb@gmail.com>
2020-02-05 12:04:12 +0900

    9252

--- src/BaekJoon/P9252.java
+++ src/BaekJoon/P9252.java
@@ -1,9 +1,10 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P9252 {
-	static int i,j,c[][]=new int[1001][1001];
+	static int i,j,c[][]=new int[10][10];
 	static void o(int i,int j) {
 		
 	}
@@ -11,6 +12,7 @@ public class P9252 {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next(),r="";
 		for(;++i<=a.length();)for(;++j<=b.length();)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
+		for(int[]x:c)System.out.println(Arrays.toString(x));
 		System.out.print(c[i-2][j-2]);
 	}
 }
