79ea36e298148e23f3de13ce0da94b3e6aa6bd1d
nn98 <jkllhgb@gmail.com>
2020-02-05 13:33:59 +0900

    9613

--- src/BaekJoon/P9613.java
+++ src/BaekJoon/P9613.java
@@ -1,9 +1,28 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class P9613 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),m,v[],i,j;
+		boolean[][]a;
+		for(;n-->0;) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			m=Integer.parseInt(t.nextToken());
+			a=new boolean[m][];
+			v=new int[m];
+			for(;m-->0;) {
+				i=Integer.parseInt(t.nextToken());
+				a[m]=new boolean[i+1];
+				for(j=1;j<=i/2;j++)if(i%j==0)a[m][j]=a[m][i/j]=true;
+			}
+			for(boolean[]X:a)System.out.println(Arrays.toString(X));
+		}
 	}
 
 }
