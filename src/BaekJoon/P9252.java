0566beef21e489f43f4b1f68002ca945ca2ec610
nn98 <jkllhgb@gmail.com>
2020-02-05 12:16:31 +0900

    9252

--- src/BaekJoon/P9252.java
+++ src/BaekJoon/P9252.java
@@ -4,17 +4,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P9252 {
-	static int i,j,c[][]=new int[1001][1001];
-	static StringBuffer f;
-	static void o(int i,int j) {
-		f.append(0);
-		System.out.println(f);
-	}
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next(),r="";
+		int i=0,j=0,c[][]=new int[1001][1001],x,y;
 		for(;++i<=a.length();)for(j=0;++j<=b.length();)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
-		System.out.print(c[i-1][j-1]);
-		o(0,0);
+		StringBuffer f=new StringBuffer();
+		x=--i;
+		y=--j;
+		System.out.print(c[i][j]);
 	}
 }
