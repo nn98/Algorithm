96abd5d75adccbc1108ca44bb228a21aec0c5d1c
nn98 <jkllhgb@gmail.com>
2020-02-05 12:44:21 +0900

    5582

--- src/BaekJoon/P5582_2.java
+++ src/BaekJoon/P5582_2.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P5582_2 {
-
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
