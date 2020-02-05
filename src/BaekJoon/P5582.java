5ba0f3a64163c3c3702c494cbfe1e8301238f4c6
nn98 <jkllhgb@gmail.com>
2020-02-05 12:31:19 +0900

    5582

--- src/BaekJoon/P5582.java
+++ src/BaekJoon/P5582.java
@@ -1,12 +1,13 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P5582 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
-		int i=1,j=1,c[][]=new int[1001][1001],v=0,x,y;
+		int i=1,j=1,c[][]=new int[20][20],v=0,x,y;
 		for(;i<=a.length();i++)
 			for(j=1;j<=b.length();j++)
 				c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
@@ -20,6 +21,7 @@ public class P5582 {
 					y++;
 				}
 			}
+		for(int[]o:c)System.out.println(Arrays.toString(o));
 		System.out.print(v);
 	}
 }
