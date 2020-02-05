4a7dd7531577807949acb9b0f7fe75ca68f79cfe
nn98 <jkllhgb@gmail.com>
2020-02-05 12:16:34 +0900

    9252

--- src/BaekJoon/P9252.java
+++ src/BaekJoon/P9252.java
@@ -9,9 +9,16 @@ public class P9252 {
 		String a=s.next(),b=s.next(),r="";
 		int i=0,j=0,c[][]=new int[1001][1001],x,y;
 		for(;++i<=a.length();)for(j=0;++j<=b.length();)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
-		StringBuffer f=new StringBuffer();
 		x=--i;
 		y=--j;
-		System.out.print(c[i][j]);
+		while(c[x][y]!=0) {
+			if(c[x][y]==c[x][y-1])y--;
+			else if(c[x][y]==c[x-1][y])x--;
+			else if(c[x][y]-1==c[x-1][y-1]) {
+				r+=x--;
+				y--;
+			}
+		}
+		System.out.print(c[i][j]+r);
 	}
 }
