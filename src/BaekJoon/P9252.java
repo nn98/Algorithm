20b7b098417e490f301a21988ecaf5fb018a7d4b
nn98 <jkllhgb@gmail.com>
2020-02-05 12:05:07 +0900

    9252

--- src/BaekJoon/P9252.java
+++ src/BaekJoon/P9252.java
@@ -11,7 +11,7 @@ public class P9252 {
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next(),r="";
-		for(;++i<=a.length();)for(;++j<=b.length();)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
+		for(;++i<=a.length();)for(j=0;++j<=b.length();)c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
 		for(int[]x:c)System.out.println(Arrays.toString(x));
 		System.out.print(c[i-2][j-2]);
 	}
