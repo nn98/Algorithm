8073a95d7938b81c0a388b848e892bdfc6003d12
nn98 <jkllhgb@gmail.com>
2020-02-05 14:00:58 +0900

    9613

--- src/BaekJoon/P9613.java
+++ src/BaekJoon/P9613.java
@@ -6,8 +6,7 @@ import java.util.StringTokenizer;
 public class P9613 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),m,v[],i,j,M;
-		long R=0;
+		int n=Integer.parseInt(r.readLine()),m,v[],i,j,M,R;
 		boolean[][]a;
 		for(;n-->0;) {
 			StringTokenizer t=new StringTokenizer(r.readLine());
