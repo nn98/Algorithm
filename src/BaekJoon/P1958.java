47be8b47cc9f96f4b2d53f34a20b44e61b8d51c8
nn98 <jkllhgb@gmail.com>
2020-02-05 12:58:01 +0900

    1958

--- src/BaekJoon/P1958.java
+++ src/BaekJoon/P1958.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P1958 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next(),d=s.next();
