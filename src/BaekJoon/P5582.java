1fff3232e9401c16d8cc19d14382edd84e410a3a
nn98 <jkllhgb@gmail.com>
2020-02-05 12:31:22 +0900

    5582

--- src/BaekJoon/P5582.java
+++ src/BaekJoon/P5582.java
@@ -7,19 +7,21 @@ public class P5582 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
-		int i=1,j=1,c[][]=new int[20][20],v=0,x,y;
+		int i=1,j=1,c[][]=new int[20][20],v=0,x,y,p;
 		for(;i<=a.length();i++)
 			for(j=1;j<=b.length();j++)
 				c[i][j]=a.charAt(i-1)==b.charAt(j-1)?c[i-1][j-1]+1:Math.max(c[i-1][j],c[i][j-1]);
 		for(i=1;i<=a.length();i++)
 			for(j=1;j<b.length();j++) {
+				p=0;
 				x=i;
 				y=j;
 				while(c[x][y]==c[x-1][y-1]+1) {
-					v=v>c[x][y]?v:c[x][y];
+					p++;
 					x++;
 					y++;
 				}
+				v=v>p?v:p;
 			}
 		for(int[]o:c)System.out.println(Arrays.toString(o));
 		System.out.print(v);
