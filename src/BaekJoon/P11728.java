a6e76ade8780bfbc00b00a90460217bd56f4dc8e
nn98 <jkllhgb@gmail.com>
2020-02-06 22:43:17 +0900

    11728

--- src/BaekJoon/P11728.java
+++ src/BaekJoon/P11728.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class P11728 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),i=0,j=0,a[]=new int[n],b[]=new int[m],r;
+		int n=s.nextInt(),m=s.nextInt(),i=0,j=0,a[]=new int[n],b[]=new int[m];
+		StringBuffer r=new StringBuffer();
 		for(;i<n;a[i++]=s.nextInt());
 		for(;j<m;b[j++]=s.nextInt());
 		i=j=0;
 		while(i<n||j<m) {
-			if(i==n)r=b[j++];
-			else if(j==m)r=a[i++];
+			if(i==n)r.append(b[j++]+" ");
+			else if(j==m)r.append(a[i++]+" ");
 			else {
-				r=a[i]<b[j]?a[i++]:b[j++];
+				r.append(a[i]<b[j]?a[i++]:b[j++]+" ");
 			}
-			System.out.print(r+" ");
 		}
+		System.out.print(r);
 	}
 }
