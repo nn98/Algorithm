3c7464431465bc4d366afd4ab8e1eb870def08ac
nn98 <jkllhgb@gmail.com>
2020-02-06 23:10:28 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -20,13 +20,13 @@ public class P2981_5 {
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+//		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,c,v=0;
 		for(;i<n;a[i++]=Integer.parseInt(r.readLine()));
 		Arrays.sort(a);
 		for(i=1;i<n;a[i-1]=a[i]-a[i++-1]);
 		System.out.println(Arrays.toString(a));
-//		for(i=1;i<n;c=G(a[i-1],a[i]),v=v>c?v:c,i++);
+		for(i=1;i<n-1;c=G(a[i-1],a[i]),v=v>c?v:c,i++);
 		System.out.println(v);
 	}
 }
