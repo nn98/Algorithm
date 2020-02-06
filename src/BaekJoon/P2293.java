62e57dbd916567efef7dfc8f306c1eb5e8c5ef04
nn98 <jkllhgb@gmail.com>
2020-02-07 00:59:33 +0900

    2293

--- src/BaekJoon/P2293.java
+++ src/BaekJoon/P2293.java
@@ -1,5 +1,5 @@
 package BaekJoon;
-
+//https://extracold.tistory.com/5
 import java.util.Scanner;
 
 public class P2293 {
@@ -8,6 +8,7 @@ public class P2293 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),v=s.nextInt(),a[]=new int[n],h[]=new int[v+1],i=0,j,r=0;
 		for(;i<n;a[i++]=s.nextInt());
+		while(i-->0)a[i]=s.nextInt();
 		h[0]=1;
 		for(i=0;i<n;i++)for(j=1;j<=v;j++)if(j-a[i]>=0)h[j]+=h[j-a[i]];
 		System.out.print(h[v]);
