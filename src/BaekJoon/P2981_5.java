5027c60f31ac8a7a1e8566566c06df1818a8ff5b
nn98 <jkllhgb@gmail.com>
2020-02-06 23:10:24 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -4,7 +4,13 @@ import java.io.*;
 import java.util.Arrays;
 public class P2981_5 {
 	static int G(int a,int b) {
-		int t=b%a;
+		int t;
+		if(a>b) {
+			t=a;
+			a=b;
+			b=t;
+		}
+		t=b%a;
 		while(t!=0) {
 			b=a;
 			a=t;
@@ -18,7 +24,9 @@ public class P2981_5 {
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,c,v=0;
 		for(;i<n;a[i++]=Integer.parseInt(r.readLine()));
 		Arrays.sort(a);
-		for(i=1;i<n;c=G(a[i-1],a[i]),v=v>c?v:c,i++);
+		for(i=1;i<n;a[i-1]=a[i]-a[i++-1]);
+		System.out.println(Arrays.toString(a));
+//		for(i=1;i<n;c=G(a[i-1],a[i]),v=v>c?v:c,i++);
 		System.out.println(v);
 	}
 }
