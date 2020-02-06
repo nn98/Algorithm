dd3d15b7930ffc5c40a15d506b837d734d76a52e
nn98 <jkllhgb@gmail.com>
2020-02-07 01:15:57 +0900

    2294

--- src/BaekJoon/P2294.java
+++ src/BaekJoon/P2294.java
@@ -1,9 +1,20 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2294 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],b[]=new int[m+1],i=0,j;
+		for(;i<n;a[i++]=s.nextInt());
+		Arrays.sort(a);
+		Arrays.fill(b,100001);
+		b[0]=0;
+		for(i=0;i<n;i++)
+			for(j=a[i];j<=m;j++)b[j]=b[j-a[i]]+1<b[j]?b[j-a[i]]+1:b[j];
+		System.out.println(b[m]);
 	}
 
 }
