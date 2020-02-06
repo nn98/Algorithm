e8ebb83faab6fea47760149558a4826f939f4000
nn98 <jkllhgb@gmail.com>
2020-02-06 23:41:07 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -4,24 +4,13 @@ import java.io.*;
 import java.util.Arrays;
 public class P2981_5 {
 	static int G(int a,int b) {
-		int t;
-		if(a>b) {
-			t=a;
-			a=b;
-			b=t;
-		}
-		t=b%a;
-		while(t!=0) {
-			b=a;
-			a=t;
-			t=b%a;
-		}
-		return a;
+		if(a%b==0)return b;
+		return G(b,a%b);
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-				BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,c,v=0;
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,v=0;
 		for(;i<n;a[i++]=Integer.parseInt(r.readLine()));
 		Arrays.sort(a);
 		for(i=1;i<n;a[i-1]=a[i]-a[i++-1]);
@@ -32,12 +21,11 @@ public class P2981_5 {
 			for(i=2;i<n-1;v=G(v,a[i++]));
 		}
 		for(i=2;i<=v;w.write(v%i==0?i+" ":""),i++);
-//		boolean[]b=new boolean[v+1];
-//		for(i=0;i++<v/2;b[i]=b[v/i]=v%i==0);
+		//		boolean[]b=new boolean[v+1];
+		//		for(i=0;i++<v/2;b[i]=b[v/i]=v%i==0);
 		//		System.out.println(Arrays.toString(b));
 		//		System.out.println(v);
-//		for(i=1;++i<=v;w.write(b[i]?i+" ":""));
-		
+		//		for(i=1;++i<=v;w.write(b[i]?i+" ":""));
 		w.flush();
 	}
 }
