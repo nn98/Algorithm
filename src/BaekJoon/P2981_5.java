c1e64522f9f18fa8c6629133b1791ed26098ffb9
nn98 <jkllhgb@gmail.com>
2020-02-06 22:52:04 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -1,12 +1,7 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.io.*;
 public class P2981_5 {
-
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
