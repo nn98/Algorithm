ee19d6a78cdf6ac53b5d9880bf9754387d2ea977
nn98 <jkllhgb@gmail.com>
2020-02-06 22:40:14 +0900

    11728

--- src/BaekJoon/P11728.java
+++ src/BaekJoon/P11728.java
@@ -8,7 +8,7 @@ public class P11728 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0,j=0,a[]=new int[n],b[]=new int[m],r;
 		for(;i<n;a[i++]=s.nextInt());
-		for(;j<m;a[j++]=s.nextInt());
+		for(;j<m;b[j++]=s.nextInt());
 		System.out.println(Arrays.toString(a));
 		i=j=0;
 		while(i<n&&j<m) {
