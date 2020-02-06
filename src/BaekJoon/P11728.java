1599749c435dec41abc7769123c8c85b2b5cc93c
nn98 <jkllhgb@gmail.com>
2020-02-06 22:40:30 +0900

    11728

--- src/BaekJoon/P11728.java
+++ src/BaekJoon/P11728.java
@@ -1,6 +1,5 @@
 package BaekJoon;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class P11728 {
@@ -9,7 +8,6 @@ public class P11728 {
 		int n=s.nextInt(),m=s.nextInt(),i=0,j=0,a[]=new int[n],b[]=new int[m],r;
 		for(;i<n;a[i++]=s.nextInt());
 		for(;j<m;b[j++]=s.nextInt());
-		System.out.println(Arrays.toString(a));
 		i=j=0;
 		while(i<n||j<m) {
 			if(i==n)r=b[j++];
