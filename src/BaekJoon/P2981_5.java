4dfabf97c4e8926396cd81817d290206314851f5
nn98 <jkllhgb@gmail.com>
2020-02-06 23:29:22 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -20,18 +20,21 @@ public class P2981_5 {
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		//		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,c,v=0;
 		for(;i<n;a[i++]=Integer.parseInt(r.readLine()));
 		Arrays.sort(a);
 		for(i=1;i<n;a[i-1]=a[i]-a[i++-1]);
-//		System.out.println(Arrays.toString(a));
+		//		System.out.println(Arrays.toString(a));
 		if(n==2)v=a[0];
-		for(i=1;i<n-1;c=G(a[i-1],a[i]),v=v>c?v:c,i++);
+		else {
+			v=G(a[0],a[1]);
+			for(i=2;i<n-1;v=G(v,a[i++]));
+		}
 		boolean[]b=new boolean[v+1];
 		for(i=0;i++<v/2;b[i]=b[v/i]=v%i==0);
-//		System.out.println(Arrays.toString(b));
-//		System.out.println(v);
+		//		System.out.println(Arrays.toString(b));
+		//		System.out.println(v);
 		for(i=1;++i<=v;System.out.print(b[i]?i+" ":""));
 	}
 }
