fc748d9fa0a96b0f5f580b4a8521b8d36942c722
nn98 <jkllhgb@gmail.com>
2020-02-06 22:51:32 +0900

    2981

--- src/BaekJoon/P2981_5.java
+++ src/BaekJoon/P2981_5.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+
 public class P2981_5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreaWriter(System.out));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0;
+		for(;i<n;a[i++]=Integer.parseInt(r.readLine()));
 	}
-
 }
