576a182685819461e22f971555517c460b7586f8
nn98 <jkllhgb@gmail.com>
2020-02-06 23:54:22 +0900

    2981

--- src/BaekJoon/P2981_6S.java
+++ src/BaekJoon/P2981_6S.java
@@ -1,10 +1,22 @@
 package BaekJoon;
+//?????
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class P2981_6S {
-
+	static int G(int a,int b) {
+		if(b==0)return a;
+		else return G(b,a%b);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0,m;
+		for(;i<n;a[i++]=s.nextInt());
+		Arrays.sort(a);
+		m=a[1]-a[0];
+		for(i=2;i<n;m=G(m,a[i]-a[i++-1]));
+		for(i=2;i<=m;System.out.print(m%i==0?i+" ":""));
 	}
 
 }
