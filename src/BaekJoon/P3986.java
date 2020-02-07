ab4da2141a3b82c33dd0105489266fa8cbe9621c
nn98 <jkllhgb@gmail.com>
2020-02-07 23:25:18 +0900

    3986

--- src/BaekJoon/P3986.java
+++ src/BaekJoon/P3986.java
@@ -1,8 +1,23 @@
 package BaekJoon;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class P3986 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r=0,i,j;
+		while(n-->0) {
+			Stack<String>t=new Stack();
+			String[]a=s.next().split("");
+			j=a.length;
+			for(i=0;i<a.length;i++) {
+				if(t.isEmpty()||!t.peek().equals(a[i]))t.push(a[i]);
+				else t.pop();
+			}
+			
+		}
 	}
 
 }
