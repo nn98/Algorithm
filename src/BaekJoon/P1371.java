44cfe3dcefb215fa96e81a4d0406277bde106518
nn98 <jkllhgb@gmail.com>
2020-02-07 23:58:50 +0900

    1371

--- src/BaekJoon/P1371.java
+++ src/BaekJoon/P1371.java
@@ -1,17 +1,21 @@
 package BaekJoon;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class P1371 {
 
 	public static void main(String[] args)throws Exception{
+		System.out.println('z');
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		String s;
+		int i,a[]=new int[26];
 		StringBuffer f=new StringBuffer();
 		while(!(s=r.readLine()).equals("")) {
-			f.append(s);
+			for(i=0;i<s.length();i++) {
+				char c=s.charAt(i);
+				if(c>96&&c<11);
+			}
 		}
-		int[]a=new int[26];
-		
 	}
 }
