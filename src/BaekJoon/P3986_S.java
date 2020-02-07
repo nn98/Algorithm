2124f82dd23daada60df22bd442e9ff48a6d0cb1
nn98 <jkllhgb@gmail.com>
2020-02-07 23:36:05 +0900

    3986

---
+++ src/BaekJoon/P3986_S.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P3986_S {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r=0;
+		while(n-->0) {
+			String t=s.next();
+			while(t.contains("AA")||t.contains("BB"))t=t.replace("AA|BB","");
+			if(t.equals(""))r++;
+		}
+		System.out.print(r);
+	}
+
+}
