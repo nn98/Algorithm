3c712e48f997bbe35cd262e74149ccc46cfccc51
nn98 <jkllhgb@gmail.com>
2020-02-07 23:59:31 +0900

    1371

--- src/BaekJoon/P1371.java
+++ src/BaekJoon/P1371.java
@@ -2,9 +2,7 @@ package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
 public class P1371 {
-
 	public static void main(String[] args)throws Exception{
 		System.out.println('z');
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
