90ebc67c24959b8831ac2ee6456a996e71c12d49
nn98 <jkllhgb@gmail.com>
2020-02-07 23:57:04 +0900

    1371

--- src/BaekJoon/P1371.java
+++ src/BaekJoon/P1371.java
@@ -1,10 +1,17 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+
 public class P1371 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		StringBuffer f=new StringBuffer();
+		while(!(s=r.readLine()).equals("")) {
+			f.append(s);
+		}
+		int[]a=new int[26];
+		
 	}
-
 }
