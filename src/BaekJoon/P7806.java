4e8676edcd2927343b331994cfe641babebcc97f
nn98 <jkllhgb@gmail.com>
2020-02-08 21:18:12 +0900

    7806

--- src/BaekJoon/P7806.java
+++ src/BaekJoon/P7806.java
@@ -8,7 +8,7 @@ public class P7806 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		while(s.hasNext()) {
-			int n=s.nextInt(),m=s.nextInt(),a[]=new int[1000000001],i=0;
+			int n=s.nextInt(),m=s.nextInt(),i=0;
 			boolean[]k=new boolean[1000000001];
 			for(;++i<=Math.sqrt(m);k[i]=k[m/i]=m%i==0);
 			System.out.println(Arrays.toString(k));
