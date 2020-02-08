6bca70cf6bd86a1782c4021c0a3d85476e1c1359
nn98 <jkllhgb@gmail.com>
2020-02-08 21:42:51 +0900

    7806

--- src/BaekJoon/P7806_C.java
+++ src/BaekJoon/P7806_C.java
@@ -10,6 +10,39 @@ public class P7806_C {
 		while(s.hasNext()) {
 			n=s.nextInt();
 			m=s.nextInt();
-			
+			long tmp = m;
+			long ans = 1;
+			for (int i = 2; i*i <= tmp; i++)
+			{
+				int powM = 0;
+				// m에 i가 몇개 들어있는지 확인
+				while(m % i == 0)
+				{
+					m /= i;
+					powM += 1;
+				}
+
+				// i가 1개이상 들어있다면
+				if (powM>0)
+				{
+					int powN = 0;
+					// n에는 i가 몇개 있는지 확인
+					for (int j = i; j <= n; j *= i)
+						powN += n / j;
+
+					// powN와 powM는 서로 m과 n의 지수승을 의미(m^powN , n^powM)
+					// 따라서 작은것을 따라감
+					for (int j = 0; j < Math.min(powN, powM); j++)
+						ans *= i;
+				}
+
+				if(m < i)
+					break;
+			}
+
+			if (m > 1 && m <= n)
+				ans *= m;
+			System.out.println(ans);
+		}
 	}
 }
