af1d97f7078ddd03c189b6caf0838674bd2dc8f3
nn98 <jkllhgb@gmail.com>
2020-02-08 22:52:10 +0900

    15353

--- src/BaekJoon/P15353.java
+++ src/BaekJoon/P15353.java
@@ -2,13 +2,10 @@ package BaekJoon;
 
 import java.math.BigInteger;
 import java.util.Scanner;
-
 public class P15353 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		BigInteger a=new BigInteger(s.next());
 		System.out.print(a.add(new BigInteger(s.next())));
 	}
-
 }
