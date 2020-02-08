5d4e4443acb8be9bcdf84f3666208d5ab1109343
nn98 <jkllhgb@gmail.com>
2020-02-08 22:10:15 +0900

    12858

--- src/BaekJoon/P12858.java
+++ src/BaekJoon/P12858.java
@@ -2,10 +2,17 @@ package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class P12858 {
+public class P12858{
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine());
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],m=Integer.parseInt(r.readLine()),i,j,b[]=new int[3];
+		for(;m-->0;) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			for(n=0;n<3;b[n++]=Integer.parseInt(t.nextToken()))
+			if(b[0]==0) {
+			}
+		}
 	}
 }
