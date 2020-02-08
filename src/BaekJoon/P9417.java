98c78b6a5348bcab3ef501a975b26bbc9da51e7e
nn98 <jkllhgb@gmail.com>
2020-02-08 21:54:25 +0900

    9417

--- src/BaekJoon/P9417.java
+++ src/BaekJoon/P9417.java
@@ -2,9 +2,11 @@ package BaekJoon;
 
 public class P9417 {
 
+	static int G(int a,int b) {
+		return b==0?a:G(b,a%b);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
