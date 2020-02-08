227b67ced8fa0aa88c7211a91084e7b6e8e823ea
nn98 <jkllhgb@gmail.com>
2020-02-08 21:39:25 +0900

    7806

--- src/BaekJoon/P7806.java
+++ src/BaekJoon/P7806.java
@@ -6,10 +6,25 @@ public class P7806 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		while(s.hasNext()) {
-			int n=s.nextInt(),m=s.nextInt(),i=0;
-			boolean[]k=new boolean[1000000001];
-			for(;++i<=Math.sqrt(m);k[i]=k[m/i]=m%i==0);
-			for(i=0;i<=m;i++)System.out.print(k[i]+" ");
+			int n=s.nextInt(),m=s.nextInt(),M=m,i=2,j,r=1;
+			for(;i*i<=M;i++) {
+				int P=0;
+				while(m%i==0) {
+					m/=i;
+					P++;
+				}
+				if(P>0) {
+					int Q=0;
+					for(j=1;j<=n;j*=i)Q+=n/j;
+					for(j=0;j<(P<Q?P:Q);j++)r*=i;
+				}
+				if(m<i)break;
+			}
+			if(m>1&&m<=n)r*=m;
+			System.out.println(r);
+//			boolean[]k=new boolean[1000000001];
+//			for(;++i<=Math.sqrt(m);k[i]=k[m/i]=m%i==0);
+//			for(i=0;i<=m;i++)System.out.print(k[i]+" ");
 		}
 	}
 }
