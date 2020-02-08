c4d4b20e3b2175663a367dd4c44331eda6e52ff4
nn98 <jkllhgb@gmail.com>
2020-02-08 21:16:59 +0900

    7806

--- src/BaekJoon/P7806.java
+++ src/BaekJoon/P7806.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P7806 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		while(s.hasNext()) {
+			int n=s.nextInt(),m=s.nextInt(),a[]=new int[1000000001],i=0;
+			boolean[]k=new boolean[1000000001];
+			for(;++i<=Math.sqrt(m);k[i]=k[m/i]=m%i==0);
+		}
 	}
 
 }
