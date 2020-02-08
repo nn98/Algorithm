ed126ed4dd40446bbac6263e2f4e20101f968399
nn98 <jkllhgb@gmail.com>
2020-02-08 22:22:07 +0900

    12858

--- src/BaekJoon/P12858.java
+++ src/BaekJoon/P12858.java
@@ -19,6 +19,7 @@ public class P12858{
 			System.out.println(Arrays.toString(a));
 			t=new StringTokenizer(r.readLine());
 			for(n=0;n<3;b[n++]=Integer.parseInt(t.nextToken())-1);
+			System.out.println(Arrays.toString(b));
 			if(b[0]>=0)for(;b[1]<b[2];a[b[1]++]+=b[0]);
 			else {
 				p=a[b[1]];
