25cb7a4af390ba4cb8bf6f997a0b0dc2149c46d5
nn98 <jkllhgb@gmail.com>
2020-02-08 20:39:57 +0900

    1992

--- src/BaekJoon/P1992.java
+++ src/BaekJoon/P1992.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P1992 {
 
 	public static void main(String[] args) {
-		System.out.println('0');
+		System.out.println('1');
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0;
 		char[][]a=new char[n][];
