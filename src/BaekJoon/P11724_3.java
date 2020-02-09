686bc8bbfacfc1c6a539a9d4691051b240ef630e
nn98 <jkllhgb@gmail.com>
2020-02-10 03:27:32 +0900

    11724

--- src/BaekJoon/P11724_3.java
+++ src/BaekJoon/P11724_3.java
@@ -1,10 +1,14 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P11724_3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
