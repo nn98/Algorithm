bcdc6a1d9752317c88fbcfd7ff54783694376363
nn98 <jkllhgb@gmail.com>
2020-02-10 03:21:39 +0900

    11689

--- src/BaekJoon/P11689.java
+++ src/BaekJoon/P11689.java
@@ -1,9 +1,31 @@
 package BaekJoon;
+
+import java.util.Scanner;
+
 //Hold
 public class P11689 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		long n=s.nextLong(),c=-1,r=n,t,k=2,i=2;
+		if(n==1) {
+			System.out.print(1);
+			return;
+		}
+		t=(long) Math.sqrt(n);
+		for(;i<=t;i++)while(n%i==0) {
+			n/=i;
+			if(c!=i) {
+				c=i;
+				r/=i;
+				r*=(i-1);
+			}
+		}
+		if(n>1) {
+			r/=n;
+			r*=(n-1);
+		}
+		System.out.print(r);
 	}
 
 }
