1c8175fb6c4b824149d25808139d602a00c24680
nn98 <jkllhgb@gmail.com>
2020-02-13 07:10:20 +0900

    11724

--- src/BaekJoon/P11724_3.java
+++ src/BaekJoon/P11724_3.java
@@ -27,18 +27,18 @@ public class P11724_3 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		n=Integer.parseInt(t.nextToken());
+		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
 		a=new boolean[n][n];
 		h=new boolean[n][n];
 		for(;m-->0;) {
 			t=new StringTokenizer(r.readLine());
-			p=Integer.parseInt(t.nextToken())-1;
-			q=Integer.parseInt(t.nextToken())-1;
+			p=Integer.parseInt(t.nextToken());
+			q=Integer.parseInt(t.nextToken());
 			a[p][q]=a[q][p]=true;
 		}
 		for(boolean[]b:a)System.out.println(Arrays.toString(b));
-		for(m=0;m<n;m++)o(m,true,m,true);
+		for(m=1;m<n;m++)o(m,true,m,true);
 		System.out.println(Arrays.toString(h));
 		System.out.println(R);
 	}
