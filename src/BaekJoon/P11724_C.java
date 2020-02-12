e2e6889d705b7a05e561b2cda6771b42dee480a8
nn98 <jkllhgb@gmail.com>
2020-02-13 07:56:21 +0900

    11724

--- src/BaekJoon/P11724_C.java
+++ src/BaekJoon/P11724_C.java
@@ -3,14 +3,11 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P11724_C {
-
 	static int n,m,a[][],h[],x,y,t=1;
-	
 	static void D(int i,int c) {
 		h[i]=c;
 		for(int j=1;j<n;j++)if(a[i][j]==1&&h[j]==0)D(j,c);
 	}
-	
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt()+1;
@@ -21,5 +18,4 @@ public class P11724_C {
 		for(m=1;m<n;m++)if(h[m]==0)D(m,t++);
 		System.out.println(t-1);
 	}
-
 }
