6de67bcdaba8ebda0449832f55431a83f008286e
nn98 <jkllhgb@gmail.com>
2020-02-13 07:37:52 +0900

    11724

--- src/BaekJoon/P11724_5.java
+++ src/BaekJoon/P11724_5.java
@@ -27,6 +27,7 @@ public class P11724_5 {
 		for(int j=1;j<n;j++)
 			if(a[i][j])
 				o(j,f);
+		System.out.println(Arrays.toString(b));
 		if(b.length>0) {
 			boolean c=false;
 			for(String s:b) {
