3155fcd3387475f9b63bda0d3928cb6a7bf2d86a
nn98 <jkllhgb@gmail.com>
2020-02-13 07:19:13 +0900

    11724

--- src/BaekJoon/P11724_4.java
+++ src/BaekJoon/P11724_4.java
@@ -1,5 +1,46 @@
 package BaekJoon;
 
-public class P11724_4 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class P11724_4 {
+	static int n,m,R=0,x=0,y,p,q;
+	static boolean h[],a[][];
+	static void o(int i,boolean c,int f,boolean F) {
+		System.out.println(f+" "+i+" "+c+" "+F);
+		if(h[i])return;
+		if(!F) {
+			if(i!=f) {
+				R+=c?1:0;
+				return;
+			}
+		}
+		else F=false;
+		if(h[i])c=false;
+		else h[i]=true;
+		for(int k=0;k<n;k++)if(a[i][k])
+			o(k,c,i,F);
+		
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken())+1;
+		m=Integer.parseInt(t.nextToken());
+		a=new boolean[n][n];
+		h=new boolean[n];
+		for(;m-->0;) {
+			t=new StringTokenizer(r.readLine());
+			p=Integer.parseInt(t.nextToken());
+			q=Integer.parseInt(t.nextToken());
+//			a[p][q]=a[q][p]=true;
+			a[p][q]=true;
+		}
+		for(boolean[]b:a)System.out.println(Arrays.toString(b));
+		for(m=1;m<n;m++)o(m,true,m,true);
+		System.out.println(Arrays.toString(h));
+		System.out.println(R);
+	}
 }
