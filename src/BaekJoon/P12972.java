40df810d20973fd45311996e0c9246d3a01bcec8
nn98 <jkllhgb@gmail.com>
2020-02-18 20:26:01 +0900

    12972

--- src/BaekJoon/P12972.java
+++ src/BaekJoon/P12972.java
@@ -1,14 +1,17 @@
 package BaekJoon;
+import java.util.ArrayList;
 //WTF
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class P12972 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[][]=new int[n][n],i=0,j,c[]=new int[1000000001];
-		for(;i++<n*n;c[s.nextInt()]++);
-		System.out.println(Arrays.toString(c));
+		int n=s.nextInt(),a[][]=new int[n][n],i=0,j;
+		List<Integer>l=new ArrayList();
+		for(;i++<n*n;l.add(s.nextInt()));
+		System.out.println(Arrays.toString(l));
 //		for(;i<n;i++)for(j=0;j<n;a[i][j++]=s.nextInt());
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
