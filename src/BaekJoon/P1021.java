e0288ebfcc946fcccd41e15b7415f3e5538e8bcc
nn98 <jkllhgb@gmail.com>
2020-02-18 12:40:09 +0900

    1021

--- src/BaekJoon/P1021.java
+++ src/BaekJoon/P1021.java
@@ -1,9 +1,14 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1021 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m],x,i=0,j=0,l=0,r=0;
+		boolean[]b=new boolean[n];
+		for(;i<m;a[i++]=s.nextInt()-1);
 	}
 
 }
