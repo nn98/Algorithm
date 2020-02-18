8ec7259148b77abdfbffdcffb5cade96d1d8f686
nn98 <jkllhgb@gmail.com>
2020-02-18 20:31:41 +0900

    12972

--- src/BaekJoon/P12972.java
+++ src/BaekJoon/P12972.java
@@ -9,10 +9,23 @@ public class P12972 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[][]=new int[n][n],i=0,j;
-		List<Integer>l=new ArrayList();
+		List<Integer>l=new ArrayList(),r=new ArrayList();
 		for(;i++<n*n;l.add(s.nextInt()));
 		Collections.sort(l);
+		Integer t=null;
+		for(Integer I:l) {
+			if(t==null) t=I;
+			else {
+				if(t==I) {
+					t=null;
+				} else {
+					r.add(t);
+					t=null;
+				}
+			}
+		}
 		System.out.println(l);
+		System.out.println(r);
 //		for(;i<n;i++)for(j=0;j<n;a[i][j++]=s.nextInt());
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
