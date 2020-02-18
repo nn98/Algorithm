d5fdfa5c714b441c39d5ff495c757aa29ddb3ce1
nn98 <jkllhgb@gmail.com>
2020-02-18 20:26:05 +0900

    12972

--- src/BaekJoon/P12972.java
+++ src/BaekJoon/P12972.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class P12972 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[][]=new int[n][n],i=0,j;
