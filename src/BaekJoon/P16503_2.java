d2351b591a4e2b40408f65a36ca31cfa2648a1b0
nn98 <jkllhgb@gmail.com>
2020-02-19 23:34:37 +0900

    16503

--- src/BaekJoon/P16503_2.java
+++ src/BaekJoon/P16503_2.java
@@ -28,6 +28,7 @@ public class P16503_2 {
 		}
 		i=o(a[0],o(a[1],a[2],1),0);
 		j=o(a[2],o(a[0],a[1],0),1);
-		System.out.println(i<j?i:j+"\n"+(i<j?j:i));
+		System.out.println(i+" "+j);
+		System.out.println(i<j?i:j+"\n"+(i>j?i:j));
 	}
 }
