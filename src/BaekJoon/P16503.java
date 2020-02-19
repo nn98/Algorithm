6dff6ddd492c83ce0c1c68be319cf16b22614d1b
nn98 <jkllhgb@gmail.com>
2020-02-19 23:11:55 +0900

    16503

--- src/BaekJoon/P16503.java
+++ src/BaekJoon/P16503.java
@@ -1,7 +1,24 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P16503 {
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		String[]a=s.nextLine().split(" ");
+		int q[]=new int[2],i=1,j=0,k=0;
+		for(;j<2;i+=2,j++,k+=2)
+			switch(a[i]) {
+			case "+":
+				q[j]=Integer.parseInt(a[k])+Integer.parseInt(a[k+2]);
+			case "-":
+				q[j]=Integer.parseInt(a[k])-Integer.parseInt(a[k+2]);
+			case "*":
+				q[j]=Integer.parseInt(a[k])*Integer.parseInt(a[k+2]);
+			case "/":
+				q[j]=Integer.parseInt(a[k])/Integer.parseInt(a[k+2]);
+			}
+		System.out.println(Arrays.toString(q));
 	}
 }
