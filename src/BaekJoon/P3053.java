3df048cbaca61f91f4d600ab2103d4355adf2769
nn98 <jkllhgb@gmail.com>
2020-02-20 00:00:40 +0900

    3053

--- src/BaekJoon/P3053.java
+++ src/BaekJoon/P3053.java
@@ -2,7 +2,7 @@ package BaekJoon;
 public class P3053 {
 	public static void main(String[] args) {
 		double n=new java.util.Scanner(System.in).nextInt();
-		
+		System.out.print(Math.PI*n*n+"\n"+n*n*2);
 //		System.out.printf("%.6f\n%.6f",n*n*Math.PI,n*n*2);
 	}
 }
