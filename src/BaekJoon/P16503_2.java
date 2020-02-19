ccb9281da4fcce28b433c849b94ac1b61df6aad0
nn98 <jkllhgb@gmail.com>
2020-02-19 23:22:19 +0900

    16503

--- src/BaekJoon/P16503_2.java
+++ src/BaekJoon/P16503_2.java
@@ -1,10 +1,29 @@
 package BaekJoon;
 
 public class P16503_2 {
-
+	static int a[]=new int[3],i=0,j=0,m=0;
+	static String[]b=new String[2];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(String s:new java.util.Scanner(System.in).nextLine().split(" ")) {
+			if(i%2==0)a[j++]=Integer.parseInt(s);
+			else b[m++]=s;
+			i++;
+		}
+		for(;j-->0;) {
+			switch(b[--m]) {
+			case "+":
+				
+				break;
+			case "-":
+				
+				break;
+			case "*":
+				
+				break;
+			case "/":
+				
+				break;
+			}
+		}
 	}
-
 }
