8b184d6fdbce23f0e1587d4e58a6634ac9f84741
nn98 <jkllhgb@gmail.com>
2020-02-24 22:57:33 +0900

    1062

--- src/BaekJoon/P1062.java
+++ src/BaekJoon/P1062.java
@@ -5,12 +5,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P1062 {
-
+	static boolean[]h=new boolean[26];
+	static int n,m,l=0;
+	static void o(int a,int c) {
+		
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken());
-		
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		if(m>4) {
+			if(m==26)l=26;
+			else {
+				
+			}
+		}
+		System.out.print(l);
 	}
-
 }
