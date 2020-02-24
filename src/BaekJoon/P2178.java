78f7097de0c465a210e2e9864ff8632de592a3a3
nn98 <jkllhgb@gmail.com>
2020-02-25 00:26:39 +0900

    2178

--- src/BaekJoon/P2178.java
+++ src/BaekJoon/P2178.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 
 public class P2178 {
 
-	static int x,y,a[][],i=0,j,r=99999;
+	static int x,y,a[][],h[][],i=0,j,r=99999,X[]= {1,0,-1,0},Y[]= {0,1,0,-1};
+	static void o(int p,int q,int c) {
+		if(p==x&&q==y) {
+			r=r<c?r:c;
+		}
+		h[p][q]=c;
+		int k=0,P,Q;
+		for(;k<4;k++) {
+			P=p+X[k];
+			Q=q+Y[k];
+			if(a[P][Q]==49)
+				if(h[P][Q]>c)
+					o(P,Q,c+1);
+		}
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		x=s.nextInt();
 		y=s.nextInt();
-		a=new int[x+1][y+1];
+		a=new int[x+2][y+2];
+		h=new int[x+2][y+2];
 		for(;i++<x;) {
 			j=1;
 			for(char k:s.next().toCharArray())
@@ -18,5 +33,7 @@ public class P2178 {
 		}
 		for(int[]k:a)
 			System.out.println(Arrays.toString(k));
+		o(1,1,1);
+		System.out.println(r);
 	}
 }
