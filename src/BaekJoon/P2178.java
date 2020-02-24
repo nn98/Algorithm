5b068b71f5fcc6cd296af8548ba820395534664b
nn98 <jkllhgb@gmail.com>
2020-02-24 23:49:09 +0900

    2178

--- src/BaekJoon/P2178.java
+++ src/BaekJoon/P2178.java
@@ -1,9 +1,23 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2178 {
 
+	static int x,y,a[][],i=0,j,r=99999;
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		x=s.nextInt();
+		y=s.nextInt();
+		a=new int[x+1][y+1];
+		for(;i++<x;) {
+			j=1;
+			for(char k:s.next().toCharArray())
+				a[i][j++]=k;
+		}
+		for(int[]k:a)
+			System.out.println(Arrays.toString(k));
 	}
 
 }
