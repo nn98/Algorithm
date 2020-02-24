2a11074e5d8d2f68e6a634d211c829370a846800
nn98 <jkllhgb@gmail.com>
2020-02-24 23:06:25 +0900

    1004

--- src/BaekJoon/P1004.java
+++ src/BaekJoon/P1004.java
@@ -1,9 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1004 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,a[]=new int[4];
 	}
 
 }
