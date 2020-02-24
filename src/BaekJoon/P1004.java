d6d2ba417fa4db478d64f38c739214f5d9c3cf6e
nn98 <jkllhgb@gmail.com>
2020-02-24 23:11:25 +0900

    1004

--- src/BaekJoon/P1004.java
+++ src/BaekJoon/P1004.java
@@ -6,7 +6,10 @@ public class P1004 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m,a[]=new int[4];
+		int n=s.nextInt(),m,a[]=new int[4],i,j,b[]=new int[3];
+		for(;n-->0;) {
+			for(i=0;i<4;a[i++]=s.nextInt());
+		}
 	}
 
 }
