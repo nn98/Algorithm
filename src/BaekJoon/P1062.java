02b5be0f548b9b379cf27098e51ad3912bedf4c2
nn98 <jkllhgb@gmail.com>
2020-02-24 22:45:46 +0900

    1062

--- src/BaekJoon/P1062.java
+++ src/BaekJoon/P1062.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P1062 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken());
+		
 	}
 
 }
