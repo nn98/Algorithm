69388b5ff7b50cb5678f079575dc0de6be4cc416
nn98 <jkllhgb@gmail.com>
2020-02-25 22:46:28 +0900

    7790

--- src/BaekJoon/P7790.java
+++ src/BaekJoon/P7790.java
@@ -1,9 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P7790 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int r=0,i;
+		while(s.hasNext()) {
+			char[]t=s.nextLine().toCharArray();
+			for(i=0;i<t.length-3;i++)
+				if(t[i]=='j'&&t[i+1]=='o'&&t[i+2]=='k'&&t[i+3]=='e')
+					r++;
+		}
+		System.out.println(r);
 	}
 
 }
