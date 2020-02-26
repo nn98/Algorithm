5028d2f3c675586a267416609a76c5482421a753
nn98 <jkllhgb@gmail.com>
2020-02-26 14:00:10 +0900

    1449

--- src/BaekJoon/P1449.java
+++ src/BaekJoon/P1449.java
@@ -1,10 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1449 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],i=0;
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
