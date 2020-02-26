0086f2105d9c7d36270b95cbbd161c3842e864c3
nn98 <jkllhgb@gmail.com>
2020-02-26 13:58:42 +0900

    1138

--- src/BaekJoon/P1138.java
+++ src/BaekJoon/P1138.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1138 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],i=0,j;
