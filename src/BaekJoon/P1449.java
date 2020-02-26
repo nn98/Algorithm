1964860111ca961eb761674045c2fd1f97911028
nn98 <jkllhgb@gmail.com>
2020-02-26 14:13:51 +0900

    1449

--- src/BaekJoon/P1449.java
+++ src/BaekJoon/P1449.java
@@ -2,9 +2,7 @@ package BaekJoon;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class P1449 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],i=0,t=0,j;
