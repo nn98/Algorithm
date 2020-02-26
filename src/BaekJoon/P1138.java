24a792af79d118ae1861d921efa79626d0593ee4
nn98 <jkllhgb@gmail.com>
2020-02-26 13:55:11 +0900

    1138

--- src/BaekJoon/P1138.java
+++ src/BaekJoon/P1138.java
@@ -9,7 +9,6 @@ public class P1138 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],i=0,j;
 		for(;i<n;a[i++]=s.nextInt());
-		System.out.println(Arrays.toString(a));
 		for(i=0;i<n;i++) {
 			j=0;
 			while(a[i]!=0){
