514606c3d37658d2c6dbda9a62cd93178dd062b5
nn98 <jkllhgb@gmail.com>
2020-02-26 13:45:23 +0900

    1138

--- src/BaekJoon/P1138.java
+++ src/BaekJoon/P1138.java
@@ -1,9 +1,22 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1138 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],i=0,j;
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n;i++) {
+			j=0;
+			while(a[i]!=0) {
+				if(b[j]==0)
+					a[i]--;
+				j++;
+			}
+			b[j]=i+1;
+		}
 	}
 
 }
