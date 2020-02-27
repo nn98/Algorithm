5b73dc6a034518a2487785ad606db2caedfffb63
nn98 <jkllhgb@gmail.com>
2020-02-28 02:14:20 +0900

    1264

--- src/BaekJoon/P1264.java
+++ src/BaekJoon/P1264.java
@@ -8,7 +8,7 @@ public class P1264 {
 		Scanner s=new Scanner(System.in);
 		String in;
 		int i=0,r=0,a[],j;
-		System.out.println((int)'e');
+		System.out.println((int)'e'-'a');
 		while(!"#".equals(in=s.nextLine())) {
 			a=new int[26];
 			for(;i<in.length();j=in.charAt(i++)-'a',a[j<26&&j>=0?j:1]++);
