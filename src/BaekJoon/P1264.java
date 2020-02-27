0e25048103035e8a209d65f0f19c0e009f8a3b1c
nn98 <jkllhgb@gmail.com>
2020-02-28 02:13:15 +0900

    1264

--- src/BaekJoon/P1264.java
+++ src/BaekJoon/P1264.java
@@ -7,10 +7,10 @@ public class P1264 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String in;
-		int i=0,r=0,a[];
+		int i=0,r=0,a[],j;
 		while(!"#".equals(in=s.nextLine())) {
 			a=new int[26];
-			for(;i<in.length();a[in.charAt(i++)-'a']++);
+			for(;i<in.length();j=in.charAt(i++)-'a',a[j<26&&j>=0?j:1]++);
 			System.out.println(a[0]+a[4]+a[8]+a[17]+a[20]);
 		}
 		
