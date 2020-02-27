4335683e4ed23b6fa9763700d2a8bc1d04ccadfd
nn98 <jkllhgb@gmail.com>
2020-02-28 02:09:25 +0900

    1264

--- src/BaekJoon/P1264.java
+++ src/BaekJoon/P1264.java
@@ -1,9 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1264 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		String in;
+		int i=0,r=0,a[]=new int[26];
+		while(!"#".equals(in=s.nextLine())) {
+			for(;i<in.length();a[in.charAt(i++)-'a']++);
+		}
 	}
 
 }
