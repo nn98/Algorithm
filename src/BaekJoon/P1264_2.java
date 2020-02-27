10f42aeaa812f73f4d1a5d34eebe51eaf9ee21c1
nn98 <jkllhgb@gmail.com>
2020-02-28 02:20:46 +0900

    1264

--- src/BaekJoon/P1264_2.java
+++ src/BaekJoon/P1264_2.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1264_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String in;
+		int r,i;
+		while(!"#".equals(in=s.nextLine())) {
+			for()
+		}
 	}
 
 }
