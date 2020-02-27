49ff089387ef087ca1760bee9673887574efecac
nn98 <jkllhgb@gmail.com>
2020-02-28 02:17:41 +0900

    1264

--- src/BaekJoon/P1264.java
+++ src/BaekJoon/P1264.java
@@ -10,7 +10,7 @@ public class P1264 {
 		int i=0,r=0,a[],j;
 		while(!"#".equals(in=s.nextLine())) {
 			a=new int[26];
-			for(;i<in.length();j=in.charAt(i++)-'a',a[j<26&&j>=0?j:1]++);
+			for(;i<in.length();j=in.charAt(i++),j=j<97?j+=32:j,j-='a',a[j<26&&j>=0?j:1]++);
 			System.out.println(a[0]+a[4]+a[8]+a[14]+a[20]);
 		}
 		
