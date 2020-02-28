d995f836eded91097bf4551557e489d5525daed2
nn98 <jkllhgb@gmail.com>
2020-02-29 01:11:57 +0900

    1564

--- src/BaekJoon/P1564_7.java
+++ src/BaekJoon/P1564_7.java
@@ -1,5 +1,14 @@
 package BaekJoon;
-
 public class P1564_7 {
-
+	public static void main(String[] args)throws Exception{
+		long n=new java.util.Scanner(System.in).nextLong(),m=1,r=1,d=1000000000000L;
+		while(m++<n) {
+			r*=m;
+			while(r%10==0)r/=10;
+//			? ¾Æ´Ï ½¬¹ú?
+			r%=d;
+		}
+		String R=""+r;
+		System.out.print(R.substring(R.length()-5,R.length()));
+	}
 }
