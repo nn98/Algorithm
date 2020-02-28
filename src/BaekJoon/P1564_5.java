feffd527f401e2c9542b8ccc652c406dec82b303
nn98 <jkllhgb@gmail.com>
2020-02-29 01:00:00 +0900

    1564

--- src/BaekJoon/P1564_5.java
+++ src/BaekJoon/P1564_5.java
@@ -1,10 +1,18 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1564_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		long n=s.nextLong(),m=1,r=1;
+		while(m++<n) {
+			r*=m;
+			while(r%10==0)r/=10;
+		}
+		String R=""+r;
+		System.out.println(R.substring(R.length()-5,R.length()));
 	}
 
 }
