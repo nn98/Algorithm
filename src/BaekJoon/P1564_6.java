490745066d973869a0dca19168d5dfee2522f2eb
nn98 <jkllhgb@gmail.com>
2020-02-29 01:02:30 +0900

    1564

--- src/BaekJoon/P1564_6.java
+++ src/BaekJoon/P1564_6.java
@@ -1,5 +1,17 @@
 package BaekJoon;
 
-public class P1564_6 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class P1564_6 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		long n=Long.parseLong(br.readLine()),m=1,r=1;
+		while(m++<n) {
+			r*=m;
+			while(r%10==0)r/=10;
+		}
+		String R=""+r;
+		System.out.println(R.substring(R.length()-5,R.length()));
+	}
 }
