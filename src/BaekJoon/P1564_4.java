4085b07f4b7c495d5d70d0be138f4a1fd5c006fa
nn98 <jkllhgb@gmail.com>
2020-02-29 00:27:10 +0900

    1564

--- src/BaekJoon/P1564_4.java
+++ src/BaekJoon/P1564_4.java
@@ -7,14 +7,14 @@ public class P1564_4 {
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),i;
 		BigInteger b=new BigInteger(""+n--);
-		String t;
+		StringBuffer t;
 		while(n>1) {
 			b=b.multiply(new BigInteger(""+n--));
-			t=b.toString();
+			t=new StringBuffer(b.toString());
 			for(i=t.length();t.charAt(--i)=='0';);
 			b=new BigInteger(t.substring(0,i+1));
 		}
-		t=b.toString();
+		t=new StringBuffer(b.toString());
 		System.out.println(t.substring(t.length()-5,t.length()));
 	}
 }
