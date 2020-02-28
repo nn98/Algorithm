d675312e3c481de24412911040042baae464eb06
nn98 <jkllhgb@gmail.com>
2020-02-29 01:06:15 +0900

    1564

--- src/BaekJoon/P1564_6.java
+++ src/BaekJoon/P1564_6.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class P1564_6 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		long n=Long.parseLong(br.readLine()),m=1,r=1;
+		long n=Long.parseLong(br.readLine()),m=1,r=1,d=1000000000000L;
 		while(m++<n) {
 			r*=m;
 			while(r%10==0)r/=10;
