5898e9da2b884e604c0c7be3f6259fc0264281c7
nn98 <jkllhgb@gmail.com>
2020-03-01 22:25:35 +0900

    14888

--- src/BaekJoon/P14888.java
+++ src/BaekJoon/P14888.java
@@ -4,11 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P14888 {
-
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],b[]=new int[4],M=Integer.MIN_VALUE,m=Integer.MAX_VALUE;
 		
 	}
-
 }
