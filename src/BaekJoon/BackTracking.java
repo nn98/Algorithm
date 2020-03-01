27a52fd62e1f5202a8b1d61f7669757347e54338
nn98 <jkllhgb@gmail.com>
2020-03-01 22:38:15 +0900

    backT

---
+++ src/BaekJoon/BackTracking.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackTracking {
+
+	static int n,m,a[],b[];
+	static void PrintA() {
+		for(int i:b)
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("count of Integer: ");
+		n=s.nextInt();
+		a=new int[n];
+		System.out.print("count of Set: ");
+		m=s.nextInt();
+		b=new int[m];
+		
+	}
+
+}
