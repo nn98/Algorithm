32f5774949994d9ccde1c9368b0078d7f56220dc
nn98 <jkllhgb@gmail.com>
2020-03-01 22:52:59 +0900

    18258

--- src/BaekJoon/P18258.java
+++ src/BaekJoon/P18258.java
@@ -1,9 +1,31 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P18258 {
 
+	static class Node{
+		int v;
+		Node n;
+		Node(int n){n=v;}
+	}
+	static Node h=null,t=null;
+	static int c;
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m;
+		for(;n-->0;) {
+			String g=s.next();
+			switch(g) {
+			case "push":
+				c++;
+				Node N=new Node(s.nextInt());
+				if(h==null)h=N;
+				if(t!=null)t.n=N;
+				t=N;
+				break;
+			}
+		}
 	}
 
 }
