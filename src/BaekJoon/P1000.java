817e5a91c9a4530f527a50c36a7463748c2fe8a6
nn98 <jkllhgb@gmail.com>
2020-03-02 19:52:32 +0900

    1000

---
+++ src/BaekJoon/P1000.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P1000 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println(s.nextInt()+s.nextInt());
+	}
+
+}
