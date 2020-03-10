dcd4525e92dfdcafed98b7917fe21e028cbed9d3
nss <nss@121.125.203.105>
2020-03-10 18:37:33 +0900

    11066

--- src/BaekJoon/P11066_2.java
+++ src/BaekJoon/P11066_2.java
@@ -1,13 +1,20 @@
 package BaekJoon;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P11066_2 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),m,a[],h[];
-				
+		int n=Integer.parseInt(r.readLine()),m,a[],h[],i,j;
+		for(;n-->0;) {
+			m=Integer.parseInt(r.readLine());
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			a=new int[m];
+			for(i=0;i<m;a[i++]=Integer.parseInt(t.nextToken()));
+		}
 	}
 
 }
