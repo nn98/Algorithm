64e0bc504fa6351f0fae427850d58d65c9e860bc
nss <nss@121.125.203.105>
2020-03-10 18:37:28 +0900

    이게 되넼ㅋㅋㅋㅋㅋㅋ

--- src/BaekJoon/P11066_2.java
+++ src/BaekJoon/P11066_2.java
@@ -1,9 +1,13 @@
 package BaekJoon;
 
+import java.io.*;
+
 public class P11066_2 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),m,a[],h[];
+				
 	}
 
 }
