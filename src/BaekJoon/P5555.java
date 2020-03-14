e268430814b58d631adf797c2b0f69c1a50a5f91
nn98 <jkllhgb@gmail.com>
2020-03-14 20:09:54 +0900

    5555

--- src/BaekJoon/P5555.java
+++ src/BaekJoon/P5555.java
@@ -1,10 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5555 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String a=s.next(),b;
+		int i=s.nextInt(),r=0;
+		for(;i-->0;) {
+			b=s.next();
+			b=b+b;
+			if(b.contains(a))r++;
+		}
+		System.out.print(r);
 	}
 
 }
