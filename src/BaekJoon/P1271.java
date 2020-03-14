ead0329646ff75acf17ba4134c675a8c835971d0
nn98 <jkllhgb@gmail.com>
2020-03-14 20:18:23 +0900

    1271

--- src/BaekJoon/P1271.java
+++ src/BaekJoon/P1271.java
@@ -1,8 +1,14 @@
 package BaekJoon;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P1271 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		BigInteger m=new BigInteger(s.next()),n=new BigInteger(s.next());
+		System.out.print(m.divide(n)+"\n"+m.mod(n));
 	}
 
 }
