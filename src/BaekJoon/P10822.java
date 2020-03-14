4143db432f13140bcaacd7f13d7c4c94d7e8b130
nn98 <jkllhgb@gmail.com>
2020-03-14 20:16:30 +0900

    10822

--- src/BaekJoon/P10822.java
+++ src/BaekJoon/P10822.java
@@ -1,11 +1,9 @@
 package BaekJoon;
 
 public class P10822 {
-
 	public static void main(String[] args) {
 		int r=0;
 		for(String a:new java.util.Scanner(System.in).next().split(","))r+=Integer.parseInt(a);
 		System.out.print(r);
 	}
-
 }
