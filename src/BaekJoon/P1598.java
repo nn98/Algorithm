32e5b771d1823a36109dd5202b2462d9fdad8da4
nn98 <jkllhgb@gmail.com>
2020-03-19 13:50:35 +0900

    1598

--- src/BaekJoon/P1598.java
+++ src/BaekJoon/P1598.java
@@ -5,6 +5,6 @@ public class P1598 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt();
-		System.out.print(Math.abs(n/4-m/4)+Math.abs(n%4==0?4:n%4-m%4==0?4:m%4));
+		System.out.print(Math.abs(n%4==0?n/4-1:n/4-m%4==0?m/4-1:m/4)+Math.abs(n%4==0?4:n%4-m%4==0?4:m%4));
 	}
 }
