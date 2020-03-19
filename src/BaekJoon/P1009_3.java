778ddb20555a48a9d181b1ad826d68bb122c8fb7
nn98 <jkllhgb@gmail.com>
2020-03-19 13:27:40 +0900

    1009

--- src/BaekJoon/P1009_3.java
+++ src/BaekJoon/P1009_3.java
@@ -1,5 +1,7 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 //1
 //2 4 8 6 2
 //3 9 7 1 3
@@ -12,8 +14,19 @@ package BaekJoon;
 public class P1009_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,i=0,k,a[][]= {
+				{1},
+				{2,4,8,6},
+				{3,9,7,1},
+				{4,6},
+				{5},
+				{6},
+				{7,9,3,1},
+				{8,4,2,6},
+				{9,1},
+		};
+		for(;n-->0;m=s.nextInt(),k=s.nextInt()-1,System.out.println(a[m][k%a[m].length]));
 	}
 
 }
