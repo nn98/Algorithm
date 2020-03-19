3887ba94ea4b5b73b16f338c7e4353e1454f8539
nn98 <jkllhgb@gmail.com>
2020-03-19 13:35:43 +0900

    1009

--- src/BaekJoon/P1009_4.java
+++ src/BaekJoon/P1009_4.java
@@ -1,5 +1,22 @@
 package BaekJoon;
 
-public class P1009_4 {
+import java.util.Scanner;
 
+public class P1009_4 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		long n=s.nextLong(),m,i=0,k,a[][]= {
+				{1},
+				{2,4,8,6},
+				{3,9,7,1},
+				{4,6},
+				{5},
+				{6},
+				{7,9,3,1},
+				{8,4,2,6},
+				{9,1},
+				{10}
+		};
+		for(;n-->0;m=s.nextInt()-1,k=s.nextInt()-1,System.out.println(a[(int) (m%10)][(int) (k%a[(int) m].length)]));
+	}
 }
