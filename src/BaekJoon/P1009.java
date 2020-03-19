6d45feef8d6d551a3c7c6d47c8a45cbd3aae3bfc
nn98 <jkllhgb@gmail.com>
2020-03-19 13:17:29 +0900

    1009

--- src/BaekJoon/P1009.java
+++ src/BaekJoon/P1009.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P1009 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
