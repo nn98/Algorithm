1cc3794a407c1f00a4c8f46fbf15b97812606369
nn98 <jkllhgb@gmail.com>
2020-03-19 13:59:30 +0900

    2525

--- src/BaekJoon/P2525.java
+++ src/BaekJoon/P2525.java
@@ -1,8 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2525 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt()+s.nextInt();
+		System.out.print((n+m/60)%24+" "+m%60);
 	}
 
 }
