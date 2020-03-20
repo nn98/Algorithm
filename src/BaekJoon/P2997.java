82ab9855e023321663e6fa27f93dab54970d2ba7
nn98 <jkllhgb@gmail.com>
2020-03-20 21:14:24 +0900

    2997

--- src/BaekJoon/P2997.java
+++ src/BaekJoon/P2997.java
@@ -8,5 +8,8 @@ public class P2997 {
 		int a[]=new int[3],i=0,n,m;
 		for(;i<3;a[i++]=s.nextInt());
 		Arrays.sort(a);
+		n=a[1]-a[0];
+		m=a[2]-a[1];
+		System.out.println(n==m?n:(n<m?n:m));
 	}
 }
