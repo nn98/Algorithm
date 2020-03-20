3cb5b993097c53404773a91a55ec7d7f0df625a2
nn98 <jkllhgb@gmail.com>
2020-03-20 21:25:30 +0900

    3034

--- src/BaekJoon/P3034.java
+++ src/BaekJoon/P3034.java
@@ -7,6 +7,7 @@ public class P3034 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt(),p=(int)Math.sqrt(m*m+k*k);
+		for(;n-->0;System.out.println(s.nextInt()<=p?"DA":"NE"));
 	}
 
 }
