ceb116ef6b69dbf2dc633a5e36ad0ae040cf49e6
nn98 <jkllhgb@gmail.com>
2020-03-20 21:24:29 +0900

    3034

--- src/BaekJoon/P3034.java
+++ src/BaekJoon/P3034.java
@@ -1,9 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P3034 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt(),p=(int)Math.sqrt(m*m+k*k);
 	}
 
 }
