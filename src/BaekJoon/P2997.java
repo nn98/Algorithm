927c7a50aab24b629ca976b40190636c8c5f280d
nn98 <jkllhgb@gmail.com>
2020-03-20 21:11:57 +0900

    2997

--- src/BaekJoon/P2997.java
+++ src/BaekJoon/P2997.java
@@ -2,9 +2,7 @@ package BaekJoon;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class P2997 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a[]=new int[3],i=0,n,m;
