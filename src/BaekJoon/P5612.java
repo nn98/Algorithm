ee99a6a6e7d9eca5bd395a058b5df9c0a688ce4f
nn98 <jkllhgb@gmail.com>
2020-03-22 02:34:43 +0900

    5612

--- src/BaekJoon/P5612.java
+++ src/BaekJoon/P5612.java
@@ -8,6 +8,6 @@ public class P5612 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),r=s.nextInt(),m=0;
 		for(;n-->0;r+=s.nextInt()-s.nextInt(),m=m>r?m:r);
-		System.out.println(m);
+		System.out.print(m);
 	}
 }
