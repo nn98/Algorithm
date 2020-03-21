1118efdc169aeb19546b3c8d9450483d8192ad7d
nn98 <jkllhgb@gmail.com>
2020-03-21 23:04:50 +0900

    4493

--- src/BaekJoon/P4493.java
+++ src/BaekJoon/P4493.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P4493 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m,i,a,b;
