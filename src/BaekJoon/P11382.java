5263753ab6c407616d153aac5e32871006f6b38a
nn98 <jkllhgb@gmail.com>
2020-03-22 02:39:19 +0900

    11382

--- src/BaekJoon/P11382.java
+++ src/BaekJoon/P11382.java
@@ -1,10 +1,14 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P11382 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		long r=0,i=3;
+		for(;i-->0;r+=s.nextLong());
+		System.out.print(r);
 	}
 
 }
