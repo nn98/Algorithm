df85f0cf1fa859e2a722a99771234539ebb4378b
nn98 <jkllhgb@gmail.com>
2020-03-23 01:05:16 +0900

    11382

--- src/BaekJoon/P11382.java
+++ src/BaekJoon/P11382.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class P11382 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		long r=0,i=3;
 		for(;i-->0;r+=s.nextLong());
