7a5cf0484a0165ac444683ae0288801ccd6133c4
nn98 <jkllhgb@gmail.com>
2020-03-25 23:13:33 +0900

    2798

--- src/BaekJoon/P2798.java
+++ src/BaekJoon/P2798.java
@@ -1,5 +1,14 @@
 package BaekJoon;
 
-public class P2798 {
+import java.util.Scanner;
 
+public class P2798 {
+	static int n,m,a[],r,i;
+	public static void main(String[]z) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		a=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+	}
 }
