d6989e69e2795e569e302ab64531a105a9f3ad97
nn98 <jkllhgb@gmail.com>
2020-03-26 08:08:56 +0900

    2798

--- src/BaekJoon/P2798_3.java
+++ src/BaekJoon/P2798_3.java
@@ -1,9 +1,21 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2798_3 {
 
 	public static void main(String[] args) {
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],i=0,j,k,r=0,u;
+		for(;i<n;a[i++]=s.nextInt());
+		outline:for(i=0;i<n-2;i++)
+			for(j=i+1;j<n-1;j++)
+				for(k=i+2;k<n;k++) {
+					u=a[i]+a[j]+a[k];
+					if(u<=m)r=r>u?r:u;
+					if(r==m)break outline;
+				}
+		System.out.println(r);
 	}
 
 }
