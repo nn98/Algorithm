f70d42ddfc12c2daac9f0b5141a5b36eb5458e52
nn98 <jkllhgb@gmail.com>
2020-03-25 23:18:37 +0900

    2798

--- src/BaekJoon/P2798.java
+++ src/BaekJoon/P2798.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class P2798 {
 	static int n,m,a[],r,i;
+	static void o(int x,int v,int c) {
+		if(c>2)return;
+		if(v>m)return;
+		if(r==m)return;
+		if(x==n)r=r>v?r:v;
+		o(x+1,v+a[x],c+1);
+		o(x+1,v,c);
+	}
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		m=s.nextInt();
 		a=new int[n];
 		for(;i<n;a[i++]=s.nextInt());
+		System.out.print(r);
 	}
 }
