af35f6e3979a236a8fd59c6729ae9666d2e0a105
nn98 <jkllhgb@gmail.com>
2020-03-25 23:05:19 +0900

    14491

--- src/BaekJoon/P14491.java
+++ src/BaekJoon/P14491.java
@@ -3,7 +3,14 @@ package BaekJoon;
 public class P14491 {
 
 	public static void main(String[] args) {
-		
+		int n=new java.util.Scanner(System.in).nextInt(),m=6561;
+		String r="";
+		while(m>0) {
+			r+=n/m;
+			n%=m;
+			m/=9;
+		}
+		System.out.print(r);
 	}
 
 }
