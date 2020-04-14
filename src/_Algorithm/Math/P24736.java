88b57ca4c925e6494bd4805f45746cb07bc0e45a
nn98 <jkllhgb@gmail.com>
2020-04-14 17:04:10 +0900

    24736

--- src/_Algorithm/Math/P24736.java
+++ src/_Algorithm/Math/P24736.java
@@ -1,10 +1,19 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P24736 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=0,j,u=0,a[]= {6,3,2,1,2};
+		for(;i<10;) {
+			u+=s.nextInt()*a[i++%5];
+			if(i%5==0) {
+				System.out.println(u);
+				u=0;
+			}
+		}
 	}
 
 }
