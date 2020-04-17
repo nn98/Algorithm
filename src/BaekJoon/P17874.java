01453dae164bb313672eb153c4eae0458c26a61c
nn98 <jkllhgb@gmail.com>
2020-04-17 23:38:42 +0900

    17874

--- src/BaekJoon/P17874.java
+++ src/BaekJoon/P17874.java
@@ -1,13 +1,10 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P17874 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
 		System.out.print(Math.max(a-b,b)*Math.max(a-c,c)*4);
 	}
-
 }
