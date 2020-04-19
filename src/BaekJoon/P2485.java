259bf0e71b9910b0a56820c0f797b1bd38a530f7
nn98 <jkllhgb@gmail.com>
2020-04-19 23:16:08 +0900

    2485

--- src/BaekJoon/P2485.java
+++ src/BaekJoon/P2485.java
@@ -9,9 +9,9 @@ public class P2485 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],m=Integer.MAX_VALUE,i=0,j;
+		for(;i<n;a[i++]=s.nextInt());
 		List<Integer>l=new ArrayList();
-		for(;i<n;i++) {
-			a[i]=s.nextInt();
+		for(i=0;i<n;i++) {
 			if(i>0) {
 				j=a[i]-a[i-1];
 				m=m<j?m:j;
