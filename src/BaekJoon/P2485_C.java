167339c9833ed16d79bc1b9631b474878487c95e
nn98 <jkllhgb@gmail.com>
2020-04-19 23:57:35 +0900

    2485

--- src/BaekJoon/P2485_C.java
+++ src/BaekJoon/P2485_C.java
@@ -2,8 +2,17 @@ package BaekJoon;
 
 public class P2485_C {
 
+	static int G(int a,int b) {
+		for(;;) {
+			int c=a%b;
+			if(c==0)return b;
+			a=b;
+			b=c;
+		}
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		System.out.println(G(3,17));
 	}
 
 }
