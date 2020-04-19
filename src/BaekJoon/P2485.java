f095330fa5c7fc6564522e15095ff88d3d385e1c
nn98 <jkllhgb@gmail.com>
2020-04-19 23:20:19 +0900

    2485

--- src/BaekJoon/P2485.java
+++ src/BaekJoon/P2485.java
@@ -18,7 +18,7 @@ public class P2485 {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(G(23,11));
+		System.out.println(G(24,6));
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],m=Integer.MAX_VALUE,i=0,j;
 		for(;i<n;a[i++]=s.nextInt());
