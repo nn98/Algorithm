ba366877084c953ca7a021d9782c951560215dbc
nn98 <jkllhgb@gmail.com>
2020-04-19 23:07:19 +0900

    2485

--- src/BaekJoon/P2485.java
+++ src/BaekJoon/P2485.java
@@ -1,8 +1,23 @@
 package BaekJoon;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P2485 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],m=Integer.MAX_VALUE,i=0,j;
+		List<Integer>l=new ArrayList();
+		for(;i<n;) {
+			a[i++]=s.nextInt();
+			if(i>1) {
+				j=a[i]-a[i-1];
+				m<j?m:j;
+				l.add(j);
+			}
+		}
 		
 	}
 
