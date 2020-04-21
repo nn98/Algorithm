5948f98a3ab086e4dd986b6c3c13cee523c91642
nn98 <jkllhgb@gmail.com>
2020-04-22 07:47:21 +0900

    16926

--- src/BaekJoon/P16926.java
+++ src/BaekJoon/P16926.java
@@ -1,10 +1,20 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P16926 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,a[][],i,j,k;
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		k=Integer.parseInt(t.nextToken());
+		a=new int[n][m];
 	}
 
 }
