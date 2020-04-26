9bf759feb4e8ffe20c924c5f76131798c4063fb8
nn98 <jkllhgb@gmail.com>
2020-04-26 14:44:05 +0900

    7567

--- src/BaekJoon/P7567.java
+++ src/BaekJoon/P7567.java
@@ -1,12 +1,10 @@
 package BaekJoon;
 
 public class P7567 {
-
 	public static void main(String[] args) {
 		String[]a=new java.util.Scanner(System.in).next().split("");
 		int r=10,i=1;
 		for(;i<a.length;r+=a[i].equals(a[i-1])?5:10,i++);
 		System.out.print(r);
 	}
-
 }
