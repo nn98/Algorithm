ea1d82a6e002fbcfe1bd0850dfac1c50caa9a944
nn98 <jkllhgb@gmail.com>
2020-04-26 14:42:06 +0900

    7567

--- src/BaekJoon/P7567.java
+++ src/BaekJoon/P7567.java
@@ -3,8 +3,10 @@ package BaekJoon;
 public class P7567 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[]a=new java.util.Scanner(System.in).next().split("");
+		int r=10,i=1;
+		for(;i<a.length;r+=a[i++].equals(a[i-1])?5:10);
+		System.out.print(r);
 	}
 
 }
