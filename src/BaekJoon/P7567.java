4fca96b1e0dcd16e9f897c10bc512d0ea25e0a35
nn98 <jkllhgb@gmail.com>
2020-04-26 14:43:13 +0900

    7567

--- src/BaekJoon/P7567.java
+++ src/BaekJoon/P7567.java
@@ -5,7 +5,7 @@ public class P7567 {
 	public static void main(String[] args) {
 		String[]a=new java.util.Scanner(System.in).next().split("");
 		int r=10,i=1;
-		for(;i<a.length;r+=a[i++].equals(a[i-1])?5:10);
+		for(;i<a.length;r+=a[i].equals(a[i-1])?5:10,i++);
 		System.out.print(r);
 	}
 
