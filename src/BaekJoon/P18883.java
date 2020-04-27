32287a7291bd053232f43a861d1e6a02b0d2f413
nn98 <jkllhgb@gmail.com>
2020-04-27 21:42:32 +0900

    18883

--- src/BaekJoon/P18883.java
+++ src/BaekJoon/P18883.java
@@ -7,7 +7,7 @@ public class P18883 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=1;
-		for(;i<=n*m;System.out.println(i%m==0?"\n":i+++" "));
+		for(;i<=n*m;System.out.println(i%m==0?i+++"\n":i+++" "));
 	}
 
 }
