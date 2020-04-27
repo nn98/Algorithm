b47d84ac10e4ec8e3e73f193256c45b7f91e2aed
nn98 <jkllhgb@gmail.com>
2020-04-27 21:41:05 +0900

    18883

--- src/BaekJoon/P18883.java
+++ src/BaekJoon/P18883.java
@@ -1,9 +1,13 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P18883 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=1;
+		for(;i<=n*m;System.out.println(i%m==0?"\n":i+++" "));
 	}
 
 }
