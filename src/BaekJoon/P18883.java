0b622d77b1f6d853ee1fb67dcaf49fc9e426ba48
nn98 <jkllhgb@gmail.com>
2020-04-27 21:47:50 +0900

    18883

--- src/BaekJoon/P18883.java
+++ src/BaekJoon/P18883.java
@@ -1,13 +1,16 @@
 package BaekJoon;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class P18883 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0;
-		for(;i<n*m;System.out.print(++i%m==0?i+"\n":i+" "));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		for(;i<n*m;w.write(++i%m==0?i+"\n":i+" "));
 	}
 
 }
