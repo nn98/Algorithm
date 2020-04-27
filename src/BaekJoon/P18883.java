53336f67cf4b0e00e14152f9f580f61b004abaa5
nn98 <jkllhgb@gmail.com>
2020-04-27 21:57:05 +0900

    18883

--- src/BaekJoon/P18883.java
+++ src/BaekJoon/P18883.java
@@ -3,9 +3,7 @@ package BaekJoon;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
-
 public class P18883 {
-
 	public static void main(String[] args)throws Exception{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0;
@@ -13,5 +11,4 @@ public class P18883 {
 		for(;i<n*m;w.write(++i%m==0?i+"\n":i+" "));
 		w.flush();
 	}
-
 }
