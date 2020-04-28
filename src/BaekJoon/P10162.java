fa05b5b5f4a482192510fdacb5a2574ddb0b6d8a
nn98 <jkllhgb@gmail.com>
2020-04-28 21:30:17 +0900

    10162

--- src/BaekJoon/P10162.java
+++ src/BaekJoon/P10162.java
@@ -3,6 +3,6 @@ package BaekJoon;
 public class P10162 {
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt();
-		System.out.print(n/300+" "+n%300/60+" "+n%60/10);
+		System.out.print(n%10==0?n/300+" "+n%300/60+" "+n%60/10:-1);
 	}
 }
