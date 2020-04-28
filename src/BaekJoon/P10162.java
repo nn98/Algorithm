fab69e61a936d3c02bea88538086746886aa0e79
nn98 <jkllhgb@gmail.com>
2020-04-28 21:14:00 +0900

    10162

--- src/BaekJoon/P10162.java
+++ src/BaekJoon/P10162.java
@@ -3,7 +3,14 @@ package BaekJoon;
 public class P10162 {
 
 	public static void main(String[] args) {
-		
+		int n=new java.util.Scanner(System.in).nextInt(),r=0;
+		r+=n/300;
+		n%=300;
+		r+=n/60;
+		n%=60;
+		r+=n/10;
+		n%=10;
+		System.out.print(r);
 	}
 
 }
