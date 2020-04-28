cafa21e48075189e4e9858b962e77924ab3b50c9
nn98 <jkllhgb@gmail.com>
2020-04-28 21:17:21 +0900

    10162

--- src/BaekJoon/P10162.java
+++ src/BaekJoon/P10162.java
@@ -1,10 +1,8 @@
 package BaekJoon;
 
 public class P10162 {
-
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt();
 		System.out.print(n/300+" "+n%300/60+" "+n%60/10);
 	}
-
 }
