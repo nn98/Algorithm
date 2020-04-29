1585e7fc228871b33e564cb4cb7791a891c4657b
nn98 <jkllhgb@gmail.com>
2020-04-29 20:31:58 +0900

    1919

--- src/BaekJoon/P1919.java
+++ src/BaekJoon/P1919.java
@@ -7,7 +7,16 @@ public class P1919 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String[]a=s.next().split(""),b=s.next().split("");
-		
+		int i=0,j=b.length-1,k,c=0;
+		for(;i<a.length;i++) {
+			for(k=j;k>=0;k--) {
+				if(a[i].equals(b[k])) {
+					j=k-1;
+					c++;
+				}
+			}
+		}
+		System.out.println(c);
 	}
 
 }
