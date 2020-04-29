19f725f7c74cb82415ec2ac51ea665e311facbd8
nn98 <jkllhgb@gmail.com>
2020-04-29 20:17:34 +0900

    1919

--- src/BaekJoon/P1919.java
+++ src/BaekJoon/P1919.java
@@ -1,8 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1919 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String[]a=s.next().split(""),b=s.next().split("");
 		
 	}
 
