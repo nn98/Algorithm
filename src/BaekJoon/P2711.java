0200169b0ea58b21d6e95de03c0be26c59621bf5
nn98 <jkllhgb@gmail.com>
2020-04-30 23:25:33 +0900

    2711

--- src/BaekJoon/P2711.java
+++ src/BaekJoon/P2711.java
@@ -7,13 +7,10 @@ public class P2711 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i;
-		StringBuffer t,r=new StringBuffer();
 		for(;n-->0;) {
 			i=s.nextInt();
-			t=new StringBuffer(s.next());
-			t.replace(i-1,i,"");
-			r.append(t+"\n");
+			StringBuffer t=new StringBuffer(s.next());
+			System.out.println(t.deleteCharAt(i-1));
 		}
-		System.out.print(r);
 	}
 }
