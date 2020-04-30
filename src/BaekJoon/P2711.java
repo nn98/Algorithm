ac18c3994fa1f39d1d4e0f69e87f1d2c905a088c
nn98 <jkllhgb@gmail.com>
2020-04-30 23:26:13 +0900

    2711

--- src/BaekJoon/P2711.java
+++ src/BaekJoon/P2711.java
@@ -2,7 +2,7 @@ package BaekJoon;
 
 import java.util.Scanner;
 public class P2711 {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i;
 		for(;n-->0;) {
