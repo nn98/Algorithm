219adf0658e786672b39a2a0dc52ed07b2f90ce5
nn98 <jkllhgb@gmail.com>
2020-04-30 23:20:03 +0900

    2711

--- src/BaekJoon/P2711.java
+++ src/BaekJoon/P2711.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2711 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i,j,k;
+		for(;n-->0;) {
+			i=s.nextInt();
+			String t=s.next();
+		}
 	}
 
 }
