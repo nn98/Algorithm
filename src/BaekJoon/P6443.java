32593b32742a17b5bdeb1e7dbb016d8f67ecef2b
nn98 <jkllhgb@gmail.com>
2020-05-01 23:11:51 +0900

    6443

--- src/BaekJoon/P6443.java
+++ src/BaekJoon/P6443.java
@@ -1,9 +1,24 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P6443 {
 
-	public static void main(String[] args) {
-		
+	static BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n,i;
+	static char[]a;
+	static boolean[]p;
+	
+	public static void main(String[] args)throws Exception{
+		n=Integer.parseInt(r.readLine());
+		for(;i<n;i++) {
+			a=r.readLine().toCharArray();
+			p=new boolean[26];
+		}
 	}
 
 }
