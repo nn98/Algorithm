f0bb407131cc3c836774a869896a89b240e76795
nn98 <jkllhgb@gmail.com>
2020-05-01 23:42:38 +0900

    6443

--- src/BaekJoon/P6443.java
+++ src/BaekJoon/P6443.java
@@ -12,15 +12,30 @@ public class P6443 {
 	static BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	static int n,i;
-	static char[]a;
-	static boolean[]p;
+	static char[]p,f=new char[1001];
+	
+	static void D(int x,int d)throws Exception{
+		if(d==n) {
+			w.write(f.toString()+"\n");
+			return;
+		}
+		for(int j=0;j<26;j++)
+			if(p[j]>0) {
+				p[j]--;
+				f[x]=(char)('a'+j);
+				D(j,x+1);
+				p[j]++;
+			}
+	}
 	
 	public static void main(String[] args)throws Exception{
 		n=Integer.parseInt(r.readLine());
 		for(;i<n;i++) {
-			a=r.readLine().toCharArray();
-			p=new boolean[26];
+			p=new char[26];
+			for(char j:r.readLine().toCharArray())p[j-'a']++;
+			D(0,0);
 		}
+		w.flush();
 	}
 
 }
