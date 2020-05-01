cfa74ccc868bf0f297fd42385fb07fe4c454f534
nn98 <jkllhgb@gmail.com>
2020-05-01 23:58:57 +0900

    1302

--- src/BaekJoon/P1302.java
+++ src/BaekJoon/P1302.java
@@ -9,7 +9,12 @@ public class P1302 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		HashMap<String,Integer>t=new HashMap();
-		
+		for(;n-->0;) {
+			String a=s.next();
+			if(t.get(a)==null)t.put(a,1);
+			else t.put(a,t.get(a));
+		}
+		System.out.println(t);
 	}
 
 }
