f1e09bea46a3d19cb6e6d036cfb6f9dff54fba04
nn98 <jkllhgb@gmail.com>
2020-05-02 00:14:23 +0900

    1302

--- src/BaekJoon/P1302_2.java
+++ src/BaekJoon/P1302_2.java
@@ -1,8 +1,18 @@
 package BaekJoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class P1302_2 {
 
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),r=0,a;
+		List<String>l=new ArrayList(),k=new ArrayList();
+		for(;n-->0;l.add(s.next()));
+		Collections.sort(l);
 		
 	}
 
