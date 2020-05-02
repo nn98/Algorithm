3c749b4dde52e54ba49571ad9aa9560753bbcaf8
nn98 <jkllhgb@gmail.com>
2020-05-03 01:00:13 +0900

    18406

--- src/BaekJoon/P18406_2.java
+++ src/BaekJoon/P18406_2.java
@@ -1,10 +1,9 @@
 package BaekJoon;
-
-public class P18406_2 {
+public class P18406_2{
 	public static void main(String[]z) {
 		char[]a=new java.util.Scanner(System.in).next().toCharArray();
 		int n=0,m=a.length-1,i=0,j=0;
 		for(;n<m;i+=a[n++]-'0',j+=a[m--]-'0');
-		System.out.println(i==j?"LUCKY":"READY");
+		System.out.print(i==j?"LUCKY":"READY");
 	}
 }
