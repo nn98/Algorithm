6c1724f71a14d693ff010fbff012008ea95b869b
nn98 <jkllhgb@gmail.com>
2020-05-03 00:54:56 +0900

    18406

--- src/BaekJoon/P18406.java
+++ src/BaekJoon/P18406.java
@@ -3,7 +3,10 @@ package BaekJoon;
 public class P18406 {
 
 	public static void main(String[] args) {
+		String a=new java.util.Scanner(System.in).next();
+		int n=a.length()/2,p=Integer.parseInt(a.substring(0,n)),q=Integer.parseInt(a.substring(n)),i,j,m=1;
 		
+		System.out.println();
 	}
 
 }
