f0db9d158bee593e9bdc7e9240f331ce9eda1b2e
nn98 <jkllhgb@gmail.com>
2020-05-02 18:29:06 +0900

    6443

--- src/BaekJoon/P6443_2.java
+++ src/BaekJoon/P6443_2.java
@@ -1,5 +1,35 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P6443_2 {
+	static int n,m,k;
+	static char[]p=new char[26],f=new char[1001],a;
+
+	static void D(int x,int d){
+		if(d==m) {
+			System.out.println(String.valueOf(f));
+			return;
+		}
+		for(int i=0;i<26;i++)
+			if(0<p[i]) {
+				p[i]--;
+				f[d]=(char)('a'+i);
+				D(i,d+1);
+				p[i]++;
+			}
+	}
+
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(;k<n;k++) {
+			a=s.next().toCharArray();
+			m=a.length;
+			for(char j:a)p[j-'a']++;
+			D(0,0);
+			for(int G=0;G<26;G++)p[G]=0;
+		}
+	}
 
 }
