92b57cf1b02c60ee4594e2640d7c070a896f294f
nn98 <jkllhgb@gmail.com>
2020-05-04 00:42:49 +0900

    15829

--- src/BaekJoon/P15829_3.java
+++ src/BaekJoon/P15829_3.java
@@ -7,6 +7,14 @@ public class P15829_3 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0;
+		long p=0,q=1;
+		for(char c:s.next().toCharArray()) {
+			p+=(c-96)*q;
+			q*=31;
+			p%=1234567891;
+			q%=1234567891;
+		}
+		System.out.print(p);
 	}
 
 }
