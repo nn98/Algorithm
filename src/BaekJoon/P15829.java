ac654f57277c2bbb905db16e26aed1109b31f70b
nn98 <jkllhgb@gmail.com>
2020-05-04 00:30:41 +0900

    15829

--- src/BaekJoon/P15829.java
+++ src/BaekJoon/P15829.java
@@ -7,11 +7,11 @@ public class P15829 {
 
 	public static void main(String[] args) {
 		 Scanner s=new Scanner(System.in);
-		 int n=s.nextInt(),i=0,m=31;
+		 long n=s.nextInt(),i=0,m=31;
 		 char[]a=s.next().toCharArray();
 		 BigInteger r=new BigInteger("0");
 		 for(;i<n;i++) {
-			 r=r.add(new BigInteger(""+(a[i]-96)*(int)Math.pow(m,i)));
+			 r=r.add(new BigInteger(""+(a[(int)i]-96)*(long)Math.pow(m,i)));
 		 }
 		 System.out.println(r.mod(new BigInteger("1234567891")));
 	}
