a6a3c99a541771951795f0ead07c3f83718dc210
nn98 <jkllhgb@gmail.com>
2020-05-04 00:32:39 +0900

    15829

--- src/BaekJoon/P15829_2.java
+++ src/BaekJoon/P15829_2.java
@@ -1,5 +1,17 @@
 package BaekJoon;
 
-public class P15829_2 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class P15829_2 {
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 long n=s.nextInt(),i=0,m=31;
+		 char[]a=s.next().toCharArray();
+		 BigInteger r=new BigInteger("0");
+		 for(;i<n;i++) {
+			 r=r.add(new BigInteger(""+(a[(int)i]-96)*(long)Math.pow(m,i)));
+		 }
+		 System.out.println(r.mod(new BigInteger("1234567891")));
+	}
 }
