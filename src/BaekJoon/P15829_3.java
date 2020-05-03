0baac7ee9c67ea344423ce8667fafbd72fc45807
nn98 <jkllhgb@gmail.com>
2020-05-04 00:40:54 +0900

    15829

--- src/BaekJoon/P15829_3.java
+++ src/BaekJoon/P15829_3.java
@@ -1,10 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P15829_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0;
 	}
 
 }
