4761f3fe6c6485e47b37989b3d55230b4ec692d6
nn98 <jkllhgb@gmail.com>
2020-05-05 00:34:00 +0900

    1021

--- src/BaekJoon/P1021_2.java
+++ src/BaekJoon/P1021_2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P1021_2 {
@@ -15,7 +16,7 @@ public class P1021_2 {
 		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken()),N=0,a[]=new int[n];
 		t=new StringTokenizer(r.readLine());
 		for(;n-->0;a[Integer.parseInt(t.nextToken())]++);
-		
+		System.out.println(Arrays.toString(a));
 	}
 
 }
