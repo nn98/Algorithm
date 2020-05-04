c76c17512c383904029e9486f279ff82a5828ea5
nn98 <jkllhgb@gmail.com>
2020-05-05 00:33:52 +0900

    1021

--- src/BaekJoon/P1021_2.java
+++ src/BaekJoon/P1021_2.java
@@ -1,10 +1,21 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class P1021_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken()),N=0,a[]=new int[n];
+		t=new StringTokenizer(r.readLine());
+		for(;n-->0;a[Integer.parseInt(t.nextToken())]++);
+		
 	}
 
 }
