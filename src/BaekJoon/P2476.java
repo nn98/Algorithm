a01160a1c961a54c442470c38e882fa6b3e44787
nn98 <jkllhgb@gmail.com>
2020-05-05 00:25:59 +0900

    2476

--- src/BaekJoon/P2476.java
+++ src/BaekJoon/P2476.java
@@ -1,10 +1,25 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2476 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a,b,c,m=0,v=0;
+		for(;n-->0;) {
+			a=s.nextInt();
+			b=s.nextInt();
+			c=s.nextInt();
+			if(a==b||b==c||a==c) {
+				if(a==b&&b==c)v=10000+a*1000;
+				if(a==b||a==c)v=1000+a*100;
+				if(b==c)v=1000+b*100;
+			}
+			else v=Math.max(a, Math.max(c, b))*100;
+			m=m>v?m:v;
+		}
+		System.out.println(m);
 	}
 
 }
