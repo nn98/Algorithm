66f8aed0626cb3421e2a272bf4c8ca710d43b0b3
nn98 <jkllhgb@gmail.com>
2020-05-06 05:23:44 +0900

    5361

--- src/BaekJoon/P5361.java
+++ src/BaekJoon/P5361.java
@@ -1,12 +1,9 @@
 package BaekJoon;
 
 import java.util.Scanner;
-
 public class P5361 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		for(int n=s.nextInt();n-->0;)System.out.println("$"+(s.nextInt()*350.34+s.nextInt()*230.90+s.nextInt()*190.55+s.nextInt()*125.30+s.nextInt()*180.90));
 	}
-
 }
