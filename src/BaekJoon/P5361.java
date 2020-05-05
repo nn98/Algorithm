08dc4adeca9a8fd152905c88f435e7bee9f2ba2c
nn98 <jkllhgb@gmail.com>
2020-05-06 05:27:34 +0900

    5361

--- src/BaekJoon/P5361.java
+++ src/BaekJoon/P5361.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class P5361 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		for(int n=s.nextInt();n-->0;)System.out.println("$"+(s.nextInt()*350.34+s.nextInt()*230.90+s.nextInt()*190.55+s.nextInt()*125.30+s.nextInt()*180.90));
+		for(int n=s.nextInt();n-->0;)System.out.println("$"+String.format("%.2f",s.nextInt()*350.34+s.nextInt()*230.90+s.nextInt()*190.55+s.nextInt()*125.30+s.nextInt()*180.90));
 	}
 }
