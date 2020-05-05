9de3febe60917f81ca41765a7cc1f3d5708b7bc1
nn98 <jkllhgb@gmail.com>
2020-05-06 05:23:37 +0900

    5361

--- src/BaekJoon/P5361.java
+++ src/BaekJoon/P5361.java
@@ -1,10 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P5361 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		for(int n=s.nextInt();n-->0;)System.out.println("$"+(s.nextInt()*350.34+s.nextInt()*230.90+s.nextInt()*190.55+s.nextInt()*125.30+s.nextInt()*180.90));
 	}
 
 }
