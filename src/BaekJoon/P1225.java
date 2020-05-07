c035e823eba0b74d140180060fc302853a158c49
nn98 <jkllhgb@gmail.com>
2020-05-07 10:40:32 +0900

    1225

--- src/BaekJoon/P1225.java
+++ src/BaekJoon/P1225.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1225 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
+		int i=0,j,r=0;
+		for(;i<a.length;i++)
+			for(j=0;j<b.length;j++)
+				r+=(a[i]-'0')*(b[j]-'0');
+		System.out.print(r);
 	}
 
 }
