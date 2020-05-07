a4f628859eba9e4a43a02bd78d1076a44c99a3fe
nn98 <jkllhgb@gmail.com>
2020-05-07 10:43:15 +0900

    1225

--- src/BaekJoon/P1225.java
+++ src/BaekJoon/P1225.java
@@ -1,5 +1,6 @@
 package BaekJoon;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class P1225 {
@@ -7,10 +8,11 @@ public class P1225 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
-		int i=0,j,r=0;
+		int i=0,j;
+		BigInteger r=BigInteger.ZERO;
 		for(;i<a.length;i++)
 			for(j=0;j<b.length;j++)
-				r+=(a[i]-'0')*(b[j]-'0');
+				r=new BigInteger(""+(a[i]-'0')*(b[j]-'0'));
 		System.out.print(r);
 	}
 
