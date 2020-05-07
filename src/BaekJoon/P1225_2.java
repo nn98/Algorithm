6d3e854c46ca131f3eb9f8e4ec1a3d8d585c1bab
nn98 <jkllhgb@gmail.com>
2020-05-07 11:33:33 +0900

    1225

--- src/BaekJoon/P1225_2.java
+++ src/BaekJoon/P1225_2.java
@@ -11,12 +11,14 @@ public class P1225_2 {
 		for(;i<2;i++)
 			for(char c:s.next().toCharArray())
 				a[i][c-'0']++;
-		for(int[]c:a)System.out.println(Arrays.toString(c));
+//		for(int[]c:a)System.out.println(Arrays.toString(c));
 //		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
 //		Arrays.sort(a);
 //		Arrays.sort(b);
 		BigInteger r=BigInteger.ZERO;
-
+		for(i=1;i<10;i++)
+			for(j=1;j<10;j++)
+				r=r.add(new BigInteger(""+i*j*a[0][i]*a[1][j]));
 //		r=new BigInteger(""+(a[i]-'0')*(b[j]-'0'));
 		System.out.print(r);
 	}
