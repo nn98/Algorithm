83fdf196950b4bba0e8be13f7d518eaef7a29f95
nn98 <jkllhgb@gmail.com>
2020-05-07 10:20:47 +0900

    2139

--- src/BaekJoon/P2139.java
+++ src/BaekJoon/P2139.java
@@ -1,9 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P2139 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int a,b,c,d[]=new int[12];
 	}
 
 }
