782fd7081dee5da3b190fb2681651f3e85b523f6
nn98 <jkllhgb@gmail.com>
2020-05-07 11:49:06 +0900

    1225

--- src/BaekJoon/P1225_3.java
+++ src/BaekJoon/P1225_3.java
@@ -1,10 +1,19 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1225_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String a=s.nextLine();
+		long b=0,m=a.indexOf(" "),c=0;
+		int n=-1;
+		for(;++n<a.length();) {
+			if(n<m)b+=a.charAt(n)-48;
+			if(n>m)c+=a.charAt(n)-48;
+		}
+		System.out.println(b*c);
 	}
 
 }
