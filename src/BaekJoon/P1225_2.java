02153ffd419cab094990aca2efdf09121e874fb2
nn98 <jkllhgb@gmail.com>
2020-05-07 10:49:09 +0900

    1225

--- src/BaekJoon/P1225_2.java
+++ src/BaekJoon/P1225_2.java
@@ -8,13 +8,16 @@ public class P1225_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=0,j=0,a[][]=new int[2][10];
-		for(char c:s.next().toCharArray())
+		for(;i<2;i++)
+			for(char c:s.next().toCharArray())
+				a[i][c-'0']++;
+		
 //		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
 //		Arrays.sort(a);
 //		Arrays.sort(b);
 		BigInteger r=BigInteger.ZERO;
 
-		r=new BigInteger(""+(a[i]-'0')*(b[j]-'0'));
+//		r=new BigInteger(""+(a[i]-'0')*(b[j]-'0'));
 		System.out.print(r);
 	}
 
