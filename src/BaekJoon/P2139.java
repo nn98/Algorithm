62097946c7ebfdabaec54d7ac0d9be71b848c622
nn98 <jkllhgb@gmail.com>
2020-05-07 10:32:21 +0900

    2139

--- src/BaekJoon/P2139.java
+++ src/BaekJoon/P2139.java
@@ -6,7 +6,22 @@ public class P2139 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a,b,c,d[]=new int[12];
+		int a,b,c,d[]=new int[13],r,i;
+		d[2]=-3;
+		d[4]=d[6]=d[9]=d[11]=-1;
+		do {
+			a=s.nextInt();
+			b=s.nextInt();
+			c=s.nextInt();
+			if(c%4==0) {
+				if(c%100==0)d[2]-=1;
+				if(c%400==0)d[2]+=1;
+				d[2]+=1;
+			}
+			r=0;
+			for(i=1;i<b;r+=31+d[i++]);
+			r+=a;
+		}while(a!=0);
 	}
 
 }
