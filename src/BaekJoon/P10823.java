739521678059d0ec7d1fedfd309735ff5f9bb5c0
nn98 <jkllhgb@gmail.com>
2020-05-08 23:06:33 +0900

    10823

--- src/BaekJoon/P10823.java
+++ src/BaekJoon/P10823.java
@@ -1,10 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10823 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		StringBuffer a=new StringBuffer();
+		long r=0;
+		while(s.hasNext())	a.append(s.next());
+		for(String b:a.toString().split(","))r+=Long.parseLong(b);
+		System.out.print(r);
 	}
 
 }
