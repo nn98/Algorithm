ee7eaaf1a594ff238962c02a3c0765347b738819
nn98 <jkllhgb@gmail.com>
2020-05-08 23:08:20 +0900

    10823

--- src/BaekJoon/P10823.java
+++ src/BaekJoon/P10823.java
@@ -1,9 +1,6 @@
 package BaekJoon;
-
 import java.util.Scanner;
-
 public class P10823 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		StringBuffer a=new StringBuffer();
@@ -12,5 +9,4 @@ public class P10823 {
 		for(String b:a.toString().split(","))r+=Long.parseLong(b);
 		System.out.print(r);
 	}
-
 }
