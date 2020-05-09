e12e7f6f08d58e97ac570549c113455832bb2a00
nn98 <jkllhgb@gmail.com>
2020-05-10 00:05:20 +0900

    2495

--- src/BaekJoon/P2495.java
+++ src/BaekJoon/P2495.java
@@ -6,9 +6,16 @@ public class P2495 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int i=3,r,c;
+		int i=3,r,c,m;
 		for(;i-->0;) {
 			char[]a=s.next().toCharArray();
+			m=r=c=1;
+			for(;c<8;) {
+				if(a[c]==a[c-1])r++;
+				else r=1;
+				m=m>r?m:r;
+			}
+			System.out.println(r);
 		}
 	}
 
