ed628961d4b474474734cffec1facde2e139ac52
nn98 <jkllhgb@gmail.com>
2020-05-11 08:51:32 +0900

    13977

--- src/_Algorithm/P13977.java
+++ src/_Algorithm/P13977.java
@@ -1,5 +1,43 @@
 package _Algorithm;
 
-public class P13977 {
+import java.util.Scanner;
 
+public class P13977 {
+	static final long P=1000000007l;
+
+	static final int S=4000000;
+
+	static long sol3(long x,long y) {
+		long r=1;
+		while(y>0) {
+			if(y%2!=0) {
+				r*=x;
+				r%=P;
+			}
+			x*=x;
+			x%=P;
+			y/=2;
+		}
+		return r;
+	}
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		//		sol 3 
+		long fac[]=new long[S+1],x,y,inv[]=new long[S+1];
+		fac[1]=1;
+		for(int i=2;i<=S;i++)
+			fac[i]=(fac[i-1]*i)%P;
+		/* 위 코드에서 오류발생 원본 코드는 for(int i=1...
+		 * fac[1]에 fac[0]*1%P 하면 당연히 다 0
+		 * 코드만든놈 뭐여 불펌자룐가
+		 */
+		//		for(int i=0;i<20;System.out.print(fac[i++]+" ")); 테스트: for(int i=2로 시작-팩토리얼 성공
+
+		inv[S]=sol3(fac[S],P-2);
+		for(int i=S-1;i>=0;i--)
+			inv[i]=(inv[i+1]*(i+1))%P;
+		//		System.out.println(Arrays.toString(inv));
+		
+	}
 }
