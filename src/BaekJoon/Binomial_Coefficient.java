cf0414a454f0ffc35436f612aa8d7d004ec742a2
nn98 <jkllhgb@gmail.com>
2020-05-10 18:19:27 +0900

    이항 계수, 조합

--- src/BaekJoon/Binomial_Coefficient.java
+++ src/BaekJoon/Binomial_Coefficient.java
@@ -2,9 +2,19 @@ package BaekJoon;
 
 public class Binomial_Coefficient {
 
+	static long sol1(long n,long r) {
+		if(n==r||r==0) return 1;
+		return sol1(n-1,r-1)+sol1(n-1,r);
+	}
+	
+	static long[][]h=new long[1001][1001];
+	
+	static long sol2(int n,int r) {
+		if(h[n][r]>0)return h[n][r];
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
