307730376a48224b1628c25d330821fb0c167452
nn98 <jkllhgb@gmail.com>
2020-05-11 07:56:12 +0900

    solve

--- src/BaekJoon/Binomial_Coefficient.java
+++ src/BaekJoon/Binomial_Coefficient.java
@@ -64,7 +64,12 @@ public class Binomial_Coefficient {
 		fac[1]=1;
 		for(int i=2;i<=S;i++)
 			fac[i]=(fac[i-1]*i)%P;
-		for(int i=0;i<20;System.out.print(fac[i++]+" "));
+		/* 위 코드에서 오류발생 원본 코드는 for(int i=1...
+		 * fac[1]에 fac[0]*1%P 하면 당연히 다 0
+		 * 코드만든놈 뭐여 불펌자룐가
+		 */
+//		for(int i=0;i<20;System.out.print(fac[i++]+" ")); 테스트: for(int i=2로 시작-팩토리얼 성공
+		
 		inv[S]=sol3(fac[S],P-2);
 		for(int i=S-1;i>=0;i--)
 			inv[i]=(inv[i+1]*(i+1))%P;
