731443003519e480c56c85af56130ab7f2f01d88
nn98 <jkllhgb@gmail.com>
2020-05-10 18:10:52 +0900

    10807

--- src/BaekJoon/P10807.java
+++ src/BaekJoon/P10807.java
@@ -1,9 +1,14 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P10807 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[201];
+		for(;n-->0;a[s.nextInt()+100]++);
+		System.out.print(a[s.nextInt()+100]);
 	}
 
 }
