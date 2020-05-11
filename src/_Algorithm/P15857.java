b92a87312ea7d305e4d405b97058c51b27087365
nn98 <jkllhgb@gmail.com>
2020-05-11 09:00:17 +0900

    15847

--- src/_Algorithm/P15857.java
+++ src/_Algorithm/P15857.java
@@ -1,9 +1,15 @@
 package _Algorithm;
 
+import java.util.Scanner;
+
 public class P15857 {
 
 	public static void main(String[] args) {
+		char r='u';
+		System.out.println(r);
+		switch(new java.util.Scanner(System.in).nextInt()) {
 		
+		}
 	}
 
 }
