45de9dfbb4983a1e9a0ec8ba779cfd9169404546
nn98 <jkllhgb@gmail.com>
2020-05-11 19:34:31 +0900

    1011

--- src/BaekJoon/P1011_2.java
+++ src/BaekJoon/P1011_2.java
@@ -1,10 +1,34 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1011_2 {
 
+	static int n,i,a,b,j;
+	
+	static void o(int f, int t){
+		i=1;
+		a=f;
+		b=t;
+		j=0;
+		if(t-f==1)System.out.println(1);
+		else {
+			for(;;) {
+				a+=i;
+				j++;
+				if(a>=b)break;
+				b-=i;
+				j++;
+				if(a>=b)break;
+				i++;
+			}
+			System.out.println(j);
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		for(n=s.nextInt();n-->0;o(s.nextInt(),s.nextInt()));
 	}
 
 }
