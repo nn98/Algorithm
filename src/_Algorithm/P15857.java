31de76cce05ae03286cc0b8e981154efc85cd4a1
nn98 <jkllhgb@gmail.com>
2020-05-11 09:02:46 +0900

    15857

--- src/_Algorithm/P15857.java
+++ src/_Algorithm/P15857.java
@@ -1,13 +1,34 @@
 package _Algorithm;
 
-import java.util.Scanner;
-
 public class P15857 {
 
 	public static void main(String[] args) {
 		char r='u';
 		switch(new java.util.Scanner(System.in).nextInt()) {
-		
+		case 1:
+			r='a';
+			break;
+		case 2:
+			r='b';
+			break;
+		case 3:
+			r='b';
+			break;
+		case 4:
+			r='c';
+			break;
+		case 5:
+			r='d';
+			break;
+		case 6:
+			r='d';
+			break;
+		case 7:
+			r='d';
+			break;
+		case 8:
+			r='c';
+			break;
 		}
 	}
 
