d7a324570c36d25cf5c03dbddd6fecceede98ea5
nn98 <jkllhgb@gmail.com>
2020-05-11 09:09:37 +0900

    16395

--- src/_Algorithm/P16395.java
+++ src/_Algorithm/P16395.java
@@ -1,9 +1,6 @@
 package _Algorithm;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class P16395 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int m=31,a[][]=new int[m][m],i=0,j;
