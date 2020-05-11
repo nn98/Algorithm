457f87fe2ced7777ecac7a19cc6bb132a4f48827
nn98 <jkllhgb@gmail.com>
2020-05-11 09:09:28 +0900

    16395

--- src/_Algorithm/P16395.java
+++ src/_Algorithm/P16395.java
@@ -6,12 +6,12 @@ public class P16395 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),m=31,a[][]=new int[m][m],i=0,j;
+		int m=31,a[][]=new int[m][m],i=0,j;
 		for(;i<m;a[i++][0]=1);
 		for(i=1;i<m;i++)
 			for(j=1;j<=i;j++)
 				a[i][j]=a[i-1][j-1]+a[i-1][j];
-		System.out.print(a[n-1][k-1]);
+		System.out.print(a[s.nextInt()-1][s.nextInt()-1]);
 	}
 
 }
