d79b7998f299f9086e72c1b6977bafdb4a13c7a6
nn98 <jkllhgb@gmail.com>
2020-05-12 13:11:11 +0900

    fibo

--- src/_Algorithm/FibonacciCal2.java
+++ src/_Algorithm/FibonacciCal2.java
@@ -1,9 +1,32 @@
 package _Algorithm;
 
+import java.util.Scanner;
+
 public class FibonacciCal2 {
 
+	static long f1(long n) {
+		if(n==0)return 0;
+		if(n==1)return 1;
+		return f1(n-1)+f1(n-2);
+	}
+	
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
 		
+//		f1: Recursive 단순 재귀 구현
+		long time=System.currentTimeMillis();
+		System.out.print("F1: "+f1(s.nextLong()));
+		System.out.println("Runtime: "+(System.currentTimeMillis()-time));
+		
+//		f2: DP
+		time=System.currentTimeMillis();
+		System.out.print("F1: "+f1(s.nextLong()));
+		System.out.println("Runtime: "+(System.currentTimeMillis()-time));
+		
+//		f3: 
+		time=System.currentTimeMillis();
+		System.out.print("F1: "+f1(s.nextLong()));
+		System.out.println("Runtime: "+(System.currentTimeMillis()-time));
 	}
 
 }
