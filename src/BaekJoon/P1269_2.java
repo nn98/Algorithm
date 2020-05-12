07afafef66840cbb64f34583638da7f2fd2b19a2
nn98 <jkllhgb@gmail.com>
2020-05-12 10:26:34 +0900

    1269

--- src/BaekJoon/P1269_2.java
+++ src/BaekJoon/P1269_2.java
@@ -1,9 +1,6 @@
 package BaekJoon;
-
 import java.util.Scanner;
-
 public class P1269_2 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),o=100000001;
