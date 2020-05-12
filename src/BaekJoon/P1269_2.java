f60da990bb846ec7e8b75465f41ae0831eaf0753
nn98 <jkllhgb@gmail.com>
2020-05-12 10:24:40 +0900

    1269

--- src/BaekJoon/P1269_2.java
+++ src/BaekJoon/P1269_2.java
@@ -1,10 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1269_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),o=100000001;
+		boolean[]a=new boolean[o],b=new boolean[o];
+		for(;n-->0;a[s.nextInt()]=true);
+		for(;m-->0;b[s.nextInt()]=true);
+		System.out.println(n);
+//		for(;o-->0;a[o])
 	}
 
 }
