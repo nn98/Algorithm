cce59ea412ca7f5ca1d20bfd331edda1b2b09ee6
nn98 <jkllhgb@gmail.com>
2020-05-12 10:18:36 +0900

    1269

--- src/BaekJoon/P1269.java
+++ src/BaekJoon/P1269.java
@@ -1,9 +1,26 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P1269 {
 
 	public static void main(String[] args) {
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,j,r=0,v=0;
+		s.nextLine();
+		String[]a=s.nextLine().split(" "),b=s.nextLine().split(" ");
+		Arrays.sort(a);
+		Arrays.sort(b);
+		for(;i<n;i++)
+			for(j=v;j<m;j++) {
+				if(a[i].equals(b[j])) {
+					r-=2;
+					v=j+1;
+				}
+				if(a[i].compareTo(b[j])<0)break;
+			}
+		System.out.print(n+m+r);
 	}
 
 }
