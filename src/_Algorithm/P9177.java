be79922e13e32c4ba4bba6c38df25a5fa4fbef5b
nn98 <jkllhgb@gmail.com>
2020-05-14 23:05:02 +0900

    9177

--- src/_Algorithm/P9177.java
+++ src/_Algorithm/P9177.java
@@ -1,10 +1,21 @@
 package _Algorithm;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class P9177 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(r.readLine()),i=0;
+		for(;i++<n;) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			System.out.println(t.nextToken());
+		}
 	}
 
 }
