0b9933308750ede760d3f1884a505e7aca209c94
nn98 <jkllhgb@gmail.com>
2020-05-14 21:54:31 +0900

    Fibo

--- src/_Algorithm/FibonacciCal2.java
+++ src/_Algorithm/FibonacciCal2.java
@@ -7,7 +7,6 @@ public class FibonacciCal2 {
 	static long time;
 	
 	static long f1(long n) {
-		time=System.currentTimeMillis();
 		if(n==0)return 0;
 		if(n==1)return 1;
 		return f1(n-1)+f1(n-2);
@@ -15,7 +14,6 @@ public class FibonacciCal2 {
 
 	static long h[];
 	static long f2(long n) {
-		time=System.currentTimeMillis();
 		h=new long[(int)n+1];
 		h[0]=0;
 		h[1]=1;
@@ -26,7 +24,6 @@ public class FibonacciCal2 {
 	
 	static long p[];
 	static long f3(long n,int d) {
-		time=System.currentTimeMillis();
 		int f0=0,f1=1,f2=1,r=0,i=2;
 		for(;i<=n;i++) {
 			f2=(f0+f1)%d;
@@ -52,7 +49,6 @@ public class FibonacciCal2 {
 	
 	static long mod=1000000007;
 	static long f4(long n) {
-		time=System.currentTimeMillis();
 		long N=n--,m[][]={{1,1},{1,0}},aM[][]={{1,0},{0,1}};
 		while(n>0) {
 			if(n%2==1)aM=mMultiply(aM,m);
@@ -78,22 +74,26 @@ public class FibonacciCal2 {
 		Scanner s=new Scanner(System.in);
 		
 //		f1: Recursive 단순 재귀 구현
-		
+
+		time=System.currentTimeMillis();
 		System.out.println("F1: "+f1(s.nextLong()));
 		System.out.println("Runtime: "+(System.currentTimeMillis()-time)+" O(2**n)");
 		
 //		f2: DP
-		
+
+		time=System.currentTimeMillis();
 		System.out.println("F2: "+f2(s.nextLong()));
 		System.out.println("Runtime: "+(System.currentTimeMillis()-time)+" O(n)");
 
 //		f3: Pisano Period 피사노 주기
-		
+
+		time=System.currentTimeMillis();
 		System.out.println("F3: "+f3(s.nextLong(),s.nextInt()));
 		System.out.println("Runtime: "+(System.currentTimeMillis()-time)+" O(n)");
 		
 //		f4: Matrix 행렬 연산
-		
+
+		time=System.currentTimeMillis();
 		System.out.println("F4: "+f4(s.nextLong()));
 		System.out.println("Runtime: "+(System.currentTimeMillis()-time)+" O(n)");
 	}
