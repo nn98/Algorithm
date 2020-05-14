b888f179a3a0e9e7a7e171ef7a15e51a25eb6b03
nn98 <jkllhgb@gmail.com>
2020-05-14 23:20:31 +0900

    9177

--- src/_Algorithm/P9177.java
+++ src/_Algorithm/P9177.java
@@ -13,7 +13,7 @@ public class P9177 {
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(r.readLine()),i=0,j,x,y,z;
 		for(;i++<n;) {
-//			StringTokenizer t=new StringTokenizer(r.readLine());
+			//			StringTokenizer t=new StringTokenizer(r.readLine());
 			x=y=z=j=0;
 			char a[][]=new char[3][],t;
 			for(String b:r.readLine().split(" "))a[j++]=b.toCharArray();
@@ -21,9 +21,16 @@ public class P9177 {
 			j=0;
 			while(c&&j<a[2].length) {
 				t=a[2][j++];
-				if(t==a[0][x])x++;
-				else if(t==a[1][y])y++;
-				else c=!c;
+				if(x==a[0].length)
+					if(t==a[1][y])y++;
+					else c=!c;
+				else if(y==a[1].length)
+					if(t==a[0][x])x++;
+					else c=!c;
+				else 
+					if(t==a[0][x])x++;
+					else if(t==a[1][y])y++;
+					else c=!c;
 			}
 			w.write("Data set "+i+(c?": yes":": no"));
 		}
