57ca7a349b014147e6e7d39e83e5df856edcf67f
nn98 <jkllhgb@gmail.com>
2020-05-16 14:20:31 +0900

    Prime

--- src/_Algorithm/PrimeNumber.java
+++ src/_Algorithm/PrimeNumber.java
@@ -3,8 +3,14 @@ package _Algorithm;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int z=10000001,i=1,j;
+		boolean[]a=new boolean[z];
+		a[i]=true;
+		for(;i<z;i++)
+			if(!a[i])
+				for(j=i*2;j<z;j+=i)
+					a[j]=true;
+		for(i=1;i<20;System.out.println(a[i]))
 	}
 
 }
