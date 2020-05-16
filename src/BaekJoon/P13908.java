2c322e6c7578ab83ecf60f15fe3c72e040e1a740
nn98 <jkllhgb@gmail.com>
2020-05-16 18:57:40 +0900

    13908

--- src/BaekJoon/P13908.java
+++ src/BaekJoon/P13908.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class P13908 {
 
-	static int n,m;
+	static int n,m,r;
 	static boolean[]a;
+	static void o(String s,int l) {
+		if(s.length()==n) {
+			boolean c=true;
+			for(int i=0;i++<9&&c;c=a[i]?s.contains(""+i):c);
+			r+=c?1:0;
+		}
+	}
 	
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
