3c924373dc03eceb32b973adec6c02c306490bfb
nn98 <jkllhgb@gmail.com>
2020-05-16 18:58:39 +0900

    13908

--- src/BaekJoon/P13908.java
+++ src/BaekJoon/P13908.java
@@ -7,11 +7,13 @@ public class P13908 {
 	static int n,m,r;
 	static boolean[]a;
 	static void o(String s,int l) {
+		System.out.println(s);
 		if(s.length()==n) {
 			boolean c=true;
 			for(int i=0;i++<9&&c;c=a[i]?s.contains(""+i):c);
 			r+=c?1:0;
 		}
+		for(int i=0;i<10;o(s+i,l+1));
 	}
 	
 	public static void main(String[] args) {
@@ -19,6 +21,7 @@ public class P13908 {
 		n=s.nextInt();
 		a=new boolean[10];
 		for(m=s.nextInt();m-->0;a[s.nextInt()]=true);
+		o("",0);
 	}
 
 }
