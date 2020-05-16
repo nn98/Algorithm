3e13ef1bf33168b5882056a448aef233e6116aac
nn98 <jkllhgb@gmail.com>
2020-05-16 18:16:21 +0900

    2023

--- src/BaekJoon/P2023_2.java
+++ src/BaekJoon/P2023_2.java
@@ -3,7 +3,7 @@ package BaekJoon;
 public class P2023_2 {
 
 	static int n=new java.util.Scanner(System.in).nextInt();
-	
+
 	static void o(String s,int l) {
 		if(l==n)
 			System.out.println(s);
@@ -13,16 +13,16 @@ public class P2023_2 {
 					o(s+i,l+1);
 		}
 	}
-	
+
 	static boolean p(int m) {
 		if(m==1)return false;
 		for(int i=(int)Math.sqrt(m);i>1;i--)
 			if(m%i==0)return false;
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
-		o("",0)
-;	}
+		o("",0);
+	}
 
 }
