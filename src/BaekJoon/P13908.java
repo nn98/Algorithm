95fd09c5fd675bca2d3c5d331b57a1d0858984d3
nn98 <jkllhgb@gmail.com>
2020-05-16 18:52:08 +0900

    13908

--- src/BaekJoon/P13908.java
+++ src/BaekJoon/P13908.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P13908 {
 
+	static int n,m;
+	boolean[]a;
+	
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new boolean[10];
+		for(m=s.nextInt();m-->0;a[s.nextInt()]=true);
 	}
 
 }
