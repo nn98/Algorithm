2dddc17f2cef5e75a24400f53817545ca3763c38
nn98 <jkllhgb@gmail.com>
2020-05-17 21:22:48 +0900

    16916

--- src/BaekJoon/P16916.java
+++ src/BaekJoon/P16916.java
@@ -1,9 +1,22 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P16916 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
+		int i=0,j,r,B=b.length,c=0;
+		for(;i<a.length&&c==0;i++)
+			if(a[i]==b[0]) {
+				r=0;
+				for(j=0;j<b.length;j++)
+					if(a[i+j]==b[j])r++;
+					else break;
+				if(r==B)c++;
+			}
+		System.out.print(c);
 	}
 
 }
