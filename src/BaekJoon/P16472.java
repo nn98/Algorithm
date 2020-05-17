5b6967889aedd0f8d6b1158487142db016f1210f
nn98 <jkllhgb@gmail.com>
2020-05-17 19:11:19 +0900

    16472

--- src/BaekJoon/P16472.java
+++ src/BaekJoon/P16472.java
@@ -1,10 +1,27 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P16472 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,c,j,m=0;
+		char[]a=s.next().toCharArray();
+		boolean[]b;
+		for(;i<a.length;) {
+			b=new boolean[26];
+			j=c=0;
+			while(c<n) {
+				if(!b[a[j]-'a']) {
+					c++;
+					b[a[j]-'a']=true;
+				}
+				j++;
+			}
+			for(j=i+1;a[j-1]==a[j++];);
+			i=j;
+		}
 	}
 
 }
