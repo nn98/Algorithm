cdfbcc6841f6c0f71c9e177458b3de4603d570bf
nn98 <jkllhgb@gmail.com>
2020-05-17 21:45:31 +0900

    16916

--- src/BaekJoon/P16916_4.java
+++ src/BaekJoon/P16916_4.java
@@ -1,5 +1,22 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P16916_4 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		char[]a=r.readLine().toCharArray(),b=r.readLine().toCharArray();
+		int i=0,j,r,A=a.length,B=b.length,c=0;
+		for(;i<A&&c==0;i++)
+			if(a[i]==b[0]) {
+				r=0;
+				for(j=0;i+j<A&&j<B;j++)
+					if(a[i+j]==b[j])r++;
+					else break;
+				if(r==B)c++;
+			}
+		System.out.print(c);
+	}
 
 }
