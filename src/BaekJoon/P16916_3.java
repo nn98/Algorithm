c6e3de32ba9683f3b9d3e79c15f59556bb696dfd
nn98 <jkllhgb@gmail.com>
2020-05-17 21:52:47 +0900

    16916

--- src/BaekJoon/P16916_3.java
+++ src/BaekJoon/P16916_3.java
@@ -1,8 +1,9 @@
 package BaekJoon;
 
+//KMP Algorithm
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class P16916_3 {
 	static int ans=0;
@@ -21,7 +22,6 @@ public class P16916_3 {
 	
 	static void KMP(String origin, String ptn) {
 		int[] pi = getPi(ptn);
-		System.out.println(Arrays.toString(pi));
 		int j=0;
 		for(int i=0;i<origin.length();i++) {
 			while(j>0 && origin.charAt(i)!=ptn.charAt(j)) {
