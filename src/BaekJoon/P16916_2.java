fcf00b00c4691740fa1473a2aeccb15517735a68
nn98 <jkllhgb@gmail.com>
2020-05-17 21:36:38 +0900

    16916

--- src/BaekJoon/P16916_2.java
+++ src/BaekJoon/P16916_2.java
@@ -9,8 +9,15 @@ public class P16916_2 {
 		Scanner s=new Scanner(System.in);
 		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
 		BigInteger n=BigInteger.ZERO,m;
-		for(int i=0;i++<b.length;n=n.add(new BigInteger(""+i*b[i-1])));
-		System.out.println(n);
+		int i=0,j,c=0;
+		for(;i++<b.length;n=n.add(new BigInteger(""+i*b[i-1])));
+		for(i=0;i<a.length-b.length&&c==0;i++) {
+			m=BigInteger.ZERO;
+			for(j=1;j<b.length;j++)
+				m=m.add(new BigInteger(""+j*a[i+j-1]));
+			if(n.equals(m))c++;
+		}
+		System.out.println(c);
 	}
 
 }
