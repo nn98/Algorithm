342b84ea5dadd746ff5b0b897065b359e0e94f53
nn98 <jkllhgb@gmail.com>
2020-05-18 00:54:09 +0900

    BackT

--- src/_Algorithm/BackTracking/P15656.java
+++ src/_Algorithm/BackTracking/P15656.java
@@ -1,5 +1,46 @@
 package _Algorithm.BackTracking;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class P15656 {
+
+	static int n,m;
+	static int[]a,f;
+	static boolean[]c;
+	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	static void o(int x)throws Exception{
+		if(x==m) {
+			for(int a:f)
+				w.write(a+" ");
+			w.newLine();
+		}else
+			for(int i=0;i++<n;)
+				if(c[i])continue;
+				else {
+					f[x]=a[i-1];
+					c[i]=true;
+					o(x+1);
+					c[i]=false;
+				}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		a=new int[n];
+		f=new int[m];
+		c=new boolean[n+1];
+		t=new StringTokenizer(r.readLine());
+		for(int i=0;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		Arrays.sort(a);
+		o(0);
+		w.flush();
+	}
 
 }
