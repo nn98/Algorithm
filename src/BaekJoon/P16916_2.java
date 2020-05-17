df7cfc3b3c874caccf4a55df01d48d76e099978a
nn98 <jkllhgb@gmail.com>
2020-05-17 21:31:35 +0900

    16916

--- src/BaekJoon/P16916_2.java
+++ src/BaekJoon/P16916_2.java
@@ -1,9 +1,15 @@
 package BaekJoon;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P16916_2 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
+		BigInteger n=BigInteger.ZERO,m;
+		for(int i=0;i++<b.length;n=n.add(new BigInteger(""+i*b[i-1])));
 	}
 
 }
