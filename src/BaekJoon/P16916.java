786b8b51db14f15d5955edde4921983ee7400846
nn98 <jkllhgb@gmail.com>
2020-05-17 21:27:22 +0900

    16916

--- src/BaekJoon/P16916.java
+++ src/BaekJoon/P16916.java
@@ -7,11 +7,11 @@ public class P16916 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		char[]a=s.next().toCharArray(),b=s.next().toCharArray();
-		int i=0,j,r,B=b.length,c=0;
-		for(;i<a.length&&c==0;i++)
+		int i=0,j,r,A=a.length,B=b.length,c=0;
+		for(;i<A&&c==0;i++)
 			if(a[i]==b[0]) {
 				r=0;
-				for(j=0;j<b.length;j++)
+				for(j=0;i+j<A&&j<B;j++)
 					if(a[i+j]==b[j])r++;
 					else break;
 				if(r==B)c++;
