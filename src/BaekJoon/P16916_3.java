f8304226b3a425971789973742ba923f30f7154f
nn98 <jkllhgb@gmail.com>
2020-05-17 21:42:54 +0900

    16916

--- src/BaekJoon/P16916_3.java
+++ src/BaekJoon/P16916_3.java
@@ -1,5 +1,46 @@
 package BaekJoon;
 
-public class P16916_3 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class P16916_3 {
+	static int ans=0;
+	static int[] getPi(String pattern) {
+		int[] pi = new int[pattern.length()];
+		int j=0;
+		for(int i=1;i<pattern.length();i++) {
+			while(j>0 && pattern.charAt(i)!=pattern.charAt(j)) {
+				j = pi[j-1];
+			}
+			if(pattern.charAt(i)==pattern.charAt(j)) 
+				pi[i] = ++j;
+		}
+		return pi;
+	}
+	
+	static void KMP(String origin, String ptn) {
+		int[] pi = getPi(ptn);
+		int j=0;
+		for(int i=0;i<origin.length();i++) {
+			while(j>0 && origin.charAt(i)!=ptn.charAt(j)) {
+				j=pi[j-1];
+			}
+			if(origin.charAt(i)==ptn.charAt(j)) {
+				if(j==ptn.length()-1) {
+					ans=1;
+					break;
+				}
+				else
+					j++;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String origin = br.readLine();
+		String pattern = br.readLine();
+		KMP(origin,pattern);
+		System.out.println(ans);
+	}
 }
