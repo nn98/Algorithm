3d7c45b8283b37923e19973ef70d6b33ac8ca4f3
nn98 <jkllhgb@gmail.com>
2020-05-18 00:31:46 +0900

    15650 BackT

--- src/_Algorithm/BackTracking/P15649.java
+++ src/_Algorithm/BackTracking/P15649.java
@@ -9,19 +9,19 @@ import java.util.StringTokenizer;
 public class P15649 {
 
 	static int n,m;
-	static char[]r;
+	static char[]f;
 	static boolean[]c;
 	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	static void o(int x)throws Exception{
 		if(x==m) {
-			for(char a:r)
+			for(char a:f)
 				w.write(a+" ");
 			w.newLine();
 		}else
 			for(int i=0;i++<n;)
 				if(c[i])continue;
 				else {
-					r[x]=(char)(i+'0');
+					f[x]=(char)(i+'0');
 					c[i]=true;
 					o(x+1);
 					c[i]=false;
@@ -32,6 +32,8 @@ public class P15649 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
+		f=new char[m];
+		c=new boolean[n+1];
 		o(0);
 		w.flush();
 	}
