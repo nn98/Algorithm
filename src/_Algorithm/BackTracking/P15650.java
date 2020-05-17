c772e2934e73e90ff320cb638590b76e182ac8b4
nn98 <jkllhgb@gmail.com>
2020-05-18 00:31:49 +0900

    15650 BackT

--- src/_Algorithm/BackTracking/P15650.java
+++ src/_Algorithm/BackTracking/P15650.java
@@ -1,5 +1,41 @@
 package _Algorithm.BackTracking;
 
-public class P15650 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P15649 {
+
+	static int n,m;
+	static char[]f;
+	static boolean[]c;
+	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	static void o(int x)throws Exception{
+		if(x==m) {
+			for(char a:f)
+				w.write(a+" ");
+			w.newLine();
+		}else
+			for(int i=0;i++<n;)
+				if(c[i])continue;
+				else {
+					f[x]=(char)(i+'0');
+					c[i]=true;
+					o(x+1);
+					c[i]=false;
+				}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		f=new char[m];
+		c=new boolean[n+1];
+		o(0);
+		w.flush();
+	}
 
 }
