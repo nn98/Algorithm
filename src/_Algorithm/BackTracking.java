eb5a635c9d1e51028255731a484db9b17012c5fa
nn98 <jkllhgb@gmail.com>
2020-05-18 00:15:03 +0900

    BackT

--- src/_Algorithm/BackTracking.java
+++ src/_Algorithm/BackTracking.java
@@ -5,12 +5,20 @@ import java.util.Scanner;
 public class BackTracking {
 
 	static int n,m,a[],b[];
-	static void PrintA() {
-		for(int i:b);
+	static char[]r;
+	static boolean[]c;
+	
+	static void o(int x,int l) {
+		if(x==m)
+			System.out.println(r);
+		else
+			for(int i=0;i<n;i++) {
+				r[x]=(char) (i+'0');
+				o(x+1,0);
+			}
 	}
+	
 	public static void main(String[] args) {
-		char[]c= {'a','b','b','b','b'};
-		System.out.println(c);
 		Scanner s=new Scanner(System.in);
 		System.out.print("count of Integer: ");
 		n=s.nextInt();
@@ -18,6 +26,7 @@ public class BackTracking {
 		System.out.print("count of Set: ");
 		m=s.nextInt();
 		b=new int[m];
-		
+		r=new char[m];
+		o(0,0);
 	}
 }
