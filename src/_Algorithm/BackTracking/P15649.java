e86091c522880e1e822e0d9e315570fde6472fa6
nn98 <jkllhgb@gmail.com>
2020-05-18 00:26:49 +0900

    15649

--- src/_Algorithm/BackTracking/P15649.java
+++ src/_Algorithm/BackTracking/P15649.java
@@ -1,10 +1,37 @@
 package _Algorithm.BackTracking;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class P15649 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m;
+	static char[]r;
+	static boolean[]c;
+	static BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	static void o(int x)throws Exception{
+		if(x==m) {
+			for(char a:r)
+				w.write(a+" ");
+			w.newLine();
+		}else
+			for(int i=0;i++<n;)
+				if(c[i])continue;
+				else {
+					r[x]=(char)(i+'0');
+					c[i]=true;
+					o(x+1);
+					c[i]=false;
+				}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
 	}
 
 }
