29b2857289d79dc730f61fe127e4d30f23d2ba4f
nn98 <jkllhgb@gmail.com>
2020-05-19 20:24:23 +0900

    8595

--- src/_Algorithm/String/P8595.java
+++ src/_Algorithm/String/P8595.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class P8595 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,f=0,t,r=0,c=0;
+		int n=s.nextInt(),i=0,f=0,c=0;
+		long r=0;
 		String a=s.next();
+		System.out.println(a.substring(0,n));
 		for(;i<n;i++) {
 			if(c>0)
 				if(a.charAt(i)>='0'&&a.charAt(i)<='9')continue;
@@ -21,6 +23,7 @@ public class P8595 {
 				}
 				
 		}
+		System.out.println(i);
 		if(c>0)r+=Integer.parseInt(a.substring(f,i+1));
 		System.out.print(r);
 	}
