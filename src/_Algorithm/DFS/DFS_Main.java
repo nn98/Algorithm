dfdfd924b21ca181a44672276913080aa984c385
nn98 <jkllhgb@gmail.com>
2020-05-21 07:46:16 +0900

    DFS

--- src/_Algorithm/DFS/DFS_Main.java
+++ src/_Algorithm/DFS/DFS_Main.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class DFS_Main {
 
 	static int n,m,i,j;
-	static boolean[][]a;
-	
-	static void o(int x) {
-		
+	static boolean a[][],h[];
+
+	static void o(int x,StringBuffer f) {
+		f.append(x+" ");
+		int y=0,c=0;
+		for(;++y<n;) {
+			if(a[x][y])
+				if(h[y])continue;
+				else {
+					c++;
+					o(y,new StringBuffer(f));
+					h[y]=true;
+				}
+		}
+		if(c==0)System.out.println(f);
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt()+1;
 		m=s.nextInt();
 		a=new boolean[n][n];
+		b=new boolean[n];
 		for(;m-->0;i=s.nextInt(),j=s.nextInt(),a[i][j]=a[j][i]=true);
 	}
 }
