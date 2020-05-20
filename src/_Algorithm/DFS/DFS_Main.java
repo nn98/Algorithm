e34fb4be3d4f94831860dfee8a8668c250ba462a
nn98 <jkllhgb@gmail.com>
2020-05-20 21:45:32 +0900

    DFS

--- src/_Algorithm/DFS/DFS_Main.java
+++ src/_Algorithm/DFS/DFS_Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class DFS_Main {
 
+	static int n,m,i;
+	static boolean[][]a;
+	
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt();
+		
 	}
 
 }
