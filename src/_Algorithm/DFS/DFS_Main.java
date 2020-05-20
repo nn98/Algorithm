a15d2023bf17c26c7250e95ae9f9eaaa75e01c91
nn98 <jkllhgb@gmail.com>
2020-05-20 21:43:05 +0900

    DFS

--- src/_Algorithm/DFS/DFS_Main.java
+++ src/_Algorithm/DFS/DFS_Main.java
@@ -1,9 +1,12 @@
 package _Algorithm.DFS;
 
+import java.util.Scanner;
+
 public class DFS_Main {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt();
 	}
 
 }
