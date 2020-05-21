6d9bd615ce2ec55110c247f696557d0bf7bdb907
nn98 <jkllhgb@gmail.com>
2020-05-21 21:54:20 +0900

    11266

--- src/_Algorithm/DFS/P11266.java
+++ src/_Algorithm/DFS/P11266.java
@@ -1,10 +1,18 @@
 package _Algorithm.DFS;
 
+import java.util.Scanner;
+
 public class P11266 {
 
+	static int n,m,r,c[],i,j;
+	static boolean[]a,b[];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()+1,m=s.nextInt();
+		a=new boolean[n];
+		b=new boolean[n][n];
+		c=new int[n];
+		for(;m-->0;i=s.nextInt(),j=s.nextInt(),b[i][j]=b[j][i]=true);
 	}
 
 }
