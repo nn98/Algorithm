e7946f133268309da1346d0d31c7ff65c429ef42
nn98 <jkllhgb@gmail.com>
2020-05-24 08:20:01 +0900

    11400

--- src/_Algorithm/DFS/P11400.java
+++ src/_Algorithm/DFS/P11400.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class P11400 {
 
-	static List z,l[];
+	static List[]l;
 	static int m,dis[],i,j;
 	static int o(int x,int p) {
 		dis[x]=++m;
@@ -20,7 +20,7 @@ public class P11400 {
 				continue;
 			}
 			v=o(n,x);
-			if(v>dis[x])z.add((x<n?x:n)+" "+(x>n?x:n));
+			if(v>dis[x])l[0].add((x<n?x:n)+" "+(x>n?x:n));
 			r=r<v?r:v;
 		}
 		return r;
@@ -29,14 +29,13 @@ public class P11400 {
 		Scanner s=new Scanner(System.in);
 		int v=s.nextInt()+1,e=s.nextInt();
 		dis=new int[v];
-		z=new ArrayList();
 		l=new ArrayList[v];
-		for(i=v;i-->1;l[i]=new ArrayList());
+		for(i=v;i-->0;l[i]=new ArrayList());
 		for(;e-->0;i=s.nextInt(),j=s.nextInt(),l[i].add(j),l[j].add(i));
 		for(i=0;++i<v;j=dis[i]==0?o(i,0):0);
-		System.out.println(z.size());
-		Collections.sort(z);
-		for(i=0;i<z.size();System.out.println(z.get(i++)));
+		System.out.println(l[0].size());
+		Collections.sort(l[0]);
+		for(i=0;i<l[0].size();System.out.println(l[0].get(i++)));
 	}
 
 }
