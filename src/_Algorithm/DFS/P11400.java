b966532d4347fbcd76ad1f18fa5034faa18101fa
nn98 <jkllhgb@gmail.com>
2020-05-24 08:07:35 +0900

    11400

--- src/_Algorithm/DFS/P11400.java
+++ src/_Algorithm/DFS/P11400.java
@@ -1,9 +1,21 @@
 package _Algorithm.DFS;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P11400 {
 
+	static List[]l,c;
+	static int num,dis[],i,j;
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int v=s.nextInt()+1,e=s.nextInt();
+		dis=new int[v];
+		l=new ArrayList[v];
+		c=new ArrayList[v];
+		for(i=v;i-->1;l[i]=new ArrayList());
+		for(;e-->0;i=s.nextInt(),j=s.nextInt(),l[i].add(j),l[j].add(i));
 	}
 
 }
