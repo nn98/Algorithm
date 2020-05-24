9b26ea445014d8020bd05455a988453ef71b2372
nn98 <jkllhgb@gmail.com>
2020-05-24 11:44:38 +0900

    11400

--- src/_Algorithm/DFS/P11400_2.java
+++ src/_Algorithm/DFS/P11400_2.java
@@ -2,29 +2,29 @@ package _Algorithm.DFS;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-import _Algorithm.DFS.P11400.Pair;
 
 public class P11400_2 {
 	static List[]l;
 	static int m,dis[],i,j;
-	static int o(int x,int p) {
-		dis[x]=++m;
-		int ret=dis[x],i=0;
-		for(;i<l[x].size();i++) {
-			int nex=(int)l[x].get(i);
-			if(nex==p)continue;
-			if(dis[nex]>0) {
-				ret=Math.min(ret,dis[nex]);
-				continue;
-			}
-			int pre=o(nex,x);
-			if(pre>dis[x])l[0].add(new Pair(x<nex?x:nex,x>nex?x:nex));
-			ret=Math.min(ret, pre);
+	static int o(int a,int p) {
+		dis[a]=++m;
+		int r=dis[a],i;
+		for(Object I:l[a]) {
+			i=(Integer)I;
+			if(i==p)continue;
+			if(dis[i]==0) {
+				int q=o(i,a);
+				if(q>dis[a])
+					if(i<a)l[0].add(new int[] {i,a});
+					else l[0].add(new int[] {a,i});
+				r=r<q?r:q;
+			} else r=r<dis[i]?r:dis[i];
 		}
-		return ret;
+		return r;
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
@@ -33,9 +33,18 @@ public class P11400_2 {
 		l=new ArrayList[v];
 		for(i=v;i-->0;l[i]=new ArrayList());
 		for(;e-->0;i=s.nextInt(),j=s.nextInt(),l[i].add(j),l[j].add(i));
-		for(i=0;++i<v;j=dis[i]==0?o(i,0):0);
+		o(1,0);
 		System.out.println(l[0].size());
-		Collections.sort(l[0]);
-		for(i=0;i<l[0].size();System.out.println(l[0].get(i++)));
+		Collections.sort(l[0],new Comparator<int[]>() {
+			@Override
+			public int compare(int[]o0,int[]o1) {
+				if(o0[0]==o1[0])return o0[1]-o1[1];
+				return o0[0]-o1[0];
+			}
+		});
+		for(Object r:l[0]) {
+			int[]R=(int[]) r;
+			System.out.println(R[0]+" "+R[1]);
+		}
 	}
 }
