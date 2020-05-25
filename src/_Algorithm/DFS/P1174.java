b3fb13382fd7c302326a4fc7904603515a68be71
nn98 <jkllhgb@gmail.com>
2020-05-25 12:26:04 +0900

    1174

--- src/_Algorithm/DFS/P1174.java
+++ src/_Algorithm/DFS/P1174.java
@@ -7,16 +7,18 @@ public class P1174 {
 
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=0,j,r=-1;
+		String t="";
 		if(n<1023) {
 			r=0;
 			Queue<String>q=new LinkedList();
-			for(;i<10;) {
-				q.add(""+i++);
+			for(;i<10;q.add(""+i++));
+			while(r<n) {
+				t=q.poll();
+				for(j=0;j<(t.charAt(t.length()-1)-'0');j++)q.add(t+j);
 				r++;
 			}
-			System.out.println(q);
-			System.out.println(r);
 		}
+		System.out.println(r<0?r:t);
 	}
 
 }
