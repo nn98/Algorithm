f0df7f5ec80b74bfbd7e3283e05eb4d392d96d7a
nn98 <jkllhgb@gmail.com>
2020-05-26 08:30:54 +0900

    Sort

--- src/_Algorithm/Sorting/Bubble.java
+++ src/_Algorithm/Sorting/Bubble.java
@@ -6,8 +6,8 @@ public class Bubble {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n];
-		for(;--n>0;a[n]=s.nextInt());
+		int n=s.nextInt(),a[]=new int[n],i=0,j;
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
