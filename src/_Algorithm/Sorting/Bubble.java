ea87d0f7edfed3c68e14986532973406df9a1a21
nn98 <jkllhgb@gmail.com>
2020-05-26 08:42:35 +0900

    Bubble adv

--- src/_Algorithm/Sorting/Bubble.java
+++ src/_Algorithm/Sorting/Bubble.java
@@ -10,8 +10,8 @@ public class Bubble {
 		int n=s.nextInt(),a[]=new int[n],i=0,j,m;
 		for(;i<n;a[i++]=s.nextInt());
 		System.out.println(Arrays.toString(a));
-		for(i=0;i<n-1;i++)
-			for(j=i+1;j<n;j++)
+		for(i=0;i<n;i++)
+			for(j=1;j<n-i;j++)
 				if(a[j-1]>a[j]) {
 					m=a[j];
 					a[j]=a[j-1];
