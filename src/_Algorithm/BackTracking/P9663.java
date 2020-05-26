067ddd531ac864dcd15de0b9e9cbfad0707fe294
nn98 <jkllhgb@gmail.com>
2020-05-26 23:09:51 +0900

    9663

--- src/_Algorithm/BackTracking/P9663.java
+++ src/_Algorithm/BackTracking/P9663.java
@@ -2,6 +2,22 @@ package _Algorithm.BackTracking;
 
 public class P9663 {
 
+	static int n=new java.util.Scanner(System.in).nextInt(),r,i,j,c;
+	static boolean[][]a=new boolean[n][n];
+	static void o(int x) {
+		
+	}
+	static void d(int x) {
+		if(x==n)r++;
+		else {
+			for(int i=1;i<=n;i++) {
+				
+			}
+		}
+	}
+	static void p(int x) {
+		
+	}
 	public static void main(String[] args) {
 		
 	}
