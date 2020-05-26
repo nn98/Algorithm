c5d9f6bbee176b9a25855c4b4b5b092427ede170
nn98 <jkllhgb@gmail.com>
2020-05-26 22:34:54 +0900

    11729

--- src/_Algorithm/Recursive/P11729.java
+++ src/_Algorithm/Recursive/P11729.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public class P11729 {
 	static List l=new ArrayList();
-	static int n=new java.util.Scanner(System.in).nextInt();
-	static void o(int f,int t) {
-		
+	static void o(int n,int f,int b,int t) {
+		if(n==1)l.add(f+" "+t);
+		else {
+			o(n-1,f,t,b);
+			l.add(f+" "+t);
+			o(n-1,b,f,t);
+		}
 	}
 	public static void main(String[] args) {
-		o(1,3);
+		o(new java.util.Scanner(System.in).nextInt(),1,2,3);
+		int i=-2,s=l.size();
+		for(;++i<s;System.out.println(i<0?s:l.get(i)));
 	}
 
 }
