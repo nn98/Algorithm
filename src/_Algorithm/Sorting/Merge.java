4826e68ead62cad2ff3531b057105d4f139c693c
nn98 <jkllhgb@gmail.com>
2020-05-26 10:18:36 +0900

    Merge

--- src/_Algorithm/Sorting/Merge.java
+++ src/_Algorithm/Sorting/Merge.java
@@ -1,16 +1,41 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Merge {
 
 	static int n,i,a[],b[];
+	static void o(int f,int t) {
+		if(f<t) {
+			int m=(f+t)/2;
+			o(f,m);
+			o(m+1,t);
+			m(f,t,m);
+		}
+	}
+	static void m(int f,int t,int m) {
+		System.out.println("\t"+Arrays.toString(a));
+		int x=f,y=m+1,z=0,w=0;
+		System.out.println(Arrays.toString(b));
+		while(!(x>m&&y>t)) {
+			if(x>m)b[z++]=a[y++];
+			else if(y>t)b[z++]=a[x++];
+			else b[z++]=a[x]<a[y]?a[x++]:a[y++];
+		}
+		System.out.println(Arrays.toString(b));
+		for(;f<=t;a[f++]=b[w++]);
+		System.out.println("\t"+Arrays.toString(a));
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n];
 		b=new int[n];
 		for(;i<n;a[i++]=s.nextInt());
+		System.out.println(Arrays.toString(a));
+		o(0,n-1);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
