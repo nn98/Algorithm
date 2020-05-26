c55c9ef5729c999566562dc24cd61427244e9652
nn98 <jkllhgb@gmail.com>
2020-05-26 22:10:19 +0900

    11729

---
+++ src/_Algorithm/Recursive/P11729.java
@@ -0,0 +1,17 @@
+package _Algorithm.Recursive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P11729 {
+	
+	static List l=new ArrayList();
+	static int n=new java.util.Scanner(System.in).nextInt();
+	static void o(int f,int t) {
+		
+	}
+	public static void main(String[] args) {
+		o(1,3);
+	}
+
+}
