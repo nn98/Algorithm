1d492507ed33320980b5901635510e3a5a49af7f
nn98 <jkllhgb@gmail.com>
2020-05-26 10:03:55 +0900

    Merge

--- src/_Algorithm/Sorting/Merge.java
+++ src/_Algorithm/Sorting/Merge.java
@@ -1,9 +1,16 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class Merge {
 
+	static int n,i,a[],b[];
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		b=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
