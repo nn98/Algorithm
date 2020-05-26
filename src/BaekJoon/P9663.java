0eb033096454c61487437c9a9094f03aae03484b
nn98 <jkllhgb@gmail.com>
2020-05-26 13:32:54 +0900

    9663

--- src/BaekJoon/P9663.java
+++ src/BaekJoon/P9663.java
@@ -3,8 +3,9 @@ package BaekJoon;
 public class P9663 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt();
+		boolean[][]a=new boolean[n][n];
+		
 	}
 
 }
