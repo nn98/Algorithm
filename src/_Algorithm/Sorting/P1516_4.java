1cee88e32bd28e1f27c2cf7d301704d9e454807d
nn98 <jkllhgb@gmail.com>
2020-05-28 03:41:48 +0900

    1516

--- src/_Algorithm/Sorting/P1516_4.java
+++ src/_Algorithm/Sorting/P1516_4.java
@@ -1,8 +1,35 @@
 package _Algorithm.Sorting;
 
+import java.io.*;
+import java.util.*;
 public class P1516_4 {
 
-	public static void main(String[] args) {
+	static int n,m,v[],c[],i,j;
+	static List<Integer>[]l;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine())+1;
+		v=new int[n];
+		c=new int[n];
+		l=new List[n];
+		for(;++i<n;l[i]=new ArrayList());
+		for(i=0;++i<n;) {
+			j=0;
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			while((m=Integer.parseInt(t.nextToken()))>=0) {
+				if(j==0)v[i]=m;
+				else {
+					l[i].add(Integer.parseInt(t.nextToken()));
+					c[i]++;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(v));
+		System.out.println(Arrays.toString(c));
+		for(i=0;++i<n;System.out.println(l[i].toString()));
+		
+		
 		
 	}
 
