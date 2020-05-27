530108aba0227a9df939bed409753e6d0bcbcd18
nn98 <jkllhgb@gmail.com>
2020-05-28 02:20:36 +0900

    1516

--- src/_Algorithm/Sorting/P1516.java
+++ src/_Algorithm/Sorting/P1516.java
@@ -1,9 +1,16 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1516 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[][]=new int[n][2],i=0,j;
+		for(;i<n;) {
+			for(j=0;j<2;a[i][j++]=s.nextInt());
+			if(++i>1)s.next();
+		}
 	}
 
 }
