2a3ed7dacd7c377108dd7e9e60513b94d36bc6cf
nn98 <jkllhgb@gmail.com>
2020-05-28 02:28:53 +0900

    Topological

--- src/_Algorithm/Sorting/Topological.java
+++ src/_Algorithm/Sorting/Topological.java
@@ -1,10 +1,25 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Topological {
 
+	static int n,i,j,v[],t[];
+	static boolean[][]a;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		v=new int[n];
+		t=new int[n];
+		a=new boolean[n][n];
+		for(;i<n;i++) {
+			v[i]=s.nextInt();
+			while((j=s.nextInt())>=0)a[i][j]=true;
+		}
+		System.out.println(Arrays.toString(v));
+		for(boolean[]b:a)
+			System.out.println(Arrays.toString(b));
 	}
 
 }
