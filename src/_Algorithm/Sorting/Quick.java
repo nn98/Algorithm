b35a1ecb3d436c5775221da17a20da027b6f610b
nn98 <jkllhgb@gmail.com>
2020-05-29 01:40:45 +0900

    Quick

--- src/_Algorithm/Sorting/Quick.java
+++ src/_Algorithm/Sorting/Quick.java
@@ -6,17 +6,35 @@ import java.util.Scanner;
 public class Quick {
 
 	static int n,a[],i,j;
-	static void p(int f,int t) {
-		
+	static void w(int f,int t) {
+		int x=a[f];
+		a[f]=a[t];
+		a[t]=x;
 	}
-	static void o(int f,int t) {
-		
+	static void p(int f,int t) {
+		int m=(f+t)/2;
+		o(f,m,(f+m)/2);
+		o(m+1,t,(m+1+t)/2);
+	}
+	static void o(int f,int t,int m) {
+		if((t-f)>2) {
+			w(t,m);
+			i=f-1;
+			j=f;
+			for(;j<t;j++) {
+				if(a[j]<a[t]) {
+					w(j,++i)
+				}
+			}
+		}
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		a=new int[n];
 		for(;i<n;a[i++]=s.nextInt());
+		System.out.println(Arrays.toString(a));
+		o(0,n-1,n/2);
 		System.out.println(Arrays.toString(a));
 	}
 
