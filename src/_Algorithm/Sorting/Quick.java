d767361bfae78d754fa5790cee0d284316eacc0d
nn98 <jkllhgb@gmail.com>
2020-05-29 01:23:05 +0900

    Quick

--- src/_Algorithm/Sorting/Quick.java
+++ src/_Algorithm/Sorting/Quick.java
@@ -1,10 +1,21 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class Quick {
 
+	static int n,a[],i,j;
+	static void p(int f,int t) {
+		
+	}
+	static void o(int f,int t) {
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
