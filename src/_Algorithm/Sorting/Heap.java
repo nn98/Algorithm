071e85edc603cedb2f9ab66d32a697d7e28b641e
nn98 <jkllhgb@gmail.com>
2020-05-31 10:47:28 +0900

    Heap

--- src/_Algorithm/Sorting/Heap.java
+++ src/_Algorithm/Sorting/Heap.java
@@ -3,11 +3,18 @@ package _Algorithm.Sorting;
 import java.util.Scanner;
 
 public class Heap {
+	
+	static int n,S=2,l,r,a[];
+	static void h(int x) {
+		l=x*2+1;
+		r=x*2+2;
+	}
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),l=3,c=4;
-		if(n>1)for(;n>l;l+=c,c*=2);
-		System.out.println(l);
+		n=s.nextInt();
+		while(S-1>n)S*=2;
+		System.out.println(S);
+		a=new int[S];
 	}
 }
