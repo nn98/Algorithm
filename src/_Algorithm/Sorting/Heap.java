9e2e37c4e504b37ac7a16f71723e41e82e26496a
nn98 <jkllhgb@gmail.com>
2020-05-31 10:29:27 +0900

    Heap

--- src/_Algorithm/Sorting/Heap.java
+++ src/_Algorithm/Sorting/Heap.java
@@ -7,8 +7,8 @@ public class Heap {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=1,c=0;
-		if(n>1)for(;n>Math.pow(2,++c)+1;);
-		System.out.println(Math.pow(2,c)+1);
+		if(n>1)for(;n>2*++c+1;);
+		System.out.println(2*c+1);
 	}
 
 }
