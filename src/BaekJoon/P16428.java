f958dc6b815f7b2eb05451f7c4a0fcaf36cff600
nn98 <jkllhgb@gmail.com>
2020-06-02 00:38:13 +0900

    16428

--- src/BaekJoon/P16428.java
+++ src/BaekJoon/P16428.java
@@ -1,10 +1,15 @@
 package BaekJoon;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P16428 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		BigInteger a=new BigInteger(s.next()),b=new BigInteger(s.next());
+		System.out.println(a.divide(b));
+		System.out.print(a.mod(b));
 	}
 
 }
