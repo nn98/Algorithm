c37fc22fb3c28fb36b59aa103c6555da41f7fb5e
nn98 <jkllhgb@gmail.com>
2020-06-02 01:31:31 +0900

    3003

--- src/BaekJoon/P3003.java
+++ src/BaekJoon/P3003.java
@@ -1,9 +1,6 @@
 package BaekJoon;
-
 import java.util.Scanner;
-
 public class P3003 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=0,a[]={1,1,2,2,2,8};
