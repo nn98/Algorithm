10381303ac3b4640aca1838e87f4e23dd674c08e
nn98 <jkllhgb@gmail.com>
2020-06-02 00:43:32 +0900

    16428

--- src/BaekJoon/P16428.java
+++ src/BaekJoon/P16428.java
@@ -8,14 +8,16 @@ public class P16428 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		BigInteger a=new BigInteger(s.next()),b=new BigInteger(s.next()),c=a.divide(b),d;
-		try {
-			d=a.mod(b);
-		} catch(Exception e) {
-			c=c.add(BigInteger.ONE);
-			System.out.print(c);
-			d=c.add(BigInteger.ONE);
-			System.out.println(a.subtract(b.multiply(c)));
-		}
+//		try {
+//			d=a.mod(b);
+//		} catch(Exception e) {
+//			c=c.add(BigInteger.ONE);
+//			System.out.print(c);
+//			d=c.add(BigInteger.ONE);
+//			System.out.println(a.subtract(b.multiply(c)));
+//		}
+		System.out.println(c);
+		System.out.println(a.remainder(b));
 	}
 
 }
