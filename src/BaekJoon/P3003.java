55658f9816cc2f09ffdbbcae836567dc005a00f0
nn98 <jkllhgb@gmail.com>
2020-06-02 01:31:20 +0900

    3003

--- src/BaekJoon/P3003.java
+++ src/BaekJoon/P3003.java
@@ -7,7 +7,7 @@ public class P3003 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int i=0,a[]={1,1,2,2,2,8};
-		for(;i<7;System.out.print(a[i++]-s.nextInt()+" "));
+		for(;i<6;System.out.print(a[i++]-s.nextInt()+" "));
 	}
 
 }
