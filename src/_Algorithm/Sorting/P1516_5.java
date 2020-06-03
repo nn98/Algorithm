a38bbac1dd1a27b599dd81c7bdfc270e2b42f806
nn98 <jkllhgb@gmail.com>
2020-06-04 01:35:21 +0900

    1516

--- src/_Algorithm/Sorting/P1516_5.java
+++ src/_Algorithm/Sorting/P1516_5.java
@@ -1,15 +1,22 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1516_5 {
 
-	static int n,t[],a[][];
+	static int n,m,i,j,t[],a[][];
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt()+1;
 		t=new int[n];
 		a=new int[n][n];
+		for(;++i<n;m=0)
+			while(m!=-1)
+				if(m==0)t[i]=m=s.nextInt();
+				else a[i][s.nextInt()]++;
+		System.out.println(Arrays.toString(t));
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
