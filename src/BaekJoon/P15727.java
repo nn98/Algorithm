a5cf6def9ebd4e56723c1c54bf23be55bfdf8ec1
nn98 <jkllhgb@gmail.com>
2020-06-03 17:36:31 +0900

    15727

--- src/BaekJoon/P15727.java
+++ src/BaekJoon/P15727.java
@@ -3,7 +3,8 @@ package BaekJoon;
 public class P15727 {
 
 	public static void main(String[] args) {
-
+		int n=new java.util.Scanner(System.in).nextInt();
+		System.out.print(n%5==0?n/5:n/5+1);
 	}
 
 }
