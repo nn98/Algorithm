a1ef0b2efb871b92e9bb9b3076939e12400659a9
nn98 <jkllhgb@gmail.com>
2020-06-03 17:50:25 +0900

    BackT Test

--- src/_Algorithm/BackTracking/Test.java
+++ src/_Algorithm/BackTracking/Test.java
@@ -2,8 +2,20 @@ package _Algorithm.BackTracking;
 
 public class Test {
 
+	static int n;
+	static char[]a;
+	static StringBuffer f=new StringBuffer();
+	static void o(int x) {
+		if(x==n)System.out.println(a);
+		else {
+			for(int i=0;++i<n;a[x]=(char)('0'+i),o(x+1));
+		}
+	}
 	public static void main(String[] args) {
-		
+		n=new java.util.Scanner(System.in).nextInt()+1;
+		a=new char[n];
+		o(0);
+		System.out.println(f);
 	}
 
 }
