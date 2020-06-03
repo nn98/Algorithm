e7e33532870ce0764eb3b089a9ff0117f3a766b7
nn98 <jkllhgb@gmail.com>
2020-06-04 01:27:41 +0900

    1516

--- src/_Algorithm/Sorting/P1516_5.java
+++ src/_Algorithm/Sorting/P1516_5.java
@@ -1,10 +1,15 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1516_5 {
 
+	static int n,t[],a[][];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt()+1;
+		t=new int[n];
+		a=new int[n][n];
 	}
 
 }
