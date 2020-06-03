ea6d5f8447727fa4c71af80f4f7bac7e19b00f98
nn98 <jkllhgb@gmail.com>
2020-06-04 01:56:05 +0900

    1516

--- src/_Algorithm/Sorting/P1516_5.java
+++ src/_Algorithm/Sorting/P1516_5.java
@@ -9,7 +9,7 @@ public class P1516_5 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt()+1,m=0,i=0,r[],t[],a[][];
+		int n=s.nextInt()+1,m=0,i=0,j,r[],t[],a[][];
 		t=new int[n];
 		r=new int[n];
 		a=new int[n][n];
@@ -19,17 +19,25 @@ public class P1516_5 {
 				else {
 					if((m=s.nextInt())==-1)break;
 					a[i][m]++;
-					r[i]++;
 				}
 		System.out.println(Arrays.toString(t));
-		System.out.println(Arrays.toString(r));
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 		Queue<Integer> q=new LinkedList();
 		m=n;
 		while(m>1) {
 			for(i=0;++i<n;)if(r[i]==0)q.add(i);
-			
+			System.out.println(q.toString());
+			for(int k:q) {
+				for(j=0;++j<n;)
+					if(a[j][k]>0) {
+						a[j][k]--;
+						r[j]=r[j]>t[k]?r[j]:t[k];
+					}
+				r[k]+=t[k];
+				m--;
+			}
 		}
+		System.out.println(Arrays.toString(r));
 	}
 
 }
