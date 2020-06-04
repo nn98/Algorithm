8b1a274075eb2e3b362b091d6c516c93d83fa3c3
nn98 <jkllhgb@gmail.com>
2020-06-05 01:57:42 +0900

    1766

--- src/_Algorithm/Sorting/P1766_2.java
+++ src/_Algorithm/Sorting/P1766_2.java
@@ -1,7 +1,8 @@
 package _Algorithm.Sorting;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class P1766_2 {
@@ -9,22 +10,22 @@ public class P1766_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt()+1,m=s.nextInt(),i=0,j;
-		List<Integer>[]a=new List[n];
-		for(;++i<n;a[i]=new ArrayList());
+		Queue<Integer>[]a=new Queue[n];
+		for(;++i<n;a[i]=new LinkedList());
 		for(;m-->0;) {
 			i=s.nextInt();
 			j=s.nextInt();
 			a[j].add(i);
 		}
-		for(List L:a)System.out.println(L);
-//		m=n;
-//		for(;m>1;) {
-//			boolean C=true,D;
-//			for(i=0;++i<n&&C;C=c[i]>0);
-//			if(!C)i--;
-////				D=true;
-////				for(j=0;++j<n&&D;D=a[i][j]==0);
-////				C=D;
+		for(Queue L:a)System.out.println(L);
+		m=n;
+		for(;m>1;) {
+			boolean C=true,D;
+			for(i=0;++i<n&&C;C=!a[i].isEmpty());
+			if(!C)i--;
+//				D=true;
+//				for(j=0;++j<n&&D;D=a[i][j]==0);
+//				C=D;
 //			System.out.print(i+" ");
 //			for(j=0;++j<n;)if(a[j][i]>0) {
 //				a[j][i]--;
@@ -32,7 +33,7 @@ public class P1766_2 {
 //			}
 //			c[i]++;
 //			m--;
-//		}
+		}
 	}
 
 }
