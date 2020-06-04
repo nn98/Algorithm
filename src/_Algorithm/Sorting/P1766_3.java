ba1476711620b7cb7d9e95d90079982615bd1e2b
nn98 <jkllhgb@gmail.com>
2020-06-05 02:22:58 +0900

    1766

--- src/_Algorithm/Sorting/P1766_3.java
+++ src/_Algorithm/Sorting/P1766_3.java
@@ -1,9 +1,26 @@
 package _Algorithm.Sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class P1766_3 {
 
+	static int n,m,i,j,d[];
 	public static void main(String[] args) {
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt()+1;
+		m=s.nextInt();
+		List<List>l=new ArrayList();
+		d=new int[n];
+		for(;i++<n;l.add(new ArrayList()));
+		for(;m-->0;) {
+			l.get(s.nextInt()).add((i=s.nextInt()));
+			d[i]++;
+		}
+		System.out.println(Arrays.toString(d));
+		for(List a:l)System.out.println(a);
 	}
 
 }
