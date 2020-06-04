179222c712a71f02826a5835447728dd6a477fb4
nn98 <jkllhgb@gmail.com>
2020-06-05 01:23:17 +0900

    1766

--- src/_Algorithm/Sorting/P1766.java
+++ src/_Algorithm/Sorting/P1766.java
@@ -1,10 +1,21 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P1766 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt()+1,m=s.nextInt(),a[][]=new int[n][n],c[]=new int[n],r[]=new int[n],i,j;
+		for(;m-->0;) {
+			i=s.nextInt();
+			j=s.nextInt();
+			a[i][j]++;
+			c[j]++;
+		}
+		System.out.println(Arrays.toString(c));
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
