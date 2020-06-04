45a622ae8ed1da1cd47076b61ca7d09cf68b32d3
nn98 <jkllhgb@gmail.com>
2020-06-05 01:57:44 +0900

    1766

--- src/_Algorithm/Sorting/P1766_2.java
+++ src/_Algorithm/Sorting/P1766_2.java
@@ -1,8 +1,7 @@
 package _Algorithm.Sorting;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class P1766_2 {
@@ -10,14 +9,14 @@ public class P1766_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt()+1,m=s.nextInt(),i=0,j;
-		Queue<Integer>[]a=new Queue[n];
-		for(;++i<n;a[i]=new LinkedList());
+		List<Integer>[]a=new List[n];
+		for(;++i<n;a[i]=new ArrayList());
 		for(;m-->0;) {
 			i=s.nextInt();
 			j=s.nextInt();
 			a[j].add(i);
 		}
-		for(Queue L:a)System.out.println(L);
+		for(List L:a)System.out.println(L);
 		m=n;
 		for(;m>1;) {
 			boolean C=true,D;
@@ -26,13 +25,10 @@ public class P1766_2 {
 //				D=true;
 //				for(j=0;++j<n&&D;D=a[i][j]==0);
 //				C=D;
-//			System.out.print(i+" ");
-//			for(j=0;++j<n;)if(a[j][i]>0) {
-//				a[j][i]--;
-//				c[j]--;
-//			}
-//			c[i]++;
-//			m--;
+			System.out.print(i+" ");
+			for(j=0;++j<n;)if(a[j].contains(i))a[j].remove((Integer)i);
+			a[i].add(-1);
+			m--;
 		}
 	}
 
