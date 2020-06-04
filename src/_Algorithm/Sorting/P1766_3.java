ac95be33c16e9cc33228b5e1edfb51cb8d972405
nn98 <jkllhgb@gmail.com>
2020-06-05 02:23:00 +0900

    1766

--- src/_Algorithm/Sorting/P1766_3.java
+++ src/_Algorithm/Sorting/P1766_3.java
@@ -3,24 +3,38 @@ package _Algorithm.Sorting;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class P1766_3 {
 
 	static int n,m,i,j,d[];
+	static List<List>l=new ArrayList();
+	static void o() {
+		PriorityQueue<Integer>q=new PriorityQueue();
+		for(i=0;++i<=n;)if(d[i]==0)q.offer(i);
+		while(!q.isEmpty()) {
+			m=q.poll();
+			for(Object t:l.get(m)) {
+				int c=(Integer)t;
+				d[c]--;
+				if(d[c]==0)q.offer(c);
+			}
+			System.out.println(m+" ");
+		}
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt()+1;
 		m=s.nextInt();
-		List<List>l=new ArrayList();
 		d=new int[n];
 		for(;i++<n;l.add(new ArrayList()));
 		for(;m-->0;) {
 			l.get(s.nextInt()).add((i=s.nextInt()));
 			d[i]++;
 		}
-		System.out.println(Arrays.toString(d));
-		for(List a:l)System.out.println(a);
+//		System.out.println(Arrays.toString(d));
+//		for(List a:l)System.out.println(a);
 	}
 
 }
