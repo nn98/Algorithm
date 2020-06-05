9041cae443eee15dca97cb8974edb1ba2ba1a008
nn98 <jkllhgb@gmail.com>
2020-06-05 13:47:54 +0900

    DFS

--- src/_Algorithm/DFS/DFS_Test.java
+++ src/_Algorithm/DFS/DFS_Test.java
@@ -1,10 +1,16 @@
 package _Algorithm.DFS;
 
+import java.util.*;
+import java.io.*;
 public class DFS_Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j;
+	static boolean[]a[],b[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		
 	}
 
 }
