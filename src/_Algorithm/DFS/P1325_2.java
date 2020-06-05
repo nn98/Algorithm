d5967dea8035f4857d27cecd29af44e906d69f98
nn98 <jkllhgb@gmail.com>
2020-06-05 13:47:51 +0900

    1325

--- src/_Algorithm/DFS/P1325_2.java
+++ src/_Algorithm/DFS/P1325_2.java
@@ -4,13 +4,12 @@ import java.io.*;
 import java.util.*;
 
 public class P1325_2 {
-	static int n,m,i,j,r;
+	static int n,m,i,j,r[];
 	static boolean[]a[],h,f;
 
 	static void o(int x,StringBuffer f) {
 		if(h[x])return;
 		h[x]=true;
-		r++;
 		f.append(x+" ");
 		int y=0,c=0;
 		for(;++y<n;) {
@@ -30,10 +29,11 @@ public class P1325_2 {
 		StringTokenizer t=new StringTokenizer(br.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
-		a=new boolean[n][n];
+		r=new int[n];
 		h=new boolean[n];
 		f=new boolean[n];
+		a=new boolean[n][n];
 		for(;m-->0;t=new StringTokenizer(br.readLine()),i=Integer.parseInt(t.nextToken()),j=Integer.parseInt(t.nextToken()),a[j][i]=true);
-		for(m=0;++m<n;r=0,o(m,new StringBuffer()),System.out.println(r),h=new boolean[n]);
+		for(m=0;++m<n;o(m,new StringBuffer()),System.out.println(r),h=new boolean[n]);
 	}
 }
