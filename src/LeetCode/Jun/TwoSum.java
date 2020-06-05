64d80436140ebda6c91ccf20a5eaa66f7dccea5d
nn98 <jkllhgb@gmail.com>
2020-06-05 19:54:49 +0900

    06/05 TwoSum

--- src/LeetCode/Jun/TwoSum.java
+++ src/LeetCode/Jun/TwoSum.java
@@ -1,5 +1,17 @@
 package LeetCode.Jun;
 
 public class TwoSum {
-
+	public int[] twoSum(int[] nums, int target) {
+        int i=0,r[]=new int[2],j,l=nums.length;
+        for(;i<l;i++){
+            for(j=i+1;j<l;j++){
+                if(nums[i]+nums[j]==target){
+                    r[0]=i;
+                    r[1]=j;
+                    return r;
+                }
+            }
+        }
+        return r;
+    }
 }
