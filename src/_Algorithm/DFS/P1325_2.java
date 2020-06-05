048de3156ae2d1fe78e22dba3fdede0d974c52c4
nn98 <jkllhgb@gmail.com>
2020-06-05 13:34:21 +0900

    1325

--- src/_Algorithm/DFS/P1325_2.java
+++ src/_Algorithm/DFS/P1325_2.java
@@ -1,5 +1,45 @@
 package _Algorithm.DFS;
 
-public class P1325_2 {
+import java.util.Scanner;
 
+public class P1325_2 {
+	static int n,m,i,j,r;
+	static boolean[]a[],h,f;
+
+	static void o(int x,StringBuffer f) {
+		if(h[x])return;
+		h[x]=true;
+		r++;
+		f.append(x+" ");
+		int y=0,c=0;
+		for(;++y<n;) {
+			if(a[x][y])
+				if(h[y])continue;
+				else {
+					c++;
+					o(y,new StringBuffer(f));
+				}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt()+1;
+		m=s.nextInt();
+		a=new boolean[n][n];
+		h=new boolean[n];
+		f=new boolean[n];
+		for(;m-->0;i=s.nextInt(),j=s.nextInt(),a[j][i]=true);
+		i=0;
+		for(m=0;++m<n;h=new boolean[n]) {
+			r=0;
+			o(m,new StringBuffer());
+			if(r>i) {
+				f=new boolean[n];
+				f[m]=true;
+				i=r;
+			}else if(r==i)f[m]=true;
+		}
+		for(i=0;++i<n;System.out.print(f[i]?i+" ":""));
+	}
 }
