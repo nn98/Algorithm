c1f11e8ba48914e7a735d0449faa36b2fdcc2ea2
nn98 <jkllhgb@gmail.com>
2020-06-05 13:53:54 +0900

    DFS

--- src/_Algorithm/DFS/DFS_Test.java
+++ src/_Algorithm/DFS/DFS_Test.java
@@ -5,12 +5,25 @@ import java.io.*;
 public class DFS_Test {
 
 	static int n,m,i,j;
-	static boolean[]a[],b[];
+	static boolean[]a[],b;
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		
+		System.out.println("Directed? (y/n): ");
+		boolean c=r.readLine().equals("n");
+		n=Integer.parseInt(t.nextToken())+1;
+		m=Integer.parseInt(t.nextToken());
+		a=new boolean[n][n];
+		b=new boolean[n];
+		for(;m-->0;) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			a[i][j]=true;
+			if(c)a[j][i]=true;
+		}
+		for(boolean[]d:a)System.out.println(Arrays.toString(d));
 	}
 
 }
