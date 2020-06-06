723a58118e403a7fac79160421177d3989e39961
nn98 <jkllhgb@gmail.com>
2020-06-06 18:26:04 +0900

    06/06 3Sum

--- src/LeetCode/Jun/ThreeSum.java
+++ src/LeetCode/Jun/ThreeSum.java
@@ -1,5 +1,19 @@
 package LeetCode.Jun;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //¿Ã∏ß∫¡ §µ§≤
 public class ThreeSum {
-	
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>>r=new ArrayList();
+		int i=0,j,k,l=nums.length;
+		for(;i<l-2;i++)
+			for(j=i+1;j<l-1;j++)
+				for(k=j+1;k<l;k++)
+					if(nums[i]+nums[j]+nums[k]==0)
+						r.add(Arrays.asList(nums[i],nums[j],nums[k]));
+		return r;
+	}
 }
