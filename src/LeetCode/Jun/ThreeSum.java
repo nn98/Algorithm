a62e49a8855ff22ca59e3d2271627b7583b4c75c
nn98 <jkllhgb@gmail.com>
2020-06-06 20:51:11 +0900

    3-2> T2: ThreeSum

--- src/LeetCode/Jun/ThreeSum.java
+++ src/LeetCode/Jun/ThreeSum.java
@@ -2,18 +2,29 @@ package LeetCode.Jun;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //이름봐 ㅅㅂ
 public class ThreeSum {
 	public static List<List<Integer>> threeSum(int[] nums) {
 		List<List<Integer>>r=new ArrayList();
-		int i=0,j,k,l=nums.length;
+//		중복제거 x
+//		int i=0,j,k,l=nums.length;
+//		for(;i<l-2;i++)
+//			for(j=i+1;j<l-1;j++)
+//				for(k=j+1;k<l;k++)
+//					if(nums[i]+nums[j]+nums[k]==0)
+//						r.add(Arrays.asList(nums[i],nums[j],nums[k]));
+		Set<Integer>s=new HashSet(Arrays.asList(nums));
+		List<Integer>L=new ArrayList(s);
+		int l=s.size(),i=0,j,k;
 		for(;i<l-2;i++)
 			for(j=i+1;j<l-1;j++)
-				for(k=j+1;k<l;k++)
-					if(nums[i]+nums[j]+nums[k]==0)
-						r.add(Arrays.asList(nums[i],nums[j],nums[k]));
+				for(k=i+2;k<l;k++)
+					if(L.get(i)+L.get(j)+L.get(k)==0)
+						r.add(Arrays.asList(L.get(i),L.get(j),L.get(k)));
 		return r;
 	}
 }
