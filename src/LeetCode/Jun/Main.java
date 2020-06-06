fff76230e5d3fe9680a121e5f7cef76c06336258
nn98 <jkllhgb@gmail.com>
2020-06-06 18:31:55 +0900

    Clipboard test

--- src/LeetCode/Jun/Main.java
+++ src/LeetCode/Jun/Main.java
@@ -1,10 +1,30 @@
 package LeetCode.Jun;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Clipboard test 1. --textArea?--
+//		Clipboard clip=Toolkit.getDefaultToolkit().getSystemClipboard();
+//		String c=textArea.getSelectedText();
+//		if(c!=null) {
+//			StringSelection con=new StringSelection(c);
+//			clip.setContents(con,null);
+//		}
 
+//		Clipboard test 2. --txtCommand?--
+//		String ctc = txtCommand.getText().toString();
+//	    StringSelection stringSelection = new StringSelection(ctc);
+//	    Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+//	    clpbrd.setContents(stringSelection, null);
+		
+		String myString = "This text will be copied into clipboard";
+		StringSelection stringSelection = new StringSelection(myString);
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(stringSelection, null);
 	}
 
 }
