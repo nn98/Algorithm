c46286c80a55ae933ffac25ba8eeb8dbefb8d884
nn98 <jkllhgb@gmail.com>
2020-06-08 01:45:01 +0900

    12846

--- src/BaekJoon/P12846_4.java
+++ src/BaekJoon/P12846_4.java
@@ -1,9 +1,17 @@
 package BaekJoon;
 
+import java.io.*;
+import java.util.*;
+
 public class P12846_4 {
 
-	public static void main(String[] args) {
-		
+	static int n,a[],i;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
 	}
 
 }
