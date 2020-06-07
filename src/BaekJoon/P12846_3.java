7fd26f363c3db72481bb069deab882afd3918c03
nn98 <jkllhgb@gmail.com>
2020-06-08 01:28:01 +0900

    12846

--- src/BaekJoon/P12846_3.java
+++ src/BaekJoon/P12846_3.java
@@ -1,9 +1,14 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P12846_3 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0;
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
