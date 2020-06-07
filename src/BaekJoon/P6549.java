0ea30772d21e909850537e5791820b5de55138b4
nn98 <jkllhgb@gmail.com>
2020-06-08 04:07:35 +0900

    6549

--- src/BaekJoon/P6549.java
+++ src/BaekJoon/P6549.java
@@ -1,10 +1,36 @@
 package BaekJoon;
+import java.io.*;
+import java.util.*;
 
-public class P6549 {
+public class P12846_4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int n,a[],i,R;
+	static void o(int f,int t) {
+//		System.out.println(f+" "+t);
+		if(t<f)return;
+		if(f==t)R=R>a[f]?R:a[f];
+		else {
+			int m=a[f],v,l=t-f+1,i=f,j=f;
+			for(;i++<t;)
+				if(a[i]<m) {
+					j=i;
+					m=a[i];
+				}
+			v=m*l;
+			R=R>v?R:v;
+				o(f,j-1);
+				o(j+1,t);
+		}
 
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		o(0,n-1);
+		System.out.print(R);
 	}
 
 }
