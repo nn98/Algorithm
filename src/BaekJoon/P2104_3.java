3a4d057fd8e9852be530e7d2e48ca0c47ae8e1ef
nn98 <jkllhgb@gmail.com>
2020-06-08 02:58:49 +0900

    2104

--- src/BaekJoon/P2104_3.java
+++ src/BaekJoon/P2104_3.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 public class P2104_3 {
 
-	static int n,a[],i,R,V[][],j,k;
+	static int n,a[],i,R,j,k,m;
 	static void o(int f,int t) {
 		if(t==f)R=R>a[f]*a[f]?R:a[f]*a[f];
 		else if(t>f) {
@@ -14,7 +14,7 @@ public class P2104_3 {
 					m=a[i];
 				}
 			}
-			v=V[f][t]*m;
+//			v=V[f][t]*m;
 			R=R>v?R:v;
 				o(f,j-1);
 				o(j+1,t);
@@ -23,12 +23,13 @@ public class P2104_3 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(r.readLine());
-		V=new int[n][n];
+		a=new int[(n+1)*n/2+n%2==0?0:n/2+1];
+		System.out.println();
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;i<n;i++) {
-			V[i][i]=a[i]=Integer.parseInt(t.nextToken());
-			if(i==0)continue;
-			for(j=i;j>=0;V[j][i]=V[j--][i-1]+a[i]);
+//			V[i][i]=a[i]=Integer.parseInt(t.nextToken());
+//			if(i==0)continue;
+//			for(j=i;j>=0;V[j][i]=V[j--][i-1]+a[i]);
 		}
 		o(0,n-1);
 		System.out.print(R);
