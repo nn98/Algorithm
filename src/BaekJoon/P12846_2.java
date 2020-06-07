22f979d1d164ad505ea63fa5301357dc9011ebf1
nn98 <jkllhgb@gmail.com>
2020-06-07 09:15:37 +0900

    12846

--- src/BaekJoon/P12846_2.java
+++ src/BaekJoon/P12846_2.java
@@ -1,10 +1,12 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P12846_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
 	}
 
 }
