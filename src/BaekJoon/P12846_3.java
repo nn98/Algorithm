d479ccd1903a389b457f034fb07496cc061de770
nn98 <jkllhgb@gmail.com>
2020-06-08 01:28:23 +0900

    12846

--- src/BaekJoon/P12846_3.java
+++ src/BaekJoon/P12846_3.java
@@ -2,6 +2,7 @@ package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P12846_3 {
 
@@ -9,6 +10,7 @@ public class P12846_3 {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0;
 		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
 	}
 
 }
