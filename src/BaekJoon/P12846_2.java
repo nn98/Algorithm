8ce6d7eeea4967706f2b9f50054f799b1ba5efdb
nn98 <jkllhgb@gmail.com>
2020-06-07 09:19:07 +0900

    12846

--- src/BaekJoon/P12846_2.java
+++ src/BaekJoon/P12846_2.java
@@ -6,7 +6,10 @@ public class P12846_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
+		int n=s.nextInt(),i=0,j,a[]=new int[n],m,t,r;
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n;i++)
+			for(j=i+1;j<n;j++);
 	}
 
 }
