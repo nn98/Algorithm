edca70327ff780fd3cebba590ad2ae0537342e00
nn98 <jkllhgb@gmail.com>
2020-06-08 01:35:35 +0900

    12846

--- src/BaekJoon/P12846_3.java
+++ src/BaekJoon/P12846_3.java
@@ -8,8 +8,17 @@ public class P12846_3 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0;
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,j,m,v,e;
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		for(i=0;i<n;i++) {
+			m=1000000;
+			e=0;
+			for(j=i;j<n;j++) {
+				m=m<a[j]?m:a[j];
+				e=Math.max(e,m*(j-i+1));
+				v=v>e?v:e;
+			}
+		}
 	}
 }
