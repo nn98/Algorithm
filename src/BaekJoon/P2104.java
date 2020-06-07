ebab6a6ff4f744c40d6bdd8fb9fae3c02a41408e
nn98 <jkllhgb@gmail.com>
2020-06-08 01:59:10 +0900

    2104

--- src/BaekJoon/P2104.java
+++ src/BaekJoon/P2104.java
@@ -1,10 +1,17 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P2104 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
 	}
 
 }
