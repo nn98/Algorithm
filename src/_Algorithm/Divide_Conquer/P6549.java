9eb30ac719457abb9ff534dc397517be6b0fc6df
nn98 <jkllhgb@gmail.com>
2020-06-08 04:17:33 +0900

    Divide_Conquer

---
+++ src/_Algorithm/Divide_Conquer/P6549.java
@@ -0,0 +1,42 @@
+package BaekJoon;
+//분할정복을 못하는건가 문제들이 개같은건가
+
+import java.io.*;
+import java.util.*;
+
+public class P6549 {
+
+	static int n,a[],i,R;
+	static void o(int f,int t) {
+		//		System.out.println(f+" "+t);
+		if(t<f)return;
+		if(f==t)R=R>a[f]?R:a[f];
+		else {
+			int m=a[f],v,l=t-f+1,i=f,j=f;
+			for(;i++<t;)
+				if(a[i]<m) {
+					j=i;
+					m=a[i];
+				}
+			v=m*l;
+			R=R>v?R:v;
+				o(f,j-1);
+				o(j+1,t);
+		}
+
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String s=r.readLine();
+		while(!s.equals("0")) {
+			StringTokenizer t=new StringTokenizer(s);
+			n=Integer.parseInt(t.nextToken());
+			a=new int[n];
+			for(i=0;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+			o(0,n-1);
+			System.out.println(R);
+			s=r.readLine();
+		}
+	}
+
+}
