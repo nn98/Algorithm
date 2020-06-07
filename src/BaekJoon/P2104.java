5e5feca80a437efa6ca0132a580c4cc859110408
nn98 <jkllhgb@gmail.com>
2020-06-08 02:06:24 +0900

    2104

--- src/BaekJoon/P2104.java
+++ src/BaekJoon/P2104.java
@@ -6,13 +6,16 @@ import java.util.StringTokenizer;
 
 public class P2104 {
 
-	static int n,a[],i,R;
+	static int n,a[],i,R,V[][],j,k;
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(r.readLine());
 		a=new int[n];
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		for(;i<n;i++) {
+			V[i][i]=a[i]=Integer.parseInt(t.nextToken());
+			for(j=i;j>0;V[j][i]=V[j][i-1]+a[i]);
+		}
 	}
 
 }
