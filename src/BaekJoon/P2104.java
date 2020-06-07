fba5394d7c16a1c6c88eae25513314002702c378
nn98 <jkllhgb@gmail.com>
2020-06-08 02:18:47 +0900

    2104

--- src/BaekJoon/P2104.java
+++ src/BaekJoon/P2104.java
@@ -1,9 +1,7 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class P2104 {
 
