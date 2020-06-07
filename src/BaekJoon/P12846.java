b96530e6b01468837eec3e97c12c626c8a0183ee
nn98 <jkllhgb@gmail.com>
2020-06-07 09:15:18 +0900

    12846

--- src/BaekJoon/P12846.java
+++ src/BaekJoon/P12846.java
@@ -1,6 +1,6 @@
 package BaekJoon;
 import java.util.*;
-public class P12846 {
+public class P12846{
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,m=1000000,t,r=0;
