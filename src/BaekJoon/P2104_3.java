70926e30d8d292e44127ba51932b61cfbe1af7f3
nn98 <jkllhgb@gmail.com>
2020-06-08 03:37:24 +0900

    2104

--- src/BaekJoon/P2104_3.java
+++ src/BaekJoon/P2104_3.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 public class P2104_3 {
 
-	static int n,a[],i,R,j,k,m,p;
+	static int n,a[],i,R,j,k,m,p,b[];
 	static void o(int f,int t) {
 		if(t==f)R=R>a[f]*a[f]?R:a[f]*a[f];
 		else if(t>f) {
@@ -24,14 +24,17 @@ public class P2104_3 {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		k=n=Integer.parseInt(r.readLine());
 		a=new int[n*(n+1)/2];
+		b=new int[n];
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;i<n;i++,k--) {
+			b[i]=m;
 			a[m]=Integer.parseInt(t.nextToken());
 			p=k;
 			for(j=m-p;j>0;a[j]=a[m]+a[j-1],j-=++p);
 			m+=k;
 		}
 		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
 		o(0,n-1);
 		System.out.print(R);
 	}
