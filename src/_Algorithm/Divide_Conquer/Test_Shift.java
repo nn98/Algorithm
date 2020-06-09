c80514e836357f9b048d19eda3444206b1fb4475
nn98 <jkllhgb@gmail.com>
2020-06-09 22:30:09 +0900

    lShift 1 == Div 2

--- src/_Algorithm/Divide_Conquer/Test_Shift.java
+++ src/_Algorithm/Divide_Conquer/Test_Shift.java
@@ -3,11 +3,11 @@ package _Algorithm.Divide_Conquer;
 public class Test_Shift {
 
 	public static void main(String[] args) {
-//		for(int i=0;i++<100;System.out.printf("%d / 2 = %d\t|%b|\t%d >> 1 == %d\n", i, i/2, i, i>>1));
-		for(int i=0;i++<100;System.out.println(i+" / 2 = "+(i/2)+"\t|"+((i/2)==(i>>1))+"|\t"+i+" >> 1 == "+(i>>1)));
+		for(int i=0;i++<100;System.out.printf("%d\t/\t2\t=\t%d\t|%b|\t%d\t>>\t1\t==\t%d\n", i, i/2, ((i/2)==(i>>1)), i, i>>1));
+//		for(int i=0;i++<100;System.out.println(i+" / 2 = "+(i/2)+"\t|"+((i/2)==(i>>1))+"|\t"+i+" >> 1 == "+(i>>1)));
 //		/2 == >>1
 //		bool 서식문자 뭐여
-		System.out.printf("%b",false);
+//		System.out.printf("%b",false); 왜 또 돼 시발
 	}
 
 }
