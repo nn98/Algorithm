3667366fe62f11eb12d7d931cdc3b1f250202369
nn98 <jkllhgb@gmail.com>
2020-06-09 22:09:21 +0900

    2104

--- src/_Algorithm/Divide_Conquer/P2104_5.java
+++ src/_Algorithm/Divide_Conquer/P2104_5.java
@@ -1,9 +1,16 @@
 package _Algorithm.Divide_Conquer;
 //https://navy-apple.com/algorithms/BOJ/2104
+
+import java.io.*;
+import java.util.*;
 public class P2104_5 {
-
-	public static void main(String[] args) {
-		
+	static int n,m,i,j,k;
+	static long[]a=new long[100001];
+	static String[]s;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i++<n;a[i]=Integer.parseInt(t.nextToken()));
 	}
-
 }
