1926a6a84cb55fcbfa2a8ec085526c2dcab83915
nn98 <jkllhgb@gmail.com>
2020-06-09 21:47:10 +0900

    2104

--- src/_Algorithm/Divide_Conquer/P2104_5.java
+++ src/_Algorithm/Divide_Conquer/P2104_5.java
@@ -1,10 +1,9 @@
 package _Algorithm.Divide_Conquer;
-
+//https://navy-apple.com/algorithms/BOJ/2104
 public class P2104_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
