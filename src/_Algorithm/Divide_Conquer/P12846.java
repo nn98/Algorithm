7fc7d56960cf6c8a90abc6a0aa8275f6c0b2cad1
nn98 <jkllhgb@gmail.com>
2020-06-09 21:46:23 +0900

    왜 지맘대로 위치가 변경;

---
+++ src/_Algorithm/Divide_Conquer/P12846.java
@@ -0,0 +1,10 @@
+package _Algorithm.Divide_Conquer;
+import java.util.*;
+public class P12846{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,m=1000000,t,r=0;
+		for(;++i<=n;t=s.nextInt(),m=m<t?m:t,r=r>m*i?r:m*i);
+		System.out.print(r);
+	}
+}
