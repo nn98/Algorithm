f7c6407793f6f97be5b30b6aa87aec3ff3754dd3
nn98 <jkllhgb@gmail.com>
2020-06-09 22:20:39 +0900

    shift

--- src/_Algorithm/Divide_Conquer/Test_Shift.java
+++ src/_Algorithm/Divide_Conquer/Test_Shift.java
@@ -3,8 +3,7 @@ package _Algorithm.Divide_Conquer;
 public class Test_Shift {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i++<100;System.out.printf("%d / 2 = %d | %d >> 1 == %d", i, i/2, i, i>>1));
 	}
 
 }
