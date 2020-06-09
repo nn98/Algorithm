8f4cc4a383b36fa85dfe22f29ca06de409de0af2
nn98 <jkllhgb@gmail.com>
2020-06-09 22:30:04 +0900

    lShift 1 == Div 2

--- src/_Algorithm/Divide_Conquer/P2104_6.java
+++ src/_Algorithm/Divide_Conquer/P2104_6.java
@@ -6,21 +6,21 @@ public class P2104_6{
 	static int n,i;
 	static long[]a=new long[100001];
 	static long o(int f,int t) {
-		if(f==t)return a[f]*a[f];							//
-		int m=(f+t)>>1,l=m,r=m;								//
-		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;	//
+		if(f==t)return a[f]*a[f];							// ÇÑÄ­Àº Á¦°ö ¸®ÅÏ 			+
+		int m=(f+t)>>1,l=m,r=m;								// ÀÎµ¦½ºÀÇ ÇÕÀ» 1>? µ¿ÀÏ º¯¼ö	-
+		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;	// A ¾ÕµÞºÎºÐ Àç±Í Å«°ª
 		System.out.println(f+" "+t+" "+m);					//
 		while(r-l<t-f) {									//
 			p=l>f?(a[l-1]+s)*Math.min(n,a[l-1]):-1;			//
 			q=r<t?(a[r+1]+s)*Math.min(n,a[r+1]):-1;			//
-			if(p>q) {
-				s+=a[l-1];
-				n=Math.min(n,a[l---1]);
-			}else {
-				s+=a[r+1];
-				n=Math.min(n, a[r+++1]);
-			}
-			A=Math.max(A, s*n);
+			if(p>q) {										//
+				s+=a[l-1];									//
+				n=Math.min(n,a[l---1]);						//
+			}else {											//
+				s+=a[r+1];									//
+				n=Math.min(n, a[r+++1]);					//
+			}								
+			A=Math.max(A, s*n);								//
 		}
 		return A;
 	}
