b8af3b6074f97fc65cc186ed082464e661291ae0
nn98 <jkllhgb@gmail.com>
2020-06-10 05:17:53 +0900

    2104

--- src/_Algorithm/Divide_Conquer/P2104_6.java
+++ src/_Algorithm/Divide_Conquer/P2104_6.java
@@ -8,10 +8,15 @@ public class P2104_6{
 	static long o(int f,int t) {
 		if(f==t)return a[f]*a[f];							// 한칸은 제곱 리턴 			+
 		int m=(f+t)>>1,l=m,r=m;								// 인덱스의 합을 1>? 동일 변수	-
-		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;	// A 앞뒷부분 재귀 큰값
-		System.out.println(f+" "+t+" "+m);					//
-		while(r-l<t-f) {									//
-			p=l>f?(a[l-1]+s)*Math.min(n,a[l-1]):-1;			//
+//		/2를 왜 저렇게쓰냐;
+//		중간 인덱스, 인덱스 각각 변수에 복사
+		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;	// A=앞뒷부분 재귀 큰값 n=중간인덱스 값, 값 복사
+//		양사이드 재귀중 큰부분, 중간 인덱스값, 동일값, 이래에 사용될 값
+//		System.out.println(f+" "+t+" "+m);					// 나누기였구연
+		while(r-l<t-f) {									
+//			중간 인덱스 복사했던 값 r,l 길이가 초기 길이보다 작을 동안
+			p=l>f?(a[l-1]+s)*Math.min(n,a[l-1]):-1;			
+//			p=현재 앞이 초기 앞보다 크면 
 			q=r<t?(a[r+1]+s)*Math.min(n,a[r+1]):-1;			//
 			if(p>q) {										//
 				s+=a[l-1];									//
