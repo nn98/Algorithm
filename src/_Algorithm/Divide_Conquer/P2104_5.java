773249937a06b8f9c3cd17cdf58042369c7829e3
nn98 <jkllhgb@gmail.com>
2020-06-09 22:14:40 +0900

    2104

--- src/_Algorithm/Divide_Conquer/P2104_5.java
+++ src/_Algorithm/Divide_Conquer/P2104_5.java
@@ -1,18 +1,19 @@
 package _Algorithm.Divide_Conquer;
 //https://navy-apple.com/algorithms/BOJ/2104
+
 import java.io.*;
 import java.util.*;
 public class P2104_5 {
 	static int n,i;
 	static long[]a=new long[100001];
 	static long o(int f,int t) {
-		if(f==t)return a[f]*a[f];							//
-		int m=(f+t)>>1,l=m,r=m;								//
-		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;	//
-		System.out.println(f+" "+t+" "+m);					//
-		while(r-l<t-f) {									//
-			p=l>f?(a[l-1]+s)*Math.min(n,a[l-1]):-1;			//
-			q=r<t?(a[r+1]+s)*Math.min(n,a[r+1]):-1;			//
+		if(f==t)return a[f]*a[f];
+		int m=(f+t)>>1,l=m,r=m;
+		long A=Math.max(o(f,m),o(m+1,t)),n=a[m],s=n,p,q;
+		System.out.println(f+" "+t+" "+m);
+		while(r-l<t-f) {
+			p=l>f?(a[l-1]+s)*Math.min(n,a[l-1]):-1;
+			q=r<t?(a[r+1]+s)*Math.min(n,a[r+1]):-1;
 			if(p>q) {
 				s+=a[l-1];
 				n=Math.min(n,a[l---1]);
