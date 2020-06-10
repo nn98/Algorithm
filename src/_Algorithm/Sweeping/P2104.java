e7be4eb2b2c09825b3cf3da33063177d735f472c
nn98 <jkllhgb@gmail.com>
2020-06-11 01:55:41 +0900

    2104

--- src/_Algorithm/Sweeping/P2104.java
+++ src/_Algorithm/Sweeping/P2104.java
@@ -1,9 +1,39 @@
 package _Algorithm.Sweeping;
 
+import java.io.*;
+import java.util.*;
 public class P2104 {
 
-	public static void main(String[] args) {
-		
+	static int n,m,i,j,k,a[];
+	static long R,w,e;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		Stack<Integer>t=new Stack();
+		for(String s:r.readLine().split(" ")) {
+			j=a[i]=Integer.parseInt(s);
+			w=0;
+			m=Integer.MAX_VALUE;
+			while(!t.isEmpty()&&a[t.peek()]>j) {
+				k=t.pop();
+				m=m<k?m:k;
+				w+=k;
+				e=w*m;
+				R=R>e?R:e;
+			}
+			t.push(i++);
+		}
+		w=0;
+		m=Integer.MAX_VALUE;
+		while(!t.isEmpty()&&a[t.peek()]>j) {
+			k=t.pop();
+			m=m<k?m:k;
+			w+=k;
+			e=w*m;
+			R=R>e?R:e;
+		}
+		System.out.print(R);
 	}
 
 }
