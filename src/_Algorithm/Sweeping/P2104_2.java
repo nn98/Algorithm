488c512b3285cec9130c3c00b93f0bc6432ba536
nn98 <jkllhgb@gmail.com>
2020-06-11 02:13:31 +0900

    2104

--- src/_Algorithm/Sweeping/P2104_2.java
+++ src/_Algorithm/Sweeping/P2104_2.java
@@ -5,18 +5,17 @@ import java.io.*;
 import java.util.*;
 public class P2104_2 {
 
-	static int n,m,i,k,v,a[];
+	static int n,m,i=1,k,v,a[];
 	static long R,w,e,u[];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		n=Integer.parseInt(r.readLine());
+		n=Integer.parseInt(r.readLine())+1;
 		a=new int[n];
 		u=new long[n];
 		Stack<Integer>t=new Stack();
 		for(String s:r.readLine().split(" ")) {
 			a[i]=Integer.parseInt(s);
-			if(i>0)u[i]=u[i-1]+a[i];
-			else u[i]=a[i];
+			u[i]=u[i-1]+a[i];
 			while(!t.isEmpty()&&a[t.peek()]>a[i]) {
 				k=t.pop();
 				v=a[k];
