699c8487dee14e848afbc1806f8373fc6ceeb169
nn98 <jkllhgb@gmail.com>
2020-06-12 01:52:01 +0900

    2104

--- src/_Algorithm/Sweeping/P2104_6.java
+++ src/_Algorithm/Sweeping/P2104_6.java
@@ -7,14 +7,25 @@ public class P2104_6 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt()+1,a[],b[],m,i=0,j;
+		int n=s.nextInt()+1,a[],b[],m,i=0,j=0;
 		long u[],v,w=0;
 		a=new int[n+1];
 		b=new int[n];
 		u=new long[n];
-		for(;++i<n;b[i]=b[i-1]+(a[i]=s.nextInt()));
-		System.out.println(Arrays.toString(a));
-		System.out.println(Arrays.toString(b));
+		for(;++i<n;u[i]=u[i-1]+(a[i]=s.nextInt()));
+		a[n]=0;
+		for(i=0;++i<=n;) {
+			while(a[b[j]]>=a[i]) {
+				m=a[b[j--]];
+				if(j<0)break;
+				v=m*(u[i-1]-u[b[j]]);
+				w=w>v?w:v;
+			}
+			b[++j]=i;
+		}
+		System.out.print(w);
+//		System.out.println(Arrays.toString(a));
+//		System.out.println(Arrays.toString(b));
 	}
 
 }
