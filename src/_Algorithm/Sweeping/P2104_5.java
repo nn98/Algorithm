2309df9426d389979ae3fb48bde70c012d5bd43d
nn98 <jkllhgb@gmail.com>
2020-06-12 01:35:56 +0900

    2104 WTF

--- src/_Algorithm/Sweeping/P2104_5.java
+++ src/_Algorithm/Sweeping/P2104_5.java
@@ -7,22 +7,23 @@ public class P2104_5 {
 	static long R,w,e,h,u[];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		n=Integer.parseInt(r.readLine())+1;	//걍 n에 1 더애허 해도 ㅇㅋ
+		n=Integer.parseInt(r.readLine())+1;	// 걍 n에 1 더애허 해도 ㅇㅋ
 		a=new int[n+1];
 		u=new long[n];
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;++i<n;) {
 			a[i]=Integer.parseInt(t.nextToken());
-			u[i]=u[i-1]+a[i];
+			u[i]=u[i-1]+a[i];				// 입력받으면서 합 배열에 합 저장
 		}
-		a[n]=0;
-		s=new int[n];
-		for(i=1;i<=n;i++) {
-			while(a[s[m]]>=a[i]) {
-				int h=a[s[m--]];
-//				System.out.println(i+" "+m+" "+w);
-				if(m<0)break;
-				long v=h*(u[i-1]-u[s[m]]);
+		a[n]=0;								// 값 배열 마지막 칸(n+1)에 0
+		s=new int[n];						// 스택 대용 인덱스 배열?
+		for(i=1;i<=n;i++) {					// 1부터 마지막 칸(n+1)까지
+			System.out.println(Arrays.toString(a)+"\n"+Arrays.toString(u)+"\n"+Arrays.toString(s)+"\n"+w);
+			while(a[s[m]]>=a[i]) {			// 현재 [인덱스 배열 [인덱스]]의 값이 현재 [i]값보다 크거나 같을 동안
+				int h=a[s[m--]];			// h=현재 [인덱스 배열 [인덱스]]의 값
+				if(m<0)break;				// 인덱스가 음수면 아웃
+				long v=h*(u[i-1]-u[s[m]]);	// 결과값은 h(현재 [인덱스 배열 [인덱스]]의 값)*
+				System.out.printf("\t%d * ( %d - %d ) = %d\n",h,u[i-1],u[s[m]],v);
 				if(w<v)w=v;
 			}
 			s[++m]=i;
