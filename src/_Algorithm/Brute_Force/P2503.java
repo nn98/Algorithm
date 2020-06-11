b465ccb52131c734803d995f4c1a2175139123be
nn98 <jkllhgb@gmail.com>
2020-06-12 02:46:01 +0900

    2503

--- src/_Algorithm/Brute_Force/P2503.java
+++ src/_Algorithm/Brute_Force/P2503.java
@@ -1,21 +1,24 @@
 package _Algorithm.Brute_Force;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2503 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,S,B,m,a[]=new int[3],b[][]=new int[3][10]; 
-		for(;i++<n;) {
+		int n=s.nextInt(),i,S,B,m,a[]=new int[3],b[][]=new int[3][10],c[][]=new int[3][10],r[]=new int[10]; 
+		for(;n-->0;) {
 			m=s.nextInt();
-			b=m/100;
-			c=m%100/10;
-			d=m%10;
+			a[0]=m/100;
+			a[1]=m%100/10;
+			a[2]=m%10;
 			S=s.nextInt();
-			if(S==0)a[0][b]=
 			B=s.nextInt();
+			if(S==0&&B==0)
+				for(i=0;i<3;r[a[i++]]=-1);
 		}
+		System.out.println(Arrays.toString(r));
 	}
 
 }
