fc0c592b3487ae4629060076a0fddfeb14b48374
nn98 <jkllhgb@gmail.com>
2020-06-12 02:36:26 +0900

    2503

--- src/_Algorithm/Brute_Force/P2503.java
+++ src/_Algorithm/Brute_Force/P2503.java
@@ -1,10 +1,21 @@
 package _Algorithm.Brute_Force;
 
+import java.util.Scanner;
+
 public class P2503 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,S,B,m,a[]=new int[3],b[][]=new int[3][10]; 
+		for(;i++<n;) {
+			m=s.nextInt();
+			b=m/100;
+			c=m%100/10;
+			d=m%10;
+			S=s.nextInt();
+			if(S==0)a[0][b]=
+			B=s.nextInt();
+		}
 	}
 
 }
