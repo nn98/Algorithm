518159fdd23a2945c6669aa0726d7a96ecc09d4c
nn98 <jkllhgb@gmail.com>
2020-06-12 21:44:41 +0900

    1021

--- src/BaekJoon/P1021_3.java
+++ src/BaekJoon/P1021_3.java
@@ -1,10 +1,38 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1021_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m],i=0,x=0,j,I,J,k=0,r=0;
+		boolean[]b=new boolean[n+1];
+		b[0]=true;
+		for(;i<m;a[i++]=s.nextInt());
+		for(;m>0;) {
+			i=j=x;
+			I=J=0;
+			for(;;i++,j--,I++,J++) {
+				if(a[k]==i) {
+					r+=I;
+					m--;
+					b[i]=true;
+					x=i;
+					break;
+				}
+				if(a[k]==j) {
+					r+=J;
+					m--;
+					b[j]=true;
+					x=j;
+					break;
+				}
+				if(b[i])I--;
+				if(b[j])J--;
+			}
+		}
+		System.out.print(r);
 	}
 
 }
