5452ca2ff4adc26a248eb889f35fea1772a89cc3
nn98 <jkllhgb@gmail.com>
2020-06-12 22:20:18 +0900

    1021

--- src/BaekJoon/P1021_3.java
+++ src/BaekJoon/P1021_3.java
@@ -6,34 +6,34 @@ public class P1021_3 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m],i=0,x=0,j,I,J,k=0,r=0;
+		int n=s.nextInt(),m=s.nextInt(),a[]=new int[m],i=0,x=1,j,I,J,k=0,r=0;
 		boolean[]b=new boolean[n+1];
 		b[0]=true;
 		for(;i<m;a[i++]=s.nextInt());
 		for(;m>0;) {
-			if(x==n)x=0;
-			i=j=++x;
+			x=x>n?1:x;
+			i=j=x;
 			I=J=0;
 			for(;;i++,j--,I++,J++) {
 				i=i>n?0:i;
 				j=j<0?n:j;
 				while(b[i]) {
-					I--;
+//					I--;
 					i++;
 					i=i>n?0:i;
 				}
 				while(b[j]) {
-					J--;
+//					J--;
 					j--;
 					j=j<0?n:j;
 				}
-				System.out.printf("base:%d forw:%d back:%d\n",x,i,j);
+				System.out.printf("base:%d forw:%d(+%d) back:%d(-%d)\n",x,i,I,j,J);
 				if(a[k]==i) {
 					System.out.println("i(forw): "+a[k]+" "+i+" "+I);
 					r+=I;
 					m--;
 					b[i]=true;
-					x=i;
+					x=i+1;
 					k++;
 					break;
 				}
@@ -42,7 +42,7 @@ public class P1021_3 {
 					r+=J;
 					m--;
 					b[j]=true;
-					x=j;
+					x=j+1;
 					k++;
 					break;
 				}
