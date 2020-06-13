0fac0401c341c13685b2c26d91b3476ddc8a71b0
nn98 <jkllhgb@gmail.com>
2020-06-14 07:48:20 +0900

    2042

--- src/_Algorithm/Divide_Conquer/P2042_2.java
+++ src/_Algorithm/Divide_Conquer/P2042_2.java
@@ -1,10 +1,16 @@
 package _Algorithm.Divide_Conquer;
 
+import java.util.Scanner;
+
 public class P2042_2 {
 
+	static int n,m,i,x,d,a[]=new int[n];
+	static long[]t; 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt()+s.nextInt();
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
