82f988e0794032e8f257f1f2060917923dfd33fa
nn98 <jkllhgb@gmail.com>
2020-06-14 08:59:44 +0900

    10868

--- src/_Algorithm/Divide_Conquer/P10868.java
+++ src/_Algorithm/Divide_Conquer/P10868.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 
 public class P10868 {
 
-	static int n,m,i,a[];
-	static long[]t;
-	static long o(int n,int s,int e) {
+	static int n,m,i,j,a[],t[];
+	static int o(int n,int s,int e) {
 		if(s==e)return t[n]=a[s];
-		int m=(s+e)/2;
-		return t[n]=o(n*2,s,m)+o(n*2+1,m+1,e);
+		int m=(s+e)/2,i=o(n*2,s,m),j=o(n*2+1,m+1,e);
+		return t[n]=i<j?i:j;
+	}
+	static int p(int n,int s,int e) {
+		if(i>e|j<s)return Integer.MAX_VALUE;
+		return 0;
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
@@ -19,9 +22,12 @@ public class P10868 {
 		for(;i<n;a[i++]=s.nextInt());
 		for(i=1;i<n;i*=2);
 		i*=2;
-		t=new long[i];
+		t=new int[i];
 		o(1,0,--n);
-		System.out.println(Arrays.toString(t));
+		for(;m-->0;) {
+			i=s.nextInt();
+			j=s.nextInt();
+		}
 	}
 
 }
