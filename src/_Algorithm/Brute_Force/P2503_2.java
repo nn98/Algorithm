bb33563bb509406877d17fa4fed0479cb52b7c22
nn98 <jkllhgb@gmail.com>
2020-06-14 03:45:03 +0900

    2503

--- src/_Algorithm/Brute_Force/P2503_2.java
+++ src/_Algorithm/Brute_Force/P2503_2.java
@@ -3,14 +3,12 @@ package _Algorithm.Brute_Force;
 import java.util.Scanner;
 
 public class P2503_2 {
-	static int n,i,j;
-	static char[]a=new char[3];
+	static int n,i,j,d[][];
+	static char[]a=new char[3],b[];
 	static boolean[]c=new boolean[10];
 	static void o(int x) {
 		if(x==3) {
 			System.out.println(a);
-			
-			System.out.println(Integer.parseInt(a));
 		}
 		else for(int k=0;++k<10;)
 			if(!c[k]) {
@@ -24,5 +22,8 @@ public class P2503_2 {
 		Scanner s=new Scanner(System.in);
 		o(0);
 		n=s.nextInt();
+		b=new char[n][];
+		d=new int[n][2];
+		for(;n-->0;b[n]=s.next().toCharArray(),d[n][0]=s.nextInt(),d[n][0]=s.nextInt());
 	}
 }
