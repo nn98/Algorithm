653f7c250fd4cb86b4533f4c13ca6061c99c0fc3
nn98 <jkllhgb@gmail.com>
2020-06-14 08:28:31 +0900

    2042

--- src/_Algorithm/Divide_Conquer/P2042_2.java
+++ src/_Algorithm/Divide_Conquer/P2042_2.java
@@ -1,5 +1,6 @@
 package _Algorithm.Divide_Conquer;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2042_2 {
@@ -7,7 +8,6 @@ public class P2042_2 {
 	static int n,m,i,x;
 	static long d,a[],t[];
 	static long o(int n,int s,int e) {
-		System.out.println(n+" "+s+" "+e);
 		if(s==e)return t[n]=a[s];
 		int m=(s+e)/2;
 		return t[n]=o(n*2,s,m)+o(n*2+1,m+1,e);
@@ -23,7 +23,7 @@ public class P2042_2 {
 	}
 	static long u(int n,int s,int e,int l,int r) {
 		if(l>e||r<s)return 0;
-		if(l<=s&&r>=e)return t[n];
+		if(l<=s&&e<=r)return t[n];
 		int m=(s+e)/2;
 		return u(n*2,s,m,l,r)+u(n*2+1,m+1,e,l,r);
 	}
@@ -37,11 +37,13 @@ public class P2042_2 {
 		i*=2;
 		t=new long[i];
 		o(1,0,--n);
+		System.out.println(Arrays.toString(t));
 		for(;m-->0;) {
 			if(s.nextInt()==1) {
 				x=s.nextInt()-1;
-				d=a[x]-s.nextInt();
+				d=s.nextInt()-a[x];
 				p(1,0,n,x,d);
+				System.out.println(Arrays.toString(t));
 			}else
 				System.out.println(u(1,0,n,s.nextInt(),s.nextInt()));
 		}
