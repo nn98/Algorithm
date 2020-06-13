6f0590c56591b6338467903549ded29214c78c03
nn98 <jkllhgb@gmail.com>
2020-06-14 08:39:22 +0900

    2042 ???

--- src/_Algorithm/Divide_Conquer/P2042_3.java
+++ src/_Algorithm/Divide_Conquer/P2042_3.java
@@ -1,5 +1,26 @@
 package _Algorithm.Divide_Conquer;
 
-public class P2042_3 {
+import java.util.Scanner;
 
+//된다고 이게? 시벌?
+public class P2042_3 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k,a[]=new int[n],i=0,b,c;s.next();k=s.nextInt();
+		for(;i<n;a[i++]=s.nextInt());
+		for(;k-->0;) {
+			long r=0;
+			while(true) {
+				i=s.nextInt();
+				b=s.nextInt();
+				c=s.nextInt();
+				a[b-1]=i==1?c:a[b-1];
+				if(i==2) {
+					for(i=b-1;i<c;r+=a[i++]);
+					break;
+				}
+			}
+			System.out.println(r);
+		}
+	}
 }
