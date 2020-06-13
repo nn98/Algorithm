3246fbafaa2d6ed7198c3e8b1cfb5bdf51a284a5
nn98 <jkllhgb@gmail.com>
2020-06-14 04:48:02 +0900

    Seg T

--- src/_Algorithm/Divide_Conquer/Segment_Tree.java
+++ src/_Algorithm/Divide_Conquer/Segment_Tree.java
@@ -2,8 +2,12 @@ package _Algorithm.Divide_Conquer;
 
 public class Segment_Tree {
 
+	static int n=6,m,i,j,a[]={3,1,6,4,5,2},t[];
+	static void o(int n,int s,int e) {
+		if(s==e)return;
+	}
 	public static void main(String[] args) {
-		
+		t=new int[(int)Math.pow(2,n+1)-1];
 	}
 
 }
