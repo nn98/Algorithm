0f69f460ed183c2976d2b812d3c3dfe3ffa9955b
nn98 <jkllhgb@gmail.com>
2020-06-14 02:44:10 +0900

    2503 DFS

--- src/_Algorithm/Brute_Force/P2503_2.java
+++ src/_Algorithm/Brute_Force/P2503_2.java
@@ -1,5 +1,24 @@
 package _Algorithm.Brute_Force;
 
-public class P2503_2 {
+import java.util.Scanner;
 
+public class P2503_2 {
+	static int n,i,j;
+	static char[]a=new char[3];
+	static boolean[]c=new boolean[10];
+	static void o(int x) {
+		if(x==3)System.out.println(a);
+		else for(int k=0;++k<10;)
+			if(!c[k]) {
+				a[x]=(char)(k+'0');
+				c[k]=true;
+				o(x+1);
+				c[k]=false;
+			}
+	}
+	public static void main(String[]z) {
+		Scanner s=new Scanner(System.in);
+		o(0);
+		n=s.nextInt();
+	}
 }
