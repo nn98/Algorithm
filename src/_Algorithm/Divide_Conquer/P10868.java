6b0cb313d12a88a2ed83fd19fe1b82624647d6ca
nn98 <jkllhgb@gmail.com>
2020-06-14 08:48:27 +0900

    10868

--- src/_Algorithm/Divide_Conquer/P10868.java
+++ src/_Algorithm/Divide_Conquer/P10868.java
@@ -1,10 +1,26 @@
 package _Algorithm.Divide_Conquer;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P10868 {
 
+	static int n,m,i,a[];
+	static long[]t;
+	static long o(int n,int s,int e) {
+		if(s==e)return t[n]=a[s];
+		int m=(s+e)/2;
+		return t[n]=o(n*2,s,m)+o(n*2+1,m+1,e);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();m=s.nextInt();
+		a=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=1;i<n;i*=2);
+		i*=2;
+		t=new long[i];
+		System.out.println(Arrays.toString(t));
 	}
 
 }
