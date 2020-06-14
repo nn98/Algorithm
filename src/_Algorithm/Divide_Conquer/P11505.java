35e5b8e4ed831c352d3d9f6ab611a5f15cac2b54
nn98 <jkllhgb@gmail.com>
2020-06-14 09:13:43 +0900

    11505

--- src/_Algorithm/Divide_Conquer/P11505.java
+++ src/_Algorithm/Divide_Conquer/P11505.java
@@ -1,5 +1,49 @@
 package _Algorithm.Divide_Conquer;
 
-public class P11505 {
+import java.util.Scanner;
 
+public class P11505 {
+	static int n,m,i,x;
+	static long d,a[],t[];
+	static long o(int n,int s,int e) {
+		if(s==e)return t[n]=a[s];
+		int m=(s+e)/2;
+		return t[n]=o(n*2,s,m)*o(n*2+1,m+1,e)%1000000007;
+	}
+	static void p(int n,int s,int e,int x,long d) {
+		if(x<s||x>e)return;
+		t[n]+=d;
+		if(s!=e) {
+			int m=(s+e)/2;
+			p(n*2,s,m,x,d);
+			p(n*2+1,m+1,e,x,d);
+		}
+	}
+	static long u(int n,int s,int e,int l,int r) {
+		if(l>e||r<s)return 0;
+		if(l<=s&&e<=r)return t[n];
+		int m=(s+e)/2;
+		return u(n*2,s,m,l,r)+u(n*2+1,m+1,e,l,r);
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt()+s.nextInt();
+		a=new long[n];
+		for(;i<n;a[i++]=s.nextLong());
+		for(i=1;i<n;i*=2);
+		i*=2;
+		t=new long[i];
+		o(1,0,--n);
+		for(;m-->0;) {
+			if(s.nextInt()==1) {
+				x=s.nextInt()-1;
+				i=s.nextInt();
+				d=i-a[x];
+				a[x]=i;
+				p(1,0,n,x,d);
+			}else
+				System.out.println(u(1,0,n,s.nextInt()-1,s.nextInt()-1));
+		}
+	}
 }
