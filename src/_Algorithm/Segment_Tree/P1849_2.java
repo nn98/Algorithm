2d8a78756935946d68b543996ec4fcc4c52012cb
nn98 <jkllhgb@gmail.com>
2020-06-16 06:29:00 +0900

    1849

--- src/_Algorithm/Segment_Tree/P1849_2.java
+++ src/_Algorithm/Segment_Tree/P1849_2.java
@@ -3,9 +3,11 @@ package _Algorithm.Segment_Tree;
 import java.util.Scanner;
 
 public class P1849_2 {
-	static int n,m,i,j,a[],t[];
+	static int n,m,i,j,a[],t[],x;
 	static void u() {
-		
+		x=j+i-1;
+		t[x]=m;
+		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
 	}
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
@@ -13,6 +15,7 @@ public class P1849_2 {
 		a=new int[n+1];
 		for(i=1;i<n*2;i*=2);
 		t=new int[i];
+		j=i/2;
 		m=1;
 		for(i=0;i++<n;)u();
 	}
