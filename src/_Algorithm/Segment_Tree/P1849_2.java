194ec1296c4d7e49488de7876e4d2b634f39e18d
nn98 <jkllhgb@gmail.com>
2020-06-16 06:37:26 +0900

    1849

--- src/_Algorithm/Segment_Tree/P1849_2.java
+++ src/_Algorithm/Segment_Tree/P1849_2.java
@@ -4,11 +4,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1849_2 {
-	static int n,m,i,j,a[],t[],x;
+	static int n,m,i,j,a[],t[],x,e,p;
+//	재귀 대신 하향식 값 지정
 	static void u() {
 		x=j+i-1;
 		t[x]=m;
 		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
+	}
+	static int f(int x,int v) {
+		if(x>=j)return x-j+1;
+		int p=t[x*2];
+		if(v<p)return f(x*2,v);
+		return f(x*2+1,v-p);
 	}
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
@@ -19,6 +26,14 @@ public class P1849_2 {
 		j=i/2;
 		m=1;
 		for(i=0;i++<n;)u();
-		System.out.println(Arrays.toString(t));
+//		System.out.println(Arrays.toString(t));
+		m=0;
+		for(;e++<n;) {
+			p=s.nextInt();
+			i=f(1,p);
+			a[i]=e;
+			u();
+		}
+		System.out.println(Arrays.toString(a));
 	}
 }
