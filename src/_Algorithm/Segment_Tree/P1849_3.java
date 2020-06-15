cde334f0fad788e9ee4c29bf8ad06e59c10454e1
nn98 <jkllhgb@gmail.com>
2020-06-16 06:40:21 +0900

    1849

--- src/_Algorithm/Segment_Tree/P1849_3.java
+++ src/_Algorithm/Segment_Tree/P1849_3.java
@@ -1,5 +1,37 @@
 package _Algorithm.Segment_Tree;
 
-public class P1849_3 {
+import java.io.*;
 
+public class P1849_3 {
+	static int n,m,i,j,a[],t[],x,e,p;
+	static void u() {
+		x=j+i-1;
+		t[x]=m;
+		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
+	}
+	static int f(int x,int v) {
+		if(x>=j)return x-j+1;
+		int p=t[x*2];
+		if(v<p)return f(x*2,v);
+		return f(x*2+1,v-p);
+	}
+	public static void main(String[]z)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n+1];
+		for(i=1;i<n*2;i*=2);
+		t=new int[i];
+		j=i/2;
+		m=1;
+		for(i=0;i++<n;)u();
+		m=0;
+		for(;e++<n;) {
+			p=Integer.parseInt(r.readLine());
+			i=f(1,p);
+			a[i]=e;
+			u();
+		}
+		for(i=0;i++<n;System.out.println(a[i]));
+	}
 }
