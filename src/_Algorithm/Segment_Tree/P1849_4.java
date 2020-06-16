b03fa3bf4a85cddc353dd8ffb69e91f1dab7b427
nn98 <jkllhgb@gmail.com>
2020-06-17 08:25:59 +0900

    1849

--- src/_Algorithm/Segment_Tree/P1849_4.java
+++ src/_Algorithm/Segment_Tree/P1849_4.java
@@ -15,11 +15,19 @@ public class P1849_4 {
 		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);	// 계덱스 1보다 클 동안 2씩 나누며		아 시바 트리만드는거네 그냥
 		// ㄴㄴ 계덱스 1 즉 탑까지 다 돌면서 트리 생성
 	}
-	static int f(int x,int v) {
-		if(x>=j)return x-j+1;
-		int p=t[x*2];
-		if(v<p)return f(x*2,v);
-		return f(x*2+1,v-p);
+//	create and update OK
+	
+	static int f(int x,int v) {					// x index, v value, j mid index
+		System.out.println(x+" "+v);
+//		ㅁ: 1, 5	ㄹ: 3, 1	ㅂ: 7, 0	ㅍ: 14, 0
+		if(x>=j)return x-j+1;					// 인덱스 중간값보다 크거나 같으면 중간만큼 빼고 +1	ㅁ: 8==1
+//		ㄴ		ㄴ		ㄴ		ㅇ: 14-8+1 = 7
+		int p=t[x*2];							// p 트리 왼쪽노드값
+//		ㅔ = 4	ㅔ = 1	ㅔ = 1
+		if(v<p)return f(x*2,v);					// 대상이 작으면 왼쪽노드에,
+//		ㄴ		ㄴ		ㅇ		ㅍ: 14, 0
+		return f(x*2+1,v-p);					// 크거나 같으면 오른쪽노드로	가는데 왜 p를 빼 시벌
+//		ㄹ: 3, 1	ㅂ: 7, 0
 	}
 	public static void main(String[]z)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -31,6 +39,7 @@ public class P1849_4 {
 		j=i/2;
 		m=1;
 		for(i=0;i++<n;)u();
+		System.out.println(Arrays.toString(t));
 		m=0;
 		for(;e++<n;) {
 			p=Integer.parseInt(r.readLine());				//값 입력받고
