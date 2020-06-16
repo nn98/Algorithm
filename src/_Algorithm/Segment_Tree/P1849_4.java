95a8e10e308f7663121059c41a2aec8d8e54b38b
nn98 <jkllhgb@gmail.com>
2020-06-17 07:55:12 +0900

    1849

--- src/_Algorithm/Segment_Tree/P1849_4.java
+++ src/_Algorithm/Segment_Tree/P1849_4.java
@@ -1,10 +1,23 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P1849_4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,a[],t[],i,j,m;
+	public static void main(String[]z)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n+1];
+		for(i=1;i<n*2;i*=2);
+		t=new int[i];
+		j=i/2;
+		m=1;
+		
 	}
 
 }
