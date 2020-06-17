3a542559bc4493cb9f791fdbfed742af963fd26e
nn98 <jkllhgb@gmail.com>
2020-06-18 00:59:13 +0900

    3653

--- src/_Algorithm/Segment_Tree/P3653_2.java
+++ src/_Algorithm/Segment_Tree/P3653_2.java
@@ -1,10 +1,13 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.*;
+import java.util.*;
 public class P3653_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
