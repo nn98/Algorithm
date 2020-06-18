5c39a935eca5f9c0476cc5122db0a72830c7ea5e
nn98 <jkllhgb@gmail.com>
2020-06-19 05:49:58 +0900

    11505

--- src/_Algorithm/Segment_Tree/P11505_3.java
+++ src/_Algorithm/Segment_Tree/P11505_3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class P11505_3 {
 
-	static int n,m,i,j,x;
+	static int n,m,i,j,x,p,q;
 	static long t[],M=1000000007;
 	static void o() {
 		for(;x>1;x/=2,t[x]=t[x*2]*t[x*2+1]%M);
@@ -18,8 +18,9 @@ public class P11505_3 {
 		j=i;
 		i*=2;
 		t=new long[i];
+		Arrays.fill(t,1);
 //		for(;j<i;x=j++,t[x]=s.nextInt(),o());
-		for(;m++<n;x=j++,t[x]=s.nextInt(),o());
+		for(;p++<n;x=j++,t[x]=s.nextInt(),o());
 		System.out.println(Arrays.toString(t));
 	}
 
