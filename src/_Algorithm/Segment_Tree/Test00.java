ec7bf5d4f4b95bf0f211a257ff8d4666b430a3c1
nn98 <jkllhgb@gmail.com>
2020-06-22 06:00:34 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test00.java
+++ src/_Algorithm/Segment_Tree/Test00.java
@@ -1,10 +1,22 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Scanner;
+
 public class Test00 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,i=0,j,a[],r[];
+		a=new int[n];
+		r=new int[n];
+		for(;i++<n;) {
+			j=0;
+			m=s.nextInt();
+			while(m-->0)while(a[++j]>0);
+			System.out.println(j);
+			a[j]++;
+			r[j]=i;
+		}
 	}
 
 }
