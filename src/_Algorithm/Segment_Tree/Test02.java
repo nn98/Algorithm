4846a8fb49eae00afc019c9274f6f302011c45d0
nn98 <jkllhgb@gmail.com>
2020-06-22 06:38:05 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test02.java
+++ src/_Algorithm/Segment_Tree/Test02.java
@@ -7,7 +7,17 @@ import java.io.OutputStreamWriter;
 
 public class Test02 {
 
-	public static void main(String[] args)throws Exception{
+	static int n,m,i=1,j,k,x,y,v,a[],t[];
+	static void o() {
+		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
+	}
+	static void p() {
+		if(x>=j)y=x-j+1;
+		else {
+			y=t[x*=2];
+		}
+	}
+	public static void main(String[]z)throws Exception{
 		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter W=new BufferedWriter(new OutputStreamWriter(System.out));
 
