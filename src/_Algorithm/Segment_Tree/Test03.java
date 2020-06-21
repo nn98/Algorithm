8374b3f09dc7c4fa9ee9cd9eb2d5f80898ab6ebf
nn98 <jkllhgb@gmail.com>
2020-06-22 06:53:33 +0900

    1777 test

--- src/_Algorithm/Segment_Tree/Test03.java
+++ src/_Algorithm/Segment_Tree/Test03.java
@@ -1,9 +1,10 @@
 package _Algorithm.Segment_Tree;
 
 import java.io.*;
+import java.util.*;
 
 public class Test03 {
-	static int n,m,i=1,j,k,x,y,v,a[],t[];
+	static int n,m,i=1,j,k,x,y,v,a[],b[],t[];
 	static void o() {
 		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
 	}
@@ -23,12 +24,14 @@ public class Test03 {
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		n=Integer.parseInt(r.readLine());
 		a=new int[n];
+		b=new int[n];
 		for(;i<n;i*=2);
 		j=i;
 		t=new int[i*2];
 		for(i=0;i<n;t[x=j+i++]=1,o());
+		StringTokenizer T=new StringTokenizer(r.readLine());
+		for(;k<n;b[k++]=Integer.parseInt(T.nextToken()));
 		for(i=0;i++<n;) {
-			v=Integer.parseInt(r.readLine());
 			p();
 			a[y]=i;
 			t[x=j+y]=0;
