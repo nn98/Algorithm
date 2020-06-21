4dc1d243c6b71c87089861736a68088a184e7d8c
nn98 <jkllhgb@gmail.com>
2020-06-22 06:07:24 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test00.java
+++ src/_Algorithm/Segment_Tree/Test00.java
@@ -1,18 +1,19 @@
 package _Algorithm.Segment_Tree;
 
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 
 public class Test00 {
 
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m,i=0,j,a[],r[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter W=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(R.readLine()),m,i=0,j,a[],r[];
 		a=new int[n];
 		r=new int[n];
 		for(;i++<n;) {
 			j=0;
-			m=s.nextInt();
+			m=Integer.parseInt(R.readLine());
 			while(a[j]>0)j++;
 			while(m>0) {
 				j++;
@@ -23,7 +24,8 @@ public class Test00 {
 			a[j]++;
 			r[j]=i;
 		}
-		for(i=0;i<n;System.out.println(r[i++]));
+		for(i=0;i<n;W.write(r[i++]));
+		W.flush();
 	}
 
 }
