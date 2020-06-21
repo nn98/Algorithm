c6a48626dbd046088a692a649758d7267d313d62
nn98 <jkllhgb@gmail.com>
2020-06-22 06:45:05 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test02.java
+++ src/_Algorithm/Segment_Tree/Test02.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Test02 {
 
@@ -30,13 +31,15 @@ public class Test02 {
 		for(;i<n;i*=2);
 		j=i;
 		t=new int[i*2];
-		for(i=0;i<n;t[x=j+i++]=1);
+		for(i=0;i<n;t[x=j+i++]=1,o());
+		System.out.println(Arrays.toString(t));
 		for(i=0;i++<n;) {
 			v=Integer.parseInt(r.readLine());
 			p();
 			a[y]=i;
 			t[x=j+y]=0;
 			o();
+			System.out.println(Arrays.toString(t));
 		}
 		for(i=0;i<n;w.write(a[i++]+"\n"));
 		w.flush();
