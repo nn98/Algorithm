cc8f11c7fa6b10a0e2b7d6d73f95af6b5f23ecc4
nn98 <jkllhgb@gmail.com>
2020-06-22 06:43:27 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test02.java
+++ src/_Algorithm/Segment_Tree/Test02.java
@@ -12,15 +12,26 @@ public class Test02 {
 		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
 	}
 	static void p() {
-		if(x>=j)y=x-j+1;
+		if(x>=j)y=x-j;
 		else {
 			y=t[x*=2];
+			if(v>=y) {
+				x++;
+				v-=y;
+			}
+			p();
 		}
 	}
 	public static void main(String[]z)throws Exception{
-		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter W=new BufferedWriter(new OutputStreamWriter(System.out));
-
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		for(;i<n;i*=2);
+		j=i;
+		t=new int[i*2];
+		for(i=0;i<n;t[x=j+i++]=1);
+		
 	}
 
 }
