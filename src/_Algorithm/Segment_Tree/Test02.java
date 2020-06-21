77b84877773dbe725885e432cebccf38de387f96
nn98 <jkllhgb@gmail.com>
2020-06-22 06:33:59 +0900

    1849 test

--- src/_Algorithm/Segment_Tree/Test02.java
+++ src/_Algorithm/Segment_Tree/Test02.java
@@ -1,9 +1,15 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Test02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception{
+		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter W=new BufferedWriter(new OutputStreamWriter(System.out));
 
 	}
 
