d6bf50994c2804604d45a7079534ad491ea124a2
nn98 <jkllhgb@gmail.com>
2020-06-21 12:50:13 +0900

    1300

--- src/_Algorithm/Binary_Search/P1300.java
+++ src/_Algorithm/Binary_Search/P1300.java
@@ -1,10 +1,23 @@
 package _Algorithm.Binary_Search;
 
+import java.util.Scanner;
+
 public class P1300 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),m,f=1,t=k,i,u,r=0;
+		while(f<=t) {
+			m=(f+t)/2;
+			u=0;
+			for(i=0;++i<=n;u+=m/i<n?m/i:n);
+			if(u<k)f=m+1;
+			else {
+				r=m;
+				t=m-1;
+			}
+		}
+		System.out.print(r);
 	}
 
 }
