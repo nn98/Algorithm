5cc3e56cd75887f837aa50ab6483c9187bb3f8ac
nn98 <jkllhgb@gmail.com>
2020-06-24 06:18:30 +0900

    3653 T

--- src/_Algorithm/Segment_Tree/Test04.java
+++ src/_Algorithm/Segment_Tree/Test04.java
@@ -1,5 +1,6 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test04 {
@@ -10,11 +11,18 @@ public class Test04 {
 	}
 	static int u(int n,int s,int e) {
 		if(r<s||e<l)return 0;
+		if(l<=s&&e<=r)return t[n];
+		int m=(s+e)/2;
+		return u(n*2,s,m)+u(n*2+1,m+1,e);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
-		
+		for(i=1;i<n;i*=2);
+		j=i;
+		t=new int[i*2];
+		for(;m<n;x=j+m++,t[x]=s.nextInt(),o());
+		System.out.println(Arrays.toString(t));
 	}
 
 }
