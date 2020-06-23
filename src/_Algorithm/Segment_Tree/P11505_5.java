85275de7397ba59bd3f18206ac810663d1b9c46a
nn98 <jkllhgb@gmail.com>
2020-06-24 07:50:49 +0900

    11505

--- src/_Algorithm/Segment_Tree/P11505_5.java
+++ src/_Algorithm/Segment_Tree/P11505_5.java
@@ -7,13 +7,13 @@ public class P11505_5 {
 	static int x,n,m,i,j,k,l,r;
 	static long t[],M=1000000007;
 	static void o() {
-		for(;x>1;x/=2,t[x]=t[x*2]*t[x*2+1]);
+		for(;x>1;x/=2,t[x]=Math.abs(t[x*2]*t[x*2+1]));
 	}
 	static long u(int n,int s,int e) {
 		if(r<s||e<l)return 1;
 		if(l<=s&&e<=r)return t[n];
 		int m=(s+e)/2;
-		return u(n*2,s,m)*u(n*2+1,m+1,e)%M;
+		return Math.abs(u(n*2,s,m)*u(n*2+1,m+1,e))%M;
 	}
 	public static void main(String[]Z)throws Exception{
 		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
@@ -33,7 +33,7 @@ public class P11505_5 {
 			if(k>1) {
 				l=Integer.parseInt(z.nextToken())-1;
 				r=Integer.parseInt(z.nextToken())-1;
-				System.out.println(u(1,0,j-1));
+				System.out.println(Math.abs(u(1,0,j-1)));
 			}else {
 				x=Integer.parseInt(z.nextToken())-1+j;
 				t[x]=Integer.parseInt(z.nextToken());
