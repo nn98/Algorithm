e2f2c23fb3af40238b168c672c289a7462379552
nn98 <jkllhgb@gmail.com>
2020-06-24 06:08:19 +0900

    3653 T

--- src/_Algorithm/Segment_Tree/Test04.java
+++ src/_Algorithm/Segment_Tree/Test04.java
@@ -1,10 +1,20 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Scanner;
+
 public class Test04 {
 
+	static int x,n,m,i,j,k,p,q,l,r,a[],t[];
+	static void o() {
+		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
+	}
+	static int u(int n,int s,int e) {
+		if(r<s||e<l)return 0;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		
 	}
 
 }
