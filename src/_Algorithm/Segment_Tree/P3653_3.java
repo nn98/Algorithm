9bd008b379c2c15d2f591ba78e5e5307fb6b528a
nn98 <jkllhgb@gmail.com>
2020-06-24 06:08:14 +0900

    3653 T

--- src/_Algorithm/Segment_Tree/P3653_3.java
+++ src/_Algorithm/Segment_Tree/P3653_3.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 
 public class P3653_3 {
 
-	static int n,m,i,j,k,a[],b[],t[],x,p,q;
+	static int n,m,i,j,k,a[],b[],t[],x,p,l,r;
 	static void o() {
 		for(;x>1;x/=2,t[x]=t[x*2]+t[x*2+1]);
+	}
+	static int u(int n,int s,int e) {
+		if(r<s||e<l)return t[n];
+		if(l<=s&&e<=r)return t[n];
+		int m=(s+e)/2;
+		return u(n*2,s,m)+u(n*2+1,m+1,e);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
