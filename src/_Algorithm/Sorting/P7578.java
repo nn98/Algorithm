ba03c086fbb06db0865e2c7d316b6dd4dd3b10a5
nn98 <jkllhgb@gmail.com>
2020-06-25 19:55:19 +0900

    7578

--- src/_Algorithm/Sorting/P7578.java
+++ src/_Algorithm/Sorting/P7578.java
@@ -1,5 +1,51 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P7578 {
+	static int n,i,a[],b[];
+	static long R;
+	static void o(int f,int t) {
+		if(f<t) {
+			int m=(f+t)/2;
+			o(f,m);
+			o(m+1,t);
+			m(f,m,t);
+		}
+	}
+	static void m(int p, int q, int r){
+		int idx = p;
+		int i = p, j = q+1;
+		int cnt = 0;
+		while(i<=q || j<=r){
+			System.out.println(i<=q | j<=r);
+			if(i > q){
+				System.out.println(55);
+				b[idx++] = a[j++]; cnt++;
+			}else if(j > r){
+				System.out.println(66);
+				b[idx++] = a[i++]; R += cnt;
+			}else if(a[i] <= a[j]){
+				System.out.println(77);
+				b[idx++] = a[i++]; R += cnt;
+			}else{
+				System.out.println(88);
+				b[idx++] = a[j++]; cnt++;
+			}
+		}
+		for(i=p; i<=r; i++) a[i] = b[i];
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[500010];
+		b=new int[500010];
+		for(;i<n;a[i++]=s.nextInt());
+//		System.out.println(Arrays.toString(a));
+		o(0,n-1);
+//		for(i=0;i<n;System.out.println(a[i++]));
+//		System.out.println(Arrays.toString(a));
+		System.out.println(R);
+	}
 
 }
