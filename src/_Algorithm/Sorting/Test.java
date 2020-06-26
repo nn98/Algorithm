2c3739f7738d16cc6e165962e7808f0c945a0b0b
nn98 <jkllhgb@gmail.com>
2020-06-26 23:23:51 +0900

    1377 T

--- src/_Algorithm/Sorting/Test.java
+++ src/_Algorithm/Sorting/Test.java
@@ -1,10 +1,17 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[][]=new int[n][2],i=0;
+		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=i+1);
+		for(int[]b:a)System.out.println(Arrays.toString(b));
+		Arrays.sort(a);
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
