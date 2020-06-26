4bc4fdd803f5c56d6fc749dacbde97f2d1121587
nn98 <jkllhgb@gmail.com>
2020-06-26 23:42:23 +0900

    Quick

--- src/_Algorithm/Sorting/QT.java
+++ src/_Algorithm/Sorting/QT.java
@@ -1,8 +1,15 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class QT {
 
+	static int n,a[],m,i;
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
