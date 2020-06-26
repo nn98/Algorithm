41599936bfb16be1bb9056784a24cadaf72eb87c
nn98 <jkllhgb@gmail.com>
2020-06-26 23:07:24 +0900

    1377

--- src/_Algorithm/Sorting/P1377.java
+++ src/_Algorithm/Sorting/P1377.java
@@ -9,7 +9,7 @@ public class P1377 {
 		int n=s.nextInt()+1,i=0,j,k=1,a[]=new int[n];
 		for(;++i<n;a[i]=s.nextInt());
 		for(i=1;++i<n;k+=a[i-1]>a[i]?1:0);
-		System.out.println(n-k-1);
+		System.out.println(n-k);
 	}
 
 }
