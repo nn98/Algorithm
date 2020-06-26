82dd6b46b2d7aea0c71b11b8ecd8c766decab2ff
nn98 <jkllhgb@gmail.com>
2020-06-26 23:26:56 +0900

    1377 T

--- src/_Algorithm/Sorting/Test.java
+++ src/_Algorithm/Sorting/Test.java
@@ -1,16 +1,25 @@
 package _Algorithm.Sorting;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Test {
 
+	static int n,a[][],i;
+	static void w(int i,int j) {
+		int[]t=a[i];
+		a[i]=a[j];
+		a[j]=t;
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[][]=new int[n][2],i=0;
+		n=s.nextInt();
+		a=new int[n][2];
 		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=i);
 		for(int[]b:a)System.out.println(Arrays.toString(b));
-		Arrays.sort(a);
+		w(1,4);
+		System.out.println();
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
