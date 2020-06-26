ae65318625973ebd7395d1be53cac64c48c1b7a7
nn98 <jkllhgb@gmail.com>
2020-06-26 13:39:09 +0900

    9463

--- src/_Algorithm/Sorting/P9463.java
+++ src/_Algorithm/Sorting/P9463.java
@@ -1,5 +1,45 @@
 package _Algorithm.Sorting;
 
-public class P9463 {
+import java.util.Scanner;
 
+public class P9463 {
+	static int n,i,a[],b[],A[];
+	static long R;
+	static void o(int f,int t) {
+		if(f<t) {
+			int m=(f+t)/2;
+			o(f,m);
+			o(m+1,t);
+			m(f,m,t);
+		}
+	}
+	static void m(int p, int q, int r){
+		int idx = p;
+		int i = p, j = q+1;
+		int cnt = 0;
+		while(i<=q || j<=r){
+			if(i > q){
+				b[idx++] = a[j++]; cnt++;
+			}else if(j > r){
+				b[idx++] = a[i++]; R += cnt;
+			}else if(a[i] <= a[j]){
+				b[idx++] = a[i++]; R += cnt;
+			}else{
+				b[idx++] = a[j++]; cnt++;
+			}
+		}
+		for(i=p; i<=r; i++) a[i] = b[i];
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		A=new int[1000001];
+		for(;i<n;A[s.nextInt()]=i++);
+		a=new int[500010];
+		b=new int[500010];
+		for(i=0;i<n;a[i++]=A[s.nextInt()]);
+//		System.out.println(Arrays.toString(a));
+		o(0,n-1);
+		System.out.println(R);
+	}
 }
