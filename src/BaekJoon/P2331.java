e07431d393236c3a2f1bf9c6c67f72048f602347
nn98 <jkllhgb@gmail.com>
2020-06-28 19:53:45 +0900

    2331

--- src/BaekJoon/P2331.java
+++ src/BaekJoon/P2331.java
@@ -1,22 +1,26 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2331 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a[]=new int[10000],p,i=0,j,k=1,q;
+		int a[]=new int[1000],p,i=0,j,k=1,q;
 		a[i++]=s.nextInt();
 		p=s.nextInt();
-		for(;i<10000;) {
+		for(;i<20;) {
 			q=0;
 			j=a[i-1];
 			for(k=1;k<=j;k*=10);
+			System.out.println(k);
 			for(;k>1;k/=10,q+=j/k*p,q%=k);
+			System.out.println(" "+q);
 			a[i++]=q;
 			for(;k<i;k++)if(a[k]==a[i-1])break;
 		}
+		System.out.println(Arrays.toString(a));
 		System.out.println(k);
 	}
 
