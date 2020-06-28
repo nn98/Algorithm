9c753020820a012da449535b6b1ea0bfaf7da9aa
nn98 <jkllhgb@gmail.com>
2020-06-28 20:37:17 +0900

    2331

--- src/BaekJoon/P2331.java
+++ src/BaekJoon/P2331.java
@@ -8,20 +8,24 @@ public class P2331 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		long p,j,k=1,q,a[]=new long[100000];
-		int i=0,x=0;
+		long p,j,k=1,q,a[]=new long[10000000];
+		int i=0,x=0,r=0;
 		a[i++]=s.nextInt();
 		p=s.nextInt();
 		loop:
-		for(;i<20;i++) {
-			q=0;
-			j=a[i-1];
-			for(k=1;k<=j;k*=10);
-			for(;k>1;k/=10,q+=Math.pow(j/k,p),j%=k);
-			a[i]=q;
-			for(x=0;x<i;x++)if(a[x]==a[i])break loop;
-		}
-//		System.out.println(Arrays.toString(a));
-		System.out.println(x);
+			for(;i<20;i++) {
+				q=0;
+				j=a[i-1];
+				for(k=1;k<=j;k*=10);
+				for(;k>1;k/=10,q+=Math.pow(j/k,p),j%=k);
+				a[i]=q;
+				for(x=0;x<i;x++)
+					if(a[x]==a[i]) {
+						r=x;
+						break loop;
+					}
+			}
+		//		System.out.println(Arrays.toString(a));
+		System.out.println(r);
 	}
 }
