f2b3e44843eeb7dbf47cc352f8372c9f3a863eaa
nn98 <jkllhgb@gmail.com>
2020-06-28 19:49:27 +0900

    2331

--- src/BaekJoon/P2331.java
+++ src/BaekJoon/P2331.java
@@ -9,7 +9,7 @@ public class P2331 {
 		int a[]=new int[10000],p,i=0,j,k=1,q;
 		a[i++]=s.nextInt();
 		p=s.nextInt();
-		for(;;) {
+		for(;i<10000;) {
 			q=0;
 			j=a[i-1];
 			for(k=1;k<=j;k*=10);
