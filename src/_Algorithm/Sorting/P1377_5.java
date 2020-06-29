de92ccf1be658ad5105fedab96984a4913584f5d
nn98 <jkllhgb@gmail.com>
2020-06-29 14:08:54 +0900

    1377

--- src/_Algorithm/Sorting/P1377_5.java
+++ src/_Algorithm/Sorting/P1377_5.java
@@ -1,5 +1,43 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1377_5 {
+	static int n,i,j,b[][],c[][],t[],m,v;
+	static void w(int i,int j) {
+		t=b[i];
+		b[i]=b[j];
+		b[j]=t;
+	}
+	static void o(int s,int e) {
+		if(s<e) {
+			p(s,e);
+			o(s,j);
+			o(j+2,e);
+		}
+	}
+	static void p(int s,int e) {
+		m=(s+e)/2;
+		w(m,e);
+		v=b[e][0];
+		j=s-1;
+		for(;s<e;s++)if(b[s][0]<v)w(++j,s);
+		w(j+1,e);
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		b=new int[n][2];
+		c=new int[n][2];
+		for(;i<n;c[i][0]=b[i][0]=s.nextInt(),c[i][1]=b[i][1]=i++);
+		o(0,n-1);
+		m=0;
+		for(i=n;i-->0;)
+			for(j=i;j-->0;)
+				if(c[i][0]==b[j][0]&c[i][1]==b[j][1])
+					m=m>i-j?m:i-j;
+
+					System.out.println(m+1);
+	}
 
 }
