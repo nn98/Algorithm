acc39c8ddac4614d0b309620559c7a0b864e2d43
nn98 <jkllhgb@gmail.com>
2020-06-29 14:37:35 +0900

    1377

--- src/_Algorithm/Sorting/P1377_6.java
+++ src/_Algorithm/Sorting/P1377_6.java
@@ -1,11 +1,30 @@
 package _Algorithm.Sorting;
 
 import java.io.*;
+import java.util.*;
 
 public class P1377_6 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),j=1000001,i=0,R,c=0,k;
+		List<Integer>[]a=new List[1000001];
+		for(;i++<n;) {
+			R=Integer.parseInt(r.readLine());
+			if(a[R]==null)a[R]=new ArrayList();
+			a[R].add(i);
+		}
+		R=0;
+		for(i=0;i<j;i++) {
+			if(a[i]!=null) {
+				Collections.sort(a[i]);
+				for(k=0;k<a[i].size();k++) {
+					c++;
+					R=Math.min(a[i].get(k)-c,R);
+				}
+			}
+		}
+		System.out.println(R+1);
 	}
 
 }
