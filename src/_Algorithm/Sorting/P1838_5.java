524c13a5d7ca9d4a8539c831cf05c69067c5ba4b
nn98 <jkllhgb@gmail.com>
2020-06-29 15:19:19 +0900

    1838

--- src/_Algorithm/Sorting/P1838_5.java
+++ src/_Algorithm/Sorting/P1838_5.java
@@ -1,5 +1,34 @@
 package _Algorithm.Sorting;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
 public class P1838_5 {
+	static class P implements Comparable<P>{
+		int x,v;
+		P(int X,int V){
+			x=X;
+			v=V;
+		}
+		public int compareTo(P p){
+			return v-p.v;
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),R=0,x=0,i=0;
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		PriorityQueue<P>q=new PriorityQueue();
+		for(;i<n;q.add(new P(i++,Integer.parseInt(t.nextToken()))));
+		while(!q.isEmpty()) {
+			P p=q.poll();
+			R=R>p.x-x?R:p.x-x;
+		x++;
+		}
+		System.out.println(R);
+	}
 
 }
