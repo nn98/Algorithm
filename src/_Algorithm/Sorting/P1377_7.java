d801009834266e27d46a1f0cb6a2d1551971267e
nn98 <jkllhgb@gmail.com>
2020-06-29 14:48:42 +0900

    1377

--- src/_Algorithm/Sorting/P1377_7.java
+++ src/_Algorithm/Sorting/P1377_7.java
@@ -7,8 +7,7 @@ public class P1377_7 {
 		int n=s.nextInt(),i=0,a[][]=new int[n][2],r=0;
 		for(;i<n;a[i][0]=s.nextInt(),a[i][1]=i++);
 		Arrays.sort(a,(b,c)->b[0]-c[0]);
-		for(i=0;i<n;i++)
-			r=Math.max(r,a[i][1]-i);
+		for(i=0;i<n;i++)r=Math.max(r,a[i][1]-i);
 		System.out.print(r+1);
 	}
 }
