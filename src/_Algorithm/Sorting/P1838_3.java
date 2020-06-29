c05774f5ef2bfcec8019f3092c79d0473a387b07
nn98 <jkllhgb@gmail.com>
2020-06-29 15:08:14 +0900

    1838

--- src/_Algorithm/Sorting/P1838_3.java
+++ src/_Algorithm/Sorting/P1838_3.java
@@ -1,9 +1,24 @@
 package _Algorithm.Sorting;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class P1838_3 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(R.readLine()),a[]=new int[n],i=0,b[]=new int[n],j,r=0;
+		StringTokenizer t=new StringTokenizer(R.readLine());
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		Arrays.sort(a);
+		for(i=0;i<n;i++)
+			for(j=n;j-->0;)
+				if(a[i]==b[j])
+					r=r>j-i?r:j-i;
+
+					System.out.println(r);
 	}
 
 }
