f7326d14c68e66c12db52a0d132f1a7a7f2a69e1
nn98 <jkllhgb@gmail.com>
2020-06-29 14:15:22 +0900

    1377

--- src/_Algorithm/Sorting/P1377_6.java
+++ src/_Algorithm/Sorting/P1377_6.java
@@ -1,10 +1,11 @@
 package _Algorithm.Sorting;
 
+import java.io.*;
+
 public class P1377_6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
