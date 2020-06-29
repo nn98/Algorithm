77f382f5ab66742bed7bcd39749fe0988f17f4e3
nn98 <jkllhgb@gmail.com>
2020-06-29 15:21:46 +0900

    1838

--- src/_Algorithm/Sorting/P1838_5.java
+++ src/_Algorithm/Sorting/P1838_5.java
@@ -1,9 +1,7 @@
 package _Algorithm.Sorting;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 public class P1838_5 {
 	static class P implements Comparable<P>{
 		int x,v;
