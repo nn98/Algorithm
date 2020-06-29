bfa0c033867d2062f0d879675cf1b6bf715068b0
nn98 <jkllhgb@gmail.com>
2020-06-29 14:53:42 +0900

    1838

--- src/_Algorithm/Sorting/P1838.java
+++ src/_Algorithm/Sorting/P1838.java
@@ -7,8 +7,15 @@ public class P1838 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],i=0,b[]=new int[n];
+		int n=s.nextInt(),a[]=new int[n],i=0,b[]=new int[n],j,r=0;
 		for(;i<n;b[i]=a[i++]=s.nextInt());
+		Arrays.sort(a);
+		for(i=0;i<n;i++)
+			for(j=n;j-->0;)
+				if(a[i]==b[j])
+					r=r>j-i?r:j-i;
+					
+					System.out.println(r);
 	}
 
 }
