0fead6bc8e8aaf92f2c5d1ee1cc5419abd989810
nn98 <jkllhgb@gmail.com>
2020-06-29 14:50:56 +0900

    1838

--- src/_Algorithm/Sorting/P1838.java
+++ src/_Algorithm/Sorting/P1838.java
@@ -1,10 +1,13 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1838 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0;
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
