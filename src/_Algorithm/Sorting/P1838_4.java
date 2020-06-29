b7657d3dcd7df948f31364dd9e287008bec6a8fd
nn98 <jkllhgb@gmail.com>
2020-06-29 15:11:15 +0900

    1838

--- src/_Algorithm/Sorting/P1838_4.java
+++ src/_Algorithm/Sorting/P1838_4.java
@@ -1,9 +1,12 @@
 package _Algorithm.Sorting;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P1838_4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception{
+		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
 
 	}
 
