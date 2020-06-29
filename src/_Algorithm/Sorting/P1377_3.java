0c2133b47dd1bd9a137781ba1088931a3eb8cf7f
nn98 <jkllhgb@gmail.com>
2020-06-29 12:26:05 +0900

    1377

--- src/_Algorithm/Sorting/P1377_3.java
+++ src/_Algorithm/Sorting/P1377_3.java
@@ -1,8 +1,16 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1377_3 {
 
+	static int n,i,j,a[],b[][];
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		b=new int[2][n];
+		for(;i<n;a[i]=b[0][i]=s.nextInt(),b[1][i]=i++);
 		
 	}
 
