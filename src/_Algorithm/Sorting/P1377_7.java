a39b3e7ea9682200598dcac788db8127d28ad854
nn98 <jkllhgb@gmail.com>
2020-06-29 14:46:58 +0900

    1377

--- src/_Algorithm/Sorting/P1377_7.java
+++ src/_Algorithm/Sorting/P1377_7.java
@@ -1,10 +1,18 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P1377_7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,a[][]=new int[n][2],r=0;
+		for(;i<n;a[i][0]=s.nextInt(),a[i][1]=i++);
+		Arrays.sort(a,(b,c)->b[0]-c[0]);
+		for(i=0;i<n;i++)
+			r=Math.max(r,a[i][1]);
+		System.out.print(r+1);
 	}
 
 }
