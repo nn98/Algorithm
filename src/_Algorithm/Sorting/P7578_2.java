c6f438071a4de548e61157d1f4f1ee44e0fd387b
nn98 <jkllhgb@gmail.com>
2020-06-30 17:42:40 +0900

    7578

---
+++ src/_Algorithm/Sorting/P7578_2.java
@@ -0,0 +1,12 @@
+package _Algorithm.Sorting;
+
+import java.util.Scanner;
+
+public class P7578_2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+	}
+
+}
