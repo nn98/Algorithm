b5b8e3277a06804f3c0c8458bc9acc1780a26f52
nn98 <jkllhgb@gmail.com>
2020-07-01 00:28:49 +0900

    3770

--- src/_Algorithm/Sorting/P3770.java
+++ src/_Algorithm/Sorting/P3770.java
@@ -1,9 +1,10 @@
 package _Algorithm.Sorting;
 
+import java.io.*;
 public class P3770 {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine());
 	}
 
 }
