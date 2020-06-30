dd6e24595cd67c1198ca7327d2bfaaf820e489ed
nn98 <jkllhgb@gmail.com>
2020-06-30 21:32:57 +0900

    7578

--- src/_Algorithm/Sorting/P7578_4.java
+++ src/_Algorithm/Sorting/P7578_4.java
@@ -1,10 +1,10 @@
 package _Algorithm.Sorting;
+// 걍 사이즈늘려서 돌리면 똑같나
 
 public class P7578_4 {
 
+	static int n,i,j=500001,p,q,a[]=new int[j],b[]=new int[j],A[]=new int[1000001];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
