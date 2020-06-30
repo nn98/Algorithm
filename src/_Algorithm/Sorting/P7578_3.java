b125986aa15bee48757adf54e20d610cbef366db
nn98 <jkllhgb@gmail.com>
2020-06-30 17:51:27 +0900

    7578

--- src/_Algorithm/Sorting/P7578_3.java
+++ src/_Algorithm/Sorting/P7578_3.java
@@ -1,5 +1,22 @@
 package _Algorithm.Sorting;
 
+import java.io.*;
+import java.util.*;
+
 public class P7578_3 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader R=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(R.readLine()),a[]=new int[n],b[],z,A[]=new int[1000001],i=0,j,r=0;
+		StringTokenizer t=new StringTokenizer(R.readLine());
+		for(;i<n;a[i]=Integer.parseInt(t.nextToken()),A[a[i]]=i++);
+		t=new StringTokenizer(R.readLine());
+		for(i=0;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		for(i=0;++i<n;) {
+			z=A[a[i]];
+			for(j=0;j<i;j++)
+				if(A[a[j]]>z)r++;
+		}
+		System.out.print(r);
+	}
 
 }
