2e0e1ba89c5634fef12e29a82201c4c9300b1eac
nn98 <jkllhgb@gmail.com>
2020-06-30 18:26:36 +0900

    Merge 7578

--- src/_Algorithm/Sorting/MTest0.java
+++ src/_Algorithm/Sorting/MTest0.java
@@ -1,10 +1,27 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class MTest0 {
 
 	static int n,i,j,a[],b[];
-	public static void main(String[] args) {
+	static void o(int s,int e) {
+		if(s<e) {
+			int m=(s+e)/2;
+			o(s,m);
+			o(m+1,e);
+			m(s,e,m);
+		}
+	}
+	static void m(int s,int e,int m) {
 		
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		b=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
