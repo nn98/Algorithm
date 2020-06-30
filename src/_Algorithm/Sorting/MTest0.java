35fd0e4d118f38b92ca5247dec37818180f42cee
nn98 <jkllhgb@gmail.com>
2020-06-30 18:30:42 +0900

    Merge 7578

--- src/_Algorithm/Sorting/MTest0.java
+++ src/_Algorithm/Sorting/MTest0.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MTest0 {
 
-	static int n,i,j,a[],b[];
+	static int n,i,j,a[],b[],x,y;
 	static void o(int s,int e) {
 		if(s<e) {
 			int m=(s+e)/2;
@@ -14,7 +14,14 @@ public class MTest0 {
 		}
 	}
 	static void m(int s,int e,int m) {
-		
+		i=s;
+		j=m+1;
+		x=y=0;
+		while(!(i>m&j>e))
+			if(i>m)b[x++]=a[j++];
+			else if(j>e)b[x++]=a[i++];
+			else b[x++]=a[i]<a[j]?a[i++]:a[j++];
+			while( )
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
