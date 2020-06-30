edfa8750d2dc928f0f5557d53515d2a5d919b5d8
nn98 <jkllhgb@gmail.com>
2020-06-30 18:53:50 +0900

    Merge 7578

--- src/_Algorithm/Sorting/P1517_3.java
+++ src/_Algorithm/Sorting/P1517_3.java
@@ -1,5 +1,55 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1517_3 {
+
+	static int n,i,j,p,q,a[],b[],r=0,c;
+	static void o(int s,int e) {
+		if(s<e-1) {
+			int m=(s+e)/2;
+			o(s,m);
+			o(m,e);
+			m(s,e,m);
+		}
+	}
+	static void m(int s,int e,int m) {
+		i=s;
+		j=m;
+		c=p=q=0;
+		while(i<m|j<e) {
+			if(i==m) {
+				b[p]=a[j++];
+				c++;
+			}
+			else if(j==e) {
+				b[p]=a[i++];
+				r+=c;
+			}
+			else {
+				if(a[i]<a[j]) {
+					b[p]=a[i++];
+					r+=c;
+				}else {
+					b[p]=a[j++];
+					c++;
+				}
+			}
+			p++;
+		}
+		if(s==0&e==n) {
+			for(int x:b)System.out.println(x);
+		}
+		while(s<e)a[s++]=b[q++];
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		b=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		o(0,n);
+		System.out.println(r);
+	}
 
 }
