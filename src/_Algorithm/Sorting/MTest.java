f1451bf9f17769fd91665f723cadffcd2478c6b4
nn98 <jkllhgb@gmail.com>
2020-06-30 17:57:22 +0900

    Merge 7578

--- src/_Algorithm/Sorting/MTest.java
+++ src/_Algorithm/Sorting/MTest.java
@@ -1,10 +1,16 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class MTest {
 
+	static int n,a[],b[],i,j;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		b=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
