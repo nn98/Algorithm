b9ab20683e37bea0a965e28a2e1b97208612147f
nn98 <jkllhgb@gmail.com>
2020-06-30 23:29:20 +0900

    7578

--- src/_Algorithm/Sorting/P7578_4.java
+++ src/_Algorithm/Sorting/P7578_4.java
@@ -1,10 +1,16 @@
 package _Algorithm.Sorting;
 // 걍 사이즈늘려서 돌리면 똑같나
 
+import java.util.Scanner;
+
 public class P7578_4 {
 
 	static int n,i,j=500001,p,q,a[]=new int[j],b[]=new int[j],A[]=new int[1000001];
 	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(;i<n;A[s.nextInt()]=++i);
+		for(i=0;i<n;a[i]=A[s.nextInt()]);
 	}
 
 }
