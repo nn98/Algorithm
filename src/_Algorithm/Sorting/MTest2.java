cf4ce38b37ee7efc494883a85f79f3dd8f1fe8b0
nn98 <jkllhgb@gmail.com>
2020-06-30 19:17:24 +0900

    1517 M

--- src/_Algorithm/Sorting/MTest2.java
+++ src/_Algorithm/Sorting/MTest2.java
@@ -1,9 +1,48 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MTest2 {
 
+	static int n,i,j,p,q,a[],b[],c;
+	static long r=0;
+	static void o(int s,int e) {
+		if(s<e) {
+			int m=(s+e)/2;
+			o(s,m);
+			o(m+1,e);
+			m(s,e,m);
+		}
+	}
+	static void m(int s,int e,int m) {
+		i=s;
+		j=m+1;
+		p=q=c=0;
+		while(i<=m&j<=e) {
+			if(i>m) {
+				b[p]=a[j++];
+				c++;
+			}else if(j>e) {
+				b[p]=a[i++];
+				r+=c;
+			}else if(a[i]<a[j]) {
+				b[p]=a[i++];
+				r+=c;
+			}else if(a[j]<a[i]) {
+				b[p]=a[j++];
+				c++;
+			}
+			p++;
+		}
+		while(s<=e)a[s++]=b[q++];
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(;i<n;a[i++]=s.nextInt());
+		o(0,n-1);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
