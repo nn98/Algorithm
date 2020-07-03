76c95455c6fd6eff1f35a45e61a7272f5a8f1f75
nn98 <jkllhgb@gmail.com>
2020-07-04 06:28:19 +0900

    2812

--- src/_Algorithm/Greedy/P2812_2.java
+++ src/_Algorithm/Greedy/P2812_2.java
@@ -1,5 +1,25 @@
 package _Algorithm.Greedy;
 
-public class P2812_2 {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class P2812_2 {
+	public static void main(String[]z) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),t;
+		n-=k;
+		Deque<Integer>d=new LinkedList();
+		for(char c:s.next().toCharArray()) {
+			t=c-'0';
+			if(!d.isEmpty())
+				if(k>0&d.peekLast()<t) {
+					d.pollLast();
+					k--;
+				}
+			d.push(t);
+		}
+		for(;n-->0;)
+			System.out.print(d.pop());
+	}
 }
