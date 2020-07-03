8eefdcf726180da7015fd429628e9b0cc504f865
nn98 <jkllhgb@gmail.com>
2020-07-04 06:19:42 +0900

    2812

--- src/_Algorithm/Greedy/P2812.java
+++ src/_Algorithm/Greedy/P2812.java
@@ -1,9 +1,22 @@
 package _Algorithm.Greedy;
 
+import java.util.Scanner;
+
 public class P2812 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),i=0,j=-1,l=n-k;
+		char[]a=s.next().toCharArray(),b=new char[l];
+		for(;j<l;i++) {
+			if(j>=0) {
+				if(k>0&j!=0&b[j]<a[i]) {
+					k--;
+					j--;
+				}
+			}
+			b[++j]=a[i];
+		}
 	}
 
 }
