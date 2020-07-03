358720a47b4b37288f9a23c08aaae656274035d0
nn98 <jkllhgb@gmail.com>
2020-07-04 06:44:44 +0900

    2812

--- src/_Algorithm/Greedy/P2812_3.java
+++ src/_Algorithm/Greedy/P2812_3.java
@@ -7,15 +7,11 @@ import java.util.Scanner;
 public class P2812_3 {
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt();
+		int n=s.nextInt(),k=s.nextInt(),i=0;
 		Deque<Character>d=new LinkedList();
-		for(char c:s.next().toCharArray()) {
-			if(!d.isEmpty())
-				if(k>0&d.peekLast()<c) {
-					d.pollLast();
-					k--;
-				}
-			d.add(c);
+		char[]a=s.next().toCharArray();
+		for(;i<n;) {
+			
 		}
 		for(n=d.size()-k;n-->0;)
 			System.out.print(d.poll());
