1fd31701fb74279b3ceb1a41327da69e1cf68883
nn98 <jkllhgb@gmail.com>
2020-07-06 02:16:20 +0900

    9177

--- src/_Algorithm/Sorting/P9177.java
+++ src/_Algorithm/Sorting/P9177.java
@@ -3,8 +3,8 @@ package _Algorithm.Sorting;
 public class P9177 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		
 	}
 
 }
