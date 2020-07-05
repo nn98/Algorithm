5bb687aa45beb0b592d5e5ddacd0e28c740c1cca
nn98 <jkllhgb@gmail.com>
2020-07-06 01:19:09 +0900

    10825

--- src/_Algorithm/Sorting/P10825.java
+++ src/_Algorithm/Sorting/P10825.java
@@ -1,9 +1,39 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P10825 {
 
+	static String w;
+	static int x,y,z;
+	static class p{
+		String n;
+		int a,b,c;
+//		p(int a,int b,int c){
+//			a=a;
+//		}
+		p(){
+			n=w;
+			a=x;
+			b=y;
+			c=z;
+		}
+		@Override
+		public int compareTo(p p) {
+			if(a==p.a) {
+				if(b==p.b) {
+					if(c==p.c)
+						return n.compareTo(p.n);
+					return c-p.c;
+				}
+				return p.b-b;
+			}
+			return a=p.a;
+		}
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
 	}
 
 }
