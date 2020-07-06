a33e31792ba9ce8c7e5200d3932028ffa0a410d4
nn98 <jkllhgb@gmail.com>
2020-07-07 08:47:02 +0900

    7453

--- src/BaekJoon/P7453.java
+++ src/BaekJoon/P7453.java
@@ -1,9 +1,22 @@
 package BaekJoon;
 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
 public class P7453 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),x=0,i;
+		BigInteger a;
+		while(n-->0) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			a=new BigInteger(t.nextToken());
+			for(i=0;i++<3;a=a.add(new BigInteger(t.nextToken())));
+			x+=a.equals(BigInteger.ZERO)?1:0;
+		}
+		System.out.print(x);
 	}
 
 }
