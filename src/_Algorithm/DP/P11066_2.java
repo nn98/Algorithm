5158d5942df8870493774bdf3a2683f979334eda
nn98 <jkllhgb@gmail.com>
2020-07-10 20:42:36 +0900

    11066

--- src/_Algorithm/DP/P11066_2.java
+++ src/_Algorithm/DP/P11066_2.java
@@ -1,5 +1,27 @@
 package _Algorithm.DP;
 
+import java.io.*;
+import java.util.*;
+
 public class P11066_2 {
+
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),m,i,a;
+		while(n-->0) {
+			a=0;
+			m=Integer.parseInt(r.readLine());
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			PriorityQueue<Integer> p=new PriorityQueue();
+			while(m-->0)p.add(Integer.parseInt(t.nextToken()));
+			while(p.size()>1) {
+				System.out.println(p+" "+a);
+				a+=i=p.poll()+p.poll();
+				p.add(i);
+			}
+			System.out.println(p);
+			System.out.println(a);
+		}
+	}
 
 }
