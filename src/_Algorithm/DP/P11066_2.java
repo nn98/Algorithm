0fa2c2df08b7f4a340fe9d9875728f4f7a1d055d
nn98 <jkllhgb@gmail.com>
2020-07-16 22:42:16 +0900

    11066

--- src/_Algorithm/DP/P11066_2.java
+++ src/_Algorithm/DP/P11066_2.java
@@ -5,14 +5,21 @@ import java.util.*;
 
 public class P11066_2 {
 
-	static int u[],h[][],f[],n,m,i,a;
+	static int u[],h[][],f[],n,m,i,j,k;
 	static int o(int s,int e) {
 		return u[e]-s==0?0:u[s-1];
 	}
 	static int p() {
 //		내가봐도 양심없다
-		h=new int[m][m];
-		return 0;
+		u[0]=f[0];
+		for(i=0;++i<m;u[i]=u[i-1]+f[i]);
+		for(i=0;i<m-1;h[i][i+1]=f[i]+f[i+++1]);
+		for(i=1;++i<m;)
+			for(j=0;i+j<m;j++)
+				for(k=j;k<i+j;k++)
+					if(h[j][i+j]==0)h[j][i+j]=h[j][k]+h[k+1][i+j]+o(j,i+j);
+					else h[j][i+j]=Math.min(h[j][i+j],h[j][k]+h[k+1][i+j]+o(j,i+j));
+		return h[0][m-1];
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +28,8 @@ public class P11066_2 {
 		while(n-->0) {
 			m=Integer.parseInt(r.readLine());
 			f=new int[m];
+			u=new int[m];
+			h=new int[m][m];
 			StringTokenizer t=new StringTokenizer(r.readLine());
 			for(i=0;i<m;f[i++]=Integer.parseInt(t.nextToken()));
 			w.write(p()+"\n");
