2b8abaa07f59ab50e325a2a868fbc6989b27bb17
nn98 <jkllhgb@gmail.com>
2020-07-16 22:57:11 +0900

    11066

--- src/_Algorithm/DP/P11066_T.java
+++ src/_Algorithm/DP/P11066_T.java
@@ -18,15 +18,16 @@ public class P11066_T {
 		u[0]=f[0];
 		for(i=0;++i<m;u[i]=u[i-1]+f[i]);
 		for(i=0;i<m-1;h[i][i+1]=f[i]+f[i+++1]);
-		for(i=1;++i<m;) {
+		for(i=1;++i<m;)
 			for(j=0;i+j<m;j++)
-				for(k=j;k<i+j;k++)
+				for(k=j;k<i+j;k++) {
 					if(h[j][i+j]==0)h[j][i+j]=h[j][k]+h[k+1][i+j]+o(j,i+j);
 					else h[j][i+j]=Math.min(h[j][i+j],h[j][k]+h[k+1][i+j]+o(j,i+j));
+					for(int[]a:h)w.write(Arrays.toString(a)+"\n");
+					w.flush();
+				}
 
-			for(int[]a:h)w.write(Arrays.toString(a)+"\n");
-			w.flush();
-		}
+			
 //		¹¹ ÀÌµû±¸·Î ±«¶öÇØ
 		return h[0][m-1];
 	}
