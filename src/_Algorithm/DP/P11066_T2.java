1f8b0f81bb641512e119dff187a1efce8e4a0b48
nn98 <jkllhgb@gmail.com>
2020-07-18 23:44:07 +0900

    11066 Analysis

--- src/_Algorithm/DP/P11066_T2.java
+++ src/_Algorithm/DP/P11066_T2.java
@@ -7,15 +7,22 @@ public class P11066_T2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m,i,j,f[],u[],h[][];
+		int n=s.nextInt(),m,i,j,k,f[],u[],h[][];
 		while(n-->0) {
 			m=s.nextInt()+1;
 			f=new int[m];
 			u=new int[m];
 			h=new int[502][502];
 			for(i=0;++i<m;f[i]=s.nextInt(),u[i]=f[i]+u[i-1]);
-			System.out.println(Arrays.toString(f));
-			System.out.println(Arrays.toString(u));
+//			System.out.println(Arrays.toString(f));
+//			System.out.println(Arrays.toString(u));
+			for(i=1;++i<m;)
+				for(j=i;--j>0;) {
+					h[j][i]=Integer.MAX_VALUE;
+					for(k=j-1;k++<i;)h[j][i]=Math.min(h[j][i],h[j][k]+h[k+1][i]);
+					h[j][i]+=u[i]-u[j-1];
+				}
+			System.out.println(h[1][m-1]);
 		}
 	}
 
