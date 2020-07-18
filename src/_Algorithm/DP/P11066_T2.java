6bd8936a24969e0f9c558dfb056bcabb2ea8bd96
nn98 <jkllhgb@gmail.com>
2020-07-18 23:35:45 +0900

    11066 Analysis

--- src/_Algorithm/DP/P11066_T2.java
+++ src/_Algorithm/DP/P11066_T2.java
@@ -1,9 +1,22 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P11066_T2 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,i,j,f[],u[],h[][];
+		while(n-->0) {
+			m=s.nextInt()+1;
+			f=new int[m];
+			u=new int[m];
+			h=new int[502][502];
+			for(i=0;++i<m;f[i]=s.nextInt(),u[i]=f[i]+u[i-1]);
+			System.out.println(Arrays.toString(f));
+			System.out.println(Arrays.toString(u));
+		}
 	}
 
 }
