22596af4b3312df1dc73ecde69711cfcfdf46329
nn98 <jkllhgb@gmail.com>
2020-07-20 07:15:42 +0900

    15927

--- src/_Algorithm/String/P15927.java
+++ src/_Algorithm/String/P15927.java
@@ -3,7 +3,20 @@ package _Algorithm.String;
 public class P15927 {
 
 	public static void main(String[] args) {
-		
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		int r=-1,i=0,j,v,n=a.length,m=n;
+		while(n-->1) {
+			if(a[n-1]==a[n]) {
+				v=2;
+				i=n-1;
+				j=n;
+				while(--i>=0&&++j<m)
+					if(a[i]==a[j])v+=2;
+					else i=0;
+				r=r>v?r:v;
+			}
+		}
+		System.out.println(r);
 	}
 
 }
