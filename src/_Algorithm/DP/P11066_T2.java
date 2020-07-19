15d75ec26a921476c88c4fd73c1f15dc761c3ca2
nn98 <jkllhgb@gmail.com>
2020-07-20 06:40:39 +0900

    11066

--- src/_Algorithm/DP/P11066_T2.java
+++ src/_Algorithm/DP/P11066_T2.java
@@ -21,6 +21,7 @@ public class P11066_T2 {
 					h[j][i]=Integer.MAX_VALUE;
 					for(k=j-1;k++<i;)h[j][i]=Math.min(h[j][i],h[j][k]+h[k+1][i]);
 					h[j][i]+=u[i]-u[j-1];
+					System.out.println("i: "+i+" j: "+j);
 					for(int[]a:h)System.out.println(Arrays.toString(a));
 					System.out.println();
 				}
