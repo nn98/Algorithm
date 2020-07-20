724a0af7f993ff57fccf889551ad19106be533d7
nn98 <jkllhgb@gmail.com>
2020-07-20 20:05:00 +0900

    vote counter

--- src/Count.java
+++ src/Count.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Count {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a[]=new int[4],i;
+		while((i=s.nextInt())!=0) {
+			a[i]++;
+		}
+		System.out.println(a[1]+" "+a[2]+" "+a[3]);
 	}
 
 }
