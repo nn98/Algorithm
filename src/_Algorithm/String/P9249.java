3eb63ed52581d9fbdf1e88a3b36e91187b7ceff0
nn98 <jkllhgb@gmail.com>
2020-07-27 06:07:39 +0900

    9249

--- src/_Algorithm/String/P9249.java
+++ src/_Algorithm/String/P9249.java
@@ -1,9 +1,12 @@
 package _Algorithm.String;
 
+import java.util.Scanner;
+
 public class P9249 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		String a=s.next(),b=s.next();
 	}
 
 }
