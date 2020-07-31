894bf3bee54dd02490717af602ef3382987baf08
nn98 <jkllhgb@gmail.com>
2020-07-31 10:07:25 +0900

    3474

--- src/_Algorithm/Math/P3474.java
+++ src/_Algorithm/Math/P3474.java
@@ -1,9 +1,18 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P3474 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a,b,c;
+		while(n-->0) {
+			a=s.nextInt();
+			c=b=0;
+			while(++b<=a) {
+				
+			}
+		}
 	}
-
 }
