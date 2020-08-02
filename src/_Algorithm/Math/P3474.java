b73a5bf76b97ebbaa4d3a14df7e37b1522ed65b3
nn98 <jkllhgb@gmail.com>
2020-08-02 20:05:47 +0900

    3474

--- src/_Algorithm/Math/P3474.java
+++ src/_Algorithm/Math/P3474.java
@@ -3,7 +3,6 @@ package _Algorithm.Math;
 import java.util.Scanner;
 
 public class P3474 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a,b,c,d;
