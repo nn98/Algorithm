f852feae6c76409d4f7fe8cf38a6f427c37a0cb8
nn98 <jkllhgb@gmail.com>
2020-08-06 21:26:55 +0900

    3474

--- src/_Algorithm/Math/P3474.java
+++ src/_Algorithm/Math/P3474.java
@@ -1,19 +1,25 @@
 package _Algorithm.Math;
 
-import java.util.Scanner;
+import java.util.*;
 public class P3474{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a,b,c,d;
+		long n=s.nextInt(),a,b,c,d,e,f;
 		while(n-->0) {
 			a=s.nextInt();
 			c=0;
 			b=d=1;
 			while(++b<=a) {
 				d*=b;
-				c+=d%10==0?1:0;
+				System.out.print(d+" -> ");
+				e=1;
+				f=0;
+				while(d%(e*=10)==0)f++;
+//				c+=d%10==0?1:0;
+				c+=f;
 				while(d%10==0)d/=10;
 				if(d>10)d%=10;
+				System.out.println(d);
 			}
 			System.out.println(c);
 		}
