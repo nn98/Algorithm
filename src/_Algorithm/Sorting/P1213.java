4028dcf97d97e810485a8b20ee90ad7f06d8da65
nn98 <jkllhgb@gmail.com>
2020-08-06 22:28:33 +0900

    1213

--- src/_Algorithm/Sorting/P1213.java
+++ src/_Algorithm/Sorting/P1213.java
@@ -3,8 +3,15 @@ package _Algorithm.Sorting;
 public class P1213 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int s=26,a[]=new int[s],i=0,j,x=0;
+		for(char c:new java.util.Scanner(System.in).next().toCharArray())
+			a[c-'A']++;
+		for(;i<s;)
+			if(a[i++]%2==1)
+				if(x>0) {
+					System.out.print("I'm Sorry Hansoo");
+					return;
+				}else x++;
 	}
 
 }
