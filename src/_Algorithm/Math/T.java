acb6485671e834a6ebf88a23783d9de22f52aaa6
nn98 <jkllhgb@gmail.com>
2020-08-06 23:58:39 +0900

    1297 Test

---
+++ src/_Algorithm/Math/T.java
@@ -0,0 +1,16 @@
+package _Algorithm.Math;
+
+public class T {
+
+	public static void main(String[] args) {
+		int a=52*52,b=9,c=16;
+		for(;;) {
+			System.out.println(a+" "+b*b+" "+c*c);
+			if(a<b*b+c*c)break;
+			b++;
+			c++;
+		}
+		System.out.println(b+" "+c);
+	}
+
+}
