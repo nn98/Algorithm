19bfe130d243472ea490244539b56984b43ba8ff
nn98 <jkllhgb@gmail.com>
2020-08-06 22:16:22 +0900

    3474

--- src/_Algorithm/Math/P3474_G.java
+++ src/_Algorithm/Math/P3474_G.java
@@ -1,10 +1,23 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P3474_G {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m,t,i,f;
+		while(n-->0) {
+			m=s.nextInt();
+			t=f=0;
+			for(i=2;i<=m;i*=2)
+				t+=m/i;
+			for(i=5;i<=m;i*=5)
+				f+=m/i;
+			System.out.println(t+" "+f);
+			System.out.println(f<t?f:t);
+		}
+		
 	}
 
 }
