84a8aea4099b3493fc8bbf81f5622af9407cac26
nn98 <jkllhgb@gmail.com>
2020-08-06 22:35:43 +0900

    1213

--- src/_Algorithm/Sorting/P1213.java
+++ src/_Algorithm/Sorting/P1213.java
@@ -3,7 +3,8 @@ package _Algorithm.Sorting;
 public class P1213 {
 
 	public static void main(String[] args) {
-		int s=26,a[]=new int[s],i=0,j,x=0;
+		int s=26,a[]=new int[s],i=0,j,x=0,y=0;
+		StringBuffer f=new StringBuffer();
 		for(char c:new java.util.Scanner(System.in).next().toCharArray())
 			a[c-'A']++;
 		for(;i<s;)
@@ -11,7 +12,14 @@ public class P1213 {
 				if(x>0) {
 					System.out.print("I'm Sorry Hansoo");
 					return;
-				}else x++;
+				}else {
+					x++;
+					y=i-1;
+				}
+		for(i=0;i<s;i++)
+			if(a[i]%2==0)
+				for(j=a[i]/2;j-->0;f.append((char)('A'+i)));
+		System.out.print(""+f+(x==1?'A'+y:"")+f.reverse());
 	}
 
 }
