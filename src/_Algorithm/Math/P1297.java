9fd300d859d7bda173092aa692355f6498f85a8d
nn98 <jkllhgb@gmail.com>
2020-08-07 13:22:25 +0900

    1297

--- src/_Algorithm/Math/P1297.java
+++ src/_Algorithm/Math/P1297.java
@@ -6,7 +6,8 @@ public class P1297 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),z=(int) Math.sqrt(b*b+c*c);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
+		double z=Math.sqrt(b*b+c*c);
 		System.out.println((int)a*b/z);
 	}
 
