c80162f06d1d60886ecfd3ce9e675040789d630a
nn98 <jkllhgb@gmail.com>
2020-08-07 13:26:09 +0900

    1297

--- src/_Algorithm/Math/P1297.java
+++ src/_Algorithm/Math/P1297.java
@@ -1,14 +1,11 @@
 package _Algorithm.Math;
 
 import java.util.Scanner;
-
 public class P1297 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
 		double z=Math.sqrt(b*b+c*c);
 		System.out.print((int)(a*b/z)+" "+(int)(a*c/z));
 	}
-
 }
