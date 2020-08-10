61f1866f88108e2896a677df00f2a225dfb38b08
nn98 <jkllhgb@gmail.com>
2020-08-11 02:01:29 +0900

    1434

--- src/BaekJoon/P1434.java
+++ src/BaekJoon/P1434.java
@@ -3,7 +3,6 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class P1434 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0,j,a[]=new int[n];
