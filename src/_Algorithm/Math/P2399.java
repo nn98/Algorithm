63988c786f6e81d12ad12b9d2f62fd3bfdbbb725
nn98 <jkllhgb@gmail.com>
2020-08-14 03:25:21 +0900

    2399

--- src/_Algorithm/Math/P2399.java
+++ src/_Algorithm/Math/P2399.java
@@ -6,11 +6,12 @@ public class P2399 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,j,r=0,a[]=new int[n];
+		int n=s.nextInt(),i=0,j,a[]=new int[n];
+		long r=0;
 		while(i<n) {
 			a[i]=s.nextInt();
 			for(j=0;j<i;j++)
-				r+=Math.abs(a[j]-a[i]);
+				r+=Math.abs(a[i]-a[j]);
 			i++;
 		}
 		System.out.print(r*2);
