631ffdab34f1abc25b05ad49384889df0abc916f
nn98 <jkllhgb@gmail.com>
2020-08-14 00:51:29 +0900

    2399

--- src/_Algorithm/Math/P2399.java
+++ src/_Algorithm/Math/P2399.java
@@ -1,9 +1,19 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P2399 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j,r=0,a[]=new int[n];
+		while(i<n) {
+			a[i]=s.nextInt();
+			for(j=0;j<i;j++)
+				r+=Math.abs(a[j]-a[i]);
+			i++;
+		}
+		System.out.print(r);
 	}
 
 }
