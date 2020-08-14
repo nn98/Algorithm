b587b76b5ba93617e8f61ff2346dec46d73c0655
nn98 <jkllhgb@gmail.com>
2020-08-15 00:32:04 +0900

    2738

--- src/_Algorithm/Math/P2738.java
+++ src/_Algorithm/Math/P2738.java
@@ -1,9 +1,21 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P2738 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,j,a[][]=new int[n][m];
+		for(;i<n;i++)
+			for(j=0;j<m;)
+				a[i][j++]=s.nextInt();
+		for(i=0;i<n;i++)
+			for(j=0;j<m;)
+				a[i][j++]+=s.nextInt();
+		for(;i<n;i++)
+			for(j=0;j<m;)
+				System.out.print(a[i][j++]+j==m-1?"\n":" ");
 	}
 
 }
