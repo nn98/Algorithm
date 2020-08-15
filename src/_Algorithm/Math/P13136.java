5189e84c4bbacda9e8dae1e4d3348bec2f1c54c9
nn98 <jkllhgb@gmail.com>
2020-08-16 06:05:33 +0900

    13136

--- src/_Algorithm/Math/P13136.java
+++ src/_Algorithm/Math/P13136.java
@@ -1,9 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P13136 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		double n=s.nextInt(),m=s.nextInt(),u=s.nextInt();
+		System.out.println(Math.ceil(n/u)*Math.ceil(m/u));
 	}
 
 }
