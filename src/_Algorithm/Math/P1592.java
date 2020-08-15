780820d18ea070c5c7ec11c288389b86b40c654e
nn98 <jkllhgb@gmail.com>
2020-08-16 06:22:28 +0900

    1592

--- src/_Algorithm/Math/P1592.java
+++ src/_Algorithm/Math/P1592.java
@@ -8,14 +8,12 @@ public class P1592 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),u=s.nextInt(),a[]=new int[n],r=-1,i=0;
 		while(true) {
-			System.out.println(i);
 			a[i]++;
 			r++;
 			if(a[i]==m) {
 				System.out.println(r);
 				return;
 			}
-			System.out.println("\t"+a[i]);
 			i+=u*(a[i]%2==0?-1:1);
 			if(i<0)i=n+i;
 			else if(i>=n)i-=n;
