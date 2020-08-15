455b0f1aac9420d764dbb610633c56de5161ec70
nn98 <jkllhgb@gmail.com>
2020-08-16 06:15:24 +0900

    1592

--- src/_Algorithm/Math/P1592.java
+++ src/_Algorithm/Math/P1592.java
@@ -1,9 +1,23 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P1592 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),u=s.nextInt(),a[]=new int[n],r=0,i=0;
+		while(true) {
+			a[i]++;
+			r++;
+			if(a[i]==m) {
+				System.out.println(r);
+				return;
+			}
+			i+=u*(i%2==0?-1:1);
+			if(i<0)i=n-i;
+			if(i>=n)i-=n;
+		}
 	}
 
 }
