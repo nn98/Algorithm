fcb8098435ea577ae34410b5d424df1231cfb0e4
nn98 <jkllhgb@gmail.com>
2020-08-17 04:09:21 +0900

    15501

--- src/_Algorithm/Math/P15501.java
+++ src/_Algorithm/Math/P15501.java
@@ -6,14 +6,17 @@ public class P15501 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[n],j=0,r=0,x=0,y=0;
+		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[n],j=0,r=0,x=0,y=0,X=0,Y=0;
 		for(;i<n;) {
 			a[i]=s.nextInt();
-			if(a[i++]==1)x=i;
+			if(a[i++]==1)X=x=i;
 		}
 		for(i=0;i<n;) {
 			b[i]=s.nextInt();
-			if(b[i++]==1)y=i;
+			if(b[i++]==1) {
+				y=i;
+				Y=i-2;
+			}
 		}
 		System.out.println(x+" "+y);
 		for(i=0;i<n;i++) {
@@ -25,6 +28,7 @@ public class P15501 {
 			x++;
 			y++;
 		}
+		System.out.println(X+" "+Y);
 		System.out.println("Straight: "+r);
 		if(r==n-1) {
 			System.out.println("good puzzle");
