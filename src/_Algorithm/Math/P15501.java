aa3da77461fc4ffe0031e1198ae7598332425309
nn98 <jkllhgb@gmail.com>
2020-08-17 04:06:17 +0900

    15501

--- src/_Algorithm/Math/P15501.java
+++ src/_Algorithm/Math/P15501.java
@@ -1,9 +1,17 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P15501 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[n],j=0,r=0,x=0;
+		for(;i<n;) {
+			a[i]=s.nextInt();
+			if(a[i++]==1)x=i-1;
+		}
+		System.out.println(x);
 	}
 
 }
