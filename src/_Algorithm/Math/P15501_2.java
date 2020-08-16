79142d8a52f34118fd95f1896f07d0f638e6ae26
nn98 <jkllhgb@gmail.com>
2020-08-17 04:15:31 +0900

    15501

--- src/_Algorithm/Math/P15501_2.java
+++ src/_Algorithm/Math/P15501_2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P15501_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[n],j=0,r=0,x=0,y=0,X=0,Y=0;
+		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[n],r=0,x=0,y=0,X=0,Y=0;
 		for(;i<n;) {
 			a[i]=s.nextInt();
 			if(a[i++]==1)X=x=i;
@@ -17,35 +17,28 @@ public class P15501_2 {
 				Y=i-2;
 			}
 		}
-		System.out.println(x+" "+y);
 		for(i=0;++i<n;) {
 			if(x>=n)x-=n;
 			if(y>=n)y-=n;
-//			r+=a[x]==b[y]?1:0;
 			if(a[x]==b[y])r++;
 			else break;
 			x++;
 			y++;
 		}
-		System.out.println("Straight: "+r);
 		if(r==n-1) {
-			System.out.println("good puzzle");
+			System.out.print("good puzzle");
 			return;
 		}
 		r=0;
-		System.out.println(X+" "+Y);
 		for(i=0;++i<n;) {
 			if(X>=n)X-=n;
 			if(Y<0)Y+=n;
-//			r+=a[x]==b[y]?1:0;
 			if(a[X]==b[Y])r++;
 			else break;
 			X++;
 			Y--;
 		}
-		System.out.println("Back: "+r);
 		System.out.println(r==n-1?"good puzzle":"bad puzzle");
-//		System.out.println(x);
 	}
 
 }
