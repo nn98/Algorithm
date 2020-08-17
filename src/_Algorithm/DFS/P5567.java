ebcc69c886d24fd67ac6a39f9c0d2cf210230e35
nn98 <jkllhgb@gmail.com>
2020-08-18 01:06:47 +0900

    5567

--- src/_Algorithm/DFS/P5567.java
+++ src/_Algorithm/DFS/P5567.java
@@ -1,9 +1,25 @@
 package _Algorithm.DFS;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P5567 {
 
-	public static void main(String[] args) {
-		
+	static int n,m,a[][],h[],i,j;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		m=Integer.parseInt(r.readLine());
+		a=new int[n][n];
+		h=new int[n];
+		while(m-->0) {
+			char[]b=r.readLine().toCharArray();
+			i=b[0]-'0';
+			j=b[2]-'0';
+		}
 	}
 
 }
