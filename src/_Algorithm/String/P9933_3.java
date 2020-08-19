8d3cb3d2917795a6b9f215d40146bada9b60f9dd
nn98 <jkllhgb@gmail.com>
2020-08-19 23:03:56 +0900

    9933

--- src/_Algorithm/String/P9933_3.java
+++ src/_Algorithm/String/P9933_3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P9933_3 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=n,j,l,r;
+		int n=s.nextInt(),i=n,j,l,r,k;
 		s.nextLine();
 		StringBuffer a[]=new StringBuffer[n],b;
 		while(i-->0)a[i]=new StringBuffer(s.nextLine());
@@ -13,9 +13,15 @@ public class P9933_3 {
 			l=a[i].length()-1;
 			for(j=0;j<=i;j++) {
 				if(a[i].length()==a[j].length()) {
-					b=a[j].reverse();
-					if(a[i].toString().equals(b.toString()))
-						System.out.println(l+1+" "+a[i].charAt(l/2));
+					if(a[i].charAt(l)==a[j].charAt(0)) {
+						r=0;
+						for(k=0;k<l/2&&r==0;k++)
+							if(a[i].charAt(k)!=a[j].charAt(l-k))
+								r++;
+						if(r==0) {
+							System.out.print(l+1+" "+a[i].charAt(l/2));
+						}
+					}
 				}
 			}
 		}
