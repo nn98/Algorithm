c647d92f77618b8d6ab2a0d6a04a8d6176a9e078
nn98 <jkllhgb@gmail.com>
2020-08-19 22:37:53 +0900

    9933

--- src/_Algorithm/String/P9933.java
+++ src/_Algorithm/String/P9933.java
@@ -6,11 +6,21 @@ public class P9933 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,j;
+		int n=s.nextInt(),i=0,j,l;
 		s.nextLine();
 		StringBuffer[]a=new StringBuffer[n];
 		while(n-->0) {
-			a[i++]=new StringBuffer(s.nextLine());
+			a[i]=new StringBuffer(s.nextLine());
+			if(i>0) {
+				for(j=0;j<i;j++) {
+					l=a[i].length();
+					if(a[j].charAt(0)==a[i].charAt(l-1))
+						if(a[j].reverse().toString().equals(a[i].toString())) {
+							System.out.print(l+" "+a[j].charAt(l/2));
+							return;
+						}
+				}
+			}
 		}
 	}
 
