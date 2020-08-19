b9e0bd4aa11c50e83d566f8cdf6fda2e1c49bf0d
nn98 <jkllhgb@gmail.com>
2020-08-19 22:37:52 +0900

    9933

--- src/_Algorithm/String/P9933.java
+++ src/_Algorithm/String/P9933.java
@@ -1,9 +1,17 @@
 package _Algorithm.String;
 
+import java.util.Scanner;
+
 public class P9933 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j;
+		s.nextLine();
+		StringBuffer[]a=new StringBuffer[n];
+		while(n-->0) {
+			a[i++]=new StringBuffer(s.nextLine());
+		}
 	}
 
 }
