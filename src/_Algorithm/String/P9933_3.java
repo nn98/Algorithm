34d4b8c39b68eded5de437420bf8f99774ed900a
nn98 <jkllhgb@gmail.com>
2020-08-20 02:12:49 +0900

    9933

--- src/_Algorithm/String/P9933_3.java
+++ src/_Algorithm/String/P9933_3.java
@@ -11,19 +11,16 @@ public class P9933_3 {
 		while(i-->0)a[i]=new StringBuffer(s.nextLine());
 		for(i=0;i<n;i++) {
 			l=a[i].length()-1;
-			for(j=0;j<=i;j++) {
-				if(a[i].length()==a[j].length()) {
+			for(j=0;j<=i;j++) 
+				if(a[i].length()==a[j].length())
 					if(a[i].charAt(l)==a[j].charAt(0)) {
 						r=0;
 						for(k=0;k<l/2&&r==0;k++)
 							if(a[i].charAt(k)!=a[j].charAt(l-k))
 								r++;
-						if(r==0) {
+						if(r==0)
 							System.out.print(l+1+" "+a[i].charAt(l/2));
-						}
 					}
-				}
-			}
 		}
 	}
 }
