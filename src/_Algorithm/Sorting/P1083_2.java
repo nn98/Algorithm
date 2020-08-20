c7f0c7b8aa3abe117f323af15e976f14aa5abda9
nn98 <jkllhgb@gmail.com>
2020-08-21 01:55:40 +0900

    1083

--- src/_Algorithm/Sorting/P1083_2.java
+++ src/_Algorithm/Sorting/P1083_2.java
@@ -1,15 +1,15 @@
 package _Algorithm.Sorting;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class P1083_2 {
 	static int n,a[],i,b,c,j,t;
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		n=s.nextInt();
-		a=new int[n];
-		for(;i<n;a[i++]=s.nextInt());
-		for(i=s.nextInt();i>0;) {
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,b,c,j,t;
+		for(String s:r.readLine().split(" "))a[i++]=Integer.parseInt(s);
+		for(i=Integer.parseInt(r.readLine());i>0;) {
 			for(b=0;b<n-2&&i>0;b++) {
 				j=0;
 				for(c=b;c<n-1&&i>0;c++)
@@ -23,7 +23,8 @@ public class P1083_2 {
 				if(j==0)i=0;
 			}
 		}
-		for(int m:a)System.out.print(m+" ");
+		for(int m:a)w.write(m+" ");
+		w.flush();
 	}
 
 }
