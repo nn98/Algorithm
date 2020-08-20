1055ea65e9f289558110f06c45e84613e9bb6246
nn98 <jkllhgb@gmail.com>
2020-08-21 01:39:14 +0900

    1655

--- src/_Algorithm/Sorting/P1655.java
+++ src/_Algorithm/Sorting/P1655.java
@@ -6,7 +6,7 @@ public class P1655 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n]
+		int n=s.nextInt(),a[]=new int[n];
 	}
 
 }
