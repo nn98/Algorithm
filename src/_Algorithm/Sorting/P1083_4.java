d823c560eac3657c47389fa147d36af835d45e87
nn98 <jkllhgb@gmail.com>
2020-08-22 03:22:29 +0900

    1083

--- src/_Algorithm/Sorting/P1083_4.java
+++ src/_Algorithm/Sorting/P1083_4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P1083_4 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],i=0,j,r=-1,t,m,u,T;
+		int n=s.nextInt(),a[]=new int[n],i=0,j,r=-1,t,m,u,T,M;
 		for(;i<n;a[i++]=s.nextInt());
 		i=s.nextInt();
 		while(i>0) {
@@ -20,12 +20,14 @@ public class P1083_4 {
 				continue;
 			}
 			m=a[T=t];
+			M=t-r;
 			u=a[r];
 //			for(j=0;j<=t;a[j+1]=a[j++]);
+			if(t==n-1)t--;
 			for(;t>=r;a[t]=a[t--+1]);
 			a[r]=m;
 			a[T]=u;
-			i-=T;
+			i-=M;
 		}
 		for(int b:a)System.out.print(b+" ");
 	}
