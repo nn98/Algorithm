cf71882721914ffe6dc60aa27c5377ec0ed6d5eb
nn98 <jkllhgb@gmail.com>
2020-08-22 03:01:33 +0900

    1083

--- src/_Algorithm/Sorting/P1083_4.java
+++ src/_Algorithm/Sorting/P1083_4.java
@@ -1,5 +1,32 @@
 package _Algorithm.Sorting;
 
-public class P1083_4 {
+import java.util.Scanner;
 
+public class P1083_4 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0,j,r=-1,t,m,u,T;
+		for(;i<n;a[i++]=s.nextInt());
+		i=s.nextInt();
+		while(i>0) {
+			r++;
+			t=r;
+			for(j=t+1;j<n;j++)
+				if(a[j]>a[t])
+					if(j-r<=i)
+						t=j;
+			if(t==r) {
+				if(r==n)break;
+				continue;
+			}
+			m=a[T=t];
+			u=a[r];
+//			for(j=0;j<=t;a[j+1]=a[j++]);
+			for(;t>=r;a[t]=a[t--+1]);
+			a[r]=m;
+			a[T]=u;
+			i-=T;
+		}
+		for(int b:a)System.out.print(b+" ");
+	}
 }
