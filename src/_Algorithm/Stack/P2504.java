d082b8fb02769e2dc142d075ccc1d0b8507f5446
nn98 <jkllhgb@gmail.com>
2020-08-24 07:48:20 +0900

    2504

--- src/_Algorithm/Stack/P2504.java
+++ src/_Algorithm/Stack/P2504.java
@@ -5,34 +5,40 @@ import java.util.Stack;
 public class P2504 {
 
 	public static void main(String[] args) {
-		int c=0,d=0,r=0,v=0,f=0;
+		int c=0,d=0,r=0,v=0,p=0,f=0;
 		Stack<Character>s=new Stack();
 		for(char a:new java.util.Scanner(System.in).next().toCharArray()) {
 			switch(a) {
 			case '(':
+				if(f>0) {
+					p+=f;
+					f=0;
+				}
 				c++;
 				break;
 			case '[':
+				if(f>0) {
+					p+=f;
+					f=0;
+				}
 				d++;
 				break;
 			case ')':
 				c--;
 				if(f==0) f=2;
 				else f*=2;
-				v+=f;
 				if(c==0&d==0) {
-					r+=v;
-					v=0;
+					v+=f;
+					f=0;
 				}
 				break;
 			case ']':
 				d--;
 				if(f==0) f=3;
 				else f*=3;
-				v+=f;
 				if(c==0&d==0) {
-					r+=v;
-					v=0;
+					v+=f;
+					f=0;
 				}
 				break;
 			}
