763f90da001cae8e3f0c5fac5108e544b719a133
nn98 <jkllhgb@gmail.com>
2020-08-24 09:05:36 +0900

    2504

--- src/_Algorithm/Stack/P2504_2.java
+++ src/_Algorithm/Stack/P2504_2.java
@@ -1,10 +1,31 @@
 package _Algorithm.Stack;
 
+import java.util.Stack;
+
 public class P2504_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char b;
+		int c=0,d=0,r=0,v=0,p=0,f=0;
+		Stack<Character>s=new Stack();
+		for(char a:new java.util.Scanner(System.in).next().toCharArray()) {
+			switch(a) {
+			case '(':
+				s.push(a);
+				break;
+			case '[':
+				s.push(a);
+				break;
+			case ')':
+				b=s.pop();
+				break;
+			case ']':
+				b=s.pop();
+				break;
+			}
+			System.out.println(a+" "+v);
+		}
+		System.out.print(r+v);
 	}
 
 }
