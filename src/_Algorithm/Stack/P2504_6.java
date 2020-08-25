09663cbb04ecec3b2ae0fb68309fc101bd46d70c
nn98 <jkllhgb@gmail.com>
2020-08-25 19:55:44 +0900

    2504

--- src/_Algorithm/Stack/P2504_6.java
+++ src/_Algorithm/Stack/P2504_6.java
@@ -1,5 +1,60 @@
 package _Algorithm.Stack;
 
+import java.util.Arrays;
+
 public class P2504_6 {
+	public static void main(String[] args) {
+		String[]a=new String[30];
+		int i=0,r=0,v=0;
+		for(String b:new java.util.Scanner(System.in).next().split("")) {
+			try {
+				switch(b) {
+				case "(":
+					a[i]=b;
+					break;
+				case "[":
+					a[i]=b;
+					break;
+				case ")":
+					if(a[i-1].equals("[")) {
+						System.out.print(0);
+						return;						
+					}else if(a[i-1].equals("(")) {
+						a[--i]="2";			
+					}else {
+						System.out.println("from "+i);
+						while(!(a[--i].equals("(")))
+							v+=Integer.parseInt(a[i]);
+						System.out.println("to "+i);
+						System.out.println(2+" * "+v);
+						a[i]=""+v*2;
+						v=0;
+					}
+					break;
+				case "]":
+					if(a[i-1].equals("(")) {
+						System.out.print(0);
+						return;
+					}else if(a[i-1].equals("[")) {
+						a[--i]="3";
+					}else {
+						while(!(a[--i].equals("[")))
+							v+=Integer.parseInt(a[i]);
+						System.out.println(3+" * "+v);
+						a[i]=""+v*3;
+						v=0;
+					}
+					break;
+				}
+				i++;
+			}catch(Exception e) {
+				System.out.print(0);
+				return;
+			}
+			System.out.println(Arrays.toString(a));
+		}
+		System.out.println(i);
+		System.out.println(Arrays.toString(a));
+	}
 
 }
