6005264ecf2ff12a501e3684fb828eb6b7a9640d
nn98 <jkllhgb@gmail.com>
2020-08-25 16:20:19 +0900

    2504

--- src/_Algorithm/Stack/P2504_2.java
+++ src/_Algorithm/Stack/P2504_2.java
@@ -6,7 +6,7 @@ public class P2504_2 {
 
 	public static void main(String[] args) {
 		char b;
-		int c=0,d=0,r=0,v=0,p=0,f=0;
+		int c=0,d=0,r=0;
 		Stack<Character>s=new Stack();
 		for(char a:new java.util.Scanner(System.in).next().toCharArray()) {
 			switch(a) {
@@ -18,14 +18,26 @@ public class P2504_2 {
 				break;
 			case ')':
 				b=s.pop();
+				if(b=='[') {
+					System.out.print(0);
+					return;
+				}
+				if(c==0)c=2;
+				else {
+					if(d!=0)c+=d;
+					c*=2;
+				}
+				if(s.isEmpty())
 				break;
 			case ']':
 				b=s.pop();
+				if(b=='(') {
+					System.out.print(0);
+					return;
+				}
 				break;
 			}
-			System.out.println(a+" "+v);
 		}
-		System.out.print(r+v);
 	}
 
 }
