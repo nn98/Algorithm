5fda642ceca5a5311c57a9291d0b8cefd0c338be
nn98 <jkllhgb@gmail.com>
2020-08-25 17:29:50 +0900

    2504

--- src/_Algorithm/Stack/P2504_4.java
+++ src/_Algorithm/Stack/P2504_4.java
@@ -5,48 +5,44 @@ import java.util.Stack;
 public class P2504_4 {
 
 	public static void main(String[] args){
-		Stack<Character> s=new Stack();
+		Stack<Integer> s=new Stack();
 		int b,r=0;
 		for(char a:new java.util.Scanner(System.in).next().toCharArray()) {
-			try {
 			switch(a) {
 			case '(':
-				s.push(a);
+				s.push((int) a);
 				break;
 			case '[':
-				s.push(a);
+				s.push((int) a);
 				break;
 			case ')':
-				b=(char)s.pop();
+				b=s.pop();
 				if(b=='[') {
 					System.out.print(0);
 					break;
 				}
-				b=(char)s.peek();
+				b=s.peek();
 				if(b=='('||b=='[')
-					s.push((char) 2);
+					s.push(2);
 				else
-					s.push((char) ((int)s.pop()*2));
+					s.push(s.pop()*2);
 				break;
 			case ']':
-				b=(char)s.pop();
+				b=s.pop();
 				if(b=='[') {
 					System.out.print(0);
 					break;
 				}
-				b=(char)s.peek();
+				b=s.peek();
 				if(b=='('||b=='[')
-					s.push((char) 3);
+					s.push(3);
 				else
-					s.push((char) ((int)s.pop()*3));
+					s.push((int)s.pop()*3);
 				break;
-			}
-			}catch(Exception e) {
-				System.out.print(0);
+
 			}
 			System.out.println(s.peek());
 		}
 		System.out.print((int)s.pop());
 	}
-
 }
