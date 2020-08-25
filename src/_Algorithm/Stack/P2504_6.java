b45d4edf9fa1e250bca5f82196138af807d5450e
nn98 <jkllhgb@gmail.com>
2020-08-25 19:58:38 +0900

    2504

--- src/_Algorithm/Stack/P2504_6.java
+++ src/_Algorithm/Stack/P2504_6.java
@@ -1,10 +1,8 @@
 package _Algorithm.Stack;
 
-import java.util.Arrays;
-
 public class P2504_6 {
 	public static void main(String[] args) {
-		String[]a=new String[30];
+		String[]a=new String[32];
 		int i=0,r=0,v=0;
 		for(String b:new java.util.Scanner(System.in).next().split("")) {
 			try {
