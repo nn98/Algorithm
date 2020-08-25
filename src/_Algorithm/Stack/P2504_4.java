4fa2348bc9ee4e7b8f85f774a5ee8f833536ed0b
nn98 <jkllhgb@gmail.com>
2020-08-25 16:51:18 +0900

    2504

--- src/_Algorithm/Stack/P2504_4.java
+++ src/_Algorithm/Stack/P2504_4.java
@@ -1,9 +1,40 @@
 package _Algorithm.Stack;
 
+import java.util.Stack;
+
 public class P2504_4 {
 
 	public static void main(String[] args) {
-		
+		System.out.println((int)'(');
+		System.out.println((int)')');
+		System.out.println((int)'[');
+		System.out.println((int)']');
+		Stack<Character>s=new Stack();
+		int b;
+		for(char a:new java.util.Scanner(System.in).next().toCharArray()) {
+			switch(a) {
+			case '(':
+				s.push(a);
+				break;
+			case '[':
+				s.push(a);
+				break;
+			case ')':
+				b=s.pop();
+				if(b=='[') {
+					System.out.print(0);
+					break;
+				}
+				break;
+			case ']':
+				b=s.pop();
+				if(b=='(') {
+					System.out.print(0);
+					break;
+				}
+				break;
+			}
+		}
 	}
 
 }
