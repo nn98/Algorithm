728ca240437dff354aaf943afe6a43313141fbc3
nn98 <jkllhgb@gmail.com>
2020-08-27 00:46:28 +0900

    1254

--- src/_Algorithm/String/P1254.java
+++ src/_Algorithm/String/P1254.java
@@ -3,7 +3,22 @@ package _Algorithm.String;
 public class P1254 {
 
 	public static void main(String[] args) {
-		
+		char[]a=new char[2000];
+		int i,j,l=0,c;
+		for(char b:new java.util.Scanner(System.in).next().toCharArray())
+			a[l++]=b;
+		l--;
+		while(true) {
+			c=i=0;
+			j=l;
+			while(i<=j&c==0)
+				if(a[i++]!=a[j--])
+					c++;
+			if(c==0) {
+				System.out.print(a);
+				return;
+			}
+		}
 	}
 
 }
