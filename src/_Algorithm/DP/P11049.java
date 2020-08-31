bffa3cbb07aa33bf56665776125bf6653250032a
nn98 <jkllhgb@gmail.com>
2020-09-01 00:01:21 +0900

    11049

--- src/_Algorithm/DP/P11049.java
+++ src/_Algorithm/DP/P11049.java
@@ -2,7 +2,7 @@ package _Algorithm.DP;
 
 import java.util.Scanner;
 public class P11049 {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt();
 	}
