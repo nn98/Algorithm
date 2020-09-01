c780096f8760f7f31c627dc709e4a0b8e8b7430c
nn98 <jkllhgb@gmail.com>
2020-09-02 01:15:03 +0900

    2493

--- src/_Algorithm/Stack/P2493_4.java
+++ src/_Algorithm/Stack/P2493_4.java
@@ -1,9 +1,15 @@
 package _Algorithm.Stack;
 
+import java.io.*;
+
 public class P2493_4 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),i=0,j,a[]=new int[n];
+		for(String s:r.readLine().split(" ")) {
+//			Breader 쓰면 에러가 나면 어카누
+		}
 	}
 
 }
