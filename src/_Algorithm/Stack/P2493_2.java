4752f4c3346461f5265fce43a6ee40db431db1c3
nn98 <jkllhgb@gmail.com>
2020-09-02 00:47:38 +0900

    2493

--- src/_Algorithm/Stack/P2493_2.java
+++ src/_Algorithm/Stack/P2493_2.java
@@ -1,12 +1,11 @@
 package _Algorithm.Stack;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class P2493_2 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(r.readLine()),a[]=new int[n],i=0,j,k;
 		for(String s:r.readLine().split(" "))a[i++]=Integer.parseInt(s);
 		for(i=n;i-->0;) {
@@ -20,7 +19,8 @@ public class P2493_2 {
 			if(k==0)a[i]=0;
 		}
 //		System.out.println(Arrays.toString(b));
-		for(int c:a)System.out.print(c+" ");
+		for(int c:a)w.write(c+" ");
+		w.flush();
 	}
 
 }
