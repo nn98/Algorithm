213de165598176f43665bc7aa97c2bd9411dab53
nn98 <jkllhgb@gmail.com>
2020-09-02 00:37:44 +0900

    2493

--- src/_Algorithm/Stack/P2493_2.java
+++ src/_Algorithm/Stack/P2493_2.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 public class P2493_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],i=0,j,k;
+		int n=s.nextInt(),a[]=new int[n],i=0,j,k;
 		for(;i<n;a[i++]=s.nextInt());
 		for(i=n;i-->0;) {
 			k=0;
 			for(j=i;j-->0;)
 				if(a[i]<=a[j]) {
-					b[i]=j+1;
+					a[i]=j+1;
 					k=1;
 					break;
 				}
+			a[i]=0;
 		}
 //		System.out.println(Arrays.toString(b));
-		for(int c:b)System.out.print(c+" ");
+		for(int c:a)System.out.print(c+" ");
 	}
 
 }
