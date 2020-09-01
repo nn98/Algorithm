f93102e25229993c9ef8eec4341bbde0385d4e43
nn98 <jkllhgb@gmail.com>
2020-09-02 01:31:12 +0900

    2493

--- src/_Algorithm/Stack/P2493_5.java
+++ src/_Algorithm/Stack/P2493_5.java
@@ -1,9 +1,30 @@
 package _Algorithm.Stack;
 
+import java.util.*;
+
 public class P2493_5 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j,k=1;
+		Stack<Integer>t=new Stack(),x=new Stack();
+		for(;i++<n;) {
+			j=s.nextInt();
+			while(!t.isEmpty())
+				if(t.peek()<j) {
+					t.pop();
+					x.pop();
+				}
+			if(t.isEmpty()) {
+				t.push(j);
+				System.out.print(0+" ");
+				x.push(i);
+			}else {
+				t.push(j);
+				System.out.print(x.peek()+1+" ");
+				x.push(i);
+			}
+		}
 	}
 
 }
