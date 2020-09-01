9d4c5344366c990af8126c3c101a3e261cf7ad13
nn98 <jkllhgb@gmail.com>
2020-09-02 00:23:27 +0900

    11049

--- src/_Algorithm/DP/P11049.java
+++ src/_Algorithm/DP/P11049.java
@@ -5,6 +5,6 @@ public class P11049 {
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt();
-		System.out.println(Math.min(a*b*d+a*d*f,b*d*f+a*c*f));
+		System.out.println(Math.min(a*b*d+a*d*f,c*d*f+a*b*f));
 	}
 }
