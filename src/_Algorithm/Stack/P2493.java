418dbfb6a7df881485e80669eb7e93cd2441e4b5
nn98 <jkllhgb@gmail.com>
2020-09-02 00:30:08 +0900

    2493

--- src/_Algorithm/Stack/P2493.java
+++ src/_Algorithm/Stack/P2493.java
@@ -1,10 +1,25 @@
 package _Algorithm.Stack;
 
+import java.util.Scanner;
+
 public class P2493 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],b[]=new int[n],i=0,j,k;
+		StringBuffer f=new StringBuffer();
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=n;i-->0;) {
+			k=0;
+			for(j=i;j-->0;)
+				if(a[i]<=a[j]) {
+					f.append(j+" ");
+					k=1;
+					break;
+				}
+			if(k==0)f.append(k+" ");
+		}
+		System.out.print(f.reverse());
 	}
 
 }
