3d410a2848a07dc61bd2654d9d4033683a062b12
nn98 <jkllhgb@gmail.com>
2020-09-02 01:16:12 +0900

    2493

--- src/_Algorithm/Stack/P2493_4.java
+++ src/_Algorithm/Stack/P2493_4.java
@@ -6,7 +6,7 @@ public class P2493_4 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),i=0,j,a[]=new int[n];
+		int n=Integer.parseInt(r.readLine()),i=0,j;
 		for(String s:r.readLine().split(" ")) {
 //			Breader 쓰면 에러가 나면 어카누
 		}
