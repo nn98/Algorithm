1c5bf17e04ca92dd34ece69d04fa4aacdb815fd9
nn98 <jkllhgb@gmail.com>
2020-09-02 11:53:51 +0900

    2493

--- src/_Algorithm/Stack/P2493_5.java
+++ src/_Algorithm/Stack/P2493_5.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class P2493_5 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 //		Scanner s=new Scanner(System.in);
 		int n=Integer.parseInt(r.readLine()),i=0,j;
