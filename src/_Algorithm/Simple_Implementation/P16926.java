f3df374ec95c31ea039261736a40871a880ebd94
nn98 <jkllhgb@gmail.com>
2020-09-04 01:52:00 +0900

    16926

--- src/_Algorithm/Simple_Implementation/P16926.java
+++ src/_Algorithm/Simple_Implementation/P16926.java
@@ -1,6 +1,13 @@
 package _Algorithm.Simple_Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P16926 {
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	}
 }
