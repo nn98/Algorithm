c9668b41edc2c3edb12773af38120e39cc7b10c7
nn98 <jkllhgb@gmail.com>
2020-09-04 23:36:35 +0900

    13023

--- src/_Algorithm/DFS/P13023.java
+++ src/_Algorithm/DFS/P13023.java
@@ -1,9 +1,23 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class P13023 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),m=Integer.parseInt(t.nextToken()),a[]=new int[n],b,c,d=0;
+		while(m-->0) {
+			t=new StringTokenizer(r.readLine());
+			b=Integer.parseInt(t.nextToken());
+			c=Integer.parseInt(t.nextToken());
+			a[b]=a[c]=Math.max(a[b],a[c])+1;
+			d=d>a[b]?d:a[b];
+		}
+		System.out.println(d>4?1:0);
 	}
 
 }
