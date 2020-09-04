7be3d4c16f00ef17069d93d416b1a96e7baaf973
nn98 <jkllhgb@gmail.com>
2020-09-04 22:25:29 +0900

    16926

--- src/_Algorithm/Simple_Implementation/P16926.java
+++ src/_Algorithm/Simple_Implementation/P16926.java
@@ -1,12 +1,8 @@
 package _Algorithm.Simple_Implementation;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-public class P16926 {
-	public static void main(String[] args) {
+import java.io.*;
+public class P16926{
+	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	}
