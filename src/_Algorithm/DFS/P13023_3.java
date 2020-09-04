cc3a5edb6f8cc0db81966746cd4f8fc2b68b1e59
nn98 <jkllhgb@gmail.com>
2020-09-05 00:26:54 +0900

    13023

--- src/_Algorithm/DFS/P13023_3.java
+++ src/_Algorithm/DFS/P13023_3.java
@@ -1,5 +1,51 @@
 package _Algorithm.DFS;
 
-public class P13023_3 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class P13023_3 {
+	static int n,m,a[][],h[],i,j,c[];
+	static int o(int x,int d) {
+		if(c[x]>0)return 0;
+//		System.out.println("from \t"+m+" "+x+" "+d);
+		c[x]=1;
+		if(x!=m)
+			if(h[x]>0)
+				return h[x];
+		int y=0,z=0,w;
+		for(;y<n;y++) {
+			if(a[x][y]>0) {
+				if(h[y]>0)w=h[y];
+				else w=o(y,d+1);
+				z=z>w?z:w;
+			}
+		}
+		h[x]=h[x]>z?h[x]:z;
+		h[x]++;
+//		System.out.println("\tat: "+x+" re: "+h[x]);
+		return h[x];
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		h=new int[n];
+		c=new int[n];
+		a=new int[n][n];
+		while(m-->0) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			a[i][j]=a[j][i]=1;
+		}
+		i=0;
+		for(;++m<n;c=new int[n],o(m,0),i=i>h[m]?i:h[m])
+			if(i>4)
+				break;
+//		System.out.println(Arrays.toString(h));
+		System.out.println(i>4?1:0);
+	}
 }
