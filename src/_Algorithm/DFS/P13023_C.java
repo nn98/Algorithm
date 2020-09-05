b62067b9df2090e84ecbcdb566e569567bc996b0
nn98 <jkllhgb@gmail.com>
2020-09-06 03:34:58 +0900

    13023

--- src/_Algorithm/DFS/P13023_C.java
+++ src/_Algorithm/DFS/P13023_C.java
@@ -1,9 +1,34 @@
 package _Algorithm.DFS;
 
+import java.util.Scanner;
+
 public class P13023_C {
 
+	static int n,m,a,b,v[][],h[];
+	static void o(int i,int x) {
+		h[i]=1;
+		if(x>3) {
+			System.out.print(1);
+			System.exit(0);
+		}
+		for(int j=0;j<n;j++) {
+			if(v[i][j]>0)
+				o(j,x+1);
+		}
+		h[i]=0;
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		h=new int[n];
+		v=new int[n][n];
+		m=s.nextInt();
+		while(m-->0) {
+			a=s.nextInt();
+			b=s.nextInt();
+			v[a][b]=v[b][a]=1;
+		}
+		for(;++m<n;o(m,0));
 	}
 
 }
