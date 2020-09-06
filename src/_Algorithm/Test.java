8666a17da6e0f5f7d335ecd420c7a9a12f8848fb
nn98 <jkllhgb@gmail.com>
2020-09-06 22:14:32 +0900

    BackT

--- src/_Algorithm/Test.java
+++ src/_Algorithm/Test.java
@@ -1,10 +1,36 @@
 package _Algorithm;
 
+import java.util.Scanner;
+
 public class Test {
 
+	static int n,m,i,j,k,h[];
+	static char a[];
+	
+	static void o(int x,int c) {
+		if(c==n)
+			System.out.println(a);
+		else {
+			for(int y=0;y<n;a[x]=(char)y++,o(x+1,c+1));
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner s=new Scanner(System.in);
+		
+//		BackTracking Test
 
+		System.out.print("Number :\t");
+		n=s.nextInt();
+
+		System.out.print("Number :\t");
+		m=s.nextInt();
+		
+		a=new char[n];
+		
+		for(;i<n;h=new int[n],o(i++,0));
+		
 	}
 
 }
