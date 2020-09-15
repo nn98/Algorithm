bbcd6207dab262f379c4ab7acabbed8b50442ee6
nn98 <jkllhgb@gmail.com>
2020-09-15 19:40:10 +0900

    2042

--- src/_Algorithm/Segment_Tree/P2042_4.java
+++ src/_Algorithm/Segment_Tree/P2042_4.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 
 public class P2042_4 {
 
-	static int n,m,i,j,t[],k,l;
+	static int n,m,i,j,k,l;
+	static long t[];
 	static void o() {
 		while(i>1)t[i/=2]=t[i*2]+t[i*2+1];
 	}
-	static int p(int n,int s,int e) {
+	static long p(int n,int s,int e) {
 		if(j<s|e<i)return 0;
 		if(i<=s&e<=j)return t[n];
 		int m=(s+e)/2;
@@ -21,7 +22,7 @@ public class P2042_4 {
 		m=s.nextInt();
 		m+=s.nextInt();
 		for(i=1;i<n;l=i*=2);
-		t=new int[l*2];
+		t=new long[l*2];
 		for(;j<n;t[l+j++]=s.nextInt());
 //		System.out.println(Arrays.toString(t));
 		for(;k<n;i=l+k++,o());
