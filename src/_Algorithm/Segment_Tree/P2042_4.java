006c57cc107c331b665e15e008105123ce95e866
nn98 <jkllhgb@gmail.com>
2020-09-15 17:55:51 +0900

    2042

--- src/_Algorithm/Segment_Tree/P2042_4.java
+++ src/_Algorithm/Segment_Tree/P2042_4.java
@@ -1,9 +1,20 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2042_4 {
 
+	static int n,m,i,j,t[];
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		m+=s.nextInt();
+		for(i=1;i<n;i*=2);
+		t=new int[i*2];
+		for(;j<n;t[i+j++]=s.nextInt());
+		System.out.println(Arrays.toString(t));
 	}
 
 }
