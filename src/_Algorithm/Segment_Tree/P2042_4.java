568057cd4d72257f4b436fb6ad600f9eb985a6bb
nn98 <jkllhgb@gmail.com>
2020-09-15 18:06:00 +0900

    2042

--- src/_Algorithm/Segment_Tree/P2042_4.java
+++ src/_Algorithm/Segment_Tree/P2042_4.java
@@ -5,21 +5,38 @@ import java.util.Scanner;
 
 public class P2042_4 {
 
-	static int n,m,i,j,t[],k;
+	static int n,m,i,j,t[],k,l;
 	static void o() {
-		while(j>1)t[j/=2]=t[j*2]+t[j*2+1];
+		while(i>1)t[i/=2]=t[i*2]+t[i*2+1];
+	}
+	static int p(int n,int s,int e) {
+		if(j<s|e<i)return 0;
+		if(i<=s&e<=j)return t[n];
+		int m=(s+e)/2;
+		return p(n*2,s,m)+p(n*2+1,m+1,e);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		m=s.nextInt();
 		m+=s.nextInt();
-		for(i=1;i<n;i*=2);
-		t=new int[i*2];
-		for(;j<n;t[i+j++]=s.nextInt());
-		System.out.println(Arrays.toString(t));
-		for(;k<n;j=i+k++,o());
-		System.out.println(Arrays.toString(t));
+		for(i=1;i<n;l=i*=2);
+		t=new int[l*2];
+		for(;j<n;t[l+j++]=s.nextInt());
+//		System.out.println(Arrays.toString(t));
+		for(;k<n;i=l+k++,o());
+//		System.out.println(Arrays.toString(t));
+		while(m-->0) {
+			k=s.nextInt();
+			i=s.nextInt();
+			j=s.nextInt();
+			if(k==1) {
+				i+=l-1;
+				t[i]=j;
+				o();
+				System.out.println(Arrays.toString(t));
+			}
+		}
 	}
 
 }
