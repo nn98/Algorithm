d2dd8cee27c8f124d95d16978dbf31c8082c02f9
nn98 <jkllhgb@gmail.com>
2020-09-15 17:55:53 +0900

    2042

--- src/_Algorithm/Segment_Tree/P2042_4.java
+++ src/_Algorithm/Segment_Tree/P2042_4.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 public class P2042_4 {
 
-	static int n,m,i,j,t[];
+	static int n,m,i,j,t[],k;
+	static void o() {
+		while(j>1)t[j/=2]=t[j*2]+t[j*2+1];
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
@@ -14,6 +17,8 @@ public class P2042_4 {
 		for(i=1;i<n;i*=2);
 		t=new int[i*2];
 		for(;j<n;t[i+j++]=s.nextInt());
+		System.out.println(Arrays.toString(t));
+		for(;k<n;j=i+k++,o());
 		System.out.println(Arrays.toString(t));
 	}
 
