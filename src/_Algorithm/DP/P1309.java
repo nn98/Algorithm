b28deea47982b804e73063c95d8cc8f8692687c8
nn98 <jkllhgb@gmail.com>
2020-09-19 13:37:33 +0900

    1309

--- src/_Algorithm/DP/P1309.java
+++ src/_Algorithm/DP/P1309.java
@@ -3,7 +3,7 @@ package _Algorithm.DP;
 public class P1309 {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),i=0,j,a[]=new int[n+1];
+		int n=new java.util.Scanner(System.in).nextInt(),i=2,j,a[]=new int[n+1];
 		a[0]=3;
 		a[1]=7;
 		for(;i<n;a[i]=(a[i-1]*2+a[i++-2])%9901);
