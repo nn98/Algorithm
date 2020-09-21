a93c3d8321ca2d11ffd2300d10974d36675ff8b7
nn98 <jkllhgb@gmail.com>
2020-09-22 01:32:31 +0900

    1699

--- src/_Algorithm/DP/P1699.java
+++ src/_Algorithm/DP/P1699.java
@@ -7,7 +7,14 @@ public class P1699 {
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=0,j=1,x=316,a[]=new int[x];
 		for(;i<x;a[i++]=j*j++);
-		System.out.println(Arrays.toString(a));
+		j=0;
+//		System.out.println(Arrays.toString(a));
+		for(i=x;i-->0;) {
+			if(n>=a[i]) {
+				n-=a[i++];
+				j++;
+			}
+		}
 	}
 
 }
