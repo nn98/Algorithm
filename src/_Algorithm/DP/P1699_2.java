579332f14d0cf0971099c3ad3ad1cd9b0afb7661
nn98 <jkllhgb@gmail.com>
2020-09-22 02:00:14 +0900

    1699

--- src/_Algorithm/DP/P1699_2.java
+++ src/_Algorithm/DP/P1699_2.java
@@ -1,10 +1,15 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
+
 public class P1699_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt(),i=2,j,a[]=new int[n];
+		for(;i<n;i++)
+			for(j=2;j*j<=i;j++)
+				a[i]=Math.min(a[i],a[i-j*j]+1);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
