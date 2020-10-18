b21302734a1689c2ed7c29a319dbafd48a2dd8cb
nn98 <jkllhgb@gmail.com>
2020-10-18 23:49:58 +0900

    2170

--- src/_Algorithm/Sweeping/P2170.java
+++ src/_Algorithm/Sweeping/P2170.java
@@ -1,6 +1,6 @@
 package _Algorithm.Sweeping;
 import java.util.Scanner;
-public class P2170 {
+public class P2170{
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a,b;
