2a33652a149ba030651454c896e432f8bd0ad5e1
nn98 <jkllhgb@gmail.com>
2020-10-20 21:10:20 +0900

    11687

--- src/_Algorithm/Math/P11687.java
+++ src/_Algorithm/Math/P11687.java
@@ -3,7 +3,11 @@ package _Algorithm.Math;
 public class P11687 {
 
 	public static void main(String[] args) {
-		
+		int n=new java.util.Scanner(System.in).nextInt(),t=0,f=0,i=2,m;
+		for(;;) {
+			m=t<f?t:f;
+			
+		}
 	}
 
 }
