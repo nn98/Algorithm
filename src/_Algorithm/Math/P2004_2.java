5c8fbc20f65638941a3c8fa11379b2c738b6a139
nn98 <jkllhgb@gmail.com>
2020-10-20 22:00:17 +0900

    2004

--- src/_Algorithm/Math/P2004_2.java
+++ src/_Algorithm/Math/P2004_2.java
@@ -1,9 +1,27 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P2004_2 {
 
+	static long o(long n) {
+		long m=0;
+		for(long i=5;i<=n;i*=5)
+			m+=n/i;
+		return m;
+	}
+
+	static long p(long n) {
+		long m=0;
+		for(long i=2;i<=n;i*=2)
+			m+=n/i;
+		return m;
+	}
+	
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		long n=s.nextInt(),m=s.nextInt();
+		System.out.print(Math.min(o(n)-(o(m)+o(n-m)),p(n)-(p(m)+p(n-m))));
 	}
 
 }
