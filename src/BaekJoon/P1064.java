9712effc0b6492596e98cdbad5e7483080955457
nn98 <jkllhgb@gmail.com>
2020-10-20 18:14:31 +0900

    1064

--- src/BaekJoon/P1064.java
+++ src/BaekJoon/P1064.java
@@ -1,5 +1,7 @@
-mport java.util.*;
-class Main{
+package BaekJoon;
+
+import java.util.*;
+class P1064{
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt(),c=s.nextInt(),u=s.nextInt();
