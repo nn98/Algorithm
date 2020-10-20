0a9656157717d1555386419940f4b1ea0d561861
nn98 <jkllhgb@gmail.com>
2020-10-20 18:31:55 +0900

    1064

--- src/BaekJoon/P1064.java
+++ src/BaekJoon/P1064.java
@@ -7,7 +7,7 @@ class P1064{
 		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt(),c=s.nextInt(),u=s.nextInt();
 		double a=Math.hypot(X-c,Y-u),b=Math.hypot(c-x, u-y),C=Math.hypot(x-X,y-Y);
 		String r="-1";
-		if(!((X-x)*(u-y)==(Y-y)*(c-x)))r=String.format("%.10f",(Math.max(a,b>c?b:c)-Math.min(a,b<c?b:c))*2);
+		if(!((X-x)*(u-y)==(Y-y)*(c-x)))r=String.format("%f",(Math.max(a,b>c?b:c)-Math.min(a,b<c?b:c))*2);
 		System.out.print(r);
 	}
 }
