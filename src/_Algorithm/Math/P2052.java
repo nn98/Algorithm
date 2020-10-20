a839a7d0bef93befcb05677003f473c5b2bd1979
nn98 <jkllhgb@gmail.com>
2020-10-20 18:45:06 +0900

    2052

--- src/_Algorithm/Math/P2052.java
+++ src/_Algorithm/Math/P2052.java
@@ -6,8 +6,8 @@ public class P2052 {
 
 	public static void main(String[] args) {
 		BigDecimal m=new BigDecimal("2");
-		int n=new java.util.Scanner(System.in).nextInt();
-		m=m.pow(n);
+		int n;
+		m=m.pow(new java.util.Scanner(System.in).nextInt());
 		System.out.println(m);
 		System.out.println(BigDecimal.ONE.divide(m));
 	}
