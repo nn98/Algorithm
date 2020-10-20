a995c05b1ea005b9a3904baebd869386e178b124
nn98 <jkllhgb@gmail.com>
2020-10-20 18:32:47 +0900

    1064

--- src/BaekJoon/P1064.java
+++ src/BaekJoon/P1064.java
@@ -6,7 +6,7 @@ class P1064{
 		Scanner s=new Scanner(System.in);
 		int x=s.nextInt(),y=s.nextInt(),X=s.nextInt(),Y=s.nextInt(),c=s.nextInt(),u=s.nextInt();
 		double a=Math.hypot(X-c,Y-u),b=Math.hypot(c-x, u-y),C=Math.hypot(x-X,y-Y);
-		System.out.println(a+" "+b+" "+C);
+//		System.out.println(a+" "+b+" "+C);
 		String r="-1";
 		if(!((X-x)*(u-y)==(Y-y)*(c-x)))r=String.format("%.10f",(Math.max(a,b>C?b:C)-Math.min(a,b<C?b:C))*2);
 		System.out.print(r);
