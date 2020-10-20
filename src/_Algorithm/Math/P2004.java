3bb31ee867f7853a98cd0cf97da621e7f7b0cb61
nn98 <jkllhgb@gmail.com>
2020-10-20 21:54:14 +0900

    2004

--- src/_Algorithm/Math/P2004.java
+++ src/_Algorithm/Math/P2004.java
@@ -1,10 +1,32 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P2004 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int n=s.nextInt(),m=s.nextInt(),f=0,i,b=n-m;
+		if(b>m) {
+			i=b;
+			b=m;
+			m=i;
+		}
+		for(;m++<n;) {
+			i=m;
+			while(i%5==0) {
+				i/=5;
+				f++;
+			}
+		}
+		for(;b>4;b--) {
+			i=b;
+			while(i%5==0) {
+				i/=5;
+				f--;
+			}
+		}
+		System.out.println(f);
 	}
 
 }
