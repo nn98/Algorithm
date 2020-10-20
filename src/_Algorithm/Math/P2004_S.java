e71d4798ee8ac361ec664962c3f4ef74845e90ab
nn98 <jkllhgb@gmail.com>
2020-10-20 22:03:28 +0900

    2004

--- src/_Algorithm/Math/P2004_S.java
+++ src/_Algorithm/Math/P2004_S.java
@@ -1,10 +1,16 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P2004_S {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),t=0,f=0;
+		long i=2;
+		for(;i<=n;i*=2)t+=(n/i)-(m/i)-((n-m)/i);
+		for(i=5;i<=n;i*=5)t+=(n/i)-(m/i)-((n-m)/i);
+		System.out.print(t<f?t:f);
 	}
 
 }
