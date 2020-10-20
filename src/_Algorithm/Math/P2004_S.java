b20b7f9374a9b8e7fe6a2e662d27867c56ad95ef
nn98 <jkllhgb@gmail.com>
2020-10-20 22:31:07 +0900

    2004

--- src/_Algorithm/Math/P2004_S.java
+++ src/_Algorithm/Math/P2004_S.java
@@ -9,7 +9,7 @@ public class P2004_S {
 		int n=s.nextInt(),m=s.nextInt(),t=0,f=0;
 		long i=2;
 		for(;i<=n;i*=2)t+=(n/i)-(m/i)-((n-m)/i);
-		for(i=5;i<=n;i*=5)t+=(n/i)-(m/i)-((n-m)/i);
+		for(i=5;i<=n;i*=5)f+=(n/i)-(m/i)-((n-m)/i);
 		System.out.print(t<f?t:f);
 	}
 
