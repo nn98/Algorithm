ffcc7ff08e5c99cbc08ca4d72bd0120b2713714f
nn98 <jkllhgb@gmail.com>
2020-10-21 23:09:31 +0900

    1672

--- src/BaekJoon/P1672.java
+++ src/BaekJoon/P1672.java
@@ -1,9 +1,16 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class P1672 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		char[]a=s.next().toCharArray(),b[]=new char[26][26];
+		System.out.println('c'-'a');
+		System.out.println('g'-'a');
+		System.out.println('t'-'a');
 	}
 
 }
