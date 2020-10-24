ffcb01181626228797d60d94700384eea3e5a740
nn98 <jkllhgb@gmail.com>
2020-10-24 13:11:48 +0900

    1672

--- src/BaekJoon/P1672.java
+++ src/BaekJoon/P1672.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class P1672{
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0;
+		int n=s.nextInt();
 		char[]a=s.next().toCharArray(),b[]=new char[20][20];
 		b[0][0]=b[6][19]=b[19][6]=b[0][2]=b[2][0]=65;
 		b[2][2]=b[0][6]=b[6][0]=67;
