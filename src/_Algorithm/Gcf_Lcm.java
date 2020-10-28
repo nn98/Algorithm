6d697f8027358c629e6ec5568c2ca64af1e628a7
nn98 <jkllhgb@gmail.com>
2020-10-28 13:12:43 +0900

    gcf lcm

--- src/_Algorithm/Gcf_Lcm.java
+++ src/_Algorithm/Gcf_Lcm.java
@@ -1,10 +1,18 @@
 package _Algorithm;
 
+import java.util.Scanner;
+
 public class Gcf_Lcm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),i=a,j=b,k;
+		while(j>0) {
+			k=i%j;
+			i=j;
+			j=k;
+		}
+		System.out.println(i+" "+j);
 	}
 
 }
