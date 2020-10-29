a6afdb73e51908137b1c596880fc36fa2607e0b8
nn98 <jkllhgb@gmail.com>
2020-10-29 21:35:17 +0900

    1645

--- src/_Algorithm/Sweeping/P1645.java
+++ src/_Algorithm/Sweeping/P1645.java
@@ -1,9 +1,18 @@
 package _Algorithm.Sweeping;
 
+import java.util.Scanner;
+
 public class P1645 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,a[]=new int[n],t[]=new int[n],j=0;
+		while(i<n)a[i++]=s.nextInt();
+		i=0;
+		t[0]=a[0];
+		while(++i<n) {
+			if(a[i]>t[j]);
+		}
 	}
 
 }
