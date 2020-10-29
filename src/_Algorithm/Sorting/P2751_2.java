472d21aa5e58729e67cdf0974d4c180a875dbcd5
nn98 <jkllhgb@gmail.com>
2020-10-29 23:17:42 +0900

    1645

--- src/_Algorithm/Sorting/P2751_2.java
+++ src/_Algorithm/Sorting/P2751_2.java
@@ -1,10 +1,13 @@
 package _Algorithm.Sorting;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P2751_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[2000001];
 	}
 
 }
