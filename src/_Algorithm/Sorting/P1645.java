dea01c8fa6e966ab939a305aba8e179d8af47701
nn98 <jkllhgb@gmail.com>
2020-10-29 22:16:09 +0900

    1645

--- src/_Algorithm/Sorting/P1645.java
+++ src/_Algorithm/Sorting/P1645.java
@@ -1,10 +1,16 @@
 package _Algorithm.Sorting;
 
+import java.util.Scanner;
+
 public class P1645 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),x=10001,a[]=new int[x],i=0;
+		while(n-->0)a[s.nextInt()]++;
+		while(++i<x)
+			if(a[i]>i)break;
+		System.out.print(i+1);
 	}
 
 }
