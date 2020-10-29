6d9688b838988e5a733e58177d2c33a3ee2eb338
nn98 <jkllhgb@gmail.com>
2020-10-29 23:16:16 +0900

    1645

--- src/_Algorithm/Sorting/P1645.java
+++ src/_Algorithm/Sorting/P1645.java
@@ -1,16 +1,21 @@
 package _Algorithm.Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1645 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),x=10001,a[]=new int[x],i=0;
-		while(n-->0)a[s.nextInt()]++;
-		while(++i<x)
-			if(a[i]>i)break;
-		System.out.print(i==x?0:i+1);
+		int n=s.nextInt(),i=0,a[]=new int[n],t[]=new int[n],j=0;
+		while(i<n)a[i++]=s.nextInt();
+		Arrays.sort(a);
+		i=0;
+		while(i<n) {
+			if(a[i]<=i)break;
+			i++;
+		}
+		System.out.println(i==n?0:i+1);
 	}
 
 }
