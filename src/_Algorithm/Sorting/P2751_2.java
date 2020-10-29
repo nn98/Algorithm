4c26793af47ddae1ccd4d544edb699e0e8f07a35
nn98 <jkllhgb@gmail.com>
2020-10-29 23:20:46 +0900

    1645

--- src/_Algorithm/Sorting/P2751_2.java
+++ src/_Algorithm/Sorting/P2751_2.java
@@ -6,7 +6,11 @@ public class P2751_2 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),a[]=new int[2000001];
+		int n=Integer.parseInt(r.readLine()),m=2000001,v=1000000,a[]=new int[m];
+		while(n-->0)
+			a[Integer.parseInt(r.readLine())+v]++;
+		while(n++<m)
+			System.out.print(a[n]>0?n+v:"");
 	}
 
 }
