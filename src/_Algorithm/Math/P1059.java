0903542d2fd82a691ccec7ba16b0348abeb61ab9
nn98 <jkllhgb@gmail.com>
2020-10-31 22:24:50 +0900

    1059

--- src/_Algorithm/Math/P1059.java
+++ src/_Algorithm/Math/P1059.java
@@ -9,7 +9,11 @@ public class P1059 {
 		int n=s.nextInt(),a[]=new int[1000],i,j;
 		while(n-->0)a[s.nextInt()]++;
 		i=j=n=s.nextInt();
-		
+		for(;!(a[i]>0&a[j]>0);i--,j++) {
+			if(a[i]>0)i++;
+			if(a[j]>0)j--;
+		}
+		System.out.print(i+" "+j);
 	}
 
 }
