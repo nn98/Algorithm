df4847384f0ee8204bb07c6d8352ee7811dd9167
nn98 <jkllhgb@gmail.com>
2020-10-31 22:17:48 +0900

    1059

--- src/_Algorithm/Math/P1059.java
+++ src/_Algorithm/Math/P1059.java
@@ -1,10 +1,15 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P1059 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[1000],i,j;
+		while(n-->0)a[s.nextInt()]++;
+		i=j=n=s.nextInt();
+		
 	}
 
 }
