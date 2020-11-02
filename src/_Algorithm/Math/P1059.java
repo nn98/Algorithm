51c2d6f9eb26fea5e53f228d872113beebacbb20
nn98 <jkllhgb@gmail.com>
2020-11-02 20:55:04 +0900

    1059

--- src/_Algorithm/Math/P1059.java
+++ src/_Algorithm/Math/P1059.java
@@ -1,7 +1,7 @@
 package _Algorithm.Math;
 import java.util.Scanner;
-public class P1059 {
-	public static void main(String[] args) {
+public class P1059{
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[1000],i,j;
 		while(n-->0)a[s.nextInt()]++;
