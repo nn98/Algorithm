ee5ee0476a67bbd2d11031a1ceeffa3fb987dfb5
nn98 <jkllhgb@gmail.com>
2020-11-04 23:14:00 +0900

    20001

--- src/BaekJoon/P20001.java
+++ src/BaekJoon/P20001.java
@@ -1,5 +1,20 @@
 package BaekJoon;
-
-public class P20001 {
-
+import java.util.*;
+class P20001{
+public static void main(String[]z){
+Scanner s=new Scanner(System.in);
+int m=0;
+String a=s.nextLine();
+while(!(a=s.nextLine()).equals("고무오리 디버깅 끝")){
+switch(a){
+case "고무오리":
+if(m>0)m--;
+else m+=2;
+break;
+case "문제":
+m++;
+break;
 }
+}
+System.out.print(m==0?"고무오리야 사랑해":"힝구");
+}}
