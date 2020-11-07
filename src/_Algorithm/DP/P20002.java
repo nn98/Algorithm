8c4c8c749def58d4765276e3e99ba2c37a349b54
nn98 <jkllhgb@gmail.com>
2020-11-07 22:37:56 +0900

    20002

--- src/_Algorithm/DP/P20002.java
+++ src/_Algorithm/DP/P20002.java
@@ -1,10 +1,12 @@
 package _Algorithm.DP;
 
+import java.util.Scanner;
+
 public class P20002 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		int n=s.nextInt();
 	}
 
 }
