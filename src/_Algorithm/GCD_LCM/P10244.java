1bf6911a068d5350d2f530d7e18386d158c01213
nn98 <jkllhgb@gmail.com>
2020-11-07 23:17:26 +0900

    10244

--- src/_Algorithm/GCD_LCM/P10244.java
+++ src/_Algorithm/GCD_LCM/P10244.java
@@ -1,11 +1,12 @@
 package _Algorithm.GCD_LCM;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class P10244 {
 
 	static int t;
 	static int o(int a,int b) {
+		System.out.println(a+" "+b);
 		while(b>0) {
 			t=a%b;
 			a=b;
@@ -15,7 +16,16 @@ public class P10244 {
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		System.out.println(o(s.nextInt(),s.nextInt()));
+		Set<Integer> t=new HashSet();
+		int n,i=0,j,m,a[],b[];
+		while((n=s.nextInt())!=0) {
+			a=new int[n+1];
+			b=new int[n+1];
+			for(;i++<n;t.add(a[i]=s.nextInt()),t.add(b[i]=o(a[i-1],a[i])));
+			System.out.println(Arrays.toString(a));
+			System.out.println(Arrays.toString(a));
+			System.out.println(t.toString());
+		}
 	}
 
 }
