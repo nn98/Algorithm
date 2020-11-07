c3ee0aa5f472adeb99828ac0a442007591de39de
nn98 <jkllhgb@gmail.com>
2020-11-07 22:50:26 +0900

    10244

--- src/_Algorithm/GCD_LCM/P10244.java
+++ src/_Algorithm/GCD_LCM/P10244.java
@@ -2,9 +2,17 @@ package _Algorithm.GCD_LCM;
 
 public class P10244 {
 
+	static int t;
+	static int o(int a,int b) {
+		while(b>1) {
+			t=a%b;
+			a=b;
+			b=t;
+		}
+		return a;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(o(24,8));
 	}
 
 }
