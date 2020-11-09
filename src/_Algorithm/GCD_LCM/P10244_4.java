6e2bf69bb3d43f3a7f30e44734c67edde8789ae6
nn98 <jkllhgb@gmail.com>
2020-11-10 08:17:48 +0900

    10244

--- src/_Algorithm/GCD_LCM/P10244_4.java
+++ src/_Algorithm/GCD_LCM/P10244_4.java
@@ -1,5 +1,22 @@
 package _Algorithm.GCD_LCM;
 
-public class P10244_4 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class P10244_4 {
+	static int o(int a,int b) {
+		int t;
+		while(b>0) {
+			t=a%b;
+			a=b;
+			b=t;
+		}
+		return a;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	}
 }
