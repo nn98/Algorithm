26469211a2198d082d0a195c33c7bb40b83a73ca
nn98 <jkllhgb@gmail.com>
2020-11-11 00:34:25 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999.java
+++ src/_Algorithm/Segment_Tree/P10999.java
@@ -1,9 +1,10 @@
 package _Algorithm.Segment_Tree;
-
+import java.io.*;
 public class P10999 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	}
 
 }
