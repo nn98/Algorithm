59b319754056cb6770785d4142dee10b855e1a2a
nn98 <jkllhgb@gmail.com>
2020-11-13 21:38:44 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999.java
+++ src/_Algorithm/Segment_Tree/P10999.java
@@ -11,9 +11,11 @@ public class P10999 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken())+Integer.parseInt(t.nextToken());
-		for(l=2;l<n;h=l*=2);
+		for(l=2;l<n;l*=2);
+		a=new long[l*2];
 		System.out.println(l+" "+h);
-//		while(i++<n)
+		while(i++<n)a[l+i]=Integer.parseInt(r.readLine());
+		System.out.println(Arrays.toString(a));
 	}
 
 }
