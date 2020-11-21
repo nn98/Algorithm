950e0366c2bc76f3982fe66abb136fce9bdc9581
nn98 <jkllhgb@gmail.com>
2020-11-21 22:56:06 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999_3.java
+++ src/_Algorithm/Segment_Tree/P10999_3.java
@@ -1,10 +1,7 @@
 package _Algorithm.Segment_Tree;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class P10999_3 {
 	static int n,m,l,h,i,x,y,z;
