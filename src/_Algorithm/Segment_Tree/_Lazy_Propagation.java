9efc48452e747281f71469c0ce209a02f1abadb6
nn98 <jkllhgb@gmail.com>
2020-11-24 23:12:20 +0900

    10999

--- src/_Algorithm/Segment_Tree/_Lazy_Propagation.java
+++ src/_Algorithm/Segment_Tree/_Lazy_Propagation.java
@@ -2,7 +2,7 @@ package _Algorithm.Segment_Tree;
 
 public class _Lazy_Propagation {
 
-	static int a[]= {1,10,3,6,5,6,4};
+	static int a[]= {1,10,3,6,5,6,4}, u[]= {1,11,14,20,25,31,35};
 	
 	public static void main(String[] args) {
 		
