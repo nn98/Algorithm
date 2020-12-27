186774b2b14eaccb77b23000f1efc6c83027a772
nn98 <jkllhgb@gmail.com>
2020-12-27 19:29:12 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999_4.java
+++ src/_Algorithm/Segment_Tree/P10999_4.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class P10999_4 {
 	
 	static long[]a,b,c;
+	static int n,i;
 
 	public static void main(String[] args)throws Exception{
 		
@@ -13,7 +14,7 @@ public class P10999_4 {
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		
-		
+		n=Integer.parseInt(t.nextToken());
 		
 	}
 
