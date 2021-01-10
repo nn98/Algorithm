85900bf99e8598166898a6ac78ba83d91ce96db5
nn98 <jkllhgb@gmail.com>
2021-01-11 00:52:14 +0900

    5676

--- src/_Algorithm/Segment_Tree/P5676.java
+++ src/_Algorithm/Segment_Tree/P5676.java
@@ -5,26 +5,20 @@ import java.util.*;
 
 public class P5676 {
 
-	static int n,m,a[],l,i,j,x,y,F;
+	static int n,m,a[],l,i,j,x,y;
 	static void o() {
 //		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
-		for(;j>1&F>0;) {
+		for(;j>1;) {
 			j/=2;
 			x=a[j*2]==0?0:(a[j*2]>0?1:-1);
 			y=a[j*2+1]==0?0:(a[j*2+1]>0?1:-1);
 			a[j]=x*y;
-			if(a[j]==0)F=0;
 		}
-		for(;j>1&F==0;j/=2,a[j]=0);
 	}
 	static int p(int n,int s,int e) {
 //		System.out.println(n+" "+s+" "+e);
-		if(F==0)return 0;
 		if(s>y|x>e)return 1;
-		if(x<=s&e<=y) {
-			if(a[n]==0)F=0;
-			return a[n];
-		}
+		if(x<=s&e<=y)return a[n];
 		int m=(s+e)/2;
 		return p(n*2,s,m)*p(n*2+1,m+1,e);
 	}
