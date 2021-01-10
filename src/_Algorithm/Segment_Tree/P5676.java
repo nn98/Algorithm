c8249c5a2efb724d845051904a30132460376642
nn98 <jkllhgb@gmail.com>
2021-01-11 00:09:55 +0900

    5676

--- src/_Algorithm/Segment_Tree/P5676.java
+++ src/_Algorithm/Segment_Tree/P5676.java
@@ -46,6 +46,7 @@ public class P5676 {
 				if(t.nextToken().equals("C")) {
 					j=Integer.parseInt(t.nextToken())+l-1;
 					a[j]=Integer.parseInt(t.nextToken());
+					F=1;
 					o();
 					System.out.println(Arrays.toString(a));
 				}else {
