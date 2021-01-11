b8ee4950416a54bf55b191e25e4026ebb52fc259
nn98 <jkllhgb@gmail.com>
2021-01-11 13:40:28 +0900

    10999

--- src/_Algorithm/Segment_Tree/_Tree_View.java
+++ src/_Algorithm/Segment_Tree/_Tree_View.java
@@ -11,10 +11,10 @@ public class _Tree_View {
 		s.replaceAll(",","");
 		S=s.split(" ");
 		System.out.println(s);
-		for(;n>0;) {
+		for(;n>0;b++) {
 			n/=2;
 			for(i=0;i++<b;System.out.print("\t"));
-			for(i=n;i<n*2;System.out.print(S[i]+" "));
+			for(i=n;i<n*2;System.out.print(S[i++]+" "));
 		}
 	}
 
