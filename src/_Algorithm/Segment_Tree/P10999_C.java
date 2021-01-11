74bb4b59945ac91ba2dd75570eb950a95f21a248
nn98 <jkllhgb@gmail.com>
2021-01-11 14:38:05 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999_C.java
+++ src/_Algorithm/Segment_Tree/P10999_C.java
@@ -1,5 +1,10 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P10999_C {
 	static int N;
 	static int Q;
