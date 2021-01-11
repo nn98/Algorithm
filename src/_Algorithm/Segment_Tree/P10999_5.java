5e9652f6b13569db335957a01f25afa3114b5333
nn98 <jkllhgb@gmail.com>
2021-01-11 12:57:10 +0900

    10999

--- src/_Algorithm/Segment_Tree/P10999_5.java
+++ src/_Algorithm/Segment_Tree/P10999_5.java
@@ -1,9 +1,13 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.*;
+import java.util.*;
+
 public class P10999_5 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out)); 
 	}
 
 }
