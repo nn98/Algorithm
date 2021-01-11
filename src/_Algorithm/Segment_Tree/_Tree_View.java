72fe09119f1445c389f9d92101eea7d32a7199ac
nn98 <jkllhgb@gmail.com>
2021-01-11 13:46:43 +0900

    10999

--- src/_Algorithm/Segment_Tree/_Tree_View.java
+++ src/_Algorithm/Segment_Tree/_Tree_View.java
@@ -6,16 +6,20 @@ public class _Tree_View {
 		String s=new java.util.Scanner(System.in).nextLine(),S[];
 		int n=1,i,b=0;
 		for(char c:s.toCharArray())if(c==',')n++;
-		System.out.println(s);
 		s=s.substring(1,s.length()-1);
 		s=s.replaceAll(",","");
 		S=s.split(" ");
-		System.out.println(s);
+		StringBuffer f=new StringBuffer();
 		for(;n>0;b++) {
-			n/=2;
+//			n/=2;
+//			for(i=0;i++<b;System.out.print("\t"));
+//			for(i=n;i<n*2;System.out.print(S[i++]+(i==n*2?"\n":"  ")));
 			for(i=0;i++<b;System.out.print("\t"));
-			for(i=n;i<n*2;System.out.print(S[i++]+(i==n*2?"\n":" ")));
+			for(i=n;i-->n/2;f.append(S[i]+(i==n/2?"\n":"  ")));
+			n/=2;
 		}
+		System.out.println(f.reverse());;
 	}
 
 }
+// [0, 15, 10, 5, 3, 7, 5, 0, 1, 2, 3, 4, 5, 0, 0, 0]
