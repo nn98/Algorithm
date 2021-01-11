5a31bc992d4ae33fe6099a79697e831f6ea4e060
nn98 <jkllhgb@gmail.com>
2021-01-11 13:36:30 +0900

    10999

--- src/_Algorithm/Segment_Tree/_Tree_View.java
+++ src/_Algorithm/Segment_Tree/_Tree_View.java
@@ -3,8 +3,17 @@ package _Algorithm.Segment_Tree;
 public class _Tree_View {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s=new java.util.Scanner(System.in).nextLine(),S[];
+		int n=1,i,b=0;
+		for(char c:s.toCharArray())if(c==',')n++;
+		s.replaceAll("[","");
+		s.replaceAll("]","");
+		s.replaceAll(",","");
+		S=s.split(" ");
+		for(;n>0;) {
+			for(i=0;i++<b;System.out.print("\t"));
+			for(i=n;i<n*2;System.out.print(S[i]+" "));
+		}
 	}
 
 }
