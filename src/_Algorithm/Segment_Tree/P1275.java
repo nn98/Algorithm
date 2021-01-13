90a572d1fd7327159bcca06e2ce6f39acd35edfc
nn98 <jkllhgb@gmail.com>
2021-01-13 18:03:40 +0900

    1275

--- src/_Algorithm/Segment_Tree/P1275.java
+++ src/_Algorithm/Segment_Tree/P1275.java
@@ -10,9 +10,9 @@ public class P1275 {
 	static void o() {
 		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
 	}
-//	static long p(int n,int s,int e) {
-//		
-//	}
+	static long p(int n,int s,int e) {
+		
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
@@ -24,6 +24,20 @@ public class P1275 {
 		a=new long[l*2];
 		for(;i<n;j=i+++l,a[j]=Integer.parseInt(t.nextToken()),o());
 		System.out.println(Arrays.toString(a));
+		while(m-->0) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			if(j>i) {
+				n=i;
+				i=j;
+				j=n;
+			}
+			w.write(p(1,1,l)+"\n");
+			j=Integer.parseInt(t.nextToken())+l-1;
+			a[j]=Integer.parseInt(t.nextToken());
+			o();
+		}
 	}
 
 }
