09681c00d7c94aa60d43948e082d6591a817dd9a
nn98 <jkllhgb@gmail.com>
2021-01-14 01:16:12 +0900

    한글테스트

--- src/_Algorithm/Simple_Implementation/P11283.java
+++ src/_Algorithm/Simple_Implementation/P11283.java
@@ -1,9 +1,14 @@
 package _Algorithm.Simple_Implementation;
 
+import java.io.UnsupportedEncodingException;
+
 public class P11283 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		System.out.printf("%d",'힣');
+		String word = "무궁화 꽃이 피었습니다.";
+
+		System.out.println("utf-8 -> euc-kr        : " + new String(word.getBytes("utf-8"), "euc-kr"));
 	}
 
 }
