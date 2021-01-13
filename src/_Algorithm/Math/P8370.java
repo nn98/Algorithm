9bb71614c8449e1defc99a3f214212356b9ff53b
nn98 <jkllhgb@gmail.com>
2021-01-13 19:41:25 +0900

    8370

--- src/_Algorithm/Math/P8370.java
+++ src/_Algorithm/Math/P8370.java
@@ -1,10 +1,12 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P8370 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		System.out.print(s.nextInt()*s.nextInt()+s.nextInt()*s.nextInt());
 	}
 
 }
