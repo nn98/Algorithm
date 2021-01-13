70dcf7edc7e60899e7ea7e00ab561874074932a4
nn98 <jkllhgb@gmail.com>
2021-01-13 18:23:44 +0900

    6749

--- src/_Algorithm/Math/P6749.java
+++ src/_Algorithm/Math/P6749.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P6749 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt();
+		System.out.println(m-n+m);
 	}
 
 }
