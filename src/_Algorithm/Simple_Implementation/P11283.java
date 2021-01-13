f6e9ec57b729ad7364606611af3b1e120155932e
nn98 <jkllhgb@gmail.com>
2021-01-14 01:28:23 +0900

    11283

--- src/_Algorithm/Simple_Implementation/P11283.java
+++ src/_Algorithm/Simple_Implementation/P11283.java
@@ -1,10 +1,8 @@
 package _Algorithm.Simple_Implementation;
 
-import java.io.UnsupportedEncodingException;
-
 public class P11283 {
 
-	public static void main(String[] args) throws UnsupportedEncodingException {
+	public static void main(String[] args){
 //		System.out.format("%d", (int) '가');
 //		System.out.format("%d",new java.util.Scanner(System.in).next().charAt(0));
 		System.out.println(new java.util.Scanner(System.in).next().charAt(0)-44031);
