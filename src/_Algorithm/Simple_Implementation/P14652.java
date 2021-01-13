34f8ffdff97959a04acf03f6e62c2e8bf602eea1
nn98 <jkllhgb@gmail.com>
2021-01-13 18:21:24 +0900

    14652

--- src/_Algorithm/Simple_Implementation/P14652.java
+++ src/_Algorithm/Simple_Implementation/P14652.java
@@ -1,13 +1,10 @@
 package _Algorithm.Simple_Implementation;
 
 import java.util.Scanner;
-
 public class P14652 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int x=s.nextInt(),y=s.nextInt(),z=s.nextInt()+1;
 		System.out.print(z/y+" "+(z%y-1));
 	}
-
 }
