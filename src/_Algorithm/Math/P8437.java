d1a056bf345f00326fa885b0793531aef9e57607
nn98 <jkllhgb@gmail.com>
2021-01-14 19:35:59 +0900

    8437

--- src/_Algorithm/Math/P8437.java
+++ src/_Algorithm/Math/P8437.java
@@ -7,7 +7,8 @@ public class P8437 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt();
-		if(n%2==0)System.out.println(n/2+m/2+" "+(n/2-m/2));
+		if(n%2==0)System.out.print(n/2+m/2+"\n"+(n/2-m/2));
+		else System.out.print(n/2+m/2+1+"\n"+(n/2-m/2));
 	}
 
 }
