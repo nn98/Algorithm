23fb2f23a37d8460f24d17d3f76752b6f4878b37
nn98 <jkllhgb@gmail.com>
2021-01-14 19:33:12 +0900

    8437

--- src/_Algorithm/Math/P8437.java
+++ src/_Algorithm/Math/P8437.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P8437 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt();
+		if(n%2==0)System.out.println(n/2+m/2+" "+(n/2-m/2));
 	}
 
 }
