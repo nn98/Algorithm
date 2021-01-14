ec005304e8141c773e8c07a859ddae02bd52df0d
nn98 <jkllhgb@gmail.com>
2021-01-14 19:43:19 +0900

    8437

--- src/_Algorithm/Math/P8437_2.java
+++ src/_Algorithm/Math/P8437_2.java
@@ -1,10 +1,17 @@
 package _Algorithm.Math;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P8437_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		BigInteger a=new BigInteger(s.next()),b=new BigInteger(s.next())
+				,c=a.divide(new BigInteger("2")),d=b.divide(new BigInteger("2"));
+		if(a.mod(new BigInteger("2")).equals(BigInteger.ZERO))
+			System.out.print(c.add(d)+"\n"+c.subtract(b));
+		else System.out.print(c.add(d).add(BigInteger.ONE)+"\n"+c.subtract(b).subtract(BigInteger.ONE));
 	}
 
 }
