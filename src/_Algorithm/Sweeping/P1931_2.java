01e4b9e71488f2ad60fd7ad7495db5f27a58581b
nn98 <jkllhgb@gmail.com>
2021-01-15 21:54:14 +0900

    1931

--- src/_Algorithm/Sweeping/P1931_2.java
+++ src/_Algorithm/Sweeping/P1931_2.java
@@ -1,10 +1,24 @@
 package _Algorithm.Sweeping;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class P1931_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[][]=new int[n][2],i=0;
+		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=s.nextInt());
+		System.out.println(Arrays.toString(a[0])+"\n"+Arrays.toString(a[1]));
+		Arrays.sort(a,new Comparator<int[]>() {
+			@Override
+			public int compare(int[] q,int[] w) {
+				if(q[1]==w[1])return q[0]-w[0];
+				return q[1]-w[1];
+			}
+		});
+		
 	}
 
 }
