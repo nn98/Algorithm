588fa987f19e6857fd5c18010b6df104230d09ff
nn98 <jkllhgb@gmail.com>
2021-01-16 01:01:54 +0900

    19622

--- src/_Algorithm/DP/P19622.java
+++ src/_Algorithm/DP/P19622.java
@@ -1,9 +1,12 @@
 package _Algorithm.DP;
 
+import java.util.Scanner;
+
 public class P19622 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[][]=new int[n+1][2];
 	}
 
 }
