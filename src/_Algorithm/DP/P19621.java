9f2a57391e0070e3ce98c920816670b79f39d736
nn98 <jkllhgb@gmail.com>
2021-01-15 22:48:44 +0900

    19621

--- src/_Algorithm/DP/P19621.java
+++ src/_Algorithm/DP/P19621.java
@@ -7,14 +7,19 @@ public class P19621 {
 
 	static int n,a[],h[],i,m;
 	static void o(int x,int b) {
-		System.out.println(x+" "+b);
-		if(x<n-1)o(x+1,b);
-		b+=a[x];
-		if(x<n-2)
-			if(h[x+2]<b)
-				o(x+2,b);
-		h[x]=h[x]>b?h[x]:b;
-		m=m>h[x]?m:h[x];
+//		System.out.println(x+" "+b);
+//		if(x<n-1)o(x+1,b);
+//		b+=a[x];
+//		if(x<n-2)
+//			if(h[x+2]<b)
+//				o(x+2,b);
+//		h[x]=h[x]>b?h[x]:b;
+//		m=m>h[x]?m:h[x];
+		if(x>=n)m=m>b?m:b;
+		else {
+			o(x+1,b);
+			o(x+2,b+a[x]);
+		}
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
