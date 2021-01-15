92463a28d4a7d71ea0492712474a8e0abf3114ef
nn98 <jkllhgb@gmail.com>
2021-01-15 22:35:10 +0900

    19621

--- src/_Algorithm/DP/P19621.java
+++ src/_Algorithm/DP/P19621.java
@@ -1,10 +1,27 @@
 package _Algorithm.DP;
 
+import java.util.Scanner;
+
 public class P19621 {
 
+	static int n,a[],h[],i;
+	static void o(int x,int b) {
+		h[x]=b;
+		o(x+1,b);
+		if(h[x+2]<b+a[x])
+			o(x+2,b+a[x]);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		while(i<n) {
+			s.next();
+			s.next();
+			a[i++]=s.nextInt();
+		}
+		for(i=0;i<n;o(i++,0));
 	}
 
 }
