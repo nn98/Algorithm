449deec5eaa5d9e77ac415c8daf489b44e7b30ad
nn98 <jkllhgb@gmail.com>
2021-01-17 23:59:25 +0900

    19622

--- src/_Algorithm/DP/P19622.java
+++ src/_Algorithm/DP/P19622.java
@@ -6,8 +6,12 @@ public class P19622 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[][]=new int[n+1][3],i=0;
+		int n=s.nextInt(),i=0;
+		long a[][]=new long[n+1][3],d[][]=new long[1000001][2];
 		for(;i++<n;a[i][0]=s.nextInt(),a[i][1]=s.nextInt(),a[i][2]=s.nextInt());
+		d[1][1]=a[1][3];
+		d[2][1]=a[2][3];
+		d[2][0]=a[1][3];
 	}
 
 }
