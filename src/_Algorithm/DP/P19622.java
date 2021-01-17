3f8f96a20766394be5042207b67e177cd30b47af
nn98 <jkllhgb@gmail.com>
2021-01-18 03:26:04 +0900

    19622

--- src/_Algorithm/DP/P19622.java
+++ src/_Algorithm/DP/P19622.java
@@ -12,7 +12,7 @@ public class P19622 {
 		d[1][1]=a[1][2];
 		d[2][1]=a[2][2];
 		d[2][0]=a[1][2];
-		for(i=0;i++<3;d[i][0]=Math.max(d[i-1][0],d[i-1][1]),d[i][1]=d[i-1][0]+a[i][2]);
+		for(i=0;i++<n;d[i][0]=Math.max(d[i-1][0],d[i-1][1]),d[i][1]=d[i-1][0]+a[i][2]);
 		System.out.println(Math.max(d[n][0],d[n][1]));
 	}
 
