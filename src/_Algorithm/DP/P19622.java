95a931c5d75bb33c246fcc66379ef61b631b67c3
nn98 <jkllhgb@gmail.com>
2021-01-18 00:08:09 +0900

    19622

--- src/_Algorithm/DP/P19622.java
+++ src/_Algorithm/DP/P19622.java
@@ -9,10 +9,10 @@ public class P19622 {
 		int n=s.nextInt(),i=0;
 		long a[][]=new long[n+1][3],d[][]=new long[1000001][2];
 		for(;i++<n;a[i][0]=s.nextInt(),a[i][1]=s.nextInt(),a[i][2]=s.nextInt());
-		d[1][1]=a[1][3];
-		d[2][1]=a[2][3];
-		d[2][0]=a[1][3];
-		for(i=0;i++<3;d[i][0]=Math.max(d[i-1][0],d[i-1][1]),d[i][1]=d[i-1][0]+a[i][3]);
+		d[1][1]=a[1][2];
+		d[2][1]=a[2][2];
+		d[2][0]=a[1][2];
+		for(i=0;i++<3;d[i][0]=Math.max(d[i-1][0],d[i-1][1]),d[i][1]=d[i-1][0]+a[i][2]);
 		System.out.println(Math.max(d[n][0],d[n][1]));
 	}
 
