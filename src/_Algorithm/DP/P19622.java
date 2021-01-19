94969d99316043792d58670b49b621e566e694f9
nn98 <jkllhgb@gmail.com>
2021-01-20 02:51:33 +0900

    19622

--- src/_Algorithm/DP/P19622.java
+++ src/_Algorithm/DP/P19622.java
@@ -12,11 +12,15 @@ public class P19622 {
 		long a[][]=new long[n+2][3],d[][]=new long[1000001][2];
 //		a 길이를 n+1로하면 인덱스 에러
 //		??
+		
 //		for(;i++<n;a[i][0]=s.nextInt(),a[i][1]=s.nextInt(),a[i][2]=s.nextInt());
+//		회의 시작/끝 시간은 필요도 없고 가중치만
 		for(;i++<n;s.next(),s.next(),a[i][2]=s.nextInt());
+		
 		d[1][1]=a[1][2];
 		d[2][1]=a[2][2];
 		d[2][0]=a[1][2];
+//		dp 배열 
 		for(i=0;i++<n;d[i][0]=Math.max(d[i-1][0],d[i-1][1]),d[i][1]=d[i-1][0]+a[i][2]);
 		System.out.println(Math.max(d[n][0],d[n][1]));
 	}
