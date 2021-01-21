110095dd95ee6ebfced4e329706c38e495bac19e
nn98 <jkllhgb@gmail.com>
2021-01-21 23:49:28 +0900

    git readme

--- src/BaekJoon_Git.java
+++ src/BaekJoon_Git.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class BaekJoon_Git {
 	public static void main(String[]z) {
 		Scanner s=new Scanner(System.in);
-		while(s.hasNext()) {
-			String[]a=s.nextLine().split(" ");
-			for(String b:a)System.out.print(String.format("[%s](https://www.acmicpc.net/problem/%s)",a,a));
-			System.out.println();
-		}
+		String[]a=s.nextLine().split(" ");
+		for(int i=0;i<a.length;System.out.print(String.format("[%s](https://www.acmicpc.net/problem/%s)",a[i],a[i])+((i+1)%10==0?"\n":" ")));
+
 	}
 }
