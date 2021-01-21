de09160b132146e0d71efb35bcf4451155551633
nn98 <jkllhgb@gmail.com>
2021-01-21 22:38:49 +0900

    17389

--- src/_Algorithm/Simple_Implementation/P17389.java
+++ src/_Algorithm/Simple_Implementation/P17389.java
@@ -7,11 +7,12 @@ public class P17389 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		s.next();
-		int r=0,c=0;
+		int r=0,c=0,i=0;
 		for(char a:s.next().toCharArray()) {
+			i++;
 			switch(a) {
 			case 'O':
-				r+=++c;
+				r+=i+c++;
 				break;
 			default:
 				c=0;
