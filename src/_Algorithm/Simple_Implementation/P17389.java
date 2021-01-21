560333a3b46099b5bc3967bacc848ed185049a0a
nn98 <jkllhgb@gmail.com>
2021-01-21 22:34:13 +0900

    17389

--- src/_Algorithm/Simple_Implementation/P17389.java
+++ src/_Algorithm/Simple_Implementation/P17389.java
@@ -1,10 +1,23 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P17389 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		s.next();
+		int r=0,c=0;
+		for(char a:s.next().toCharArray()) {
+			switch(a) {
+			case 'O':
+				r+=++c;
+				break;
+			default:
+				c=0;
+			}
+		}
+		System.out.println(r);
 	}
 
 }
