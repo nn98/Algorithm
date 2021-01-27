3607520eda72905cf7f79c66c588f0d2cfab7c77
nn98 <jkllhgb@gmail.com>
2021-01-27 23:30:32 +0900

    2268

--- src/_Algorithm/Segment_Tree/P2268.java
+++ src/_Algorithm/Segment_Tree/P2268.java
@@ -1,9 +1,28 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2268 {
 
+	static int n,m,i,j,x,y,l,a[];
+	static void o() {
+		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
+	}
+	static long p(int n,int s,int e) {
+		if(s>y|x>e)return 0;
+		if(x<=s&y<=e)return a[n];
+		int m=(s+e)/2;
+		return p(n*2,s,m)+p(n*2+1,m+1,e);
+	}
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		for(l=1;l<=n;l*=2);
+		a=new int[l/=2];
+		System.out.println(Arrays.toString(a));
+		for(;i<n;a[i+++l]=s.nextInt());
 	}
 
 }
