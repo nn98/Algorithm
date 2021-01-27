e2d0eaab723798801a108f03da1917dfc2730d97
nn98 <jkllhgb@gmail.com>
2021-01-28 05:31:00 +0900

    2268

--- src/_Algorithm/Segment_Tree/P2268_2.java
+++ src/_Algorithm/Segment_Tree/P2268_2.java
@@ -1,10 +1,43 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class P2268_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j,x,y,l,a[];
+	static void o() {
+		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
+	}
+	static long p(int n,int s,int e) {
+		if(s>y|x>e)return 0;
+		if(x<=s&e<=y)return a[n];
+		int m=(s+e)/2;
+		return p(n*2,s,m)+p(n*2+1,m+1,e);
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=s.nextInt();
+		m=s.nextInt();
+		for(l=1;l<n;l*=2);
+		a=new int[l*2];
+		while(m-->0) {
+			x=s.nextInt();
+			if(x>0) {
+				a[j=s.nextInt()-1+l]=s.nextInt();
+				o();
+			}else {
+				x=s.nextInt();
+				y=s.nextInt();
+				System.out.println(p(1,1,l));
+			}
+		}
+		
 	}
 
 }
