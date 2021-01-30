59794c4c80b17e4918f658ddac4a9ae94eade3f3
nn98 <jkllhgb@gmail.com>
2021-01-30 23:28:59 +0900

    15560

--- src/_Algorithm/Segment_Tree/P15560.java
+++ src/_Algorithm/Segment_Tree/P15560.java
@@ -3,8 +3,7 @@ package _Algorithm.Segment_Tree;
 public class P15560 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(Sysetm.in);
 	}
 
 }
