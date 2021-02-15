a18ba4f7c65c72136456cfeb8f2aaf34a5fdd0b7
nn98 <jkllhgb@gmail.com>
2021-02-15 23:24:17 +0900

    15560

--- src/_Algorithm/Segment_Tree/P15560_2.java
+++ src/_Algorithm/Segment_Tree/P15560_2.java
@@ -1,10 +1,24 @@
 package _Algorithm.Segment_Tree;
 
+import java.util.Scanner;
+
 public class P15560_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),u=s.nextInt(),v=s.nextInt(),i=0,a[]=new int[1001],j,k,r;
+		for(;i++<n;a[i]=u*s.nextByte()+v);
+		while(m-->0) {
+			n=s.nextInt();
+			i=s.nextInt();
+			j=s.nextInt();
+			if(n>0)a[i]=u*j+v;
+			else {
+				r=n=Integer.MIN_VALUE;
+				for(k=i;k<=j;n=Math.max(n,r=Math.max(r,0)+a[k]),k++);
+				System.out.println(n-v);
+			}
+		}
 	}
 
 }
