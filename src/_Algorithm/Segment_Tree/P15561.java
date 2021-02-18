869053902a5466e1cfd37192df350100bba3f9c4
nn98 <jkllhgb@gmail.com>
2021-02-18 23:55:00 +0900

    15561

--- src/_Algorithm/Segment_Tree/P15561.java
+++ src/_Algorithm/Segment_Tree/P15561.java
@@ -7,6 +7,7 @@ public class P15561 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
