711a6a1d05a901af93c578f41646002e1b34d34b
nn98 <jkllhgb@gmail.com>
2021-02-19 23:47:06 +0900

    15561

--- src/_Algorithm/Segment_Tree/P15561.java
+++ src/_Algorithm/Segment_Tree/P15561.java
@@ -5,6 +5,16 @@ import java.util.*;
 
 public class P15561 {
 
+	static int n,m,i,j,x,y,l,p,q,a[];
+	static void o() {
+		for(;i>1;i/=2,a[i]=a[i*2]+a[i*2+1]);
+	}
+	static int p(int n,int s,int e) {
+		if(e<x|y<s)return 0;
+		if(x<=s&e<=y)return a[n];
+		int m=(s+e)/2;
+		return p(n*2,s,m)+p(n*2+1,m+1,e);
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
