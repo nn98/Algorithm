0b6c5b867acc76e621cffebd6574d7e05e16f1ad
nn98 <jkllhgb@gmail.com>
2021-02-25 23:55:28 +0900

    15561

--- src/_Algorithm/Segment_Tree/P15561_2.java
+++ src/_Algorithm/Segment_Tree/P15561_2.java
@@ -1,10 +1,26 @@
 package _Algorithm.Segment_Tree;
 
 public class P15561_2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j,l,x,y,a[];
+	static class N{
+	  int l,r,v,a;
+	  N(int q,int w,int e,int s){
+	    l=q;
+	    r=w;
+	    v=e;
+	    a=s;
+	   }
+	 }
+	static N[]b;
+	public static void main(String[]z){
+	  Scanner s=new Scanner(System.in);
+	  n=s.nextInt();
+	  l=1;
+	  for(;l<=n;l*=2);
+	  a=new int[n];
+	  b=new N[n];
+	  for(;i<n;a[i++]=s.nextInt());
 	}
+	
 
 }
