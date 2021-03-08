6e97422646689a580b87eaec9783cc03d2c565a1
nn98 <jkllhgb@gmail.com>
2021-03-08 18:12:24 +0900

    1305

---
+++ src/_Algorithm/String/P1305.java
@@ -0,0 +1,26 @@
+package _Algorithm.String;
+
+import java.util.Scanner;
+
+public class P1305 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j,x,y,c,l=0;
+		StringBuffer f=new StringBuffer(s.nextLine());
+		for(;i<f.length();i++)
+			for(j=i+1;j<f.length();j++) {
+				if(f.charAt(i)==f.charAt(j)) {
+					x=i;
+					y=j;
+					c=1;
+					while(x<j&c>0) {
+						if(f.charAt(x++)!=f.charAt(y++))c--;
+					}
+					l=j-i;
+				}
+			}
+		System.out.println(l);
+	}
+
+}
