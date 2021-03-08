6b632d076503a9e6e7a3cd4d8ef913d45697febc
nn98 <jkllhgb@gmail.com>
2021-03-08 18:12:30 +0900

    1305

--- src/_Algorithm/String/P1305.java
+++ src/_Algorithm/String/P1305.java
@@ -7,7 +7,6 @@ public class P1305 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j,x,y,c,l=0;
-		s.next();
 		StringBuffer f=new StringBuffer(s.next());
 		for(;i<f.length();i++,System.out.println(1))
 			for(j=i+1;j<f.length();j++,System.out.println(2)) {
@@ -15,7 +14,7 @@ public class P1305 {
 					x=i;
 					y=j;
 					c=1;
-					while(x<j&c>0) {
+					while(x<j&c>0&y<f.length()) {
 						System.out.println(3);
 						if(f.charAt(x++)!=f.charAt(y++))c--;
 					}
