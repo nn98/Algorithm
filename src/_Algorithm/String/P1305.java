074c9e6d6b2b05fe91fa0e7b45cf3a5fe94e9254
nn98 <jkllhgb@gmail.com>
2021-03-09 23:32:42 +0900

    1305

--- src/_Algorithm/String/P1305.java
+++ src/_Algorithm/String/P1305.java
@@ -8,7 +8,6 @@ public class P1305 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j,x,y,c,l=0;
 		StringBuffer f=new StringBuffer(s.next());
-//		,System.out.println(2)
 		for(;i<f.length();i++)
 			for(j=i+1;j<f.length();j++) {
 				if(f.charAt(i)==f.charAt(j)) {
@@ -16,7 +15,6 @@ public class P1305 {
 					y=j;
 					c=1;
 					while(x<j&c>0&y<f.length()) {
-//						System.out.println(3);
 						if(f.charAt(x++)!=f.charAt(y++))c--;
 					}
 					l=j-i;
@@ -24,5 +22,5 @@ public class P1305 {
 			}
 		System.out.println(l);
 	}
-
 }
+//KMP Alg
