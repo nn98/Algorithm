20fdd8fce1aecec75205ee85ec437fff526b0c10
nn98 <jkllhgb@gmail.com>
2021-03-11 23:08:18 +0900

    1786

--- src/_Algorithm/String/KMP/P1786.java
+++ src/_Algorithm/String/KMP/P1786.java
@@ -1,10 +1,13 @@
 package _Algorithm.String.KMP;
 
+import java.util.Scanner;
+
 public class P1786 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String t=s.nextLine(),p=s.nextLine();
+		
 	}
 
 }
