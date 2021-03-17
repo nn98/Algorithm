59ee9ff083796a499dbb3e30b29195af2f777759
nn98 <jkllhgb@gmail.com>
2021-03-17 22:35:45 +0900

    1305

--- src/_Algorithm/String/KMP/P1305_2.java
+++ src/_Algorithm/String/KMP/P1305_2.java
@@ -1,10 +1,13 @@
 package _Algorithm.String.KMP;
 
+import java.util.Scanner;
+
 public class P1305_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		String a=s.next();
 	}
 
 }
