0563eedb145c6fe38aa2c018cf79eb5ef9d38893
nn98 <jkllhgb@gmail.com>
2021-03-20 23:56:50 +0900

    KMP

--- src/_Algorithm/String/KMP/KMP.java
+++ src/_Algorithm/String/KMP/KMP.java
@@ -1,15 +1,52 @@
 package _Algorithm.String.KMP;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class KMP {
 
 	static String[]H,S;
-	static int n,t[]; 
+	static int n,f[],r[];
+	
+	static void F() {
+		int n=S.length,be=1,m=0;
+		f=new int[n];
+		while(be+m<n) {
+			if(S[be+m]==S[m]) {
+				m++;
+				f[be+m-1]=m;
+			}else
+				if(m==0)be++;
+				else {
+					be+=(m-f[m-1]);
+					m=f[m-1];
+				}
+		}
+	}
+	
+	static void K() {
+		int h=H.length,s=S.length,be=0,m=0,i=0;
+		r=new int[h-s+1];
+		while(be<=h-s) {
+			if(m<s&H[be+m]==S[m]) {
+				m++;
+				if(m==s)r[i++]=be;
+			}else
+				if(m==0)be++;
+				else {
+					be+=(m-f[m-1]);
+					m=f[m-1];
+				}
+		}
+	}
 	
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		
+		H=s.next().split("");
+		S=s.next().split("");
+		F();
+		K();
+		System.out.println(Arrays.toString(r));
 //		String[]a=s.next().split(""),b=s.next().split("");
 //		int n=s.nextInt(),h=a.length,g=b.length,f[]=new int[n],
 //				r[]=new int[h-g+1],be=1,m=0,i=0,F=0,N=g;
