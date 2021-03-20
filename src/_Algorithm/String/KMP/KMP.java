cf5a7864347f77f3ea8f25938aa309be37b6797c
nn98 <jkllhgb@gmail.com>
2021-03-20 23:50:08 +0900

    KMP

--- src/_Algorithm/String/KMP/KMP.java
+++ src/_Algorithm/String/KMP/KMP.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 
 public class KMP {
 
+	static String[]H,S;
+	static int n,t[]; 
+	
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		String[]a=s.next().split(""),b=s.next().split("");
-		int n=s.nextInt(),h=a.length,g=b.length,f[]=new int[n],r[]=new int[h-g+1],be=0,m=0,i=0;
-		while(be<=h-g) {
-//			일치 개수 m < 패턴 길이 g
-//			a[be+m]과 문자 일치
-			if(m<g&a[be+m]==b[m]) {
-				m++;
-				if(m==g)r[i++]=be;
-			}else
-				if(m==0)be++;
-				else {
-					be+=(m-f[m-1]);
-					m=f[m-1];
-				}
-		}
+		
+//		String[]a=s.next().split(""),b=s.next().split("");
+//		int n=s.nextInt(),h=a.length,g=b.length,f[]=new int[n],
+//				r[]=new int[h-g+1],be=1,m=0,i=0,F=0,N=g;
+//		while(be+m<N) {
+//			if()
+//		}
+//		while(be<=h-g) {
+////			일치 개수 m < 패턴 길이 g
+////			a[be+m]과 문자 일치
+//			if(m<g&a[be+m]==b[m]) {
+//				m++;
+//				if(m==g)r[i++]=be;
+//			}else
+//				if(m==0)be++;
+//				else {
+//					be+=(m-f[m-1]);
+//					m=f[m-1];
+//				}
+//		}
 	}
 
 }
