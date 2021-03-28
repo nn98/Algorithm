7ea4ebe0afd4a6cf6b230c9ad86af8d7b7ec1c5f
nn98 <jkllhgb@gmail.com>
2021-03-28 23:44:55 +0900

    1092

--- src/_Algorithm/Greedy/P1092.java
+++ src/_Algorithm/Greedy/P1092.java
@@ -1,9 +1,19 @@
 package _Algorithm.Greedy;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class P1092 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),a[]=new int[n],m,b[],i=0,j=0;
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		m=Integer.parseInt(r.readLine());
+		b=new int[m];
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		t=new StringTokenizer(r.readLine());
+		for(;j<n;a[j++]=Integer.parseInt(t.nextToken()));
 	}
 
 }
