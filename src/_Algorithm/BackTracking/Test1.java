73fa1b7e326750c766ebcc0862c470519bd9df4f
nn98 <jkllhgb@gmail.com>
2021-04-06 23:28:37 +0900

    1759

--- src/_Algorithm/BackTracking/Test1.java
+++ src/_Algorithm/BackTracking/Test1.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Test1 {
 
-	static int n,m,b[];
+	static int n,m,b[],j;
 	static char[]a;
-	static void o(int n) {
-		
+	static void o(int x) {
+		if(x<m)
+			for(int i=0;i++<n;) {
+				a[x]=(char) ('0'+i);
+				o(x+1);
+			}
+		else System.out.println(a);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
