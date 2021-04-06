975959f7bfda27070f0215645cad91ee97a298d8
nn98 <jkllhgb@gmail.com>
2021-04-06 23:28:35 +0900

    1759

--- src/_Algorithm/BackTracking/Test1.java
+++ src/_Algorithm/BackTracking/Test1.java
@@ -1,10 +1,19 @@
 package _Algorithm.BackTracking;
 
+import java.util.Scanner;
+
 public class Test1 {
 
+	static int n,m,b[];
+	static char[]a;
+	static void o(int n) {
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		b=new int[n];
 	}
 
 }
