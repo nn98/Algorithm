fba2dc2e741e9316dddc21660ec7dd0aa9328379
nn98 <jkllhgb@gmail.com>
2021-04-07 12:59:45 +0900

    1759

--- src/_Algorithm/BackTracking/P1759.java
+++ src/_Algorithm/BackTracking/P1759.java
@@ -1,22 +1,30 @@
 package _Algorithm.BackTracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class P1759 {
 
-	static int n,m,i,c[];
+	static int n,m,i,c[],d[]=new int[2];
 	static char[]a,b;
+	static List<Character>l=new ArrayList();
 	
 	static void o(int x) {
 		if(x<n) {
 			for(int i=0;i<m;i++)
 				if(c[i]<1) {
 					c[i]++;
+					if(l.contains(a[i]))d[0]++;
+					else d[1]++;
 					b[x]=a[i];
 					o(x+1);
 					c[i]--;
+					if(l.contains(a[i]))d[0]--;
+					else d[1]--;
 				}
-		}else System.out.println(b);
+		}else if(d[0]>0&d[1]>1)System.out.println(b);
 	}
 	
 	public static void main(String[] args) {
@@ -24,9 +32,15 @@ public class P1759 {
 		n=s.nextInt();
 		m=s.nextInt();
 		c=new int[m];
-		b=new char[m];
+		b=new char[n];
 		a=new char[m];
 		for(;i<m;a[i++]=s.next().charAt(0));
+		l.add('a');
+		l.add('e');
+		l.add('i');
+		l.add('o');
+		l.add('u');
+		Arrays.sort(a);
 		o(0);
 	}
 
