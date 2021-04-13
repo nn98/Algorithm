c78ad2a3ba4d0b83158fd45f3f93fd37ef6ed9f0
nn98 <jkllhgb@gmail.com>
2021-04-13 23:28:30 +0900

    2243

--- src/_Algorithm/Segment_Tree/P2243.java
+++ src/_Algorithm/Segment_Tree/P2243.java
@@ -1,9 +1,24 @@
 package _Algorithm.Segment_Tree;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class P2243 {
 
-	public static void main(String[] args) {
-		
+	static int n,m,i,j,p,a[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		while(n-->0) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			switch(t.nextToken()) {
+			case "1":
+				break;
+			case "2":
+				
+			}
+		}
 	}
 
 }
