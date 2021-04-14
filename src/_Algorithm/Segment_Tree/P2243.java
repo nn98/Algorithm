de636c1e4eb6bd51a45a24154010d96d86cc9ce3
nn98 <jkllhgb@gmail.com>
2021-04-15 04:26:55 +0900

    2243

--- src/_Algorithm/Segment_Tree/P2243.java
+++ src/_Algorithm/Segment_Tree/P2243.java
@@ -9,6 +9,9 @@ public class P2243 {
 	static void o() {
 		for(;i>1;i/=2,a[i]=a[i*2]+a[i*2+1]);
 	}
+	static int p(int n,int s,int e) {
+		return 0;
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
