eb5957b5991e4551894540a0079f76bc3cbdce8b
nn98 <jkllhgb@gmail.com>
2021-04-18 23:52:28 +0900

    2243

--- src/_Algorithm/Segment_Tree/P2243.java
+++ src/_Algorithm/Segment_Tree/P2243.java
@@ -9,6 +9,12 @@ public class P2243 {
 	static int p(int n,int s,int e) {
 		return 0;
 	}
+	static int q(int n,int t,int s,int e) {
+		if(s==e)return s;
+		int m=(s+e)/2;
+		if(t<=a[n*2])return q(n*2,t,s,m);
+		return q(n*2+1,t-a[n*2],m+1,e);
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
