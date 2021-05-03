ec5609af4117cb4028eacca34f4d8ae3fd99ac04
nn98 <jkllhgb@gmail.com>
2021-05-03 23:46:54 +0900

    1305

--- src/_Algorithm/String/KMP/P1305_2.java
+++ src/_Algorithm/String/KMP/P1305_2.java
@@ -1,8 +1,11 @@
 package _Algorithm.String.KMP;
 import java.util.Scanner;
 public class P1305_2 {
-	static void o() {
-		
+	static int[] k(String H,String S) {
+		int h=H.length(),s=S.length(),f[],r[],be=0,m=0;
+		while(be<=h-s) {
+			m++;
+		}
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
