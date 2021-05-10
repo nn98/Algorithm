60beb5ee49c4539e40d13bf69e6342c6e51af12b
nn98 <jkllhgb@gmail.com>
2021-05-10 23:29:30 +0900

    1305

--- src/_Algorithm/String/KMP/P1305_2.java
+++ src/_Algorithm/String/KMP/P1305_2.java
@@ -1,7 +1,7 @@
 package _Algorithm.String.KMP;
 import java.util.Scanner;
 public class P1305_2 {
-	static int[] k(char[]H,char[]S) {
+	static int[] k(char[]H,char[]S){
 		int h=H.length,s=S.length,f[],r[]=new int[h-s+1],be=0,m=0;
 		while(be<=h-s) {
 			if(m<s&H[be+m]==S[m])
@@ -11,9 +11,10 @@ public class P1305_2 {
 	}
 	static int[]r(char[]p){
 		int[]r=new int[p.length];
+		
 		return r;
 	}
-	public static void main(String[] args) {
+	public static void main(String[]Z){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j,f[]=new int[n];
 		String[]a=s.next().split(""),t=new String[n];
