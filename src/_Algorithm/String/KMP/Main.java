1215cc2e055d2394b0fc205a603d45c6bc61d799
nn98 <jkllhgb@gmail.com>
2021-06-05 19:22:45 +0900

    1786

--- src/_Algorithm/String/KMP/Main.java
+++ src/_Algorithm/String/KMP/Main.java
@@ -1,10 +1,29 @@
 package _Algorithm.String.KMP;
 
+import java.util.Scanner;
+
+//P1786
 public class Main {
 
+	static int[]getP(char[]p){
+		int m=p.length,j=0;
+		int[]pi=new int[m];
+		for(int i=1;i<m;i++) {
+			while(j>0&p[i]!=p[j])
+				j=pi[j-1];
+			if(p[i]==p[j])
+				pi[i]=++j;
+		}
+		return pi;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		char[]c=s.nextLine().toCharArray(),p=s.nextLine().toCharArray();
+		System.out.println(getP(p));
+		
+		
+		
 	}
 
 }
