cb675b4bfc42b0a66d1c9650be3b848825801442
nn98 <jkllhgb@gmail.com>
2021-06-06 18:41:18 +0900

    1305

--- src/_Algorithm/String/KMP/P1305_3.java
+++ src/_Algorithm/String/KMP/P1305_3.java
@@ -4,11 +4,22 @@ import java.util.Scanner;
 
 public class P1305_3 {
 
-	
+	static int n,i,j,b[];
+	static char[]a;
+	static void o() {
+		b=new int[n];
+		for(;i<n;) {
+			i++;
+			while(j>0&a[i]!=a[j])j=b[j];
+			if(a[i]==a[j]) {
+				b[i]=++j;
+			}
+		}
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		char[]a=s.next().toCharArray();
+		n=s.nextInt();
+		a=s.next().toCharArray();
 		
 	}
 
