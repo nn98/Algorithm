f780afe81d491ab560e54fa8d2c1c17edb840162
nn98 <jkllhgb@gmail.com>
2021-06-08 18:50:43 +0900

    KMP

--- src/_Algorithm/String/KMP/GetPi.java
+++ src/_Algorithm/String/KMP/GetPi.java
@@ -1,10 +1,19 @@
 package _Algorithm.String.KMP;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GetPi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j=0,pi[]=new int[n];
+		char[]a=s.next().toCharArray();
+		for(;++i<n;) {
+			while(j>0&a[i]!=a[j])j=pi[j];
+			if(a[i]==a[j])pi[i]=++j;
+		}
+		System.out.println(Arrays.toString(pi));
 	}
 
 }
