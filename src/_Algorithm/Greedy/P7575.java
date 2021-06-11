0c15bc08d36f1dde89885743032b60bffb5469dc
nn98 <jkllhgb@gmail.com>
2021-06-11 23:52:02 +0900

    7575

--- src/_Algorithm/Greedy/P7575.java
+++ src/_Algorithm/Greedy/P7575.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class P7575 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),k=Integer.parseInt(t.nextToken()),
+				i=0,j;
+		
 	}
 
 }
