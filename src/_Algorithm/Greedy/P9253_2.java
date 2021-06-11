2893dc79eb5102081ac380b2c0721cd72af9f571
nn98 <jkllhgb@gmail.com>
2021-06-12 00:17:38 +0900

    9253

--- src/_Algorithm/Greedy/P9253_2.java
+++ src/_Algorithm/Greedy/P9253_2.java
@@ -1,10 +1,23 @@
 package _Algorithm.Greedy;
 
+import java.io.*;
+
 public class P9253_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static char[]a,b,p;
+	static int i,j,l,pi[];
+	static void g() {
+		for(;i<l;) {
+			while(j>0&p[j]!=p[i])j=pi[j-1];
+			if(p[j]==p[i])pi[j]=++j;
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		a=r.readLine().toCharArray();
+		b=r.readLine().toCharArray();
+		p=r.readLine().toCharArray();
+		l=p.length;
 	}
 
 }
