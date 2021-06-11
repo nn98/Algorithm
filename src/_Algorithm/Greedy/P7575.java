148fb81c9ed9e408c21dfe1d2391b596dc6458eb
nn98 <jkllhgb@gmail.com>
2021-06-11 23:49:54 +0900

    7575

--- src/_Algorithm/Greedy/P7575.java
+++ src/_Algorithm/Greedy/P7575.java
@@ -1,9 +1,15 @@
 package _Algorithm.Greedy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P7575 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 
 	}
 
