e9037ed483eaa9c965c578d3215bffcdab7d3c0c
nn98 <jkllhgb@gmail.com>
2021-06-12 00:08:23 +0900

    9253

--- src/_Algorithm/Greedy/P9253.java
+++ src/_Algorithm/Greedy/P9253.java
@@ -9,7 +9,7 @@ public class P9253 {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		char[][]a=new char[3][];
 		int i=0,j=0,k=0,b[]=new int[3],x=0;
-		for(;i<3;a[i]=r.readLine().toCharArray(),b[i++]=a[i].length);
+		for(;i<3;a[i]=r.readLine().toCharArray(),b[i]=a[i++].length);
 		for(i=0;i<b[0]&j<b[1]&x<1;) {
 			while(a[0][i]==a[2][k])i++;
 			while(a[1][j]==a[2][k])j++;
