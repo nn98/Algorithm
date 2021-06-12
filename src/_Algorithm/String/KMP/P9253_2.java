78e748fb7b3b4ee55771070bbd16ffbb81e3e435
nn98 <jkllhgb@gmail.com>
2021-06-12 23:50:41 +0900

    9253

--- src/_Algorithm/String/KMP/P9253_2.java
+++ src/_Algorithm/String/KMP/P9253_2.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class P9253_2 {
 
 	static char[]a,b,p;
-	static int i,j,l,pi[];
+	static int i,j,l,pi[],r[];
 	static void g() {
 		for(;++i<l;) {
 			while(j>0&p[j]!=p[i])j=pi[j-1];
@@ -15,17 +15,17 @@ public class P9253_2 {
 	}
 //	KMP
 	static void K() {
-		int h=H.length,s=S.length,be=0,m=0,i=0;
+		int h=a.length,s=p.length,be=0,m=0,i=0;
 		r=new int[h-s+1];
 		while(be<=h-s) {
-			if(m<s&H[be+m]==S[m]) {
+			if(m<s&a[be+m]==p[m]) {
 				m++;
 				if(m==s)r[i++]=be;
 			}else
 				if(m==0)be++;
 				else {
-					be+=(m-f[m-1]);
-					m=f[m-1];
+					be+=(m-pi[m-1]);
+					m=pi[m-1];
 				}
 		}
 	}
