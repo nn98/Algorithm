ddd63a4e7b2748f1ba4e67a79d822d64685e4f63
nn98 <jkllhgb@gmail.com>
2021-06-14 18:38:30 +0900

    9253

--- src/_Algorithm/String/KMP/P9253.java
+++ src/_Algorithm/String/KMP/P9253.java
@@ -10,12 +10,14 @@ public class P9253 {
 		char[][]a=new char[3][];
 		int i=0,j,k=0,b[]=new int[3],c[]=new int[2];
 		for(;i<3;a[i]=r.readLine().toCharArray(),b[i]=a[i++].length);
+		System.out.println(b[2]);
 		for(i=0;i<2;i++)
 			for(j=0;j<b[i]&c[i]<1;j++) {
 				if(a[i][j]==a[2][k]) {
 					k++;
 					if(k==b[2])c[i]++;
 				}
+				System.out.println(k+" "+c[i]);
 			}
 		System.out.print(c[0]>0&c[1]>0?"YES":"NO");
 	}
