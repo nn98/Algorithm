72796202f6676dc1038e42005c432c2527a0a8d0
nn98 <jkllhgb@gmail.com>
2021-06-14 19:44:47 +0900

    KMP

--- src/_Algorithm/String/KMP/KMP_2.java
+++ src/_Algorithm/String/KMP/KMP_2.java
@@ -1,10 +1,27 @@
 package _Algorithm.String.KMP;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class KMP_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static char[]s,p;
+	static int i,j,pi[],S,P;
+	static void g() {
+		for(;i<P;i++) {
+			while(j>0&p[i]!=p[j])j=pi[j-1];
+			if(p[i]==p[j])pi[i]=++j;
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		s=r.readLine().toCharArray();
+		p=r.readLine().toCharArray();
+		S=s.length;
+		P=p.length;
+		pi=new int[P];
+		g();
+		System.out.println(Arrays.toString(pi));
 	}
 
 }
