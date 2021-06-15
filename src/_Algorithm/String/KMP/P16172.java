6ae270750e36c1e4017adcbab0cfe257a89d8cd0
nn98 <jkllhgb@gmail.com>
2021-06-15 19:00:47 +0900

    16172

--- src/_Algorithm/String/KMP/P16172.java
+++ src/_Algorithm/String/KMP/P16172.java
@@ -8,7 +8,7 @@ public class P16172 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String a=s.next(),b=s.next();
-		a=a.replaceAll("^[0-9]*$","");
+		a=a.replaceAll("[0-9]","");
 		System.out.println(a);
 	}
 
