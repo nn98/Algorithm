11eaa09e67f5162599887c1e9e0064c538fd731c
nn98 <jkllhgb@gmail.com>
2021-06-15 21:51:41 +0900

    4766

--- src/_Algorithm/Math/P4766.java
+++ src/_Algorithm/Math/P4766.java
@@ -6,9 +6,9 @@ public class P4766 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int i,j=-11;
-		while((i=s.nextInt())!=999) {
-			if(j>-11)System.out.println(i-j);
+		double i,j=-11;
+		while((i=s.nextDouble())!=999) {
+			if(j>-11)System.out.println(String.format("%.2f",i-j));
 			j=i;
 		}
 	}
