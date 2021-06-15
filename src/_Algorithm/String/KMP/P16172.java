b59555e56bc5d3fd70578f62a1cb3b69e95b001e
nn98 <jkllhgb@gmail.com>
2021-06-15 18:48:11 +0900

    16172

--- src/_Algorithm/String/KMP/P16172.java
+++ src/_Algorithm/String/KMP/P16172.java
@@ -1,10 +1,14 @@
 package _Algorithm.String.KMP;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class P16172 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String a=s.next().replaceAll("^[0-9]*$",""),b=s.next();
+		System.out.println(a);
 	}
 
 }
