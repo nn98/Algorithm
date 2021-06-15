ecce3732eeed9c3023396d818d6b54f9577fe7ad
nn98 <jkllhgb@gmail.com>
2021-06-15 19:00:52 +0900

    16172

--- src/_Algorithm/String/KMP/P16172.java
+++ src/_Algorithm/String/KMP/P16172.java
@@ -4,12 +4,36 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class P16172 {
+	
+	static char[]a,b;
+	static int i,j,p[],A,B;
+	
+	static void g() {
+		for(;++i<B;) {
+			while(j>0&b[i]!=b[j])j=p[j-1];
+			if(b[i]==b[j])p[i]=++j;
+		}
+	}
 
+	static void k() {
+		i=j=0;
+		for(;i<A;i++) {
+			while(j>0&a[i]!=b[j])j=p[j-1];
+			if(j==B-1)return;
+			else j++;
+		}
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		String a=s.next(),b=s.next();
-		a=a.replaceAll("[0-9]","");
-		System.out.println(a);
+		String S=s.next();
+		S=S.replaceAll("[0-9]","");
+		a=S.toCharArray();
+		b=s.next().toCharArray();
+		A=a.length;
+		B=b.length;
+		g();
+		k();
+		System.out.print(j==B-1?1:0);
 	}
 
 }
