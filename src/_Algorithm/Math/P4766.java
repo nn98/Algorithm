e9d00edaeaa537b5962d29f9b6920c6d0e351ddf
nn98 <jkllhgb@gmail.com>
2021-06-15 19:30:28 +0900

    4766

--- src/_Algorithm/Math/P4766.java
+++ src/_Algorithm/Math/P4766.java
@@ -1,10 +1,16 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P4766 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i,j=-11;
+		while((i=s.nextInt())!=999) {
+			if(j>-11)System.out.println(i-j);
+			j=i;
+		}
 	}
 
 }
