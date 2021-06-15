2bf97c3385f6cf7756915e9dd4f69b928fffdc32
nn98 <jkllhgb@gmail.com>
2021-06-15 21:55:15 +0900

    2846

--- src/_Algorithm/Simple_Implementation/P2846.java
+++ src/_Algorithm/Simple_Implementation/P2846.java
@@ -1,10 +1,13 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P2846 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0;
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
