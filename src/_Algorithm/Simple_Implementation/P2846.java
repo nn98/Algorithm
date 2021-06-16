616a4a18ed53fe1fd43159ae3ab75f2c3a7f3ccf
nn98 <jkllhgb@gmail.com>
2021-06-16 18:56:22 +0900

    2846

--- src/_Algorithm/Simple_Implementation/P2846.java
+++ src/_Algorithm/Simple_Implementation/P2846.java
@@ -6,8 +6,18 @@ public class P2846 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],i=0;
-		for(;i<n;a[i++]=s.nextInt());
+		int n=s.nextInt(),a[]=new int[n],i=0,j=0,r=0;
+		for(;i<n;i++) {
+			a[i]=s.nextInt();
+			if(j==0)j=a[i];
+			if(i>0)
+				if(a[i]<=a[i-1]) {
+					r=r>a[i]-j?r:a[i]-j;
+					j=a[i];
+				}
+		}
+		r=r>a[--i]-j?r:a[i]-j;
+		System.out.print(r);
 	}
 
 }
