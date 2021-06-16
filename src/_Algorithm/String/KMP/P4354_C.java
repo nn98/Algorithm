bd153c443f56dc2db78e6412bf158dfbf6a20680
nn98 <jkllhgb@gmail.com>
2021-06-16 19:21:21 +0900

    4354

--- src/_Algorithm/String/KMP/P4354_C.java
+++ src/_Algorithm/String/KMP/P4354_C.java
@@ -1,10 +1,34 @@
 package _Algorithm.String.KMP;
 
+import java.util.Scanner;
+
 public class P4354_C {
 
+	static char[]c;
+	static int p[],i,j,l;
+	static void o() {
+		l=c.length;
+		i=1;
+		j=0;
+		while(i+j<l) {
+			if(c[i+j]==c[j]) {
+				p[i+j-1]=++j;
+			}else {
+				if(j==0)i++;
+				else {
+					i+=j-p[j-1];
+					j=p[j-1];
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String z;
+		while(!(z=s.next()).equals(".")) {
+			c=z.toCharArray();
+			o();
+		}
 	}
 
 }
