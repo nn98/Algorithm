f979d7a827c36a35aef0f98657a3c974c51b265a
nn98 <jkllhgb@gmail.com>
2021-07-11 23:19:07 +0900

    4354

--- src/_Algorithm/String/KMP/P4354_C.java
+++ src/_Algorithm/String/KMP/P4354_C.java
@@ -47,8 +47,7 @@ public class P4354_C {
 			c=z.toCharArray();
 			o();
 //			부분문자열이 없거나 마지막 나눴을때 나누어 떨어지지 않으면 1 
-			System.out.println(p[l-1]==0|p[l-1]%(l-p[l-1])>0?
-					1:l/(l-p[l-1]));
+			System.out.println(p[l-1]==0|p[l-1]%(l-p[l-1])>0?1:l/(l-p[l-1]));
 		}
 	}
 }
