b8f2619190d8524c64fd99fc93a49b6e830a25f2
nn98 <jkllhgb@gmail.com>
2021-07-17 23:37:27 +0900

    20164

--- src/_Algorithm/DP/P20164.java
+++ src/_Algorithm/DP/P20164.java
@@ -1,10 +1,15 @@
 package _Algorithm.DP;
 
 public class P20164 {
+	
+	static int i,j,n,m;
+	
+	static void o() {
+		
+	}
 
 	public static void main(String[] args) {
 		char[]a=new java.util.Scanner(System.in).next().toCharArray();
-		int i=0,r=0;
 		
 	}
 
