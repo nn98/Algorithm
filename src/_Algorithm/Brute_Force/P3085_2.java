321f35286e9aaabcc59418749cbd894d5bc6c9fd
nn98 <jkllhgb@gmail.com>
2021-08-04 01:22:26 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_2.java
+++ src/_Algorithm/Brute_Force/P3085_2.java
@@ -1,9 +1,11 @@
 package _Algorithm.Brute_Force;
 
+import java.io.*;
+
 public class P3085_2 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
