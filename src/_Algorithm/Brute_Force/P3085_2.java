ea7eb3f2b1d5814ce24aa8a3fdf9d3be0d23d2b2
nn98 <jkllhgb@gmail.com>
2021-08-07 17:17:44 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_2.java
+++ src/_Algorithm/Brute_Force/P3085_2.java
@@ -4,9 +4,17 @@ import java.io.*;
 
 public class P3085_2 {
 
+	static int n,d[][],i,r;
+	static char a[][];
+	static void o(int i,int j,int n) {
+		
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		
+		i=n=Integer.parseInt(r.readLine());
+		d=new int[n][n];
+		a=new char[n][n];
+		while(i-->0)a[n]=r.readLine().toCharArray();
 	}
 
 }
