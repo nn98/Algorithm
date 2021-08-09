37dd389c9ac580de6c2eb0e5adc4654042b50ffe
nn98 <jkllhgb@gmail.com>
2021-08-10 00:30:02 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_3.java
+++ src/_Algorithm/Brute_Force/P3085_3.java
@@ -30,61 +30,16 @@ public class P3085_3 {
 						R=R>k?R:k;
 					}
 				}
-//				for(y=0;y<n;y++) {
-//					k=1;
-//					for(x=1;x<n;x++) {
-//						if(a[x][y]==a[x-1][y])k++;
-//						else {
-//							R=R>k?R:k;
-//							k=1;
-//						}
-//						R=R>k?R:k;
-//					}
-//				}
 				for(y=0;y<n;y++) {
 					k=1;
-					for(x=0;x<n-1;x++)
-						if(a[x+1][i]==a[x][y])k++;
-						else {
-							R=R>k?R:k;
-							k=1;
-						}
-					R=R>k?R:k;
-				}
-
-				a[i][j]=a[i][j+1];
-				a[i][j+1]=t;
-				for(x=0;x<n;x++) {
-					k=1;
-					for(y=1;y<n;y++) {
-						if(a[x][y]==a[x][y-1])k++;
+					for(x=1;x<n;x++) {
+						if(a[x][y]==a[x-1][y])k++;
 						else {
 							R=R>k?R:k;
 							k=1;
 						}
 						R=R>k?R:k;
 					}
-				}
-//				for(y=0;y<n;y++) {
-//					k=1;
-//					for(x=1;x<n;x++) {
-//						if(a[x][y]==a[x-1][y])k++;
-//						else {
-//							R=R>k?R:k;
-//							k=1;
-//						}
-//						R=R>k?R:k;
-//					}
-//				}
-				for(y=0;y<n;y++) {
-					k=1;
-					for(x=0;x<n-1;x++)
-						if(a[x+1][i]==a[x][y])k++;
-						else {
-							R=R>k?R:k;
-							k=1;
-						}
-					R=R>k?R:k;
 				}
 			}
 		System.out.println(R);
