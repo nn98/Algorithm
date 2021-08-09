f42100b2b42b87e627665a2ad7133360bed7826f
nn98 <jkllhgb@gmail.com>
2021-08-10 00:30:04 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_3.java
+++ src/_Algorithm/Brute_Force/P3085_3.java
@@ -41,6 +41,35 @@ public class P3085_3 {
 						R=R>k?R:k;
 					}
 				}
+				a[i][j+1]=a[i][j];
+				a[i][j]=t;
+
+				a[i][j]=a[i+1][j];
+				a[i+1][j]=t;
+				for(x=0;x<n;x++) {
+					k=1;
+					for(y=1;y<n;y++) {
+						if(a[x][y]==a[x][y-1])k++;
+						else {
+							R=R>k?R:k;
+							k=1;
+						}
+						R=R>k?R:k;
+					}
+				}
+				for(y=0;y<n;y++) {
+					k=1;
+					for(x=1;x<n;x++) {
+						if(a[x][y]==a[x-1][y])k++;
+						else {
+							R=R>k?R:k;
+							k=1;
+						}
+						R=R>k?R:k;
+					}
+				}
+				a[i+1][j]=a[i][j];
+				a[i][j]=t;
 			}
 		System.out.println(R);
 	}
