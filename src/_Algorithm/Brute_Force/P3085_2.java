930a1e3a1849f0583a4921ea67247120325303f0
nn98 <jkllhgb@gmail.com>
2021-08-09 23:20:46 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_2.java
+++ src/_Algorithm/Brute_Force/P3085_2.java
@@ -7,8 +7,15 @@ public class P3085_2 {
 	static int n,d[][],i,r,j;
 	static char a[][],t;
 	static void o(int i,int j,int n,int c) {
+		r=r>++c?r:c;
 		if(n>0) {
-			
+			if(j<n+1)
+				if(a[i][j+1]==a[i][j])
+					o(i,j+1,n,c);
+		}else{
+			if(i<n+1)
+				if(a[i+1][j]==a[i][j])
+					o(i+1,j,n,c);
 		}
 		
 	}
@@ -33,6 +40,7 @@ public class P3085_2 {
 				a[i+1][j]=a[i][j];
 				a[i][j]=t;
 			}
+		System.out.println(r);
 	}
 
 }
