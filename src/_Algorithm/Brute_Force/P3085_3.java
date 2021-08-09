cdae786b3ffbc07b08146749d36c07ed069a8a0b
nn98 <jkllhgb@gmail.com>
2021-08-10 00:14:57 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_3.java
+++ src/_Algorithm/Brute_Force/P3085_3.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class P3085_3 {
 
-	static int n,i,j;
+	static int n,i,j,R,k,x,y;
 	static char t,a[][];
 	
 	public static void main(String[] args)throws Exception{
@@ -14,7 +14,20 @@ public class P3085_3 {
 		while(j<n)a[j++]=r.readLine().toCharArray();
 		for(;i<n;i++)
 			for(j=0;j<n;j++) {
-				
+				t=a[i][j];
+				a[i][j]=a[i][j+1];
+				a[i][j+1]=t;
+				for(x=0;x<n;x++) {
+					k=1;
+					for(y=1;y<n;y++) {
+						if(a[x][y]==a[x][y-1])k++;
+						else {
+							R=R>k?R:k;
+					k=1;
+						}
+					}
+				}
+					
 			}
 	}
 
