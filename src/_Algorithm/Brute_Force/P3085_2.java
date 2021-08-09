2a63131fd73aebc482f41f783c90fccdd8166eea
nn98 <jkllhgb@gmail.com>
2021-08-09 23:37:04 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_2.java
+++ src/_Algorithm/Brute_Force/P3085_2.java
@@ -16,8 +16,8 @@ public class P3085_2 {
 		else
 			if(j<n-1)
 				o(i,j+1,v,a[i][j+1]==a[i][j]?c:0);
-		
-		
+
+
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -26,28 +26,34 @@ public class P3085_2 {
 		a=new char[n][n];
 		while(i<n)a[i++]=r.readLine().toCharArray();
 		System.out.println();
-		for(i=0;i<n-1;i++)
-			for(j=0;j<n-1;j++) {
+		for(i=0;i<n;i++)
+			for(j=0;j<n;j++) {
 				t=a[i][j];
-				a[i][j]=a[i][j+1];
-				a[i][j+1]=t;
-				for(char[]b:a)
-					System.out.println(b);
-				System.out.println();
-				o(i,0,0,1);
-				o(0,j,0,1);
-				a[i][j+1]=a[i][j];
-				a[i][j]=t;
-				
-				a[i][j]=a[i+1][j];
-				a[i+1][j]=t;
-				for(char[]b:a)
-					System.out.println(b);
-				System.out.println();
-				o(i,0,0,1);
-				o(0,j,0,1);
-				a[i+1][j]=a[i][j];
-				a[i][j]=t;
+				if(j==n-1)
+				if(a[i][j]!=a[i][j+1]) {
+					a[i][j]=a[i][j+1];
+					a[i][j+1]=t;
+					for(char[]b:a)
+						System.out.println(b);
+					System.out.println();
+					o(i,0,0,1);
+					o(0,j,0,1);
+					a[i][j+1]=a[i][j];
+					a[i][j]=t;
+				}
+
+				if(i==n-1)
+				if(a[i][j]!=a[i+1][j]) {
+					a[i][j]=a[i+1][j];
+					a[i+1][j]=t;
+					for(char[]b:a)
+						System.out.println(b);
+					System.out.println();
+					o(i,0,0,1);
+					o(0,j,0,1);
+					a[i+1][j]=a[i][j];
+					a[i][j]=t;
+				}
 			}
 		System.out.println(R);
 	}
