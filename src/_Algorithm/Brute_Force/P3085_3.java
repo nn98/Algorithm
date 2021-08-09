80e691c531f9fd34053e29a567cb74a696753c90
nn98 <jkllhgb@gmail.com>
2021-08-10 00:06:15 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_3.java
+++ src/_Algorithm/Brute_Force/P3085_3.java
@@ -1,10 +1,21 @@
 package _Algorithm.Brute_Force;
 
+import java.io.*;
+
 public class P3085_3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,i,j;
+	static char t,a[][];
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new char[n][n];
+		while(j<n)a[j++]=r.readLine().toCharArray();
+		for(;i<n;i++)
+			for(j=0;j<n;j++) {
+				
+			}
 	}
 
 }
