e987b6e1ab0f29b18dbbb23031199655718c62c1
nn98 <jkllhgb@gmail.com>
2021-08-12 23:54:42 +0900

    3085

--- src/_Algorithm/Brute_Force/P3085_R.java
+++ src/_Algorithm/Brute_Force/P3085_R.java
@@ -1,10 +1,15 @@
 package _Algorithm.Brute_Force;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P3085_R {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		
 	}
 
 }
