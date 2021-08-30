97cc9d651bfaa898e3adf46f8cad2798654432d9
nn98 <jkllhgb@gmail.com>
2021-08-30 21:51:44 +0900

    2531

--- src/_Algorithm/Two_Pointer/P2531_2.java
+++ src/_Algorithm/Two_Pointer/P2531_2.java
@@ -1,10 +1,19 @@
 package _Algorithm.Two_Pointer;
 
+import java.io.*;
+import java.util.*;
+
 public class P2531_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		int n=Integer.parseInt(t.nextToken()),d=Integer.parseInt(t.nextToken()),
+				k=Integer.parseInt(t.nextToken()),c=Integer.parseInt(t.nextToken()),
+				a[]=new int[n],i=0,j,R=0,p;
+		for(;i<n;i++) {
+			a[i]=Integer.parseInt(r.readLine());
+		}
 	}
 
 }
