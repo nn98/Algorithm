3ebc7be99c69a6e61beaa9abd99b5b16880e91f2
nn98 <jkllhgb@gmail.com>
2021-08-30 17:42:00 +0900

    1463

--- src/_Algorithm/DP/P1463.java
+++ src/_Algorithm/DP/P1463.java
@@ -3,8 +3,14 @@ package _Algorithm.DP;
 public class P1463 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt(),m=n,i=0;
+		while(m!=1) {
+			if(m%3==0)m/=3;
+			else if(m%2==0)m/=2;
+			else m--;
+			i++;
+		}
+		System.out.println(i);
 	}
 
 }
