ee946a1f04d3361d589287439119d8beb785522f
nn98 <jkllhgb@gmail.com>
2021-08-30 21:49:46 +0900

    2531

--- src/_Algorithm/Two_Pointer/P2531.java
+++ src/_Algorithm/Two_Pointer/P2531.java
@@ -18,7 +18,7 @@ public class P2531 {
 				HashSet<Integer>s=new HashSet();
 				for(j=i,p=k;p>0;System.out.print(a[j]+" "),s.add(a[j--]),p--);
 				s.add(c);
-				System.out.println("\nfrom "+j+" to "+i+" kind - "+s.size());
+				System.out.println("\nfrom "+(j+1)+" to "+i+" kind: "+s.size());
 				R=R>s.size()?R:s.size();
 			}
 		}
