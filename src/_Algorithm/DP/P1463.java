8b51e4591564eb4cd1dd010812328ade57936b08
nn98 <jkllhgb@gmail.com>
2021-08-30 17:55:50 +0900

    1463

--- src/_Algorithm/DP/P1463.java
+++ src/_Algorithm/DP/P1463.java
@@ -3,14 +3,13 @@ package _Algorithm.DP;
 public class P1463 {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),m=n,i=0;
-		while(m!=1) {
-			if(m%3==0)m/=3;
-			else if(m%2==0)m/=2;
-			else m--;
-			i++;
+		int n=new java.util.Scanner(System.in).nextInt(),i=2,d[]=new int[n+1];
+		for(;i<=n;i++) {
+			d[i]=d[i-1]+1;
+			if(i%2==0)d[i]=Math.min(d[i],d[i/2]+1);
+			if(i%3==0)d[i]=Math.min(d[i],d[i/3]+1);
 		}
-		System.out.println(i);
+		System.out.println(d[i]);
 	}
 
 }
