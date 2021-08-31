422020e72ae18c0a0b8c01a974258a8d86c8fe52
nn98 <jkllhgb@gmail.com>
2021-08-31 14:50:58 +0900

    2531

--- src/_Algorithm/Two_Pointer/P2531_2.java
+++ src/_Algorithm/Two_Pointer/P2531_2.java
@@ -10,9 +10,16 @@ public class P2531_2 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		int n=Integer.parseInt(t.nextToken()),d=Integer.parseInt(t.nextToken()),
 				k=Integer.parseInt(t.nextToken()),c=Integer.parseInt(t.nextToken()),
-				a[]=new int[n],i=0,x,y,R=0;
+				a[]=new int[n],i=0,j=k-1,R=0,b[]=new int[d+1];
 		for(;i<n;i++) {
 			a[i]=Integer.parseInt(r.readLine());
+		}
+		HashSet<Integer>h=new HashSet();
+		for(i=0;i<=j;h.add(a[i]),b[a[i]]+=h.contains(a[i++])?1:0);
+		System.out.println(h.toString());
+		System.out.println(Arrays.toString(b));
+		for(i=0;i<n;i++) {
+			
 		}
 	}
 
