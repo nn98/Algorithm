ca4542834b1de363edef0ce847a38306856a08e4
nn98 <jkllhgb@gmail.com>
2021-09-03 14:24:52 +0900

    2811

--- src/_Algorithm/Two_Pointer/P2811.java
+++ src/_Algorithm/Two_Pointer/P2811.java
@@ -6,18 +6,22 @@ public class P2811 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],i=0,j=0,k,J=0,V=0;
+		int n=s.nextInt(),a[]=new int[n],i=0,j=0,I=0,k=0,J=0,V=0;
 		for(;i<n;i++) {
 			a[i]=s.nextInt();
 			if(a[i]<0) {
+				I=j==0?i:I;
 				j++;
 				if(j>=V) {
-					J=i;
+					J=I;
 					V=j;
 				}
-			}else j=0;
-			System.out.println("V: "+V+" J: "+J);
+			}else {
+				
+				j=0;
+			}
 		}
+			System.out.println("V: "+V+" J: "+J);
 	}
 
 }
