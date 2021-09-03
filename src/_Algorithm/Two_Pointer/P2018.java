dcb6493fb440d2afc610d223f28a7847fdaf046d
nn98 <jkllhgb@gmail.com>
2021-09-03 13:35:15 +0900

    2018

--- src/_Algorithm/Two_Pointer/P2018.java
+++ src/_Algorithm/Two_Pointer/P2018.java
@@ -5,6 +5,7 @@ public class P2018 {
 	public static void main(String[]z) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=2,r=0,v=1;
 		for(;j<=n;) {
+			System.out.println("v: "+v);
 			if(v==n)r++;
 //			r=v==n?r+1:r;
 			if(v<n)v+=++j;
