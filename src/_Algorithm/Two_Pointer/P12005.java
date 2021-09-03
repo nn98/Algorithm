5793b59fc8f6d9dbba746cc88c2ed8258d75f082
nn98 <jkllhgb@gmail.com>
2021-09-03 13:49:14 +0900

    12005

--- src/_Algorithm/Two_Pointer/P12005.java
+++ src/_Algorithm/Two_Pointer/P12005.java
@@ -7,9 +7,14 @@ public class P12005 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,r=0;
+		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,r=0,j=0;
 		for(;i<n;a[i++]=s.nextInt());
 		Arrays.sort(a);
+		for(i=0;i<n;i++) {
+			if(a[i]-a[j]>k)j++;
+			r=r>i-k+1?r:i-k+1;
+		}
+		System.out.print(r);
 	}
 
 }
