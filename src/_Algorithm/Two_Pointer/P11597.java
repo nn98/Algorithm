2a7cf85a95bbcb66d4ce4e4ab147917e785ed6f3
nn98 <jkllhgb@gmail.com>
2021-09-03 14:04:27 +0900

    11597

--- src/_Algorithm/Two_Pointer/P11597.java
+++ src/_Algorithm/Two_Pointer/P11597.java
@@ -1,10 +1,16 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P11597 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0,r=2000000;
+		for(;i<n;a[i++]=s.nextInt());
+		for(i=0;i<n/2;i++)
+			if(a[i]+a[n-i-1]<r)r=a[i]+a[n-i-1];
+		System.out.print(r);
 	}
 
 }
