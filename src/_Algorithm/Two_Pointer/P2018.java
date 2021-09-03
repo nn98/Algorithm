c2c65b558190b80902a9ef7ca2e6544c60e7bee6
nn98 <jkllhgb@gmail.com>
2021-09-03 13:33:20 +0900

    2018

--- src/_Algorithm/Two_Pointer/P2018.java
+++ src/_Algorithm/Two_Pointer/P2018.java
@@ -2,4 +2,14 @@ package _Algorithm.Two_Pointer;
 
 public class P2018 {
 
+	public static void main(String[]z) {
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=2,r=0,v=1;
+		for(;j<=n;) {
+			if(v==n)r++;
+//			r=v==n?r+1:r;
+			if(v<n)v+=++j;
+			else v-=i++;
+		}
+		System.out.println(r);
+	}
 }
