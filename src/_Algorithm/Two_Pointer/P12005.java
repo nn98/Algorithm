4796870f0460ef13b63a294b7445511bd7b1addb
nn98 <jkllhgb@gmail.com>
2021-09-03 13:49:38 +0900

    12005

--- src/_Algorithm/Two_Pointer/P12005.java
+++ src/_Algorithm/Two_Pointer/P12005.java
@@ -12,6 +12,8 @@ public class P12005 {
 		Arrays.sort(a);
 		for(i=0;i<n;i++) {
 			if(a[i]-a[j]>k)j++;
+//			한번만 반복돼서 앞쪽 값이 중복이면 정확한 값음 아님.
+//			차피 1더하고 1빼니까 결과는 문제없이 나올듯
 			r=r>i-j+1?r:i-j+1;
 		}
 		System.out.print(r);
