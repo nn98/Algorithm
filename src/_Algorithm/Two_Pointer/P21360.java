138386bbce657a9ee7d40cdbf6be49ea07028ec9
nn98 <jkllhgb@gmail.com>
2021-09-03 13:20:35 +0900

    21360

--- src/_Algorithm/Two_Pointer/P21360.java
+++ src/_Algorithm/Two_Pointer/P21360.java
@@ -6,8 +6,10 @@ public class P21360 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
+		int n=s.nextInt(),i=0,j=0;
 		char[]a=s.next().toCharArray();
+		for(;i==0;i=a[j++]=='0'?j:0);
+		System.out.println(i+" "+j);
 	}
 
 }
