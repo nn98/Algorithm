d573177b81de8e58d8f827910b7e7c0dc02989b7
nn98 <jkllhgb@gmail.com>
2021-09-06 01:30:58 +0900

    8726

--- src/_Algorithm/Two_Pointer/P8726_3.java
+++ src/_Algorithm/Two_Pointer/P8726_3.java
@@ -1,10 +1,24 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P8726_3 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),i=0,j=-1,c,v=0,r=n;
+		s.nextLine();
+		String[]a=s.nextLine().split(" ");
+		c=a[0].equals("0")?1:0;
+		for(;i<n-1;) {
+			while(c<k&v<n) {
+				v++;
+				if(a[v].equals("0"))c++;
+			}
+			if(c==k)r=r<v-i+1?r:v-i+1;
+			if(a[i].equals("0"))c--;
+			i++;
+		}
+		System.out.print(r==n?"NIE":r);
 	}
 
 }
