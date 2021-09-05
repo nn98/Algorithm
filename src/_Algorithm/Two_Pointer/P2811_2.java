b6c9203508e8788c9a80948746d5ce01f16d9390
nn98 <jkllhgb@gmail.com>
2021-09-06 00:08:47 +0900

    2811

--- src/_Algorithm/Two_Pointer/P2811_2.java
+++ src/_Algorithm/Two_Pointer/P2811_2.java
@@ -32,6 +32,7 @@ public class P2811_2 {
 //		System.out.println(V);
 		for(i=0;i<n;i++) {
 			if(a[i]==-V) {
+				System.out.println("\t"+i);
 				I=0;
 				k=V*3;
 				for(j=i-1;j>=0&k-->0;I+=b[j--]>0?0:1);
@@ -49,7 +50,7 @@ public class P2811_2 {
 		//		}
 		//		System.out.println("V: "+V+" J: "+J+" k: "+k);
 		for(i=0;i<n;r+=b[i++]>0?1:0);
-		System.out.println(Arrays.toString(b));
+//		System.out.println(Arrays.toString(b));
 		System.out.println(r);
 	}
 
