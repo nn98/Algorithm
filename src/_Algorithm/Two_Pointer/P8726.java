f1795dff7029a8bc70678b024927f9ae23b89bff
nn98 <jkllhgb@gmail.com>
2021-09-06 00:29:27 +0900

    8726

--- src/_Algorithm/Two_Pointer/P8726.java
+++ src/_Algorithm/Two_Pointer/P8726.java
@@ -8,7 +8,7 @@ public class P8726 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0,j,c;
-		s.next();
+		s.nextLine();
 		String[]a=s.nextLine().split(" ");
 		System.out.println(Arrays.toString(a));
 	}
