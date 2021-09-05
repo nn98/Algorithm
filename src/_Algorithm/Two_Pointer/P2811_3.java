fc520d22b6ab458a6400792f7924452722b8cb46
nn98 <jkllhgb@gmail.com>
2021-09-06 00:17:20 +0900

    2811

--- src/_Algorithm/Two_Pointer/P2811_3.java
+++ src/_Algorithm/Two_Pointer/P2811_3.java
@@ -1,10 +1,26 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2811_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j=0,I=0,J,V=0,c,r=0,a[]=new int[n],b[]=new int[n];
+		for(;i<n;i++) {
+			a[i]=s.nextInt();
+			if(a[i]<0) {
+				I=j==0?i:I;
+				j++;
+				if(j>V) {
+					V=j;
+					J=I;
+				}
+			}else for(j*=2;--I>=0&j>0;b[I]++);
+			a[i]=-j;
+		}
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
 	}
-
 }
