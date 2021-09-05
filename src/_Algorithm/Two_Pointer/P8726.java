66913507ed3f9ef7369c46401bd378403f9c1e85
nn98 <jkllhgb@gmail.com>
2021-09-06 00:28:38 +0900

    8726

--- src/_Algorithm/Two_Pointer/P8726.java
+++ src/_Algorithm/Two_Pointer/P8726.java
@@ -1,10 +1,13 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P8726 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,j,c;
+		String[]a=s.nextLine().split(" ");
 	}
 
 }
