db5a43f626b90e4743fea6a63dcf51b330c637c1
nn98 <jkllhgb@gmail.com>
2021-09-06 01:20:10 +0900

    8726

--- src/_Algorithm/Two_Pointer/P8726_2.java
+++ src/_Algorithm/Two_Pointer/P8726_2.java
@@ -6,9 +6,20 @@ public class P8726_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),i=0,j=-1,c=1,v=0,r=n;
+		int n=s.nextInt(),k=s.nextInt(),po=0,ko=1,dz,v=0,wy=n+1;
 		s.nextLine();
 		String[]a=s.nextLine().split(" ");
+		dz=a[0].equals("0")?1:0;
+		while(ko<n) {
+			while(dz<k&po<n) {
+				po++;
+				if(a[po].equals("0"))dz++;
+			}
+			if(dz==k)wy=Math.min(wy,po-ko+1);
+			if(a[ko].equals("0"))dz--;
+			ko++;
+		}
+		System.out.print(wy==n+1?"NIE":wy-k);
 	}
 
 }
