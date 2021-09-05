8d1a12198489e556f32ce780641e7e4c61f8694c
nn98 <jkllhgb@gmail.com>
2021-09-06 01:20:14 +0900

    8726

--- src/_Algorithm/Two_Pointer/P8726.java
+++ src/_Algorithm/Two_Pointer/P8726.java
@@ -9,8 +9,9 @@ public class P8726 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0,j=-1,c=1,v=0,r=n;
 		s.nextLine();
-		String[]a=s.nextLine().split(" ");
-		for(;i<n-1;) {
+		String[]a=new String[n+1];
+		for(String b:s.nextLine().split(" "))a[i++]=b;
+		for(i=0;i<n-1;) {
 			if(a[i].equals("0")) {
 //				System.out.println("go: "+i+" from: "+j+" c: "+c+" v: "+v);
 				j=j==-1?i+1:j;
--- src/_Algorithm/Two_Pointer/P8726_2.java
+++ src/_Algorithm/Two_Pointer/P8726_2.java
@@ -6,9 +6,10 @@ public class P8726_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),po=0,ko=1,dz,v=0,wy=n+1;
+		int n=s.nextInt(),k=s.nextInt(),po=0,ko=1,dz,v=0,wy=n+1,i=0;
 		s.nextLine();
-		String[]a=s.nextLine().split(" ");
+		String[]a=new String[n+1];
+		for(String b:s.nextLine().split(" "))a[i++]=b;
 		dz=a[0].equals("0")?1:0;
 		while(ko<n) {
 			while(dz<k&po<n) {
