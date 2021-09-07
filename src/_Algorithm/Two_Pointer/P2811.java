aaec78eaca9626ea470b2f354c51c61aa5c4401a
nn98 <jkllhgb@gmail.com>
2021-09-07 15:14:36 +0900

    Onedrive restore err

---
+++ src/_Algorithm/Two_Pointer/P2811.java
@@ -0,0 +1,39 @@
+package _Algorithm.Two_Pointer;
+
+import java.util.Scanner;
+
+public class P2811 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0,j=0,I=0,k=0,J=0,V=0,b[]=new int[n];
+		for(;i<n;i++) {
+			a[i]=s.nextInt();
+			if(a[i]<0) {
+				I=j==0?i:I;
+				j++;
+				if(j>=V) {
+					J=I;
+					V=j;
+				}
+			}else {
+				if(I-j*2<0)k+=I;
+				else k+=j*2;
+				j=0;
+			}
+//			System.out.println(k);
+		}
+		if(j>0) {
+			if(I-j*2<0)k+=I;
+			else k+=j*2;
+		}
+		i=J-V*2;
+		if(i>=0) {
+			if(i-V<0)k+=i;
+			else k+=V;
+		}
+//		System.out.println("V: "+V+" J: "+J+" k: "+k);
+		System.out.println(k);
+	}
+
+}
---
+++ src/_Algorithm/Two_Pointer/P2811_3.java
@@ -0,0 +1,37 @@
+package _Algorithm.Two_Pointer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2811_3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j=0,I=0,J,V=0,c,r=0,a[]=new int[n],b[]=new int[n];
+		for(;i<n;i++) {
+			a[i]=s.nextInt();
+			if(a[i]<0) {
+				I=j==0?i:I;
+				j++;
+				if(j>V) {
+					V=j;
+					J=I;
+				}
+			}else {
+				for(j*=2;--I>=0&j-->0;b[I]++);
+				I=j=0;
+			}
+			a[i]=-j;
+		}
+		for(j*=2;--I>=0&j-->0;b[I]++);
+		for(i=0;i<n;i++)
+			if(a[i]==-V) {
+				I=0;
+				c=V*3;
+				for(j=i+a[i];j>=0&c-->0;I+=b[j--]>0?0:1);
+				r=r>I?r:I;
+			}
+		for(i=0;i<n;r+=b[i++]>0?1:0);
+		System.out.print(r);
+	}
+}
