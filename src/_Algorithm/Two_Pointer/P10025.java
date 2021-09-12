7e1eea341289e364edd0e49e5b6452ca6a800b9c
nn98 <jkllhgb@gmail.com>
2021-09-13 03:02:04 +0900

    10025

--- src/_Algorithm/Two_Pointer/P10025.java
+++ src/_Algorithm/Two_Pointer/P10025.java
@@ -12,10 +12,11 @@ public class P10025 {
 		for(j=0;j<l;u+=a[j++]);
 		j--;
 		for(;j<v-1;) {
-			System.out.printf("%d - %d : %d",i,j,u);
+			System.out.printf("%d - %d : %d\n",i,j,u);
 			r=r>u?r:u;
 			u-=a[i++];
 			u+=a[++j];
+			if(j>15)break;
 		}
 		System.out.println(r);
 	}
