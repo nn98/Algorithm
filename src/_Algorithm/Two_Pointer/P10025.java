0974b56c9ca54cf4ea8490f4cc6a38507893e90b
nn98 <jkllhgb@gmail.com>
2021-09-14 09:51:27 +0900

    10025

--- src/_Algorithm/Two_Pointer/P10025.java
+++ src/_Algorithm/Two_Pointer/P10025.java
@@ -9,7 +9,8 @@ public class P10025 {
 		int n=s.nextInt(),m=s.nextInt(),v=1000000,i=0,l=m*2+1,j,a[]=new int[v],
 				r=0,u=0;
 		for(;n-->0;j=s.nextInt(),a[s.nextInt()]=j);
-		for(j=0;j<l;u+=a[j++]);
+		for(j=0;j<l&j<v;u+=a[j++]);
+		if(v<l)r=u;
 		j--;
 		for(;j<v-1;) {
 			r=r>u?r:u;
