ee7a49077056511b00880b5045ba8a7c3e6a9d82
nn98 <jkllhgb@gmail.com>
2021-09-18 23:13:46 +0900

    17164

--- src/_Algorithm/Two_Pointer/P17164.java
+++ src/_Algorithm/Two_Pointer/P17164.java
@@ -6,7 +6,7 @@ public class P17164 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
+		int n=s.nextInt(),i=0,j,k;
 		char[]a=s.next().toCharArray();
 	}
 
