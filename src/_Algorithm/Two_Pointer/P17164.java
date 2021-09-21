cf36f6ec603edc4033f6a772d3f09a6198fad12a
nn98 <jkllhgb@gmail.com>
2021-09-21 22:56:00 +0900

    17164

--- src/_Algorithm/Two_Pointer/P17164.java
+++ src/_Algorithm/Two_Pointer/P17164.java
@@ -1,9 +1,6 @@
 package _Algorithm.Two_Pointer;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class P17164 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j,k;
