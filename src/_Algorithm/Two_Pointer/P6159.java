a401d9fd05e0d2e08d8414b7f1501da1f8d0fa9c
nn98 <jkllhgb@gmail.com>
2021-09-22 23:15:38 +0900

    6159

--- src/_Algorithm/Two_Pointer/P6159.java
+++ src/_Algorithm/Two_Pointer/P6159.java
@@ -3,8 +3,7 @@ package _Algorithm.Two_Pointer;
 public class P6159 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
 	}
 
 }
