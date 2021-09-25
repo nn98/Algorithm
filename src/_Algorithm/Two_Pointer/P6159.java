26ab2a8d501f6dd4795b4684feb911ae0594f4d7
nn98 <jkllhgb@gmail.com>
2021-09-26 02:04:57 +0900

    6159

--- src/_Algorithm/Two_Pointer/P6159.java
+++ src/_Algorithm/Two_Pointer/P6159.java
@@ -1,7 +1,7 @@
 package _Algorithm.Two_Pointer;
 import java.util.Scanner;
 public class P6159 {
-	public static void main(String[] args) {
+	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),i=0,a[]=new int[n],j,k;
 		for(;i<n;a[i++]=s.nextInt());
