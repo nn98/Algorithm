da24472c65775c2dce5c9e1da0775585ec983d4a
nn98 <jkllhgb@gmail.com>
2021-10-07 00:23:22 +0900

    14246

--- src/_Algorithm/Two_Pointer/P14246_4.java
+++ src/_Algorithm/Two_Pointer/P14246_4.java
@@ -1,10 +1,18 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P14246_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],t[],l=1,i,j,r=0;
+		for(;l<=n;l*=2);
+		t=new int[l];
+		l/=2;
+		for(;i<n;t[l+i++]=s.nextInt());
+		System.out.println(Arrays.toString(t));
 	}
 
 }
