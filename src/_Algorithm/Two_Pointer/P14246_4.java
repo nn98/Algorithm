0e29e91fd459d4cb32a03a6ed0da185dd6c14e69
nn98 <jkllhgb@gmail.com>
2021-10-07 00:26:45 +0900

    14246

--- src/_Algorithm/Two_Pointer/P14246_4.java
+++ src/_Algorithm/Two_Pointer/P14246_4.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 
 public class P14246_4 {
 
+	static int n,a[],t[],l=1,i,j,r;
+	static void o() {
+		while(j>0)t[j/=2]=t[j*2]+t[j*2+1];
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],t[],l=1,i=0,j,r=0;
+		n=s.nextInt();
+		a=new int[n];
 		for(;l<=n;l*=2);
 		t=new int[l];
 		l/=2;
-		for(;i<n;t[l+i++]=s.nextInt());
+		for(;i<n;j=l+i++,t[j]=s.nextInt(),o());
 		System.out.println(Arrays.toString(t));
 	}
 
