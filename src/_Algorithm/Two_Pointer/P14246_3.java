18fc074ad5268cd92182d05d36594a3715870f26
nn98 <jkllhgb@gmail.com>
2021-10-07 00:19:14 +0900

    14246

--- src/_Algorithm/Two_Pointer/P14246_3.java
+++ src/_Algorithm/Two_Pointer/P14246_3.java
@@ -6,9 +6,17 @@ public class P14246_3 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,j,a[]=new int[n],k,r=0;
+		int n=s.nextInt(),i=0,j,a[]=new int[n],k,r=0,u;
 		for(;i<n;a[i++]=s.nextInt());
 		k=s.nextInt();
+		for(i=0;i<n;i++) {
+			u=0;
+			for(j=i;j<n;j++) {
+				u+=a[j];
+				if(u>k)r++;
+			}
+		}
+		System.out.println(r);
 	}
 
 }
