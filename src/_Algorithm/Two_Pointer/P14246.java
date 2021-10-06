beb2bc7e5be5b7af3bf1b3df810a94d303dbdb9b
nn98 <jkllhgb@gmail.com>
2021-10-07 00:12:06 +0900

    14246

--- src/_Algorithm/Two_Pointer/P14246.java
+++ src/_Algorithm/Two_Pointer/P14246.java
@@ -16,7 +16,7 @@ public class P14246 {
 				for(k=i;k<n;b[j][k++]+=a);
 		}
 		k=s.nextInt();
-//		for(int[]c:b)System.out.println(Arrays.toString(c));
+		for(int[]c:b)System.out.println(Arrays.toString(c));
 		a=0;
 		for(i=0;i<n;i++)
 			for(j=i;++j<n;a+=b[i][j]>k?1:0);
