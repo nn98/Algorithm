3b343c71d860947c22d16b4ae00f336d5f4b56ed
nn98 <jkllhgb@gmail.com>
2021-10-06 23:55:32 +0900

    14246

--- src/_Algorithm/Two_Pointer/P14246.java
+++ src/_Algorithm/Two_Pointer/P14246.java
@@ -7,14 +7,17 @@ public class P14246 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[]=new int[n],b[][]=new int[n][n],k,i=0,j;
+		int n=s.nextInt(),a,b[][]=new int[n][n],k,i=0,j;
 		for(;i<n;i++) {
-			a[i]=s.nextInt();
+			a=s.nextInt();
 			for(j=0;j<=i;j++)
-				for(k=i;k<n;b[j][k++]+=a[i]);
+				for(k=i;k<n;b[j][k++]+=a);
 		}
 		k=s.nextInt();
-		for(int[]c:b)System.out.println(Arrays.toString(c));
+//		for(int[]c:b)System.out.println(Arrays.toString(c));
+		a=0;
+		for(i=0;i<n;i++)
+			for(j=i;++j<n;a+=b[i][j]>k?1:0);
 	}
 
 }
