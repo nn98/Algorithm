a615f009823a2b031a9671999f2626383c23c3d1
nn98 <jkllhgb@gmail.com>
2021-10-11 22:07:06 +0900

    14264

--- src/_Algorithm/Two_Pointer/P14246_C.java
+++ src/_Algorithm/Two_Pointer/P14246_C.java
@@ -1,9 +1,20 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P14246_C {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[]=new int[n],i=0,j=0,k,r=0;
+		long u=0;
+		k=s.nextInt();
+		while(j<=n) {
+			if(u<=k)u+=a[j++];
+			else u-=a[i++];
+			if(u>k)r+=n-j+1;
+		}
+		System.out.println(r);
 	}
 
 }
