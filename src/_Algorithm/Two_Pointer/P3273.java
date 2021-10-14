1c40aa8feba5713b4d374404660510c97f781caf
nn98 <jkllhgb@gmail.com>
2021-10-14 23:22:17 +0900

    3273

--- src/_Algorithm/Two_Pointer/P3273.java
+++ src/_Algorithm/Two_Pointer/P3273.java
@@ -9,6 +9,10 @@ public class P3273 {
 		int n=s.nextInt(),a[]=new int[n],i=0,j,k,r=0;
 		for(;i<n;a[i++]=s.nextInt());
 		k=s.nextInt();
+		for(i=0;i<n-1;i++)
+			for(j=0;j<n;j++)
+				r+=a[i]+a[j]==k?1:0;
+		System.out.println(r);
 	}
 
 }
