9e87fbacba4bb5eee24cb2e985ab52e5364a21ae
nn98 <jkllhgb@gmail.com>
2021-10-14 23:35:42 +0900

    3273

--- src/_Algorithm/Two_Pointer/P3273.java
+++ src/_Algorithm/Two_Pointer/P3273.java
@@ -1,10 +1,7 @@
 package _Algorithm.Two_Pointer;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class P3273 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[]=new int[n],i=0,j,p=n-1,k,r=0;
@@ -21,5 +18,4 @@ public class P3273 {
 		}
 		System.out.println(r);
 	}
-
 }
