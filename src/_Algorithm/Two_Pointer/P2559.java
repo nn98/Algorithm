baca8d1f2a4a4edbc41953898ada59299a705534
nn98 <jkllhgb@gmail.com>
2021-10-17 23:36:59 +0900

    2559

--- src/_Algorithm/Two_Pointer/P2559.java
+++ src/_Algorithm/Two_Pointer/P2559.java
@@ -1,9 +1,12 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P2559 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
 	}
 
 }
