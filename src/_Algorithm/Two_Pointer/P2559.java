aa59423db37108340b18cf258761ddfcaf8072e7
nn98 <jkllhgb@gmail.com>
2021-10-18 22:18:14 +0900

    2559

--- src/_Algorithm/Two_Pointer/P2559.java
+++ src/_Algorithm/Two_Pointer/P2559.java
@@ -7,16 +7,12 @@ public class P2559 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),k=s.nextInt(),i=0,j=0,r=0,a[]=new int[n],R=Integer.MIN_VALUE;
-		for(;i<n;a[i++]=s.nextInt());
-		for(i=0;i<n-k;r+=a[j++]) {
-			if(j-i==k) {
+		for(;i<n;i++) {
+			r+=a[i]=s.nextInt();
+			if(i-j==k-1) {
 				R=r>R?r:R;
-				r-=a[i++];
+				r-=a[j++];
 			}
-		}
-		if(j-i==k) {
-			R=r>R?r:R;
-			r-=a[i++];
 		}
 		System.out.print(R);
 	}
