a86af94a35440677a1ff141d86bc54601698b2ba
nn98 <jkllhgb@gmail.com>
2021-10-18 22:34:28 +0900

    7795

--- src/_Algorithm/Two_Pointer/P7795.java
+++ src/_Algorithm/Two_Pointer/P7795.java
@@ -1,10 +1,28 @@
 package _Algorithm.Two_Pointer;
 
+import java.io.*;
+import java.util.*;
+
 public class P7795 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		int i=Integer.parseInt(r.readLine()),j,x,y,a[],b[],R;
+		for(;i-->0;) {
+			R=0;
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			a=new int[x=Integer.parseInt(t.nextToken())];
+			b=new int[y=Integer.parseInt(t.nextToken())];
+			t=new StringTokenizer(r.readLine());
+			for(j=0;j<x;a[j++]=Integer.parseInt(t.nextToken()));
+			t=new StringTokenizer(r.readLine());
+			for(j=0;j++<y;) {
+				for(int c:a)R+=c>Integer.parseInt(t.nextToken())?1:0;
+			}
+			System.out.println(R);
+		}
+		
 	}
 
 }
