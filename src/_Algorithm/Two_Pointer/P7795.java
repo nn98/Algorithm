33d3af6658b501d18b29e031dffabcd5fc1eebcf
nn98 <jkllhgb@gmail.com>
2021-10-18 23:16:43 +0900

    7795

--- src/_Algorithm/Two_Pointer/P7795.java
+++ src/_Algorithm/Two_Pointer/P7795.java
@@ -2,7 +2,7 @@ package _Algorithm.Two_Pointer;
 
 import java.io.*;
 import java.util.*;
-public class P7795 {
+public class P7795{
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
