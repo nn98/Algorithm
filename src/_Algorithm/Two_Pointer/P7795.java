54d2ce3d2a62950c6a07ffd2c4333ae17abcf0ee
nn98 <jkllhgb@gmail.com>
2021-10-18 23:15:06 +0900

    7795

--- src/_Algorithm/Two_Pointer/P7795.java
+++ src/_Algorithm/Two_Pointer/P7795.java
@@ -6,7 +6,7 @@ public class P7795 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		int i=Integer.parseInt(r.readLine()),j,x,y,a[];
+		int i=Integer.parseInt(r.readLine()),j,x,y,a[],k;
 		for(;i-->0;) {
 			StringTokenizer t=new StringTokenizer(r.readLine());
 			a=new int[x=Integer.parseInt(t.nextToken())];
@@ -16,7 +16,10 @@ public class P7795 {
 			t=new StringTokenizer(r.readLine());
 			for(j=0;t.hasMoreTokens();) {
 				y=Integer.parseInt(t.nextToken());
-				for(int c:a)j+=c>y?1:0;
+				for(k=x;k>0;) {
+					if(a[--k]>y)j++;
+					else break;
+				}
 			}
 			w.write(j+"\n");
 		}
