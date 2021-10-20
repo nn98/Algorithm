eac9fd17e17c5ddfc36503714a4c34a962b64f50
nn98 <jkllhgb@gmail.com>
2021-10-21 06:51:16 +0900

    14465

--- src/_Algorithm/Two_Pointer/P14465.java
+++ src/_Algorithm/Two_Pointer/P14465.java
@@ -1,9 +1,13 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P14465 {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),b=s.nextInt(),i=0,j,a[]=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
 	}
 
 }
