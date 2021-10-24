d6688d76039e899cac282c583459f715b5c0cfbf
nn98 <jkllhgb@gmail.com>
2021-10-24 22:05:37 +0900

    14465

--- src/_Algorithm/Two_Pointer/P14465.java
+++ src/_Algorithm/Two_Pointer/P14465.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class P14465 {
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),b=s.nextInt(),i=0,j,a[]=new int[n];
-		for(;i<n;a[i++]=s.nextInt());
+		int n=s.nextInt(),k=s.nextInt(),b=s.nextInt(),i=0,j=0,u=0,r=0;
+		boolean[]a=new boolean[n];
+		for(;i++<b;a[s.nextInt()]=true);
+		for(i=0;i<n-k;u-=a[i++]?1:0) {
+			for(;j<i+k;u+=a[j++]?1:0);
+			r=r<u?r:u;
+		}
 	}
 }
