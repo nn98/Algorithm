6ac08036bd0f95d8509cda081b115f97da9670a4
nn98 <jkllhgb@gmail.com>
2021-10-24 22:19:59 +0900

    14465

--- src/_Algorithm/Two_Pointer/P14465.java
+++ src/_Algorithm/Two_Pointer/P14465.java
@@ -6,10 +6,10 @@ public class P14465 {
 		int n=s.nextInt(),k=s.nextInt(),b=s.nextInt(),i=0,j=0,u=0,r=n;
 		boolean[]a=new boolean[n];
 		for(;i++<b;a[s.nextInt()-1]=true);
-		for(i=0;i<n-k;u-=a[i++]?1:0) {
-			for(;j<i+k;u+=a[j++]?1:0);
-			r=r<u?r:u;
-		}
+		i=0;
+		for(;j<k;u+=a[j++]?1:0);
+		r=r<u?r:u;
+		for(;j<n;u-=a[i++]?1:0,u+=a[j++]?1:0,r=r<u?r:u)
 		System.out.println(i+" "+j);
 		System.out.println(r);
 	}
