af7d21609a935f35075b8eaa438a78d8b12fb558
nn98 <jkllhgb@gmail.com>
2021-10-24 23:06:22 +0900

    22114

--- src/_Algorithm/Two_Pointer/P22114.java
+++ src/_Algorithm/Two_Pointer/P22114.java
@@ -6,9 +6,24 @@ public class P22114 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n-1],i=0,j,r=0;
+		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n-1],i=0,j=0,r=0,u=1,p=1;
 		s.nextLine();
 		for(String b:s.nextLine().split(" "))a[i++]=Integer.parseInt(b);
+		i=0;
+		for(;j<n;) {
+			if(a[j++]>k) {
+				if(p==0){
+					for(;p==0;) {
+						if(a[i++]>k)p--;
+						u--;
+					}
+				}
+				p++;
+			}
+			u++;
+			r=r>u?r:u;
+		}
+		System.out.println(r);
 	}
 
 }
