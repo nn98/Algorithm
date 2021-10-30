6d18429deacd3d1eafc0f41a4cead824d2b78461
nn98 <jkllhgb@gmail.com>
2021-10-31 00:43:05 +0900

    17609

--- src/_Algorithm/Two_Pointer/P17609_2.java
+++ src/_Algorithm/Two_Pointer/P17609_2.java
@@ -1,10 +1,21 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P17609_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i,j,c;
+		StringBuffer a=new StringBuffer(s.next()),b;
+		i=a.length()/2;
+		if(a.length()%2==1) {
+			b=new StringBuffer(a.subSequence(i+1,i*2+1));
+		}else b=new StringBuffer(a.subSequence(i,i*2));
+		a=new StringBuffer(a.subSequence(0,i));
+		b=b.reverse();
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
