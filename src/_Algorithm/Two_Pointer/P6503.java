ec79acb7d2c425cce19cf4bb77fff63176ee0148
nn98 <jkllhgb@gmail.com>
2021-10-31 00:14:41 +0900

    6503

--- src/_Algorithm/Two_Pointer/P6503.java
+++ src/_Algorithm/Two_Pointer/P6503.java
@@ -1,10 +1,16 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P6503 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n,i=0,j;
+		for(char t=0;t<129;System.out.println(t+" "+(int)t++));
+		while((n=s.nextInt())>0) {
+			
+		}
 	}
 
 }
