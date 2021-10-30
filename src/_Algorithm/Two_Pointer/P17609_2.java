b3df5cfb4bd419f859aa60a68d819229f0523215
nn98 <jkllhgb@gmail.com>
2021-10-31 00:43:07 +0900

    17609

--- src/_Algorithm/Two_Pointer/P17609_2.java
+++ src/_Algorithm/Two_Pointer/P17609_2.java
@@ -7,10 +7,13 @@ public class P17609_2 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i,j,c;
+		String u;
 		StringBuffer a=new StringBuffer(s.next()),b;
 		i=a.length()/2;
 		if(a.length()%2==1) {
+			u=a.substring(i,i+1);
 			b=new StringBuffer(a.subSequence(i+1,i*2+1));
+			System.out.println(u);
 		}else b=new StringBuffer(a.subSequence(i,i*2));
 		a=new StringBuffer(a.subSequence(0,i));
 		b=b.reverse();
