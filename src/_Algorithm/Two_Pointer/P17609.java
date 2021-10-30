06d3cd7368c5a0f55e4be660d98b01d002e065d7
nn98 <jkllhgb@gmail.com>
2021-10-31 00:30:58 +0900

    17609

--- src/_Algorithm/Two_Pointer/P17609.java
+++ src/_Algorithm/Two_Pointer/P17609.java
@@ -1,10 +1,28 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P17609 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,x,y,c;
+		for(;i++<n;) {
+			char[]a=s.next().toCharArray();
+			c=x=0;
+			y=a.length-1;
+			for(;x<y&c<2;) {
+				if(a[x]!=a[y]) {
+					if(a[x+1]==a[y])x++;
+					if(a[x]==a[y-1])y--;
+					else c=1;
+					c++;
+				}
+				x++;
+				y--;
+			}
+			System.out.println(c);
+		}
 	}
 
 }
