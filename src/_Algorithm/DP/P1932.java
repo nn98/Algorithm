35dac9637609bf8cb63d1121ff31be15f89e4a91
nn98 <jkllhgb@gmail.com>
2021-11-05 22:16:55 +0900

    1932

--- src/_Algorithm/DP/P1932.java
+++ src/_Algorithm/DP/P1932.java
@@ -6,7 +6,7 @@ public class P1932 {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(r.readLine()),i=0,j,u=0,a[][]=new int[n][],b[][]=new int[n][];
+		int n=Integer.parseInt(r.readLine()),i=0,j,u,a[][]=new int[n][],b[][]=new int[n][];
 		for(;i<n;) {
 			j=0;
 			a[i]=new int[i+1];
@@ -14,6 +14,10 @@ public class P1932 {
 			StringTokenizer t=new StringTokenizer(r.readLine());
 			for(;j<i;a[i-1][j++]=Integer.parseInt(t.nextToken()));
 		}
+		for(;i-->0;) {
+			for(j=0;j<i;j++)
+				b[i-1][j]=a[i][j]>a[i][j+1]?a[i][j]:a[i][j+1];
+		}
+		System.out.println(b[0][0]);
 	}
-
 }
