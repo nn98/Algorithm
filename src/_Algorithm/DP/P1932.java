4a286cb83dc88c5bf6faeef5e543030ee5cb3bfc
nn98 <jkllhgb@gmail.com>
2021-11-05 22:13:23 +0900

    1932

--- src/_Algorithm/DP/P1932.java
+++ src/_Algorithm/DP/P1932.java
@@ -1,10 +1,19 @@
 package _Algorithm.DP;
 
+import java.io.*;
+import java.util.StringTokenizer;
 public class P1932 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(r.readLine()),i=0,j,u=0,a[][]=new int[n][],b[][]=new int[n][];
+		for(;i<n;) {
+			j=0;
+			a[i]=new int[i+1];
+			b[i]=new int[i+++1];
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			for(;j<i;a[i][j++]=Integer.parseInt(t.nextToken()));
+		}
 	}
 
 }
