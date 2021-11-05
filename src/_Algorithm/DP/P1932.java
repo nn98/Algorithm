ff435503a6069ff4441374cf1c2a10f78a9055a4
nn98 <jkllhgb@gmail.com>
2021-11-05 22:17:01 +0900

    1932

--- src/_Algorithm/DP/P1932.java
+++ src/_Algorithm/DP/P1932.java
@@ -16,7 +16,7 @@ public class P1932 {
 		}
 		for(;i-->0;) {
 			for(j=0;j<i;j++)
-				b[i-1][j]=a[i][j]>a[i][j+1]?a[i][j]:a[i][j+1]+b[i-1][j];
+				b[i-1][j]=(a[i][j]>a[i][j+1]?a[i][j]:a[i][j+1])+b[i-1][j];
 		}
 		System.out.println(b[0][0]);
 	}
