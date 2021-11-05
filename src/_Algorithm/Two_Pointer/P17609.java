dd30a332ee5ab48462790917a176da197a90c826
nn98 <jkllhgb@gmail.com>
2021-11-05 20:48:30 +0900

    17609

--- src/_Algorithm/Two_Pointer/P17609.java
+++ src/_Algorithm/Two_Pointer/P17609.java
@@ -9,10 +9,8 @@ public class P17609 {
 	static int n,r,i;
 	static StringBuffer f,t;
 	static int o(int X) {
-		System.out.println(X+" "+f);
 		int i=0,a,b,x=0,y=f.length()-1;
 		for(;x<y&i<1;) {
-			System.out.println(x+" "+f.charAt(x)+" "+y+" "+f.charAt(y));
 			if(!(f.charAt(x)==f.charAt(y)))i++;
 			else {
 				x++;
@@ -20,15 +18,11 @@ public class P17609 {
 			}
 		}
 		if(i>0) {
-			System.out.println(x+" "+y);
 			if(++X>1)return X;
 			t=new StringBuffer(f);
 			f.deleteCharAt(x);
-			System.out.println(1+" "+f);
 			a=o(X);
-			System.out.println("t "+t);
 			f=new StringBuffer(t.deleteCharAt(y));
-			System.out.println(2+" "+f);
 			b=o(X);
 			return a<b?a:b;
 		}
