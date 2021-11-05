272b010cdbdee786319e58bc7fdc7084ac9f5013
nn98 <jkllhgb@gmail.com>
2021-11-05 18:37:24 +0900

    17609

--- src/_Algorithm/Two_Pointer/P17609.java
+++ src/_Algorithm/Two_Pointer/P17609.java
@@ -3,24 +3,39 @@ package _Algorithm.Two_Pointer;
 import java.util.Scanner;
 
 public class P17609 {
-
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,x,y,c;
-		for(;i++<n;) {
-			char[]a=s.next().toCharArray();
-			c=x=0;
-			y=a.length-1;
-			for(;x<y&c<2;) {
-				if(a[x]!=a[y]) {
-					if(a[x+1]==a[y])x++;
-					else if(a[x]==a[y-1])y--;
-					else c=1;
-					c++;
-				}
+	
+	static int n,x,y,c,r,i;
+	static StringBuffer f,t;
+	static int o(int X) {
+		if(X>1)return;
+		i=x=0;
+		y=f.length()-1;
+		for(;x<y&i<1;) {
+			if(!(f.charAt(x)==f.charAt(y)))i++;
+			else {
 				x++;
 				y--;
 			}
+		}
+		if(i>0) {
+			c++;
+			X++;
+			t=f;
+			f=t.deleteCharAt(x);
+			o(X);
+			if(c<2)return c;
+			c--;
+			f=t.deleteCharAt(y);
+			o(X);
+		}else return c;
+	}
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(;i++<n;) {
+			f=new StringBuffer(s.next());
+			c=0;
 			System.out.println(c);
 		}
 	}
