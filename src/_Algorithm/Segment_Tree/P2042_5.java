b037f12c0c185412cae7bb8adc76c367dca2cc43
nn98 <jkllhgb@gmail.com>
2021-11-05 22:51:35 +0900

    2042

--- src/_Algorithm/Segment_Tree/P2042_5.java
+++ src/_Algorithm/Segment_Tree/P2042_5.java
@@ -7,6 +7,9 @@ public class P2042_5 {
 
 	static int n,m,i,j,l;
 	static long a[];
+	static void o() {
+		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,8 +18,13 @@ public class P2042_5 {
 		m=Integer.parseInt(t.nextToken())+Integer.parseInt(t.nextToken());
 		for(l=1;l<n;l*=2);
 		a=new long[l*2];
-		for(;i<n;a[l+i++]=Integer.parseInt(r.readLine()));
-//		System.out.println(Arrays.toString(a));
+		for(;i<n;) {
+			j=l+i++;
+			a[j]=Integer.parseInt(r.readLine());
+			if(i%2==0|i==n)o();
+		}
+		System.out.println(Arrays.toString(a));
+		
 	}
 
 }
