dd6317119b393dc8da5494ee1d763107430b4c8c
nn98 <jkllhgb@gmail.com>
2021-11-07 23:48:04 +0900

    15565

--- src/_Algorithm/Two_Pointer/P15565_2.java
+++ src/_Algorithm/Two_Pointer/P15565_2.java
@@ -1,12 +1,15 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P15565_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,j;
+		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,j=0;
+		for(;i++<n;)if(s.nextInt()<2)a[j++]=i;
+		System.out.println(Arrays.toString(a));
 	}
 
 }
