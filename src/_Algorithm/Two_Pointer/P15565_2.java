b4b9fbc0d4c320387b2497f1a9162fbf55e40566
nn98 <jkllhgb@gmail.com>
2021-11-07 23:52:07 +0900

    15565

--- src/_Algorithm/Two_Pointer/P15565_2.java
+++ src/_Algorithm/Two_Pointer/P15565_2.java
@@ -7,10 +7,10 @@ public class P15565_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,j=0,r=1000001,c;
+		int n=s.nextInt(),k=s.nextInt()-1,a[]=new int[n],i=0,j=0,r=1000001,c;
 		for(;i++<n;)if(s.nextInt()<2)a[j++]=i;
 		for(;j-->3;) {
-			c=a[j]-a[j-2]+1;
+			c=a[j]-a[j-k]+1;
 			r=r<c?r:c;
 		}
 		System.out.println(r==1000001?-1:r);
