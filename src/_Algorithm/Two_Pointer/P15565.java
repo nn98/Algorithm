a3b303f81b6c6426e01d0539773fd5e9395b6fb9
nn98 <jkllhgb@gmail.com>
2021-11-07 23:02:30 +0900

    15565

--- src/_Algorithm/Two_Pointer/P15565.java
+++ src/_Algorithm/Two_Pointer/P15565.java
@@ -1,10 +1,14 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P15565 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,j,r=Integer.MAX_VALUE;
+		for(;i<n;a[i++]=s.nextInt());
+		
 	}
 
 }
