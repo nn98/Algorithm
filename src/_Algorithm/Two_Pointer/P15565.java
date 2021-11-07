464bf9e45b0eeeb2a9d646c51f726f761cab5820
nn98 <jkllhgb@gmail.com>
2021-11-07 23:12:53 +0900

    15565

--- src/_Algorithm/Two_Pointer/P15565.java
+++ src/_Algorithm/Two_Pointer/P15565.java
@@ -9,7 +9,7 @@ public class P15565 {
 		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],i=0,j,c=0,r=Integer.MAX_VALUE;
 		for(;i<n;a[i++]=s.nextInt());
 		for(j=i=0;i<n|j<n;) {
-			c+=a[j]>1?1:0;
+			if(j<n)c+=a[j]>1?1:0;
 			if(c>=k)r=r<c?r:c;
 			if(j<n)j++;
 			for(;c>k;c-=a[i++]>1?1:0);
