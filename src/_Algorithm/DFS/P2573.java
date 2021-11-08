94b4708d7bb633d164ba399b3c9105b29e69f42c
nn98 <jkllhgb@gmail.com>
2021-11-08 19:24:37 +0900

    2573

--- src/_Algorithm/DFS/P2573.java
+++ src/_Algorithm/DFS/P2573.java
@@ -5,16 +5,21 @@ import java.util.*;
 
 public class P2573 {
 
-	static int n,m,a[][],b[][],v[][],i,j,x[]= {1,0,-1,0},y[]= {0,1,0,-1},k;
-	
+	static int n,m,a[][],b[][],v[][],i,j,x[]= {1,0,-1,0},y[]= {0,1,0,-1},k,c,R;
+
 	static void o() {
 		for(i=0;++i<n-1;)
-			for(j=0;++j<m-1;System.out.println(i+" "+j))
+			for(j=0;++j<m-1;)
 				for(k=0;k<4;k++,a[i][j]=a[i][j]<1?0:a[i][j])
 					a[i][j]-=b[i+x[k]][j+y[k]]==0?1:0;
-		for(i=0;i<n;System.arraycopy(a[i],0,b[i],0,a[i].length),i++);
 	}
-	
+	static void d(int p,int q) {
+		v[p][q]++;
+		for(;p<n-1;p++)
+			for(;q<m-1;q++)
+				if(v[p][q]==0) d(p,q);
+	}
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
@@ -29,31 +34,27 @@ public class P2573 {
 				for(j=0;j<m;j++)
 					a[i][j]=Integer.parseInt(t.nextToken());
 		}
-		for(i=0;i<n;System.arraycopy(a[i],0,b[i],0,a[i].length),i++);
-//		System.out.println(Arrays.toString(a[2]));
-//		System.out.println(Arrays.toString(b[2]));
-//		a[2][4]=555;
-//		System.out.println(Arrays.toString(a[2]));
-//		System.out.println(Arrays.toString(b[2]));
-		for(int[]c:a)System.out.println(Arrays.toString(c));
-		System.out.println();
-		for(int[]c:b)System.out.println(Arrays.toString(c));
-		System.out.println();
-		o();
-		for(int[]c:a)System.out.println(Arrays.toString(c));
-		System.out.println();
-		for(int[]c:b)System.out.println(Arrays.toString(c));
-		System.out.println();
-		o();
-		for(int[]c:a)System.out.println(Arrays.toString(c));
-		System.out.println();
-		for(int[]c:b)System.out.println(Arrays.toString(c));
-		System.out.println();
-		o();
-		for(int[]c:a)System.out.println(Arrays.toString(c));
-		System.out.println();
-		for(int[]c:b)System.out.println(Arrays.toString(c));
-		
+		for(;;) {
+			for(i=0;i<n;System.arraycopy(a[i],0,b[i],0,a[i].length),i++);
+			v=new int[n][m];
+			c=0;
+			for(i=0;++i<n-1&c<2;c=0) {
+				for(j=0;++j<m-1&c<2;)
+					if(a[i][j]>0) {
+						c++;
+						d(i,j);
+					}
+			}
+			if(c>1) {
+				System.out.print(R);
+				System.exit(0);
+			}
+			if(c<1) {
+				System.out.print(c);
+				System.exit(0);
+			}
+			R++;
+		}
 	}
 
 }
