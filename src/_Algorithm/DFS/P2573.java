78ff52d33f0632df5735b21da6245f4c0a2ccac9
nn98 <jkllhgb@gmail.com>
2021-11-08 17:13:13 +0900

    2573

--- src/_Algorithm/DFS/P2573.java
+++ src/_Algorithm/DFS/P2573.java
@@ -9,7 +9,7 @@ public class P2573 {
 	
 	static void o() {
 		for(i=0;++i<n-1;)
-			for(j=0;++j<n-1&a[i][j]>0;)
+			for(j=0;++j<m-1&a[i][j]>0;)
 				for(k=0;k<4;k++,a[i][j]=a[i][j]<1?0:a[i][j])
 					a[i][j]-=b[i+x[k]][j+y[k]]==0?1:0;
 	}
