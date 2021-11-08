84dc62bb1190c64906e23944932b28ed7fc79d24
nn98 <jkllhgb@gmail.com>
2021-11-08 19:40:08 +0900

    2573

--- src/_Algorithm/Two_Pointer/P20922.java
+++ src/_Algorithm/Two_Pointer/P20922.java
@@ -1,10 +1,20 @@
 package _Algorithm.Two_Pointer;
 
+import java.util.Scanner;
+
 public class P20922 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),a[]=new int[n],d[]=new int[100001],i=0,j=0,r=0,v;
+		for(;i<n;a[i++]=s.nextInt());
+		for(;j<n;) {
+			d[a[j]]++;
+			for(;d[a[j]]>k;d[a[i++]]--);
+			v=j-i+1;
+			r=r>v?r:v;
+		}
+		System.out.println(r);
 	}
 
 }
