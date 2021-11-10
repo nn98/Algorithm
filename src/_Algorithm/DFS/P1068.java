8507571bab5f6cc269a0afbe80aba0342c54edbb
nn98 <jkllhgb@gmail.com>
2021-11-10 23:36:36 +0900

    1068

--- src/_Algorithm/DFS/P1068.java
+++ src/_Algorithm/DFS/P1068.java
@@ -1,10 +1,11 @@
 package _Algorithm.DFS;
-
+import java.util.Scanner;
 public class P1068 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a[][]=new int[n][n],r=0,i=0,j;
+		for(;i<n;i++) {
+			
+		}
 	}
-
 }
