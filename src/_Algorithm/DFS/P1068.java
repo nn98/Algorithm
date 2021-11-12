1db96eb4249e009a7b2a15f2e3ec34b6a279f389
nn98 <jkllhgb@gmail.com>
2021-11-13 07:43:58 +0900

    1068

--- src/_Algorithm/DFS/P1068.java
+++ src/_Algorithm/DFS/P1068.java
@@ -3,13 +3,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class P1068 {
 	static void d(int x) {
-		for(int i=0;i<n;i++) {
-			if(c[x][i]>0) {
-				if(a[i]<1)r--;
-				d(i);
+		if(a[x]>0){
+			for(int i=0;i<n;i++) {
+				if(c[x][i]>0) {
+					d(i);
+				}
 			}
 		}
-		a[p[x]]--;
+		a[x]=9;
 	}
 	static int n,c[][],p[],r,i,j,a[];
 	public static void main(String[] args) {
@@ -17,25 +18,25 @@ public class P1068 {
 		n=s.nextInt();
 		c=new int[n][n];
 		p=new int[n];
-		r=n;
 		a=new int[n];
 		for(;i<n;i++) {
 			j=s.nextInt();
 			if(j>=0) {
 				p[i]=j;
 				c[j][i]++;
-				if(a[j]<1)r--;
 				a[j]++;
 			}
 		}
-		for(int[]b:c)System.out.println(Arrays.toString(b));
-		System.out.println(Arrays.toString(p));
-		System.out.println(Arrays.toString(a));
+//		for(int[]b:c)System.out.println(Arrays.toString(b));
+//		System.out.println(Arrays.toString(p));
+//		System.out.println(Arrays.toString(a));
 		System.out.println();
 		d(i=s.nextInt());
-		for(int[]b:c)System.out.println(Arrays.toString(b));
-		System.out.println(Arrays.toString(p));
-		System.out.println(Arrays.toString(a));
+		a[p[i]]--;
+//		for(int[]b:c)System.out.println(Arrays.toString(b));
+//		System.out.println(Arrays.toString(p));
+//		System.out.println(Arrays.toString(a));
+		for(int b:a)r+=b<1?1:0;
 		System.out.println(r);
 	}
 }
