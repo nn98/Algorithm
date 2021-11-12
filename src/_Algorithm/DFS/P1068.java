2418df553008a30f854a84ea9e4fe77d226c9788
nn98 <jkllhgb@gmail.com>
2021-11-13 07:18:16 +0900

    1068

--- src/_Algorithm/DFS/P1068.java
+++ src/_Algorithm/DFS/P1068.java
@@ -1,11 +1,14 @@
 package _Algorithm.DFS;
+import java.util.Arrays;
 import java.util.Scanner;
 public class P1068 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a[][]=new int[n][n],r=0,i=0,j;
 		for(;i<n;i++) {
-			
+			j=s.nextInt();
+			if(j>=0)a[j][i]++;
 		}
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 }
