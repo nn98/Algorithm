e17e53a6f5ff67d3a3ea465448c5acaa1848d769
nn98 <jkllhgb@gmail.com>
2021-11-13 07:18:20 +0900

    1068

--- src/_Algorithm/DFS/P1068.java
+++ src/_Algorithm/DFS/P1068.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 public class P1068 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),a[][]=new int[n][n],r=0,i=0,j;
+		int n=s.nextInt(),a[][]=new int[n][n],r=n,i=0,j,c[]=new int[n];
 		for(;i<n;i++) {
 			j=s.nextInt();
-			if(j>=0)a[j][i]++;
+			if(j>=0) {
+				a[i][j]++;
+				if(c[j]<1)r--;
+				c[j]++;
+			}
 		}
 		for(int[]b:a)System.out.println(Arrays.toString(b));
+		System.out.println(Arrays.toString(c));
+		System.out.println(r);
+		j=s.nextInt();
+		
 	}
 }
