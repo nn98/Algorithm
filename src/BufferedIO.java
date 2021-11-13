60f705cb97a86e66569d394549375b21ece8a503
nn98 <jkllhgb@gmail.com>
2021-11-13 09:04:59 +0900

    IO set

--- src/BufferedIO.java
+++ src/BufferedIO.java
@@ -1,9 +1,10 @@
-
+import java.io.*;
+import java.util.*;
 public class BufferedIO {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
