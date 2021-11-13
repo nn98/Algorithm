ffee763c2aac8d7d84c4a0616bd1c4c8587a9dbf
nn98 <jkllhgb@gmail.com>
2021-11-13 09:18:29 +0900

    10216

--- src/_Algorithm/DFS/P10216.java
+++ src/_Algorithm/DFS/P10216.java
@@ -3,10 +3,18 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P10216 {
+	static int n,a[][],i,j,R,x,y,b[];
+	static void o(int x) {
+
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(r.readLine());
+		for(;n-->0;) {
+			j=Integer.parseInt(r.readLine());
+			StringTokenizer t=new StringTokenizer(r.readLine());
+		}
 	}
 
 }
