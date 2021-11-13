526cb3bce8beb51a9d4b334caac65ac74ae34088
nn98 <jkllhgb@gmail.com>
2021-11-13 09:18:27 +0900

    10216

---
+++ src/_Algorithm/DFS/P10216.java
@@ -0,0 +1,12 @@
+package _Algorithm.DFS;
+
+import java.io.*;
+import java.util.*;
+public class P10216 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+	}
+
+}
