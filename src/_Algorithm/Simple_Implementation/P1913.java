bb6550c93999951954d32d23e44d6f46b9ceee8c
nn98 <jkllhgb@gmail.com>
2021-11-14 23:11:30 +0900

    1913

--- src/_Algorithm/Simple_Implementation/P1913.java
+++ src/_Algorithm/Simple_Implementation/P1913.java
@@ -1,10 +1,15 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P1913 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),a[][]=new int[n][n],i,j;
+		for(i=0;i<=n/2;a[i][i]=(n-(i*2))*(n-(i*2)),i++);
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
