5776b4e1c59323aa9c6e831727d3d9e0e7a14551
nn98 <jkllhgb@gmail.com>
2021-11-17 23:00:15 +0900

    1913

--- src/_Algorithm/Simple_Implementation/P1913.java
+++ src/_Algorithm/Simple_Implementation/P1913.java
@@ -1,7 +1,7 @@
 package _Algorithm.Simple_Implementation;
 import java.util.*;
 public class P1913 {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),a[][]=new int[n][n],i,b,c,d,e;
 		for(i=0;i<=n/2;a[i][i]=(n-(i*2))*(n-(i*2)),i++);
