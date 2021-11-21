ab8f37122903a89b83cb85ea305b4cafc5d91ba3
nn98 <jkllhgb@gmail.com>
2021-11-21 20:35:10 +0900

    1913

--- src/_Algorithm/Simple_Implementation/P1913.java
+++ src/_Algorithm/Simple_Implementation/P1913.java
@@ -6,8 +6,9 @@ public class P1913 {
 		int n=s.nextInt(),m=s.nextInt(),a[][]=new int[n][n],i,b,c,d,e;
 		for(i=0;i<=n/2;a[i][i]=(n-(i*2))*(n-(i*2)),i++);
 		for(int[]j:a)System.out.println(Arrays.toString(j));
-		for(i=0;i<=n/2;i++) {
+		for(i=0;i<=n/2;i++){
 			for(;;);
+			
 		}
 	}
 }
