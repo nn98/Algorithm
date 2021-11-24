3faabfc18c1df39a61d8413cca01174f53e9ae7b
nn98 <jkllhgb@gmail.com>
2021-11-24 22:43:34 +0900

    15681

--- src/_Algorithm/DFS/P15681.java
+++ src/_Algorithm/DFS/P15681.java
@@ -3,7 +3,10 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P15681 {
-	static int N,R,Q,a;
+	static int N,R,Q,a[],i,j;
+	static void o(int x) {
+		
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
