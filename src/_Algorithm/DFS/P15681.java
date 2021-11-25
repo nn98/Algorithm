cc8daffa9d53152bbe82b07524c7ccc45d4644f2
nn98 <jkllhgb@gmail.com>
2021-11-25 23:56:41 +0900

    15681

--- src/_Algorithm/DFS/P15681.java
+++ src/_Algorithm/DFS/P15681.java
@@ -1,5 +1,4 @@
 package _Algorithm.DFS;
-
 import java.io.*;
 import java.util.*;
 public class P15681 {
@@ -10,8 +9,6 @@ public class P15681 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer t=new StringTokenizer(r.readLine());
-		
+		StringTokenizer t=new StringTokenizer(r.readLine());	
 	}
-
 }
