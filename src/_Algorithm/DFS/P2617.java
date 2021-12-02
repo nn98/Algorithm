d1f62556a1395fa6abb7bfaa9f83b6205ba5e5ff
nn98 <jkllhgb@gmail.com>
2021-12-02 22:39:52 +0900

    2617

--- src/_Algorithm/DFS/P2617.java
+++ src/_Algorithm/DFS/P2617.java
@@ -1,10 +1,17 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.*;
 public class P2617 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j,k;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		
 	}
 
 }
