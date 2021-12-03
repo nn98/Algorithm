2ed9dae899b2dd0014585b5524ad2a1293df77a5
nn98 <jkllhgb@gmail.com>
2021-12-03 23:45:50 +0900

    2617

--- src/_Algorithm/DFS/P2617.java
+++ src/_Algorithm/DFS/P2617.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class P2617 {
 	static int n,m,i,j,k,a[][],v[];
 	static void h(int x) {
+		k++;
 		v[x]++;
 		for(int i=0;++i<n;)
 			if(a[x][i]>0)
 				if(v[i]<1)h(i);
 	}
 	static void l(int x) {
+		k++;
 		v[x]++;
 		for(int i=0;++i<n;)
 			if(a[x][i]<0)
@@ -22,7 +24,6 @@ public class P2617 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
-		v=new int[n];
 		a=new int[n][n];
 		for(;m-->0;) {
 			t=new StringTokenizer(r.readLine());
@@ -31,6 +32,17 @@ public class P2617 {
 			a[j][i]=1;
 			a[i][j]=-1;
 		}
+		m=0;
+		for(i=0;++i<n;) {
+			v=new int[n];
+			v[i]++;
+			for(j=0;++j<n;k=0,m+=k>=n/2?1:0)
+				if(a[i][j]>0)
+					h(j);
+				else if(a[i][j]<0)
+					l(j);
+		}
+		System.out.println(m);
 	}
 
 }
