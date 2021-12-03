3a0708a52f131462c9da1ac30c2a1ad4d282f22f
nn98 <jkllhgb@gmail.com>
2021-12-03 23:29:58 +0900

    2617

--- src/_Algorithm/DFS/P2617.java
+++ src/_Algorithm/DFS/P2617.java
@@ -3,21 +3,23 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P2617 {
-	static int n,m,i,j,k,a[][];
+	static int n,m,i,j,k,a[][],v[];
 	static void h(int x) {
+		v[x]++;
 		a[x][k]=-1;
 		for(int i=0;++i<n;)
 			if(a[x][i]>0) {
 				a[k][i]=1;
-				h(i);
+				if(v[i]<1)h(i);
 			}
 	}
 	static void l(int x) {
+		v[x]++;
 		a[x][k]=1;
 		for(int i=0;++i<n;)
 			if(a[x][i]<0) {
 				a[k][i]=-1;
-				l(i);
+				if(v[i]<1)l(i);
 			}
 	}
 	public static void main(String[] args)throws Exception{
@@ -26,6 +28,7 @@ public class P2617 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
+		v=new int[n];
 		a=new int[n][n];
 		for(;m-->0;) {
 			t=new StringTokenizer(r.readLine());
@@ -33,10 +36,12 @@ public class P2617 {
 			j=Integer.parseInt(t.nextToken());
 			a[j][i]=1;
 			k=j;
+			v=new int[n];
 			h(i);
 //			for(k=0;++k<n;a[j][k]=a[i][k]>0?1:a[j][k]);
 			a[i][j]=-1;
 			k=i;
+			v=new int[n];
 			l(j);
 //			for(k=0;++k<n;a[i][k]=a[j][k]<0?-1:a[i][k]);
 //			for(int[]b:a)System.out.println(Arrays.toString(b));
