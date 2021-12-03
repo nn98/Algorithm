49b38d0f04c47a682275f849eb300c64e8ccbfe5
nn98 <jkllhgb@gmail.com>
2021-12-03 22:59:06 +0900

    2617

--- src/_Algorithm/DFS/P2617.java
+++ src/_Algorithm/DFS/P2617.java
@@ -4,14 +4,24 @@ import java.io.*;
 import java.util.*;
 public class P2617 {
 
-	static int n,m,i,j,k;
+	static int n,m,i,j,k,a[][];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
-		
+		a=new int[n][n];
+		for(;m-->0;) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			a[j][i]=1;
+			for(k=0;k<n;a[j][k++]=a[i][k]>0?1:0);
+			a[i][j]=-1;
+			for(k=0;k<n;a[i][k++]=a[j][k]<0?-1:0);
+		}
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
