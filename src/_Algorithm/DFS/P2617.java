d68b1edc250fd7593efd9246eb0238e4763e44d6
nn98 <jkllhgb@gmail.com>
2021-12-03 23:34:39 +0900

    2617

--- src/_Algorithm/DFS/P2617.java
+++ src/_Algorithm/DFS/P2617.java
@@ -7,18 +7,14 @@ public class P2617 {
 	static void h(int x) {
 		v[x]++;
 		for(int i=0;++i<n;)
-			if(a[x][i]>0) {
-				a[m][i]=1;
+			if(a[x][i]>0)
 				if(v[i]<1)h(i);
-			}
 	}
 	static void l(int x) {
 		v[x]++;
 		for(int i=0;++i<n;)
-			if(a[x][i]<0) {
-				a[m][i]=-1;
+			if(a[x][i]<0)
 				if(v[i]<1)l(i);
-			}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -33,27 +29,8 @@ public class P2617 {
 			i=Integer.parseInt(t.nextToken());
 			j=Integer.parseInt(t.nextToken());
 			a[j][i]=1;
-			for(k=0;++k<n;a[j][k]=a[i][k]>0?1:a[j][k]);
 			a[i][j]=-1;
-			for(k=0;++k<n;a[i][k]=a[j][k]<0?-1:a[i][k]);
 		}
-		k=0;
-//		for(int[]b:a)System.out.println(Arrays.toString(b));
-		for(m=0;++m<n;) {
-			for(j=0;++j<n;) {
-				if(a[m][j]!=0) {
-					v=new int[n];
-					if(a[m][j]>0)h(j);
-					else l(j);
-				}
-			}
-			i=0;
-			for(int b:a[m])i+=b==0?1:0;
-//			System.out.println(m+" "+i);
-			k+=i<=n/2?1:0;
-		}
-		System.out.println(k);
-//		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
