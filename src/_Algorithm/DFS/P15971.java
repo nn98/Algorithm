23179f12d34c1b0ac5044e5b84611779b05ebda8
nn98 <jkllhgb@gmail.com>
2021-12-04 22:45:55 +0900

    15971

--- src/_Algorithm/DFS/P15971.java
+++ src/_Algorithm/DFS/P15971.java
@@ -1,9 +1,19 @@
 package _Algorithm.DFS;
 
-public class P15971 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class P15971 {
+	
+	static int n,m,i,j,k,r,a[];
+
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 
 	}
 
