2aff4e8729224e45620e313f454fee243f3cd6a2
nn98 <jkllhgb@gmail.com>
2021-12-05 22:58:29 +0900

    15971

--- src/_Algorithm/DFS/P15971.java
+++ src/_Algorithm/DFS/P15971.java
@@ -8,13 +8,15 @@ import java.util.StringTokenizer;
 
 public class P15971 {
 	
-	static int n,m,i,j,k,r,a[];
+	static int n,x,y,i,j,k,r,a[];
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-
+		n=Integer.parseInt(t.nextToken());
+		x=Integer.parseInt(t.nextToken());
+		y=Integer.parseInt(t.nextToken());
 	}
 
 }
