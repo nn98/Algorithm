7cb8888438848798728923f7b544e8f3373592e1
nn98 <jkllhgb@gmail.com>
2021-12-17 22:26:52 +0900

    1240

--- src/_Algorithm/DFS/P1240.java
+++ src/_Algorithm/DFS/P1240.java
@@ -2,8 +2,27 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P1240 {
-	static int n,x,y,i,j,k,p,a[],b[][];
-	static void o(int x) {
+	static int n,m,i,j,k,p,v[];
+	static N[]a;
+	static class N{
+		List<L>l;
+		int i;
+		N(int i){
+			this.i=i;
+		}
+		void a(int i,int l) {
+			this.l.add(new L(a[i],l));
+		}
+	}
+	static class L{
+		N n;
+		int l;
+		L(N n,int l){
+			this.n=n;
+			this.l=l;
+		}
+	}
+	static void o(int x,int l) {
 		
 	}
 	public static void main(String[] args)throws Exception{
@@ -11,6 +30,6 @@ public class P1240 {
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken());
-		x=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
 	}
 }
