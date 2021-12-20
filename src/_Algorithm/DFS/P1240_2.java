ff68c5a87b89cda579904e8c05a48ba02317d56e
nn98 <jkllhgb@gmail.com>
2021-12-20 23:25:06 +0900

    1240

--- src/_Algorithm/DFS/P1240_2.java
+++ src/_Algorithm/DFS/P1240_2.java
@@ -1,10 +1,24 @@
 package _Algorithm.DFS;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class P1240_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		while(m-->0) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			l=Integer.parseInt(t.nextToken());
 	}
 
 }
