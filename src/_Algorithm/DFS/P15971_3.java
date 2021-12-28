24e00d8f85603b7214551e6eb591b346c3fa42aa
nn98 <jkllhgb@gmail.com>
2021-12-28 23:31:00 +0900

    15971

--- src/_Algorithm/DFS/P15971_3.java
+++ src/_Algorithm/DFS/P15971_3.java
@@ -1,5 +1,43 @@
 package _Algorithm.DFS;
 
-public class P15971_3 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
+public class P15971_3 {
+	static int n,x,y,i,j,k,p,a[],b[][],d[],q[];
+	static void o(int x,int u,int m) {
+		d[x]=u;
+		q[x]=m;
+		a[x]++;
+		for(int i=0;++i<n;) {
+			if(b[x][i]>0&a[i]<1) {
+				o(i,u+b[x][i],Math.max(b[x][i],m));
+			}
+		}
+		a[x]--;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken())+1;
+		x=Integer.parseInt(t.nextToken());
+		y=Integer.parseInt(t.nextToken());
+		a=new int[n];
+		d=new int[n];
+		q=new int[n];
+		b=new int[n][n];
+		for(;++k<n-1;) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			p=Integer.parseInt(t.nextToken());
+			b[i][j]=b[j][i]=p;
+		}
+		o(x,0,0);
+		System.out.print(d[y]-q[y]);
+	}
 }
