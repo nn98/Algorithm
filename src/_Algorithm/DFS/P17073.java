26a1bf5f705d62d54aadff889d977e2411a221c2
nn98 <jkllhgb@gmail.com>
2022-01-05 23:48:24 +0900

    17073

--- src/_Algorithm/DFS/P17073.java
+++ src/_Algorithm/DFS/P17073.java
@@ -1,12 +1,10 @@
 package _Algorithm.DFS;
-
+//BFS
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
-//BFS
 public class P17073 {
 	static int n,m,i,j,R;
 	public static void main(String[] args)throws Exception{
@@ -15,7 +13,5 @@ public class P17073 {
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
-
 	}
-
 }
