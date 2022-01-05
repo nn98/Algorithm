a4b2577d1b950a2c1a74dc05aaa8fa0fe83a046a
nn98 <jkllhgb@gmail.com>
2022-01-05 23:48:06 +0900

    17073

--- src/_Algorithm/DFS/P17073.java
+++ src/_Algorithm/DFS/P17073.java
@@ -1,9 +1,20 @@
 package _Algorithm.DFS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 //BFS
 public class P17073 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken())+1;
+		m=Integer.parseInt(t.nextToken());
 
 	}
 
