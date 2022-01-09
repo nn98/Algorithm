71a590597115aec4e3b532806355f20fcd7ddcbb
nn98 <jkllhgb@gmail.com>
2022-01-09 13:56:27 +0900

    17073

--- src/_Algorithm/DFS/P17073.java
+++ src/_Algorithm/DFS/P17073.java
@@ -17,7 +17,7 @@ public class P17073 {
 		a=new int[n+1];
 		for(;++i<n;t=new StringTokenizer(r.readLine()),
 				a[Integer.parseInt(t.nextToken())]++,a[Integer.parseInt(t.nextToken())]++);
-		for(i=0;i++<n;)if(a[i]==1)R++;
+		for(i=1;i<n;)if(a[++i]==1)R++;
 		System.out.println(String.format("%.10f",m/R));
 	}
 }
