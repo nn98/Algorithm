2a1e2d1e5a4fce9e30aa1c7ea38eaa21e86d7c19
nn98 <jkllhgb@gmail.com>
2022-01-09 13:44:47 +0900

    17073

--- src/_Algorithm/DFS/P17073.java
+++ src/_Algorithm/DFS/P17073.java
@@ -6,12 +6,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 public class P17073 {
-	static int n,m,i,j,R;
+	static int n,m,i,j,R,a[];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
+		a=new int[n];
+		for(;i++<n;t=new StringTokenizer(r.readLine()),
+				a[Integer.parseInt(t.nextToken())]++,a[Integer.parseInt(t.nextToken())]++);
+		for(i=0;i++<n;)if(a[i]==1)R++;
+		System.out.println((double)m/R);
 	}
 }
