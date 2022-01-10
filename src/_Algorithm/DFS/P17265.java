c27333d48d31f72b6c378f5dc1324f4373bde4b1
nn98 <jkllhgb@gmail.com>
2022-01-10 21:00:47 +0900

    17265

--- src/_Algorithm/DFS/P17265.java
+++ src/_Algorithm/DFS/P17265.java
@@ -57,8 +57,8 @@ public class P17265 {
 		for(;i<n;i++,j=0)
 			for(String s:r.readLine().split(" "))d[i][j++]=s;
 		for(String[]z:d)System.out.println(Arrays.toString(z));
-		p(0,0,0);
-		q(0,0,0);
+		p(0,0,Integer.parseInt(d[0][0]));
+		q(0,0,Integer.parseInt(d[0][0]));
 		System.out.println(A[n-1][n-1]+" "+B[n-1][n-1]);
 	}
 }
