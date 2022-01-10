3852090da6985af82379ea095bd2f3e4005c6fdb
nn98 <jkllhgb@gmail.com>
2022-01-10 20:28:09 +0900

    17265

--- src/_Algorithm/DFS/P17265.java
+++ src/_Algorithm/DFS/P17265.java
@@ -2,9 +2,15 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P17265 {
+	static int n,i,j,a,b,A[][],B[][];
+	static String[][]d;
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(r.readLine());
+		A=new int[n][n];
+		B=new int[n][n];
+		d=new String[n][n];
 	}
 }
