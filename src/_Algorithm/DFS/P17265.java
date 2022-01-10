28259abf0d35cd9338e3289171d68bc32b3b37ec
nn98 <jkllhgb@gmail.com>
2022-01-10 20:28:14 +0900

    17265

--- src/_Algorithm/DFS/P17265.java
+++ src/_Algorithm/DFS/P17265.java
@@ -7,10 +7,12 @@ public class P17265 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(r.readLine());
 		A=new int[n][n];
 		B=new int[n][n];
 		d=new String[n][n];
+		for(;i<n;i++,j=0)
+			for(String s:r.readLine().split(" "))d[i][j++]=s;
+
 	}
 }
