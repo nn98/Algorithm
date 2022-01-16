27b053406ab5a61946f527669c816d2aac8c65b0
nn98 <jkllhgb@gmail.com>
2022-01-16 23:35:14 +0900

    17265

--- src/_Algorithm/DFS/P17265_3.java
+++ src/_Algorithm/DFS/P17265_3.java
@@ -1,17 +1,13 @@
 package _Algorithm.DFS;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
 public class P17265_3 {
-
 	static int n;
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		n=Integer.parseInt(r.readLine());
 	}
-
 }
