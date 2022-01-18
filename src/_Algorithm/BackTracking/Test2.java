08b894cdbb69734b8a791a1c86476d56463a18ab
nn98 <jkllhgb@gmail.com>
2022-01-18 22:25:23 +0900

    17265

---
+++ src/_Algorithm/BackTracking/Test2.java
@@ -0,0 +1,10 @@
+package _Algorithm.BackTracking;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/_Algorithm/DFS/P17265_3.java
+++ src/_Algorithm/DFS/P17265_3.java
@@ -1,13 +1,42 @@
 package _Algorithm.DFS;
 import java.io.*;
 public class P17265_3 {
-	static int n,i,j,r;
+	static int n,i,j,r,L,S;
+	static String[][]a;
+	static char[]b;
+	static void o(int x,int y) {
+		b[i++]=a[x][y].charAt(0);
+		if(x==n-1&y==n-1) {
+			System.out.println(b);
+			r=b[0]-'0';
+			for(j=1;j<n*2-1;j+=2) {
+				switch(b[j]){
+				case '+':
+					r+=b[j]-'0';
+					break;
+				case '-':
+					r-=b[j]-'0';
+					break;
+				case '*':
+					r*=b[j]-'0';
+				}
+			}
+			L=L>r?L:r;
+			S=S<r?S:r;
+		}else {
+			if(x<n-1)o(x+1,y);
+			if(y<n-1)o(x,y+1);
+		}
+		i--;
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		n=Integer.parseInt(r.readLine());
-		for(;i<n;i++) {
-			
-		}
+		a=new String[n][n];
+		b=new char[n*2-1];
+		for(;i<n;a[i++]=r.readLine().split(" "));
+		i=0;
+		o(0,0);
 	}
 }
