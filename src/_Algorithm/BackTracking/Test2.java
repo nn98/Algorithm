96ec7162871bcfe499de5dfcefe168621ddf167a
nn98 <jkllhgb@gmail.com>
2022-01-18 22:25:28 +0900

    17265

--- src/_Algorithm/BackTracking/Test2.java
+++ src/_Algorithm/BackTracking/Test2.java
@@ -1,10 +1,32 @@
 package _Algorithm.BackTracking;
 
+import java.util.Scanner;
+
 public class Test2 {
 
+	static int n,m,a[],i;
+	static char[]b;
+	static void o() {
+		if(i==m)
+			System.out.println(b);
+		else 
+			for(int j=0;++j<n;) {
+				if(a[j]<1) {
+					a[j]++;
+					b[i++]=(char)(j+'0');
+					o();
+					i--;
+					a[j]--;
+				}
+			}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt()+1;
+		m=s.nextInt();
+		a=new int[n];
+		b=new char[m];
+		o();
 	}
 
 }
