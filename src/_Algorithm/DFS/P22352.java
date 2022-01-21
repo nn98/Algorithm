91d755bbcb5278a765c1c3d3b2e9f40b79a2ce65
nn98 <jkllhgb@gmail.com>
2022-01-21 21:26:33 +0900

    22352

--- src/_Algorithm/DFS/P22352.java
+++ src/_Algorithm/DFS/P22352.java
@@ -1,9 +1,12 @@
 package _Algorithm.DFS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P22352 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
