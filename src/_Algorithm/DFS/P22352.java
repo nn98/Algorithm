65acf6c5b24c8eaa0ac24935070e9b8450a2ad39
nn98 <jkllhgb@gmail.com>
2022-01-25 23:39:42 +0900

    22352

--- src/_Algorithm/DFS/P22352.java
+++ src/_Algorithm/DFS/P22352.java
@@ -2,11 +2,16 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P22352 {
-	static int n,m,i,j,a[][];
+	static int n,m,i,j,k,a[][][];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
+		for(;i<2;i++)
+			for(j=0;j<n;j++) {
+				t=new StringTokenizer(r.readLine());
+				for(k=0;j<n;a[i][j][k++]=Integer.parseInt(t.nextToken()));
+			}
 	}
 }
