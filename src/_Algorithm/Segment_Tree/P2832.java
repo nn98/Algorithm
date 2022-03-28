4af52ffbd8b50ad474a35d424eadc91badc4ab3b
nn98 <jkllhgb@gmail.com>
2022-03-28 17:04:10 +0900

    2832

--- src/_Algorithm/Segment_Tree/P2832.java
+++ src/_Algorithm/Segment_Tree/P2832.java
@@ -2,10 +2,8 @@ package _Algorithm.Segment_Tree;
 import java.io.*;
 import java.util.*;
 public class P2832 {
-
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 	}
-
 }
