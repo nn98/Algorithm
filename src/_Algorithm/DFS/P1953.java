bbfa56544abfdda8170d072bbac2dbaee0e47324
nn98 <jkllhgb@gmail.com>
2022-04-07 23:13:23 +0900

    1953

--- src/_Algorithm/DFS/P1953.java
+++ src/_Algorithm/DFS/P1953.java
@@ -5,14 +5,14 @@ import java.util.*;
 
 public class P1953 {
 
-	static int n,m,i,j,o,R,a[][],b[];
-	public static void main(String[] args)throws Exception{
-		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		n=Integer.parseInt(r.readLine());
-		for(;i<n;i++) {
-			StringTokenizer t=new StringTokenizer(r.readLine());
-		}
+    static int n,m,i,j,o,R,a[][],b[];
+    public static void main(String[] args)throws Exception{
+	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+	n=Integer.parseInt(r.readLine());
+	for(;i<n;i++) {
+	    StringTokenizer t=new StringTokenizer(r.readLine());
 	}
+    }
 
 }
