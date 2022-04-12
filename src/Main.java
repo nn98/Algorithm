f0eed4603c6898e63d6d6a87bf14acebf423cb90
nn98 <jkllhgb@gmail.com>
2022-04-12 20:38:58 +0900

    과제채점

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,67 @@
+
+
 import java.util.ArrayList;
+
+
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("hw5_1 : 김예훈");
-		ArrayList<Integer> list1 = new ArrayList<Integer>();
-
-		for (int i =1; i<11; i++)
+		// TODO Auto-generated method stub
+		System.out.println("lab5_1 : 박준희");
+		
+		//(1)
+		ArrayList<Integer> list1 = new ArrayList<Integer>();//생성
+		for(int i = 1; i <= 10; i++) {//반복문을 10번돌면서 삽입
 			list1.add(i);
-
-
-		System.out.println(list1);
-		list1.add(10,777);
-		list1.add(0,888);
-		list1.add(5,999);
-		System.out.println(list1);
-		int sum=list1.remove(12);
-		sum+=list1.remove(4);
-		sum+=list1.remove(0);
-		System.out.println(list1 + " 삭제원소합 = "+sum);
+		}
+		System.out.println(list1);//출력
+		
+		//(2)
+		list1.add(10, 777);
+		list1.add(0, 888);
+		list1.add(5, 999);
+		
+		System.out.println(list1);//출력
+		
+		//(3)
+		int removedSum = 0;//remove()로 반환된 정수를 더할 변수
+		removedSum += list1.remove(12);
+		removedSum += list1.remove(4);
+		removedSum += list1.remove(0);
+		
+		System.out.print(list1);//출력
+		System.out.print(" 삭제 원소 합 = ");
+		System.out.println(removedSum);
 		System.out.println();
-
-		//		
+		
+		//(4)
 		MyArrayList list2 = new MyArrayList(100);
-		for (int i =1; i<11; i++)
+		
+		for(int i = 1; i <= 10; i++) {//반복문을 10번돌면서 삽입
 			list2.add(i);
-		System.out.println(list2);
-		list2.add(10,777);
-		list2.add(0,888);
-		list2.add(5,999);
-		System.out.println(list2);
-		sum=list2.remove(12);
-		sum+=list2.remove(4);
-		sum+=list2.remove(0);
-		System.out.println(list2 + " 삭제원소합 = "+sum);
+		}
+		System.out.println(list2);//출력
+		
+		//------------------------추가 코드----------------------
+		//(5)
+		list2.add(10, 777);
+		list2.add(0, 888);
+		list2.add(5, 999);
+		
+		System.out.println(list2);//출력
+		
+		//(6)
+		int myRemovedSum = 0;//remove()로 반환된 정수를 더할 변수
+		myRemovedSum += list2.remove(12);
+		myRemovedSum += list2.remove(4);
+		myRemovedSum += list2.remove(0);
+		
+		System.out.print(list2);//출력
+		System.out.print(" 삭제 원소 합 = ");
+		System.out.println(myRemovedSum);
 		System.out.println();
+		
+		
 	}
 
-}
-
-class MyArrayList{
-	private int[] listArray;
-	private int listLength;
-
-	public MyArrayList(int capacity) {
-		listArray = new int[capacity];
-		listLength=0;
-	}
-
-	public void add(int item) {
-		listArray[listLength++] = item;
-	}
-
-	public void add (int i,int j) {
-		for(int y=listLength; y>i; y--) {
-			listArray[y] = listArray[y-1];
-		}
-		listArray[i] = j;
-		listLength++;
-	}
-
-	public int remove(int i) {
-		int d = listArray[i]; 
-		for(int j = i; j< listLength-1; j++)
-			listArray[j] = listArray[j+1];
-		listLength--;
-		return d;
-	}
-
-	@Override 
-	public String toString() {
-		String result = "(";
-		for(int i =0; i<listLength-1; i++) {
-			result+=listArray[i];
-			result+=", ";
-		}
-		result+= listArray[listLength-1];
-		result+=")";
-		return result;
-
-	}
 }
