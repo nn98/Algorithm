5e84ed2374ba5429a446f29d3a3d6b69e18edd24
nn98 <jkllhgb@gmail.com>
2022-05-06 23:20:49 +0900

    2018

--- src/_Algorithm/Two_Pointer/P2018_2.java
+++ src/_Algorithm/Two_Pointer/P2018_2.java
@@ -5,6 +5,7 @@ public class P2018_2 {
     public static void main(String[] args) {
 	int n=new java.util.Scanner(System.in).nextInt(),i=1,j=1,u=0,r=0;
 	for(;j<n;) {
+	    System.out.println(u);
 	    while(u<n)u+=j++;
 	    while(u>n)u-=i++;
 	    if(n==u)r++;
