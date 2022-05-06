4e20cc3ce1b5a6f05cbfddc3f26761fbfaef1b5b
nn98 <jkllhgb@gmail.com>
2022-05-06 23:56:15 +0900

    2018

--- src/_Algorithm/Two_Pointer/P2018_2.java
+++ src/_Algorithm/Two_Pointer/P2018_2.java
@@ -1,20 +1,16 @@
 package _Algorithm.Two_Pointer;
-
 public class P2018_2 {
-
     public static void main(String[] args) {
 	int n=new java.util.Scanner(System.in).nextInt(),i=1,j=1,u=0,r=1;
 	for(;j<n;) {
-//	    System.out.println(u);
-	    while(u>n)u-=i++; // 서 순
-	    while(u<n)u+=j++; // 차 이 ?
+	    while(u>n)u-=i++;
+	    while(u<n)u+=j++;
 	    if(n==u) {
 		r++;
-//		System.out.println(u+" "+j+" -"+i);
-		u+=j++; // or 이거?
+		u+=j++;
 	    }
 	}
 	System.out.println(r);
     }
-
 }
+// 반복문 2개 위치차이;
