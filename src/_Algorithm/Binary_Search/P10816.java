7b365a1ecfbcef2ee7cd989d91e52403ac5562ee
nn98 <jkllhgb@gmail.com>
2022-05-08 14:12:24 +0900

    10816

--- src/_Algorithm/Binary_Search/P10816.java
+++ src/_Algorithm/Binary_Search/P10816.java
@@ -5,10 +5,11 @@ import java.util.*;
 
 public class P10816 {
     
-    static int i,j,a[],b[],r;
+    static int i,j,a[],b[],r,n,m;
 
     public static void main(String[] args)throws Exception{
 	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	n=Integer.parseInt(r.readLine());
 	StringTokenizer t=new StringTokenizer(r.readLine());
     }
 
