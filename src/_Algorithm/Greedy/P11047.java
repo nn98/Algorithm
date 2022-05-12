7374345607ea9305d0761986c1d1489cc66fe129
nn98 <jkllhgb@gmail.com>
2022-05-13 02:20:31 +0900

    11047

--- src/_Algorithm/Greedy/P11047.java
+++ src/_Algorithm/Greedy/P11047.java
@@ -1,10 +1,14 @@
 package _Algorithm.Greedy;
 
+import java.util.*;
+import java.io.*;
 public class P11047 {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+    static int a[]=new int[2],b[],i;
+    public static void main(String[] args)throws Exception{
+	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	for(String s:r.readLine().split(" "))a[i++]=Integer.parseInt(s);
+	
     }
 
 }
