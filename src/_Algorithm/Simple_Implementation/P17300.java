3b49237dc1011bfc3c4d3c375b6bada320f7b250
nn98 <jkllhgb@gmail.com>
2022-05-14 23:41:02 +0900

    17300

--- src/_Algorithm/Simple_Implementation/P17300.java
+++ src/_Algorithm/Simple_Implementation/P17300.java
@@ -1,10 +1,12 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.*;
 public class P17300 {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	s.nextLine();
     }
 
 }
