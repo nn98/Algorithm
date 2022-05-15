ccc402a9d159b2146b0a130fc419725ddd207a49
nn98 <jkllhgb@gmail.com>
2022-05-16 01:20:48 +0900

    1388

--- src/_Algorithm/DFS/P1388_2.java
+++ src/_Algorithm/DFS/P1388_2.java
@@ -1,6 +1,7 @@
 package _Algorithm.DFS;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class P1388_2 {
 
@@ -8,6 +9,13 @@ public class P1388_2 {
     static char[][]a;
     static void o(int i,int j,int x) {
 	h[i][j]++;
+	for(int[]b:h)System.out.println(Arrays.toString(b));
+	try {
+	    Thread.sleep(1000);
+	} catch (InterruptedException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
 	R+=x;
 	if(i<n[0]-1)if(h[i+1][j]<1)if(a[i+1][j]=='|')o(i+1,j,0);
 	if(i>0)if(h[i-1][j]<1)if(a[i-1][j]=='|')o(i-1,j,0);
