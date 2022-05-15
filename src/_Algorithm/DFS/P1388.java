d2ffe9c2758049889a7a2bf02bdda38264ebf4fe
nn98 <jkllhgb@gmail.com>
2022-05-16 00:28:57 +0900

    1388

--- src/_Algorithm/DFS/P1388.java
+++ src/_Algorithm/DFS/P1388.java
@@ -10,9 +10,10 @@ public class P1388 {
     static void o(int i,int j,char c) {
 	h[i][j]++;
 	char d=a[i][j];
-	R+=c==d?0:1;
+//	R+=c==d?0:1;
+	System.out.println(i+" "+j+" c:"+c+" d:"+d);
 	if(i<n-1)if(h[i+1][j]<1)o(i+1,j,d=='|'?d:0);
-	if(j<n-1)if(h[i][j+1]<1)o(i,j+1,d=='-'?d:0);
+	if(j<m-1)if(h[i][j+1]<1)o(i,j+1,d=='-'?d:0);
     }
     public static void main(String[] args)throws Exception{
 	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
