acbd4344721f2614d112825f2ebe3f5d1cd3323d
nn98 <jkllhgb@gmail.com>
2022-05-16 00:11:09 +0900

    1388

--- src/_Algorithm/DFS/P1388.java
+++ src/_Algorithm/DFS/P1388.java
@@ -1,10 +1,19 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.*;
+
 public class P1388 {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+    static int n,m,i,j,h[][],R;
+    static char[][]a;
+    public static void main(String[] args)throws Exception{
+	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer t=new StringTokenizer(r.readLine());
+	n=r.readLine().charAt(0)-'0';
+	System.out.println(n);
+	a=new char[n][];
+	for(;i<n;a[i++]=r.readLine().toCharArray());
     }
 
 }
