e7522d611d61332a3ed51ff344f97bed025163fc
nn98 <jkllhgb@gmail.com>
2022-05-15 23:59:20 +0900

    16173

--- src/_Algorithm/DFS/P16173.java
+++ src/_Algorithm/DFS/P16173.java
@@ -1,10 +1,22 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.*;
+
 public class P16173 {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+    static int n,m,i,j,k,a[][],h[];
+    public static void main(String[] args)throws Exception{
+	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	n=Integer.parseInt(r.readLine());
+	h=new int[n];
+	a=new int[n][n];
+	StringTokenizer t;
+	for(;i<n;i++) {
+	    t=new StringTokenizer(r.readLine());
+	    for(j=0;j<n;a[i][j++]=Integer.parseInt(t.nextToken()));
+	}
+	
     }
 
 }
