5cdd2d0c97da16a1a3643d9cbd0eab3d7f3bbfd0
nn98 <jkllhgb@gmail.com>
2022-05-22 05:13:00 +0900

    import from SubPC

--- src/_Algorithm/DFS/P3187.java
+++ src/_Algorithm/DFS/P3187.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P3184 {
+public class P3187 {
 
 	static int n,m,i,j,h[][],p,q,d[]= {0,1,0,-1},b[]= {1,0,-1,0},P,Q;
 	static char[][]a;
---
+++ src/_Algorithm/Simple_Implementation/P25166.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P25166 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,j=1,u=0;
+		
+	}
+
+}
