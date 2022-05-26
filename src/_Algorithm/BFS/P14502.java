92a56ba37b92c585e95d1dec752edeb3b63b1300
nn98 <jkllhgb@gmail.com>
2022-05-26 19:40:40 +0900

    14502

--- src/_Algorithm/BFS/P14502.java
+++ src/_Algorithm/BFS/P14502.java
@@ -4,8 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class P14502{
+    static int n,m,i,j,h[][];
+    static String[][]a;
     public static void main(String[]z)throws Exception{
 	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 	StringTokenizer t=new StringTokenizer(r.readLine());
+	n=Integer.parseInt(t.nextToken());
+	m=Integer.parseInt(t.nextToken());
+	for(;i<n;i++) {
+	    t=new StringTokenizer(r.readLine());
+	    for(j=0;j<m;a[i][j++]=t.nextToken());
+	}
+	for(String[]s:a)System.out.println(Arrays.toString(s));
     }
 }
