90fe844e0d89c258b4b96549e1d61643061566b6
nn98 <jkllhgb@gmail.com>
2022-05-27 12:40:56 +0900

    TA

--- src/TA/JarResult.java
+++ src/TA/JarResult.java
@@ -1,9 +1,31 @@
 package TA;
 
+import java.io.*;
+import java.util.*;
+
 public class JarResult {
 
+    public static String execCmd(String cmd) {
+	    try {
+	        Process process = Runtime.getRuntime().exec("cmd /c " + cmd);
+	        BufferedReader reader = new BufferedReader(
+	                new InputStreamReader(process.getInputStream()));
+	        String line = null;
+	        StringBuffer sb = new StringBuffer();
+	        sb.append(cmd);
+	        while ((line = reader.readLine()) != null) {
+	            sb.append(line);
+	            sb.append("\n");
+	        }
+	        return sb.toString();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	System.out.println(execCmd("dir"));
 
     }
 
