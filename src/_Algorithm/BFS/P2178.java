8a9091f4a808e0580a5b2fa2e1e28784e676a149
nn98 <jkllhgb@gmail.com>
2022-05-27 23:07:27 +0900

    2178

--- src/_Algorithm/BFS/P2178.java
+++ src/_Algorithm/BFS/P2178.java
@@ -1,10 +1,20 @@
 package _Algorithm.BFS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P2178 {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+    static int n,m,i,j,h[][];
+    static char[][]a;
+    public static void main(String[] args)throws Exception{
+	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer t=new StringTokenizer(r.readLine());
+	n=Integer.parseInt(t.nextToken());
+	m=Integer.parseInt(t.nextToken());
+	a=new char[n][m];
+	h=new int[n][m];
     }
 
 }
