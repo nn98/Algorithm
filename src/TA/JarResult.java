f4f5d776d7b856618dff6206f8bcc6c2372b48ee
nn98 <jkllhgb@gmail.com>
2022-05-27 13:09:31 +0900

    TA - jar파일 실행 및 결과 저장용 프로그램

--- src/TA/JarResult.java
+++ src/TA/JarResult.java
@@ -5,30 +5,39 @@ import java.util.*;
 
 public class JarResult {
 
-    public static String execCmd(String cmd) {
+	static StringBuffer sb = new StringBuffer();
+	static int flag;
+    public static void execCmd(String cmd) {
 	System.out.println(cmd);
 	    try {
-		
 	        Process process = Runtime.getRuntime().exec("cmd /c " + cmd);
 	        BufferedReader reader = new BufferedReader(
 	                new InputStreamReader(process.getInputStream()));
 	        String line = null;
-	        StringBuffer sb = new StringBuffer();
-	        sb.append(cmd);
+	        
+	        sb.append(cmd+"\n");
 	        while ((line = reader.readLine()) != null) {
-//	            System.out.println(line+" "+Arrays.toString(line.split("\\.")));
+String[]arr=line.split("\\.");
+		if(arr[arr.length-1].equals("jar")){
+flag=1;
+execCmd("java -jar "+line);
+flag=0;
+}
+if(flag>0){
 	            sb.append(line);
 	            sb.append("\n");
+}
 	        }
-	        return sb.toString();
 	    } catch (Exception e) {
+System.out.println("err");
 	        e.printStackTrace();
 	    }
-	    return null;
+	    return;
 	}
     public static void main(String[] args) {
-//	System.out.println(".classpath "+Arrays.toString(".classpath".split(".")));
-	System.out.println(execCmd("dir /b/oe/on"));
+	System.out.println("Program is running...");
+execCmd("dir /b/oe/on");
+	System.out.println("Result: \n"+sb);
 
     }
 
