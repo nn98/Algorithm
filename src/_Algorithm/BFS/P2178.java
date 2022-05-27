48ac03be289df4c6ade45e59266e652f13f916ed
nn98 <jkllhgb@gmail.com>
2022-05-27 23:20:26 +0900

    2178

--- src/_Algorithm/BFS/P2178.java
+++ src/_Algorithm/BFS/P2178.java
@@ -5,13 +5,11 @@ import java.util.StringTokenizer;
 public class P2178 {
     static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0};
     static void o(int i,int j,int v) {
-//	System.out.println(i+" "+j+" "+v);
 	h[i][j]=v++;
 	int x=0,I,J;
 	for(;x<4;x++) {
 	    I=i+p[x];
 	    J=j+q[x];
-//	    System.out.println(I+" "+J+" "+v);
 	    if(I>=0&I<n&J>=0&J<m)if(a[I][J]=='1')if(h[I][J]==0|v<h[I][J])o(I,J,v);
 	}
     }
