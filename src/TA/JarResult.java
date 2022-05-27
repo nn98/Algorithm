10a1786b312057d77a491ee1096ac4e89293be2f
nn98 <jkllhgb@gmail.com>
2022-05-27 13:09:29 +0900

    TA - jar파일 실행 및 결과 저장용 프로그램

--- src/TA/JarResult.java
+++ src/TA/JarResult.java
@@ -6,7 +6,9 @@ import java.util.*;
 public class JarResult {
 
     public static String execCmd(String cmd) {
+	System.out.println(cmd);
 	    try {
+		
 	        Process process = Runtime.getRuntime().exec("cmd /c " + cmd);
 	        BufferedReader reader = new BufferedReader(
 	                new InputStreamReader(process.getInputStream()));
@@ -14,6 +16,7 @@ public class JarResult {
 	        StringBuffer sb = new StringBuffer();
 	        sb.append(cmd);
 	        while ((line = reader.readLine()) != null) {
+//	            System.out.println(line+" "+Arrays.toString(line.split("\\.")));
 	            sb.append(line);
 	            sb.append("\n");
 	        }
@@ -24,8 +27,8 @@ public class JarResult {
 	    return null;
 	}
     public static void main(String[] args) {
-	
-	System.out.println(execCmd("dir"));
+//	System.out.println(".classpath "+Arrays.toString(".classpath".split(".")));
+	System.out.println(execCmd("dir /b/oe/on"));
 
     }
 
