f84464bef86e75b71d8aa2ec58e29d91ebca942e
nn98 <jkllhgb@gmail.com>
2022-05-27 23:20:14 +0900

    2178

--- src/_Algorithm/BFS/P2178.java
+++ src/_Algorithm/BFS/P2178.java
@@ -1,11 +1,8 @@
 package _Algorithm.BFS;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 public class P2178 {
-
     static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0};
     static void o(int i,int j,int v) {
 //	System.out.println(i+" "+j+" "+v);
@@ -30,5 +27,4 @@ public class P2178 {
 	o(0,0,1);
 	System.out.println(h[n-1][m-1]);
     }
-
 }
