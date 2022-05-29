4c5fcc8dde2b9e6cde5d95328792a0f75f216d78
nn98 <jkllhgb@gmail.com>
2022-05-29 23:51:47 +0900

    ssafy 2022-05-29

--- src/ssafy/Solution.java
+++ src/ssafy/Solution.java
@@ -1,3 +1,5 @@
+package ssafy;
+
 import java.io.*;
 import java.util.*;
 
--- src/ssafy/TestCode_BT.java
+++ src/ssafy/TestCode_BT.java
@@ -1,3 +1,5 @@
+package ssafy;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
