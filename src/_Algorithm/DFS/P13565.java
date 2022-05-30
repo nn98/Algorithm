5e2853390e4c6430166302ed4ad874a04c5fd174
nn98 <jkllhgb@gmail.com>
2022-05-31 00:31:52 +0900

    13565

--- src/_Algorithm/DFS/P13565.java
+++ src/_Algorithm/DFS/P13565.java
@@ -1,5 +1,29 @@
 package _Algorithm.DFS;
 
-public class P13565 {
+import java.io.*;
 
+public class P13565 {
+	static int n,m,i,j,p[]= {0,1,0,-1},q[]= {1,0,-1,0},b[][];
+	static char[][]a;
+	static void o(int x,int y) {
+		if(x==n-1)j++;
+		if(j<1) {
+			b[x][y]++;
+			int i=0,X,Y;
+			for(;i<4;i++) {
+				X=x+p[i];
+				Y=y+q[i];
+				if(X>=0&X<n&Y>=0&Y<m)if(a[X][Y]<1)o(X,Y);
+			}
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String[]c=r.readLine().split(" ");
+		n=Integer.parseInt(c[0]);
+		m=Integer.parseInt(c[0]);
+		a=new char[n][];
+		for(;i<n;a[i++]=r.readLine().toCharArray());
+		b=new int[n][m];
+	}
 }
