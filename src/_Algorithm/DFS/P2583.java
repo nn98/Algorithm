18f252b490f93e31b147ade421f57ee70241e138
nn98 <jkllhgb@gmail.com>
2022-06-01 23:28:17 +0900

    2583

--- src/_Algorithm/DFS/P2583.java
+++ src/_Algorithm/DFS/P2583.java
@@ -4,11 +4,26 @@ import java.io.*;
 import java.util.*;
 
 public class P2583 {
+	
+	static int n,m,i,j,a[][],h[][],k,I,J,p= {1,0,-1,0},q= {0,1,0,-1},o;
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		a=new int[n][m];
+		h=new int[n][m];
+		k=Integer.parseInt(t.nextToken());
+		for(k-->0) {
+			t=new StringTokenizer(r.readLine());
+			i=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			I=Integer.parseInt(t.nextToken());
+			J=Integer.parseInt(t.nextToken());
+			for(;i<I;i++)for(o=j;o<J;o++)a[i][o]++;
+		}
+		for(int[]c:a)System.out.println(Arrays.toString(c));
 	}
 
 }
