bcad2448b4178124a5551474c6337f222651fa27
nn98 <jkllhgb@gmail.com>
2022-06-01 23:28:23 +0900

    2583

--- src/_Algorithm/DFS/P2583.java
+++ src/_Algorithm/DFS/P2583.java
@@ -7,6 +7,13 @@ public class P2583 {
 	
 	static int n,m,i,j,a[][],h[][],k,I,J,p[]= {1,0,-1,0},q[]= {0,1,0,-1},o;
 
+	static void p(int x,int y) {
+		h[x][y]++;
+		int i=0,X,Y;
+		for(;i<4;i++) {
+			
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
@@ -24,6 +31,11 @@ public class P2583 {
 			for(;i<I;i++)for(o=j;o<J;o++)a[i][o]++;
 		}
 		for(int[]c:a)System.out.println(Arrays.toString(c));
+		o=0;
+		for(i=0;i<n;i++)for(j=0;j<m;j++)if(h[i][j]<1) {
+			p(i,j);
+			o++;
+		}
 	}
 
 }
