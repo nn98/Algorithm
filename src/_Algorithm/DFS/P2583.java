f1c375cd736706cf591f052545e95428f706031c
nn98 <jkllhgb@gmail.com>
2022-06-01 23:19:26 +0900

    2583

--- src/_Algorithm/DFS/P2583.java
+++ src/_Algorithm/DFS/P2583.java
@@ -1,9 +1,13 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.*;
+
 public class P2583 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args))throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 
 	}
 
