b1e3ef0942418e41e08bc177d72c79102a9c6f43
nn98 <jkllhgb@gmail.com>
2022-06-01 23:19:33 +0900

    2583

--- src/_Algorithm/DFS/P2583.java
+++ src/_Algorithm/DFS/P2583.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class P2583 {
 
-	public static void main(String[] args))throws Exception{
+	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 
