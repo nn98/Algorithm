8a2a5cd97814079530e6647ab676726ad7ea6631
nn98 <jkllhgb@gmail.com>
2022-06-02 23:25:14 +0900

    1953

--- src/_Algorithm/DFS/P1953.java
+++ src/_Algorithm/DFS/P1953.java
@@ -5,14 +5,21 @@ import java.util.*;
 
 public class P1953 {
 
-    static int n,m,i,j,o,R,a[][],b[];
-    public static void main(String[] args)throws Exception{
-	BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-	n=Integer.parseInt(r.readLine());
-	for(;i<n;i++) {
-	    StringTokenizer t=new StringTokenizer(r.readLine());
+	static int n,m,i,p,q,R,a[][],b[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		for(;i<n;i++) {
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			m=Integer.parseInt(t.nextToken());
+			for(;m-->0;) {
+				p=Integer.parseInt(t.nextToken());
+				q=Integer.parseInt(t.nextToken());
+				a[p][q]=a[q][p]=1;
+			}
+		}
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
-    }
 
 }
