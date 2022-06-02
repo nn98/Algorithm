8306ca50aac78839bde3a1d8389838580ded5d35
nn98 <jkllhgb@gmail.com>
2022-06-02 22:59:24 +0900

    TA

--- src/TA/IDConverter.java
+++ src/TA/IDConverter.java
@@ -8,13 +8,15 @@ public class IDConverter {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Input ID_LIST. end by 0");
+		int i=0;
 		String s;
 		StringBuffer f=new StringBuffer("[");
 		while(!(s=r.readLine()).equals("0")) {
 			if(s.equals(""))continue;
 			f.append("\""+s+"\",");
+			if(++i%5==0)f.append("\n");
 		}
-		f.append("]");
+		f.replace(f.length()-1,f.length(),"]");
 		System.out.println(f);
 	}
 
