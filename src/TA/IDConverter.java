917280936848f5975d361dbc7ee59b7e9c406495
nn98 <jkllhgb@gmail.com>
2022-06-02 22:56:37 +0900

    TA

--- src/TA/IDConverter.java
+++ src/TA/IDConverter.java
@@ -1,10 +1,20 @@
 package TA;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class IDConverter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Input ID_LIST. end by 0");
+		String s;
+		StringBuffer f=new StringBuffer("[");
+		while(!(s=r.readLine()).equals("0")) {
+			f.append("\""+s+"\",");
+		}
+		f.append("]");
+		System.out.println(f);
 	}
 
 }
