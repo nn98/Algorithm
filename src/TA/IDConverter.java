8fb38613b8a0a8793d73e93c61272aafbab9d1b5
nn98 <jkllhgb@gmail.com>
2022-06-02 23:02:14 +0900

    TA set bojID_LIST

--- src/TA/IDConverter.java
+++ src/TA/IDConverter.java
@@ -12,11 +12,11 @@ public class IDConverter {
 		String s;
 		StringBuffer f=new StringBuffer("[");
 		while(!(s=r.readLine()).equals("0")) {
-			if(s.equals(""))continue;
+//			if(s.equals(""))continue;
 			f.append("\""+s+"\",");
 			if(++i%5==0)f.append("\n");
 		}
-		f.replace(f.length()-1,f.length(),"]");
+		f.replace(i%5==0?f.length()-2:f.length()-1,f.length(),"]");
 		System.out.println(f);
 	}
 
@@ -30,4 +30,15 @@ public class IDConverter {
 "songjuhwan33","a26214165","impjs17","iyuna6577","dlwldbs8015@gmail.com",
 "yjy5923","qwe916","reperfection","koreanwi","zoeyourlife",
 "hahnsh64","3021062","hun5078"]
+*/
+/*
+2반
+["minjeong2904","minjune8506","kim97","tndusy27","kshyun419",
+"jkkj0414","06zzkimzz06","rlaxogjs5656","haeunkim0807","hesy0147",
+"201632006","hyk4238","a3920679","jinsu4755","eunseo5355",
+"sonyejin6449","","20184120","hs980414","202014021",
+"kuntek1953","lsh328328","201634015","leejh0702","szljs",
+"dlaxodud1217","abcdeun","jiyoon","","vact19",
+"joseeun0805","wnehdtjr5","hackin","jinseeun1127","dd0910",
+"","gustn8523","gjwldud0719"]
 */
