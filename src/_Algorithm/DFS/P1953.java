804a38a50309f916eddc7c36d331add9ec6a08cf
nn98 <jkllhgb@gmail.com>
2022-06-02 23:51:54 +0900

    1953

--- src/_Algorithm/DFS/P1953.java
+++ src/_Algorithm/DFS/P1953.java
@@ -7,22 +7,25 @@ public class P1953 {
 
 	static int n,m,i,j,R,a[][],b[],h[];
 	static void o(int x) {
+		System.out.println(x+Arrays.toString(b));
 		h[x]++;
 		for(int i=0;++i<n;) {
 			b[i]=a[x][i];
 		}
 		for(int i=0;++i<n;) {
-			if(a[x][i]<1)if(h[i]<1){
+			if(a[x][i]<1)if(b[i]<1)if(h[i]<1){
 				p(i);
 			}
 		}
 	}
 	static void p(int y) {
+		System.out.println(y+Arrays.toString(b));
+		h[y]++;
 		for(int i=0;++i<n;) {
-			b[i]=a[y][i];
+			if(a[y][i]>0)b[i]=1;
 		}
 		for(int i=0;++i<n;) {
-			if(a[y][i]<1)if(h[i]<1){
+			if(a[y][i]<1)if(b[i]<1)if(h[i]<1){
 				p(i);
 			}
 		}
@@ -30,6 +33,7 @@ public class P1953 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer f=new StringBuffer();
 		n=Integer.parseInt(r.readLine())+1;
 		h=new int[n];
 		b=new int[n];
@@ -45,6 +49,11 @@ public class P1953 {
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 		o(1);
 		System.out.println(Arrays.toString(b));
+		for(i=0;++i<n;R+=b[i]<1?1:0,f.append(i+" "));
+		System.out.println(R+"\n"+f);
+		R=0;
+		f=new StringBuffer();
+		for(i=0;++i<n;R+=b[i]>0?1:0,f.append(i+" "));
 	}
 
 }
