856bdbacdf2f1446a9895bc7f96040f7be8a2ba7
nn98 <jkllhgb@gmail.com>
2022-06-02 23:42:44 +0900

    1953

--- src/_Algorithm/DFS/P1953.java
+++ src/_Algorithm/DFS/P1953.java
@@ -5,23 +5,44 @@ import java.util.*;
 
 public class P1953 {
 
-	static int n,m,i,p,q,R,a[][],b[];
+	static int n,m,i,j,R,a[][],b[];
+	static void o(int x) {
+		for(int i=0;++i<n;) {
+			b[i]=a[x][i];
+		}
+		for(int i=0;++i<n;) {
+			if(a[x][i]<1) {
+				p(i);
+			}
+		}
+	}
+	static void p(int y) {
+		for(int i=0;++i<n;) {
+			b[i]=a[y][i];
+		}
+		for(int i=0;++i<n;) {
+			if(a[y][i]<1) {
+				p(i);
+			}
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		n=Integer.parseInt(r.readLine());
+		n=Integer.parseInt(r.readLine())+1;
 		b=new int[n];
 		a=new int[n][n];
-		for(;i<n;i++) {
+		for(;++i<n;) {
 			StringTokenizer t=new StringTokenizer(r.readLine());
 			m=Integer.parseInt(t.nextToken());
 			for(;m-->0;) {
-				p=Integer.parseInt(t.nextToken());
-				q=Integer.parseInt(t.nextToken());
-				a[p][q]=a[q][p]=1;
+				j=Integer.parseInt(t.nextToken());
+				a[i][j]=a[j][i]=1;
 			}
 		}
 		for(int[]b:a)System.out.println(Arrays.toString(b));
+		o(1);
+		System.out.println(Arrays.toString(b));
 	}
 
 }
