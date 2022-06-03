97c6e0c45ff68ad12e114f6e4c0c9e7c7e5fa4ad
nn98 <jkllhgb@gmail.com>
2022-06-04 00:32:41 +0900

    14427

--- src/_Algorithm/Segment_Tree/P14427.java
+++ src/_Algorithm/Segment_Tree/P14427.java
@@ -6,14 +6,18 @@ import java.util.StringTokenizer;
 
 public class P14427 {
 	static int n,a[],l=1,i,j;
+	static void o() {
+		for(;j>2;j/=2,a[j]=a[j*2]<=a[j*2+1]?j*2:j*2+1);
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		n=Integer.parseInt(r.readLine());
 		for(;l<n;l*=2);
 		a=new int[l*2];
+		a[0]=Integer.MAX_VALUE;
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		for(;i<n;a[l+i++]=Integer.parseInt(t.nextToken()));
+		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),o());
 		System.out.println(Arrays.toString(a));
 	}
 
