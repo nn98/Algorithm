be7659863d72b5160cf03c5ef10b64ffa12d2b27
nn98 <jkllhgb@gmail.com>
2022-06-04 00:54:27 +0900

    14427

--- src/_Algorithm/Segment_Tree/P14427.java
+++ src/_Algorithm/Segment_Tree/P14427.java
@@ -11,7 +11,7 @@ public class P14427 {
 			j/=2;
 			a[j]=(a[j*2]==0?Integer.MAX_VALUE:a[j*2])<=(a[j*2+1]==0?Integer.MAX_VALUE:a[j*2+1])?j*2:j*2+1;
 		}
-		for(;j>2;j/=2,a[j]=(a[a[j*2]]==0?Integer.MAX_VALUE:a[a[j*2]])<=(a[a[j*2+1]]==0?Integer.MAX_VALUE:a[a[j*2+1]])?a[j*2]:a[j*2+1]);
+		for(;j>1;j/=2,a[j]=(a[a[j*2]]==0?Integer.MAX_VALUE:a[a[j*2]])<=(a[a[j*2+1]]==0?Integer.MAX_VALUE:a[a[j*2+1]])?a[j*2]:a[j*2+1]);
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -23,17 +23,18 @@ public class P14427 {
 //		Arrays.fill(a,Integer.MAX_VALUE);
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),o());
+		System.out.println(Arrays.toString(a));
 		for(n=Integer.parseInt(r.readLine());n-->0;) {
 			t=new StringTokenizer(r.readLine());
 			k=Integer.parseInt(t.nextToken());
-			if(k>1)w.write(a[1]+"\n");
+			if(k>1)w.write(a[1]-l+1+"\n");
 			else {
 				a[j=Integer.parseInt(t.nextToken())+l-1]=Integer.parseInt(t.nextToken());
 				o();
+				System.out.println(Arrays.toString(a));
 			}
 		}
 		w.flush();
-//		System.out.println(Arrays.toString(a));
 	}
 
 }
