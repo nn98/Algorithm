3ee47f77424a61d3a75fe4571fb46c5e3adda112
nn98 <jkllhgb@gmail.com>
2022-06-03 21:51:15 +0900

    1953

--- src/_Algorithm/DFS/P1953_2.java
+++ src/_Algorithm/DFS/P1953_2.java
@@ -7,9 +7,14 @@ public class P1953_2 {
 
 	static int n,m,i,j,R,a[][],b[],h[];
 	static void o(int x,int t) {
+		System.out.println("x: "+x+" t: "+t);
 		b[x] = t;
 		h[x] = 1;
-		for(int i=0;++i<n;)if(a[x][i]>0&h[i]<0)o(i,-t);
+		for(int i=0;++i<n;) {
+			System.out.println(a[x][i]>0);
+			System.out.println(h[i]<1);
+			if(a[x][i]>0&h[i]<1)o(i,-t);
+		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -27,7 +32,10 @@ public class P1953_2 {
 		}
 		h=new int[n];
 		b=new int[n];
-		for(i=0;++i<n;)if(h[i]<1)o(i,1);
+		for(i=0;++i<n;)if(h[i]<1) {
+			System.out.println("o");
+			o(i,1);
+		}
 		System.out.println(Arrays.toString(b));
 		System.out.println(Arrays.toString(h));
 		for(i=0;++i<n;)m+=b[i]>0?1:0;
