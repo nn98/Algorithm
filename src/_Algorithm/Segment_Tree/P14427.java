7f1af9a14522097de9953be8ee67665b6d379f96
nn98 <jkllhgb@gmail.com>
2022-06-04 00:27:29 +0900

    14427

---
+++ src/_Algorithm/Segment_Tree/P14427.java
@@ -0,0 +1,20 @@
+package _Algorithm.Segment_Tree;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P14427 {
+	static int n,a[],l=1,i,j;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		n=Integer.parseInt(r.readLine());
+		for(;l<i;l*=2);
+		a=new int[l*2];
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		for(;i<n;a[l+i++]=Integer.parseInt(t.nextToken()));
+		System.out.println(Arrays.toString(a));
+	}
+
+}
