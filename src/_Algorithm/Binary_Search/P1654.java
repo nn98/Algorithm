5772f488ee792889cc4657aa6eac0da9c2b94648
nn98 <jkllhgb@gmail.com>
2022-06-04 23:51:54 +0900

    1654

--- src/_Algorithm/Binary_Search/P1654.java
+++ src/_Algorithm/Binary_Search/P1654.java
@@ -1,10 +1,18 @@
 package _Algorithm.Binary_Search;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P1654 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String[]s=r.readLine().split(" ");
+		int n=Integer.parseInt(s[0]),m=Integer.parseInt(s[1]),a[]=new int[n],i=0,M=0,N=1,D;
+		for(;i<n;a[i]=Integer.parseInt(r.readLine()),M=M>a[i]?M:a[i],i++);
+		for(;N<M;) {
+			D=N+(M-N)/2;
+		}
 	}
 
 }
