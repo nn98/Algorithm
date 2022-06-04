01ab40dff3b0697aa4262b4a70f7cc64e171ec92
nn98 <jkllhgb@gmail.com>
2022-06-04 23:59:42 +0900

    1654

--- src/_Algorithm/Binary_Search/P1654.java
+++ src/_Algorithm/Binary_Search/P1654.java
@@ -8,11 +8,12 @@ public class P1654 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		String[]s=r.readLine().split(" ");
-		int n=Integer.parseInt(s[0]),m=Integer.parseInt(s[1]),a[]=new int[n],i=0,M=0,N=1,D;
+		int n=Integer.parseInt(s[0]),m=Integer.parseInt(s[1]),a[]=new int[n],i=0;
+		long M=0,c,N=0,D;
 		for(;i<n;a[i]=Integer.parseInt(r.readLine()),M=M>a[i]?M:a[i]+1,i++);
 		for(;N<M;) {
 			D=N+(M-N)/2;
-			long c=0;
+			c=0;
 			for(i=0;i<n;c+=a[i++]/D);
 			if(c<m)M=D;
 			else N=D+1;
