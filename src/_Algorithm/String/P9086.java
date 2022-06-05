b4de381ddbb50b8caa640174c520e6bbb4e72636
nn98 <jkllhgb@gmail.com>
2022-06-05 23:56:50 +0900

    9086

--- src/_Algorithm/String/P9086.java
+++ src/_Algorithm/String/P9086.java
@@ -1,10 +1,14 @@
 package _Algorithm.String;
 
+import java.util.Scanner;
+
 public class P9086 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0;
+		String a;
+		for(;n-->0;a=s.next(),System.out.println((char)a.charAt(0)+(char)a.charAt(a.length()-1)));
 	}
 
 }
