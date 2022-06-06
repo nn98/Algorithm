1c126cab1c7a94268b509554c592a3bae4f2e4cc
user <user@DESKTOP-J4G4RDR>
2022-06-06 14:07:52 +0900

    c819

---
+++ src/_Contest/c819/A.java
@@ -0,0 +1,15 @@
+package _Contest.c819;
+
+import java.io.*;
+import java.util.*;
+
+public class A {
+
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String[]a=r.readLine().split(" ");
+		double n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),c=n-n*m*.01;
+		System.out.println(c<100?1:0);
+	}
+
+}
