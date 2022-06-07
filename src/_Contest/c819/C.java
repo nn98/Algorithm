6e8ead6be4e827e07c2359d507f28b47fab33586
nn98 <jkllhgb@gmail.com>
2022-06-07 13:42:27 +0900

    c819

--- src/_Contest/c819/C.java
+++ src/_Contest/c819/C.java
@@ -7,6 +7,6 @@ public class C {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		String[]a=r.readLine().split(" ");
-		int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),i=0,j,
+		int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),i=0,j;
 	}
 }
