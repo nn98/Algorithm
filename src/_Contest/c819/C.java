07b513eef8cf331ac72b5c84ce896ae5dc2523b9
nn98 <jkllhgb@gmail.com>
2022-06-07 13:42:25 +0900

    c819

--- src/_Contest/c819/C.java
+++ src/_Contest/c819/C.java
@@ -1,5 +1,12 @@
 package _Contest.c819;
+import java.io.*;
+import java.util.*;
 
 public class C {
 
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String[]a=r.readLine().split(" ");
+		int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),i=0,j,
+	}
 }
