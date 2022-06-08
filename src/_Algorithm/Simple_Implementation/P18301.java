0aa24aca35c3375e78cd4a75f795f6d9c7444b92
nn98 <jkllhgb@gmail.com>
2022-06-08 23:52:46 +0900

    18301

--- src/_Algorithm/Simple_Implementation/P18301.java
+++ src/_Algorithm/Simple_Implementation/P18301.java
@@ -1,10 +1,12 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P18301 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		System.out.println((s.nextInt()+1)*(s.nextInt()+1)/(s.nextInt()+1)-1);
 	}
 
 }
