a6c5570ebd99576f257bd17b9145153aa82b3eac
nn98 <jkllhgb@gmail.com>
2022-06-10 22:58:57 +0900

    21736

--- src/_Algorithm/BFS/P21736.java
+++ src/_Algorithm/BFS/P21736.java
@@ -4,8 +4,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P21736 {
-	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0};
+	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0},r;
 	static char[][]a;
+	static void o(int x,int y) {
+		h[x][y]++;
+		if(a[x][y]=='P')r++;
+		int i=0,X,Y;
+		for(;i<4;i++) {
+			X=x+p[i];
+			Y=y+q[i];
+			if(X>=0&X<n&Y>=0&Y<m)if(h[X][Y]<1)o(X,Y);
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		String[]b=r.readLine().split(" ");
