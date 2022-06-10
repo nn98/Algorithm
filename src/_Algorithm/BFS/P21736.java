6eb31e322566d7a11299edd79b2659af512ec6a8
nn98 <jkllhgb@gmail.com>
2022-06-10 23:04:21 +0900

    21736

--- src/_Algorithm/BFS/P21736.java
+++ src/_Algorithm/BFS/P21736.java
@@ -8,7 +8,7 @@ public class P21736 {
 	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0},R;
 	static char[][]a;
 	static void o(int x,int y) {
-		System.out.println(x+" "+y);
+//		System.out.println(x+" "+y);
 		h[x][y]++;
 		if(a[x][y]=='P')R++;
 		int i=0,X,Y;
@@ -27,7 +27,7 @@ public class P21736 {
 		h=new int[n][m];
 		for(;i<n;a[i++]=r.readLine().toCharArray());
 		for(;j<n;j++)for(i=0;i<m;i++)if(a[j][i]=='I')o(j,i);
-		for(int[]c:h)System.out.println(Arrays.toString(c));
+//		for(int[]c:h)System.out.println(Arrays.toString(c));
 		System.out.println(R<1?"TT":R);
 	}
 
