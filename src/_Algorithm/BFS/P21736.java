d68e7939a5b25c80aa390085b4aea2625bfa506d
nn98 <jkllhgb@gmail.com>
2022-06-10 22:54:22 +0900

    21736

--- src/_Algorithm/BFS/P21736.java
+++ src/_Algorithm/BFS/P21736.java
@@ -1,10 +1,18 @@
 package _Algorithm.BFS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P21736 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0};
+	static char[][]a;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		String[]b=r.readLine().split(" ");
+		n=Integer.parseInt(b[0]);
+		m=Integer.parseInt(b[0]);
+		a=new char[n][];
+		for(;i<n;a[i++]=r.readLine().toCharArray());
 	}
 
 }
