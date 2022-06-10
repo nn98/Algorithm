c4190edc8c9468197446391a510563b725183c66
nn98 <jkllhgb@gmail.com>
2022-06-10 23:04:18 +0900

    21736

--- src/_Algorithm/BFS/P21736.java
+++ src/_Algorithm/BFS/P21736.java
@@ -2,13 +2,15 @@ package _Algorithm.BFS;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class P21736 {
-	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0},r;
+	static int n,m,i,j,h[][],p[]= {0,1,0,-1},q[]= {1,0,-1,0},R;
 	static char[][]a;
 	static void o(int x,int y) {
+		System.out.println(x+" "+y);
 		h[x][y]++;
-		if(a[x][y]=='P')r++;
+		if(a[x][y]=='P')R++;
 		int i=0,X,Y;
 		for(;i<4;i++) {
 			X=x+p[i];
@@ -20,11 +22,13 @@ public class P21736 {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		String[]b=r.readLine().split(" ");
 		n=Integer.parseInt(b[0]);
-		m=Integer.parseInt(b[0]);
+		m=Integer.parseInt(b[1]);
 		a=new char[n][];
+		h=new int[n][m];
 		for(;i<n;a[i++]=r.readLine().toCharArray());
 		for(;j<n;j++)for(i=0;i<m;i++)if(a[j][i]=='I')o(j,i);
-		System.out.println(r<1?"TT":r);
+		for(int[]c:h)System.out.println(Arrays.toString(c));
+		System.out.println(R<1?"TT":R);
 	}
 
 }
