7c07f55df7984124f766ed8fb7955dcff39a1765
nn98 <jkllhgb@gmail.com>
2022-06-13 02:15:01 +0900

    1238

--- src/_Algorithm/DFS/P1238.java
+++ src/_Algorithm/DFS/P1238.java
@@ -12,6 +12,8 @@ public class P1238 {
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
 		x=Integer.parseInt(t.nextToken());
+		a=new int[n][n];
+		h=new int[n][n];
 		for(;z++<m;t=new StringTokenizer(r.readLine()),i=Integer.parseInt(t.nextToken()),j=Integer.parseInt(t.nextToken()),a[i][j]=a[j][i]=Integer.parseInt(t.nextToken()));
 		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
