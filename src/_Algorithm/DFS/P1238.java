b96a3879d89b46d6c12cb984f2cd379c77af9df6
nn98 <jkllhgb@gmail.com>
2022-06-13 02:14:59 +0900

    1238

--- src/_Algorithm/DFS/P1238.java
+++ src/_Algorithm/DFS/P1238.java
@@ -1,10 +1,19 @@
 package _Algorithm.DFS;
 
+import java.io.*;
+import java.util.*;
+
 public class P1238 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,x,z,i,j,a[][],h[][];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		x=Integer.parseInt(t.nextToken());
+		for(;z++<m;t=new StringTokenizer(r.readLine()),i=Integer.parseInt(t.nextToken()),j=Integer.parseInt(t.nextToken()),a[i][j]=a[j][i]=Integer.parseInt(t.nextToken()));
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
