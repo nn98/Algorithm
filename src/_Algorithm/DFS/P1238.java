f9a4efe34e14e7a3bd9926421a573142b3812be4
nn98 <jkllhgb@gmail.com>
2022-06-14 08:14:52 +0900

    1238

--- src/_Algorithm/DFS/P1238.java
+++ src/_Algorithm/DFS/P1238.java
@@ -6,12 +6,15 @@ import java.util.*;
 public class P1238 {
 
 	static int n,m,x,z,i,j,a[][],h[][];
+	static void o(int x) {
+		
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 		n=Integer.parseInt(t.nextToken())+1;
 		m=Integer.parseInt(t.nextToken());
-		x=Integer.parseInt(t.nextToken());
+		x=Integer.parseInt(t.nextToken())+1;
 		a=new int[n][n];
 		h=new int[n][n];
 		for(;z++<m;t=new StringTokenizer(r.readLine()),a[Integer.parseInt(t.nextToken())][Integer.parseInt(t.nextToken())]=Integer.parseInt(t.nextToken()));
