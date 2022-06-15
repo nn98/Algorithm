93f4a85b1245514f975cb38b11487959b4ce33a1
nn98 <jkllhgb@gmail.com>
2022-06-16 00:20:56 +0900

    11659

--- src/_Algorithm/Prefix_Sum/P11659.java
+++ src/_Algorithm/Prefix_Sum/P11659.java
@@ -8,9 +8,12 @@ public class P11659 {
 	static void o() {
 		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
 	}
-//	static int p(int n,int s,int e) {
-//		
-//	}
+	static int p(int n,int s,int e) {
+		if(s>j|i>e)return 0;
+		if(i<=s&e<=j)return a[n];
+		int m=(s+e)/2;
+		return p(n*2,s,m)+p(n*2+1,m+1,e);
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
@@ -21,6 +24,7 @@ public class P11659 {
 		t=new StringTokenizer(r.readLine());
 		for(;i<n;j=i+++l,a[j]=Integer.parseInt(t.nextToken()),o());
 		System.out.println(Arrays.toString(a));
+		for(;m-->0;t=new StringTokenizer(r.readLine()),System.out.println(p(1,Integer.parseInt(t.nextToken()),Integer.parseInt(t.nextToken()))));
 	}
 
 }
