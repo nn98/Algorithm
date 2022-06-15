9b3cd5910edb62a55945e06ecf3f772dc7fab6ff
nn98 <jkllhgb@gmail.com>
2022-06-16 00:11:16 +0900

    11659

--- src/_Algorithm/Prefix_Sum/P11659.java
+++ src/_Algorithm/Prefix_Sum/P11659.java
@@ -1,10 +1,26 @@
 package _Algorithm.Prefix_Sum;
 
+import java.io.*;
+import java.util.*;
+
 public class P11659 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,l=1,a[],i,j;
+	static void o() {
+		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
+	}
+//	static int p(int n,int s,int e) {
+//		
+//	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		for(;l<n;l*=2);
+		a=new int[l*2];
+		t=new StringTokenizer(r.readLine());
+		for(;i<n;j=i+++l,a[j]=Integer.parseInt(t.nextToken()),o());
+		System.out.println(Arrays.toString(a));
 	}
 
 }
