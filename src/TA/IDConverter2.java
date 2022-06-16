900e4cea3ec9360006c78feb818bdac6384b8fa8
nn98 <jkllhgb@gmail.com>
2022-06-16 16:09:58 +0900

    TA/SoftP

--- src/TA/IDConverter2.java
+++ src/TA/IDConverter2.java
@@ -1,10 +1,23 @@
 package TA;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class IDConverter2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		int i=0;
+		String s;
+		StringTokenizer t;
+		StringBuffer f=new StringBuffer();
+		while(!(s=r.readLine()).equals("0")) {
+			t=new StringTokenizer(s);
+			f.append(String.format("insert into Student (ID, name, bojid) values (%d, \"%s\", \"%s\"",
+					Integer.parseInt(t.nextToken()),t.nextToken(),t.nextToken()));
+			if(++i%5==0)f.append("\n");
+		}
 	}
 
 }
