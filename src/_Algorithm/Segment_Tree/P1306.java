3bef72cc0a71456f71882fe6abb9a1ed71d069f8
nn98 <jkllhgb@gmail.com>
2022-06-18 17:04:10 +0900

    1306

--- src/_Algorithm/Segment_Tree/P1306.java
+++ src/_Algorithm/Segment_Tree/P1306.java
@@ -10,7 +10,6 @@ public class P1306 {
 		for(;j>1;j/=2,a[j]=a[j*2]>a[j*2+1]?a[j*2]:a[j*2+1]);
 	}
 	static int p(int n,int s,int e) {
-//		System.out.println(n+" "+s+" "+e);
 		if(e<x|y<s)return 0;
 		if(s>=x&e<=y)return a[n];
 		int m=(s+e)/2,a=p(n*2,s,m),b=p(n*2+1,m+1,e);
@@ -27,11 +26,9 @@ public class P1306 {
 		for(;l<=n;l*=2);
 		a=new int[l*2];
 		for(;i<n;j=i+++l,a[j]=Integer.parseInt(t.nextToken()),o());
-//		System.out.println(Arrays.toString(a));
 		for(i=m;i<n-m;i++) {
 			x=i-m+1;
 			y=i+m+1;
-//			System.out.println(i+" "+x+" "+y);
 			w.write(p(1,1,l)+" ");
 		}
 		w.flush();
