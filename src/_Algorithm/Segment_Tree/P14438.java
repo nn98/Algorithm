becafc82e9751b5f9a5be2370d8c52ffd44b9b52
nn98 <jkllhgb@gmail.com>
2022-06-20 21:13:44 +0900

    14438

--- src/_Algorithm/Segment_Tree/P14438.java
+++ src/_Algorithm/Segment_Tree/P14438.java
@@ -6,22 +6,21 @@ import java.util.*;
 public class P14438 {
 	static int n,a[],l=1,i,j,k,M=Integer.MAX_VALUE;
 	static void o() {
-		System.out.println("\n"+Arrays.toString(a));
 //		for(;j>1;j/=2,a[j]=(a[j*2]==0?Integer.MAX_VALUE:a[j*2])<=(a[j*2+1]==0?Integer.MAX_VALUE:a[j*2+1])?a[j*2]:a[j*2+1]);
-		for(;j>1;j/=2,a[j]=a[a[j*2]]<a[a[j*2+1]]?a[j*2]:a[j*2+1]);
+		for(;j>1;j/=2,a[j]=Math.min(a[j*2],a[j*2+1]));
 	}
 	static int p(int n,int l,int r) {
 //		System.out.println(n+" "+l+" "+r);
 		if(l>i|j>r) {
 //			System.out.println("re 0");
-			return 0;
+			return M;
 		}
 		if(j<=l&r<=i) {
 //			System.out.println("re "+a[n]);
 			return a[n];
 		}
-		int m=(l+r)/2,x=p(n*2,l,m),y=p(n*2+1,m+1,r);
-		return a[x]>a[y]?y:x;
+		int m=(l+r)/2;
+		return Math.min(p(n*2,l,m),p(n*2+1,m+1,r));
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -29,10 +28,10 @@ public class P14438 {
 		n=Integer.parseInt(r.readLine());
 		for(;l<n;l*=2);
 		a=new int[l*2];
-		a[0]=Integer.MAX_VALUE;
-		Arrays.fill(a,l,l*2,M);
+//		a[0]=Integer.MAX_VALUE;
+		Arrays.fill(a,M);
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),j/=2,a[j]=a[j*2]>a[j*2+1]?j*2+1:j*2,o());
+		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),o());
 //		System.out.println(Arrays.toString(a));
 		for(n=Integer.parseInt(r.readLine());n-->0;) {
 			t=new StringTokenizer(r.readLine());
@@ -40,13 +39,11 @@ public class P14438 {
 			j=Integer.parseInt(t.nextToken());
 			i=Integer.parseInt(t.nextToken());
 			if(k>1) {
-				w.write(p(1,1,l)-l+"\n");
+				w.write(p(1,1,l)+"\n");
 			}
 			else {
 				j+=l-1;
 				a[j]=i;
-				j/=2;
-				a[j]=a[j*2]>a[j*2+1]?j*2+1:j*2;
 				o();
 //				System.out.println(Arrays.toString(a));
 			}
