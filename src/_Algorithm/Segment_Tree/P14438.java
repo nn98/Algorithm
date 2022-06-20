208853eac60a19ca5637830a3322909661fba068
nn98 <jkllhgb@gmail.com>
2022-06-20 21:13:39 +0900

    14428 14438

--- src/_Algorithm/Segment_Tree/P14438.java
+++ src/_Algorithm/Segment_Tree/P14438.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class P14438 {
 	static int n,a[],l=1,i,j,k,M=Integer.MAX_VALUE;
 	static void o() {
+		System.out.println("\n"+Arrays.toString(a));
 //		for(;j>1;j/=2,a[j]=(a[j*2]==0?Integer.MAX_VALUE:a[j*2])<=(a[j*2+1]==0?Integer.MAX_VALUE:a[j*2+1])?a[j*2]:a[j*2+1]);
 		for(;j>1;j/=2,a[j]=a[a[j*2]]<a[a[j*2+1]]?a[j*2]:a[j*2+1]);
 	}
@@ -20,7 +21,7 @@ public class P14438 {
 			return a[n];
 		}
 		int m=(l+r)/2,x=p(n*2,l,m),y=p(n*2+1,m+1,r);
-		return a[x]<a[y]?x:y;
+		return a[x]>a[y]?y:x;
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -28,10 +29,10 @@ public class P14438 {
 		n=Integer.parseInt(r.readLine());
 		for(;l<n;l*=2);
 		a=new int[l*2];
-//		a[0]=Integer.MAX_VALUE;
-		Arrays.fill(a,M);
+		a[0]=Integer.MAX_VALUE;
+		Arrays.fill(a,l,l*2,M);
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),o());
+		for(;i<n;j=l+i++,a[j]=Integer.parseInt(t.nextToken()),j/=2,a[j]=a[j*2]>a[j*2+1]?j*2+1:j*2,o());
 //		System.out.println(Arrays.toString(a));
 		for(n=Integer.parseInt(r.readLine());n-->0;) {
 			t=new StringTokenizer(r.readLine());
@@ -39,11 +40,13 @@ public class P14438 {
 			j=Integer.parseInt(t.nextToken());
 			i=Integer.parseInt(t.nextToken());
 			if(k>1) {
-				w.write(p(1,1,l)+"\n");
+				w.write(p(1,1,l)-l+"\n");
 			}
 			else {
 				j+=l-1;
 				a[j]=i;
+				j/=2;
+				a[j]=a[j*2]>a[j*2+1]?j*2+1:j*2;
 				o();
 //				System.out.println(Arrays.toString(a));
 			}
