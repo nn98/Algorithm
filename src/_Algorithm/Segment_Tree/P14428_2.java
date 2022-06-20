6f77653c09c40fdcc0f89b0610b49248cc008f37
nn98 <jkllhgb@gmail.com>
2022-06-20 21:51:49 +0900

    14428

--- src/_Algorithm/Segment_Tree/P14428_2.java
+++ src/_Algorithm/Segment_Tree/P14428_2.java
@@ -4,9 +4,15 @@ import java.io.*;
 import java.util.*;
 
 public class P14428_2 {
-	static int n,m,i,j,l,a[],b[];
+	static int n,m,k,i,j,l,a[],b[];
 	static void o() {
 		for(;j>1;j/=2,a[j]=b[a[j*2]]>b[a[j*2+1]]?a[j*2+1]:a[j*2]);
+	}
+	static int p(int n,int x,int y) {
+		if(y<j|x>i)return 0;
+		if(j<=x&y<=i)return a[n];
+		int m=(x+y)/2,l=p(n*2,x,m),r=p(n*2+1,m+1,y);
+		return b[l]>b[r]?r:l;
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +27,19 @@ public class P14428_2 {
 		for(;i<n;j=i+++l,b[i]=Integer.parseInt(t.nextToken()),a[j]=i,o());
 		System.out.println(Arrays.toString(a));
 		System.out.println(Arrays.toString(b));
-		m=Integer.parseInt(r.readLine());
-		
+		for(m=Integer.parseInt(r.readLine());m-->0;) {
+			t=new StringTokenizer(r.readLine());
+			k=Integer.parseInt(t.nextToken());
+			j=Integer.parseInt(t.nextToken());
+			i=Integer.parseInt(t.nextToken());
+			if(k<2) {
+				b[j]=i;
+				j+=l-1;
+				o();
+				System.out.println(Arrays.toString(a));
+			}else w.write(p(1,1,l)+"\n");
+		}
+		w.flush();
 	}
 
 }
