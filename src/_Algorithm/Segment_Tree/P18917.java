f38002662a809394daca124251a202cd6693c167
nn98 <jkllhgb@gmail.com>
2022-06-22 23:00:56 +0900

    18917

--- src/_Algorithm/Segment_Tree/P18917.java
+++ src/_Algorithm/Segment_Tree/P18917.java
@@ -5,10 +5,10 @@ import java.io.*;
 import java.util.*;
 public class P18917 {
 	public static void main(String[] args)throws Exception{
-		System.out.println(0^3);
-		System.out.println(1^3);
-		System.out.println(0^4);
-		System.out.println(0^3^1^4);
+//		System.out.println(0^3);
+//		System.out.println(1^3);
+//		System.out.println(0^4);
+//		System.out.println(0^3^1^4);
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(r.readLine()),i,j,u=0,o=0,x=1;
@@ -18,13 +18,19 @@ public class P18917 {
 			switch(i) {
 			case 1:
 				j=Integer.parseInt(t.nextToken());
+				u+=j;
+				o^=j;
 				break;
 			case 2:
 				j=Integer.parseInt(t.nextToken());
+				u-=j;
+				o^=j;
 				break;
 			case 3:
+				w.write(u+"\n");
 				break;
 			case 4:
+				w.write(o+"\n");
 			}
 		}
 	}
