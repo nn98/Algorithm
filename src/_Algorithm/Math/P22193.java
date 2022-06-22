1f5aaecfb9c9e60cb729c00d0b0680bf3db14e67
nn98 <jkllhgb@gmail.com>
2022-06-22 22:34:00 +0900

    22193

--- src/_Algorithm/Math/P22193.java
+++ src/_Algorithm/Math/P22193.java
@@ -1,10 +1,16 @@
 package _Algorithm.Math;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P22193 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		s.next();
+		s.next();
+		BigInteger a;
+		System.out.println(new BigInteger(s.next()).multiply(new BigInteger(s.next())));
 	}
 
 }
