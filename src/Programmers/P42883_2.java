8c356827a98510857db3453412a28add241dd93f
nn98 <jkllhgb@gmail.com>
2022-06-23 21:31:15 +0900

    42883

--- src/Programmers/P42883_2.java
+++ src/Programmers/P42883_2.java
@@ -3,14 +3,15 @@ package Programmers;
 public class P42883_2 {
     static char[]a;
     static int i,l;
-    static String s="0",d;
+    static String s,d;
     static void o(int x,int v) {
     	if(v!=0) {
     		a[x]='X';
     		o(x+1,v-1);
     	}else {
     		d=a.toString().replaceAll("X","");
-    		s=s.compareTo(d)>0?d:s;
+    		if(s==null)s=d;
+    		else s=s.compareTo(d)>0?d:s;
     	}
     }
 	public static String solution(String number, int k) {
