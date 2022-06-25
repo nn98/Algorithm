d82317c30096a6069c45657a5389185a9b3d912f
nn98 <jkllhgb@gmail.com>
2022-06-25 23:42:51 +0900

    7576

--- src/_Algorithm/BFS/P7576_2.java
+++ src/_Algorithm/BFS/P7576_2.java
@@ -1,10 +1,23 @@
 package _Algorithm.BFS;
 
+import java.io.*;
+import java.util.*;
 public class P7576_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,a[][],i,j,k,R;
+	static void o(int x,int y) {
+		
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		a=new int[n][m];
+		for(;i<n;i++) {
+			t=new StringTokenizer(r.readLine());
+			for(j=0;j<m;a[i][j++]=Integer.parseInt(t.nextToken()));
+		}
 	}
 
 }
