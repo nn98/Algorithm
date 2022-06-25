837118df5576cba051b9bb60b03a1e87b3822c36
nn98 <jkllhgb@gmail.com>
2022-06-26 00:01:48 +0900

    7576

--- src/_Algorithm/BFS/P7576_2.java
+++ src/_Algorithm/BFS/P7576_2.java
@@ -4,9 +4,19 @@ import java.io.*;
 import java.util.*;
 public class P7576_2 {
 
-	static int n,m,a[][],i,j,k,R,s[]=new int[3];
-	static void o(int x,int y) {
-		
+	static int n,m,a[][],i,j,k,R,s[]=new int[3],h[][],p[]= {1,0,-1,0},q[]= {0,1,0,-1};
+	static void o(int x,int y,int v) {
+		System.out.println(x+" "+y+" "+v);
+		h[x][y]=v++;
+		R=R>h[x][y]?R:h[x][y];
+		int i=0,X,Y;
+		for(;i<4;) {
+			X=x+p[i];
+			Y=y+q[i++];
+			if(X>=0&X<n&Y>=0&Y<m) {
+				if(a[X][Y]>-1)if(h[X][Y]==0)o(X,Y,v);else if(h[X][Y]>v)o(X,Y,v);
+			}
+		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +24,15 @@ public class P7576_2 {
 		m=Integer.parseInt(t.nextToken());
 		n=Integer.parseInt(t.nextToken());
 		a=new int[n][m];
+		h=new int[n][m];
 		for(;i<n;i++) {
 			t=new StringTokenizer(r.readLine());
 			for(j=0;j<m;a[i][j]=Integer.parseInt(t.nextToken()),s[a[i][j++]+1]++);
 		}
+		for(i=0;i<n;i++)for(j=0;j<m;j++)if(a[i][j]==1)o(i,j,1);
 		System.out.println(Arrays.toString(s));
+		for(int[]b:h)System.out.println(Arrays.toString(b));
+		System.out.println(R-1);
 	}
 
 }
