fd123cbed321e3449515dd15cd6e3b6279ba9d52
nn98 <jkllhgb@gmail.com>
2022-06-25 23:58:57 +0900

    7576

--- src/_Algorithm/BFS/P7576_2.java
+++ src/_Algorithm/BFS/P7576_2.java
@@ -4,20 +4,21 @@ import java.io.*;
 import java.util.*;
 public class P7576_2 {
 
-	static int n,m,a[][],i,j,k,R;
+	static int n,m,a[][],i,j,k,R,s[]=new int[3];
 	static void o(int x,int y) {
 		
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
+		n=Integer.parseInt(t.nextToken());
 		a=new int[n][m];
 		for(;i<n;i++) {
 			t=new StringTokenizer(r.readLine());
-			for(j=0;j<m;a[i][j++]=Integer.parseInt(t.nextToken()));
+			for(j=0;j<m;a[i][j]=Integer.parseInt(t.nextToken()),s[a[i][j++]+1]++);
 		}
+		System.out.println(Arrays.toString(s));
 	}
 
 }
