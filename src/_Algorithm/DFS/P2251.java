3d8d56a8379c7e0b5361cc4211279151e8423abe
nn98 <jkllhgb@gmail.com>
2022-06-28 04:16:09 +0900

    2251

--- src/_Algorithm/DFS/P2251.java
+++ src/_Algorithm/DFS/P2251.java
@@ -1,14 +1,9 @@
 package _Algorithm.DFS;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class P2251 {
-
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
-
 }
