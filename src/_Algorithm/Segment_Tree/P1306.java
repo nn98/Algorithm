d290330ac68e607b151691ff3473fa391621dbbc
nn98 <jkllhgb@gmail.com>
2022-07-01 16:50:09 +0900

    1306

--- src/_Algorithm/Segment_Tree/P1306.java
+++ src/_Algorithm/Segment_Tree/P1306.java
@@ -1,40 +1,32 @@
 package _Algorithm.Segment_Tree;
+
 import java.io.*;
 import java.util.*;
+
 public class P1306 {
-	static int j,l=1;
-	static long n,m,i,a[];
+
+	static int n,m,i,j,x,y,l,p,q,a[],u,v;
 	static void o() {
-		for(;j>1;j/=2,a[j]=a[j*2]+a[j*2+1]);
-		System.out.println(Arrays.toString(a));
+		for(;i>1;i/=2,a[i]=a[i*2]>a[i*2+1]?a[i*2]:a[i*2+1]);
 	}
-	static long p(int n,int s,int e) {
-		if(s>j|e<i)return 0;
-		if(s>=i&e<=j)return a[n];
-		int m=(s+e)/2;
-		return p(n*2,s,m)+p(n*2+1,m+1,e);
+	static int p(int n,int s,int e) {
+		if(e<x|y<s)return 0;
+		if(x<=s&e<=y)return a[n];
+		int m=(s+e)/2,a=p(n*2,s,m),b=p(n*2+1,m+1,e);
+		return a>b?a:b;
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer t=new StringTokenizer(r.readLine());
-		n=Long.parseLong(t.nextToken());
-		for(;l<n;l*=2);
-		a=new long[l*2];
-		for(m=Long.parseLong(t.nextToken());m-->0;) {
-			t=new StringTokenizer(r.readLine());
-			switch(t.nextToken()) {
-			case "1":
-				j=Integer.parseInt(t.nextToken())+l-1;
-				a[j]+=Long.parseLong(t.nextToken());
-				o();
-				break;
-			default:
-				i=Long.parseLong(t.nextToken());
-				j=Integer.parseInt(t.nextToken());
-				w.write(p(1,1,l)+"\n");
-			}
-		}
-		w.flush();
+		n=Integer.parseInt(t.nextToken());
+		m=Integer.parseInt(t.nextToken());
+		t=new StringTokenizer(r.readLine());
+		l=1;
+		for(;l<=n;l*=2);
+		a=new int[l*2];
+		for(;i<n;j=i+++l,a[j]=Integer.parseInt(t.nextToken()),o(),System.out.println(i));
+		System.out.println(Arrays.toString(a));
 	}
+
 }
