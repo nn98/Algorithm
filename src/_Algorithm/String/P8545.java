1ab784f9168e0e910f6c563d41987e2c0499ba16
nn98 <jkllhgb@gmail.com>
2022-07-02 17:04:10 +0900

    8545

---
+++ src/_Algorithm/String/P8545.java
@@ -0,0 +1,10 @@
+package _Algorithm.String;
+
+public class P8545 {
+
+	public static void main(String[] args) {
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		for(int i=3;i-->0;System.out.print(a[i]));
+	}
+
+}
