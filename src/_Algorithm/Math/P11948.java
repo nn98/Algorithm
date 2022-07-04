a61525a0994aece0a76b44214ec78226b8d7507c
nn98 <jkllhgb@gmail.com>
2022-07-05 00:09:23 +0900

    11948

--- src/_Algorithm/Math/P11948.java
+++ src/_Algorithm/Math/P11948.java
@@ -1,10 +1,21 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P11948 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int m=100,u=0,n,i=0;
+		for(;i++<6;) {
+			u+=n=s.nextInt();
+			m=m<n?m:n;
+			if(i==4|i==6) {
+				u-=m;
+				m=100;
+			}
+		}
+		System.out.println(u);
 	}
 
 }
