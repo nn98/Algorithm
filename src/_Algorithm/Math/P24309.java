6e6a128f1d7a1c2ff2abc5f01d0b530c2de89b29
nn98 <jkllhgb@gmail.com>
2022-07-04 23:34:44 +0900

    24309

--- src/_Algorithm/Math/P24309.java
+++ src/_Algorithm/Math/P24309.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class P24309 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		System.out.print(new BigInteger(s.next()).divide(new BigInteger(s.next()).subtract(new BigInteger(s.next()))));
 	}
 
 }
