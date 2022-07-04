6cc1f6845ce4065518ee27b1e5b4e5e2ffb6a628
nn98 <jkllhgb@gmail.com>
2022-07-05 00:05:27 +0900

    3733

--- src/_Algorithm/Math/P3733.java
+++ src/_Algorithm/Math/P3733.java
@@ -1,13 +1,9 @@
 package _Algorithm.Math;
-
 import java.util.Scanner;
-
 public class P3733 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n;
 		for(;s.hasNext();n=s.nextInt()+1,System.out.println(s.nextInt()/n));
 	}
-
 }
