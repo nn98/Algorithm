d91f1a0ce5da89a907bd6f1c901ccc52b32bf135
nn98 <jkllhgb@gmail.com>
2022-07-04 23:34:47 +0900

    24309

--- src/_Algorithm/Math/P24309.java
+++ src/_Algorithm/Math/P24309.java
@@ -7,7 +7,8 @@ public class P24309 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		System.out.print(new BigInteger(s.next()).divide(new BigInteger(s.next()).subtract(new BigInteger(s.next()))));
+		BigInteger a=new BigInteger(s.next());
+		System.out.print(new BigInteger(s.next()).subtract(new BigInteger(s.next())).divide(a));
 	}
 
 }
