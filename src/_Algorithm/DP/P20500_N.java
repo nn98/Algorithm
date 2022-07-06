6ff0854f528eacba24152c5f07a8c0b8e1018c16
nn98 <jkllhgb@gmail.com>
2022-07-07 08:04:11 +0900

    20500

--- src/_Algorithm/DP/P20500_N.java
+++ src/_Algorithm/DP/P20500_N.java
@@ -1,5 +1,17 @@
 package _Algorithm.DP;
-
 public class P20500_N {
-
+	public static void main(String[] args) {
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,a[][]=new int[3][n],m=1000000007;
+		if(n<2) {
+			System.out.println(0);
+			return;
+		}
+		a[0][1]=a[1][1]=1;
+		for(;i<n-1;i++) {
+			a[0][i+1]=(a[1][i]+a[2][i])%m;
+			a[1][i+1]=(a[0][i]+a[2][i])%m;
+			a[2][i+1]=(a[0][i]+a[1][i])%m;
+		}
+		System.out.println(a[0][n-1]);
+	}
 }
