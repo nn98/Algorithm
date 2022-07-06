f8cc40dd108884ca02a6596798c04d772bcc1ca3
nn98 <jkllhgb@gmail.com>
2022-07-07 08:01:13 +0900

    20500

--- src/_Algorithm/DP/P20500_C.java
+++ src/_Algorithm/DP/P20500_C.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class P20500_C {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),i=1,a[]=new int[n+1],m=1000000007;
+		int n=new java.util.Scanner(System.in).nextInt(),i=1;
+		long a[]=new long[n+1],m=1000000007;
 		a[0]=a[1]=1;
 		for(;++i<n-1;a[i]=(a[i-2]*2+a[i-1])%m);
 		System.out.println(Arrays.toString(a));
