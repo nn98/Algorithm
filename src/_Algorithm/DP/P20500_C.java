f92f55ce4e56c8a679aa8fefc1f770d1b459dd65
nn98 <jkllhgb@gmail.com>
2022-07-07 07:55:13 +0900

    20500

--- src/_Algorithm/DP/P20500_C.java
+++ src/_Algorithm/DP/P20500_C.java
@@ -3,8 +3,10 @@ package _Algorithm.DP;
 public class P20500_C {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,a[]=new int[n+2],m=1000000007;
+		a[0]=a[1]=1;
+		for(;i++<n;a[i]=(a[i-2]*2+a[i-1])%m);
+		System.out.print(n<2?0:a[n-1]);
 	}
 
 }
