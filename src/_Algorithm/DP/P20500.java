bfe3e14c02b475e035a085eedc65c7ef9d77fa26
nn98 <jkllhgb@gmail.com>
2022-07-07 07:45:10 +0900

    20500

--- src/_Algorithm/DP/P20500.java
+++ src/_Algorithm/DP/P20500.java
@@ -11,9 +11,9 @@ public class P20500 {
 			return;
 		}
 		a[0][1]=a[1][1]=1;
-		for(int[]b:a)System.out.println(Arrays.toString(b));
+//		for(int[]b:a)System.out.println(Arrays.toString(b));
 		for(;++i<n-1;a[0][i+1]=(a[1][i]+a[2][i])%m,a[1][i+1]=(a[0][i]+a[2][i])%m,a[2][i+1]=(a[0][i]+a[1][i])%m);
-		for(int[]b:a)System.out.println(Arrays.toString(b));
+//		for(int[]b:a)System.out.println(Arrays.toString(b));
 		System.out.println(a[0][n-1]);
 	}
 
