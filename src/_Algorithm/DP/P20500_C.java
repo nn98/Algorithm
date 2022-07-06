85881ddfad6ba2c2c1b44a1300e8e4100f28c16f
nn98 <jkllhgb@gmail.com>
2022-07-07 08:01:11 +0900

    20500

--- src/_Algorithm/DP/P20500_C.java
+++ src/_Algorithm/DP/P20500_C.java
@@ -1,12 +1,15 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
+
 public class P20500_C {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt(),i=1,a[]=new int[n+2],m=1000000007;
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,a[]=new int[n+1],m=1000000007;
 		a[0]=a[1]=1;
-		for(;i++<n;a[i]=(a[i-2]*2+a[i-1])%m);
-		System.out.print(n<2?0:a[n-1]);
+		for(;++i<n-1;a[i]=(a[i-2]*2+a[i-1])%m);
+		System.out.println(Arrays.toString(a));
+		System.out.print(n<2?0:a[n-2]);
 	}
 
 }
