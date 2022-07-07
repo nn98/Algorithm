2e63f14da2fc108a14742d5005c0dcf0e1c517da
nn98 <jkllhgb@gmail.com>
2022-07-07 22:58:30 +0900

    2293

--- src/BaekJoon/P2293.java
+++ src/BaekJoon/P2293.java
@@ -1,4 +1,5 @@
 package BaekJoon;
+import java.util.Arrays;
 //https://extracold.tistory.com/5
 import java.util.Scanner;
 public class P2293 {
@@ -6,9 +7,9 @@ public class P2293 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),v=s.nextInt(),a[]=new int[n],h[]=new int[v+1],i=0,j,r=0;
 		for(;i<n;a[i++]=s.nextInt());
-		while(i-->0)a[i]=s.nextInt();
+//		while(i-->0)a[i]=s.nextInt();
 		h[0]=1;
-		for(i=0;i<n;i++)for(j=1;j<=v;j++)if(j-a[i]>=0)h[j]+=h[j-a[i]];
+		for(i=0;i<n;i++,System.out.println(Arrays.toString(h)))for(j=1;j<=v;j++)if(j-a[i]>=0)h[j]+=h[j-a[i]];
 		System.out.print(h[v]);
 	}
 }
