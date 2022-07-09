1907e5f1390548c6fa0cef36be37bc4946d3a7ed
nn98 <jkllhgb@gmail.com>
2022-07-09 22:30:47 +0900

    C838

--- src/ConTest/C838_1.java
+++ src/ConTest/C838_1.java
@@ -1,10 +1,25 @@
 package ConTest;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class C838_1 {
-
+	static int n,m,a[],i,j;
+	static long r;
+	static void o(int x,int v) {
+		if(++v==m) {
+			r+=n-x;
+			return;
+		}
+		for(int i=x+1;;);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		m=s.nextInt();
+		a=new int[n];
+		Arrays.sort(a);
+		o(0,0);
 	}
 
 }
