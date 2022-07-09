1d910aecb3ed187aefbd8fcf349173407a556c5f
nn98 <jkllhgb@gmail.com>
2022-07-10 01:40:22 +0900

    C838

--- src/ConTest/C838_1.java
+++ src/ConTest/C838_1.java
@@ -6,20 +6,26 @@ import java.util.Scanner;
 public class C838_1 {
 	static int n,m,a[],i,j;
 	static long r;
-	static void o(int x,int v) {
-		if(++v==m) {
-			r+=n-x;
-			return;
-		}
-		for(int i=x+1;;);
-	}
+//	static void o(int x,int v) {
+//		if(++v==m) {
+//			r+=n-x;
+//			return;
+//		}
+//		for(int i=x+1;;);
+//	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		m=s.nextInt();
 		a=new int[n];
+		r=m-1;
 		Arrays.sort(a);
-		o(0,0);
+		for(i=m;i<n;i++) {
+			r+=(m-1)*(m*(i-m+1));
+//			System.out.println(r);
+		}
+		System.out.println(r*2);
+//		o(m,0);
 	}
 
 }
