c5fcaa4ef3fc565792abc8e690ee7f9012254be6
nn98 <jkllhgb@gmail.com>
2022-07-10 05:39:12 +0900

    17388

--- src/_Algorithm/Simple_Implementation/P17388.java
+++ src/_Algorithm/Simple_Implementation/P17388.java
@@ -1,10 +1,16 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P17388 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=0,n=s.nextInt(),m=0,j=0,u=0;
+		for(;++i<3;u+=m)if((m=s.nextInt())<n) {
+			j=i;
+			n=m;
+		}
+		System.out.println(u>99?"OK":(j<1?"Soongsil":(j<2?"Korea":"Hanyang")));
 	}
-
 }
