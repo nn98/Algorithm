e425c5d3c32add430b0ed61ccb3d01a27e0b3ef6
nn98 <jkllhgb@gmail.com>
2022-07-10 22:33:20 +0900

    2832

--- src/_Algorithm/Two_Pointer/P2832.java
+++ src/_Algorithm/Two_Pointer/P2832.java
@@ -1,10 +1,21 @@
 package _Algorithm.Two_Pointer;
 
+import java.io.*;
+import java.util.*;
+
 public class P2832 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,a[],i,j,k,p;
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		StringTokenizer t=new StringTokenizer(r.readLine());
+		a=new int[n];
+		for(;i<n;a[i++]=Integer.parseInt(t.nextToken()));
+		for(;++k<n-1;) {
+			if(a[k-1]<a[k]) {
+				for(i=k-1;i>=j;p=a[i],a[i]=a[j],a[j]=p);
+			}
+		}
 	}
 
 }
