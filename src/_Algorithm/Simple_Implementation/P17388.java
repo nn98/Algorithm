783bc7e33cfc52752d908dd24facc03d20a4aed4
nn98 <jkllhgb@gmail.com>
2022-07-10 22:33:26 +0900

    17388

--- src/_Algorithm/Simple_Implementation/P17388.java
+++ src/_Algorithm/Simple_Implementation/P17388.java
@@ -6,10 +6,11 @@ public class P17388 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int i=0,n=s.nextInt(),m=0,j=0,u=0;
-		for(;++i<3;u+=m)if((m=s.nextInt())<n) {
-			j=i;
-			n=m;
+		int i=0,n=s.nextInt(),m=n,j=0,u=0;
+		for(;i<3;i++) {
+			n=s.nextInt();
+			j=m<n?j:i;
+			m=m<n?m:n;
 		}
 //		System.out.println(u>99?"OK":(j<1?"Soongsil":(j<2?"Korea":"Hanyang")));
 		if(u>99)System.out.println("OK");
