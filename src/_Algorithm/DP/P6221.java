1ab4cd4817868960a395fdc345a34693ef1dc09c
nn98 <jkllhgb@gmail.com>
2022-07-13 16:24:15 +0900

    6221

--- src/_Algorithm/DP/P6221.java
+++ src/_Algorithm/DP/P6221.java
@@ -1,10 +1,24 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P6221 {
-
+	static int n,m,i,j,a[][],h[];
+	static void o(int x) {
+		for(int i=0;i<n;i++) {
+			if(a[i][0]>a[x][0]&a[i][1]>a[x][1])h[x]=h[i];
+		}
+		h[x]++;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n][2];
+		h=new int[n];
+		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=s.nextInt());
+		for(;i-->0;o(i));
+		System.out.println(Arrays.toString(h));
 	}
 
 }
