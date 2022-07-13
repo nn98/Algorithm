908b97cb775fa587cad17c7f4c78536adc303607
nn98 <jkllhgb@gmail.com>
2022-07-13 17:02:57 +0900

    12355

--- src/_Algorithm/DP/P12355.java
+++ src/_Algorithm/DP/P12355.java
@@ -3,16 +3,21 @@ package _Algorithm.DP;
 import java.util.Scanner;
 
 public class P12355 {
-
+	static int n,m,i,j,a[],r;
+	static void o(int x,int v) {
+		r=r>v?r:v;
+		for(int i=0;i<m;i++)if(a[i]>a[x])o(i,v+1);
+	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m,i=0,j,a[],r;
+		n=s.nextInt();
 		for(;i++<n;) {
 			m=s.nextInt();
-			a=new int[m+1];
+			a=new int[m];
 			r=0;
-			for(j=0;j++<m;a[j]=s.nextInt(),r+=a[j]>a[j-1]?0:1);
-			System.out.println("Case #"+i+": "+r);
+			for(j=0;j<m;a[j++]=s.nextInt());
+			for(;j-->0;o(j,1));
+			System.out.println("Case #"+i+": "+(m-r));
 		}
 	}
 
