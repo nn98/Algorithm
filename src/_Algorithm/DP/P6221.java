06d152704cf92f7d1c818165a84ca4c048f4dd6b
nn98 <jkllhgb@gmail.com>
2022-07-13 16:46:42 +0900

    6221

--- src/_Algorithm/DP/P6221.java
+++ src/_Algorithm/DP/P6221.java
@@ -4,14 +4,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P6221 {
-	static int n,m,i,j,a[][],h[];
-	static void o(int x) {
-		System.out.println(x+" "+a[x][0]+" "+a[i][1]);
+	static int n,m,i,r,j,a[][],h[];
+	static int o(int x,int v) {
+//		System.out.println(x+" "+a[x][0]+" "+a[i][1]);
 		for(int i=0;i<n;i++) {
-			System.out.println(String.format("%d %d", a[i][0],a[i][1]));
-			if(a[i][0]>a[x][0]&a[i][1]>a[x][1])h[x]=h[i];
+//			System.out.println(String.format("\t%d %d", a[i][0],a[i][1]));
+			if(a[i][0]>a[x][0]&a[i][1]>a[x][1]) {
+//				System.out.println("In");
+//				h[x]=Math.max(h[x],h[i]);
+				h[x]=Math.max(o(i,v+1),h[x]);
+			}
 		}
 		h[x]++;
+		r=r>h[x]?r:h[x];
+		return h[x];
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
@@ -19,7 +25,7 @@ public class P6221 {
 		a=new int[n][2];
 		h=new int[n];
 		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=s.nextInt());
-		for(;i-->0;o(i));
+		for(;i-->0;o(i,0));
 		System.out.println(Arrays.toString(h));
 	}
 
