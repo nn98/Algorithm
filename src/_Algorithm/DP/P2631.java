a909309a38d5a97dc31fb6e370c8d096979c36b3
nn98 <jkllhgb@gmail.com>
2022-07-13 16:04:00 +0900

    2631

--- src/_Algorithm/DP/P2631.java
+++ src/_Algorithm/DP/P2631.java
@@ -1,10 +1,24 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P2631 {
-
+	static int n,m,i,j,a[],h[];
+	static void o(int x) {
+		for(int i=x;++i<n;) {
+			if(a[i]>a[x])h[x]=Math.max(h[x],h[i]);
+		}
+		h[x]++;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(;i-->0;o(i));
+		System.out.println(Arrays.toString(h));
 	}
 
 }
