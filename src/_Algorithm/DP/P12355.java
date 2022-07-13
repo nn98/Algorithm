4ddb266c601f9a85d1184de469cddedc5c875d43
nn98 <jkllhgb@gmail.com>
2022-07-13 17:20:43 +0900

    12355

--- src/_Algorithm/DP/P12355.java
+++ src/_Algorithm/DP/P12355.java
@@ -1,13 +1,21 @@
 package _Algorithm.DP;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P12355 {
-	static int n,m,i,j,a[],r;
-	static void o(int x,int v) {
-		System.out.println(x+" "+v);
-		r=r>v?r:v;
-		for(int i=x;++i<m;)if(a[i]>a[x])o(i,v+1);
+	static int n,m,i,j,a[],r,h[];
+	static int o(int x,int c) {
+//		System.out.println(x+" "+v);
+		for(int i=0;i<c;i++,System.out.print("\t"));
+		System.out.println(x);
+		for(int i=x;++i<m;)if(a[i]>a[x]) {
+			System.out.println("\tgo: "+i);
+			h[x]=Math.max(h[x],(h[i]>0?h[i]:o(i,c+1)));
+		}
+		h[x]++;
+		System.out.println(Arrays.toString(h));
+		return h[x];
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
@@ -15,11 +23,12 @@ public class P12355 {
 		for(;i++<n;) {
 			m=s.nextInt();
 			a=new int[m];
+			h=new int[m];
 			r=0;
 			for(j=0;j<m;a[j++]=s.nextInt());
-			for(;j-->0;o(j,1));
+			for(;j-->0;o(j,0));
+			System.out.println(Arrays.toString(h));
 			System.out.println("Case #"+i+": "+(m-r));
 		}
 	}
-
 }
