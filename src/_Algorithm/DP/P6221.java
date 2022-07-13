ee7ff5805e5a31d89760692b9897f69f42d5cc6d
nn98 <jkllhgb@gmail.com>
2022-07-13 16:46:46 +0900

    6221

--- src/_Algorithm/DP/P6221.java
+++ src/_Algorithm/DP/P6221.java
@@ -4,19 +4,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class P6221 {
-	static int n,m,i,r,j,a[][],h[];
-	static int o(int x,int v) {
+	static int n,m,i,r,j,a[][],h[],v[];
+	static int o(int x,int c) {
+		v[x]++;
 //		System.out.println(x+" "+a[x][0]+" "+a[i][1]);
+		for(int i=0;i<c;i++,System.out.print("\t"));
+		System.out.println(x+" "+c);
 		for(int i=0;i<n;i++) {
 //			System.out.println(String.format("\t%d %d", a[i][0],a[i][1]));
-			if(a[i][0]>a[x][0]&a[i][1]>a[x][1]) {
+			if(v[i]<1&a[i][0]>a[x][0]&a[i][1]>a[x][1]) {
 //				System.out.println("In");
 //				h[x]=Math.max(h[x],h[i]);
-				h[x]=Math.max(o(i,v+1),h[x]);
+//				h[x]=Math.max(o(i,c+1),h[x]);
+				o(i,c+1);
 			}
 		}
-		h[x]++;
+		h[x]=h[x]>c?h[x]:c;
+		for(int i=0;i<c;i++,System.out.print("\t"));
+//		System.out.println("re: "+c);
+		System.out.println(Arrays.toString(h));
 		r=r>h[x]?r:h[x];
+		v[x]--;
 		return h[x];
 	}
 	public static void main(String[] args) {
@@ -25,8 +33,9 @@ public class P6221 {
 		a=new int[n][2];
 		h=new int[n];
 		for(;i<n;a[i][0]=s.nextInt(),a[i++][1]=s.nextInt());
-		for(;i-->0;o(i,0));
-		System.out.println(Arrays.toString(h));
+		for(;i-->0;v=new int[n],o(i,1));
+//		System.out.println(Arrays.toString(h));
+		System.out.println(r);
 	}
 
 }
