21a7035268134f16b35d9e3f3fd2e0d102a067e5
nn98 <jkllhgb@gmail.com>
2022-07-14 19:19:55 +0900

    16486

--- src/_Algorithm/Math/P16486.java
+++ src/_Algorithm/Math/P16486.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P16486 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt();
+		System.out.println(n*2+(m*2*3.141592));
 	}
 
 }
