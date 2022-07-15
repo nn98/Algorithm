7bb0659e284b8b86184f502d3885066e955d93b7
nn98 <jkllhgb@gmail.com>
2022-07-15 21:05:21 +0900

    20499

--- src/_Algorithm/Math/P20499.java
+++ src/_Algorithm/Math/P20499.java
@@ -3,8 +3,8 @@ package _Algorithm.Math;
 public class P20499 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[]a=new java.util.Scanner(System.in).next().split("/");
+		System.out.println(a[1].equals("0")|(Integer.parseInt(a[0])+Integer.parseInt(a[2])<Integer.parseInt(a[1]))?"hasu":"gosu");
 	}
 
 }
