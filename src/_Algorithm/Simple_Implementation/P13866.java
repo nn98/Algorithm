930838b97219e686c4c326b6fff3ec447fd17baa
nn98 <jkllhgb@gmail.com>
2022-07-17 22:44:45 +0900

    13866

--- src/_Algorithm/Simple_Implementation/P13866.java
+++ src/_Algorithm/Simple_Implementation/P13866.java
@@ -1,10 +1,15 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P13866 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int[]a={s.nextInt(),s.nextInt(),s.nextInt(),s.nextInt()};
+		Arrays.sort(a);
+		System.out.println(Math.abs(a[0]+a[3])-(a[1]+a[2]));
 	}
 
 }
