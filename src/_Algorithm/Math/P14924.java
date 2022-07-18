d5f615cc5f470adb57abdb6b0f6e26aed55700ea
nn98 <jkllhgb@gmail.com>
2022-07-18 23:54:02 +0900

    14924

--- src/_Algorithm/Math/P14924.java
+++ src/_Algorithm/Math/P14924.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P14924 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),l=s.nextInt();
+		System.out.print(l/(n*2)*m);
 	}
 
 }
