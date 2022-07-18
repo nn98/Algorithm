ad8bedc682d769dfcc02079eb5694ed2b7cb3643
nn98 <jkllhgb@gmail.com>
2022-07-18 23:22:09 +0900

    2550

--- src/_Algorithm/LIS/P2550.java
+++ src/_Algorithm/LIS/P2550.java
@@ -1,10 +1,17 @@
 package _Algorithm.LIS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P2550 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,i,j,a[],b[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(r.readLine());
+		a=new int[n];
+		h=new int[n];
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
