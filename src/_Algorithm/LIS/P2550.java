fe1106d495e7539b61e606804886a215bc2f51a9
nn98 <jkllhgb@gmail.com>
2022-07-18 23:22:11 +0900

    2550

--- src/_Algorithm/LIS/P2550.java
+++ src/_Algorithm/LIS/P2550.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P2550 {
-	static int n,m,i,j,a[],b[];
+	static int n,m,i,j,a[],b[],h[];
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		n=Integer.parseInt(r.readLine());
