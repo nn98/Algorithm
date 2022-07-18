01c5e581cd84c3396607abfddfd155da7716d92e
nn98 <jkllhgb@gmail.com>
2022-07-18 23:18:39 +0900

    1818

--- src/_Algorithm/LIS/P1818.java
+++ src/_Algorithm/LIS/P1818.java
@@ -1,10 +1,23 @@
 package _Algorithm.LIS;
+import java.util.Scanner;
 
 public class P1818 {
-
+	static int n,m,i,j,a[],h[];
+	static void o(int x) {
+		for(int i=x;++i<n;) {
+			if(a[i]>a[x])h[x]=Math.max(h[x],h[i]);
+		}
+		h[x]++;
+		m=m>h[x]?m:h[x];
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=new int[n];
+		h=new int[n];
+		for(;i<n;a[i++]=s.nextInt());
+		for(;i-->0;o(i));
+		System.out.println(n-m);
 	}
 
 }
