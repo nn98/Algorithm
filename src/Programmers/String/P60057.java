a1e7cec5856915a73011d0dff8c559528a83a81a
nn98 <jkllhgb@gmail.com>
2022-07-20 19:44:55 +0900

    P-60057

--- src/Programmers/String/P60057.java
+++ src/Programmers/String/P60057.java
@@ -28,7 +28,7 @@ public class P60057 {
     }
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
+		System.out.println(solution(s.next()));
 	}
 
 }
