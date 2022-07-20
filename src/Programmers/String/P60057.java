c0ce61c7258817d30874e060752764646f743acf
nn98 <jkllhgb@gmail.com>
2022-07-20 19:10:11 +0900

    P-60057

--- src/Programmers/String/P60057.java
+++ src/Programmers/String/P60057.java
@@ -1,10 +1,34 @@
 package Programmers.String;
 
+import java.util.Scanner;
+
+//문자열 압축
+//https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java
 public class P60057 {
-
+	static public int solution(String s) {
+        String c="",n;
+        int a=s.length(),i=0,j,b;
+        for(;i++<s.length()/2;){
+            b=1;
+            c=s.substring(0,i);
+            StringBuffer r=new StringBuffer();
+            for(j=i;j<=s.length();j+=i){
+                n=s.substring(j,j+i>s.length()?s.length():i+j);
+                if(c.equals(n))b++;
+                else{
+                    r.append((b<2?"":b)+c);
+                    c=n;
+                    b=1;
+                }
+            }
+            r.append(c);
+            a=Math.min(a,r.length());
+        }
+        return a;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
 	}
 
 }
