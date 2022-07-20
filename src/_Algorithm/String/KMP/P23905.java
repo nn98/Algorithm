7a89782ea0b209947bf8f98d58ef18b5196bb20f
nn98 <jkllhgb@gmail.com>
2022-07-20 14:25:10 +0900

    23905

--- src/_Algorithm/String/KMP/P23905.java
+++ src/_Algorithm/String/KMP/P23905.java
@@ -6,7 +6,7 @@ public class P23905 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,x,y,a[],r,j,k,b[];
+		int n=s.nextInt(),i=0,x,y,a[],r,j,k;
 		for(;i++<n;) {
 			x=s.nextInt();
 			y=s.nextInt();
@@ -14,12 +14,12 @@ public class P23905 {
 			r=0;
 			for(j=0;j<x;a[j++]=s.nextInt());
 			for(j=0;j<x;j++)if(a[j]==y) {
-				System.out.println("j: "+j);
+//				System.out.println("j: "+j);
 				for(k=j;++k<x&k-j<y&a[k]==a[k-1]-1;);
-				System.out.println(k);
+//				System.out.println(k);
 				if(k-j==y)r++;
 			}
-			System.out.println("Case #"+i+" "+r);
+			System.out.println("Case #"+i+": "+r);
 		}
 	}
 
