e0c3f7f72efa0e610ceef0dc5661319a5f4bad5c
nn98 <jkllhgb@gmail.com>
2022-07-21 23:43:21 +0900

    1701

--- src/_Algorithm/String/KMP/P1701_2.java
+++ src/_Algorithm/String/KMP/P1701_2.java
@@ -3,8 +3,17 @@ package _Algorithm.String.KMP;
 public class P1701_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[]a=new java.util.Scanner(System.in).next().toCharArray();
+		int i=0,j,n,r=0,k,l=a.length,h[]=new int[l];
+		for(;i<l;i++) {
+			j=i;
+			n=l-i-1;
+			for(k=i+1;k<n;k++) {
+				while(j>0&a[i]!=a[j])j=h[j-1];
+				if(a[i]==a[j])r=r>++j?r:j;
+			}
+		}
+		System.out.println(r);
 	}
 
 }
