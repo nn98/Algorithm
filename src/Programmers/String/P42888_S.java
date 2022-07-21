ad236dc755ab50b7b2284caa0130531525616f7c
nn98 <jkllhgb@gmail.com>
2022-07-21 21:46:34 +0900

    42888

--- src/Programmers/String/P42888_S.java
+++ src/Programmers/String/P42888_S.java
@@ -1,48 +1,31 @@
 package Programmers.String;
-
 import java.util.*;
-
 public class P42888_S {
 	static public String[] solution(String[] record) {
         String b[],e="님이 들어왔습니다.",l="님이 나갔습니다.";
-//        StringBuffer f=new StringBuffer();
         List<String[]>t=new ArrayList();
         HashMap<String,String>m=new HashMap();
         for(String s:record) {
         	b=s.split(" ");
         	switch(b[0]) {
         	case "Enter":
-//        		if(m.get(b[1])!=null)m.remove(b[1]);
         		m.put(b[1],b[2]);
         		String[]a={b[1],"e"};
-//        		f.append(b[1]+e);
         		t.add(a);
         		break;
         	case "Leave":
-//        		f.append(b[1]+l);
         		String[]c= {b[1],"l"};
         		t.add(c);
         		break;
         	default:
-//        		if(m.get(b[1])!=null)m.remove(b[1]);
         		m.put(b[1],b[2]);
         	}
         }
-//        e=f.toString();
-//        System.out.println(e);
-//        for(String[]s:t)System.out.println(Arrays.toString(s));
         b=new String[t.size()];
         int i=0;
         for(String[]s:t) {
-//        	System.out.println(s+" "+m.get(s));
-//        	e=e.replaceAll(s,m.get(s));
-//        	System.out.println(Arrays.toString(s));
-//        	System.out.println(s[1]);
-//        	System.out.println(m.get(s[0]));
-//        	System.out.println(s[1].equals("e"));
         	if(s[1].equals("e"))b[i++]=m.get(s[0])+e;
         	else b[i++]=m.get(s[0])+l;
-//        	System.out.println(Arrays.toString(b));
         }
         return b;
     }
