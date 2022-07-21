e1f515a0285fbd1a2e5b03648afc297dda24d994
nn98 <jkllhgb@gmail.com>
2022-07-21 23:29:02 +0900

    1701

--- src/_Algorithm/String/KMP/P1701.java
+++ src/_Algorithm/String/KMP/P1701.java
@@ -6,13 +6,10 @@ public class P1701 {
 		char[]a=new java.util.Scanner(System.in).next().toCharArray();
 		int i=0,j,n=0,r=1,k=1,l=a.length,h[];
 		for(;i<l;i++) {
-			for(j=i+1;j<l;j++,r=k>r?k:r,n=0) {
-				if(a[i]==a[j]) {
-					for(k=1;i+k<l&j+k<l&n<1;n=a[i+k]==a[j+k++]?0:1,System.out.println(String.format("i:%d %c\tj:%d %c\tk:%d",i,a[i],j,a[j],k)));
-				}
-			}
+			for(j=i+1;j<l;j++,r=k>r?k:r,k=1,n=0)if(a[i]==a[j])for(;i+k<l&j+k<l&n<1;n=a[i+k]==a[j+k++]?0:1,System.out.println(String.format("i:%d %c\tj:%d %c\tk:%d",i+k,a[i+k],j+k,a[j+k],k)));
 		}
 		System.out.println(r-1);
 	}
 
 }
+
