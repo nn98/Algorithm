a3ba7dec86f636bbf611d527f050172aa2a1607d
nn98 <jkllhgb@gmail.com>
2022-07-22 23:39:57 +0900

    1325

--- src/_Algorithm/DFS/P1325_4.java
+++ src/_Algorithm/DFS/P1325_4.java
@@ -6,6 +6,18 @@ import java.util.*;
 public class P1325_4 {
 	static List<Integer>q=new ArrayList(),l[],p=new ArrayList();
 	static int n,m,i,k,j,h[],v[];
+	static int o(int x) {
+		System.out.print("at-"+x);
+		h[x]++;
+		int v=1;
+		for(int y:l[x]) {
+			if(h[y]<1) {
+				v+=o(y);
+			}
+		}
+		System.out.println("\tre-"+v);
+		return v;
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer t=new StringTokenizer(r.readLine());
@@ -19,39 +31,13 @@ public class P1325_4 {
 			t=new StringTokenizer(r.readLine());
 			i=Integer.parseInt(t.nextToken());
 			j=Integer.parseInt(t.nextToken());
-//			l[i].add(j);
 			l[j].add(i);
 		}
 		for(List L:l) {
 			System.out.println(L.toString());
 		}
 		for(i=0;++i<n;) {
-			k=1;
-			h[i]++;
-			q.add(i);
-			for(int x:l[i]) {
-				k++;
-				h[x]++;
-				q.add(x);
-				p.add(x);
-			}
-			System.out.println("q: "+q+"\tk: "+k);
-			while(!q.isEmpty()) {
-				j=q.remove(0);
-				System.out.println("at-"+j);
-				for(int x:l[j]) {
-					System.out.println("\tin-"+x);
-					if(h[x]<1) {
-						h[x]++;
-						k++;
-						q.add(x);
-						p.add(x);
-					}
-				}
-			}
-			System.out.println(Arrays.toString(h)+" "+k);
-			for(int x:p)v[x]=k;
-			System.out.println(Arrays.toString(v));
+			if(h[i]<1)o(i);
 		}
 		System.out.println(Arrays.toString(v));
 	}
