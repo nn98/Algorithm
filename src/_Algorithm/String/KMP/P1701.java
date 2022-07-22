10e454d763cd1e3eb176dd7dd5905f74e41f131e
nn98 <jkllhgb@gmail.com>
2022-07-22 22:58:08 +0900

    1701

--- src/_Algorithm/String/KMP/P1701.java
+++ src/_Algorithm/String/KMP/P1701.java
@@ -2,9 +2,9 @@ package _Algorithm.String.KMP;
 //System.out.println(String.format("i:%d %c\tj:%d %c\tk:%d",i+k,a[i+k],j+k,a[j+k],k)),
 public class P1701 {
 
-	public static void main(String[] args) {
-		char[]a=new java.util.Scanner(System.in).next().toCharArray();
-		int i=0,j,n=0,r=0,k=0,l=a.length,h[];
+	public static void main(String[] args)throws Exception{
+		char[]a=new java.io.BufferedReader(new java.io.InputStreamReader(System.in)).readLine().toCharArray();
+		int i=0,j,n=0,r=0,k=0,l=a.length;
 		for(;i<l;i++) {
 			for(j=i+1;j<l;j++,r=k>r?k:r,n=0) {
 				if(a[i]==a[j]) {
