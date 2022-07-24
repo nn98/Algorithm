732a8685de7a02299403e78d48dc3fe2b10bd033
nn98 <jkllhgb@gmail.com>
2022-07-24 23:12:36 +0900

    15726

--- src/_Algorithm/Math/P15726.java
+++ src/_Algorithm/Math/P15726.java
@@ -7,7 +7,7 @@ public class P15726 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		double a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
-		System.out.println((int)Math.max(a*b/c,a/c*b));
+		System.out.println((int)Math.max(a*b/c,a/b*c));
 	}
 
 }
