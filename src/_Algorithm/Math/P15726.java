31600ca9b8e08053d2450f3c728793803e01c13d
nn98 <jkllhgb@gmail.com>
2022-07-24 23:12:34 +0900

    15726

--- src/_Algorithm/Math/P15726.java
+++ src/_Algorithm/Math/P15726.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P15726 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		double a=s.nextInt(),b=s.nextInt(),c=s.nextInt();
+		System.out.println((int)Math.max(a*b/c,a/c*b));
 	}
 
 }
