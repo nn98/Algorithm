4a81472a80780a6a0c64c6a4b4a31369cdd67809
nn98 <jkllhgb@gmail.com>
2022-07-28 06:30:44 +0900

    16204

--- src/_Algorithm/Math/P16204.java
+++ src/_Algorithm/Math/P16204.java
@@ -6,7 +6,8 @@ public class P16204 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt();
+		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt(),a=n-m,b=n-k;
+		System.out.println(Math.min(a, b)+Math.min(m, k));
 	}
 
 }
