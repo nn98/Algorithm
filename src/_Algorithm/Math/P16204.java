6af30233891393119fc0ac65930f3efb224a901e
nn98 <jkllhgb@gmail.com>
2022-07-28 06:30:46 +0900

    16204

--- src/_Algorithm/Math/P16204.java
+++ src/_Algorithm/Math/P16204.java
@@ -1,13 +1,9 @@
 package _Algorithm.Math;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class P16204 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),m=s.nextInt(),k=s.nextInt(),a=n-m,b=n-k;
 		System.out.println(Math.min(a, b)+Math.min(m, k));
 	}
-
 }
