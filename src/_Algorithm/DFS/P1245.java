17f67f10ba20586f5c2fd6cbea733fbb714b89e2
nn98 <jkllhgb@gmail.com>
2022-07-30 22:49:57 +0900

    1245

--- src/_Algorithm/DFS/P1245.java
+++ src/_Algorithm/DFS/P1245.java
@@ -2,21 +2,24 @@ package _Algorithm.DFS;
 import java.io.*;
 import java.util.*;
 public class P1245 {
-	static int n,m,i,j,a[][],b[][],R,p[]= {1,0,-1,0},q[]= {0,1,0,-1},h[][];
-	static void o(int x,int y) {
+	static int n,m,i,j,a[][],R,p[]= {1,1,1,0,0,-1,-1,-1},q[]= {1,0,-1,1,-1,-1,0,1},h[][];
+	static boolean o(int x,int y,int v) {
 		h[x][y]++;
-		int i=0,X,Y,Z=a[x][y],W=b[x][y];
-		a[x][y]=-1;
-		b[x][y]=-1;
-		for(;i<4;) {
+		int i=0,X,Y,Z=a[x][y];
+		boolean c=true;
+		for(;i<8;) {
 			X=x+p[i];
 			Y=y+q[i++];
 			if(X>=0&X<n&Y>=0&Y<m)
 				if(h[X][Y]<1)
-					if(a[X][Y]==Z)
-						if(b[X][Y]==W)o(X,Y);
-						else R=2;
+					if(a[X][Y]>v) {
+						o(X,Y,a[X][Y]);
+						return false;
+					}else if(a[X][Y]==v) {
+						c=o(X,Y,v);
+					}
 		}
+		return c;
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
@@ -24,31 +27,19 @@ public class P1245 {
 		n=Integer.parseInt(t.nextToken());
 		m=Integer.parseInt(t.nextToken());
 		a=new int[n][m];
-		b=new int[n][m];
 		h=new int[n][m];
 		for(;i<n;i++) {
 			t=new StringTokenizer(r.readLine());
 			for(j=0;j<m;a[i][j++]=Integer.parseInt(t.nextToken()));
 		}
-		for(i=0;i<n;i++) {
-			t=new StringTokenizer(r.readLine());
-			for(j=0;j<m;b[i][j++]=Integer.parseInt(t.nextToken()));
-		}
 		for(i=0;i<n&R<2;i++) {
 			for(j=0;j<m&R<2;j++) {
 				if(h[i][j]<1) {
-					if(a[i][j]!=b[i][j]) {
-//						System.out.println("E: "+i+" "+j);
-						R++;
-					}
-					if(R<2) {
-						o(i,j);
-//						for(int[]c:h)System.out.println(Arrays.toString(c));
-					}
+					System.out.print("o in "+i+","+j);
+					o(i,j,a[i][j]);
 				}
 			}
 		}
-//		System.out.println(R);
 		System.out.println(R<2?"YES":"NO");
 	}
 
