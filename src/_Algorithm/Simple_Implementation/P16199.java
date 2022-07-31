b3f5d92b571583fe7d9a180096e2ab9bfe4c35ca
nn98 <jkllhgb@gmail.com>
2022-07-31 22:57:13 +0900

    16199

--- src/_Algorithm/Simple_Implementation/P16199.java
+++ src/_Algorithm/Simple_Implementation/P16199.java
@@ -1,14 +1,10 @@
 package _Algorithm.Simple_Implementation;
-
 import java.util.Scanner;
-
 public class P16199 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt();
 //		System.out.print((e>=b&f>=c?d-a:d-a-1)+"\n"+(d-a+1)+"\n"+(d-a));
 		System.out.print((b<e?d-a:(b==e?(c<=f?d-a:d-a-1):d-a-1))+"\n"+(d-a+1)+"\n"+(d-a));
 	}
-
 }
