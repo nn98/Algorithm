94c3f0ec3d634ed90286a483fa48b6c778621a28
nn98 <jkllhgb@gmail.com>
2022-07-31 22:56:59 +0900

    16199

--- src/_Algorithm/Simple_Implementation/P16199.java
+++ src/_Algorithm/Simple_Implementation/P16199.java
@@ -1,10 +1,13 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P16199 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt();
+		System.out.print((e>=b&f>=c?d-a:d-a-1)+"\n"+(d-a+1)+"\n"+(d-a));
 	}
 
 }
