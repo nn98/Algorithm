d58afcd1433e30877cfaf47b9cea667e993f4933
nn98 <jkllhgb@gmail.com>
2022-08-02 23:20:54 +0900

    14623

--- src/_Algorithm/Math/P14623.java
+++ src/_Algorithm/Math/P14623.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class P14623 {
 
 	public static void main(String[] args) {
+		System.out.println('0');
 		Scanner s=new Scanner(System.in);
 		String n=s.next(),m=s.next();
 		int p=n.length(),q=m.length(),i=p;
 		long r,N=0,M=0;
-		for(;i-->0;N+=n.charAt(i)>0?Math.pow(2,p-i):0);
-		for(i=q;i-->0;M+=m.charAt(i)>0?Math.pow(2,q-i):0);
+		for(;i-->0;N+=n.charAt(i)>0?Math.pow(2,p-i-1):0);
+		for(i=q;i-->0;M+=m.charAt(i)>0?Math.pow(2,q-i-1):0);
 		System.out.println(N+" "+M);
 	}
 
