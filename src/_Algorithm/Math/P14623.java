b1134a8f7c663ef0699eeb154c7ed4180c86f8a6
nn98 <jkllhgb@gmail.com>
2022-08-02 23:17:58 +0900

    14623

--- src/_Algorithm/Math/P14623.java
+++ src/_Algorithm/Math/P14623.java
@@ -1,10 +1,17 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P14623 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		String n=s.next(),m=s.next();
+		int p=n.length(),q=m.length(),i=p;
+		long r,N=0,M=0;
+		for(;i-->0;N+=n.charAt(i)>0?Math.pow(2,p-i):0);
+		for(i=q;i-->0;M+=m.charAt(i)>0?Math.pow(2,q-i):0);
+		System.out.println(N*M);
 	}
 
 }
