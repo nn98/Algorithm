a3874f12d4815dd652b15c21cbe02e66b5b3f1d7
nn98 <jkllhgb@gmail.com>
2022-08-04 07:28:46 +0900

    11282

--- src/_Algorithm/Simple_Implementation/P11282.java
+++ src/_Algorithm/Simple_Implementation/P11282.java
@@ -3,8 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P11282 {
 
 	public static void main(String[] args) {
-		char c='\uAC00';
-		System.out.println((char)('\uAC00'+new java.util.Scanner(System.in).nextInt()));
+		System.out.println((char)(44031+new java.util.Scanner(System.in).nextInt()));
 	}
 
 }
