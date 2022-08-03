8bf039a97beb859c2cf361c1a0701e9814d6bc7b
nn98 <jkllhgb@gmail.com>
2022-08-03 09:36:29 +0900

    14623

--- src/_Algorithm/Math/P14623.java
+++ src/_Algorithm/Math/P14623.java
@@ -11,7 +11,9 @@ public class P14623 {
 		long r,N=0,M=0;
 		for(;i-->0;N+=n.charAt(i)>48?Math.pow(2,p-i-1):0);
 		for(i=q;i-->0;M+=m.charAt(i)>48?Math.pow(2,q-i-1):0);
-		System.out.println(N+" "+M);
+		r=N*M;
+		for(i=1;i<r;i*=2);
+		for(;i>0;System.out.print(r/i),r%=i,i/=2);
 	}
 
 }
