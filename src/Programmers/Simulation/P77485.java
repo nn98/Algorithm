0ef6f099ad7c0168665ac9f00d312ddf0ede5950
nn98 <jkllhgb@gmail.com>
2022-08-04 22:05:44 +0900

    77485

--- src/Programmers/Simulation/P77485.java
+++ src/Programmers/Simulation/P77485.java
@@ -14,6 +14,7 @@ public class P77485 {
 			M=t=a[i][y];
 			for(u=y;u>j;u--) {
 				a[i][u]=a[i][u-1];
+				M=Math.min(a[i][u],M);
 			}
 			for(int[]b:a)System.out.println(Arrays.toString(b));
 			System.out.println();
@@ -30,6 +31,7 @@ public class P77485 {
 			for(u=j;u<y;u++) {
 				if(u==y-1) a[x][u]=w;
 				else a[x][u]=a[x][u+1];
+				M=Math.min(a[x][u],M);
 			}
 			for(int[]b:a)System.out.println(Arrays.toString(b));
 			System.out.println();
@@ -37,6 +39,7 @@ public class P77485 {
 			for(u=i;u<x;u++) {
 				if(u==x-1) a[u][j]=t;
 				else a[u][j]=a[u+1][j];
+				M=Math.min(a[u][j],M);
 			}
 			for(int[]b:a)System.out.println(Arrays.toString(b));
 			System.out.println();
