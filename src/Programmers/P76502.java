bef09ce62983352dd1e31eb833bfc2c506e39cea
nn98 <jkllhgb@gmail.com>
2022-08-04 16:30:44 +0900

    76502

--- src/Programmers/P76502.java
+++ src/Programmers/P76502.java
@@ -1,5 +1,79 @@
 package Programmers;
+import java.util.*;
+
 
 public class P76502 {
-
+	static public int solution(String s) {
+        int answer = 0;
+        Stack<String> stack = new Stack<>();
+        String[] arr;
+        boolean flag = false;
+        
+        for(int i=0; i<s.length(); ++i) {
+            arr = s.split("");
+            for(int c=0; c<i; ++c) {
+                String first = arr[0];
+                for(int j=0; j<arr.length-1; ++j) {
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length-1] = first;
+            }
+             System.out.println(Arrays.toString(arr));
+            for(int k=0; k<arr.length; ++k) {
+                switch(arr[k]) {
+                    case "[": stack.push("["); continue;
+                    case "(": stack.push("("); continue;
+                    case "{": stack.push("{"); continue;
+                    case "]": if(stack.empty()) {
+                        flag = true;
+                        break;
+                    }else {
+                        if(stack.contains("[")){
+                            stack.pop();
+                            continue;
+                        } else {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    case ")": if(stack.empty()) {
+                        flag = true;
+                        break;
+                    }else {
+                        if(stack.contains("(")){
+                            stack.pop();
+                            continue;
+                        } else {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    case "}": if(stack.empty()) {
+                        flag = true;
+                        break;
+                    }else {
+                        if(stack.contains("{")){
+                            stack.pop();
+                            continue;
+                        } else {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    default: break;
+                }
+            }
+            
+            if(stack.empty() && flag == false){
+                answer++;
+            } else {
+                flag = false;
+                stack.clear();
+            }
+        }
+        return answer;
+    }
+	public static void main(String[]z) {
+		System.out.println(solution("[({})]"));
+	}
 }
