37441508b6cdc993f5b0c705a5821fb2f866c9a9
nn98 <jkllhgb@gmail.com>
2022-08-06 21:43:59 +0900

    10845

--- src/_Algorithm/Data_Structure/P10845.java
+++ src/_Algorithm/Data_Structure/P10845.java
@@ -39,48 +39,47 @@ public class P10845 {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(r.readLine()),size=0;
 		StringTokenizer st;
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 		for(int i=0;i<n;i++) {
 			st=new StringTokenizer(r.readLine());
 			String t=st.nextToken();
 			switch(t) {
 			case "push":
-				//				System.out.print("push ");
 				push(Integer.parseInt(st.nextToken()));
 				size++;
 				break;
 			case "pop":
 				if(root==null) {
-					System.out.print("-1\n");
+					w.write("-1\n");
 				}
 				else {
-					System.out.print(""+pop()+"\n");
+					w.write(""+pop()+"\n");
 				}
 				if(size>0) size--;
 				break;
 			case "size":
-				System.out.print(gS()+"\n");
+				w.write(gS()+"\n");
 				break;
 			case "empty":
 				if(root==null&&rear==null)
-					System.out.print(""+1+"\n");
+					w.write(""+1+"\n");
 				else 
-					System.out.print(""+0+"\n");
+					w.write(""+0+"\n");
 				break;
 			case "front":
 				if(root==null)
-					System.out.print(""+-1+"\n");
+					w.write(""+-1+"\n");
 				else
-					System.out.print(""+root.v+"\n");
+					w.write(""+root.v+"\n");
 				break;
 			case "back":
 				if(rear==null)
-					System.out.print(""+-1+"\n");
+					w.write(""+-1+"\n");
 				else
-					System.out.print(""+rear.v+"\n");
+					w.write(""+rear.v+"\n");
 				break;
 			}
 		}
-		//		bw.flush();
+		w.flush();
 	}
 }
