f4f4f3a5f72a5431061a3924d6fa7511b4a107a6
nn98 <jkllhgb@gmail.com>
2022-08-07 23:37:49 +0900

    2566

--- src/_Algorithm/Simple_Implementation/P2566.java
+++ src/_Algorithm/Simple_Implementation/P2566.java
@@ -6,7 +6,7 @@ public class P2566 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int max=0,index=0,jndex=0;
+		int max=-1,index=0,jndex=0;
 		for(int i=0;i<9;i++) {
 			for(int j=0;j<9;j++) {
 				int a=s.nextInt();
