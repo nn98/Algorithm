7f60d9ddba75550a598d4dfd790849ffa0942db1
nn98 <jkllhgb@gmail.com>
2022-08-10 00:39:39 +0900

    P-42587

--- src/Programmers/Data_Structure/P42587.java
+++ src/Programmers/Data_Structure/P42587.java
@@ -1,10 +1,16 @@
 package Programmers.Data_Structure;
-
+import java.util.*;
+import java.util.stream.Collectors;
 public class P42587 {
-
+	static public int solution(int[]p, int x) {
+		List<Integer>l=Arrays.stream(p).boxed().collect(Collectors.toList());
+		System.out.println(l);
+        int answer = 0;
+        return answer;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		solution(new int[] {1,2,3,4},5);
 	}
 
 }
