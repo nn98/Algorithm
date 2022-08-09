8fd418629c9605aeb76cd2f34510ab4a699ccc40
nn98 <jkllhgb@gmail.com>
2022-08-10 00:23:55 +0900

    13985

--- src/_Algorithm/Simple_Implementation/P13985.java
+++ src/_Algorithm/Simple_Implementation/P13985.java
@@ -3,8 +3,8 @@ package _Algorithm.Simple_Implementation;
 public class P13985 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[]a=new java.util.Scanner(System.in).nextLine().toCharArray();
+		System.out.print((a[0]+a[4]-'0')==a[8]-'0'?"YES":"NO");
 	}
 
 }
