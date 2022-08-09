4bef2378899e00ace2ceed460b6909150097acf7
nn98 <jkllhgb@gmail.com>
2022-08-10 05:50:07 +0900

    P-42587

--- src/Programmers/Data_Structure/P42587.java
+++ src/Programmers/Data_Structure/P42587.java
@@ -3,14 +3,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class P42587 {
 	static public int solution(int[]p, int x) {
-		List<Integer>l=Arrays.stream(p).boxed().collect(Collectors.toList());
-		System.out.println(l);
-        int answer = 0;
-        return answer;
-    }
+		//		List<Integer>l=Arrays.stream(p).boxed().collect(Collectors.toList());
+		//		System.out.println(l);
+		int r=0,a[]=p.clone(),i=0,j=0,l=p.length;
+		Arrays.sort(a);
+		for(;j<p.length&r<1;i++) {
+			if(p[i%l]>0)
+				if(p[i%l]==a[j]) {
+					p[i%l]=-1;
+					j++;
+					if(i%l==x)r++;
+				}
+		}
+		return j;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		solution(new int[] {1,2,3,4},5);
+		solution(new int[] {2,1,3,2},1);
 	}
 
 }
