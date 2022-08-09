bae212c879d37999387fb33483171087e252668b
nn98 <jkllhgb@gmail.com>
2022-08-10 06:37:37 +0900

    15921

--- src/_Algorithm/Math/P15921.java
+++ src/_Algorithm/Math/P15921.java
@@ -1,13 +1,24 @@
 package _Algorithm.Math;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P15921 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[101],u=0;
-		for(;i<n;u+=a[i]=s.nextInt(),b[a[i++]]++);
+		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[101];
+		double u=0,r=0;
+		if(n>0) {
+			for(;i<n;u+=a[i]=s.nextInt(),b[a[i++]]++);
+			u/=n;
+			System.out.println(Arrays.toString(a));
+			System.out.println(Arrays.toString(b));
+			for(i=0;i<n;r+=a[i]*((double)b[a[i++]]/n),System.out.println(r));
+			System.out.println(u);
+			System.out.println(r);
+		}
+		
 	}
 
 }
