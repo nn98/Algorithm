0b1f3ed8e4418c464629f9f8fdc1b74ea4798dff
nn98 <jkllhgb@gmail.com>
2022-08-10 06:29:19 +0900

    15921

--- src/_Algorithm/Math/P15921.java
+++ src/_Algorithm/Math/P15921.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P15921 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,a[]=new int[n],b[]=new int[101],u=0;
+		for(;i<n;u+=a[i]=s.nextInt(),b[a[i++]]++);
 	}
 
 }
