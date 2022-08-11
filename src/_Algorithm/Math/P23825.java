47a0afbb670f500428005c16fb10863927daaa46
nn98 <jkllhgb@gmail.com>
2022-08-11 23:08:01 +0900

    23825

--- src/_Algorithm/Math/P23825.java
+++ src/_Algorithm/Math/P23825.java
@@ -1,12 +1,8 @@
 package _Algorithm.Math;
-
 import java.util.Scanner;
-
 public class P23825 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		System.out.println(Math.min(s.nextInt()/2,s.nextInt()/2));
 	}
-
 }
