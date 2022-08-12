9469a543a3b0e234ce8dc66c5407ab528ec01611
nn98 <jkllhgb@gmail.com>
2022-08-12 23:19:04 +0900

    25191

--- src/_Algorithm/Math/P25191.java
+++ src/_Algorithm/Math/P25191.java
@@ -1,9 +1,12 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P25191 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		System.out.println(Math.min(s.nextInt(),s.nextInt()/2+s.nextInt()/2));
 	}
 
 }
