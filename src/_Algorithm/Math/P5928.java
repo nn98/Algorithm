6664d46c7c45576790adf84ca85bbfaaab2f14d7
nn98 <jkllhgb@gmail.com>
2022-08-13 09:58:14 +0900

    5928

--- src/_Algorithm/Math/P5928.java
+++ src/_Algorithm/Math/P5928.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class P5928 {
 
 	public static void main(String[] args) {
-		System.out.println(24*60);
 		Scanner s=new Scanner(System.in);
-		int n=1440*s.nextInt();
+		System.out.println(1440*s.nextInt()+60*s.nextInt()+s.nextInt()-16511);
 	}
 }
