e2028058f79467ae01ea718f4976e4d26c1d244d
nn98 <jkllhgb@gmail.com>
2022-08-13 09:55:44 +0900

    5928

--- src/_Algorithm/Math/P5928.java
+++ src/_Algorithm/Math/P5928.java
@@ -1,10 +1,13 @@
 package _Algorithm.Math;
 
+import java.util.Scanner;
+
 public class P5928 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(24*60);
+		Scanner s=new Scanner(System.in);
+		int n=24*60*s.nextInt();
 	}
 
 }
