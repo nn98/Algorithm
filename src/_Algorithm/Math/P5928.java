bee35371a587e68989294843824966be9bea6432
nn98 <jkllhgb@gmail.com>
2022-08-13 09:55:46 +0900

    5928

--- src/_Algorithm/Math/P5928.java
+++ src/_Algorithm/Math/P5928.java
@@ -7,7 +7,6 @@ public class P5928 {
 	public static void main(String[] args) {
 		System.out.println(24*60);
 		Scanner s=new Scanner(System.in);
-		int n=24*60*s.nextInt();
+		int n=1440*s.nextInt();
 	}
-
 }
