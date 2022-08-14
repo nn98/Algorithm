cb911b5c752ebc558458a19a04b476fbf3d68617
nn98 <jkllhgb@gmail.com>
2022-08-15 07:44:39 +0900

    3584

--- src/_Algorithm/DFS/P3584.java
+++ src/_Algorithm/DFS/P3584.java
@@ -1,10 +1,30 @@
 package _Algorithm.DFS;
-
+import java.io.*;
 public class P3584 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n,m,K,i,j,a[][],h[],R;
+	static void o(int x) {
+		if(h[x]>0)R=x;
+		else {
+			h[x]++;
+			for(int y=0;++y<n;)if(a[x][y]>0)o(y);
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		for(K=Integer.parseInt(r.readLine());K-->0;) {
+			n=Integer.parseInt(r.readLine())+1;
+			String[]b;
+			a=new int[n][n];
+			for(;--n>0;) {
+				b=r.readLine().split(" ");
+				a[Integer.parseInt(b[1])][Integer.parseInt(b[0])]++;
+			}
+			b=r.readLine().split(" ");
+			o(Integer.parseInt(b[0]));
+			o(Integer.parseInt(b[1]));
+			System.out.println(R);
+		}
 	}
 
 }
