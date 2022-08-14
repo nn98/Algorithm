c971669aa68a718cd59793d5e4fba7860c30563f
nn98 <jkllhgb@gmail.com>
2022-08-15 00:04:39 +0900

    17863

--- src/_Algorithm/Simple_Implementation/P17863.java
+++ src/_Algorithm/Simple_Implementation/P17863.java
@@ -3,8 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P17863 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(new java.util.Scanner(System.in).next().substring(0,3).equals("555"));
 	}
 
 }
