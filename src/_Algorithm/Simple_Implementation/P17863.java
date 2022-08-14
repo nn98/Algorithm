b889b0545e27184925ddce034462886c07d18153
nn98 <jkllhgb@gmail.com>
2022-08-14 20:19:19 +0900

    17863

--- src/_Algorithm/Simple_Implementation/P17863.java
+++ src/_Algorithm/Simple_Implementation/P17863.java
@@ -3,7 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P17863 {
 
 	public static void main(String[] args) {
-		System.out.println(new java.util.Scanner(System.in).next().substring(0,3).equals("555")?"YES":"NO");
+		System.out.print(new java.util.Scanner(System.in).next().substring(0,3).equals("555")?"YES":"NO");
 	}
 
 }
