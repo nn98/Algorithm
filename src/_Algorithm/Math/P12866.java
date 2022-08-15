81f3b9eefeb3195799e040292d10b0956aee87b7
nn98 <jkllhgb@gmail.com>
2022-08-16 06:35:05 +0900

    12866

--- src/_Algorithm/Math/P12866.java
+++ src/_Algorithm/Math/P12866.java
@@ -1,10 +1,15 @@
 package _Algorithm.Math;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P12866 {
 
 	public static void main(String[] args) {
-		int i,a[]=new int[20],m=1000000007;
-		for(char c:new java.util.Scanner(System.in).next().toCharArray())a[c-'A']++;
+		Scanner s=new Scanner(System.in);
+		int i=s.nextInt(),a[]=new int[20],m=1000000007;
+		for(char c:s.next().toCharArray())a[c-'A']++;
+		System.out.println(Arrays.toString(a));
 		i=a[0]%m;
 		i=i*a[2]%m;
 		i=i*a[6]%m;
