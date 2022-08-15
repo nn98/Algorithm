140b80b9c4cdf763c963bded8f7be1be5f5605c2
nn98 <jkllhgb@gmail.com>
2022-08-16 06:16:00 +0900

    25345

--- src/_Algorithm/Math/P25345.java
+++ src/_Algorithm/Math/P25345.java
@@ -1,10 +1,17 @@
 package _Algorithm.Math;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P25345 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),k=s.nextInt(),a[][]=new int[n][n],i=0,j,p;
+		for(;i<n;i++) {
+			for(j=0;j<=i;j++)if(j==0|j==i)a[i][j]=1;else a[i][j]=a[i-1][j-1]+a[i-1][j];
+		}
+		for(int[]b:a)System.out.println(Arrays.toString(b));
 	}
 
 }
