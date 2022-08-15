88265afc54bc9ed55e430e1fc4fa418ef6d8357c
nn98 <jkllhgb@gmail.com>
2022-08-16 06:29:42 +0900

    12866

--- src/_Algorithm/Math/P12866.java
+++ src/_Algorithm/Math/P12866.java
@@ -3,8 +3,11 @@ package _Algorithm.Math;
 public class P12866 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println((int)'C');
+		System.out.println((int)'G');
+		System.out.println((int)'T');
+		
+		int i=0,a[]=new int[4];
 	}
 
 }
