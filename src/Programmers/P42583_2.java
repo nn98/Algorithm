458a73bf1ec73d469a9cdc811cfb746f8640f4f4
nn98 <jkllhgb@gmail.com>
2022-08-16 12:43:53 +0900

    P-42583

--- src/Programmers/P42583_2.java
+++ src/Programmers/P42583_2.java
@@ -2,8 +2,8 @@ package Programmers;
 
 public class P42583_2 {
 	static public int solution(int b, int w, int[] t) {
-		int a=1,r=0,i=0,j,v=0;
-		for(;i<t.length;) {
+		int a=1,r=1,i=0,j,v=0;
+		for(;i<t.length;i++) {
 			if(v+t[i]<=w) {
 				v+=t[i];
 			}else {
--- src/Programmers/P42583_3.java
+++ src/Programmers/P42583_3.java
@@ -1,5 +1,23 @@
 package Programmers;
 
 public class P42583_3 {
+	static public int solution(int b, int w, int[] t) {
+		int a=1,r=1,i=0,v=0,c[]=new int[1000000000];
+		for(;i<t.length;i++) {
+			if(v+t[i]<=w) {
+				v+=t[i];
+			}else {
+				a++;
+				r++;
+				v=t[i];
+			}
+		}
+		return a+b*r;
+	}
+	public static void main(String[] args) {
+		System.out.println(solution(2,10,new int[] {7,4,5,6}));
+		System.out.println(solution(100,100,new int[] {10}));
+		System.out.println(solution(100,100,new int[] {10,10,10,10,10,10,10,10,10,10}));
+	}
 
 }
