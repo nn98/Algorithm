c8f10c5e4212bb6edc4ae571c016f9939910443f
nn98 <jkllhgb@gmail.com>
2022-08-16 10:38:49 +0900

    P-84512

--- src/Programmers/P84512.java
+++ src/Programmers/P84512.java
@@ -1,12 +1,11 @@
 package Programmers;
 
 public class P84512 {
-	static char[]a= {' ', 'A', 'E', 'I', 'O', 'U'},b;
-	static int[]h=new int[6];
+	static char[]a= {' ', 'A', 'E', 'I', 'O', 'U'},b=new char[5];;
+	static int i,h[]=new int[6];
 	static void o(int x) {
-		if(x==6)System.out.println(b);
-		for(int i=0;i<6;i++) {
-			if(x<1)continue;
+		if(x==5)System.out.println(++i+"\t"+new String(b));
+		else for(int i=x<1?1:0;i<(x>1?(b[x-1]==' '?1:6):6);i++) {
 			b[x]=a[i];
 			o(x+1);
 		}
