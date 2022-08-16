a50f9eac879ca6ce15311eab1c296382a3607db6
nn98 <jkllhgb@gmail.com>
2022-08-16 10:54:31 +0900

    P-42583

--- src/Programmers/P42583_2.java
+++ src/Programmers/P42583_2.java
@@ -1,5 +1,22 @@
 package Programmers;
 
 public class P42583_2 {
+	static public int solution(int b, int w, int[] t) {
+		int answer = 0,r=0,i=0,j,v=0;
+		for(;i<t.length;) {
+			v+=t[i++];
+			if(v>w) {
+				v-=t[--i];
+				r++;
+				v=0;
+			}
+		}
+		return ++r*b;
+	}
+	public static void main(String[] args) {
+		System.out.println(solution(2,10,new int[] {7,4,5,6}));
+		System.out.println(solution(100,100,new int[] {10}));
+		System.out.println(solution(100,100,new int[] {10,10,10,10,10,10,10,10,10,10}));
+	}
 
 }
