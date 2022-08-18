d18bc64c33d23d8750d9f3b6159809464b4f18cb
nn98 <jkllhgb@gmail.com>
2022-08-18 21:03:15 +0900

    24883

--- src/_Algorithm/Simple_Implementation/P24883.java
+++ src/_Algorithm/Simple_Implementation/P24883.java
@@ -2,7 +2,9 @@ package _Algorithm.Simple_Implementation;
 
 public class P24883 {
 
-	public static void main(String[] args) {r
+	public static void main(String[] args) {
+		char c=new java.util.Scanner(System.in).next().charAt(0);
+		System.out.println(c=='n'|c=='N'?"Naver D2":"Naver Whale");
 	}
 
 }
