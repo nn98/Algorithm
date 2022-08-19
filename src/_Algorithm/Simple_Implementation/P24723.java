e6958010b7788da8ac05625d6472aa3f3fa91667
nn98 <jkllhgb@gmail.com>
2022-08-19 22:11:06 +0900

    24723

--- src/_Algorithm/Simple_Implementation/P24723.java
+++ src/_Algorithm/Simple_Implementation/P24723.java
@@ -1,9 +1,6 @@
 package _Algorithm.Simple_Implementation;
-
 public class P24723 {
-
 	public static void main(String[] args) {
 		System.out.print((int)Math.pow(2,new java.util.Scanner(System.in).nextInt()));
 	}
-
 }
