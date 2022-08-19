602a06a2d45033dcfd91cbc04639b3b74bc80e01
nn98 <jkllhgb@gmail.com>
2022-08-19 22:03:27 +0900

    14173

--- src/_Algorithm/Simple_Implementation/P14173.java
+++ src/_Algorithm/Simple_Implementation/P14173.java
@@ -3,8 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P14173 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
 	}
 
 }
