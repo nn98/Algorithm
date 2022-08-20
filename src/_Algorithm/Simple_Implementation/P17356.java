b304e444c1d64e417d9a04114a21bdff50dc5ccd
nn98 <jkllhgb@gmail.com>
2022-08-20 22:48:05 +0900

    17356

--- src/_Algorithm/Simple_Implementation/P17356.java
+++ src/_Algorithm/Simple_Implementation/P17356.java
@@ -6,7 +6,7 @@ public class P17356 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		double a=s.nextInt(),m=1/(1+Math.pow(10,(s.nextInt()-a)/400));
+		double a=s.nextInt();
 		System.out.print(1/(1+Math.pow(10,(s.nextInt()-a)/400)));
 	}
 
