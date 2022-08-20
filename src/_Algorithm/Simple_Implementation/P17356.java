9abc9342ed71b45a6d517f7dbc7d965a69928f81
nn98 <jkllhgb@gmail.com>
2022-08-20 22:45:53 +0900

    17356

--- src/_Algorithm/Simple_Implementation/P17356.java
+++ src/_Algorithm/Simple_Implementation/P17356.java
@@ -1,10 +1,13 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P17356 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		double a=s.nextInt(),m=(double)1/((double)1+Math.pow(10, (s.nextInt()-a)/400));
+		System.out.println(m);
 	}
 
 }
