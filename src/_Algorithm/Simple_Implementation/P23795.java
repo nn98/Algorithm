d97046696c1f031ade86572a1ea3feff0b1cd9da
nn98 <jkllhgb@gmail.com>
2022-08-21 23:22:48 +0900

    23795

--- src/_Algorithm/Simple_Implementation/P23795.java
+++ src/_Algorithm/Simple_Implementation/P23795.java
@@ -1,10 +1,14 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P23795 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int i=s.nextInt(),u=0;
+		for(;i>0;u+=i,i=s.nextInt());
+		System.out.print(u);
 	}
 
 }
