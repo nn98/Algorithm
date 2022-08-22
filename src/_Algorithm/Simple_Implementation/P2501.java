1979211c232e4edc250367bba3560a9fb5d08c5a
nn98 <jkllhgb@gmail.com>
2022-08-22 23:04:36 +0900

    2501

--- src/_Algorithm/Simple_Implementation/P2501.java
+++ src/_Algorithm/Simple_Implementation/P2501.java
@@ -1,10 +1,14 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P2501 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,r=0;
+		for(;i++<n&r<3;)if(n%i==0)r++;
+		System.out.println(r<3?0:i);
 	}
 
 }
