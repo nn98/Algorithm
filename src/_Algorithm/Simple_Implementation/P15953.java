2d9a3311c2cb5ff5cfa06354e3f0a2a4ad2d7bd8
nn98 <jkllhgb@gmail.com>
2022-08-23 22:55:50 +0900

    15953

--- src/_Algorithm/Simple_Implementation/P15953.java
+++ src/_Algorithm/Simple_Implementation/P15953.java
@@ -1,10 +1,18 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P15953 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,p,j;
+		for(;++i<n;) {
+			j=0;
+			p=s.nextInt();
+			j+=p<2?500:p<4?300:p<7?200:p<11?50:p<16?30:p<22?10:0;
+			System.out.println(j);
+		}
 	}
 
 }
