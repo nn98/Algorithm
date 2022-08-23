d516942238b6c8f0ceeff1033315096d38df25e3
nn98 <jkllhgb@gmail.com>
2022-08-23 22:56:49 +0900

    15953

--- src/_Algorithm/Simple_Implementation/P15953.java
+++ src/_Algorithm/Simple_Implementation/P15953.java
@@ -7,7 +7,7 @@ public class P15953 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,p,j;
-		for(;++i<n;) {
+		for(;i++<n;) {
 			j=0;
 			p=s.nextInt();
 			j+=p<2?500:p<4?300:p<7?200:p<11?50:p<16?30:p<22?10:0;
