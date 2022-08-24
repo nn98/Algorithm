558697647312a67db801ea94cb90d0730519a9db
nn98 <jkllhgb@gmail.com>
2022-08-24 16:57:38 +0900

    P-42626

--- src/Programmers/P42626.java
+++ src/Programmers/P42626.java
@@ -7,13 +7,14 @@ public class P42626 {
         int a = 0,i=0,c=0,l=s.length;
         try {
             for(;c<l;) {
-            	for(;i<l;)if(s[i]<K) {
+            	for(;i<l;i++)if(s[i]<K) {
             		s[i+1]=s[i]+s[i+1]*2;
             		s[i]=-1;
             		a++;
             		Arrays.sort(s);
             	}
             	for(i=0,c=0;i<l;c+=s[i]<0|s[i]>=K?1:0,i++);
+//            	System.out.println(c);
             	i=0;
             }
         }catch(Exception e) {
