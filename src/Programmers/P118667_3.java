565f89a85c5794c6fcbecbd55bbfda7a13219022
nn98 <jkllhgb@gmail.com>
2022-08-24 17:31:51 +0900

    P-118667

--- src/Programmers/P118667_3.java
+++ src/Programmers/P118667_3.java
@@ -3,24 +3,27 @@ package Programmers;
 import java.util.Arrays;
 //날먹 실패
 public class P118667_3 {
-	static int h[][],r=Integer.MAX_VALUE,a[],l;
+	static int h[][],r,a[],l;
 	static void o(int x,int y,int c,long X,long Y) {
 		x%=l*2;
 		y%=l*2;
+		System.out.println(String.format("x:%d y:%d c:%d X:%d Y:%d %b",x,y,c,X,Y,X==Y));
 		h[x][y]++;
 		if(X==Y)r=r<c?r:c;
-		if(h[x+1][y]<0)o(x+1,y,c+1,X-a[x],y+a[x]);
-		if(h[x][y+1]<0)o(x,y+1,c+1,X+a[y],y+a[y]);
+		if(x<l*2-1)if(h[x+1][y]<1)o(x+1,y,c+1,X-a[x],Y+a[x]);
+		if(y<l*2-1)if(h[x][y+1]<1)o(x,y+1,c+1,X+a[y],Y-a[y]);
 	}
 	static public int solution(int[] q, int[] p) {
+		r=Integer.MAX_VALUE;
 		l=q.length;
 		a=new int[l*2];
-        int i=0,x=0,y=l,r=0,c=0;
+		h=new int[l*2][l*2];
+        int i=0,x=0,y=l;
         long A=0,B=0;
         for(;i<l*2;a[i]=i<l?q[i]:p[i%l],A+=i<l?a[i]:0,B+=i<l?0:a[i],i++);
         o(x,y,0,A,B);
         System.out.println(Arrays.toString(a));
-        return r;
+        return r==Integer.MAX_VALUE?-1:r;
     }
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {3,2,7,2},new int[] {4,6,5,1}));
