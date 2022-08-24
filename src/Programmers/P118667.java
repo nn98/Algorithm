73ae14ff5d26910f172f41eda80f4c480fca6ac9
nn98 <jkllhgb@gmail.com>
2022-08-24 16:44:20 +0900

    P-118667

--- src/Programmers/P118667.java
+++ src/Programmers/P118667.java
@@ -10,14 +10,14 @@ public class P118667 {
         int l=q.length,a[]=new int[l*2],i=0,x=0,y=l,r=0;
         long A=0,B=0;
         for(;i<l*2;a[i]=i<l?q[i]:p[i%l],A+=i<l?a[i]:0,B+=i<l?0:a[i],i++);
-        System.out.println(Arrays.toString(a)+" "+x+" "+y+" "+A+" "+B);
-        for(;;) {
+        for(;;r++) {
+            System.out.println(Arrays.toString(a)+" "+x+" "+y+" "+A+" "+B);
         	if(A<B) {
             	A+=a[++x%(l*2)];
             	B-=a[y++%(l*2)];
-            }else if(B>A) {
+            }else if(A>B) {
             	A-=a[x++%(l*2)];
-            	B-=a[++y%(l*2)];
+            	B+=a[++y%(l*2)];
             }else {
                 return r;        	
             }
