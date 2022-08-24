03726ca519c544a2a4d6fc42623b2157cb496144
nn98 <jkllhgb@gmail.com>
2022-08-24 15:30:07 +0900

    P-118668

--- src/Programmers/P118668_2.java
+++ src/Programmers/P118668_2.java
@@ -5,17 +5,27 @@ import java.util.Arrays;
 public class P118668_2 {
 	static int solution(int a,int c,int[][]p) {
 		Arrays.sort(p,(X,Y)->X[0]==Y[0]?X[1]-Y[1]:X[0]-X[0]);
-		int r=0,b[][]=new int[151][151],i=0,j=0,d,x=a,y=c,t=0,g;
-		for(;x<151;x++,t++)for(y=c,g=t;y<151;b[x][y++]=g++);
+		int r=0,b[][],i=0,j=0,x=a,y=c,u,n,g,t=0,q,A=0,C=0;
+		for(;i<p.length;A=Math.max(A,p[i][0]),C=Math.max(C,p[i++][1]));
+		System.out.println(A+" "+C);
+		b=new int[A+1][C+1];
+		for(;x<=A;x++,t++)for(y=c,g=t;y<=C;b[x][y++]=g++);
 		for(int[]z:p) {
 			i=i>z[0]?i:z[0];
 			j=j>z[1]?j:z[1];
 			g=b[z[0]][z[1]];
-			for(x=z[0]+z[2],y=z[1]+z[3];x<151&y<151;b[x][y]=Math.min(b[x][y],g+z[4]),x+=z[2],y+=z[3],g+=z[4]);
+			for(x=z[0]+z[2],y=z[1]+z[3];x<=A&y<=C;) {
+				b[x][y]=Math.min(b[x][y],g+z[4]);
+//				for(u=x,t=b[x][y];u<x+z[2]&u<151;u++,t++)for(n=u==x?y+1:y,q=u==x?t+1:t;n<y+z[3]&n<151;b[u][n++]=q++);
+				for(n=x;n<A;b[++n][y]=b[n-1][y]+1)for(u=y+1;u<=C;System.out.println(n+" "+u+" "+"="+(b[n][u-1]+1)),b[n][u]=b[n][u++-1]+1);
+				x+=z[2];
+				y+=z[3];
+				g+=z[4];
+			}
 		}
-		for(x=a;x<151;System.out.println(),x++)for(y=c;y<151;)System.out.print(b[x][y++]+"\t");
+		for(x=a;x<=A;System.out.println(),x++)for(y=c;y<=C;)System.out.print(b[x][y++]+"\t");
 //		for(int[]z:b)System.out.println(Arrays.toString(z));
-		return r;
+		return b[i][j];
 	}
 	public static void main(String[] args) {
 		System.out.println(solution(10,10,new int[][] {{10,15,2,1,2},{20,20,3,3,4}}));
