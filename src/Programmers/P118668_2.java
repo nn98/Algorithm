11b036b834e83000820e9cedd4ea5db2a195e381
nn98 <jkllhgb@gmail.com>
2022-08-24 15:30:09 +0900

    P-118668

--- src/Programmers/P118668_2.java
+++ src/Programmers/P118668_2.java
@@ -29,7 +29,7 @@ public class P118668_2 {
 	}
 	public static void main(String[] args) {
 		System.out.println(solution(10,10,new int[][] {{10,15,2,1,2},{20,20,3,3,4}}));
-//		System.out.println(solution(0,0,new int[][] {{0,0,2,1,2},{4,5,3,1,2},{4,11,4,0,2},{10,4,0,4,2}}));
+		System.out.println(solution(0,0,new int[][] {{0,0,2,1,2},{4,5,3,1,2},{4,11,4,0,2},{10,4,0,4,2}}));
 	}
 
 }
