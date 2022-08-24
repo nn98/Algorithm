4f932a88d20909422c18b6f0f3a164d7f31a309d
nn98 <jkllhgb@gmail.com>
2022-08-24 16:44:23 +0900

    P-118667

--- src/Programmers/P118667.java
+++ src/Programmers/P118667.java
@@ -7,21 +7,22 @@ public class P118667 {
 //		
 //	}
 	static public int solution(int[] q, int[] p) {
-        int l=q.length,a[]=new int[l*2],i=0,x=0,y=l,r=0;
+        int l=q.length,a[]=new int[l*2],i=0,x=0,y=l,r=0,c=0;
         long A=0,B=0;
         for(;i<l*2;a[i]=i<l?q[i]:p[i%l],A+=i<l?a[i]:0,B+=i<l?0:a[i],i++);
-        for(;;r++) {
+        for(;c<3000;r++,c++) {
             System.out.println(Arrays.toString(a)+" "+x+" "+y+" "+A+" "+B);
         	if(A<B) {
-            	A+=a[++x%(l*2)];
+            	A+=a[y%(l*2)];
             	B-=a[y++%(l*2)];
             }else if(A>B) {
-            	A-=a[x++%(l*2)];
-            	B+=a[++y%(l*2)];
+            	A-=a[x%(l*2)];
+            	B+=a[x++%(l*2)];
             }else {
                 return r;        	
             }
         }
+        return -1;
     }
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {3,2,7,2},new int[] {4,6,5,1}));
