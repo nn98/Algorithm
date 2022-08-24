e162f70b36817d9e5a62236e08046e6cf1a98703
nn98 <jkllhgb@gmail.com>
2022-08-24 17:31:48 +0900

    P-118667

--- src/Programmers/P118667_3.java
+++ src/Programmers/P118667_3.java
@@ -3,16 +3,24 @@ package Programmers;
 import java.util.Arrays;
 //날먹 실패
 public class P118667_3 {
-//	static void o(int x,int y,int c,int X,int Y) {
-//		
-//	}
-	static int h[][];
+	static int h[][],r=Integer.MAX_VALUE,a[],l;
+	static void o(int x,int y,int c,long X,long Y) {
+		x%=l*2;
+		y%=l*2;
+		h[x][y]++;
+		if(X==Y)r=r<c?r:c;
+		if(h[x+1][y]<0)o(x+1,y,c+1,X-a[x],y+a[x]);
+		if(h[x][y+1]<0)o(x,y+1,c+1,X+a[y],y+a[y]);
+	}
 	static public int solution(int[] q, int[] p) {
-        int l=q.length,a[]=new int[l*2],i=0,x=0,y=l,r=0,c=0;
+		l=q.length;
+		a=new int[l*2];
+        int i=0,x=0,y=l,r=0,c=0;
         long A=0,B=0;
         for(;i<l*2;a[i]=i<l?q[i]:p[i%l],A+=i<l?a[i]:0,B+=i<l?0:a[i],i++);
+        o(x,y,0,A,B);
         System.out.println(Arrays.toString(a));
-        return -1;
+        return r;
     }
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {3,2,7,2},new int[] {4,6,5,1}));
