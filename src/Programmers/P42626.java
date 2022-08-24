0b987c21cb3057c89b7b27a7d3d4c4d56e7640b3
nn98 <jkllhgb@gmail.com>
2022-08-24 16:54:40 +0900

    P-42626

--- src/Programmers/P42626.java
+++ src/Programmers/P42626.java
@@ -1,10 +1,28 @@
 package Programmers;
 
+import java.util.Arrays;
+
 public class P42626 {
-
+	static int solution(int[] s, int K) {
+        int a = 0,i=0,c=0,l=s.length;
+        try {
+            for(;c<l;) {
+            	for(;i<l;)if(s[i]<K) {
+            		s[i+1]=s[i]+s[i+1]*2;
+            		s[i]=-1;
+            		a++;
+            		Arrays.sort(s);
+            	}
+            	for(i=0,c=0;i<l;c+=s[i]<0|s[i]>=K?1:0,i++);
+            	i=0;
+            }
+        }catch(Exception e) {
+        	return -1;
+        }
+        return a;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
