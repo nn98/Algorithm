b40b0611c174a4d663527ae2bc68264f413578f3
nn98 <jkllhgb@gmail.com>
2022-08-26 21:07:09 +0900

    3004

--- src/_Algorithm/Simple_Implementation/P3004.java
+++ src/_Algorithm/Simple_Implementation/P3004.java
@@ -3,8 +3,8 @@ package _Algorithm.Simple_Implementation;
 public class P3004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt();
+		System.out.println((int)n%2<1?Math.pow((n/2+1),2):((n/2+1)*(n/2+2)));
 	}
 
 }
