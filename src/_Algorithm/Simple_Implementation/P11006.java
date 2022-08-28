ab47d22f521747f951ede90612a46be4f78c917a
nn98 <jkllhgb@gmail.com>
2022-08-28 23:52:59 +0900

    11006

--- src/_Algorithm/Simple_Implementation/P11006.java
+++ src/_Algorithm/Simple_Implementation/P11006.java
@@ -1,10 +1,13 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P11006 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),a,b;
+		for(;n-->0;a=s.nextInt(),b=s.nextInt(),System.out.println(b*2-a+" "+(b-(b*2-a))));
 	}
 
 }
