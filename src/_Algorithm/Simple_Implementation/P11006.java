82c500629f2b381a0d6554188984056647439225
nn98 <jkllhgb@gmail.com>
2022-08-28 23:53:02 +0900

    11006

--- src/_Algorithm/Simple_Implementation/P11006.java
+++ src/_Algorithm/Simple_Implementation/P11006.java
@@ -1,13 +1,9 @@
 package _Algorithm.Simple_Implementation;
-
 import java.util.Scanner;
-
 public class P11006 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),a,b;
 		for(;n-->0;a=s.nextInt(),b=s.nextInt(),System.out.println(b*2-a+" "+(b-(b*2-a))));
 	}
-
 }
