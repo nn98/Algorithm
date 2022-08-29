d8e944c1b560793de0530496d8cb62c726712c5f
nn98 <jkllhgb@gmail.com>
2022-08-29 22:25:51 +0900

    21738

--- src/_Algorithm/BFS/P21738.java
+++ src/_Algorithm/BFS/P21738.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class P21738 {
 	static int n,m,i,j,p,a[][],b[],x,y,d[];
 	static void o(int x) {
-		System.out.println(x);
+		System.out.println("\t"+x);
 		j++;
 		b[x]++;
 		for(int i=0;i<n;i++)if(b[i]<1)o(i);
@@ -25,7 +25,7 @@ public class P21738 {
 			a[x][y]=a[y][x]=1;
 		}
 		b[p]++;
-		for(;m>0;j=0,o(m),d[m]=j,m--);
+		for(;m>0;j=0,System.out.print(m),o(m),d[m]=j,m--);
 		System.out.println(Arrays.toString(d));
 	}
 
