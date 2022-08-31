3ba6fd7ab6a37c6d7d0e4681f502a0df1338a90e
nn98 <jkllhgb@gmail.com>
2022-08-31 15:15:35 +0900

    14720

---
+++ src/_Algorithm/Simple_Implementation/P14720.java
@@ -0,0 +1,20 @@
+package _Algorithm.Simple_Implementation;
+import java.util.Scanner;
+
+public class P14720 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0,j,r=0;
+		for(;n-->0;) {
+			j=s.nextInt();
+			if(j==i) {
+				i++;
+				i%=3;
+				r++;
+			}
+		}
+		System.out.print(r);
+	}
+
+}
