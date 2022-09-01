26cfe19c1cda1b8e47476b4a7fd90e1653fcb17c
nn98 <jkllhgb@gmail.com>
2022-09-01 23:43:18 +0900

    5073

--- src/_Algorithm/Simple_Implementation/P5073.java
+++ src/_Algorithm/Simple_Implementation/P5073.java
@@ -10,7 +10,7 @@ public class P5073 {
 		for(;;) {
 			String[]S=s.nextLine().split(" ");
 			Arrays.sort(S);
-			int a=Integer.parseInt(S[0]),b=Integer.parseInt(S[0]),c=Integer.parseInt(S[0]),d=(a==b?1:0)+(b==c?1:0);
+			int a=Integer.parseInt(S[0]),b=Integer.parseInt(S[1]),c=Integer.parseInt(S[2]),d=(a==b?1:0)+(b==c?1:0);
 			if(a+b+c<1)break;
 			System.out.println(a+b>c?(d>1?"Equilateral":(d<1?"Scalene":"Isosceles")):"invaild");
 		}
