8438b9284aef0bf7f353cd1a26064a9869c11026
nn98 <38521355+nn98@users.noreply.github.com>
2022-09-02 22:32:03 +0900

    Create P13118.java

---
+++ src/_Algorithm/Simple_Implementation/P13118.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[]z){
+Scanner s=new Scanner(System.in);
+int a[]=new int[4],i=0,j,r=0;
+for(;i<4;a[i++]=s.nextInt());
+j=s.nextInt();
+for(i=0;i<4;r=j==a[i++]?i:r);
+System.out.print(r);
+}}
