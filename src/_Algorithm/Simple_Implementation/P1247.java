e1d631a2a3c4546d0db074e1bc43dadeba869c97
nn98 <jkllhgb@gmail.com>
2022-09-03 23:35:48 +0900

    1247

---
+++ src/_Algorithm/Simple_Implementation/P1247.java
@@ -0,0 +1,18 @@
+package _Algorithm.Simple_Implementation;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P1247 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=0,i;
+		for(;n++<3;) {
+			BigInteger j=BigInteger.ONE;
+			for(i=s.nextInt();i-->0;j=j.add(s.nextBigInteger()));
+			System.out.println(j.signum());
+		}
+	}
+
+}
