7e0be60a0d5f1a916c368f2a727eb65967408983
nn98 <jkllhgb@gmail.com>
2022-09-04 21:49:32 +0900

    17614

---
+++ src/_Algorithm/Simple_Implementation/P17614.java
@@ -0,0 +1,11 @@
+package _Algorithm.Simple_Implementation;
+
+public class P17614 {
+
+	public static void main(String[] args) {
+		int n=new java.util.Scanner(System.in).nextInt(),r=0;
+		for(;n>2;)for(char c:String.valueOf(n).toCharArray())if(c=='3'|c=='6'|c=='9')r++;
+		System.out.println(r);
+	}
+
+}
