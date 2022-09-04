0e48183b5bdaf7b4d11393ffcaa93c5a66298cdd
nn98 <jkllhgb@gmail.com>
2022-09-04 21:49:40 +0900

    17614

--- src/_Algorithm/Simple_Implementation/P17614.java
+++ src/_Algorithm/Simple_Implementation/P17614.java
@@ -4,7 +4,7 @@ public class P17614 {
 
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),r=0;
-		for(;n>2;)for(char c:String.valueOf(n).toCharArray())if(c=='3'|c=='6'|c=='9')r++;
+		for(;n>2;n--)for(char c:String.valueOf(n).toCharArray())if(c=='3'|c=='6'|c=='9')r++;
 		System.out.println(r);
 	}
 
