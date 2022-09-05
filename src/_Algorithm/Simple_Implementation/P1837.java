fb6701a2e352c75aa923f9fa123ac24b13a95f38
nn98 <jkllhgb@gmail.com>
2022-09-05 22:31:53 +0900

    1837

--- src/_Algorithm/Simple_Implementation/P1837.java
+++ src/_Algorithm/Simple_Implementation/P1837.java
@@ -1,6 +1,7 @@
 package _Algorithm.Simple_Implementation;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1837 {
@@ -8,8 +9,11 @@ public class P1837 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		BigInteger a=new BigInteger(s.next());
-		for(int i=s.nextInt();i>1;i--)if(a.mod(new BigInteger(i+"")).equals(BigInteger.ZERO)) {
-			System.out.println("BAD "+i);
+		int i=s.nextInt(),b[]=new int[i+1],j=2,k;
+		for(;j<=i;j++)if(b[j]<1)for(k=j*2;k<=i;b[k]++,k+=j);
+		System.out.println(Arrays.toString(b));
+		for(j=2;j<=i;j++)if(b[j]<1)if(a.mod(new BigInteger(j+"")).equals(BigInteger.ZERO)) {
+			System.out.println("BAD "+j);
 			return;
 		}
 		System.out.println("GOOD");
