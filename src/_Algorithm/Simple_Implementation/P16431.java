22fbc15cfbe18398ce55ce84c9c8b82c9ca58dbc
nn98 <jkllhgb@gmail.com>
2022-09-06 21:37:10 +0900

    16431

--- src/_Algorithm/Simple_Implementation/P16431.java
+++ src/_Algorithm/Simple_Implementation/P16431.java
@@ -1,10 +1,15 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P16431 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=s.nextInt(),j=s.nextInt(),x=s.nextInt(),y=s.nextInt(),
+				a=Math.min(Math.abs(n-x),Math.abs(m-y)),b=Math.abs(i-x)+Math.abs(j-y);
+		System.out.println(a+" "+b);
+		
 	}
 
 }
