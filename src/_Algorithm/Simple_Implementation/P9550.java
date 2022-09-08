6b83cb0245d6fad248b7a1aed67ae7cb2e5c978b
nn98 <jkllhgb@gmail.com>
2022-09-08 10:41:41 +0900

    9550

---
+++ src/_Algorithm/Simple_Implementation/P9550.java
@@ -0,0 +1,18 @@
+package _Algorithm.Simple_Implementation;
+
+import java.util.Scanner;
+
+public class P9550 {
+    public static void main(String[]z){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),m,i,j,a[],k;
+        for(;n-->0;){
+            m=s.nextInt();
+            k=s.nextInt();
+            j=0;
+            // a=new int[m];
+            for(i=0;i++<m;j+=s.nextInt()/k);
+            System.out.println(j);
+        }
+    }
+}
