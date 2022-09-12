34fdd43c908ddcbcd3b39b7fbcb987b92db16e5e
nn98 <jkllhgb@gmail.com>
2022-09-13 07:22:52 +0900

    2022-1SoftP

--- src/ID_Converter.java
+++ src/ID_Converter.java
@@ -1,4 +1,14 @@
+import java.io.*;
 
 public class ID_Converter {
-
+    public static void main(String[]z)throws Exception{
+        BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+        for(;;){
+            String s[]=r.readLine().split("\t");
+            if(s.length<3)break;
+            w.write(String.format("insert into student values (%s,\"%s\",\"%s\");\n", s[0], s[1], s[2]));
+        }
+        w.flush();
+    }
 }
