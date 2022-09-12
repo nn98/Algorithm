e7f7ba3757689dcfe43313136b56748e10db6d5c
nn98 <jkllhgb@gmail.com>
2022-09-13 07:22:55 +0900

    2022-1SoftP

--- src/ID_Converter.java
+++ src/ID_Converter.java
@@ -4,10 +4,23 @@ public class ID_Converter {
     public static void main(String[]z)throws Exception{
         BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-        for(;;){
-            String s[]=r.readLine().split("\t");
-            if(s.length<3)break;
-            w.write(String.format("insert into student values (%s,\"%s\",\"%s\");\n", s[0], s[1], s[2]));
+        System.out.println("Select option: (1-Student talbe generator 2-Learn talbe generator) ");
+        switch(r.readLine()) {
+        case "1":
+            for(;;){
+                String s[]=r.readLine().split("\t");
+                if(s.length<3)break;
+                w.write(String.format("insert into Student values (%s,\"%s\",\"%s\");\n", s[0], s[1], s[2]));
+            }
+        	break;
+        case "2":
+            for(;;){
+            	System.out.println("Input Lecture ID: ");
+                String lID=r.readLine(),s[]=r.readLine().split("\t");
+                if(s.length<1)break;
+                w.write(String.format("insert into Learn values (%s,\"%s\",\"%s\");\n", s[0]));
+            }
+        	break;
         }
         w.flush();
     }
