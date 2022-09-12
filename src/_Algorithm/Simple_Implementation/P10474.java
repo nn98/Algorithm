79141ded714ecef131713422a6d23f993ce5bb49
nn98 <jkllhgb@gmail.com>
2022-09-12 10:23:22 +0900

    10474

--- src/_Algorithm/Simple_Implementation/P10474.java
+++ src/_Algorithm/Simple_Implementation/P10474.java
@@ -1,10 +1,16 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P10474 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		for(;;) {
+			int a=s.nextInt(),b=s.nextInt();
+			if(a+b<1)break;
+			System.out.println(a/b+" "+a%b+" / "+b);
+		}
 	}
 
 }
