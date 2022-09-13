23814dedea991b9cc0836641b24de45c344f1747
nn98 <jkllhgb@gmail.com>
2022-09-13 23:00:14 +0900

    5217

--- src/_Algorithm/Simple_Implementation/P5217.java
+++ src/_Algorithm/Simple_Implementation/P5217.java
@@ -6,12 +6,16 @@ public class P5217 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i,j;
+		int n=s.nextInt(),i,j,c;
 		for(;n-->0;) {
+			c=0;
 			i=s.nextInt();
 			System.out.print("Pairs for "+i+":");
 			for(j=1;j<=i/2;j++) {
-				if(i-j!=j)System.out.print(" "+j+" "+(i-j));
+				if(i-j!=j) {
+					System.out.print(c>0?",":""+" "+j+" "+(i-j));
+					c++;
+				}
 			}
 			System.out.println();
 		}
