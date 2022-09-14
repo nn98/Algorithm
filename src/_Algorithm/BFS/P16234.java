156d8cb4cd13c3677b92b5b22fd946b1951cb2e6
nn98 <jkllhgb@gmail.com>
2022-09-14 13:58:21 +0900

    16234

--- src/_Algorithm/BFS/P16234.java
+++ src/_Algorithm/BFS/P16234.java
@@ -1,10 +1,12 @@
 package _Algorithm.BFS;
 
+import java.io.BufferedReader;
+
 public class P16234 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReaer(System.in));
+		StringTokenizer t=new StringTokenizer(r.readLine());
 	}
 
 }
