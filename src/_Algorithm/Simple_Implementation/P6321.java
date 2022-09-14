321137ad3a3acb2e86675b060112a9e9042f2835
nn98 <jkllhgb@gmail.com>
2022-09-14 10:21:12 +0900

    6321

--- src/_Algorithm/Simple_Implementation/P6321.java
+++ src/_Algorithm/Simple_Implementation/P6321.java
@@ -8,7 +8,8 @@ public class P6321 {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0;
 		for(;n-->0;) {
-			System.out.print((i>1?"\n\n":"")+"String #"+ ++i);
+			System.out.println(i>0);
+			System.out.print((i>0)+(i>0?"\n\n":"")+"String #"+ ++i);
 			for(char c:s.next().toCharArray())System.out.print((char)(c>89?'A':c+1));
 			System.out.println();
 		}
