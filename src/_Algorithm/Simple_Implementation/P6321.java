2c841f8bae6d134ae552df0401228653d79288f1
nn98 <jkllhgb@gmail.com>
2022-09-14 10:15:14 +0900

    6321

--- src/_Algorithm/Simple_Implementation/P6321.java
+++ src/_Algorithm/Simple_Implementation/P6321.java
@@ -1,10 +1,16 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P6321 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=0;
+		for(;n-->0;) {
+			System.out.println("String #"+ ++i);
+			for(char c:s.next().toCharArray())System.out.println(c>89?'A':c+1);
+		}
 	}
 
 }
