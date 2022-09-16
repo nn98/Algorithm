923e5e356f149b4691bf95e3eb0a69709f3ee198
nn98 <jkllhgb@gmail.com>
2022-09-16 23:44:37 +0900

    9517

--- src/_Algorithm/Simple_Implementation/P9517.java
+++ src/_Algorithm/Simple_Implementation/P9517.java
@@ -1,9 +1,7 @@
 package _Algorithm.Simple_Implementation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 public class P9517 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
