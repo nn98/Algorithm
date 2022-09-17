c7acc23b2c82438208490fd0a3064d42f46483ab
nn98 <jkllhgb@gmail.com>
2022-09-17 15:22:04 +0900

    13549

--- src/_Algorithm/BFS/P13549.java
+++ src/_Algorithm/BFS/P13549.java
@@ -1,10 +1,10 @@
 package _Algorithm.BFS;
 
 public class P13549 {
-
+	static String[]a=new java.util.Scanner(System.in).nextLine().split(" ");
+	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[0]),i,j,r;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
