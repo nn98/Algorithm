e26bf4ba0130fbb60e4ab19b97092049ebb55ba7
nn98 <jkllhgb@gmail.com>
2022-09-17 18:06:05 +0900

    9094

--- src/_Algorithm/Simple_Implementation/P9094.java
+++ src/_Algorithm/Simple_Implementation/P9094.java
@@ -1,10 +1,19 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P9094 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		double n=s.nextInt(),i,a,b,j,r;
+		for(;n-->0;) {
+			r=0;
+			a=s.nextInt();
+			b=s.nextInt();
+			for(i=0;++i<a-1;)for(j=i;++j<n;)if((i*i+j*j+b)/(a*b)%1==0)r++;
+			System.out.println(r);
+		}
 	}
 
 }
