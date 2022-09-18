02e819c83f728283b976a95d49d355a8e3be5beb
nn98 <jkllhgb@gmail.com>
2022-09-18 10:19:53 +0900

    13549

--- src/_Algorithm/BFS/P13549.java
+++ src/_Algorithm/BFS/P13549.java
@@ -5,9 +5,12 @@ public class P13549 {
 	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),l=100001,b[]=new int[l];
 	static void o(int x,int v) {
 		b[x]=v;
-		if(x*2<l&b[x*2]<v)o(x*2,v);
-		if(x<l+1&b[x+1]<v+1)o(x+1,v+1);
-		if(x>0&b[x-1]<v+1)o(x-1,v+1);
+		int X=x*2;
+		if(X<l)if(b[x*2]<v)o(x*2,v);
+		X=x+1;
+		if(X<l)if(b[x+1]<v+1)o(x+1,v+1);
+		X=x-1;
+		if(X>0)if(b[x-1]<v+1)o(x-1,v+1);
 	}
 	public static void main(String[] args) {
 		o(n,0);
