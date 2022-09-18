c0a35494da80aa61743cbacbe76255e02129e94e
nn98 <jkllhgb@gmail.com>
2022-09-18 10:21:53 +0900

    13549

--- src/_Algorithm/BFS/P13549.java
+++ src/_Algorithm/BFS/P13549.java
@@ -6,11 +6,12 @@ public class P13549 {
 	static void o(int x,int v) {
 		b[x]=v;
 		int X=x*2;
-		if(X<l)if(b[x*2]<v)o(x*2,v);
+		if(X<l)if(b[x*2]>v)o(x*2,v);
 		X=x+1;
-		if(X<l)if(b[x+1]<v+1)o(x+1,v+1);
+		v++;
+		if(X<l)if(b[x+1]>v)o(x+1,v);
 		X=x-1;
-		if(X>0)if(b[x-1]<v+1)o(x-1,v+1);
+		if(X>0)if(b[x-1]>v)o(x-1,v);
 	}
 	public static void main(String[] args) {
 		o(n,0);
