33f7b2682e828153650cc033654011302676b53b
nn98 <jkllhgb@gmail.com>
2022-09-18 10:19:51 +0900

    13549

--- src/_Algorithm/BFS/P13549.java
+++ src/_Algorithm/BFS/P13549.java
@@ -2,11 +2,11 @@ package _Algorithm.BFS;
 
 public class P13549 {
 	static String[]a=new java.util.Scanner(System.in).nextLine().split(" ");
-	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),i,j,r,b[]=new int[100001];
+	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),l=100001,b[]=new int[l];
 	static void o(int x,int v) {
 		b[x]=v;
-		if(b[x*2]<v)o(x*2,v);
-		if(b[x+1]<v+1)o(x+1,v+1);
+		if(x*2<l&b[x*2]<v)o(x*2,v);
+		if(x<l+1&b[x+1]<v+1)o(x+1,v+1);
 		if(x>0&b[x-1]<v+1)o(x-1,v+1);
 	}
 	public static void main(String[] args) {
