6d33fac992c9e3bc88df1fa83452685096503861
nn98 <jkllhgb@gmail.com>
2022-09-18 10:25:54 +0900

    13549

--- src/_Algorithm/BFS/P13549.java
+++ src/_Algorithm/BFS/P13549.java
@@ -7,13 +7,14 @@ public class P13549 {
 	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),l=100001,b[]=new int[l];
 	static void o(int x,int v) {
 		b[x]=v;
+		System.out.println(Arrays.toString(b));
 		int X=x*2;
 		if(X<l)if(b[x*2]>v)o(x*2,v);
 		X=x+1;
 		v++;
-		if(X<l)if(b[x+1]>v)o(x+1,v);
+		if(X<l)if(b[X]>v)o(X,v);
 		X=x-1;
-		if(X>0)if(b[x-1]>v)o(x-1,v);
+		if(X>0)if(b[X]>v)o(X,v);
 	}
 	public static void main(String[] args) {
 		Arrays.fill(b,Integer.MAX_VALUE);
