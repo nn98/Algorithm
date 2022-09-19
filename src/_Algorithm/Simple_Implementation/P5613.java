5e527ffba3da3e0b6224c03abe73fc45a654b139
nn98 <jkllhgb@gmail.com>
2022-09-20 00:48:04 +0900

    5613

--- src/_Algorithm/Simple_Implementation/P5613.java
+++ src/_Algorithm/Simple_Implementation/P5613.java
@@ -1,10 +1,29 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P5613 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		for(;;)switch(s.next()) {
+		case"+":
+			a+=s.nextInt();
+			break;
+		case"-":
+			a-=s.nextInt();
+			break;
+		case"*":
+			a*=s.nextInt();
+			break;
+		case"/":
+			a/=s.nextInt();
+			break;
+		case"=":
+			System.out.println(a);
+			return;
+		}
 	}
 
 }
