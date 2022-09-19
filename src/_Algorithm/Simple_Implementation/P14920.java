2f5331b27c8e1e55942607403a3264357addb794
nn98 <jkllhgb@gmail.com>
2022-09-20 00:44:21 +0900

    14920

--- src/_Algorithm/Simple_Implementation/P14920.java
+++ src/_Algorithm/Simple_Implementation/P14920.java
@@ -3,7 +3,9 @@ package _Algorithm.Simple_Implementation;
 public class P14920 {
 
 	public static void main(String[] args) {
-		for(int n=new java.util.Scanner(System.in).nextInt(),i=1;n>1;++i,n=n%2<1?n/2:n*3+1,System.out.print(n>1?"":i));
+		int n=new java.util.Scanner(System.in).nextInt(),i=1;
+		for(;n>1;++i,n=n%2<1?n/2:n*3+1);
+		System.out.print(i);
 	}
 
 }
