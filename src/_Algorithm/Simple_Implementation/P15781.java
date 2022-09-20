5e5a6bfb49b9047b1783b4c247134433882d8ac3
nn98 <jkllhgb@gmail.com>
2022-09-20 23:11:07 +0900

    15781

--- src/_Algorithm/Simple_Implementation/P15781.java
+++ src/_Algorithm/Simple_Implementation/P15781.java
@@ -1,10 +1,15 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P15781 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),m=s.nextInt(),i=0,j=0;
+		for(;n-->0;i=Math.max(s.nextInt(),i));
+		for(;m-->0;j=Math.max(s.nextInt(),j));
+		System.out.println(i+j);
 	}
 
 }
