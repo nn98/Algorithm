ec677be593c26c0e999c1f237a496d4701204f5a
nn98 <jkllhgb@gmail.com>
2022-09-23 23:38:51 +0900

    10419

--- src/_Algorithm/Simple_Implementation/P10419.java
+++ src/_Algorithm/Simple_Implementation/P10419.java
@@ -1,10 +1,16 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P10419 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i,j;
+		for(;n-->0;) {
+			i=s.nextInt();
+			System.out.println((int)(Math.sqrt(i))>1?(int)(Math.sqrt(i)):0);
+		}
 	}
 
 }
