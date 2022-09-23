e464a634ef71a0024d20fb965b4da851c65dede5
nn98 <jkllhgb@gmail.com>
2022-09-23 23:42:49 +0900

    10419

--- src/_Algorithm/Simple_Implementation/P10419.java
+++ src/_Algorithm/Simple_Implementation/P10419.java
@@ -1,9 +1,6 @@
 package _Algorithm.Simple_Implementation;
-
 import java.util.Scanner;
-
 public class P10419 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i,j;
@@ -17,5 +14,4 @@ public class P10419 {
 			}
 		}
 	}
-
 }
