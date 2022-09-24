fc82ab3b33792219d84c93cbd391d0222f737abe
nn98 <jkllhgb@gmail.com>
2022-09-24 23:09:16 +0900

    15780

--- src/_Algorithm/Simple_Implementation/P15780.java
+++ src/_Algorithm/Simple_Implementation/P15780.java
@@ -1,10 +1,14 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P15780 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i=s.nextInt(),j=0;
+		for(;i-->0;j+=(s.nextInt()+1)/2);
+		System.out.println(n>j?"NO":"YES");
 	}
 
 }
