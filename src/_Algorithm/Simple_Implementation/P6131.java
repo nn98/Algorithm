8bd35116f3fca467d1df4af20cbd5e5fbe76aede
nn98 <jkllhgb@gmail.com>
2022-09-25 20:29:30 +0900

    6131

--- src/_Algorithm/Simple_Implementation/P6131.java
+++ src/_Algorithm/Simple_Implementation/P6131.java
@@ -3,7 +3,6 @@ public class P6131 {
 	public static void main(String[] args) {
 		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=1,I,J,r=0;
 		for(;i<=500|j<=500;) {
-//			System.out.println(i+" "+j);
 			I=i*i;
 			J=j*j;
 			if(J-I==n)r++;
