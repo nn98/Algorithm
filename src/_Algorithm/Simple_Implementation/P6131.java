7c72ada3cc7d938f014a27a87e397750ef1627d9
nn98 <jkllhgb@gmail.com>
2022-09-25 20:26:09 +0900

    6131

--- src/_Algorithm/Simple_Implementation/P6131.java
+++ src/_Algorithm/Simple_Implementation/P6131.java
@@ -3,8 +3,14 @@ package _Algorithm.Simple_Implementation;
 public class P6131 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=new java.util.Scanner(System.in).nextInt(),i=1,j=1,I,J,r=0;
+		for(;i<=500|j<=500;) {
+			I=i*i;
+			J=j*j;
+			if(J-I==n)r++;
+			else if(J-I<n)j++;
+			else i++;
+		}
 	}
 
 }
