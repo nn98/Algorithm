af83f8b0fe6fb40fc521f564a944be56c67abd26
nn98 <jkllhgb@gmail.com>
2022-09-27 00:19:23 +0900

    2991

--- src/_Algorithm/Simple_Implementation/P2991.java
+++ src/_Algorithm/Simple_Implementation/P2991.java
@@ -8,20 +8,19 @@ public class P2991 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),p=s.nextInt(),
-				m=s.nextInt(),n=s.nextInt(),i=0,j=0,l=1000,x[]=new int[l],y[]=new int[l],k;
+				m=s.nextInt(),n=s.nextInt(),i=0,j=0,l=1000,x[]=new int[l],k;
 		for(;i<l|j<l;) {
 			for(k=0;k<a&k+i<l;x[k+++i]++);
 			i+=k+b;
 			System.out.println(i);
-			for(k=0;k<c&k+j<l;y[k+++j]++);
+			for(k=0;k<c&k+j<l;x[k+++j]++);
 			j+=k+d;
 			System.out.println(j);
 		}
 		System.out.println(Arrays.toString(x));
-		System.out.println(Arrays.toString(y));
-		System.out.println(x[p]);
-		System.out.println(x[m]);
-		System.out.println(x[n]);
+		System.out.println(x[p-1]);
+		System.out.println(x[m-1]);
+		System.out.println(x[n-1]);
 	}
 
 }
