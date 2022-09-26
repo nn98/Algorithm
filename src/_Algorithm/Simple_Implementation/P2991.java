f2d1a38e739a8f56ee88d9e91114a2e1f55f32ee
nn98 <jkllhgb@gmail.com>
2022-09-27 00:13:45 +0900

    2991

--- src/_Algorithm/Simple_Implementation/P2991.java
+++ src/_Algorithm/Simple_Implementation/P2991.java
@@ -1,10 +1,19 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P2991 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),p=s.nextInt(),
+				m=s.nextInt(),n=s.nextInt(),i=0,j=0,l=1000,x[]=new int[l],k;
+		for(;i<l|j<l;) {
+			for(k=i;k<a&k<l;x[k++]++);
+			i+=k-1+b;
+			for(k=j;k<c&k<l;x[k++]++);
+			j+=k-1+d;
+		}
 	}
 
 }
