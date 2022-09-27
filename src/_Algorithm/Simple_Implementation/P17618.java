ee9756315871dfad915ea51adf9fca0fcdca14ea
nn98 <jkllhgb@gmail.com>
2022-09-27 23:31:25 +0900

    17618

--- src/_Algorithm/Simple_Implementation/P17618.java
+++ src/_Algorithm/Simple_Implementation/P17618.java
@@ -3,7 +3,13 @@ package _Algorithm.Simple_Implementation;
 public class P17618 {
 
 	public static void main(String[] args) {
-		int n=new java.util.Scanner(System.in).nextInt();
+		int n=new java.util.Scanner(System.in).nextInt(),i=0,j,k,r=0;
+		for(;i++<n;) {
+			j=0;
+			for(k=1;j/k>0;j+=i/k,k*=10);
+			if(i%j<1)r++;
+		}
+		System.out.println(r);
 	}
 
 }
