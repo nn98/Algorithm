1d498abe59cb8fb93bdd2d01b8a312a293b1f3ba
nn98 <jkllhgb@gmail.com>
2022-10-01 18:20:32 +0900

    11134

--- src/_Algorithm/Simple_Implementation/P11134.java
+++ src/_Algorithm/Simple_Implementation/P11134.java
@@ -1,13 +1,9 @@
 package _Algorithm.Simple_Implementation;
-
 import java.util.Scanner;
-
 public class P11134 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i,j;
 		for(;n-->0;i=s.nextInt(),j=s.nextInt(),System.out.println(i/j+(i%j<1?0:1)));
 	}
-
 }
