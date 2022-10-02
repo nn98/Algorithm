7188b720cd5e0647f21ed796def342e613a2ad43
nn98 <jkllhgb@gmail.com>
2022-10-02 20:39:22 +0900

    1697

--- src/_Algorithm/BFS/P1697_2.java
+++ src/_Algorithm/BFS/P1697_2.java
@@ -8,29 +8,32 @@ public class P1697_2 {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),m=s.nextInt(),i=0,j,l=100001,h[]=new int[l];
+		int n=s.nextInt(),m=s.nextInt(),i=0,j,l=100001,h[]=new int[l],z,k;
 		Queue<Integer>q=new LinkedList();
 		q.add(n);
 		for(;;) {
 			i++;
-			j=q.remove();
-			h[j]++;
-			if(j==m) {
-				System.out.println(i);
-				return;
-			}
-			else {
-				if(j>0&h[j-1]<1) {
-					q.add(j-1);
-					h[j-1]++;
+			z=q.size();
+			for(k=0;k<z;k++) {
+				j=q.remove();
+				h[j]++;
+				if(j==m) {
+					System.out.println(i);
+					return;
 				}
-				if(j+1<l&h[j+1]<1) {
-					q.add(j+1);
-					h[j+1]++;
-				}
-				if(j*2+1<l&h[j*2]<1) {
-					q.add(j*2);
-					h[j*2]++;
+				else {
+					if(j>0&h[j-1]<1) {
+						q.add(j-1);
+						h[j-1]++;
+					}
+					if(j+1<l&h[j+1]<1) {
+						q.add(j+1);
+						h[j+1]++;
+					}
+					if(j*2+1<l&h[j*2]<1) {
+						q.add(j*2);
+						h[j*2]++;
+					}
 				}
 			}
 		}
