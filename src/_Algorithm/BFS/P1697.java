6390efe05955058bf41992fa5b1838b9d11f9607
nn98 <jkllhgb@gmail.com>
2022-10-02 15:21:32 +0900

    1697

--- src/_Algorithm/BFS/P1697.java
+++ src/_Algorithm/BFS/P1697.java
@@ -6,7 +6,7 @@ public class P1697 {
 	static String[]a=new java.util.Scanner(System.in).nextLine().split(" ");
 	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),l=100001,b[]=new int[l],N=Integer.MAX_VALUE;
 	static void o(int x,int v) {
-		System.out.println(x);
+		System.out.println(x+" "+v);
 		if(x==m)N=v;
 		b[x]=v;
 		//		System.out.println(Arrays.toString(b));
@@ -17,7 +17,7 @@ public class P1697 {
 			X=x+1;
 			if(x<m)if(X<l&X<=l)if(b[X]>v|b[X]<0)o(X,v);
 			X=x-1;
-			if(x>m)if(X>0&X<=l)if(b[X]>v|b[X]<0)o(X,v);
+			if(X>0&X<=l)if(b[X]>v|b[X]<0)o(X,v);
 		}
 	}
 	public static void main(String[] args) {
