4f02385cc8d4515c9e58555fe21e9107bb5d3e7a
nn98 <jkllhgb@gmail.com>
2022-10-02 15:17:49 +0900

    1697

--- src/_Algorithm/BFS/P1697.java
+++ src/_Algorithm/BFS/P1697.java
@@ -7,22 +7,22 @@ public class P1697 {
 	static int n=Integer.parseInt(a[0]),m=Integer.parseInt(a[1]),l=100001,b[]=new int[l],N=Integer.MAX_VALUE;
 	static void o(int x,int v) {
 		System.out.println(x);
-		if(x==n)N=v;
+		if(x==m)N=v;
 		b[x]=v;
 		//		System.out.println(Arrays.toString(b));
 		v++;
 		if(v<N) {
-			int X=x/2;
-			if(x>n)if(X<l&X<=l)if(b[x*2]>v|b[X]<0)o(X,v);
+			int X=x*2;
+			if(x<m)if(X<l&X<=l)if(b[x*2]>v|b[X]<0)o(X,v);
 			X=x+1;
-			if(x<n)if(X<l&X<=l)if(b[X]>v|b[X]<0)o(X,v);
+			if(x<m)if(X<l&X<=l)if(b[X]>v|b[X]<0)o(X,v);
 			X=x-1;
-			if(x>n)if(X>0&X<=l)if(b[X]>v|b[X]<0)o(X,v);
+			if(x>m)if(X>0&X<=l)if(b[X]>v|b[X]<0)o(X,v);
 		}
 	}
 	public static void main(String[] args) {
 		Arrays.fill(b,-1);
-		o(m,0);
+		o(n,0);
 		//		System.out.println(Arrays.toString(b));
 		System.out.println(b[m]);
 	}
