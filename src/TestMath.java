ad896bbf92e0d020951191cdbb0b28379b6afa53
nn98 <jkllhgb@gmail.com>
2022-10-04 23:46:55 +0900

    Math

--- src/TestMath.java
+++ src/TestMath.java
@@ -3,7 +3,7 @@ public class TestMath {
 
 	public static void main(String[] args) {
 		double a=100,b=0,c;
-		for(;b<30;c=a+b*b++,System.out.println(Math.sqrt(c)+" "+b7));
+		for(;b<30;c=a+b*b++,System.out.println(Math.sqrt(c)+" "+b));
 		
 
 	}
