25e0540ee85903f51fe3f7c0eca5fcd7db1413fb
nn98 <jkllhgb@gmail.com>
2022-10-06 10:07:27 +0900

    PJ_Deadline

--- src/SWPJ_2022/TimeStamp.java
+++ src/SWPJ_2022/TimeStamp.java
@@ -1,15 +1,20 @@
 package SWPJ_2022;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.util.*;
+import java.io.*;
 
 public class TimeStamp {
 
-	public static void main(String[] args) {
-		Timestamp ts=new Timestamp(System.currentTimeMillis()),ds=new Timestamp(1663203965);  
-        Date d0=new Date(ts.getTime()),d1=new Date(ds.getTime());  
-        System.out.println(d0);
-        System.out.println(d1);
+	public static void main(String[] args)throws Exception{
+		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+		Timestamp ts=new Timestamp(System.currentTimeMillis());
+		for(;;) {
+			Date d=new Date(ts.getTime());
+			System.out.println(d);
+			long l=Long.parseLong(r.readLine());
+			ts=new Timestamp(l);
+		}
 	}
 
 }
