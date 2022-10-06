ef3f77fb49cad2a384c8b331e3a0a3c261626553
nn98 <jkllhgb@gmail.com>
2022-10-06 09:57:59 +0900

    TA

--- src/TA/Average.java
+++ src/TA/Average.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 
 public class Average {
 	public static void main(String[] args) {
-		int sum = 0;
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		// 정수 개수와 n개의 정수값 입력
-		System.out.print("정수 개수 입력 : ");
-		int a = scanner.nextInt();
-		
-		int intArray[] = new int[a];
-		System.out.print(intArray.length + "개의 정수값 입력 : ");
-		for (int i = 0; i < intArray.length; i++) {
-			intArray[i] = scanner.nextInt();
+		Scanner input= new Scanner(System.in);
+		System.out.println("hw3_1:노태웅");
+		System.out.println("정수 개수 입력");
+		int a= input.nextInt();
+		int[] nums = new int[a];
+		float avg=0;
+		float sum=0;
+		int over_avg=0;
+		System.out.println(a+"개의 정수 입력:");
+		for(int i=0; i<nums.length; i++) {
+			nums[i] = input.nextInt();
+			sum= sum+nums[i];	
 		}
-		
-		for (int i=0; i<intArray.length; i++) {
-			sum += intArray[i];
+		avg=sum/(nums.length);
+		System.out.println("평균="+avg);	
+		System.out.print("평균 초과=");
+		for(int j=0; j<nums.length; j++) {
+			if(avg<nums[j]) {
+				over_avg=nums[j];
+				System.out.print(over_avg+" ");
+			}
 		}
-		
-		// 평균, 평균 초과값 출력
-		System.out.print("평균 = " + (double)sum/intArray.length);
-		int x = 0;
-	    for (int i=0; i<intArray.length; i++) {
-	    	if (x > sum/intArray.length)
-	    		System.out.print("평균 초과 = "+ (double)x);
-	    }
 	}
 
 }
