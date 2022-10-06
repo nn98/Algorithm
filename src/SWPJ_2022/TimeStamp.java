034b614669bd0e3f6c364a48deff7d2536903711
nn98 <jkllhgb@gmail.com>
2022-10-06 09:59:41 +0900

    PJ_Deadline

--- src/SWPJ_2022/TimeStamp.java
+++ src/SWPJ_2022/TimeStamp.java
@@ -1,10 +1,15 @@
 package SWPJ_2022;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class TimeStamp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Timestamp ts=new Timestamp(System.currentTimeMillis()),ds=new Timestamp(1663203965);  
+        Date d0=new Date(ts.getTime()),d1=new Date(ds.getTime());  
+        System.out.println(d0);
+        System.out.println(d1);
 	}
 
 }
