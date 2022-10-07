4ba0346506a8bef0914d0a700d5f0248d8e87595
nn98 <jkllhgb@gmail.com>
2022-10-07 23:09:29 +0900

    12756

--- src/_Algorithm/Simple_Implementation/P12756.java
+++ src/_Algorithm/Simple_Implementation/P12756.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class P12756 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int a=s.nextInt(),b=s.nextInt(),x=b/s.nextInt(),y=s.nextInt()/a;
-		System.out.print(x==y?"DRAW":("PLAYER "+(x>y?"A":"B")));
+		int a=s.nextInt(),b=s.nextInt(),x=s.nextInt(),y=s.nextInt();
+		String r;
+		for(;;) {
+			if(b<1) {
+				r="PLAYER B";
+				if(y<1)r="DRAW";
+				System.out.println(r);
+				return;
+			}
+			if(y<1) {
+				r="PLAYER A";
+				if(b<1)r="DRAW";
+				System.out.println(r);
+				return;
+			}
+			b-=x;
+			y-=a;
+		}
 	}
 }
