f1e2bec9a1a1a65a56c3fb28d9cd0c4de8201ed9
nn98 <jkllhgb@gmail.com>
2022-10-07 23:02:39 +0900

    12756

--- src/_Algorithm/Simple_Implementation/P12756.java
+++ src/_Algorithm/Simple_Implementation/P12756.java
@@ -1,13 +1,9 @@
 package _Algorithm.Simple_Implementation;
-
 import java.util.Scanner;
-
 public class P12756 {
-
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt(),b=s.nextInt(),x=b/s.nextInt(),y=s.nextInt()/a;
 		System.out.print(x==y?"DRAW":("PLAYER "+(x>y?"A":"B")));
 	}
-
 }
