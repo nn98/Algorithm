aa112b2896a14ea5f32dfbfed9710b10ee4e485d
nn98 <jkllhgb@gmail.com>
2022-10-08 21:52:41 +0900

    11034

--- src/_Algorithm/Simple_Implementation/P11034.java
+++ src/_Algorithm/Simple_Implementation/P11034.java
@@ -1,10 +1,17 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.*;
+
 public class P11034 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		while(s.hasNext()) {
+			String[]a=s.nextLine().split(" ");
+			Arrays.sort(a);
+			int i=Integer.parseInt(a[1])-Integer.parseInt(a[0]),j=Integer.parseInt(a[2])-Integer.parseInt(a[1]);
+			System.out.println((i<j?j:i)-1);
+		}
 	}
 
 }
