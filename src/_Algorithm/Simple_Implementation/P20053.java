9a2cf61ba1efcfcd0837ffacc63586cb5adacc88
nn98 <jkllhgb@gmail.com>
2022-10-09 23:52:44 +0900

    20053

--- src/_Algorithm/Simple_Implementation/P20053.java
+++ src/_Algorithm/Simple_Implementation/P20053.java
@@ -7,10 +7,13 @@ public class P20053 {
 	public static void main(String[] args)throws Exception{
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
-		int n=Integer.parseInt(r.readLine()),i=0,j;
+		int n=Integer.parseInt(r.readLine()),i=0,j,a[];
 		for(;n-->0;) {
 			j=Integer.parseInt(r.readLine());
-			String[]a=r.readLine().split(" ");
+			a=new int[j];
+//			String[]a=r.readLine().split(" ");
+			StringTokenizer t=new StringTokenizer(r.readLine());
+			for(i=0;i<j;a[i++]=Integer.parseInt(t.nextToken()));
 			Arrays.sort(a);
 			w.write(a[0]+" "+a[a.length-1]+"\n");
 		}
