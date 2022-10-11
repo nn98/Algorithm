0dc6cc0625b4a80ab4c4e05c7cf671ec755ddc14
nn98 <jkllhgb@gmail.com>
2022-10-11 23:36:26 +0900

    2547

--- src/_Algorithm/Simple_Implementation/P2547.java
+++ src/_Algorithm/Simple_Implementation/P2547.java
@@ -1,12 +1,14 @@
 package _Algorithm.Simple_Implementation;
+import java.math.BigInteger;
 import java.util.*;
 class P2547{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt(),i,j,u;
+		int n=s.nextInt(),i,j;
+		BigInteger u;
 		for(;n-->0;) {
-			for(i=s.nextInt(),j=0,u=0;i-->0;u+=s.nextInt(),j++);
-			System.out.println(u%j<1?"YES":"NO");
+			for(i=s.nextInt(),j=0,u=BigInteger.ZERO;i-->0;u=u.add(s.nextBigInteger()),j++);
+			System.out.println(u.remainder(new BigInteger(""+j)).equals("0")?"YES":"NO");
 		}
 	}
 }
