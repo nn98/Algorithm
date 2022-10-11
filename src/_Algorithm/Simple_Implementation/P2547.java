3376c47786359ae77d57d9cebbb399604b4456b2
nn98 <jkllhgb@gmail.com>
2022-10-11 23:22:51 +0900

    2547

--- src/_Algorithm/Simple_Implementation/P2547.java
+++ src/_Algorithm/Simple_Implementation/P2547.java
@@ -4,10 +4,9 @@ class P2547{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt(),i=0,j=0;
-		s.nextLine();
-		s.nextLine();
+		s.next();
 		for(;n>0;) {
-			String a=s.nextLine();
+			String a=s.next();
 			if(a.equals("")){
 				System.out.println(i%j<1?"YES":"NO");
 				j=i=0;
