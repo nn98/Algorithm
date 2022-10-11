7b72cc2db5cee62924fb161bde5b33ff8d9f2378
nn98 <jkllhgb@gmail.com>
2022-10-11 23:16:19 +0900

    2547

--- src/_Algorithm/Simple_Implementation/P2547.java
+++ src/_Algorithm/Simple_Implementation/P2547.java
@@ -1,10 +1,20 @@
 package _Algorithm.Simple_Implementation;
 
+import java.util.Scanner;
+
 public class P2547 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),i,j;
+		s.nextLine();
+		for(;n>0;) {
+			String a=s.nextLine();
+			if(a.equals(""))n--;
+			j=i=0;
+			i+=Integer.parseInt(a);
+			j++;
+		}
 	}
 
 }
