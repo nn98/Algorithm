523b1943e930e6ac9bc9f366308901ab3209834e
nn98 <jkllhgb@gmail.com>
2022-10-13 23:32:27 +0900

    10599

---
+++ src/_Algorithm/Simple_Implementation/P10599.java
@@ -0,0 +1,16 @@
+package _Algorithm.Simple_Implementation;
+
+import java.util.Scanner;
+
+public class P10599 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		for(;;) {
+			int a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt();
+			if(a==0&b==0&c==0&d==0)break;
+			System.out.println(c<d?c:d-a<b?b:a+" "+(c<d?d:c-a<b?a:b));
+		}
+	}
+
+}
