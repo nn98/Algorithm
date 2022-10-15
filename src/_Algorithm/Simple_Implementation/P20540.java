3112280c8c90fc0e60d06a9d5cd58861ef7c9249
nn98 <jkllhgb@gmail.com>
2022-10-15 23:25:13 +0900

    20540

--- src/_Algorithm/Simple_Implementation/P20540.java
+++ src/_Algorithm/Simple_Implementation/P20540.java
@@ -3,8 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P20540 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(char c:new java.util.Scanner(System.in).next().toCharArray())System.out.print(c=='E'?'I':""+(c=='S'?'N':"")+(c=='T'?'F':"")+(c=='J'?'P':""));
 	}
 
 }
