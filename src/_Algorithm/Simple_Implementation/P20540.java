bf94d9ccd2e38d588eab579b7bafd96a6187a84a
nn98 <jkllhgb@gmail.com>
2022-10-15 23:25:15 +0900

    20540

--- src/_Algorithm/Simple_Implementation/P20540.java
+++ src/_Algorithm/Simple_Implementation/P20540.java
@@ -3,7 +3,7 @@ package _Algorithm.Simple_Implementation;
 public class P20540 {
 
 	public static void main(String[] args) {
-		for(char c:new java.util.Scanner(System.in).next().toCharArray())System.out.print(c=='E'?'I':""+(c=='S'?'N':"")+(c=='T'?'F':"")+(c=='J'?'P':""));
+		for(char c:new java.util.Scanner(System.in).next().toCharArray())System.out.print(c=='E'?'I':""+(c=='I'?'E':"")+(c=='N'?'S':"")+(c=='F'?'T':"")+(c=='P'?'J':"")+(c=='S'?'N':"")+(c=='T'?'F':"")+(c=='J'?'P':""));
 	}
 
 }
