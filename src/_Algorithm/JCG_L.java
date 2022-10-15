362edcce1b04ff637b4d3e2537bc61d242e30f3d
nn98 <jkllhgb@gmail.com>
2022-10-15 23:20:50 +0900

    JCG

--- src/_Algorithm/JCG_L.java
+++ src/_Algorithm/JCG_L.java
@@ -1,10 +1,21 @@
 package _Algorithm;
 
+import java.util.Scanner;
+
 public class JCG_L {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+		StringBuffer f=new StringBuffer();
+		String a,b="";
+		for(;;) {
+			a=s.nextLine().trim();
+			if(a.equals("")&b.equals(a))continue;
+			if(a.equals("00"))break;
+			f.append(a+"\n");
+			b=a;
+		}
+		System.out.println(f);
 	}
 
 }
