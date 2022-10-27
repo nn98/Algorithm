a917d4f6d3b8cfd58209be4385581aa202e39f47
nn98 <jkllhgb@gmail.com>
2022-10-28 00:06:04 +0900

    2712

---
+++ src/_Algorithm/Simple_Implementation/P2712.py
@@ -0,0 +1,16 @@
+for i in range(int(input())):
+    a,b=input().split()
+    if b=='kg':
+        a=float(a)*2.2046
+        b='lb'
+    elif b=='lb':
+        a=float(a)*0.4536
+        b='kg'
+    elif b=='l':
+        a=float(a)*0.2642
+        b='g'
+    else:
+        a=float(a)*3.7854
+        b='l'
+    print(round(a,4),b)
+
