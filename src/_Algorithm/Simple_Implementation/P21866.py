41564c79b19215a2d4f0c83a73a805cd6e8336b0
nn98 <jkllhgb@gmail.com>
2022-11-05 06:19:35 +0900

    21866

--- src/_Algorithm/Simple_Implementation/P21866.py
+++ src/_Algorithm/Simple_Implementation/P21866.py
@@ -2,8 +2,8 @@ c,d,v,u,r=0,2,100,0,''
 for i in list(map(int,input().split())):
     c+=1;
     if c>d:
-        d*=2
-        v*=2
+        d+=2
+        v+=100
     if i>v:
         r='hacker'
     u+=i
