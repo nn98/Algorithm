f92c003069f6f138a8d9919171da484c6c9fbe38
nn98 <jkllhgb@gmail.com>
2022-11-05 06:19:30 +0900

    21866

---
+++ src/_Algorithm/Simple_Implementation/P21866.py
@@ -0,0 +1,10 @@
+c,d,v,u,r=0,2,100,0,''
+for i in list(map(int,input().split())):
+    c+=1;
+    if c>d:
+        d*=2
+        v*=2
+    if i>v:
+        r='hacker'
+    u+=i
+print(r if r!='' else 'draw' if u>99 else 'none')
