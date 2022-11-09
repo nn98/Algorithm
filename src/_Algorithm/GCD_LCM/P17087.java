244617f68c01c6ff7e09c007a721d7dcf2c7a9bb
nn98 <jkllhgb@gmail.com>
2022-11-09 17:17:18 +0900

    17087

--- src/_Algorithm/GCD_LCM/P17087.java
+++ src/_Algorithm/GCD_LCM/P17087.java
@@ -1,10 +1,22 @@
 package _Algorithm.GCD_LCM;
 
+import java.util.Scanner;
+
 public class P17087 {
-
+    static int o(int a,int b) {
+        int c=a%b;
+        while(c>0) {
+            a=b;
+            b=c;
+            c=a%b;
+        }
+        return b;
+    }
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],i=0,r=0;
+        for(;i<n;a[i++]=s.nextInt(),r=r<1?a[i-1]:o(r,a[i-1]));
+        System.out.println(r);
     }
 
 }
