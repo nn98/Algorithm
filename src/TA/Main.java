69a7f88f4fe4d4cb1dc1856f460b5d7e1487410b
nn98 <jkllhgb@gmail.com>
2022-11-19 23:35:17 +0900

    Comment

--- src/TA/Main.java
+++ src/TA/Main.java
@@ -3,54 +3,54 @@ package TA;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hw6_1:최혜미");
-        //TreeSet<Integer> tree = TreeSet<Integar>();
-        MyBinarySearchTree tree=new MyBinarySearchTree();
-        System.out.println(tree);//toString출력
-        tree.contains(2);
-        tree.add(5);
-        tree.add(4);
-        tree.add(3);
-        tree.add(1);
-        tree.add(2);
-        tree.add(8);
-        tree.add(6);
-        tree.add(9);
-        tree.add(7);
-        tree.add(10);
-        tree.add(1);
-        tree.add(5);
-        System.out.println(tree);
-       tree.contains(2);
-       tree.contains(6);
-       tree.contains(5);
-       tree.contains(11);
-        
-        tree.remove(9);
-        System.out.println(tree);
-        tree.remove(1);
-        System.out.println(tree);
-        tree.remove(5);
-        
-        System.out.println(tree);
-        
-        tree.remove(9);
-        System.out.println(tree);
-        tree.remove(6);
-        System.out.println(tree);
-        tree.remove(3); 
-        System.out.println(tree);
-        tree.remove(4);
-        System.out.println(tree);
-        tree.remove(2);
-        System.out.println(tree);
-        tree.remove(7);	
-        System.out.println(tree);  
-        tree.remove(8);
-        System.out.println(tree);
-        tree.remove(10);
-        System.out.println(tree);
-        tree.remove(11);
-        System.out.println(tree);
-        System.out.println(tree);
+//        //TreeSet<Integer> tree = TreeSet<Integar>();
+//        MyBinarySearchTree tree=new MyBinarySearchTree();
+//        System.out.println(tree);//toString출력
+//        tree.contains(2);
+//        tree.add(5);
+//        tree.add(4);
+//        tree.add(3);
+//        tree.add(1);
+//        tree.add(2);
+//        tree.add(8);
+//        tree.add(6);
+//        tree.add(9);
+//        tree.add(7);
+//        tree.add(10);
+//        tree.add(1);
+//        tree.add(5);
+//        System.out.println(tree);
+//       tree.contains(2);
+//       tree.contains(6);
+//       tree.contains(5);
+//       tree.contains(11);
+//        
+//        tree.remove(9);
+//        System.out.println(tree);
+//        tree.remove(1);
+//        System.out.println(tree);
+//        tree.remove(5);
+//        
+//        System.out.println(tree);
+//        
+//        tree.remove(9);
+//        System.out.println(tree);
+//        tree.remove(6);
+//        System.out.println(tree);
+//        tree.remove(3); 
+//        System.out.println(tree);
+//        tree.remove(4);
+//        System.out.println(tree);
+//        tree.remove(2);
+//        System.out.println(tree);
+//        tree.remove(7);	
+//        System.out.println(tree);  
+//        tree.remove(8);
+//        System.out.println(tree);
+//        tree.remove(10);
+//        System.out.println(tree);
+//        tree.remove(11);
+//        System.out.println(tree);
+//        System.out.println(tree);
     }
 }
