1c1c033b84633d64184b7159808f78ffc6dc6099
nn98 <jkllhgb@gmail.com>
2022-11-20 23:50:16 +0900

    17087

--- src/_Algorithm/GCD_LCM/P17087.java
+++ src/_Algorithm/GCD_LCM/P17087.java
@@ -3,19 +3,21 @@ package _Algorithm.GCD_LCM;
 import java.util.Scanner;
 
 public class P17087 {
-    static int o(int a,int b) {
-        int c=a%b;
-        while(c>0) {
-            a=b;
-            b=c;
-            c=a%b;
+    static int o(int a, int b) {
+        int c = a % b;
+        while (c > 0) {
+            a = b;
+            b = c;
+            c = a % b;
         }
         return b;
     }
+
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt(),m=s.nextInt(),a[]=new int[n],i=0,r=0;
-        for(;i<n;a[i++]=Math.abs(s.nextInt()-m),r=r<1?a[i-1]:o(r,a[i-1]));
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(), m = s.nextInt(), a[] = new int[n], i = 0, r = 0;
+        for (; i < n; a[i++] = Math.abs(s.nextInt() - m), r = r < 1 ? a[i - 1] : o(r, a[i - 1]))
+            ;
         System.out.println(r);
     }
 
