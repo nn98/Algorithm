032b0721be7d640de68eb9e5dc10cf2501b077ae
nn98 <jkllhgb@gmail.com>
2022-11-25 03:08:29 +0900

    18130

--- src/_Algorithm/Simple_Implementation/P3566.py
+++ src/_Algorithm/Simple_Implementation/P3566.py
@@ -14,17 +14,5 @@ class Moniter:
         return 0
 W,H,h,v=map(int,input().split())
 for _ in range(int(input())):
-    list=[]
-    m=int(input())
-    for _ in range(m):
-        a,b=input().split()
-        crab=Crab(int(a),int(b))
-        list.append(crab)
-    j=0
-    for i in range(m):
-        if list[i].calc()<list[j].calc():
-            j=i
-        elif list[i].calc()==list[j].calc():
-            if list[i].price<list[j].price:
-                j=i
-    print(list[j].price)
+    print()
+print(list[j].price)
