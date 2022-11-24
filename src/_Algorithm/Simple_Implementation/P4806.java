5de6e0e33c37af3f40d7ef3c9ca5e05d9cba3b38
nn98 <jkllhgb@gmail.com>
2022-11-25 01:22:44 +0900

    4806

---
+++ src/_Algorithm/Simple_Implementation/P4806.java
@@ -0,0 +1,15 @@
+package _Algorithm;
+
+import java.util.Scanner;
+
+public class P4806 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int i=0;
+        while(s.hasNext()){
+            s.nextLine();
+            i++;
+        }
+        System.out.println(i);
+    }
+}
