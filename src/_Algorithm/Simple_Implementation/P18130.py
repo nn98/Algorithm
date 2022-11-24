55e52c9ff7ae9596868fde65b3be231d017e43d5
nn98 <jkllhgb@gmail.com>
2022-11-25 02:06:22 +0900

    18130

---
+++ src/_Algorithm/Simple_Implementation/P18130.py
@@ -0,0 +1,18 @@
+class Fan:
+    def __init__(self,price,additionalTime,additionalCost) -> None:
+        self.price=price
+        self.additionalTime=additionalTime
+        self.additionalCost=additionalCost
+    def calc(self,time):
+        return self.price+int(time/self.additionalTime)*self.additionalCost
+
+n,m=map(int,input().split())
+list=[]
+for _ in range(n):
+    a,b,c=map(int,input().split())
+    list.append(Fan(a,b,c))
+j=0
+for i in range(n):
+    if list[i].calc()<list[j].calc():
+        j=i
+print(j,list[j].calc(m))
