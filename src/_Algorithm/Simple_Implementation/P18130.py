180e05277eb1d2b01e9641aaffee820787ff9b20
nn98 <jkllhgb@gmail.com>
2022-11-25 02:46:27 +0900

    18130

--- src/_Algorithm/Simple_Implementation/P18130.py
+++ src/_Algorithm/Simple_Implementation/P18130.py
@@ -5,20 +5,15 @@ class Fan:
         self.additionalCost=additionalCost
     def calc(self,time):
         price=self.price
-        count=1
-        for i in range(0,time+1,self.additionalTime):
-            price+=self.additionalCost*count
-            count+=1
-        return price
-
+        time=int((time-1)/self.additionalTime)
+        price+=(time*(time+1)/2)*self.additionalCost
+        return int(price)
 n,m=map(int,input().split())
 list=[]
-for _ in range(n):
-    a,b,c=map(int,input().split())
-    list.append(Fan(a,b,c))
 j=0
 for i in range(n):
-    print(list[i].calc(m))
+    a,b,c=map(int,input().split())
+    list.append(Fan(a,b,c))
     if list[i].calc(m)<list[j].calc(m):
         j=i
 print(j+1,list[j].calc(m))
