f6954b83c6de2a965c4ee72c5baf60fabcb27288
nn98 <jkllhgb@gmail.com>
2022-11-28 11:18:40 +0900

    6118

--- src/_Algorithm/BFS/P6118.java
+++ src/_Algorithm/BFS/P6118.java
@@ -1,20 +1,29 @@
 package _Algorithm.BFS;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P6118 {
+    static Scanner s = new Scanner(System.in);
+    static int n = s.nextInt(), m = s.nextInt(), i, j, x = 0, a[][] = new int[n][n], h[] = new int[n], r[] = new int[n];
+
+    static void o(int x, int v) {
+        h[x] = v++;
+        for (int i = 0; i < n; i++) {
+            if (a[x][i] > 0)
+                if (h[i] > v)
+                    o(i, v);
+        }
+    }
 
     public static void main(String[] args) {
-        static void o(int x,int y,int v){
-            
-        }
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt(), m = s.nextInt(), i, j, x = 0, a[][] = new int[n][n], h[] = new int[n];
         for (; x < m; x++) {
-            i = s.nextInt();
-            j = s.nextInt();
-            a[i][j]=a[j][i]=1;
+            i = s.nextInt() - 1;
+            j = s.nextInt() - 1;
+            a[i][j] = a[j][i] = 1;
         }
+        o(0, 0);
+        System.out.println(Arrays.toString(h));
     }
 
 }
