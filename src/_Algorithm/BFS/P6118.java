4178d7c047397750f606435237c815b71c4b41ee
nn98 <jkllhgb@gmail.com>
2022-11-28 11:27:01 +0900

    6118

--- src/_Algorithm/BFS/P6118.java
+++ src/_Algorithm/BFS/P6118.java
@@ -9,7 +9,6 @@ public class P6118 {
 
     static void o(int x, int v) {
         h[x] = v++;
-        r = r > h[x] ? r : h[x];
         for (int i = 0; i < n; i++) {
             if (a[x][i] > 0)
                 if (h[i] > v)
@@ -27,7 +26,10 @@ public class P6118 {
         o(0, 0);
         i = j = 0;
         System.out.println(Arrays.toString(h));
-        for (x = n; x-- > 0;) {
+        for (x = 0; x < n; x++) {
+            r = r > h[x] ? r : h[x];
+        }
+        for (; x-- > 0;) {
             if (h[x] == r) {
                 i = x;
                 j++;
