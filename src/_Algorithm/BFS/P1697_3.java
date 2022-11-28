3cf710f4273b08a51b40ebad3a91222232237c8e
nn98 <jkllhgb@gmail.com>
2022-11-28 17:22:43 +0900

    1697

--- src/_Algorithm/BFS/P1697_3.java
+++ src/_Algorithm/BFS/P1697_3.java
@@ -1,10 +1,48 @@
 package _Algorithm.BFS;
 
+import java.util.*;
+
 public class P1697_3 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),m=s.nextInt(),i=0,j,l=100001,a[]=new int[l],r=Integer.MAX_VALUE,R=0,X;
+        Arrays.fill(a,-1);
+        a[n]=0;
+        Queue<Integer>q=new LinkedList();
+        q.add(n);
+        while(!q.isEmpty()) {
+            j=q.poll();
+            i=a[j];
+            if(j==m) {
+                if(r>i) {
+                    r=0;
+                    R=0;
+                }
+                if(r==i) {
+                    R++;
+                }
+            }
+            if(i<r) {
+                X=j*2;
+                i++;
+                if(X<l)if(a[X]<i|a[X]<0) {
+                    q.add(X);
+                    a[X]=i;
+                }
+                X=j+1;
+                if(X<=m)if(a[X]<i|a[X]<0) {
+                    q.add(X);
+                    a[X]=i;
+                }
+                X=j-1;
+                if(X>0&X>m)if(a[X]<i|a[X]<0) {
+                    q.add(X);
+                    a[X]=i;
+                }
+            }
+        }
+        
     }
 
 }
