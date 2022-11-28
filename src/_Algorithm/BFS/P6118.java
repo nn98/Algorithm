0a633cb6642ce9979103df6683a0f389bb894585
nn98 <jkllhgb@gmail.com>
2022-11-28 11:18:37 +0900

    6118

--- src/_Algorithm/BFS/P6118.java
+++ src/_Algorithm/BFS/P6118.java
@@ -1,10 +1,20 @@
 package _Algorithm.BFS;
 
+import java.util.Scanner;
+
 public class P6118 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        static void o(int x,int y,int v){
+            
+        }
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(), m = s.nextInt(), i, j, x = 0, a[][] = new int[n][n], h[] = new int[n];
+        for (; x < m; x++) {
+            i = s.nextInt();
+            j = s.nextInt();
+            a[i][j]=a[j][i]=1;
+        }
     }
 
 }
