d2e71401479cfa923c5eed934584ed075e3a0a09
nn98 <jkllhgb@gmail.com>
2022-11-28 17:28:52 +0900

    1697

--- src/_Algorithm/BFS/P1697_3.java
+++ src/_Algorithm/BFS/P1697_3.java
@@ -24,20 +24,20 @@ public class P1697_3 {
                     R++;
                 }
             }
-            if(i<=r) {
+            if(i<r) {
                 X=j*2;
                 i++;
-                if(X<l)if(a[X]<i|a[X]<0) {
+                if(X<l)if(a[X]>i|a[X]<0) {
                     q.add(X);
                     a[X]=i;
                 }
                 X=j+1;
-                if(a[X]<i|a[X]<0) {
+                if(a[X]>i|a[X]<0) {
                     q.add(X);
                     a[X]=i;
                 }
                 X=j-1;
-                if(X>0)if(a[X]<i|a[X]<0) {
+                if(X>0)if(a[X]>i|a[X]<0) {
                     q.add(X);
                     a[X]=i;
                 }
