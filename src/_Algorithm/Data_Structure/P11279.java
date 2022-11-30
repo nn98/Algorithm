d0c5de2a47e3356423773eb0395929ed3dad65e3
nn98 <jkllhgb@gmail.com>
2022-11-30 16:04:06 +0900

    11279

--- src/_Algorithm/Data_Structure/P11279.java
+++ src/_Algorithm/Data_Structure/P11279.java
@@ -1,4 +1,13 @@
-package _Algorithm.Data_Structure;
-
-public class P11279 {
+import java.util.*;
+class P11279 {
+    public static void main(String[]z) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),m;
+        PriorityQueue<Integer>q=new PriorityQueue<>((a, b) -> b - a);
+        for(;n-->0;) {
+            m=s.nextInt();
+            if(m==0)System.out.println(q.isEmpty()?0:q.poll());
+            else q.add(m);
+        }
+    }
 }
