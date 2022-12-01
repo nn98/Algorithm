c154270a9652a89810c0dc2457d9424da65b28b0
nn98 <jkllhgb@gmail.com>
2022-12-01 20:48:32 +0900

    '11279'

--- src/_Algorithm/Data_Structure/P11279.java
+++ src/_Algorithm/Data_Structure/P11279.java
@@ -1,2 +1,18 @@
-package _Algorithm.Data_Structure;public class P11279 {
+package _Algorithm.Data_Structure;
+import java.io.*;
+import java.util.*;
+public class P11279 {
+    public static void main(String[]z)throws Exception{
+        BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(r.readLine()),m;
+        PriorityQueue<Integer>q=new PriorityQueue<>((a, b) -> b - a);
+        for(;n-->0;) {
+            m=Integer.parseInt(r.readLine());
+            if(m==0)w.write((q.isEmpty()?0:q.poll())+"\n");
+            else q.add(m);
+        }
+        w.flush();
+    }
+}
 }
