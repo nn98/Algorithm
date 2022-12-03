df94a220a3704629cfa4fc645c0df7023572a71a
nn98 <jkllhgb@gmail.com>
2022-12-04 07:07:32 +0900

    '16479'

--- src/_Algorithm/Simple_Implementation/P16479.java
+++ src/_Algorithm/Simple_Implementation/P16479.java
@@ -1,2 +1,11 @@
-package _Algorithm.Simple_Implementation;public class P16479 {
+package _Algorithm.Simple_Implementation;
+
+import java.util.Scanner;
+
+public class P16479 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int k=s.nextInt(),a=s.nextInt(),b=s.nextInt();
+        System.out.println(a==b?k*k:k*k-Math.abs(a-b)/2*Math.abs(a-b)/2);
+    }
 }
