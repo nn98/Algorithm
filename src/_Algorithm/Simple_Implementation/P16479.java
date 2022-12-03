2156590f58dbb9eb8223a74698a26c0c0688aa4a
nn98 <jkllhgb@gmail.com>
2022-12-04 07:13:17 +0900

    '16479'

--- src/_Algorithm/Simple_Implementation/P16479.java
+++ src/_Algorithm/Simple_Implementation/P16479.java
@@ -6,6 +6,6 @@ public class P16479 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int k=s.nextInt(),a=s.nextInt(),b=s.nextInt();
-        System.out.println(a==b?k*k:k*k-Math.abs(a-b)/2*Math.abs(a-b)/2);
+        System.out.println(a==b?k*k:k*k-Math.abs(a-b)/(double)2*Math.abs(a-b)/(double)2);
     }
 }
