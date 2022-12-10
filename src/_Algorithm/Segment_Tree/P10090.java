17bf38a7ae835cfaedc95d32ac5fbca0a6e5a4ef
nn98 <jkllhgb@gmail.com>
2022-12-10 16:01:20 +0900

    10090

--- src/_Algorithm/Segment_Tree/P10090.java
+++ src/_Algorithm/Segment_Tree/P10090.java
@@ -1,2 +1,34 @@
-package _Algorithm.Segment_Tree;public class P10090 {
+package _Algorithm.Segment_Tree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P10090 {
+    static int n, l=1, i, j, t[];
+    static long R;
+
+    static void o() {
+        for (; j > 1; j /= 2, t[j] = t[j * 2] + t[j * 2 + 1]) ;
+    }
+
+    static int p(int n,int s,int e) {
+        if(s>j|e<i) return 0;
+        if(i<=s&e<=j)return t[n];
+        int m=(s+e)/2;
+        return p(n*2,s,m)+p(n*2+1,m+1,e);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(r.readLine());
+        for(;l<n;l*=2);
+        t=new int[l*2];
+        for(String s:r.readLine().split(" ")){
+            j=l+i++;
+            t[j]=Integer.parseInt(s);
+            o();
+            R+=p(1,1,n);
+        }
+        System.out.println(R);
+    }
 }
