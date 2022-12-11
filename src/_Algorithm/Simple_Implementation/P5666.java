987db3e48a9f08db9c7643d9592b9d807d93b62f
nn98 <jkllhgb@gmail.com>
2022-12-11 20:09:32 +0900

    5666

--- src/_Algorithm/Simple_Implementation/P5666.java
+++ src/_Algorithm/Simple_Implementation/P5666.java
@@ -1,2 +1,13 @@
-package _Algorithm.Simple_Implementation;public class P5666 {
+package _Algorithm.Simple_Implementation;
+
+import java.util.Scanner;
+
+public class P5666 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        while(s.hasNext()){
+            String[]a=s.nextLine().split(" ");
+            System.out.println(String.format("%.2f",Double.parseDouble(a[0])/Double.parseDouble(a[1])));
+        }
+    }
 }
