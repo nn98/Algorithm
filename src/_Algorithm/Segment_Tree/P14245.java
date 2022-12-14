13cf391aae63a679cbf1e6e254d3200c933024fe
nn98 <jkllhgb@gmail.com>
2022-12-14 22:51:20 +0900

    14245

--- src/_Algorithm/Segment_Tree/P14245.java
+++ src/_Algorithm/Segment_Tree/P14245.java
@@ -1,2 +1,8 @@
-package _Algorithm.Segment_Tree;public class P14245 {
+package _Algorithm.Segment_Tree;
+
+public class P14245 {
+    public static void main(String[] args) {
+        System.out.println(1^9);
+        System.out.println(1^9^9);
+    }
 }
