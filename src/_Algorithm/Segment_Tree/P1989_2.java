677af7fbe73462d3153c9133f82a0dc49fec5278
nn98 <jkllhgb@gmail.com>
2022-12-15 23:24:22 +0900

    1989

--- src/_Algorithm/Segment_Tree/P1989_2.java
+++ src/_Algorithm/Segment_Tree/P1989_2.java
@@ -6,20 +6,21 @@ import java.util.*;
 public class P1989_2 {
     public static void main(String[] args) throws Exception {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        long size = Integer.parseInt(r.readLine()), m = 0, a[] = new long[(int) size], j, u=0;
-        int i = 0;
+        long size = Integer.parseInt(r.readLine()), m = 0, a[] = new long[(int) size], u,c,M;
+        int i = 0, j;
         int L = 0, R = 0;
         Stack<Integer> k = new Stack();
         StringTokenizer t = new StringTokenizer(r.readLine());
         for (; i < size; i++) {
             a[(int) i] = Integer.parseInt(t.nextToken());
-//                스택 꼭대기값이 지금 인덱스의 값보다 크면
+            u=0;
+            M=1000001;
             while (!k.isEmpty() && a[(Integer) k.peek()] > a[i]) {
-//                    꼭대기값 꺼내서
-//                    해당 값 * (지금 인덱스 - (다음 꼭대기값+1))==길이 > 직사각형 크기
-                long w = i;
-                if (!k.isEmpty()) w -= k.peek() + 1;
-                long c = u * w;
+                j=k.pop();
+                u+=a[j];
+                M=M<a[j]?M:a[j];
+                c = u * M;
+                if(!k.isEmpty())System.out.println(a[(Integer) k.peek()] +" "+ a[i]+" "+u+" "+M+" "+i);
                 if (m < c) {
                     if (!k.isEmpty()) System.out.print(k.peek() + " " + i);
                     else System.out.print(0 + " " + i);
@@ -33,17 +34,20 @@ public class P1989_2 {
                     System.out.println(" " + c);
                     m = c;
                 }
-                u-=a[k.pop()];
             }
             k.push(i);
-            u+=a[i];
         }
-        while (!k.isEmpty()) {
-            j = k.pop();
-            long w = i;
-            if (!k.isEmpty()) w -= k.peek() + 1;
-            long c = a[(int) j] * w;
+        u=0;
+        M=1000001;
+        while (!k.isEmpty() && a[(Integer) k.peek()] > a[i]) {
+            j=k.pop();
+            u+=a[j];
+            M=M<a[j]?M:a[j];
+            c = u * M;
+            if(!k.isEmpty())System.out.println(a[(Integer) k.peek()] +" "+ a[i]+" "+u+" "+M+" "+i);
             if (m < c) {
+                if (!k.isEmpty()) System.out.print(k.peek() + " " + i);
+                else System.out.print(0 + " " + i);
                 if (!k.isEmpty()) {
                     L = k.peek();
                     R = i;
@@ -51,6 +55,7 @@ public class P1989_2 {
                     L = 0;
                     R = i;
                 }
+                System.out.println(" " + c);
                 m = c;
             }
         }
