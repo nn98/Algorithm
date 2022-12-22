b8da82f0d62f965aa3498f83cf93e359de63aa0b
nn98 <jkllhgb@gmail.com>
2022-12-23 05:20:08 +0900

    23794

--- src/_Algorithm/Simple_Implementation/P23794.java
+++ src/_Algorithm/Simple_Implementation/P23794.java
@@ -1,2 +1,27 @@
-package _Algorithm.Simple_Implementation;public class P23794 {
+package _Algorithm.Simple_Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P23794 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n+2; i++) sb.append('@');
+        sb.append('\n');
+        for (int i = 0; i < n; i++) {
+            sb.append('@');
+            for (int j = 0; j < n; j++) {
+                sb.append(' ');
+            }
+            sb.append('@');
+            sb.append('\n');
+        }
+        for (int i = 0; i < n+2; i++) sb.append('@');
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        new P23794().solution();
+    }
 }
