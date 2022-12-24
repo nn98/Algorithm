01021a110760e04d1aef57f13e0437a501ccea33
nn98 <jkllhgb@gmail.com>
2022-12-25 02:59:03 +0900

    11658

--- src/_Algorithm/Prefix_Sum/P11658.java
+++ src/_Algorithm/Prefix_Sum/P11658.java
@@ -1,7 +1,9 @@
 package _Algorithm.Prefix_Sum;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -11,20 +13,43 @@ public class P11658 {
     }
     public static void main(String[] args) throws Exception{
         BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st=new StringTokenizer(reader.readLine());
         int n=Integer.parseInt(st.nextToken()),m=Integer.parseInt(st.nextToken()),i=0,j,
                 a[][]=new int[n][n],v[][]=new int[n][n],x1,y1,x2,y2;
-        for(st=new StringTokenizer(st.nextToken());i<n;i++,st=new StringTokenizer(st.nextToken()))
+        for(;i<n;i++){
+            st=new StringTokenizer(reader.readLine());
             for(j=0;j<n;v[i][j]=Integer.parseInt(st.nextToken()),o(i,j,v[i][j++],a));
+        }
         for(int[]b:a) System.out.println(Arrays.toString(b));
-        for(st=new StringTokenizer(st.nextToken());m-->0;st=new StringTokenizer(st.nextToken())){
+        for(;m-->0;){
+            st=new StringTokenizer(reader.readLine());
             i=Integer.parseInt(st.nextToken());
-            x1=Integer.parseInt(st.nextToken());
-            y1=Integer.parseInt(st.nextToken());
+            x1=Integer.parseInt(st.nextToken())-1;
+            y1=Integer.parseInt(st.nextToken())-1;
             if(i<1){
                 j=Integer.parseInt(st.nextToken());
-
+                int V=j-v[x1][y1];
+                v[x1][y1]=j;
+                o(x1,y1,V,a);
+            }else {
+                x2=Integer.parseInt(st.nextToken())-1;
+                if(x2<x1){
+                    int t=x1;
+                    x1=x2;
+                    x2=t;
+                }
+                y2=Integer.parseInt(st.nextToken())-1;
+                if(y2<y1){
+                    int t=y1;
+                    y1=y2;
+                    y2=t;
+                }
+                int V=0;
+                for(;x1<=x2;x1++,V+=y1-y2);
+                writer.write(V+"\n");
             }
         }
+        writer.flush();
     }
 }
