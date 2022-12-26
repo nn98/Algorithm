fa50c8938b710e062f89168fa0870993f6f3b056
nn98 <jkllhgb@gmail.com>
2022-12-27 05:57:25 +0900

    23804

---
+++ src/_Algorithm/Simple_Implementation/P23804.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+N = int(input())
+for i in range(5*N) :
+    if i < N :
+        for j in range(5*N) :
+            print("@", end="")
+    if i >= (5*N) - N :
+        for j in range(5*N) :
+            print("@", end="")
+    if i >= N and i < (5*N) - N :
+        for j in range(N) :
+            print("@", end="")
+    print()
