c5443d001ce7d684edf074071781a018630eaa48
nn98 <jkllhgb@gmail.com>
2022-12-26 14:22:06 +0900

    14929

--- src/_Algorithm/Simple_Implementation/P14929.java
+++ src/_Algorithm/Simple_Implementation/P14929.java
@@ -1,2 +1,14 @@
-package _Algorithm.Simple_Implementation;public class P14929 {
+package _Algorithm.Simple_Implementation;
+
+import java.util.Scanner;
+
+public class P14929 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), i = 0, a[] = new int[n + 1], b[] = new int[n + 1];
+        long r = 0;
+        for (; i++ < n; b[i] = a[i] = scanner.nextInt(), a[i] += +a[i - 1]) ;
+        for(i=1;i++<n;r+=b[i]*a[i-1]);
+        System.out.println(r);
+    }
 }
