2f16b477682a24da803ba6b016a74eb183ec0dc9
nn98 <jkllhgb@gmail.com>
2023-01-05 14:47:41 +0900

    3770

--- src/_Algorithm/Fenwick_Tree/P3770.java
+++ src/_Algorithm/Fenwick_Tree/P3770.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class P3770 {
-    static int n;
+    static int n,k;
     static int[] tree;
 
     static void update(int idx, int val) {
@@ -26,30 +26,34 @@ public class P3770 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int X=Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int x=0;x++<X;){
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        tree = new int[n + 1];
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            map.computeIfAbsent(a, list -> new ArrayList<>()).add(b);
-        }
-
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        Collections.sort(keyList);
-
-        int idx = 0;
-        long res = 0;
-        for (int key : keyList) {
-            for (int num : map.get(key)) {
-                res += (idx++) - sum(num);
-                update(num, 1);
+            tree = new int[(n>m?n:m) + 1];
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                map.computeIfAbsent(a, list -> new ArrayList<>()).add(b);
             }
+
+            List<Integer> keyList = new ArrayList<>(map.keySet());
+            Collections.sort(keyList);
+
+            int idx = 0;
+            long res = 0;
+            for (int key : keyList) {
+                for (int num : map.get(key)) {
+                    res += (idx++) - sum(num);
+                    update(num, 1);
+                }
+            }
+            System.out.print(res);
         }
-        System.out.print(res);
     }
 }
