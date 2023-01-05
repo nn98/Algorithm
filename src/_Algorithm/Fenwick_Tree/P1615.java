feef984b74060b81e1765c585707bbdb75da2259
nn98 <jkllhgb@gmail.com>
2023-01-05 14:43:00 +0900

    1615

--- src/_Algorithm/Fenwick_Tree/P1615.java
+++ src/_Algorithm/Fenwick_Tree/P1615.java
@@ -1,2 +1,50 @@
-package _Algorithm.Fenwick_Tree;public class P1615 {
+package _Algorithm.Fenwick_Tree;
+import java.io.*;
+import java.util.*;
+public class P1615 {
+    static int n;
+    static int[] tree;
+    static void update(int idx, int val) {
+        while(idx <= n) {
+            tree[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+    static long sum(int idx) {
+        long res =0;
+        while(idx > 0) {
+            res += tree[idx];
+            idx -= idx & -idx;
+        }
+        return res;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        tree = new int[n+1];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map.computeIfAbsent(a, list -> new ArrayList<>()).add(b);
+        }
+
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList);
+
+        int idx=0;
+        long res = 0;
+        for(int key : keyList) {
+            for(int num : map.get(key)) {
+                res += (idx++)-sum(num);
+                update(num, 1);
+            }
+        }
+        System.out.print(res);
+    }
 }
