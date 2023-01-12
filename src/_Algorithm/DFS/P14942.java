502166d632b2d04a3d05cb0bf8ee2ff5b557f7ed
nn98 <jkllhgb@gmail.com>
2023-01-13 08:01:49 +0900

    14942

--- src/_Algorithm/DFS/P14942.java
+++ src/_Algorithm/DFS/P14942.java
@@ -4,25 +4,39 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class P14942 {
-    static int number, power[], i, cave[][], from, to, length;
+    static class Connect {
+        int target;
+        int value;
 
-    //    static List<Integer>
+        Connect(int target, int value) {
+            this.target = target;
+            this.value = value;
+        }
+    }
+
+    static int number, power[], i, from, to, length;
+
+    static List<Connect>[] cave;
+
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         number = Integer.parseInt(reader.readLine());
         power = new int[number];
-        cave = new int[number][number];
-        for (i = 0; i < number; power[i++] = Integer.parseInt(reader.readLine())) ;
+        cave = new List[number + 1];
+        for (i = 0; i < number; power[i++] = Integer.parseInt(reader.readLine()), cave[i] = new ArrayList<>()) ;
         for (i = 1; i < number; i++) {
             StringTokenizer st = new StringTokenizer(reader.readLine());
             from = Integer.parseInt(st.nextToken());
             to = Integer.parseInt(st.nextToken());
             length = Integer.parseInt(st.nextToken());
-            cave[from][to] = cave[to][from] = length;
+            cave[from].add(new Connect(to, length));
+            cave[to].add(new Connect(from, length));
         }
     }
 }
