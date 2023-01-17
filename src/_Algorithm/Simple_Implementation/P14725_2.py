8290ade406339f20712f92c2d79fa61f4b58de37
nn98 <jkllhgb@gmail.com>
2023-01-17 15:15:29 +0900

    14725

--- src/_Algorithm/Simple_Implementation/P14725_2.py
+++ src/_Algorithm/Simple_Implementation/P14725_2.py
@@ -1,3 +1,5 @@
+# 편하지만 미숙함
+# 스스로의 공간지각능력에 실망했다
 import sys
 n=int(sys.stdin.readline())
 a={}
@@ -6,30 +8,17 @@ for i in range(n):
     count=int(line[0])
     if line[1] not in a.keys():
         a[line[1]]={}
-    # depth=''
-    # insert=[]
     depth=0
     temp=a[line[1]]
     for j in range(2,count+1):
         depth+=1
-        # print(line[j] , temp, temp.keys())
         if line[j] not in temp.keys():
             temp[line[j]]= {}
-        # print(a)
         temp=temp[line[j]]
-        # print(temp)
-# a=dict(sorted(a.items()))
-# print(a)
-# print(len(a))
-# print(len(a['B']['A']))
 def printResult(cave,depth):
-    # print('pR:',dict,depth)
     cave=dict(sorted(cave.items()))
     for key in cave.keys():
         print(depth*'--'+key)
         if len(cave[key]) != 0:
             printResult(cave[key],depth+1)
 printResult(a,0)
-# for v1 in a.keys():
-#     v1=dict(sorted(v1.items()))
-#     print(v1)
