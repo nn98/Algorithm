9bc3311dc5ed2de4c13a9e020732d19bb59acd4b
nn98 <jkllhgb@gmail.com>
2023-01-17 13:44:06 +0900

    14725

--- src/_Algorithm/Simple_Implementation/P14725.py
+++ src/_Algorithm/Simple_Implementation/P14725.py
@@ -7,17 +7,25 @@ for i in range(n):
     if line[1] not in a.keys():
         a[line[1]]=[]
     # depth=''
+    insert=[]
     depth=0
     for j in range(2,count+1):
         # depth+='--'
         depth+=1
-        a[line[1]].append([depth,line[j]])
-        # print(a)
+        # a[line[1]].append([depth,line[j]])
+        insert.append([depth,line[j]])
+    a[line[1]].append(insert)
+    # print(a)
 a=dict(sorted(a.items()))
-for key in a.keys():
-    a[key].sort(key = lambda x : (x[0],x[1]))
+# for key in a.keys():
+#     a[key].sort(key = lambda x : (x[0],x[1]))
 # print(a)
+# for key in a.keys():
+#     print(key)
+#     for data in a[key]:
+#         print(data[0]*'--'+data[1])
 for key in a.keys():
     print(key)
     for data in a[key]:
-        print(data[0]*'--'+data[1])
+        for detail in data:
+            print(detail[0]*'--'+detail[1])
