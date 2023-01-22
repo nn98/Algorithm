0938f2231ebdabc36056c06da2adb1c69575b198
nn98 <jkllhgb@gmail.com>
2023-01-23 05:07:46 +0900

    11437

--- src/_Algorithm/LCA/P11437_2.java
+++ src/_Algorithm/LCA/P11437_2.java
@@ -12,14 +12,16 @@ public class P11437_2 {
     static int number;
     //        두 노드 중 하나는 부모가 존재한다 가장
 //        int[][] parentOf = new int[number][number];
-    static int[][] tree;
+    static int[][] tree,pa;
     static boolean[] visited;
     static void dfs(int x){
         System.out.println(x);
         visited[x]=true;
         for(int i=0;i<number;i++) {
-            if (tree[x][i] > 0 & !visited[i])
+            if (tree[x][i] > 0 & !visited[i]) {
+                pa[0][i]=x;
                 dfs(i);
+            }
         }
     }
     public static void main(String[] args) throws Exception {
@@ -29,6 +31,7 @@ public class P11437_2 {
 //        두 노드 중 하나는 부모가 존재한다 가장
 //        int[][] parentOf = new int[number][number];
         tree = new int[number][number];
+        pa=new int[20][100005];
         visited = new boolean[number];
         for (int i = 1; i < number - 1; i++) {
             String[] input = reader.readLine().split(" ");
@@ -37,9 +40,27 @@ public class P11437_2 {
             tree[n0][n1]=tree[n1][n0]=1;
         }
 //        for(int a:parentOf) System.out.println(a);
+//        for(int[]a:tree) System.out.println(Arrays.toString(a));
+//        System.out.println();
         dfs(1);
+//        for(int[]a:pa) System.out.println(Arrays.toString(a));
+//        System.out.println();
+        for(int i=0;i<19;i++){
+            for (int j=1;j<=number;j++){
+                if(pa[i][j]>0){
+                    pa[i+1][j]=pa[i][pa[i][j]];
+                }
+            }
+        }
+//        for(int[]a:pa) System.out.println(Arrays.toString(a));
+//        System.out.println();
         int vertexes = Integer.parseInt(reader.readLine());
-
+        for(int k=0;k<vertexes;k++){
+            String[] input = reader.readLine().split(" ");
+            int n0 = Integer.parseInt(input[0]);
+            int n1 = Integer.parseInt(input[1]);
+            int diff =
+        }
         writer.flush();
     }
 }
