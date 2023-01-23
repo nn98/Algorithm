a5949417927c1019132170b6a20e3bcbf5876909
nn98 <jkllhgb@gmail.com>
2023-01-23 17:08:20 +0900

    11437: 변수명 수정

--- src/_Algorithm/LCA/P11437_2.java
+++ src/_Algorithm/LCA/P11437_2.java
@@ -12,22 +12,22 @@ public class P11437_2 {
     static int number, length, index,sIndex,lIndex;
     //        두 노드 중 하나는 부모가 존재한다 가장
 //        int[][] parentOf = new int[number][number];
-    static int[] seg, search, sNode,lSeg;
+    static int[] serial2no, no2serial, sNode,locInTrip;
     static int[][] tree;
     static boolean[] visited;
 
     static void dfs(int x) {
-        lSeg[x]=index;
+        locInTrip[x]=index;
         sNode[++sIndex]=x;
-        search[x]=sIndex;
-        seg[index++] = x;
+        no2serial[x]=sIndex;
+        serial2no[index++] = x;
         System.out.println(x);
         visited[x] = true;
         for (int i = 0; i < number; i++) {
             if (tree[x][i] > 0 & !visited[i]) {
 //                pa[0][i]=x;
                 dfs(i);
-                seg[index++] = x;
+                serial2no[index++] = x;
             }
         }
     }
@@ -39,12 +39,12 @@ public class P11437_2 {
 //        두 노드 중 하나는 부모가 존재한다 가장
 //        int[][] parentOf = new int[number][number];
         tree = new int[number][number];
-        search = new int[number];
+        no2serial = new int[number];
         sNode = new int[number];
-        lSeg = new int[number];
+        locInTrip = new int[number];
 //        pa=new int[20][100005];
         for (length = 1; length < number; length *= 2) ;
-        seg = new int[length*2];
+        serial2no = new int[length*2];
         visited = new boolean[number];
         for (int i = 1; i < number - 1; i++) {
             String[] input = reader.readLine().split(" ");
@@ -57,10 +57,10 @@ public class P11437_2 {
 //        System.out.println();
         dfs(1);
         for(int[]a:tree) System.out.println(Arrays.toString(a));
-        System.out.println("\n"+Arrays.toString(lSeg));
-        System.out.println("\n"+Arrays.toString(search));
+        System.out.println("\n"+Arrays.toString(locInTrip));
+        System.out.println("\n"+Arrays.toString(no2serial));
         System.out.println("\n"+Arrays.toString(sNode));
-        System.out.println("\n"+Arrays.toString(seg));
+        System.out.println("\n"+Arrays.toString(serial2no));
 //        for(int[]a:pa) System.out.println(Arrays.toString(a));
 //        System.out.println();
 //        for(int i=0;i<19;i++){
