4b7c8727d9df019b5178b33ef1c82c1ff0831a63
nn98 <jkllhgb@gmail.com>
2023-01-27 23:34:29 +0900

    14942

--- src/BaekJoon/DFS/P14942_3.java
+++ src/BaekJoon/DFS/P14942_3.java
@@ -1,10 +1,40 @@
 package BaekJoon.DFS;
+// 고급알고리즘을 이애하기엔 부족하지만 때려맞추기만은 할수있다
+/*
+13
+19
+23
+25
+34
+13
+19
+23
+25
+34
+13
+19
+23
+30
+1 2 10
+1 3 10
+1 4 10
+2 5 10
+2 6 10
+3 7 10
+4 8 10
+4 9 10
+4 10 10
+6 11 10
+8 12 10
+10 13 10
 
+ */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -27,20 +57,31 @@ public class P14942_3 {
         }
     }
 
-    static int number, power[], i, from, to, length, result, history[];
+    static int number, power[], i, from, to, length, history[],result[];
     static List<Connect>[] cave;
 
-    static void o(int x,int p,int r) {
-//        System.out.println(i+" "+x+" "+p);
-        history[x]=p;
-        if(p<=power[i])r=x;
-        if(x==1) {
-            result=r;
-            return;
+    static void o(int x,int p,List<Connect> c) {
+//        System.out.println(x+" "+p+" "+c);
+        history[x]=1;
+        if(p<=power[x])result[x]=1;
+        else{
+//            System.out.println("도달불가");
+            int P=p;
+            int i=0;
+            for(;P>power[x];){
+//                System.out.println("- "+c.get(i).value);
+                P-=c.get(i).value;
+                i++;
+//                System.out.println("index: "+i);
+            }
+            result[x]=c.get(i-1).target;
         }
-        for(Connect c:cave[x]){
-            if(history[c.target]<1||history[c.target]>p+c.value){
-                o(c.target,p+c.value,r);
+        for(Connect C:cave[x]){
+            if(history[C.target]<1){
+                int s=c.size();
+                c.add(s,C);
+                o(C.target,p+C.value,c);
+                c.remove(s);
             }
         }
     }
@@ -49,6 +90,8 @@ public class P14942_3 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         number = Integer.parseInt(reader.readLine()) + 1;
+        history = new int[number];
+        result = new int[number];
         power = new int[number];
         cave = new List[number];
         for (i = 0; ++i < number; power[i] = Integer.parseInt(reader.readLine()), cave[i] = new ArrayList<>()) ;
@@ -60,13 +103,17 @@ public class P14942_3 {
             cave[from].add(new Connect(to, length));
             cave[to].add(new Connect(from, length));
         }
+
 //        writer.write(Arrays.toString(power)+"\n");
 //        for(List l:cave)writer.write(l.toString()+"\n");
-        for(i=0;++i<number;){
-            history = new int[number];
-            o(i,0,0);
-            writer.write(result+"\n");
-        }
+//        for(i=0;++i<number;){
+//            history = new int[number];
+//            o(i,0,0);
+//            writer.write(result+"\n");
+//        }
+        o(1,0,new ArrayList<Connect>());
+//        System.out.println(Arrays.toString(result));
+        for(i=0;++i<number; writer.write(result[i]+"\n"));
         writer.flush();
     }
 }
