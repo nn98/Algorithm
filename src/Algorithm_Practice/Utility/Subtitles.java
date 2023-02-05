4863dea36677fe69ddff9c217f756aa55156704a
nn98 <jkllhgb@gmail.com>
2023-02-06 00:46:23 +0900

    25175

--- src/Algorithm_Practice/Utility/Subtitles.java
+++ src/Algorithm_Practice/Utility/Subtitles.java
@@ -1,2 +1,21 @@
-package Algorithm_Practice.Utility;public class Subtitles {
+package Algorithm_Practice.Utility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Subtitles {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+//        int number=Integer.parseInt(reader.readLine());
+        StringBuffer stringBuffer=new StringBuffer();
+        while(true){
+            String line= reader.readLine();
+            if(line.equals("")){
+                reader.readLine();
+                reader.readLine();
+            } else if (line.equals("xx")) break;
+            else stringBuffer.append(line+"\n");
+        }
+        System.out.println(stringBuffer);
+    }
 }
