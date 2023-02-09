16aac74233a1945e40040d491f77ff894769d3ee
nn98 <jkllhgb@gmail.com>
2023-02-09 23:54:02 +0900

    6588

--- src/BaekJoon/Math/P6588.java
+++ src/BaekJoon/Math/P6588.java
@@ -1,2 +1,28 @@
-package BaekJoon.Math;public class P6588 {
+package BaekJoon.Math;
+
+import BaekJoon._Contest.c819.B;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+public class P6588 {
+
+    public static void main(String[] args) throws Exception {
+        boolean[] arr=new boolean[1000000];
+        for(int i=2;i<arr.length;i++)if(!arr[i])for(int j=i+i;j<arr.length;j+=i)arr[j]=true;
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(reader.readLine());
+        while(n!=0) {
+            boolean f=false;
+            for(int i=2;i<n;i++) {
+                if(!arr[i]&&!arr[n-i]) {
+                    System.out.println(n+" = "+i+" + "+(n-i));
+                    f=true;
+                    break;
+                }
+            }
+            if(!f) System.out.println("Goldbach's conjecture is wrong.");
+            n=Integer.parseInt(reader.readLine());
+        }
+    }
 }
