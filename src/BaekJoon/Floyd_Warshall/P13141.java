a9820cd0b570d6520b46de86b534c1e4c9cf4c18
nn98 <jkllhgb@gmail.com>
2023-02-21 12:44:49 +0900

    13141

--- src/BaekJoon/Floyd_Warshall/P13141.java
+++ src/BaekJoon/Floyd_Warshall/P13141.java
@@ -1,2 +1,16 @@
-package BaekJoon.Floyd_Warshall;public class P13141 {
+package BaekJoon.Floyd_Warshall;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class P13141 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] a = reader.readLine().split(" ");
+        int n = Integer.parseInt(a[0]), m = Integer.parseInt(a[1]), i, j, k, d[][] = new int[222][222], s[] = new int[20202], e[] = new int[20202];
+        long X[] = new long[20202];
+    }
 }
