0a3c52083dc056429b0afed5d249fa04a8d7deba
nn98 <jkllhgb@gmail.com>
2023-02-23 08:10:06 +0900

    1321

--- src/BaekJoon/Fenwick_Tree/P1321.java
+++ src/BaekJoon/Fenwick_Tree/P1321.java
@@ -6,34 +6,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P1321 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()), i=0;
-        FenwickTree tree=new FenwickTree(n);
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        for(;i<n;tree.add(++i,Integer.parseInt(st.nextToken())));
+        int n = Integer.parseInt(br.readLine()), i = 0, m;
+        FenwickTree tree = new FenwickTree(n);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (; i < n; tree.add(++i, Integer.parseInt(st.nextToken()))) ;
         System.out.println(Arrays.toString(tree.tree));
-        for(i=0;i++<n; System.out.println(tree.sum(i)));
-        for(i=0;i++<n; System.out.println(tree.sum(i)-tree.sum(i-1)));
+        for (i = 0; i++ < n; System.out.println(tree.sum(i))) ;
+        for (i = 0; i++ < n; System.out.println(tree.sum(i) - tree.sum(i - 1))) ;
+        m = Integer.parseInt(br.readLine());
     }
+
     static class FenwickTree {
         static int[] tree;
 
         public FenwickTree(int size) {
-            tree = new int[size+1];
+            tree = new int[size + 1];
         }
 
-        long sum(int pos){
+        long sum(int pos) {
             long result = 0;
-            while(pos > 0){
+            while (pos > 0) {
                 result += tree[pos];
-                pos &= (pos-1);
+                pos &= (pos - 1);
             }
             return result;
         }
 
-        void add(int pos, int val){
-            while(pos < tree.length){
+        void add(int pos, int val) {
+            while (pos < tree.length) {
                 System.out.println(pos);
                 tree[pos] += val;
                 pos += (pos & -pos);
