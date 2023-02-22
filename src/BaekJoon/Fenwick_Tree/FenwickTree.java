7d48d0a0c7e3d58364359e8029db6ae5e8245f20
nn98 <jkllhgb@gmail.com>
2023-02-23 07:58:19 +0900

    1321

--- src/BaekJoon/Fenwick_Tree/FenwickTree.java
+++ src/BaekJoon/Fenwick_Tree/FenwickTree.java
@@ -1,2 +1,25 @@
-package BaekJoon.Fenwick_Tree;public class Fenwick {
+package BaekJoon.Fenwick_Tree;
+
+public class FenwickTree {
+    static int[] tree;
+
+    public FenwickTree(int size) {
+        tree = new int[size+1];
+    }
+
+    long sum(int pos){
+        long result = 0;
+        while(pos > 0){
+            result += tree[pos];
+            pos &= (pos-1);
+        }
+        return result;
+    }
+
+    void add(int pos, int val){
+        while(pos < tree.length){
+            tree[pos] += val;
+            pos += (pos & -pos);
+        }
+    }
 }
