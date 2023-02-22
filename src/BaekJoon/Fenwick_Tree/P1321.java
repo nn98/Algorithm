d0257c977244853b4dbe6252234ab0462b674cba
nn98 <jkllhgb@gmail.com>
2023-02-23 08:30:55 +0900

    1321 - case2:AC

--- src/BaekJoon/Fenwick_Tree/P1321.java
+++ src/BaekJoon/Fenwick_Tree/P1321.java
@@ -1,21 +1,59 @@
 package BaekJoon.Fenwick_Tree;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P1321 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()), i = 0, m;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine()), i = 0, j, m, f;
         FenwickTree tree = new FenwickTree(n);
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (; i < n; tree.add(++i, Integer.parseInt(st.nextToken()))) ;
-        System.out.println(Arrays.toString(tree.tree));
-        for (i = 0; i++ < n; System.out.println(tree.sum(i))) ;
-        for (i = 0; i++ < n; System.out.println(tree.sum(i) - tree.sum(i - 1))) ;
+//        System.out.println(Arrays.toString(tree.tree));
+//        for (i = 0; i++ < n; System.out.println(tree.sum(i))) ;
+//        for (i = 0; i++ < n; System.out.println(tree.sum(i) - tree.sum(i - 1))) ;
         m = Integer.parseInt(br.readLine());
+        for (; m-- > 0; ) {
+            st = new StringTokenizer(br.readLine());
+            f = Integer.parseInt(st.nextToken());
+            i = Integer.parseInt(st.nextToken());
+            if (f == 1) {
+                j = Integer.parseInt(st.nextToken());
+                tree.add(i, j);
+//                System.out.println(Arrays.toString(tree.tree));
+            } else {
+//                case 1: O(n)
+//                for (j = 0; j++ < n; ) {
+//                    if (tree.sum(j) >= i) {
+//                        bw.write(j + "\n");
+//                        break;
+//                    }
+//                }
+//                case 2: O(logN)
+                int left = 1, right = n, mid = (left + right) / 2;
+                while (true) {
+//                    System.out.println(left+" "+right+" "+mid);
+                    if (tree.sum(mid) >= i) {
+                        if (tree.sum(mid - 1) < i) {
+                            bw.write(mid + "\n");
+                            break;
+                        }
+                        right = mid - 1;
+                    }
+                    else {
+                        left = mid + 1;
+                    }
+                    mid = (left + right) / 2;
+                }
+            }
+        }
+        bw.flush();
     }
 
     static class FenwickTree {
@@ -36,7 +74,6 @@ public class P1321 {
 
         void add(int pos, int val) {
             while (pos < tree.length) {
-                System.out.println(pos);
                 tree[pos] += val;
                 pos += (pos & -pos);
             }
