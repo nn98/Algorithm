2bdac11e6b31b8b532df6b2b775042b34b6d6b35
nn98 <jkllhgb@gmail.com>
2023-02-24 16:30:36 +0900

    1439

--- src/BaekJoon/Simple_Implementation/P1439.java
+++ src/BaekJoon/Simple_Implementation/P1439.java
@@ -1,2 +1,17 @@
-package BaekJoon.Simple_Implementation;public class P1439 {
+package BaekJoon.Simple_Implementation;
+
+public class P1439 {
+    public static void main(String[] args) {
+        String s = new java.util.Scanner(System.in).next();
+        int o = 0, z = 0, i = 0;
+        if (s.charAt(i) == '0') z++;
+        else o++;
+        for (; ++i < s.length(); ) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                if (s.charAt(i) == '0') z++;
+                else o++;
+            }
+        }
+        System.out.println(z<o?z:o);
+    }
 }
