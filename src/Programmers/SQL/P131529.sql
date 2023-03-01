3a9f63206510c4e648ade74f1c7a8716dc5a4728
nn98 <jkllhgb@gmail.com>
2023-03-01 21:01:12 +0900

    P-SQL 131529

--- src/Programmers/SQL/P131529.sql
+++ src/Programmers/SQL/P131529.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
 -- # SELECT product_code, SUBSTRING(product_code,1,2) from product;
-SELECT left(product_code,2) as category, count(*) as products from product group by category;
+SELECT left (product_code, 2) as category, count (*) as products
+from product
+group by category;
 -- # select * from product
