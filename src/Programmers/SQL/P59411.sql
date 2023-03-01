f960789da768d8a659131fbfa0c6fe656a5780e0
nn98 <jkllhgb@gmail.com>
2023-03-01 21:01:04 +0900

    P-SQL 59411

--- src/Programmers/SQL/P59411.sql
+++ src/Programmers/SQL/P59411.sql
@@ -3,22 +3,21 @@ SELECT i.animal_id, i.name
 from animal_ins i
          join animal_outs o
               using (animal_id)
-order by o.datetime - i.datetime desc
-    limit 2;
+order by o.datetime - i.datetime desc limit 2;
 
-# SELECT animal_id, name
-    # from animal_ins
-               # where animal_id = (
-                                    #     SELECT i.animal_id, i.name, o.datetime - i.datetime as period
-#     from animal_ins i
-#     join animal_outs o
-#     using (animal_id)
-#     order by period desc
-# );
-# SELECT i.animal_id, i.name, i.datetime as Idate, o.datetime as Odate, o.datetime - i.datetime as period
-      # from animal_ins i
-      # join animal_outs o
-# using (animal_id)
-# order by period desc
-      # limit 2
-      # ;
+-- # SELECT animal_id, name
+--     # from animal_ins
+--                # where animal_id = (
+--                                     #     SELECT i.animal_id, i.name, o.datetime - i.datetime as period
+-- #     from animal_ins i
+-- #     join animal_outs o
+-- #     using (animal_id)
+-- #     order by period desc
+-- # );
+-- # SELECT i.animal_id, i.name, i.datetime as Idate, o.datetime as Odate, o.datetime - i.datetime as period
+--       # from animal_ins i
+--       # join animal_outs o
+-- # using (animal_id)
+-- # order by period desc
+--       # limit 2
+--       # ;
