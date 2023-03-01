7c463e9fca28c82023bb6ca21224def7bfd6910b
nn98 <jkllhgb@gmail.com>
2023-03-01 14:08:29 +0900

    2618

--- src/BaekJoon/DP/P2618.java
+++ src/BaekJoon/DP/P2618.java
@@ -18,16 +18,16 @@ public class P2618 {
         }
     }
 
-    static int n, m, i, j, x1, y1, x2 = n - 1, y2 = n - 1, r = -1;
+    static int n, m, i, r = -1;
     static int[][] a;
     static boolean[][] h;
     static List<Coordinate> list = new ArrayList<>();
     static StringBuffer rRoute;
 
     static void o(int x1, int y1, int x2, int y2, int index, int value, StringBuffer route) {
-        System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + " " + index + " " + value + " " + route);
+        System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + " " + index + " " + value + "\n" + route);
         if (index == m) {
-            if (r < 0 | r>value) {
+            if (r < 0 | r > value) {
                 r = value;
                 rRoute = route;
             }
@@ -35,9 +35,9 @@ public class P2618 {
         }
         Coordinate target = list.get(index);
         int distance = Math.abs(x1 - target.x) + Math.abs(y1 - target.y);
-        o(target.x, target.y, x2, y2, index + 1, value + distance, route.append("1\n"));
+        o(target.x, target.y, x2, y2, index + 1, value + distance, new StringBuffer(route+"1\n"));
         distance = Math.abs(x2 - target.x) + Math.abs(y2 - target.y);
-        o(x1, y1, target.x, target.y, index + 1, value + distance, route.append("2\n"));
+        o(x1, y1, target.x, target.y, index + 1, value + distance, new StringBuffer(route+"2\n"));
     }
 
     public static void main(String[] args) throws Exception {
@@ -47,14 +47,13 @@ public class P2618 {
         m = Integer.parseInt(reader.readLine());
         a = new int[n][n];
         h = new boolean[2][n];
-        for (; m-- > 0; ) {
+        for (; i < m; i++) {
             String[] input = reader.readLine().split(" ");
-            i = Integer.parseInt(input[0]);
-            j = Integer.parseInt(input[1]);
-            list.add(new Coordinate(i, j));
+            list.add(new Coordinate(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
         }
-        o(0, 0,n-1,n-1,0,0,new StringBuffer());
-        writer.write(r+"\n");
+        o(0, 0, n - 1, n - 1, 0, 0, new StringBuffer());
+        writer.write(r + "\n");
         writer.write(rRoute.toString());
+        writer.flush();
     }
 }
