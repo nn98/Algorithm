34c66ed99af47cbb2a151c37b2ab2ffe7353c579
nn98 <jkllhgb@gmail.com>
2023-03-05 21:59:56 +0900

    P-161988, 42895

--- src/Programmers/DP/P161988.java
+++ src/Programmers/DP/P161988.java
@@ -1,2 +1,46 @@
-package Programmers.DP;public class P161988 {
+package Programmers.DP;
+
+import java.util.Arrays;
+
+public class P161988 {
+    static int n;
+    static long r;
+    static long[] seq, hA,hB;
+    static void o(int x,int f) {
+        if(x<0)return;
+        if(f==1) {
+            hA[x]=seq[x];
+            hB[x]=seq[x]*-1;
+        } else {
+            hA[x]=seq[x]*-1;
+            hB[x]=seq[x];
+        }
+        if(hA[x+1]>0) hA[x]+=hA[x+1];
+        if(hB[x+1]>0) hB[x]+=hB[x+1];
+        System.out.println(x+" "+f+" "+hA[x]);
+        r=Math.max(r,hA[x]);
+        r=Math.max(r,hB[x]);
+        o(x-1,f==1?0:1);
+    }
+    static public long solution(int[] sequence) {
+        r=Long.MIN_VALUE;
+        n=sequence.length;
+        seq=new long[n+1];
+        for(int i=0;i<n;seq[i]=sequence[i++]);
+        hA=new long[n+1];
+        hB=new long[n+1];
+        o(n-1,1);
+        // System.out.println(Arrays.toString(hA));
+        // System.out.println(Arrays.toString(hB));
+        return r;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(solution(new int[]{2, 3, -6, 1, 3, -1, 2, 4}));
+//        System.out.println(solution(new int[]{1}));
+        int[] a=new int[500000];
+        Arrays.fill(a,-100000);
+        for(int i=0;i<500000;a[i]*=-1,i+=2);
+        System.out.println(solution(a));
+    }
 }
--- src/Programmers/DP/P42895.java
+++ src/Programmers/DP/P42895.java
@@ -1,2 +1,38 @@
-package Programmers.DP;public class P42895 {
+package Programmers.DP;
+import java.util.*;
+public class P42895 {
+    static HashSet<Integer>[] DP = new HashSet[8 + 1];
+
+    static public int solution(int N, int number) {
+        if (N == number) return 1;
+        DP[1] = new HashSet<Integer>();
+        DP[1].add(N);
+        int NN = N;
+        for (int i = 2; i <= 8; ++i) {
+            DP[i] = new HashSet<Integer>();
+            NN = NN * 10 + N;
+            DP[i].add(NN);
+            for (int j = 1; j < i; ++j) {
+                var DP1 = DP[j];
+                var DP2 = DP[i - j];
+                for (int value1 : DP1) {
+                    for (int value2 : DP2) {
+                        DP[i].add(value1 + value2);
+                        DP[i].add(value1 - value2);
+                        DP[i].add(value1 * value2);
+                        if (value2 != 0) DP[i].add(value1 / value2);
+                    }
+                    if (DP[i].contains(number)) return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(5, 12));
+        for(HashSet h:DP)System.out.println(h==null?"":h.toString());
+        System.out.println(solution(2, 11));
+        for(HashSet h:DP)System.out.println(h==null?"":h.toString());
+    }
+
 }
