1fe7c4d65e9ac00d88d812d7d902b6b6cbbd67ee
nn98 <jkllhgb@gmail.com>
2023-04-16 09:50:01 +0900

    9063

--- src/BaekJoon/Simple_Implementation/P9063.java
+++ src/BaekJoon/Simple_Implementation/P9063.java
@@ -1,10 +1,12 @@
+package BaekJoon.Simple_Implementation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class P9063 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
