298d0176148643b86ebc4a246828b1a290f527f2
nn98 <jkllhgb@gmail.com>
2023-04-16 09:26:22 +0900

    3770

--- src/BaekJoon/Segment_Tree/P3770.java
+++ src/BaekJoon/Segment_Tree/P3770.java
@@ -1,9 +1,38 @@
 package BaekJoon.Segment_Tree;
 
-public class P3770 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-	public static void main(String[] args) {
-		
-	}
+public class P3770 {
+    static class IO {
+        static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        static String readLine() throws Exception {
+            return reader.readLine();
+        }
+        static void write(String line) throws Exception {
+            writer.write(line);
+        }
+        static void flush(String line) throws Exception {
+            writer.write(line);
+        }
+    }
+
+    static int init() throws Exception {
+        int caseCount = Integer.parseInt(IO.reader.readLine());
+        return 0;
+    }
+
+    static int solution() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 
 }
