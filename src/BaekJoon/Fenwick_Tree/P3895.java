103e6c914479da636c32f5eada1edc94e69501b8
nn98 <jkllhgb@gmail.com>
2023-04-18 10:24:35 +0900

    3895

---
+++ src/BaekJoon/Fenwick_Tree/P3895.java
@@ -0,0 +1,16 @@
+package BaekJoon.Fenwick_Tree;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class P3895 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(true){
+
+        }
+    }
+}
