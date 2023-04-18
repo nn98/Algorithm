14c66323e936b8c3ff3030518707d8b343b41a18
nn98 <jkllhgb@gmail.com>
2023-04-19 08:21:50 +0900

    17400

--- src/BaekJoon/Fenwick_Tree/P17400.java
+++ src/BaekJoon/Fenwick_Tree/P17400.java
@@ -8,15 +8,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P17400 {
-    static int[][] flag = new int[2][];
-    static int flags, oL, eL;
+    static long[][] flag = new long[2][];
+    static int[] length = new int[2];
+    static int flags;
 
-//    Pair 형식이 없다보니 모듈화가 안되나
-//    static int calcIdx(int idx) {
-//        int div = idx / 2;
-//        int remain = idx % 2;
-//
-//    }
+    static void add(int OE, int idx, int val) {
+        while (idx < length[OE]) {
+            flag[OE][idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+
+    static long sum(int OE, int idx) {
+        long res = 0;
+        while (idx > 0) {
+            res += flag[OE][idx];
+            idx &= (idx - 1);
+        }
+        return res;
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +35,48 @@ public class P17400 {
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
         flags = n;
-        oL = flags / 2 + flags % 2 + 1;
-        eL = flags / 2 + 1;
-        flag[0] = new int[oL];
-        flag[1] = new int[eL];
+        length[0] = flags / 2 + flags % 2 + 1;
+        length[1] = flags / 2 + 1;
+        flag[0] = new long[length[0]];
+        flag[1] = new long[length[1]];
         st = new StringTokenizer(reader.readLine());
         for (int i = 1; i++ <= flags; ) { // 0 홀 1 짝 0 홀 ... + 제일 앞 비우기 == i=1; i++
-            flag[i % 2][i / 2] = Integer.parseInt(st.nextToken());
+            add(i % 2, i / 2, Integer.parseInt(st.nextToken()));
         }
         System.out.println(Arrays.toString(flag[0]));
         System.out.println(Arrays.toString(flag[1]));
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(reader.readLine());
+            int f = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()) + 1;
+            int b = Integer.parseInt(st.nextToken());
+            if (f < 2) {
+                int oddFront = a / 2 + a % 2;
+                int oddTail = b / 2 + b % 2;
+                int evenFront = a / 2;
+                int evenTail = b / 2;
+                System.out.println(String.format("0: %d ~ %d", oddFront, oddTail));
+                long oSum = sum(0, oddTail) - sum(0, oddFront - 1);
+                System.out.println(oSum);
+                System.out.println(String.format("1: %d ~ %d", evenFront, evenTail));
+                long eSum = sum(1, evenTail) - sum(1, evenFront - 1);
+                System.out.println(eSum);
+                long res = Math.abs(oSum - eSum);
+                writer.write(res+"\n");
+            } else {
+                add(a % 2, a / 2, b);
+                System.out.println("\t" + Arrays.toString(flag[0]));
+                System.out.println("\t" + Arrays.toString(flag[1]));
+            }
+        }
+        writer.flush();
     }
 }
+
+/*
+[0, 3, 4, 5]
+[0, 1, 1, 9]
+
+[0, 3, 7, 5]
+[0, 1, 2, 9]
+ */
