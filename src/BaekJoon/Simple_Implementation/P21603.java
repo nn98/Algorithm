1238717a2fdb28c4d3d3b00441150411a07f3b25
nn98 <jkllhgb@gmail.com>
2023-05-20 23:38:08 +0900

    21603

--- src/BaekJoon/Simple_Implementation/P21603.java
+++ src/BaekJoon/Simple_Implementation/P21603.java
@@ -1,10 +1,12 @@
+package BaekJoon.Simple_Implementation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class P21603 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
