006c17905bda528ee5060710fc2321708029360d
Andrew Kim <38521355+nn98@users.noreply.github.com>
2023-05-22 23:43:03 +0900

    9237

---
+++ src/BaekJoon/Simple_Implementation/P9237.py
@@ -0,0 +1,11 @@
+n = int(input())
+t = list(map(int, input().split()))
+
+t.sort(reverse=True)
+max_day = 0
+for i in range(n):
+    candidate = t[i] + i + 1
+    if candidate > max_day:
+        max_day = candidate
+
+print(max_day + 1)
