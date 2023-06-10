2d41cf47a3165228c9a61474efa9ae46c756d378
nn98 <jkllhgb@gmail.com>
2023-06-10 21:43:12 +0900

    15660

--- src/BaekJoon/Brute_Force/P15660.java
+++ src/BaekJoon/Brute_Force/P15660.java
@@ -1,2 +1,16 @@
-package BaekJoon.Brute_Force;public class P15660 {
+package BaekJoon.Brute_Force;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15660 {
+    static int x;
+    static int y;
+    static int max;
+    static int[][] matrix;
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+    }
 }
