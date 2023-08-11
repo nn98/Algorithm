ba34638db680ccd95cbecbe4c5208621f2271789
Andrew Kim <38521355+nn98@users.noreply.github.com>
2023-08-11 22:06:59 +0900

    4355

---
+++ src/BaekJoon/Math/P4355.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+           
+        while(true) {
+            int N = Integer.parseInt(br.readLine());
+            if(N == 0) break;
+            if(N == 1) {
+                bw.write(0 + "\n");
+                continue;
+            }
+            bw.write(EulerPhi(N) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+ 
+    private static long EulerPhi(int n) {
+        long ret = n;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                ret -= ret / i;
+            }
+        }
+        if (n > 1) {
+            ret -= ret / n;
+        }
+        return ret;
+    }
+}
