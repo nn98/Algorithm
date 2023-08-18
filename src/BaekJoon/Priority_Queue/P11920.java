fc779d25bf41d713e3267dd5c2d10c04d7b6b171
nn98 <jkllhgb@gmail.com>
2023-08-18 13:26:36 +0900

    11920

--- src/BaekJoon/Priority_Queue/P11920.java
+++ src/BaekJoon/Priority_Queue/P11920.java
@@ -1,2 +1,29 @@
-package BaekJoon.Priority_Queue;public class P11920 {
+package BaekJoon.Priority_Queue;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class P11920 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int K = Integer.parseInt(tokenizer.nextToken());
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int V = Integer.parseInt(tokenizer.nextToken());
+            queue.offer(V);
+            if (i >= K) {
+                writer.write(queue.poll() + " ");
+            }
+        }
+        while (queue.isEmpty()) {
+            writer.write(queue.poll() + " ");
+        }
+        writer.flush();
+    }
 }
