f3a686d241350d57d1c38462b6406451c88fd17c
Andrew Kim <38521355+nn98@users.noreply.github.com>
2023-08-20 00:03:03 +0900

    14908

--- src/BaekJoon/Greedy/P14908.py
+++ src/BaekJoon/Greedy/P14908.py
@@ -1,1 +1,11 @@
+n = int(input())
+arr = []
 
+for i in range(1, n+1):
+    a, b = map(float, input().split())
+    arr.append((a / b, i))
+
+arr.sort()
+
+for i in range(n):
+    print(arr[i][1], end=' ')
