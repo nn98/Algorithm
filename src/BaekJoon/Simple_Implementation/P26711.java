af6edae49fc3a5f2eedb733301f2a03c9a47e9a2
nn98 <jkllhgb@gmail.com>
2023-12-14 19:33:35 +0900

    26711

--- src/BaekJoon/Simple_Implementation/P26711.java
+++ src/BaekJoon/Simple_Implementation/P26711.java
@@ -1,2 +1,14 @@
-package BaekJoon.Simple_Implementation;public class P26711 {
+package BaekJoon.Simple_Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class P26711 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(reader.readLine());
+        BigInteger b = new BigInteger(reader.readLine());
+        System.out.printf(a.add(b).toString());
+    }
 }
