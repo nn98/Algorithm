2ae5f899cc0b8c8c5d8eaeb6616c8ee39fcd783d
nn98 <jkllhgb@gmail.com>
2025-01-22 10:19:46 +0900

    BOJ(Sort): 14602

--- src/BaekJoon/Sorting/P14602.py
+++ src/BaekJoon/Sorting/P14602.py
@@ -5,9 +5,9 @@ def main():
     n, m, k, w = map(int,readline().split())
     matrix = [list(map(int,readline().split())) for _ in range(n)]
     result = [[0] * (m-w+1) for _ in range(n-w+1)]
-    for j in range(m-w):
+    for j in range(m-w+1):
         arr = []
-        for i in range(n-w):
+        for i in range(n-w+1):
             if not arr:
                 for k in range(w):
                     arr.extend(matrix[i][j:j+w])
@@ -15,6 +15,7 @@ def main():
                 arr.extend(matrix[i][j:j+w])
             temp = arr.copy()
             temp.sort()
+            print('i:',i,'j:',j,'arr:',arr)
             result[j][i] = temp[w//2]
             arr = arr[w:]
     print('\n'.join(' '.join(map(str, val)) for val in result))
