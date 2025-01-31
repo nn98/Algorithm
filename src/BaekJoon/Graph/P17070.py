83cd716df886bf985da5d887054f0eff51a83031
nn98 <jkllhgb@gmail.com>
2025-01-31 13:43:01 +0900

    BOJ(Graph): 17070

---
+++ src/BaekJoon/Graph/P17070.py
@@ -0,0 +1,11 @@
+import sys
+readline = sys.stdin.readline
+
+def main():
+    n = int(readline())
+    home = [readline().split() for _ in range(n)]
+    q = deque()
+
+
+if __name__ == "__main__":
+    main()
