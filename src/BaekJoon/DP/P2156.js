014e3310ae812f64064bb330b0350ed6d185e23b
nn98 <jkllhgb@gmail.com>
2025-02-06 13:38:29 +0900

    BOJ(DP) : 2156

--- src/BaekJoon/DP/P2156.js
+++ src/BaekJoon/DP/P2156.js
@@ -3,4 +3,4 @@ const filepath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 const input = fs.readFileSync(filepath, { encoding : 'utf8' }).toString().trim().split('\n');
 const num = (str) => parseInt(str);
 
-console.log(input);
+console.log(Number(input[0]));
