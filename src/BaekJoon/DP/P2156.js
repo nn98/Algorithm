1924f8fe6fc0e2745bec735e5690b83f95e330cc
nn98 <jkllhgb@gmail.com>
2025-02-08 16:22:26 +0900

    BOJ(DP) : 2156

--- src/BaekJoon/DP/P2156.js
+++ src/BaekJoon/DP/P2156.js
@@ -3,7 +3,8 @@ const filepath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 const input = fs.readFileSync(filepath, { encoding : 'utf8' }).toString().trim().split('\n');
 
 let n = Number(input[0]);
-let arr = input.slice(1);
+let arr = input.slice(1).map(Number);
+console.log('arr',arr);
 let dp = Array.from({length: n}, () => new Array(3).fill(0));
 for (let i = 0; i < n; i++) {
   solution(i);
@@ -11,14 +12,11 @@ for (let i = 0; i < n; i++) {
 console.log(dp.join('\n'));
 
 function solution(idx) {
-  if(idx == 1) {
-    dp[idx][0] = arr[idx];
-  } else if(idx == 2) {
-    dp[idx][0] = arr[idx];
-    dp[idx][1] = arr[idx] + dp[idx-1][0];
+  if(idx == 0) {
+    dp[idx][0] = dp[idx][1] = arr[idx];
   } else {
     dp[idx][0] = arr[idx] + dp[idx-1][2];
     dp[idx][1] = arr[idx] + dp[idx-1][0];
-    dp[idx][2] = Math.max(dp[idx-1]);
+    dp[idx][2] = Math.max(...dp[idx-1]);
   }
 }
