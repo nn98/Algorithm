688b8bca187525d8042975c4385dda8bd485fc7d
nn98 <jkllhgb@gmail.com>
2025-02-15 16:11:24 +0900

    BOJ(DP) : 19947

--- src/BaekJoon/DP/P19947.py
+++ src/BaekJoon/DP/P19947.py
@@ -3,14 +3,15 @@ readline = sys.stdin.readline
 
 def main():
     h, y = map(int,readline().split())
-    f = y // 5
-    y %= 5
-    t = y // 3
-    y %= 3
-    h *= 1.35 * f if f > 0 else 1
-    h *= 1.2 * t if t > 0 else 1
-    h *= 1.05 * y if y > 0 else 1
-    print(int(h))
+    dp = [0] * (y+1)
+    dp[0] = h
+    for i in range(1, y+1):
+        dp[i] = int(dp[i-1] * 1.05)
+        if i >= 3:
+            dp[i] = int(max(dp[i],dp[i-3] * 1.2))
+        if i>= 5:
+            dp[i] = int(max(dp[i],dp[i-5] * 1.35))
+    print(dp[y])
 
 if __name__ == "__main__":
     main()
