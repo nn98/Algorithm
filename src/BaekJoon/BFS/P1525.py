b60f69bf0aefd0f623743da31d83b0e91c920c9a
nn98 <jkllhgb@gmail.com>
2025-02-18 14:24:36 +0900

    BOJ(BFS) : 1525

--- src/BaekJoon/BFS/P1525.py
+++ src/BaekJoon/BFS/P1525.py
@@ -1,48 +1,48 @@
 import sys
-from heapq import heappush, heappop
+from collections import deque
 readline = sys.stdin.readline
 
 def main():
-    start = ''.join(''.join(readline().rstrip().split()) for _ in range(3))
-    end = '123456780'
+    # start = ''.join(''.join(readline().rstrip().split()) for _ in range(3))
+    start = tuple(int(c) for _ in range(3) for c in readline().rstrip().split())
+    end = tuple(map(int,list('123456780')))
     dist = {start: 0}
-    q = []
-    heappush(q, (dist[start], start))
+    q = deque()
+    q.append(start)
     while q:
-        state, count = heappop(q)
-        if dist[state] < count: continue
+        state = q.popleft()
+        # if state in dist and dist[state] < count: continue
         if state == end:
-            print(count)
+            print(dist[state])
             return
         for next in find_case(state):
-            if next in dist and dist[next] < count + 1:
-                continue
-            dist[next] = count + 1
-            heappush(q, (dist[next], next))
+            if next not in dist:
+                dist[next] = dist[state] + 1
+                q.append(next)
 
-    print(dist[end])
+    print(-1)
 
 def find_case(state):
-    idx = state.index('0')
+    idx = state.index(0)
     ans = []
     row = idx // 3
     col = idx % 3
     if row > 0:
-        temp = state
-        temp[idx], temp[idx + 3] = temp[idx + 3], temp[idx]
-        ans.append(temp)
-    if row < 2:
-        temp = state
+        temp = list(state)
         temp[idx], temp[idx - 3] = temp[idx - 3], temp[idx]
-        ans.append(temp)
+        ans.append(tuple(temp))
+    if row < 2:
+        temp = list(state)
+        temp[idx], temp[idx + 3] = temp[idx + 3], temp[idx]
+        ans.append(tuple(temp))
     if col > 0:
-        temp = state
+        temp = list(state)
         temp[idx], temp[idx - 1] = temp[idx - 1], temp[idx]
-        ans.append(temp)
+        ans.append(tuple(temp))
     if col < 2:
-        temp = state
+        temp = list(state)
         temp[idx], temp[idx + 1] = temp[idx + 1], temp[idx]
-        ans.append(temp)
+        ans.append(tuple(temp))
     return ans
 
 if __name__ == '__main__':
