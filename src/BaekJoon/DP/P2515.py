effd33c59cd45faf173770db6070bce5ae339370
nn98 <jkllhgb@gmail.com>
2025-02-25 13:30:48 +0900

    BOJ(DP) : 2515

--- src/BaekJoon/DP/P2515.py
+++ src/BaekJoon/DP/P2515.py
@@ -1,11 +1,26 @@
 import sys
 readline = sys.stdin.readline
 
+def sol(idx, hei, val):
+    hei = pics[idx][0]
+    val += pics[idx][1]
+    dp[idx] = val
+    for i in range(idx, -1,-1):
+        if pics[i][0] <= hei - S and dp[i] < val+pics[i][1]:
+            sol(i, hei, val)
+
 N, S = map(int, readline().split())
 pics = []
 for _ in range(N):
     height, value = map(int, readline().split())
-    if height > S:
-        pics.append((height, value))
-pics.sort(key=lambda x: (x[1], x[0]))
+    pics.append((height, value))
+pics.sort(key=lambda x: (x[0], -x[1]))
 print(pics)
+dp = [0] * len(pics)
+# his = [False] * len(pics)
+
+for i in range(N-1, -1, -1):
+    if dp[i] < pics[i][1]:
+        sol(i, pics[i][0], 0)
+
+print(dp)
