c684159bf800585f28035b252c7d3590ad73e401
nn98 <jkllhgb@gmail.com>
2025-03-18 12:04:31 +0900

    BOJ(Greedy) : 1202

--- src/BaekJoon/Greedy/P1202.py
+++ src/BaekJoon/Greedy/P1202.py
@@ -11,18 +11,17 @@ for i in range(N):
 for i in range(K):
     bags.append(int(readline()))
 jewels.sort(key=lambda x: (-x[1], -x[0]))
-bags.sort(reverse=True)
+bags.sort()
+# print(jewels)
+# print(bags)
 
 ans = 0
-for weight, value in jewels:
-    if not bags: break
-    handle = False
-    for i in range(len(bags)):
-        if weight > bags[i] and handle:
-            print(weight, value, bags[i-1])
+while bags:
+    for weight, value in jewels:
+        if bags[0] >= weight:
+            # print(weight, value, bags[0])
             ans += value
-            bags.remove(bags[i-1])
-            break;
-        else :
-            handle = True
+            bags.pop(0)
+            jewels.remove([weight, value])
+            break
 print(ans)
