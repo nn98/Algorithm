817b16008c37a3a3761675b10ffdca4cdca8b9ce
nn98 <jkllhgb@gmail.com>
2025-03-20 12:04:31 +0900

    BOJ(Greedy) : 1202

--- src/BaekJoon/Greedy/P1202.py
+++ src/BaekJoon/Greedy/P1202.py
@@ -1,17 +1,25 @@
 import sys
-
 readline = sys.stdin.readline
 
 N, K = map(int, readline().split())
 jewels = []
 bags = []
+
 for i in range(N):
     a, b = map(int, readline().split())
     jewels.append([a, b])
 for i in range(K):
-    a, b = map(int, readline().split())
-    bags.append([a, b])
-jewels.sort(key=lambda x: (x[1], x[0]))
-bags.sort()
-print(jewels)
-print(bags)
+    bags.append(int(readline()))
+jewels.sort(key=lambda x: (-x[1], -x[0]))
+bags.sort(reverse=True)
+
+ans = 0
+for weight, value in jewels:
+    if not bags: break
+    for bag in bags:
+        print(weight, value, bag)
+        if weight <= bag:
+            ans += value
+            bags.remove(bag)
+            break
+print(ans)
