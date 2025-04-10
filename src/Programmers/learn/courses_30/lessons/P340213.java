dfa40fbb33eac5527e85628b0f6b49b7f2cae527
nn98 <jkllhgb@gmail.com>
2025-04-10 11:01:52 +0900

    Rename : move/rename err files

--- src/Programmers/learn/courses_30/lessons/P340213.java
+++ src/Programmers/learn/courses_30/lessons/P340213.java
@@ -1,4 +1,4 @@
-class Solution {
+class P340213 {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         String answer = "";
         // int min = 5;
---
+++ src/Soldesk/Example.java
@@ -0,0 +1,4 @@
+package Soldesk;
+
+public class Example {
+}
