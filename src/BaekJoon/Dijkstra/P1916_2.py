d764331cddc0796bb784963004699a92f8411a04
nn98 <jkllhgb@gmail.com>
2025-04-14 19:55:29 +0900

    1916

--- src/BaekJoon/Dijkstra/P1916_2.py
+++ src/BaekJoon/Dijkstra/P1916_2.py
@@ -2,30 +2,31 @@ import sys
 import heapq
 
 readLine = sys.stdin.readline
-num_city = int(readLine())
-num_bus = int(readLine())
+num_city, num_bus = map(int, readLine().split())
 INF = int(1e9)
 dist = [INF] * (num_city+1)
+route = []*(num_city+1)
 graph = [[] for _ in range(num_city + 1)]
 
 for _ in range(num_bus):
     departure, arrival, cost = map(int, readLine().split())
     graph[departure].append((arrival, cost))
+    graph[arrival].append((departure, cost))
 
 def dijkstra(start):
     q = []
-    heapq.heappush(q, (start, 0))
+    heapq.heappush(q, (0, start))
     dist[start] = 0
 
     while q:
-        now, cost = heapq.heappop(q)
+        print(dist)
+        cost, now = heapq.heappop(q)
         if dist[now] < cost: continue
-        for next in graph[now]:
-            nextArrival, nextCost = next
+        for nextArrival, nextCost in graph[now]:
             newCost = nextCost + dist[now]
-            if dist[nextArrival] > newCost:
+            if newCost < dist[nextArrival]:
                 dist[nextArrival] = newCost
-                heapq.heappush(q, (nextArrival, newCost))
+                heapq.heappush(q, (newCost, nextArrival))
 
 
 final_departure, final_arrival = map(int, readLine().split())
