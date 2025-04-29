c6f11c2b309780a073cb86261c561f7d0adb0944
nn98 <jkllhgb@gmail.com>
2025-04-29 13:02:08 +0900

    11000, Greedy

--- src/BaekJoon/Greedy/P11000.py
+++ src/BaekJoon/Greedy/P11000.py
@@ -3,6 +3,23 @@ import heapq
 readline = sys.stdin.readline
 n = int(readline())
 q = []
+arr = []
 for i in range(n):
-    a, b = map(int, readline().split())
-    heapq.heappush(q, [a, b])
+    arr.append(tuple(map(int,readline().rstrip().split())))
+# print(arr)
+arr.sort(key=lambda x:(x[0], x[1]))
+ans = 0
+count = 0
+for i in range(n):
+    heapq.heappush(q,arr[i][1])
+    if q[0] <= arr[i][0]: heapq.heappop(q);
+
+# print(q)
+print(len(q))
+# a, b = heapq.heappop(q)
+# while q:
+#     nextA, nextB = heapq.heappop(q)
+#     while nextA < b:
+#         nextA, nextB = heapq.heappop(q)
+
+
