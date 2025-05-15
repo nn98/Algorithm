72a17a05fb624bdaa8976396c79e3b645d6c1b81
nn98 <jkllhgb@gmail.com>
2025-05-15 23:56:59 +0900

    2025-05-15 K-Job STAR 서울청 코딩대회 3번문제

---
+++ src/Soldesk/2025-05-15_K-Job_STAR_Seoul/Problem3.java
@@ -0,0 +1,45 @@
+package Soldesk.2025-05-15_K-Job_STAR_Seoul;
+import java.io.*;
+import java.util.*;
+// 검토가 중요하다
+public class Problem3 {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        String[] arr = br.readLine().split(" ");
+        //   String[] history = new String[n];
+        List<String> history = new ArrayList();
+        //   history.add("5");
+        //   history.add("1");
+        //   history.add("8");
+        //   history.add("15");
+        //   history.remove(2);
+        //   System.out.println(history.lastIndexOf("8"));
+
+        int nowIndex=0;
+        int nowHistory=0;
+        int ans = 0;
+        while(nowIndex < n) {
+            if(history.contains(arr[nowIndex])) {
+                int removeUntil = history.lastIndexOf(arr[nowIndex]);
+                ans += history.size()-removeUntil-1;
+                for(int i=history.size()-1;i>removeUntil;i-=1) {
+                    //   System.out.println("now : "+i+" history : "+history+" arr[nowIndex] : "+history.indexOf(arr[nowIndex]));
+                    history.remove(i);
+                }
+            } else {
+                history.add(arr[nowIndex]);
+                ans += t;
+            }
+            nowIndex += 1;
+        }
+        System.out.println(ans);
+    }
+}
+/*
+10 10
+1 2 4 2 4 6 8 6 4 2
+결과를 몰라요잉
+*/
