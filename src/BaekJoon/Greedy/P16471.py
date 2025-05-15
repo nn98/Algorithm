91afb03e4fa33169f0cc36fc3bc6af4edf2fdd46
nn98 <jkllhgb@gmail.com>
2025-05-15 18:39:26 +0900

    16471, Greedy

--- src/BaekJoon/Greedy/P16471.py
+++ src/BaekJoon/Greedy/P16471.py
@@ -2,17 +2,18 @@ import sys
 readline = sys.stdin.readline
 n = int(readline())
 player = list(map(int,readline().split()))
-player.sort()
+player.sort(reverse=True)
 master = list(map(int,readline().split()))
 master.sort()
 print(player)
 print(master)
+i=0
+j=n-1
 count = 0
-for i in range(n):
-    # for j in range(len(master)):
-    #     if player[i] < master[j]:
-    #         master.pop(j)
-    #         count += 1
-    #         break
-    count += 1 if player[i] < master[i] else 0
+while i < n and j >= 0:
+    if player[i] < master[j]:
+        count += 1
+        j-=1
+    i+=1
+print(count)
 print('YES' if count >= (n+1)/2 else 'NO')
