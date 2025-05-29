08a84c46e820f8037b54b6a5e39aad959ac1cca6
nn98 <jkllhgb@gmail.com>
2025-05-29 19:34:59 +0900

    CT(K-Job) : 2025-05-29 - Problem04

--- src/Coding_Test/K-Job_STAR_Seoul/Test_2025-05-29/Problem04.py
+++ src/Coding_Test/K-Job_STAR_Seoul/Test_2025-05-29/Problem04.py
@@ -1,20 +1,21 @@
 import sys
+
 readline = sys.stdin.readline
 
-N, T = map(int,readline().split())
-time = list(map(int,readline().split()))
+N, T = map(int, readline().split())
+time = list(map(int, readline().split()))
 ans = [0] * N
 repeat = 1
 while 0 in ans:
     for i in range(N):
-        if
-    # print('i:',i,'repeat:',repeat,'T*repeat:',T*repeat,'time[i] / (T*repeat):',(time[i] / (T*repeat)), 'T*repeat - (time[i]%T):',T*repeat - (time[i]%T))
-    # if ans[i] == 0 and time[i] / (T*repeat) <= 1:
-    #     ans[i] = (0 if i == 0 else i-1) *repeat + (time[i]%T)
+        print('i:', i, 'repeat:', repeat, 'T*repeat:', T * repeat, 'time[i] / (T*repeat):', (time[i] / (T * repeat)),
+              'T*repeat - (time[i]%T):', T * repeat - (time[i] % T))
+        if ans[i] == 0 and time[i] / (T * repeat) <= 1:
+            ans[i] = (0 if i == 0 else i - 1) * repeat + (time[i] % T)
 
     print(ans)
     repeat += 1
-    if repeat > 10:break
+    if repeat > 10: break
 
 for i in range(N):
-    print(ans[i],end=' ')
+    print(ans[i], end=' ')
