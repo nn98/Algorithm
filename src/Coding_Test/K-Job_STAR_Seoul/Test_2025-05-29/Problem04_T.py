d1284cca4ab05abf29bc5ce57171e3d29bc51095
nn98 <jkllhgb@gmail.com>
2025-06-02 12:55:07 +0900

    CT(K-Job) : 2025-05-29 - Problem04_Solve

---
+++ src/Coding_Test/K-Job_STAR_Seoul/Test_2025-05-29/Problem04_T.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+readline = sys.stdin.readline
+
+N, T = map(int, readline().split())
+time = list(map(int, readline().split()))
+ans = [0] * N
+repeat = 1
+while 0 in ans:
+    for i in range(N):
+        print('i:', i, 'repeat:', repeat, 'T*repeat:', T * repeat, 'time[i] / (T*repeat):', (time[i] / (T * repeat)),
+              'T*repeat - (time[i]%T):', T * repeat - (time[i] % T))
+        if ans[i] == 0 and time[i] / (T * repeat) <= 1:
+            ans[i] = (0 if i == 0 else i - 1) * repeat + (time[i] % T)
+
+    print(ans)
+    repeat += 1
+    if repeat > 10: break
+
+for i in range(N):
+    print(ans[i], end=' ')
