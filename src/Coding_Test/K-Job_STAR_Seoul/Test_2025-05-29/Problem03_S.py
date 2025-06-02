587685cc396a4c1da6c534810aff15af4cd3fb33
nn98 <jkllhgb@gmail.com>
2025-06-02 11:54:45 +0900

    CT(K-Job) : 2025-05-29 - Problem03_Solve

---
+++ src/Coding_Test/K-Job_STAR_Seoul/Test_2025-05-29/Problem03_S.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+readline = sys.stdin.readline
+
+N, M, S = map(int, readline().split())
+K = int(readline())
+cul = list(map(int, readline().split()))
+
+# 인접 리스트 생성
+adj = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, readline().split())
+    adj[a].append(b)
+    adj[b].append(a)
+
+distance = [-1] * (N + 1)  # 초기 거리 -1 (도달 불가)
+q = deque()
+
+# cul 노드들을 시작점으로 설정
+for node in cul:
+    distance[node] = 0
+    q.append(node)
+
+# BFS로 최단 거리 계산
+while q:
+    current = q.popleft()
+    for neighbor in adj[current]:
+        if distance[neighbor] == -1:
+            distance[neighbor] = distance[current] + 1
+            q.append(neighbor)
+
+# S 이하 거리 노드 카운트
+count = 0
+for i in range(1, N+1):
+    if 0 <= distance[i] <= S:
+        count += 1
+
+print(count)
