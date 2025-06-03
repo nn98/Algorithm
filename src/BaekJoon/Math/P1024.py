2789f60cd8fb7dadd2c636901ae875ad571413b3
nn98 <jkllhgb@gmail.com>
2025-06-03 23:57:42 +0900

    BOJ(Math) : 1024

---
+++ src/BaekJoon/Math/P1024.py
@@ -0,0 +1,21 @@
+import sys
+
+n, l = map(int, sys.stdin.readline().split())
+
+while True:
+    const = 0
+    first = 0
+
+    for i in range(1, l):
+        const += i
+    first = (n - const) / l
+
+    if first >= 0:
+        for i in range(l):
+            print(first + i, end=' ')
+    else:
+        if l == 100:
+            print(-1)
+            exit(0)
+        else:
+            l += 1
