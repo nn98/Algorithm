80c4a0bbcd63ac8619f6c4691c923a955ad69389
nn98 <jkllhgb@gmail.com>
2025-06-05 10:02:10 +0900

    BOJ(DP) : 2038

--- src/BaekJoon/DP/P2038.py
+++ src/BaekJoon/DP/P2038.py
@@ -1,9 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline())
-dp = [0] * (n + 1)
-dp[1] = 1
-for i in range(2, n + 1):
-    dp[i] = 1 + dp[i - dp[dp[i - 1]]]
+dp = [0, 1]
+sum_ = 1
+i = 2
+while sum_ < n:
+    dp.append(1 + dp[i - dp[dp[i-1]]])
+    sum_ += dp[i]
+    i += 1
 
-print(dp[n])
+print(i - 1)
