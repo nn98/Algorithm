270a44ca07d7a60278486aa51fcdcf75738b1e39
nn98 <jkllhgb@gmail.com>
2025-06-11 11:45:33 +0900

    BOJ(DP) : 1949

--- src/BaekJoon/DP/P1949.py
+++ src/BaekJoon/DP/P1949.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 readline = sys.stdin.readline
 
 def main():
@@ -9,8 +10,23 @@ def main():
         a, b = map(lambda x: int(x)-1, readline().split())
         conn[a].append(b)
         conn[b].append(a)
-    dp = [[int(-1e9)] for _ in range(2)]
-    print(dp)
+    dp = [[int(-1e9)]*n for _ in range(2)]
+    visit = [False]*n
+    q = deque()
+    q.append((0,-1,-1))
+    while q:
+        now, before, b_before = q.popleft()
+        visit[now] = True
+        if before == -1:
+            dp[now][0] = 0
+            dp[now][1] = population[now]
+        else:
+            dp[now][0] = max(dp[before][1], 0 if b_before == -1 else dp[b_before][1])
+            dp[now][1] = population[now] + dp[before][0]
+        for next in conn[now]:
+            if not visit[next]:
+                q.append((next, now, before))
+    print('\n'.join(map(str, dp)))
 
 if __name__ == '__main__':
     main()
