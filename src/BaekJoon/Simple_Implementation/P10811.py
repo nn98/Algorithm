ad2d72ed4918dfdd5701ca12b826896630641bfa
nn98 <jkllhgb@gmail.com>
2025-06-18 11:45:52 +0900

    BOJ(Simple) : 10811

---
+++ src/BaekJoon/Simple_Implementation/P10811.py
@@ -0,0 +1,13 @@
+import sys
+readline = sys.stdin.readline
+
+def main():
+    n, m = map(int,readline().split())
+    arr = [i+1 for i in range(n)]
+    for _ in range(m):
+        a, b = map(lambda x: int(x)-1, readline().split())
+        arr[a], arr[b] = arr[b], arr[a]
+    print(' '.join(map(str,arr)))
+
+if __name__ == "__main__":
+    main()
