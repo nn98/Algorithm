0ada085dacb5df2335d512d89fabd22459d3cc07
nn98 <jkllhgb@gmail.com>
2025-06-18 11:48:53 +0900

    BOJ(Simple) : 10811

--- src/BaekJoon/Simple_Implementation/P10811.py
+++ src/BaekJoon/Simple_Implementation/P10811.py
@@ -6,7 +6,8 @@ def main():
     arr = [i+1 for i in range(n)]
     for _ in range(m):
         a, b = map(lambda x: int(x)-1, readline().split())
-        arr[a], arr[b] = arr[b], arr[a]
+        for i in range(0, (b-a)/2):
+            arr[a+i], arr[b-i] = arr[b-i], arr[a+i]
     print(' '.join(map(str,arr)))
 
 if __name__ == "__main__":
