9f583c9b8f94b99d81ac27f00c05e1101b638737
nn98 <jkllhgb@gmail.com>
2025-06-19 16:44:28 +0900

    BOJ(DS) : 14425

--- src/BaekJoon/Data_Structure/P14425.py
+++ src/BaekJoon/Data_Structure/P14425.py
@@ -4,7 +4,8 @@ readline = sys.stdin.readline
 def main():
     n, m = map(int,readline().split())
     arr = set([readline() for _ in range(n)])
-    print(arr)
+    ans = sum([1 if readline() in arr else 0 for _ in range(m)])
+    print(ans)
 
 if __name__ == "__main__":
     main()
