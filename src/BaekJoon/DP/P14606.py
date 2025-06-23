46c0af916d2d47dd2a8a2586e56d3b1f266c5f04
nn98 <jkllhgb@gmail.com>
2025-06-23 14:44:25 +0900

    BOJ(DP) : 14606

---
+++ src/BaekJoon/DP/P14606.py
@@ -0,0 +1,14 @@
+import sys
+readline = sys.stdin.readline
+
+def main():
+    n = int(readline())
+    dp = [0] * 10**9
+    dp[2] = 1
+    dp[3] = 3
+    for i in range(4,11):
+        dp[i] = (dp[i//2] * 2 + i * i // 4) if i % 2 < 1 else (dp[i//2] + dp[i//2+1] + i//2 * (i//2+1))
+    print(dp[n])
+
+if __name__ == "__main__":
+    main()
