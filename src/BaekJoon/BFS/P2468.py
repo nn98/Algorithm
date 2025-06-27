d40b2f12d355f83e0743466f11333d59d12c1ab9
nn98 <jkllhgb@gmail.com>
2025-06-27 16:20:04 +0900

    BOJ(BFS) : 2468

--- src/BaekJoon/BFS/P2468.py
+++ src/BaekJoon/BFS/P2468.py
@@ -7,19 +7,20 @@ next_Y = [1,0,-1,0]
 def main():
     n = int(readline())
     area = [list(map(int,readline().split())) for _ in range(n)]
-    visit = [[False] * n for _ in range(n)]
     min_val = min(map(min,area))
     before = 1
+    answer = 1
     while True:
         now = 0
         visit = [[False] * n for _ in range(n)]
         for i in range(n):
             for j in range(n):
-                if not visit[i][j]:
+                if area[i][j] > min_val and not visit[i][j]:
                     bfs(area, n, i, j, visit, min_val)
                     now += 1
-        if now < before: break
+        if now < answer: break
         before = now
+        answer = max(answer, now)
         min_val += 1
     print(before)
 
@@ -28,7 +29,7 @@ def bfs(area, n, x, y, visit, min_val):
     for i in range(4):
         new_x = x + next_X[i]
         new_y = y + next_Y[i]
-        if 0 <= new_x < n and 0 <= new_y < n and area[new_x][new_y] > min_val and visit[new_x][new_y]:
+        if 0 <= new_x < n and 0 <= new_y < n and area[new_x][new_y] > min_val and not visit[new_x][new_y]:
             bfs(area, n, new_x, new_y, visit, min_val)
 
 if __name__ == "__main__":
