2bfcc4db9e8ddd42f3bff023997b6be25c027d8c
nn98 <jkllhgb@gmail.com>
2025-06-27 17:01:33 +0900

    BOJ(BFS) : 2468

--- src/BaekJoon/BFS/P2468_2.py
+++ src/BaekJoon/BFS/P2468_2.py
@@ -9,12 +9,12 @@ def main():
     n = int(readline())
     area = [list(map(int,readline().split())) for _ in range(n)]
     min_val = min(map(min,area))
-    visit = [[False] * n for _ in range(n)]
     before = 1
     answer = 1
     q = deque()
     while True:
         now = 0
+        visit = [[False] * n for _ in range(n)]
         for i in range(n):
             for j in range(n):
                 if area[i][j] > min_val and not visit[i][j]:
