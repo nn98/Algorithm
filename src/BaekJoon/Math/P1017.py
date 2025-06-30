1785096e939e19f9db812d2e5b62f0d7f08cb991
nn98 <jkllhgb@gmail.com>
2025-06-30 17:25:17 +0900

    BOJ(Math) : 1017

--- src/BaekJoon/Math/P1017.py
+++ src/BaekJoon/Math/P1017.py
@@ -4,13 +4,24 @@ readline = sys.stdin.readline
 def main():
     n = int(readline())
     arr = list(map(int,readline().split()))
-    prime = [True] * 50000
-    prime[1] = False
-    for idx in range(50000):
+    m_size = 50
+    prime = [True] * m_size
+    prime[0] = prime[1] = False
+    for idx in range(m_size):
         if prime[idx]:
-            for idx_next in range(idx, 50000, idx):
+            for idx_next in range(idx*2, m_size, idx):
                 prime[idx_next] = False
     print(prime)
+    his = [False] * n
+    his[0] = True
+    sum_val(0,arr,his,arr[0],prime)
+
+def sum_val(idx, arr, his, val, prime):
+    print(his, val)
+    for idx_next in range(len(arr)):
+        if not his[idx_next]:
+            his[idx_next] = True
+            sum_val(idx_next, arr, his, val+arr[idx_next], prime)
 
 if __name__ == "__main__":
     main()
