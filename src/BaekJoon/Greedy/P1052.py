b1741e1132c1ccca4daba088755c47c3387ea27f
nn98 <jkllhgb@gmail.com>
2025-07-02 00:01:08 +0900

    BOJ(Greedy) : 1052

---
+++ src/BaekJoon/Greedy/P1052.py
@@ -0,0 +1,7 @@
+n, k = map(int, input().split())
+answer = 0
+while bin(n).count('1') > k:
+    idx = bin(n)[::-1].index('1') # 1이 오른쪽에서 몇 번째에 있는지 찾기
+    answer += 2 ** idx # 2 ^ (idx) 만큼 더하기
+    n += 2 ** idx # 2 ^ (idx) 만큼 더하기
+print(answer)
