e1ecde8a8597dd268fb4fc5fb0de35c7f390e21e
nn98 <jkllhgb@gmail.com>
2025-07-05 09:58:14 +0900

    BOJ(BFS): 2589

--- src/BaekJoon/BFS/P2589.py
+++ src/BaekJoon/BFS/P2589.py
@@ -6,11 +6,13 @@ def main():
     n, m = map(int,readline().split())
     ans = 0
     treasure = [readline().strip() for _ in range(n)]
+    print(treasure)
     hist = [[1e9]*m for _ in range(n)]
     for i in range(n):
         for j in range(m):
+            print(i, j, treasure[i][j], hist[i][j])
             if treasure[i][j] == 'L' and hist[i][j] == 1e9:
-                val = bfs(treasure, i, j, hist, 0)
+                val = bfs(treasure, i, j, hist, 1e9)
                 ans = max(ans, val[0])
     print(ans)
 
@@ -28,11 +30,11 @@ def bfs(treasure, i, j, hist, long):
         if dist > max_data[0]:
             max_data = (dist, x, y)
         dist += 1
-        for i in range(4):
-            next_x = x + X[i]
-            next_y = y + Y[i]
+        for k in range(4):
+            next_x = x + X[k]
+            next_y = y + Y[k]
             # print(next_x, next_y, len(treasure), len(treasure[0]), '\t', dist)
-            if 0 <= next_x < len(treasure) and 0 <= next_y < len(treasure[0]) and treasure[next_x][next_y]:
+            if 0 <= next_x < len(treasure) and 0 <= next_y < len(treasure[0]) and treasure[next_x][next_y] == 'L':
                 if hist[next_x][next_y] > dist:
                     hist[next_x][next_y] = dist
                     q.append((dist, next_x, next_y))
