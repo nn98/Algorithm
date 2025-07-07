d4ac1eaaa8bb31f4399a5ee3114916a623af3cfd
nn98 <jkllhgb@gmail.com>
2025-07-07 17:35:15 +0900

    BOJ(Simulation): 14503

--- src/BaekJoon/Simulation/P14503.py
+++ src/BaekJoon/Simulation/P14503.py
@@ -16,20 +16,26 @@ def main():
     print(ans)
 
 def checkNext(room, x, y, F):
-    if room[x][y] == '0': ans += 1
+    # print(x, y, F)
+    global ans
+    if room[x][y] == '0':
+        ans += 1
+        room[x][y] = '2'
     isReachable = reachSpace(room, x, y)
+    # print('\t', isReachable)
     if isReachable:
         F -= 1
         F %= 4
-        if next:
+        if next(room, x, y, F):
             return x + X[F], y + Y[F] , F
         else:
             return x, y, F
     else:
+        # print('\t\t', isWallBehind(room, x, y, F))
         if isWallBehind(room, x, y, F):
-            return x - X[F], y - Y[F] , F
-        else:
             return -1,-1,-1
+        else:
+            return x - X[F], y - Y[F] , F
 
 def reachSpace(room, x, y):
     X = [-1, 0, 1, 0]
@@ -40,15 +46,18 @@ def reachSpace(room, x, y):
         nextY = y + Y[i]
         result = result or (0 <= nextX < len(room) and 0 <= nextY < len(room[x]) and room[nextX][nextY] == '0')
         if result: return result
+    return result
 
 def isWallBehind(room, x, y, F):
     nextX =  x - X[F]
-    nextY =  y - X[F]
-    return 0 <= nextX < len(room) and 0 <= nextY < len(room[x]) and room[nextX][nextY] == '1'
+    nextY =  y - Y[F]
+    if 0 <= nextX < len(room) and 0 <= nextY < len(room[x]):
+        return room[nextX][nextY] == '1'
+    return False
 
 def next(room, x, y, F):
     nextX =  x + X[F]
-    nextY =  y + X[F]
+    nextY =  y + Y[F]
     return 0 <= nextX < len(room) and 0 <= nextY < len(room[x]) and room[nextX][nextY] == '0'
 
 if __name__ == "__main__":
