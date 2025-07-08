986012c5b4f8b1de65feec4c9e733ca66b7e44ce
nn98 <jkllhgb@gmail.com>
2025-07-08 10:39:52 +0900

    BOJ(Graph): 17070

--- src/BaekJoon/Graph/P17070.py
+++ src/BaekJoon/Graph/P17070.py
@@ -1,11 +1,34 @@
 import sys
+from collections import deque
+
+next_index = [(0,1),(1,0),(1,1)]
+move_case = [(0,2),(1,2),(0,1,2)]
+
 readline = sys.stdin.readline
 
 def main():
     n = int(readline())
     home = [readline().split() for _ in range(n)]
+    ans = 0
     q = deque()
+    q.append((0,1,'h'))
+    while q:
+        x, y, case = q.popleft()
+        if x == n-1 and y == n-1:
+           ans += 1
+        else:
+            q.append(get_next(home, x, y, case))
+    print(ans)
 
+def get_next(home, x, y, case):
+    result = []
+    for move in move_case[case]:
+        x_move, y_move = next_index[move]
+        x_move += x
+        y_move += y
+        if x_move <= len(home) and y_move <= len(home) and home[x_move][y_move] == '0':
+            result.append((x_move, y_move, move))
+    return result
 
 if __name__ == "__main__":
     main()
