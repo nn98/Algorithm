1631fb4b727c907b9fd62d21f0452ffb57160118
nn98 <jkllhgb@gmail.com>
2025-07-08 11:23:26 +0900

    BOJ(Graph): 17070

--- src/BaekJoon/Graph/P17070.py
+++ src/BaekJoon/Graph/P17070.py
@@ -9,16 +9,23 @@ readline = sys.stdin.readline
 def main():
     n = int(readline())
     home = [readline().split() for _ in range(n)]
+    dp = [[[0] * n for _ in range(n)] for _ in range(3)]
+    dp[0][0][1] = 1
+    print(dp)
     ans = 0
     q = deque()
     q.append((0,1,0))
     while q:
         x, y, case = q.popleft()
         if x == n-1 and y == n-1:
-            ans += 1
+            continue
         else:
-            q.extend(get_next(home, x, y, case))
-    print(ans)
+            for next_x, next_y, next_case in get_next(home, x, y, case):
+                print(next_x, next_y, next_case)
+                dp[next_case][next_x][next_y] += dp[case][x][y]
+                q.append((next_x, next_y, next_case))
+    print(dp)
+    # print()
 
 def get_next(home, x, y, case):
     result = []
@@ -26,7 +33,7 @@ def get_next(home, x, y, case):
         x_move, y_move = next_index[move]
         x_move += x
         y_move += y
-        if x_move < len(home) and y_move < len(home) and (home[x_move-1][y_move] == home[x_move][y_move-1] == '0' if move == 2 else home[x_move][y_move] == '0'):
+        if x_move < len(home) and y_move < len(home) and (home[x_move-1][y_move] == home[x_move][y_move-1] == home[x_move][y_move] == '0' if move == 2 else home[x_move][y_move] == '0'):
             result.append((x_move, y_move, move))
     return result
 
