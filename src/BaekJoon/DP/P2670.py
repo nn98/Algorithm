da7d2800f2b9c9175b2729fa7a69fd6e12f7b822
nn98 <jkllhgb@gmail.com>
2025-07-16 14:11:14 +0900

    BOJ(DP): 2670

--- src/BaekJoon/DP/P2670.py
+++ src/BaekJoon/DP/P2670.py
@@ -8,12 +8,11 @@ def main():
         arr.append(float(readline()))
 
     dp = [[0]*n for _ in range(2)]
-    dp[0][0] = dp[0][1] = arr[0]
+    dp[0][0] = dp[1][0] = arr[0]
     for i in range(1, n):
-        dp[0][i] = max(dp[0][i-1],dp[0][i-1]) * arr[i]
+        dp[0][i] = max(max(dp[0][i-1],dp[1][i-1]) * arr[i], arr[i])
         dp[1][i] = dp[1][i-1] * arr[i]
-    print(dp[0])
-    print(dp[1])
+    print(round(max(max(dp[0]),max(dp[1])),3))
 
 if __name__ == "__main__":
     main()
