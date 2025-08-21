a74c669b84ca9b3d1efbeddff67f37b827ab81e0
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-08-21 11:12:16 +0900

    BOJ(Graph): 3182

--- src/BaekJoon/Graph/P3182.java
+++ src/BaekJoon/Graph/P3182.java
@@ -4,28 +4,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int[] seniors = new int[n];
-    int[] history;
-    int[] sums = new int[n];
-    for (int i = 0; i < n; i++) seniors[i] = Integer.parseInt(br.readLine());
-    for (int i = 0; i < n; i++) {
-        history = new int[n];
-        int now = i;
-        int count = 0;
-        while (history[now] < 1) {
-            history[now] = 1;
-            count++;
-            int next = seniors[i];
-            if(sums[next] > 0) {
-                count += sums[next];
+public class P3182 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int max = -1;
+        int[] seniors = new int[n+1];
+        int[] history;
+        int[] sums = new int[n+1];
+        for (int i = 1; i <= n; i++) seniors[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            history = new int[n+1];
+            int now = i;
+            int count = 0;
+            while (history[now] < 1) {
+                history[now] = 1;
+                count++;
+                int next = seniors[now];
+//                if(sums[next] > 0) {
+//                    count += sums[next];
+//                    break;
+//                }
+//                System.out.println(String.format("i : %d / now : %d / count : %d / next : %d", i, now, count, next));
+                now = next;
+            }
+//            System.out.println(String.format("i : %d / count : %d / sums : %s", i, count, Arrays.toString(sums)));
+            sums[i] = count;
+            max = Math.max(max, count);
+        }
+//        System.out.println(Arrays.toString(sums));
+        for (int i = 1; i <= n; i++) {
+            if (sums[i] == max) {
+                System.out.print(i);
                 break;
             }
-            now = next;
         }
-        sums[i] = count;
     }
-    System.out.println(Arrays.toString(sums));
 }
