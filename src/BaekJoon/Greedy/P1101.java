208196b1904059ce13177e58264de0740b5a3d72
nn98 <jkllhgb@gmail.com>
2025-08-26 12:15:43 +0900

    BOJ(Greedy): 1101

---
+++ src/BaekJoon/Greedy/P1101.java
@@ -0,0 +1,32 @@
+package BaekJoon.Greedy;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class P1101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Arrays.stream(a)
+                        .filter(x -> x != 0)
+                        .count() - Arrays.stream(b)
+                        .filter(x -> x != 0)
+                        .count();
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+    }
+}
