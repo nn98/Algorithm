89cc4d6d5dfcb26d7cdb74d1e04254f0217107d4
nn98 <jkllhgb@gmail.com>
2025-08-27 12:23:27 +0900

    BOJ(Graph): 2206

--- src/BaekJoon/Graph/P2206.java
+++ src/BaekJoon/Graph/P2206.java
@@ -2,6 +2,8 @@ package BaekJoon.Graph;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P2206 {
@@ -11,18 +13,46 @@ public class P2206 {
         String[] tokens = inputLine.split(" ");
         int N = Integer.parseInt(tokens[0]);
         int M = Integer.parseInt(tokens[1]);
-        chat[][][] graph = new chat[N][M][2];
+        char[][] graph = new char[N][M];
+
         for (int i = 0; i < N; i++) {
             inputLine = br.readLine();
             for (int j = 0; j < M; j++) {
-                graph[i][j][0] = inputLine.charAt(j);
+                graph[i][j] = inputLine.charAt(j);
             }
         }
-        for(int [][]arr : graph) {
-            for(int i=0;i<arr.length;i++) {
-                System.out.print(arr[i][0] + " ");
+
+        //        for(char []arr : graph) for(int i=0;i<arr.length;i++) System.out.print(arr[i] + (i == arr.length-1 ? "\n" : " "));
+
+        int[][][] visited = new int[N][M][2];
+        int[] nX = {0,1,0,-1};
+        int[] nY = {1,0,-1,0};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0,0,1});
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
+            int x = arr[0];
+            int y = arr[1];
+            int z = arr[2];
+            int distance = arr[3];
+            if(visited[x][y][z]==0) visited[x][y][z]=arr[3];
+            if(visited[x][y][z]<distance) continue;
+            visited[x][y][z] = distance;
+            distance++;
+            for(int i=0;i<4;i++) {
+                int nextX = x + nX[i];
+                int nextY = y + nY[i];
+                if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
+                if(graph[nextX][nextY]=='1') {
+                    if (z == 1) continue;
+                    z++;
+                }
+                if (visited[nextX][nextY][z] == 0 || visited[nextX][nextY][z] > distance) {
+                    visited[nextX][nextY][z] = distance;
+                    q.add(new int[]{nextX,nextY,z+1});
+                }
             }
-            System.out.println();
         }
+        System.out.println(Math.min(visited[N-1][M-1][0], visited[N-1][M-1][1]));
     }
 }
