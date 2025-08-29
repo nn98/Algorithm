dfd3550c9fa4aac404870ae7fd8c2bdf915a8d06
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-08-29 17:17:30 +0900

    BOJ(BFS): ㅋㅋ 뻘짓을 했다해도 2시간은 에반데

--- src/BaekJoon/BFS/P16236.java
+++ src/BaekJoon/BFS/P16236.java
@@ -11,28 +11,42 @@ public class P16236 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[][] space = new int[n][n];
-		int[][][] visited = new int[n][n][7];
+		int[][] visited = null;
 		Coordinate start = null;
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				space[i][j] = Integer.parseInt(st.nextToken());
 				if (space[i][j] == 9) {
-					start = new Coordinate(1,i,j);
+					start = new Coordinate(0,i,j);
+					space[i][j] = 0;
 				}
 			}
 		}
 		PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-		queue.add(start);
-		for(int i=2;i<7;i++) {
+		int ans = 0;
+		int levelCount = 0;
+		for(int i=2;true;) {
+			visited = new int[n][n];
+			for(int[]a:visited)Arrays.fill(a,Integer.MAX_VALUE);
+			queue.add(start);
 			sol(queue, space, visited, i);
-			for(int x=0;x<n;x++) for(int y=0;y<n;y++) System.out.print(visited[x][y][i] + (y == n-1 ? "\n" : " "));
+			// for(int x=0;x<n;x++) for(int y=0;y<n;y++) System.out.print(visited[x][y][i] + (y == n-1 ? "\n" : " "));
 
 			Coordinate next = getClosest(space, visited, i);
-			System.out.println(next);
-			break;
+			if(next == null) break;
+			ans += next.distance;
+			levelCount++;
+			// System.out.println(next + " level : " + i);
+			if(i==levelCount) {
+				i++;
+				levelCount = 0;
+			}
+			space[next.x][next.y] = 0;
+			start = next;
+			start.distance = 0;
 		}
-		System.out.println(queue.poll());
+		System.out.println(ans);
 	}
 	static class Coordinate implements Comparable<Coordinate> {
 		int distance;
@@ -55,7 +69,7 @@ public class P16236 {
 			return String.format("(distance : %d, x : %d, y : %d)", distance, x, y);
 		}
 	}
-	static void sol(PriorityQueue<Coordinate> queue, int[][] space, int[][][] visited, int level) {
+	static void sol(PriorityQueue<Coordinate> queue, int[][] space, int[][] visited, int level) {
 		int[] dx = {-1, 0, 1, 0};
 		int[] dy = {0, -1, 0, 1};
 		while (!queue.isEmpty()) {
@@ -63,31 +77,28 @@ public class P16236 {
 			int distance = curr.distance;
 			int x = curr.x;
 			int y = curr.y;
-			if (visited[x][y][level] == 0) visited[x][y][level] = distance;
-			if (visited[x][y][level] < distance) continue;
-			visited[x][y][level] = distance;
+			if (visited[x][y] < distance) continue;
+			visited[x][y] = distance;
 			distance++;
 			for (int i =0 ; i < 4; i++) {
 				int nextX = x + dx[i];
 				int nextY = y + dy[i];
-				System.out.println("\t" + nextX + " " + nextY);
 				if (nextX < 0 || nextX >= space.length || nextY < 0 || nextY >= space[0].length) continue;
 				if (space[nextX][nextY] > level) continue;
-				if (visited[nextX][nextY][level] == 0 || visited[nextX][nextY][level] > distance) {
-					visited[nextX][nextY][level] = distance;
+				if (visited[nextX][nextY] > distance) {
+					visited[nextX][nextY] = distance;
 					queue.add(new Coordinate(distance, nextX, nextY));
 				}
 			}
 		}
 	}
-	static Coordinate getClosest(int[][] space, int[][][] visited, int level) {
+	static Coordinate getClosest(int[][] space, int[][] visited, int level) {
 		Coordinate closest = null;
-		int closestDistance = Integer.MAX_VALUE;
 		for (int i = 0 ; i < space.length ; i++) {
 			for (int j = 0 ; j < space[i].length ; j++) {
-				if (space[i][j] != 0 && space[i][j] <= level) {
-					if (closest == null) closest = new Coordinate(visited[i][j][level], i, j);
-					else if (closest.distance > visited[i][j][level]) closest = new Coordinate(visited[i][j][level], i, j);
+				if (space[i][j] != 0 && space[i][j] < level && visited[i][j] != Integer.MAX_VALUE) {
+					if (closest == null) closest = new Coordinate(visited[i][j], i, j);
+					else if (closest.distance > visited[i][j]) closest = new Coordinate(visited[i][j], i, j);
 				}
 			}
 		}
