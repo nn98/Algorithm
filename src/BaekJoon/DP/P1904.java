dc7413637843a93db422f04dfdfc0b036a951145
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-09-15 01:40:53 +0900

    BOJ(DP): 1904

--- src/BaekJoon/DP/P1904.java
+++ src/BaekJoon/DP/P1904.java
@@ -1,4 +1,23 @@
 package BaekJoon.DP;
 
+import java.util.Scanner;
+
 public class P1904 {
+	static long[] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		dp = new long[10000001];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] =2;
+		for (int i = 3; i <= N; i++) {
+			dp(i);
+		}
+		System.out.println(dp[N]);
+	}
+	public static void dp(int i) {
+		dp[i] = (dp[i-1]+dp[i-2])%15746;
+	}
 }
