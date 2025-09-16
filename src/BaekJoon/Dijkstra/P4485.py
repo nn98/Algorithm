5a8cb66b0b4cb2ec4f8ab0567bbd73ae2782eb51
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-09-16 16:06:54 +0900

    BOJ(Dijkstra): 4485

--- src/BaekJoon/Dijkstra/P4485.py
+++ src/BaekJoon/Dijkstra/P4485.py
@@ -11,20 +11,22 @@ while CASE:
     count+=1
     matrix = [list(map(int,readline().split())) for _ in range(CASE)]
     # print(matrix)
-    visited = [[0] * CASE for _ in range(CASE)]
+    visited = [[1e9] * CASE for _ in range(CASE)]
     q = []
-    heapq.heappush(q,(0,0,matrix[0][0]))
+    heapq.heappush(q,(matrix[0][0],0,0))
     while q:
-        x, y, dist = heapq.heappop(q)
-        if visited[x][y] != 0 and visited[x][y] < dist: continue
-        print(x,y,dist)
+        dist, x, y = heapq.heappop(q)
+        if visited[x][y] < dist: continue
+        if x == CASE-1 and y == CASE-1: break
+        # print(x,y,dist)
         visited[x][y] = dist
         for i in range(4):
             nX = x + next_x[i]
             nY = y + next_y[i]
             if 0 <= nX < CASE and 0 <= nY < CASE:
-                if visited[nX][nY] == 0 or (visited[nX][nY] != 0 and visited[nX][nY] > dist + matrix[nX][nY]):
-                    heapq.heappush(q, (nX, nY, dist + matrix[nX][nY]))
+                if visited[nX][nY] > dist + matrix[nX][nY]:
+                    visited[nX][nY] = dist + matrix[nX][nY]
+                    heapq.heappush(q, (dist + matrix[nX][nY], nX, nY))
     # print('\n'.join(map(str, visited)))
     print('Problem %d: %d' % (count,visited[CASE-1][CASE-1]))
     CASE = int(readline())
