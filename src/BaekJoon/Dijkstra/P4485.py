21d045ac7ba41265d2e03869534f29fcd99decaf
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-09-16 16:01:07 +0900

    BOJ(Dijkstra): 4485 timeout

--- src/BaekJoon/Dijkstra/P4485.py
+++ src/BaekJoon/Dijkstra/P4485.py
@@ -2,20 +2,22 @@ import sys
 import heapq
 readline = sys.stdin.readline
 
+count = 0
 next_x = [0,1,0,-1]
 next_y = [1,0,-1,0]
 
 CASE = int(readline())
 while CASE:
+    count+=1
     matrix = [list(map(int,readline().split())) for _ in range(CASE)]
-    print(matrix)
+    # print(matrix)
     visited = [[0] * CASE for _ in range(CASE)]
     q = []
     heapq.heappush(q,(0,0,matrix[0][0]))
     while q:
         x, y, dist = heapq.heappop(q)
-        print(x,y,dist)
         if visited[x][y] != 0 and visited[x][y] < dist: continue
+        print(x,y,dist)
         visited[x][y] = dist
         for i in range(4):
             nX = x + next_x[i]
@@ -23,5 +25,6 @@ while CASE:
             if 0 <= nX < CASE and 0 <= nY < CASE:
                 if visited[nX][nY] == 0 or (visited[nX][nY] != 0 and visited[nX][nY] > dist + matrix[nX][nY]):
                     heapq.heappush(q, (nX, nY, dist + matrix[nX][nY]))
-    print('\n'.join(map(str, visited)))
+    # print('\n'.join(map(str, visited)))
+    print('Problem %d: %d' % (count,visited[CASE-1][CASE-1]))
     CASE = int(readline())
