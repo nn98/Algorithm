954b331340b128bfc576d8f6d300312af8a197c8
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-02 23:32:05 +0900

    BOJ(Dijkstra): 2665

--- src/BaekJoon/Dijkstra/P2665.java
+++ src/BaekJoon/Dijkstra/P2665.java
@@ -1,4 +1,63 @@
 package BaekJoon.Dijkstra;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class P2665 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] matrix = new int[n][n];
+		int[][] history = new int[n][n];
+		int[] nX = {1, -1, 0, 0};
+		int[] nY = {0, 0, 1, -1};
+		for(int i=0;i<n;i++) {
+			matrix[i] = new int[n];
+			history[i] = new int[n];
+			Arrays.fill(history[i], Integer.MAX_VALUE);
+			int j = 0;
+			for(char c: br.readLine().toCharArray()) matrix[i][j++] = c-'0';
+		}
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(0,0,1));
+		while(!pq.isEmpty()) {
+			Node node = pq.poll();
+			int nowX = node.x;
+			int nowY = node.y;
+			int nowD = node.dis;
+			if(history[nowX][nowY] <= nowD)continue;
+			history[nowX][nowY] = nowD;
+			for(int i=0;i<4;i++) {
+				int newX = nowX + nX[i];
+				int newY = nowY + nY[i];
+				if(newX >= 0 && newX < n && newY >=0 && newY < n) {
+					int flag = matrix[newX][newY] < 1 ? 1 : 0;
+					if(history[newX][newY] < nowD + flag) continue;
+					history[newX][newY] = nowD + flag;
+					pq.add(new Node(newX, newY, nowD + flag));
+				}
+			}
+		}
+		for(int[]a: history) {
+			System.out.println(Arrays.toString(a));
+		}
+	}
+	static class Node implements Comparable<Node> {
+		int x;
+		int y;
+		int dis;
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.dis, o.dis);
+		}
+		Node(int x, int y, int dis) {
+			this.x = x;
+			this.y = y;
+			this.dis = dis;
+		}
+	}
 }
