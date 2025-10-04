a7c9b87d4aa75b4227976f7292d5ba1f5ad868bf
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-04 16:16:57 +0900

    BOJ(Graph): 1507

--- src/BaekJoon/Dijkstra/P1507.java
+++ src/BaekJoon/Dijkstra/P1507.java
@@ -1,4 +1,21 @@
 package BaekJoon.Dijkstra;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P1507 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[][] matrix = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
 }
