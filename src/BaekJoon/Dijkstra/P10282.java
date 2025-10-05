fc4f9c9e68fc81bf57cc894b6648d5ac055e164b
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-06 00:28:48 +0900

    BOJ(Dijkstra): 10282

--- src/BaekJoon/Dijkstra/P10282.java
+++ src/BaekJoon/Dijkstra/P10282.java
@@ -5,14 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class P10282 {
+	static class Edge {
+		int to, w;
+		Edge(int to, int w) { this.to = to; this.w = w; }
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int nx = Integer.parseInt(br.readLine());
@@ -21,50 +23,51 @@ public class P10282 {
 			int n = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			HashMap<Integer, Integer>[] matrix = new HashMap[n+1];
+
+			@SuppressWarnings("unchecked")
+			List<Edge>[] matrix = new ArrayList[n + 1];
+			for (int i = 0; i <= n; i++) matrix[i] = new ArrayList<>();
+
 			for (int i = 0; i < d; i++) {
 				st = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
 				int dis = Integer.parseInt(st.nextToken());
-				if (matrix[end] == null) matrix[end] = new HashMap<>();
-				matrix[end].put(dis, start);
+				// end -> start (b가 감염되면 a가 s초 뒤 감염)
+				matrix[end].add(new Edge(start, dis));
 			}
-			int[] history = new int[n+1];
+
+			int[] history = new int[n + 1];
 			Arrays.fill(history, Integer.MAX_VALUE);
-			PriorityQueue<Node> pq = new PriorityQueue<>();
-			pq.add(new Node(c,0));
+
+			// PQ<int[]>: [0]=정점, [1]=누적거리 (오름차순)
+			PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+			pq.add(new int[]{c, 0});
+
 			while (!pq.isEmpty()) {
-				Node cur = pq.poll();
-				int nowIdx = cur.target;
-				int nowDis = cur.dis;
-				if (history[nowIdx] < nowDis || matrix[nowIdx] == null) continue;
-				history[nowIdx] = cur.dis;
-				for(Map.Entry<Integer, Integer> entry : matrix[nowIdx].entrySet()) {
-					int nextDis = entry.getKey();
-					int nextIdx = entry.getValue();
-					if (history[nextIdx] < nowDis + nextDis) continue;
-					history[nextIdx] = nowDis + nextDis;
-					pq.add(new Node(nextIdx,nextDis));
+				int[] cur = pq.poll();
+				int nowIdx = cur[0];
+				int nowDis = cur[1];
+				if (history[nowIdx] < nowDis) continue;
+				history[nowIdx] = nowDis;
+
+				for (Edge e : matrix[nowIdx]) {
+					int nextIdx = e.to;
+					int nd = nowDis + e.w;
+					if (history[nextIdx] <= nd) continue;
+					history[nextIdx] = nd;
+					pq.add(new int[]{nextIdx, nd});
 				}
 			}
-			// System.out.println(Arrays.toString(history));
-			// System.out.println(Arrays.stream(history).filter(x -> x != Integer.MAX_VALUE).count() + " " + Arrays.stream(history).filter(x -> x != Integer.MAX_VALUE).max().getAsInt());
-			long count = Arrays.stream(history).filter(x -> x != Integer.MAX_VALUE).count();
-			int maxVal = Arrays.stream(history).filter(x -> x != Integer.MAX_VALUE).max().getAsInt();
-			System.out.println(count + " " + maxVal);
-		}
-	}
-	static class Node implements Comparable<Node>{
-		int target;
-		int dis;
-		Node(int target, int dis) {
-			this.target = target;
-			this.dis = dis;
-		}
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.dis, o.dis);
+
+			int count = 0, max = 0;
+			for (int val : history) {
+				if (val != Integer.MAX_VALUE) {
+					count++;
+					if (val > max) max = val;
+				}
+			}
+			System.out.println(count + " " + max);
 		}
 	}
 }
