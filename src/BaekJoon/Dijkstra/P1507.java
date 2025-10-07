ea01b6317a062aee5560195e5f8a1e2f44948d72
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-08 00:41:54 +0900

    BOJ(Dijkstra): 1507

--- src/BaekJoon/Dijkstra/P1507.java
+++ src/BaekJoon/Dijkstra/P1507.java
@@ -3,10 +3,16 @@ package BaekJoon.Dijkstra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class P1507 {
 	public static void main(String[] args) throws IOException {
+		// HashMap<Integer, Integer> map = new HashMap<>();
+		// System.out.println(map.put(0, 0));
+		// System.out.println(map.put(2, 0));
+		// System.out.println(map.get(0));
+		// System.out.println(map.entrySet());
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st;
