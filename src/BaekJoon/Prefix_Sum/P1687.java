e848635aad3fa354ee30a774ef2d8d946bbdea23
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-06 01:04:10 +0900

    BOJ(PS): 1687

--- src/BaekJoon/Prefix_Sum/P1687.java
+++ src/BaekJoon/Prefix_Sum/P1687.java
@@ -1,4 +1,52 @@
 package BaekJoon.Prefix_Sum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P1687 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] prefixSum = new int[N + 1][M + 1];
+        for (int i = 0; i < N; i++) {
+            char[] c = br.readLine().toCharArray();
+
+            for (int j = 0; j < M; j++) {
+                prefixSum[i + 1][j + 1] = (c[j] - '0')
+                        + prefixSum[i][j + 1] + prefixSum[i + 1][j]
+                        - prefixSum[i][j];
+            }
+        }
+
+        int answer = 0;
+        for (int startCol = 1; startCol <= M; startCol++) {
+            for (int endCol = 1; endCol <= M; endCol++) {
+                int startRow = 1;
+                int prePrefixSum = 0;
+
+                for (int endRow = 1; endRow <= N; endRow++) {
+                    int culPrefixSum = prefixSum[endRow][endCol] - prefixSum[endRow][startCol - 1];
+
+                    if (culPrefixSum == prePrefixSum) {
+                        answer = Math.max(answer, (endCol - startCol + 1) * (endRow - startRow + 1));
+                    }
+
+                    if (culPrefixSum > prePrefixSum) {
+                        startRow = endRow + 1;
+                    }
+
+                    prePrefixSum = culPrefixSum;
+                }
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
 }
