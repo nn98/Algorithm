1bcd37842c493025dce0de6c806415335b1f0287
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-07 21:05:19 +0900

    BOJ(LIS): 11054 - 감소수열은 역순으로, 양 수열 값 비교해서 최대값 계산

--- src/BaekJoon/LIS/P11054.py
+++ src/BaekJoon/LIS/P11054.py
@@ -9,21 +9,37 @@ dp_asc = [0] * n
 dp_desc = [0] * n
 
 dp_asc[0] = 1
-dp_desc[0] = 1
+dp_desc[n-1] = 1
 
 for i in range(1, n):
     j = i
     asc_max_val = 0
-    desc_max_val = 0
     while j > 0:
         j -= 1
         if arr[j] < arr[i]:
             asc_max_val = max(asc_max_val, dp_asc[j])
-        if arr[j] > arr[i]:
-            desc_max_val = max(desc_max_val, dp_desc[j])
-
     dp_asc[i] = asc_max_val + 1
+
+for i in range(n-2, -1, -1):
+    j = i
+    desc_max_val = 0
+    while j < n-1:
+        j += 1
+        if arr[j] < arr[i]:
+            desc_max_val = max(desc_max_val, dp_desc[j])
     dp_desc[i] = desc_max_val + 1
 
-print(dp_asc)
-print(dp_desc)
+# print(dp_asc)
+# print(dp_desc)
+
+answer = 0
+
+for i in range(n):
+    left = dp_asc[i]
+    right = 0
+    for j in range(i, n):
+        if arr[i] > arr [j]:
+            right = max(right, dp_desc[j])
+    answer = max(answer, left + right)
+
+print(answer)
