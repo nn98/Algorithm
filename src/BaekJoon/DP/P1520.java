4585e385fb95d1b55c6b2fb2c390936ad4e62a9d
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-10 22:32:52 +0900

    BOJ(DP): 1520

--- src/BaekJoon/DP/P1520.java
+++ src/BaekJoon/DP/P1520.java
@@ -1,4 +1,53 @@
 package BaekJoon.DP;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class P1520 {
+    static int n,m;
+    static int[][] map, dp;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        dp = new int[n][m];
+
+        for (int i = 0; i<n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println(dfs(0,0));
+    }
+    static int dfs(int x, int y) {
+        if(x == n-1 && y == m-1) return 1;
+        if (dp[x][y] != -1) return dp[x][y];
+
+        int pos = map[x][y];
+        dp[x][y] = 0;
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx <0 || nx > n-1 || ny < 0 || ny > m-1) continue;
+
+            int nxt = map[nx][ny];
+            if (pos > nxt) {
+                dp[x][y] += dfs(nx, ny);
+            }
+        }
+        return dp[x][y];
+    }
 }
