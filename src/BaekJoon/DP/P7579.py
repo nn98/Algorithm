0889acb2a826681c9dfab8c9f4ab84eb36d3badb
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-12 07:06:38 +0900

    BOJ(DP): 7579 - https://www.youtube.com/watch?v=rhda6lR5kyQ&t=259s

--- src/BaekJoon/DP/P7579.py
+++ src/BaekJoon/DP/P7579.py
@@ -5,30 +5,21 @@ readline = sys.stdin.readline
 n, m = map(int, readline().split())
 
 memory = list(map(int, readline().split()))
-disable = list(map(int, readline().split()))
+cost = list(map(int, readline().split()))
 
-arr = []
+max_cost = sum(cost)
 
-for mem, dis in zip(memory, disable):
-    arr.append((mem, dis))
+dp = [[0] * (max_cost + 1) for _ in range(n + 1)]
 
 answer = 1e9
 
-dp = [0] * (sum(disable) + 1)
-
-def sol(index, resource, mem):
-    if mem < dp[resource]:
-        return
-    dp[resource] = mem
-    global answer
-    if mem >= m:
-        answer = min(answer, resource)
-        return
-    if index == n:
-        return
-    sol(index + 1, resource, mem)
-    sol(index + 1, resource + disable[index], mem + memory[index])
-
-sol(0, 0, 0)
+for i in range(1, n + 1):
+    for j in range(1, max_cost + 1):
+        if cost[i-1] > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-cost[i-1]] + memory[i-1], dp[i-1][j])
+        if dp[i][j] >= m:
+            answer = min(answer, j)
 
 print(answer)
