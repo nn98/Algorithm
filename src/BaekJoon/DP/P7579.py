16f3d881b7299a59dc74189affc5ef9e38015b8a
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-12 06:27:39 +0900

    BOJ(DP): 7579

--- src/BaekJoon/DP/P7579.py
+++ src/BaekJoon/DP/P7579.py
@@ -12,15 +12,11 @@ arr = []
 for mem, dis in zip(memory, disable):
     arr.append((mem, dis))
 
-mem_arr = sorted(arr, key=lambda x: x[0])
-dis_arr = sorted(arr, key=lambda x: x[1])
-
 answer = 1e9
 
 dp = [0] * (sum(disable) + 1)
 
 def sol(index, resource, mem):
-    print(index, resource, mem)
     if mem < dp[resource]:
         return
     dp[resource] = mem
@@ -30,10 +26,8 @@ def sol(index, resource, mem):
         return
     if index == n:
         return
-    if dis_arr[index][1] != 0 and dp[resource] < mem:
-        sol(index + 1, resource, mem)
-    if dp[resource + dis_arr[index][1]] < mem + dis_arr[index][0]:
-        sol(index + 1, resource + dis_arr[index][1], mem + dis_arr[index][0])
+    sol(index + 1, resource, mem)
+    sol(index + 1, resource + disable[index], mem + memory[index])
 
 sol(0, 0, 0)
 
