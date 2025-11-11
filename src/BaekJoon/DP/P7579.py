2e36e843da6091f436b009d89b69ee5704bfa95e
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-12 06:25:52 +0900

    BOJ(DP): 7579

--- src/BaekJoon/DP/P7579.py
+++ src/BaekJoon/DP/P7579.py
@@ -17,17 +17,23 @@ dis_arr = sorted(arr, key=lambda x: x[1])
 
 answer = 1e9
 
+dp = [0] * (sum(disable) + 1)
+
 def sol(index, resource, mem):
     print(index, resource, mem)
+    if mem < dp[resource]:
+        return
+    dp[resource] = mem
     global answer
     if mem >= m:
         answer = min(answer, resource)
         return
     if index == n:
         return
-    if dis_arr[index][1] != 0:
+    if dis_arr[index][1] != 0 and dp[resource] < mem:
         sol(index + 1, resource, mem)
-    sol(index + 1, resource + dis_arr[index][1], mem + dis_arr[index][0])
+    if dp[resource + dis_arr[index][1]] < mem + dis_arr[index][0]:
+        sol(index + 1, resource + dis_arr[index][1], mem + dis_arr[index][0])
 
 sol(0, 0, 0)
 
