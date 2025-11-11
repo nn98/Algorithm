6d8b2fafc250268261a2d4bd4c09da1063beb6e0
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-12 06:05:03 +0900

    BOJ(DP): 7579

---
+++ src/BaekJoon/DP/P7579.py
@@ -0,0 +1,34 @@
+import sys
+
+readline = sys.stdin.readline
+
+n, m = map(int, readline().split())
+
+memory = list(map(int, readline().split()))
+disable = list(map(int, readline().split()))
+
+arr = []
+
+for mem, dis in zip(memory, disable):
+    arr.append((mem, dis))
+
+mem_arr = sorted(arr, key=lambda x: x[0])
+dis_arr = sorted(arr, key=lambda x: x[1])
+
+answer = 1e9
+
+def sol(index, resource, mem):
+    print(index, resource, mem)
+    global answer
+    if mem >= m:
+        answer = min(answer, resource)
+        return
+    if index == n:
+        return
+    if dis_arr[index][1] != 0:
+        sol(index + 1, resource, mem)
+    sol(index + 1, resource + dis_arr[index][1], mem + dis_arr[index][0])
+
+sol(0, 0, 0)
+
+print(answer)
