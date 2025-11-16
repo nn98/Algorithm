a80e4e37c9314907208374ea17595adf34ecf4ec
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-16 18:56:41 +0900

    BOJ(Math): 26148

--- src/BaekJoon/Math/P26148.java
+++ src/BaekJoon/Math/P26148.java
@@ -1,4 +1,21 @@
 package BaekJoon.Math;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class P26148 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int N = Integer.parseInt(br.readLine());
+
+        int res = ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0) ? 30 : 29;
+
+        System.out.println(res);
+
+    }
+
 }
