c1d393a437244ccf00c5d3c74c587aab12896e74
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-19 22:16:17 +0900

    BOJ(PS): 5874

--- src/BaekJoon/Prefix_Sum/P5874.java
+++ src/BaekJoon/Prefix_Sum/P5874.java
@@ -1,4 +1,24 @@
 package BaekJoon.Prefix_Sum;
 
+import java.util.Scanner;
+
 public class P5874 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String[] line = input.nextLine().split("");
+            int count = 0;
+            int sum = 0;
+            for (int i = 1; i < line.length; i++) {
+                if (line[i].compareTo(line[i-1]) == 0) {
+                    if (line[i].compareTo("(") == 0)
+                        count++;
+                    else
+                        sum += count;
+                }
+            }
+            System.out.println(sum);
+        }
+        input.close();
+    }
 }
