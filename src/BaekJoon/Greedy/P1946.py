ac3c8f8e2a4e921f9225851077c72abab213eb2e
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-27 00:09:32 +0900

    BOJ(Greedy): 1946

--- src/BaekJoon/Greedy/P1946.py
+++ src/BaekJoon/Greedy/P1946.py
@@ -2,17 +2,20 @@ import sys
 readline = sys.stdin.readline
 
 T = int(readline())
-
 while T > 0:
-
     N = int(readline())
     applied = []
     for i in range(N):
         paper, interview = map(int, readline().split())
         applied.append((paper, interview))
+
     applied.sort(key = lambda x:x[0])
-    paper_standard = applied[0][0]
+    min_interview = applied[0][1]
+    result = 1
+    for paper, interview in applied:
+        if interview < min_interview:
+            result += 1
+            min_interview = interview
 
-
-
+    print(result)
     T -= 1
