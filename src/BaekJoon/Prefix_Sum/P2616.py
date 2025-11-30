c9ab6bdefb2784540537a62c5cefc660e972f031
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-30 23:51:59 +0900

    BOJ(PS): 2616

--- src/BaekJoon/Prefix_Sum/P2616.py
+++ src/BaekJoon/Prefix_Sum/P2616.py
@@ -5,4 +5,15 @@ N = int(readline())
 carriage = list(map(int,readline().split()))
 M = int(readline())
 
+head = 0
+pre_sum = [0] * N
+pre_sum[0] = carriage[0]
+for i in range(1, N):
+    for j in range(0, M):
+        idx = i - j
+        if idx < 0:
+            break
+        pre_sum[idx] += carriage[i]
+
 print(carriage)
+print(pre_sum)
