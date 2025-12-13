e2136439c29992081150da48415bff31043dfc54
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-12-14 00:13:11 +0900

    BOJ(Sorting): 10431

--- src/BaekJoon/Sorting/P10431.java
+++ src/BaekJoon/Sorting/P10431.java
@@ -1,4 +1,35 @@
 package BaekJoon.Sorting;
 
+import java.io.*;
+import java.util.*;
+
 public class P10431 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int t = Integer.parseInt(st.nextToken());
+        int[] arr = new int[20];
+        
+        for (int i = 0; i < t; i++) {
+            int cnt = 0;
+            
+            st = new StringTokenizer(br.readLine());
+            int testNumber = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 20; j++) {
+                arr[j]= Integer.parseInt(st.nextToken());
+            }
+            
+            for (int k = 0; k < 20; k++) {
+                for (int l = 0; l < k; l++) {
+                    if(arr[k] < arr[l]){
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(testNumber+" " +cnt);
+        }
+    }
 }
